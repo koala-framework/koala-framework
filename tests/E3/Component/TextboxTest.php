73c7f40d2c769ebf5982806d1d674b8f8657d7c5
Niko Sams <ns@vivid-planet.com>
2007-03-20 17:19:39 +0000

    test aktuallisiert, id wird mitgeliefert

--- tests/E3/Component/TextboxTest.php
+++ tests/E3/Component/TextboxTest.php
@@ -14,19 +14,22 @@ class E3_Component_TextboxTest extends E3_Test
         $textbox = new E3_Component_Textbox(1, $this->_dao);
         $templateVars = $textbox->getTemplateVars();
         $this->assertEquals(array('content'=>'inhalt von home',
-                                  'template'=>'Textbox.html'),
+                                  'template'=>'Textbox.html',
+                                  'id'=>1),
                                   $templateVars);
         
         $textbox = new E3_Component_Textbox(-1, $this->_dao);
         $templateVars = $textbox->getTemplateVars();
         $this->assertEquals(array('content'=>null,
-                                  'template'=>'Textbox.html'),
+                                  'template'=>'Textbox.html',
+                                  'id'=>-1),
                                   $templateVars);
 
         $db->query("INSERT INTO component_textbox SET component_id=-1, content='unit-test-content'");
         $templateVars = $textbox->getTemplateVars();
         $this->assertEquals(array('content'=>'unit-test-content',
-                                  'template'=>'Textbox.html'),
+                                  'template'=>'Textbox.html',
+                                  'id'=>-1),
                                   $templateVars);
     }
 }
