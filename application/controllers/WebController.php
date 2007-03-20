4c9245cc632f53d0eac7e4fe6a9b78841fcfadfd
Franz Unger <ufx@vivid-planet.com>
2007-03-20 16:35:10 +0000

    Frontend Editing: erste Version hardcodiert

--- application/controllers/WebController.php
+++ application/controllers/WebController.php
@@ -5,26 +5,25 @@ class WebController extends Zend_Controller_Action
     protected $_pageCollection;
     public function indexAction()
     {
-        echo "WebController::indexAction()<br />";
-        $component = $this->getTemplateVars();
-        
-        $view = new E3_View_Smarty('../application/views',
-                        array('compile_dir'=>'../application/views_c'));
-        $view->assign('component', $component);
-        $body = $view->render('master/default.html');
-
-        $response = $this->getResponse();
-        if ($response->canSendHeaders()) {
-        	$response->setHeader('Content-Type', 'text/html');
-        }
-        $response->appendBody($body);
-        return $response;        
+		$dao = $this->createDao();
+        $pageCollectionConfig = new Zend_Config_Ini('../application/config.ini', 'pagecollection');
+        $pageCollection = new $pageCollectionConfig->pagecollection->type($dao);
+        $page = $pageCollection->getPageByPath($this->getRequest()->getPathInfo());
+        $this->renderPage($page);
     }
 
     public function ajaxAction()
     {
         echo "WebController::ajaxAction()<br />";
-        p($this->getTemplateVars());
+    }
+
+    public function frontendEditingAction()
+    {
+        $componentId = $this->getRequest()->getQuery("componentId");
+        if (!is_null($componentId)) {
+	        $component = E3_Component_Abstract::createComponent($this->createDao(), $componentId);
+	        $this->renderPage($component, true);
+        }
     }
 
     public function filesAction()
@@ -32,23 +31,32 @@ class WebController extends Zend_Controller_Action
         echo "WebController::filesAction()<br />";
     }
     
-    private function getTemplateVars()
+    private function createDao()
     {
-        $return = array();
-
-        $pageCollectionConfig = new Zend_Config_Ini('../application/config.ini', 'pagecollection');
         $dbConfig = new Zend_Config_Ini('../application/config.db.ini', 'web');
         $dbConfig = $dbConfig->database->asArray();
         $db = Zend_Db::factory('PDO_MYSQL', $dbConfig);
-        $dao = new E3_Dao($db);
-
-        $pageCollection = new $pageCollectionConfig->pagecollection->type($dao);
-        $page = $pageCollection->getPageByPath($this->getRequest()->getPathInfo());
-        if ($page != null) {
-           	$return = $page->getTemplateVars();
+        return new E3_Dao($db);
+    }
+    
+    private function renderPage($page, $usePageTemplate = false)
+    {
+        $templateVars = $page->getTemplateVars();
+        $view = new E3_View_Smarty('../application/views',
+                        array('compile_dir'=>'../application/views_c'));
+        $view->assign('component', $templateVars);
+        if ($usePageTemplate) {
+        	$body = $view->render($templateVars['template']);
+        } else {
+        	$body = $view->render('master/default.html');
         }
 
-        return $return;
+        $response = $this->getResponse();
+        if ($response->canSendHeaders()) {
+        	$response->setHeader('Content-Type', 'text/html');
+        }
+        $response->appendBody($body);
+        return $response;        
     }
 
 }
