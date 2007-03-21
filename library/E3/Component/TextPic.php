eb2f802dcd3957813c06358551a0894025867537
Niko Sams <ns@vivid-planet.com>
2007-03-21 08:25:04 +0000

    - manuelles menü - componente Pic - componente Text+Pic

---
+++ library/E3/Component/TextPic.php
@@ -0,0 +1,21 @@
+<?php
+class E3_Component_TextPic extends E3_Component_Abstract
+{
+    private $_textboxComponent;
+    private $_picComponent;
+
+    public function getTemplateVars()
+    {
+        $this->_paragraphs = array();
+        $componentKey = $this->getComponentKey();
+        if($componentKey!='') $componentKey .= ".";
+
+        $this->_textboxComponent = new E3_Component_Textbox($this->_dao, $this->getComponentId(), '', $componentKey.'1');
+        $this->_picComponent = new E3_Component_Pic($this->_dao, $this->getComponentId(), '', $componentKey.'1');
+
+        $ret['textbox'] = $this->_textboxComponent->getTemplateVars();
+        $ret['pic'] = $this->_picComponent->getTemplateVars();
+        $ret['template'] = 'TextPic.html';
+        return $ret;
+    }
+}
