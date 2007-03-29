b7682a9e2c52f6ccd6b19c4d29004e9e2797856a
Niko Sams <ns@vivid-planet.com>
2007-03-29 12:18:48 +0000

    frontend-editing für file-uploads

--- library/E3/Component/Abstract.js
+++ library/E3/Component/Abstract.js
@@ -7,15 +7,25 @@ E3.Component.Abstract = function(componentId, componentClass) {
   this.componentClass = componentClass;
   this.htmlelement = document.getElementById('container_' + this.componentId);
   this.el = new YAHOO.util.Element(this.htmlelement);
-  this.success = this.handleSuccess;
-  this.failure = this.handleFailure;
   this.init();
 };
 
 E3.Component.Abstract.prototype.init = function() {
-  this.el.on('mouseover', this.handleMouseOver, this.el);
-  this.el.on('mouseout', this.handleMouseOut, this.el);
-  this.el.on('click', this.handleClick, this);
+  this.editButton = new YAHOO.util.Element(document.createElement('a'));
+  this.editButton.set('innerHTML', 'edit...');
+  this.editButton.set('href', 'javascript:void(0)');
+  this.editButton.setStyle('position', 'absolute');
+  this.editButton.setStyle('backgroundColor', 'white');
+  this.editButton.setStyle('left', '0');
+  this.editButton.setStyle('border', '1px solid black');
+  this.editButton.setStyle('padding', '2px');
+  this.editButton.setStyle('display', 'none');
+
+  this.el.insertBefore(this.editButton, this.el.get('firstChild'));
+
+  this.el.on('mouseover', this.handleMouseOver, this);
+  this.el.on('mouseout', this.handleMouseOut, this);
+  this.editButton.on('click', this.handleClick, this);
 };
 
 E3.Component.Abstract.prototype.handleSuccess = function(o) {
@@ -25,13 +35,11 @@ E3.Component.Abstract.prototype.handleSuccess = function(o) {
 
 E3.Component.Abstract.prototype.handleEditSuccess = function(o)
 {
-    this.htmlelement.innerHTML = o.responseText;
+  this.htmlelement.innerHTML = o.responseText;
 
   this.el.removeListener('mouseover', this.el.handleMouseOver);  
   this.el.removeListener('mouseout', this.el.handleMouseOut);  
-  this.el.removeListener('click', this.el.handleClick);  
 
-  this.el.setStyle('border', '');
   var saveButton = new YAHOO.widget.Button({
                                           label: "Save", 
                                           container: this.el
@@ -48,27 +56,27 @@ E3.Component.Abstract.prototype.handleFailure = function(o) {
   alert('failure');
 };
 
-E3.Component.Abstract.prototype.handleMouseOver = function(o, el) { 
-  el.setStyle('border', '1px solid black');
+E3.Component.Abstract.prototype.handleMouseOver = function(o, scope) { 
+    scope.editButton.setStyle('display', 'block');
 };
 
-E3.Component.Abstract.prototype.handleMouseOut = function(o, el) { 
-  el.setStyle('border', '');
+E3.Component.Abstract.prototype.handleMouseOut = function(o, scope) { 
+    scope.editButton.setStyle('display', 'none');
 };
 
 E3.Component.Abstract.prototype.handleSave = function() {
-    var inputs = this.htmlelement.getElementsByTagName('input');
-    var data = '';
-    for(var i=0;i<inputs.length;i++) {
-        data += '&'+encodeURIComponent(inputs[i].name)+'='+encodeURIComponent(inputs[i].value);
-    }
-  YAHOO.util.Connect.asyncRequest('post', '/ajax/fe/save?componentId='+this.componentId+'&componentClass='+this.componentClass, this, data);
+    var form = this.htmlelement.getElementsByTagName('form')[0];
+    YAHOO.util.Connect.setForm(form);
+    YAHOO.util.Connect.asyncRequest('POST', '/ajax/fe/save?componentId='+this.componentId+'&componentClass='+this.componentClass,
+            {success: this.handleSuccess, failure: this.handleFailure, scope: this});
 };
 
 E3.Component.Abstract.prototype.handleCancel = function() { 
-  var connectionObject = YAHOO.util.Connect.asyncRequest('get', '/ajax/fe/cancel?componentId='+this.componentId+'&componentClass='+this.componentClass, this);
+  var connectionObject = YAHOO.util.Connect.asyncRequest('get', '/ajax/fe/cancel?componentId='+this.componentId+'&componentClass='+this.componentClass,
+    {success: this.handleSuccess, failure: this.handleFailure, scope: this});
 };
 
 E3.Component.Abstract.prototype.handleClick = function(o, e) {
-  YAHOO.util.Connect.asyncRequest('get', '/ajax/fe/edit?componentId='+e.componentId+'&componentClass='+e.componentClass, {success: e.handleEditSuccess, failure: e.handleFailure, scope: e});
+  YAHOO.util.Connect.asyncRequest('get', '/ajax/fe/edit?componentId='+e.componentId+'&componentClass='+e.componentClass,
+    {success: e.handleEditSuccess, failure: e.handleFailure, scope: e});
 };
--- library/E3/Component/Pic.php
+++ library/E3/Component/Pic.php
@@ -5,8 +5,19 @@ class E3_Component_Pic extends E3_Component_Abstract
     {
         $ret = parent::getTemplateVars($mode);
         $ret['pic'] = 'files/pics/'.$this->getId().'.jpg';
-       	$ret['template'] = 'Pic.html';
+        if(!file_exists($ret['pic'])) {
+            $ret['pic'] = false;
+        }
+        if ($mode == 'edit') {
+            $ret['template'] = dirname(__FILE__).'/Pic.html';
+        } else {
+       	    $ret['template'] = 'Pic.html';
+       	}
 
         return $ret;
+    }
+    public function saveFrontendEditing()
+    {
+        move_uploaded_file($_FILES['content']['tmp_name'], 'files/pics/'.$this->getId().'.jpg');
     }
 }
