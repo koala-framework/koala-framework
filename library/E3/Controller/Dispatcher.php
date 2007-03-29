ce3e58dc0a976711f8b78728442acc90380d7c4c
Franz Unger <ufx@vivid-planet.com>
2007-03-29 13:24:38 +0000

    - Neuen Dispatcher nicht eingecheckt - Admin Panel wird jetzt in preDispatch geladen

---
+++ library/E3/Controller/Dispatcher.php
@@ -0,0 +1,37 @@
+<?php
+class E3_Controller_Dispatcher extends Zend_Controller_Dispatcher_Standard
+{
+    private $_isOverwritten = null;
+    
+    public function formatControllerName($unformatted)
+    {
+        if ($this->isOverwritten()) {
+            return parent::formatControllerName($unformatted); 
+        } else {
+            $unformatted = ucfirst($unformatted);
+            return "E3_Controller_Action_$unformatted";
+        }
+    }
+
+    public function getControllerDirectory($module = null)
+    {
+        if ($this->isOverwritten()) {
+            return parent::getControllerDirectory();
+        } else {
+            return array('default' => '../library');
+        }
+    }
+    
+    private function isOverwritten()
+    {
+        if (is_null($this->_isOverwritten)) {
+            $frontController = $this->getFrontController();
+            $request = $frontController->getRequest();
+            $controllerDir = $frontController->getControllerDirectory();
+            $controllerFile = $controllerDir['default'] . '/' . $this->classToFilename(parent::formatControllerName($request->getControllerName()));
+            $this->_isOverwritten = is_file($controllerFile);
+        }
+        return $this->_isOverwritten;
+    }
+}
+?>
