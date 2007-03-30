17d9763b3a9318330a7fece961a44d811c60181b
Niko Sams <ns@vivid-planet.com>
2007-03-30 13:43:00 +0000

    - frontend-editing für produkt-details

--- library/E3/Component/Textbox.js
+++ library/E3/Component/Textbox.js
@@ -3,13 +3,4 @@ E3.Component.Textbox = function(componentId, componentClass) {
 };
 YAHOO.lang.extend(E3.Component.Textbox, E3.Component.Abstract);
 
-E3.Component.Textbox.prototype.handleSuccess = function(o) {
-    E3.Component.Textbox.superclass.handleSuccess.call(this, o);
-    var resp = eval('(' + o.responseText + ')');
-    for(var id in resp.createComponents) {
-        var className = resp.createComponents[id].replace(/_/g, '.');
-        eval('new '+className+'(\''+id+'\', \''+className+'\');');
-    }
-};
-
 
