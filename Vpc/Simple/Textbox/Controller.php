3cf523086fc3b0c56d4840e3647de8545f87f6ac
Franz Unger <ufx@vivid-planet.com>
2007-06-15 12:37:43 +0000

    Auch admin/page und Komponenten an ViewRenderer angepasst

--- Vpc/Simple/Textbox/Controller.php
+++ Vpc/Simple/Textbox/Controller.php
@@ -10,8 +10,7 @@ class Vpc_Simple_Textbox_Controller extends Vpc_Controller
     public function ajaxSaveDataAction()
     {
         $content = $this->getRequest()->getParam('content');
-        $data['success'] = $this->_createComponent()->saveContent($content);
-        $this->getResponse()->setBody(Zend_Json::encode($data));
+        $this->view->success = $this->_createComponent()->saveContent($content);
     }
     
 }
