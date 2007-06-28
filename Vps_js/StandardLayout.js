2425b6a70a4990e401736d58d404e03193aceaf7
Niko Sams <ns@vivid-planet.com>
2007-06-28 12:54:37 +0000

    StandardLayout in vps verschoben

---
+++ Vps_js/StandardLayout.js
@@ -0,0 +1,27 @@
+Vps.StandardLayout = function(renderTo, config)
+{
+    if(!config) config = {};
+
+    Ext.applyIf(config, {
+            north: {split: false, initialSize: 30},
+            center: { autoScroll: false },
+            createWorkspaceContainer: true
+        });
+    Vps.StandardLayout.superclass.constructor.call(this, renderTo, config);
+    this.beginUpdate();
+
+    this.add('north', new Ext.ContentPanel('menusContainer', {autoCreate: true, fitToFrame:true, closable:false}));
+    new Vps.Menu.Index('menusContainer');
+    
+    if (config.createWorkspaceContainer) {
+        this.add('center', new Ext.ContentPanel('workspaceContainer', {autoCreate: true, fitToFrame:true, closable:false}));
+    }
+
+    this.endUpdate();
+
+};
+
+Ext.extend(Vps.StandardLayout, Ext.BorderLayout,
+{
+
+});
