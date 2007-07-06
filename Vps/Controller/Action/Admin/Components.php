0a0d1c66a5ef72e9088b863d1520e1bfb680333a
Franz Unger <ufx@vivid-planet.com>
2007-07-06 06:34:44 +0000

    VPC-Setup ini INI-Dateien - Anfang

--- Vps/Controller/Action/Admin/Components.php
+++ Vps/Controller/Action/Admin/Components.php
@@ -1,30 +1,10 @@
 <?php
-class Vps_Controller_Action_Admin_Components extends Vps_Controller_Action_AutoGrid
+class Vps_Controller_Action_Admin_Components extends Vps_Controller_Action
 {
-    protected $_gridColumns = array(
-        array('dataIndex' => 'component_class',
-              'header'    => 'Komponente',
-              'width'     => 250,
-              'editor'    => 'TextField'),
-        array('dataIndex' => 'name',
-              'header'    => 'Bezeichnung',
-              'width'     => 200,
-              'editor'    => 'TextField')
-    );
-    protected $_gridButtons = array('delete' => true, 'add' => true, 'save' => true);
-    protected $_gridFilters = array();
-    protected $_gridDefaultOrder = 'name';
-    protected $_gridTableName = 'Vps_Dao_Vpc';
-    
     public function actionAction()
     {
-        $config['controllerUrl'] = '/admin/components/';
-        $this->view->ext('Vps.Admin.Components.Index', $config);
-    }
-    
-    public function jsonIndexAction()
-    {
-        $this->actionAction();
+        $table = new Vps_Dao_Components();
+        $components = $table->getAvailableComponents();
     }
 
 }
