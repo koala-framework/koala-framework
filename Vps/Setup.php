99ae3e87a9070d4e0d4537b140f122a518f12a8b
Franz Unger <ufx@vivid-planet.com>
2007-07-11 09:00:25 +0000

    Bug in Auswertung von config.ini bei Abprüfen ob von /www/public zugegriffen wird

--- Vps/Setup.php
+++ Vps/Setup.php
@@ -6,7 +6,7 @@ class Vps_Setup
         require_once 'Zend/Config/Ini.php';
         if (preg_match('#/www/usr/([0-9a-z]+)/#', $_SERVER['SCRIPT_FILENAME'], $m)) {
             $user = $m[1];
-        } else if (substr(__FILE__, strlen('/www/public/')) == '/www/public/') {
+        } else if (substr(__FILE__, 0, strlen('/www/public/')) == '/www/public/') {
             $user = 'vivid';
         } else {
             $user = 'production';
