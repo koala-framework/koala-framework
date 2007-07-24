520a5f0368437cb18ee2dae7d4f7ac89db93b4d0
Lorenz Maislinger <lm@vivid-planet.com>
2007-07-24 09:09:24 +0000

    - Formularkomponenten

--- Vpc/Formular/Select/Index.php
+++ Vpc/Formular/Select/Index.php
@@ -1,36 +1,152 @@
 <?php
-class Vpc_Formular_Select_Index extends Vpc_Formular_Field_Decide_Abstract
+class Vpc_Formular_Multicheckbox_Index extends Vpc_Formular_Field_Abstract
 {
-    protected $_defaultSettings = array('rows' => '10', 'name' => '');
-    protected $_options = array();
+    
+    protected $_defaultSettings = array('multicheckbox' => array(), 'name' => '', 'horiztontal' => 0);
     
     public function getTemplateVars($mode)
     {
-        $rows = $this->getSetting('rows');
-        $name = $this->getSetting('name');
-
-        $return['rows'] = $rows;
-        $return['name'] = $name;
-        if ($this->_options == null) $this->getOptions();
-		
-        $return['options'] = $this->_options;
+        $this->start();
+        
+        $return['horizontal'] = $this->getSetting('horizontal');
+        $return['multicheckbox'] = $this->getSetting('multicheckbox');
         $return['id'] = $this->getComponentId();
-		
-        $return['template'] = 'Formular/Select.html';
+        
+        $return['template'] = 'Formular/Multicheckbox.html';
         return $return;
     }
-        
-    public function getOptions ()
+    
+    
+    //erstellt Checkboxen mittels der Component Id's
+    private function _setMulitcheckboxbyIds()
     {
-        $table = $this->_getTable('Vpc_Formular_Select_OptionsModel');
-        $select = $table->fetchAll(array('component_id = ?'  => $this->getComponentId(),
-                                             'page_key = ?'      => $this->getPageKey(),
-                                             'component_key = ?' => $this->getComponentKey()));        
-        //values werden rausgeschrieben
-        $values = array();
-        foreach ($select as $option) {
-            $this->_options[] = array('value' => $option->value, 'text' => $option->value, 'selected' => $option->selected, 'id' => $option->id);
-        } 
-       
+        //ComponentId, PageKey und ComponentKey werden aus der Datenbank geholt
+        $table = $this->_getTable('Vpc_Formular_Multicheckbox_CheckboxesModel');
+        $checkboxes = $table->fetchAll(array('component_id = ?'  => $this->getComponentId(),
+        'page_key = ?'      => $this->getPageKey(),
+        'component_key = ?' => $this->getComponentKey()));
+        //ids werden rausgeschrieben
+        $ids = array();
+        foreach($checkboxes as $checkbox) {
+            $ids[] = $checkbox->id;
+        }
+        
+        //instanzen der Komponentenwerden erzeugt
+        foreach($ids as $id){
+            $temp = $this->createComponent('Vpc_Formular_Checkbox_Index', null, $id);
+            $multicheckbox[] = $temp->getTemplateVars('');
+        }
+        return $multicheckbox;
     }
+    
+    //erstellt Checkboxen mittels der übergebenen Werte
+    private function _setMulitcheckboxNew($name, $values)
+    {
+        $cnt = 0;
+        foreach($values as $value){
+            $temp = $this->createComponent('Vpc_Formular_Checkbox_Index', null, $cnt);
+            $temp->setSetting('name', $name);
+            $temp->setSetting('value', $value['value']);
+            $temp->setSetting('text', $value['text']);
+            $temp->setSetting('checked', $value['checked']);
+            $multicheckbox[] = $temp->getTemplateVars('');
+            $cnt++;
+        }
+        return $multicheckbox;
+    }
+    
+    
+    public function processInput() 
+    {
+        p("hier rein");
+        
+        //$this->setSetting('multicheckbox', $this->_setMulitcheckboxbyIds());
+        if ($_POST != array()) {
+            
+            $multicheckbox = $this->getSetting('multicheckbox');
+            foreach($multicheckbox as $key => $checkbox) {
+                $checkbox['checked'] = 0;
+                $multicheckbox[$key] = $checkbox;
+            }
+            foreach($multicheckbox as $key => $checkbox) {
+                if (array_key_exists($checkbox['name'], $_POST)) {
+                    $checkbox['checked'] = 1;
+                }
+                $multicheckbox[$key] = $checkbox;
+            }
+            
+            $this->setSetting('multicheckbox', $multicheckbox);
+        }
+        
+        
+    }
+    
+    public function validateField($mandatory)
+    {
+        
+        $multicheckbox = $this->getSetting('multicheckbox');
+        foreach($multicheckbox as $key => $checkbox) {
+            if (array_key_exists($checkbox['name'], $_POST)) {
+                return true;
+            }
+        }
+        if ($mandatory) {
+            return 'Feld '.$this->_errorField.' ist ein Pflichtfeld, bitte ausfüllen';
+        }
+        
+        return true;
+    }
+    
+    
+    public function setName($name) 
+    {
+        $this->start();
+        //hier muss als Spezialfall die Startmethode aufgerufen werden
+        $names = array();
+        $this->setSetting('name', $name);
+        
+        $multicheckbox = $this->getSetting('multicheckbox');
+        $cnt = 0;
+        $filter = new Zend_Filter_Alpha();
+        foreach($multicheckbox as $key => $checkbox) {
+            $part1 = $filter->filter($this->getName('name'));
+            $part2 = $filter->filter($checkbox['value']);
+            
+            //überprüfung ob der Feldname schon einmal existiert
+            $newName = $part1.'_'.$part2;
+            $tempName = $newName;
+            $cnt2 = 1;
+            while (in_array($newName, $names)) {
+                $newName = $tempName.$cnt2;
+                $cnt2++;
+            }
+            $names[] = $newName;
+            
+            $checkbox['name'] =  $newName;
+            $cnt++;
+            $multicheckbox[$key] = $checkbox;
+        }
+        
+        $this->setSetting('multicheckbox', $multicheckbox);
+    }
+    
+    private function start()
+    {
+        $name = $this->getSetting('name');
+        
+        // Variante 1  -> Array selber mitgehen
+        $values = array(array('value' => 'value1', 'text' => 'Box1', 'checked' => '1'),
+        array('value' => 'value2', 'text' => 'Box2', 'checked' => '0'),
+        array('value' => 'value3', 'text' => 'Box3', 'checked' => '0'),
+        array('value' => 'value4', 'text' => 'Box4', 'checked' => '1'));
+        
+        //$this->setSetting('multicheckbox', $this->_setMulitcheckboxNew($name, $values));
+        // ---
+        
+        //Variaten 2 -> Array wird mitels der ID geholt
+        if ($this->getSetting('multicheckbox') == array()) {
+            $this->setSetting('multicheckbox', $this->_setMulitcheckboxbyIds());
+        }
+    }
+    
 }
--- Vpc/Formular/Select/Setup.php
+++ Vpc/Formular/Select/Setup.php
@@ -1,24 +1,21 @@
 <?php
-class Vpc_Formular_Select_Setup extends Vpc_Setup_Abstract 
+class Vpc_Formular_Multicheckbox_Setup extends Vpc_Setup_Abstract 
 {
     public function setup()
     {   
-        $fields['rows'] = "smallint(6) NOT NULL";
-        $this->createTable('component_formular_select', $fields);
-
-        $tablename = 'component_formular_select_options';
+        $fields['horizontal'] = 'tinyint(4) NOT NULL';
+        $this->createTable('component_formular_multicheckbox', $fields);
+        
+        $tablename = 'component_formular_multicheckbox_checkboxes';
         if (!$this->_tableExits($tablename)) {
 	        $this->_db->query("CREATE TABLE `$tablename` (
-							  `id` int(11) NOT NULL auto_increment,
-							  `component_id` smallint(6) NOT NULL,
-							  `page_key` varchar(100) NOT NULL,
-							  `component_key` varchar(100) NOT NULL,
-							  `value` varchar(255) NOT NULL,
-							  `text` varchar(255) NOT NULL,
-							  `selected` tinyint(4) NOT NULL,
-							  PRIMARY KEY  (`id`),
-							  KEY `component_id` (`component_id`,`page_key`,`component_key`)
-							) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;");
-        }
+	  						   `id` int(10) unsigned NOT NULL auto_increment,
+	                           `component_key` varchar(100) NOT NULL,
+	                           `page_key` varchar(100) NOT NULL,
+	                           `component_id` int(10) unsigned NOT NULL,
+	                           PRIMARY KEY  (`id`),
+	                           KEY `component_key` (`component_key`,`page_key`,`component_id`))
+							   ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=8;");
+	        }
     }
 }
