178884453ff4590e17547f5ffc3ad2c93f2fe97e
Lorenz Maislinger <lm@vivid-planet.com>
2007-07-24 09:10:08 +0000

    - Formularkomponenten

---
+++ Vps/Controller/Action/Auto/Form/Vpc.php
@@ -0,0 +1,18 @@
+<?php
+abstract class Vps_Controller_Action_Auto_Form_Vpc extends Vps_Controller_Action_Auto_Form {
+    public function indexAction()
+    {
+       $this->view->ext('Vps.Auto.Form');
+    }
+       
+    public function jsonIndexAction()
+    {
+       $this->indexAction();
+    }
+    public function jsonLoadAction()
+    {
+        $defaultSettings = $this->component->getDefaultSettings(); // Komponente ist unter $this->component zu finden
+        $this->_table->createDefaultRow($this->_getParam('id'), $defaultSettings);
+        parent::jsonLoadAction();
+    }
+}
