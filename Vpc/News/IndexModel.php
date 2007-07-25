4fb6eaaceefdd4ae78d4756b38e1e120337e0ad9
Lorenz Maislinger <lm@vivid-planet.com>
2007-07-25 08:37:20 +0000

    News Komponente Neu begonnen

---
+++ Vpc/News/IndexModel.php
@@ -0,0 +1,5 @@
+<?php
+class Vpc_News_IndexModel extends Zend_Db_Table
+{
+    protected $_name = 'component_news';
+}
--- Vps/Dao/News.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Vps_Dao_News extends Zend_Db_Table
-{
-    protected $_name = 'news';
-}
