a1523e0b8644e7d27a50e999b8b8210cd15b896d
Lorenz Maislinger <lm@vivid-planet.com>
2007-07-25 08:37:20 +0000

    News Komponente Neu begonnen

--- Vpc/News/Index.php
+++ Vpc/News/Index.php
@@ -1,33 +1,29 @@
 <?php
 /**
  * Newskomponente
- * 
+ *
  * @package Vpc
  * @subpackage Components
  */
 class Vpc_News_Index extends Vpc_Abstract
 {
-    private $_titles;
-    
-    protected function getChildPages($filename = '')
+    public function generateHierarchy($filename = '')
     {
         $pages = array();
-        foreach($this->getDao()->getTable('Vps_Dao_News')->fetchAll() as $row) {
+        foreach($this->getDao()->getTable('Vpc_News_IndexModel')->fetchAll() as $row) {
             if ($filename != '' && $filename != $row->filename) continue;
-
             $page = $this->createPage('Vpc_News_Details', 0, $row->id);
-            $page->setNewsId($row->id);
-            $pages[$row->filename] = $page;
-            $this->_titles[$row->filename] = $row->title;
+            $this->getPagecollection()->addTreePage($page, 'Details', $row->title, $this);
         }
         return $pages;
     }
-    
+
     public function getTemplateVars($mode)
     {
         $ret = parent::getTemplateVars($mode);
-        
+
         $news = $this->generateHierarchy();
+        $ret['news'] = array();
         foreach($news as $filename => $n) {
             $data['title'] = $this->_titles[$filename];
             $data['filename'] = $n->getUrl();
