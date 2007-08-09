22f7cd3648553abe5542761ea436c79575cfaf4a
Lorenz Maislinger <lm@vivid-planet.com>
2007-08-09 13:14:38 +0000

    ImageKomponente - Skalierung, Upload, Thumbnail ..

--- Vpc/Simple/Image/FileDataModel.php
+++ Vpc/Simple/Image/FileDataModel.php
@@ -2,5 +2,6 @@
 
 class Vpc_Simple_Image_FileDataModel extends Vpc_Table
 {
-    protected $_name = 'component_simple_image';
+    protected $_name = 'vps_uploads';
+    protected $_primary = array('id');
 }
