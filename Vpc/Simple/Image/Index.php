4e0b59afe54e33efd86957c68ea16bc9ea370889
Lorenz Maislinger <lm@vivid-planet.com>
2007-08-09 12:49:05 +0000

    ImageKomponente - Skalierung, Upload, Thumbnail ..

--- Vpc/Simple/Image/Index.php
+++ Vpc/Simple/Image/Index.php
@@ -1,7 +1,7 @@
 <?php
 class Vpc_Simple_Image_Index extends Vpc_Abstract
 {
-	protected $_tablename = 'Vpc_Simple_Image_FileDataModel';
+	protected $_tablename = 'Vpc_Simple_Image_IndexModel';
     public $controllerClass = 'Vpc_Simple_Image_IndexController';
    	const NAME = 'Standard.Image';
 
@@ -19,14 +19,22 @@ class Vpc_Simple_Image_Index extends Vpc_Abstract
 	}*/
 
 	protected $_settings = array(
-        'typesAllowed' => 'jpg, gif, png',
-        'directory'    => 'SimpleImage/'
+        'typesAllowed' 	    => 'jpg, gif, png',
+        'directory'   	    => 'SimpleImage/',
+        'filesize'	   	    => 'free',
+        'default_style'		=> 'crop',
+        'enableName' 		=> 1,
+        'style' 	        => '',
+        'allow'		        => array('crop', 'scale', 'scale_bg', 'deform'), //keywords: crop, scale, scale_bg, deform
+        'default_color'		=> 'black',
+        'allow_color'		=> 1,
+		'color'				=> '',
+
         );
 
 
 	public function getTemplateVars()
 	{
-
 		$return['template'] = 'Simple/Image.html';
 		return $return;
 	}
