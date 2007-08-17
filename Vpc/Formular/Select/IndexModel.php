370f00d1611d21bdbc9cdcaa41a52db21087065a
Franz Unger <ufx@vivid-planet.com>
2007-08-17 07:25:24 +0000

    Zwischencheckin für Formularelement - nicht funktiosfähig!

--- Vpc/Formular/Select/IndexModel.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Vpc_Formular_Select_IndexModel extends Vpc_Table
-{
-    protected $_name = 'component_formular_select';
-}
--- Vpc/Formular/SelectMulti/Index.js
+++ Vpc/Formular/SelectMulti/Index.js
@@ -1,32 +1,21 @@
-Ext.namespace('Vpc.Formular.Multicheckbox');
-Vpc.Formular.Multicheckbox.Index = function(renderTo, config)
+Ext.namespace('Vpc.Formular.SelectMulti');
+Vpc.Formular.SelectMulti.Index = function(renderTo, config)
 {
-	/*var tabs = new Ext.TabPanel(renderTo);
-
-	tabs.addTab("checkboxes", "Checkboxes");
-	tabs.addTab("properties", "Eigenschaften");
-
-	tabs.activate("options");
-
-	var cb = new Vps.Auto.Grid("checkboxes", {controllerUrl: config.checkboxesControllerUrl});
-	var cb = new Vps.Auto.Form("properties", {controllerUrl: config.controllerUrl});*/
-
-
-	var layout = new Ext.BorderLayout(renderTo, {
+    var layout = new Ext.BorderLayout(renderTo, {
         center: {
             tabPosition: 'top',
             closeOnTab: true,
             alwaysShowTabs : true,
             autoScroll: true
         }
-	});
-
-	layout.beginUpdate();
-	layout.add("center", new Ext.ContentPanel("properties1", {autoCreate: true, title: 'Properties'}));
-	layout.add("center", new Ext.ContentPanel ("checkboxes1", {autoCreate: true, fitToFrame:true, title: 'Checkboxes'}));
-	var cb = new Vps.Auto.Form("properties1", {controllerUrl: config.controllerUrl, fitToFrame:true});
-	var cb = new Vps.Auto.Grid("checkboxes1", {controllerUrl: config.checkboxesControllerUrl});
-	layout.endUpdate();
+    });
+    
+    layout.beginUpdate();
+    layout.add("center", new Ext.ContentPanel("properties1", {autoCreate: true, title: 'Properties'}));
+    layout.add("center", new Ext.ContentPanel ("checkboxes1", {autoCreate: true, fitToFrame:true, title: 'Checkboxes'}));
+    var cb = new Vps.Auto.Form("properties1", {controllerUrl: config.controllerUrl, fitToFrame:true});
+    var cb = new Vps.Auto.Grid("checkboxes1", {controllerUrl: config.checkboxesControllerUrl});
+    layout.endUpdate();
 };
 
-Ext.extend(Vpc.Formular.Multicheckbox.Index, Ext.util.Observable,{})
+Ext.extend(Vpc.Formular.SelectMulti.Index, Ext.util.Observable,{})
