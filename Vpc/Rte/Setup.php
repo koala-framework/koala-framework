c5c2da8461d1cf9c7b1bda34656c4086637414e1
Franz Unger <ufx@vivid-planet.com>
2007-08-30 11:48:41 +0000

    RTE verwendbar

--- Vpc/Rte/Setup.php
+++ Vpc/Rte/Setup.php
@@ -1,10 +1,20 @@
 <?php
 class Vpc_Rte_Setup extends Vpc_Setup_Abstract
 {
-	public function setup()
+    public function setup()
     {
-		$fields['text'] = 'text NOT NULL';
-		$this->createTable('component_rte', $fields);
+        $this->copyTemplate('Rte.html');
+
+        $fields['text'] = 'text NOT NULL';
+        $this->createTable('component_rte', $fields);
     }
 
+    public function deleteEntry($pageId, $componentKey)
+    {
+        $where = array();
+        $where['page_id = ?'] = $pageId;
+        $where['component_key = ?'] = $componentKey;
+        $table = new Vpc_Rte_IndexModel(array('db'=>$this->_db));
+        $table->delete($where);
+    }
 }
