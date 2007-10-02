beb62408bd9dc7e297d627cca88823e43b6a2372
Niko Sams <ns@vivid-planet.com>
2007-10-02 13:57:26 +0000

    skalieren in hilfs-klasse ausgelagert, damit es von woandersher auch aufgerufen werden kann

--- Vpc/Basic/Image/Index.php
+++ Vpc/Basic/Image/Index.php
@@ -45,46 +45,6 @@ class Vpc_Basic_Image_Index extends Vpc_Abstract implements Vpc_FileInterface
         if ($width <= 0) { $width = 100; }
         if ($height <= 0) { $height = 100; }
 
-        $im = new Imagick();
-        $im->readImage($source);
-        if ($style == 'crop'){ // Bild wird auf allen 4 Seiten gleichmäßig beschnitten
-
-            $scale = $im->getImageGeometry();
-            if ($scale['width'] > $width) { // Wenn hochgeladenes Bild breiter als anzuzeigendes Bild ist
-                $x = ($scale['width'] - $width) / 2; // Ursprungs-X berechnen
-            } else {
-                $x = 0; // Bei 0 mit Beschneiden beginnen
-                $width = $scale['width']; // Breite auf Originalgröße begrenzen
-            }
-            if ($scale['height'] > $height) {
-                $y = ($scale['height'] - $height) / 2;
-            } else {
-                $y = 0;
-                $height = $scale['height'];
-            }
-            $im->cropImage($width, $height, $x, $y);
-
-        } elseif ($style == 'bestfit') { // Bild wird auf größte Maximale Ausdehnung skaliert
-
-            $scale = $im->getImageGeometry();
-            $widthRatio = $scale['width'] / $width;
-            $heightRatio = $scale['height'] / $height;
-            if ($widthRatio > $heightRatio){
-                $width = $scale['width'] / $widthRatio;
-                $height = $scale['height'] / $widthRatio;
-            } else {
-                $width = $scale['width'] / $heightRatio;
-                $height = $scale['height'] / $heightRatio;
-            }
-            $im->thumbnailImage($width, $height);
-
-        } elseif ($style == 'deform'){
-
-            $im->thumbnailImage($width, $height);
-
-        }
-
-        $im->writeImage($target);
-        $im->destroy();
+        Vps_Media_Image::scale($source, $target, array($width, $height), $style);
     }
 }
