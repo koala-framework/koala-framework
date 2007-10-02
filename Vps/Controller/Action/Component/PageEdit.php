945349c2ecd5472dbe7fc230642af356b449054b
Franz Unger <ufx@vivid-planet.com>
2007-10-02 12:19:09 +0000

    Bugfix

--- Vps/Controller/Action/Component/PageEdit.php
+++ Vps/Controller/Action/Component/PageEdit.php
@@ -2,9 +2,8 @@
 
 class Vps_Controller_Action_Component_PageEdit extends Vps_Controller_Action_Auto_Form
 {
-    protected $_buttons = array('save' => true);
     protected $_permissions = array('save' => true, 'add' => true);
-    
+
     protected function _initFields()
     {
         $components = Vpc_Setup_Abstract::getAvailableComponents('Vpc/');
@@ -26,13 +25,13 @@ class Vps_Controller_Action_Component_PageEdit extends Vps_Controller_Action_Aut
             ->setFieldLabel('Headline');
         $fields->add(new Vps_Auto_Field_ComboBox('component_class'))
             ->setFieldLabel('Pagetype')
-            ->setStoreData($types)
+            ->setValues($types)
             ->setTriggerAction('all')
             ->setValue('Vpc_Paragraphs_Index')
             ->setEditable(false)
             ->setForceSelection(true);
     }
-    
+
     protected function _beforeInsert(Zend_Db_Table_Row_Abstract $row)
     {
         $row->parent_id = $this->_getParam('parent_id');
