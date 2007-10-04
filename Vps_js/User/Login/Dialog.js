7b6efb9b97bb39a02ae1c9639472593ab7e459df
Franz Unger <ufx@vivid-planet.com>
2007-10-04 07:30:50 +0000

    Fehler bei Login ausgebessert

--- Vps_js/User/Login/Dialog.js
+++ Vps_js/User/Login/Dialog.js
@@ -1,51 +1,42 @@
 Ext.namespace('Vps.User.Login');
-Vps.User.Login.Dialog = function(config)
+Vps.User.Login.Dialog = Ext.extend(Ext.Window,
 {
-    this.dialog = new Ext.Window({
-        height: 160,
-        width: 310,
-        modal: false,
-        proxyDrag: true,
-        shadow: true,
-        title: 'Login',
-        closable: true,
-        collapsible: false,
-        resizable: false
-    });
-    this.dialog.render(Ext.getBody());
+    initComponent: function() {
+        this.height = 160;
+        this.width = 310;
+        this.modal = true;
+        this.proxyDrag = true;
+        this.title = 'Login';
+        this.resizable = false;
+        Vps.User.Login.Dialog.superclass.initComponent.call(this);
 
-    Ext.DomHelper.append(this.dialog.body, '<iframe id="loginframe" scrolling="no" src="/login/showForm" width="100%" height="100%" style="border: 0px"></iframe>');
-    var frame = Ext.get('loginframe');
+        this.render(Ext.getBody());
 
-    function cb(){
-        if(Ext.isIE){
-            doc = frame.dom.contentWindow.document;
-        }else {
-            doc = (frame.dom.contentDocument || window.frames[id].document);
-        }
-        if(doc && doc.body){
-            if (doc.body.innerHTML.match(/successful/)) {
-                this.dialog.hide();
-                if(Vps.menu) Vps.menu.reload();
-                if(this.success) {
-                    Ext.callback(this.success, this.scope);
+        Ext.DomHelper.append(this.body, '<iframe id="loginframe" scrolling="no" src="/login/showForm" width="100%" height="100%" style="border: 0px"></iframe>');
+        var frame = Ext.get('loginframe');
+        function cb(){
+            if(Ext.isIE){
+                doc = frame.dom.contentWindow.document;
+            }else {
+                doc = (frame.dom.contentDocument || window.frames[id].document);
+            }
+            if(doc && doc.body){
+                if (doc.body.innerHTML.match(/successful/)) {
+                    this.hide();
+                    if (this.location) {
+                        location.href = this.location;
+                    } else {
+                        if(Vps.menu) Vps.menu.reload();
+                        if(this.success) {
+                            Ext.callback(this.success, this.scope);
+                        }
+                    }
                 }
             }
         }
-    }
-
-    Ext.EventManager.on(frame, 'load', cb, this);
-
-    Vps.User.Login.Dialog.superclass.constructor.call(this, config);
-};
-
-
-Ext.extend(Vps.User.Login.Dialog, Ext.util.Observable,
-{
-    show: function() {
-        this.dialog.show();
+        Ext.EventManager.on(frame, 'load', cb, this);
     },
     showLogin: function() {
-        this.dialog.show();
+        this.show();
     }
 });
