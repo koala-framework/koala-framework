f873bfde4f0a8c69676d3a6e3dbd52dea4159876
Markus Hauser <mh@vivid-planet.com>
2007-10-18 07:39:34 +0000

    PDF Export (drucken): Im Header erscheint automatisch das aktuelle Datum

--- Vps/Auto/Grid/Pdf/Abstract.php
+++ Vps/Auto/Grid/Pdf/Abstract.php
@@ -14,8 +14,18 @@ abstract class Vps_Auto_Grid_Pdf_Abstract extends TCPDF
 
     public function footer()
     {
-//         $this->setXY($this->GetPageHeight() - 80, $this->GetPageWidth() - 80);
         $this->SetY($this->GetPageHeight() - 15);
         $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'R');
+    }
+
+    public function Header()
+    {
+        $date = new Zend_Date();
+        $headStr = $date->get(Zend_Date::WEEKDAY).', '.date('d.m.Y, H:i');
+
+        $yBefore = $this->GetY();
+        $this->SetY(10);
+        $this->Cell(0, 0, $headStr, 0, 1, 'R');
+        $this->SetY($yBefore);
     }
 }
