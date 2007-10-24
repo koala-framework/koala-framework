b22f99aa735686ee7e386e1d24f973bc78ba974a
Franz Unger <ufx@vivid-planet.com>
2007-10-24 06:51:05 +0000

    ContentImage -> ParagraphsIndex

--- Vpc/Composite/ParagraphsImage/Index.php
+++ Vpc/Composite/ParagraphsImage/Index.php
@@ -1,36 +1,36 @@
 <?php
-class Vpc_Composite_ContentImage_Index extends Vpc_Abstract
+class Vpc_Composite_ParagraphsImage_Index extends Vpc_Abstract
 {
-    const NAME = 'Standard.ContentImage';
+    const NAME = 'Standard.ParagraphsImage';
     protected $_settings = array(
-        'contentClass'      => 'Vpc_Paragraphs_Index',
-        'contentSettings'   => array(),
+        'paragraphsClass'      => 'Vpc_Paragraphs_Index',
+        'paragraphsSettings'   => array(),
         'imageClass'        => 'Vpc_Basic_Image_Index',
         'imageSettings'     => array()
     );
-    public $content;
+    public $paragraphs;
     public $image;
 
     public function getTemplateVars()
     {
         $return = parent::getTemplateVars();
-        $return['content'] = $this->content->getTemplateVars('');
+        $return['paragraphs'] = $this->paragraphs->getTemplateVars('');
         $return['image'] = $this->image->getTemplateVars('');
-        $return['template'] = 'Composite/ContentImage.html';
+        $return['template'] = 'Composite/ParagraphsImage.html';
         return $return;
     }
 
     protected function _init()
     {
-        $contentClass = $this->_getClassFromSetting('contentClass', 'Vpc_Paragraphs_Index');
+        $paragraphsClass = $this->_getClassFromSetting('paragraphsClass', 'Vpc_Paragraphs_Index');
         $imageClass = $this->_getClassFromSetting('imageClass', 'Vpc_Basic_Image_Index');
-        $this->content = $this->createComponent($contentClass, 1, $this->getSetting('contentSettings'));
+        $this->paragraphs = $this->createComponent($paragraphsClass, 1, $this->getSetting('paragraphsSettings'));
         $this->image = $this->createComponent($imageClass, 2, $this->getSetting('imageSettings'));
     }
 
     public function getChildComponents()
     {
-        return array($this->content, $this->image);
+        return array($this->paragraphs, $this->image);
     }
 
 }
