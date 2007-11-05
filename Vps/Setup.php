19f19c8ff439afea1ebbb6c039e07244a45d0c51
Niko Sams <ns@vivid-planet.com>
2007-11-05 06:53:26 +0000

    dev subdomain berücksichtigen (todo: passwortschutz)

--- Vps/Setup.php
+++ Vps/Setup.php
@@ -4,15 +4,17 @@ class Vps_Setup
     public static function createConfig()
     {
         if (preg_match('#/www/usr/([0-9a-z]+)/#', $_SERVER['SCRIPT_FILENAME'], $m)) {
-            $user = $m[1];
+            $section = $m[1];
         } else if (substr(__FILE__, 0, strlen('/www/public/')) == '/www/public/') {
-            $user = 'vivid';
+            $section = 'vivid';
+        } else if (isset($_SERVER['HTTP_HOST']) && substr($_SERVER['HTTP_HOST'], 0, 4)=='dev.') {
+            $section = 'dev';
         } else {
-            $user = 'production';
+            $section = 'production';
         }
-        $webConfig = new Zend_Config_Ini('application/config.ini', $user);
+        $webConfig = new Zend_Config_Ini('application/config.ini', $section);
 
-        $vpsConfig = new Zend_Config_Ini(VPS_PATH.'/config.ini', $user,
+        $vpsConfig = new Zend_Config_Ini(VPS_PATH.'/config.ini', $section,
                         array('allowModifications'=>true));
         $vpsConfig->merge($webConfig);
 
