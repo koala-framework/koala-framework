219ff18d0bf29afe6412aec8acb9eb686ba832a7
Franz Unger <ufx@vivid-planet.com>
2007-11-13 07:29:21 +0000

    Vpc_Basic_LinkTag weiter verbessert, Client- und Serverseitige Validierung geht jetzt

--- Vpc/Basic/LinkTag/Panel.js
+++ Vpc/Basic/LinkTag/Panel.js
@@ -5,7 +5,19 @@ Vpc.Basic.LinkTag.Panel = Ext.extend(Vps.Auto.FormPanel, {
         Vpc.Basic.LinkTag.Panel.superclass.initComponent.call(this);
         this.on('loadform', function(o, e) {
             this.getForm().findField('LinkClass').on('changevalue', function(value) {
-                this.findById('CardsContainer').layout.setActiveItem(value);
+                var cards = this.findById('CardsContainer');
+                cards.layout.setActiveItem(value);
+                cards.items.each(function(item){
+                    if (item.id != value) {
+                        item.cascade(function(i) {
+                            i.disable();
+                        }, this);
+                    } else {
+                        item.cascade(function(i) {
+                            i.enable();
+                        }, this);
+                    }
+                }, this);
             }, this);
         }, this);
     }
