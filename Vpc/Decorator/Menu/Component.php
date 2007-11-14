f63f20db75d4236f31a4493eb4af6a1d25304f51
Franz Unger <ufx@vivid-planet.com>
2007-11-14 16:58:06 +0000

    Vpc_Decorator_Menu verbessert

--- Vpc/Decorator/Menu/Component.php
+++ Vpc/Decorator/Menu/Component.php
@@ -1,66 +1,64 @@
 <?php
 /**
- * Menüdecorator. Speichert in Template-Variable alle Werte, die 
+ * Menüdecorator. Speichert in Template-Variable alle Werte, die
  * für das Menü benötigt werden.
  * @package Vpc
  * @subpackage Decorator
  */
 class Vpc_Decorator_Menu_Component extends Vpc_Decorator_Abstract
 {
+    protected $_levels = 2;
+    
     public function getTemplateVars()
     {
         $return = parent::getTemplateVars();
-        $pc = $this->_pageCollection;
+        $pc = $this->getPageCollection();
 
+        // Array mit IDs von aktueller Seiten und Parent Pages
         $currentPageIds = array();
-        $p = $this->_pageCollection->getCurrentPage();
+        $p = $pc->getCurrentPage();
         do {
             $currentPageIds[] = $p->getPageId();
         } while($p = $pc->getParentPage($p));
 
-        $pageCollectionConfig = new Zend_Config_Ini('application/config.ini', 'pagecollection');
-        foreach ($pageCollectionConfig->pagecollection->pagetypes as $type=>$i) {
+        // Hauptmenü
+        $config = new Zend_Config_Ini('application/config.ini', 'pagecollection');
+        foreach ($config->pagecollection->pagetypes as $type => $i) {
             $pages = $pc->getChildPages(null, $type);
             $return['menu'][$type] = array();
-            foreach ($pages as $i=>$page) {
-                $data = $pc->getPageData($page);
-                if ($data['hide']) { continue; }
-                $class = '';
-                if ($i==0) $class .= ' first';
-                if ($i==sizeof($pages)-1) $class .= ' last';
-                $isCurrent = in_array($page->getPageId(), $currentPageIds);
-                if ($isCurrent) $class .= ' current';
-                $return['menu'][$type][] = array('href'    => $data['url'],
-                                                 'text'    => $data['name'],
-                                                 'current' => $isCurrent,
-                                                 'class'   => trim($class),
-                                                 'rel'     => $data['rel']);
-            }
+            $return['menu'][$type] = $this->_getMenuData($pages, $currentPageIds);
         }
 
-        $levels = 2;
-
+        // Submenüs
         $level = 0;
-        $page = $pc->getCurrentPage();
-        while ($page && $level < $levels) {
+        $page = $pc->findPage(array_pop($currentPageIds));
+        while ($page && $level < $this->_levels) {
             $pages = $pc->getChildPages($page);
-            $return['submenus'][$level] = array();
-            foreach ($pages as $i=>$p) {
-                $data = $pc->getPageData($p);
-                if ($data['hide']) { continue; }
-                $class = '';
-                if ($i==0) $class .= ' first';
-                if ($i==sizeof($pages)-1) $class .= ' last';
-                $isCurrent = in_array($page->getPageId(), $currentPageIds);
-                if ($isCurrent) $class .= ' current';
-                $return['submenus'][$level][] = array('href' => $data['url'],
-                                                      'text' => $data['name'],
-                                                      'current' => $isCurrent,
-                                                      'class'   => trim($class),
-                                                      'rel'  => '');
-            }
-            $page = $pc->getParentPage($page);
-            $level--;
+            $return['submenus'][$level] = $this->_getMenuData($pages, $currentPageIds);
+            $page = $pc->findPage(array_pop($currentPageIds));
+            $level++;
+        }
+        return $return;
+    }
+    
+    protected function _getMenuData($pages, $currentPageIds)
+    {
+        $return = array();
+        foreach ($pages as $i => $page) {
+            $data = $this->getPageCollection()->getPageData($page);
+            if ($data['hide']) { continue; }
+            $class = '';
+            if ($i == 0) $class .= ' first';
+            if ($i == sizeof($pages)-1) $class .= ' last';
+            $isCurrent = in_array($page->getPageId(), $currentPageIds);
+            if ($isCurrent) $class .= ' current';
+            $return[] = array(
+                'href'    => $data['url'],
+                'text'    => $data['name'],
+                'current' => $isCurrent,
+                'class'   => trim($class),
+                'rel'     => $data['rel']
+            );
         }
         return $return;
     }
