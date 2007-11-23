7ca14ad8b1e7e96037248e010a00cf73cfedb871
Niko Sams <ns@vivid-planet.com>
2007-11-23 13:32:54 +0000

    admin->delete nur aufrufen wenn auch admin gibt

--- Vpc/Paragraphs/Row.php
+++ Vpc/Paragraphs/Row.php
@@ -4,6 +4,8 @@ class Vpc_Paragraphs_Row extends Vps_Db_Table_Row_Abstract
     protected function _delete()
     {
         $admin = Vpc_Admin::getInstance($this->component_class);
-        $admin->delete($this->page_id, $this->component_key . '-' . $this->id);
+        if ($admin) {
+            $admin->delete($this->page_id, $this->component_key . '-' . $this->id);
+        }
     }
 }
