0af8d5e2abefa2d78fae058034c2eeb0dddb18c2
Markus Hauser <mh@vivid-planet.com>
2007-11-30 11:54:46 +0000

    Gemerged von branches/vps/servicelogin: VpsConfig / WebConfig schaut, was vorhanden ist (user / vivid)

--- Vps/Setup.php
+++ Vps/Setup.php
@@ -49,21 +49,30 @@ class Vps_Setup
     {
         return new Vps_Dao(new Zend_Config_Ini('application/config.db.ini', 'database'));
     }
-    
+
     public static function createConfig()
     {
-        if (preg_match('#/www/usr/([0-9a-z]+)/#', $_SERVER['SCRIPT_FILENAME'], $m)) {
-            $section = $m[1];
-        } else if (substr(__FILE__, 0, strlen('/www/public/')) == '/www/public/') {
-            $section = 'vivid';
-        } else if (isset($_SERVER['HTTP_HOST']) && substr($_SERVER['HTTP_HOST'], 0, 4)=='dev.') {
-            $section = 'dev';
-        } else {
-            $section = 'production';
-        }
-        $webConfig = new Zend_Config_Ini('application/config.ini', $section);
+        if (preg_match('#/www/(usr|public)/([0-9a-z]+)/#', $_SERVER['SCRIPT_FILENAME'], $m)) {
+            $vpsSection = $webSection = 'vivid';
 
-        $vpsConfig = new Zend_Config_Ini(VPS_PATH.'/config.ini', $section,
+            $webConfigFull = new Zend_Config_Ini('application/config.ini', null);
+            if (isset($webConfigFull->{$m[2]})) {
+                $webSection = $m[2];
+            }
+
+            $vpsConfigFull = new Zend_Config_Ini(VPS_PATH.'/config.ini', null);
+            if (isset($vpsConfigFull->{$m[2]})) {
+                $vpsSection = $m[2];
+            }
+        } else if (isset($_SERVER['HTTP_HOST']) && substr($_SERVER['HTTP_HOST'], 0, 4)=='dev.') {
+            $vpsSection = $webSection = 'dev';
+        } else {
+            $vpsSection = $webSection = 'production';
+        }
+
+        $webConfig = new Zend_Config_Ini('application/config.ini', $webSection);
+
+        $vpsConfig = new Zend_Config_Ini(VPS_PATH.'/config.ini', $vpsSection,
                         array('allowModifications'=>true));
         $vpsConfig->merge($webConfig);
 
