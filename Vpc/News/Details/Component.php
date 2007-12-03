7c62aeed1452dfc6a5200e95229117ba68521908
Franz Unger <ufx@vivid-planet.com>
2007-12-03 15:23:34 +0000

    Bugfix zu Newskomponente

--- Vpc/News/Details/Component.php
+++ Vpc/News/Details/Component.php
@@ -2,12 +2,14 @@
 class Vpc_News_Details_Component extends Vpc_Paragraphs_Component
 {
     public $row;
+    public $news = 'true'; // Wird benötigt um Endlosschleife bei getSettings zu vermeiden
     
     public static function getSettings()
     {
         $settings = parent::getSettings();
         foreach ($settings['childComponentClasses'] as $key => $class) {
-            if (get_class() != 'Vpc_News_Details_Component' &&
+            $vars = get_class_vars($class);
+            if (!isset($vars['news']) &&
                 Vpc_Abstract::getSetting($class, 'hideInNews'))
             {
                 unset($settings['childComponentClasses'][$key]);
