72cc1a72689f76fbe8e85ead24ded8ece0f1e260
Franz Unger <ufx@vivid-planet.com>
2007-12-07 09:43:20 +0000

    Vpc_News: Buttons im News-Grid

--- Vpc/News/Controller.php
+++ Vpc/News/Controller.php
@@ -15,8 +15,11 @@ class Vpc_News_Controller extends Vps_Controller_Action_Auto_Vpc_Grid
     public function _initColumns()
     {
         $this->_columns->add(new Vps_Auto_Grid_Column('title', 'Title', 300));
+        $this->_columns->add(new Vps_Auto_Grid_Column_Button('properties', ' ', 20))
+            ->setButtonIcon('/assets/silkicons/newspaper.png')
+            ->setToolTip('Properties');
         $this->_columns->add(new Vps_Auto_Grid_Column_Button('edit', ' ', 20))
-            ->setButtonIcon('/assets/silkicons/table_edit.png')
+            ->setButtonIcon('/assets/silkicons/newspaper_go.png')
             ->setToolTip('Edit News');
         $this->_columns->add(new Vps_Auto_Grid_Column('publish_date', 'Publish Date', 50))
             ->setEditor(new Vps_Auto_Field_DateField('publish_date', 'Publish Date'));
