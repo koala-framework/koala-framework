92cbff1f29afe9b373d343e4475b884212299a2a
Niko Sams <ns@vivid-planet.com>
2007-12-20 09:36:11 +0000

    FieldSet -inhalte nicht speichern wenn disabled

--- Vps/Auto/Container/FieldSet.php
+++ Vps/Auto/Container/FieldSet.php
@@ -1,6 +1,7 @@
 <?php
 class Vps_Auto_Container_FieldSet extends Vps_Auto_Container_Abstract
 {
+    private $_checkboxHiddenField = null;
     public function __construct($title = null)
     {
         parent::__construct();
@@ -11,7 +12,23 @@ class Vps_Auto_Container_FieldSet extends Vps_Auto_Container_Abstract
 
     public function setCheckboxName($name)
     {
-        $this->fields->add(new Vps_Auto_Field_Hidden($name));
+        $this->_checkboxHiddenField = new Vps_Auto_Field_Hidden($name);
+        $this->fields->add($this->_checkboxHiddenField);
         return $this->setProperty('checkboxName', $name);
+    }
+
+    public function prepareSave($row, $postData)
+    {
+        if ($this->getCheckboxName()) {
+            if (!isset($postData[$this->getCheckboxName()])
+                || !$postData[$this->getCheckboxName()]) {
+                foreach ($this->fields as $f) {
+                    if ($f != $this->_checkboxHiddenField) {
+                        $f->setSave(false);
+                    }
+                }
+            }
+        }
+        parent::prepareSave($row, $postData);
     }
 }
