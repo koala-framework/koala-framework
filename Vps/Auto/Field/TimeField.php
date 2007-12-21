29b9699b96e5fee121055e5cf89c320c2a68a07c
Niko Sams <ns@vivid-planet.com>
2007-12-21 12:30:33 +0000

    - zeit laden bugfix

--- Vps/Auto/Field/TimeField.php
+++ Vps/Auto/Field/TimeField.php
@@ -12,4 +12,15 @@ class Vps_Auto_Field_TimeField extends Vps_Auto_Field_SimpleAbstract
         parent::_addValidators();
         $this->addValidator(new Vps_Validate_Time());
     }
+
+    public function load($row)
+    {
+        $ret = array();
+        $v = $this->getData()->load($row);
+        if (strlen($v) > 5) {
+            $v = substr($v, 0, 5);
+        }
+        $ret[$this->getFieldName()] = $v;
+        return array_merge($ret, Vps_Auto_Field_Abstract::load($row));
+    }
 }
