2b448ee912458efa264fcfd12c301057594ccd48
Niko Sams <ns@vivid-planet.com>
2008-01-08 10:46:02 +0000

    standard-breite auf 30px

--- Vps/Auto/Grid/Column/Button.php
+++ Vps/Auto/Grid/Column/Button.php
@@ -1,7 +1,7 @@
 <?php
 class Vps_Auto_Grid_Column_Button extends Vps_Auto_Grid_Column
 {
-    public function __construct($dataIndex = null, $header = '', $width = 50)
+    public function __construct($dataIndex = null, $header = '', $width = 30)
     {
         parent::__construct($dataIndex, $header, $width);
         $this->setShowIn(self::SHOW_IN_GRID);
