9294aef3fa83a1b576593fe3c4da754486338ed0
Niko Sams <ns@vivid-planet.com>
2008-01-10 14:35:37 +0000

    - eigene Column für Date um richtiges ausgabeformat für export zu haben

---
+++ Vps/Auto/Grid/Column/Date.php
@@ -0,0 +1,18 @@
+<?php
+class Vps_Auto_Grid_Column_Date extends Vps_Auto_Grid_Column
+{
+    public function __construct($dataIndex = null, $header = null, $width = 70)
+    {
+        parent::__construct($dataIndex, $header, $width);
+        $this->setType('date');
+        $this->setRenderer('localizedDate');
+    }
+
+    public function load($row, $role)
+    {
+        $ret = parent::load($row, $role);
+        $ret = substr($ret, 0, 10);
+        //todo: datum formatieren wenn export; übersetzung berücksichtigen
+        return $ret;
+    }
+}
