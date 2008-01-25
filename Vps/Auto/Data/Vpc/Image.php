0011add6b1d9d0bcf7d8085d12dd5d59c368cab6
Franz Unger <ufx@vivid-planet.com>
2008-01-25 12:25:21 +0000

    Vpc_Composite_Images erbt jetzt von Vpc_Abstract_List

--- Vps/Auto/Data/Vpc/Image.php
+++ Vps/Auto/Data/Vpc/Image.php
@@ -19,7 +19,7 @@ class Vps_Auto_Data_Vpc_Image extends Vps_Auto_Data_Abstract
         $componentKey = $this->_componentKey . '-' . $row->id;
         $row = $table->find($this->_pageId, $componentKey)->current();
         if ($row) {
-            return '<img src="' . $row->getImageUrl(null, 'mini') . '" />';
+            return '<img src="' . $row->getFileUrl(null, 'mini') . '" />';
         } else {
             return '';
         }
