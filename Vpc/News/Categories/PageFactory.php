528ab0b43036d12ac8d50d91c7e4e91ec3633551
Markus Hauser <mh@vivid-planet.com>
2008-01-29 17:38:41 +0000

    ChildComponentClass dynamisch

--- Vpc/News/Categories/PageFactory.php
+++ Vpc/News/Categories/PageFactory.php
@@ -5,6 +5,15 @@ class Vpc_News_Categories_PageFactory extends Vpc_Abstract_TablePageFactory
     protected $_componentClass = 'Vpc_News_Categories_Category_Component';
     protected $_additionalFactories = array();
 
+    protected function _init()
+    {
+        parent::_init();
+        $childComponentClasses = Vpc_Abstract::getSetting(
+            get_class($this->_component), 'childComponentClasses'
+        );
+        $this->_componentClass = $childComponentClasses['details'];
+    }
+
     protected function _getWhere()
     {
         $where = array(
