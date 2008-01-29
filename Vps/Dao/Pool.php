3d9a5ad052bb6f8e76e14675e324f3e7080f86c2
Franz Unger <ufx@vivid-planet.com>
2008-01-29 14:00:56 +0000

    Vps_Auto_Field_PoolSelect (fertig) und Vps_Auto_Field_PoolMulticheckbox (nicht fertig)

--- Vps/Dao/Pool.php
+++ Vps/Dao/Pool.php
@@ -2,4 +2,5 @@
 class Vps_Dao_Pool extends Vps_Db_Table
 {
     protected $_name = 'vps_pools';
+    protected $_rowClass = 'Vps_Dao_Row_Pool';
 }
---
+++ Vps/Dao/Row/Pool.php
@@ -0,0 +1,8 @@
+<?php
+class Vps_Dao_Row_Pool extends Vps_Db_Table_Row_Abstract
+{
+    public function __toString()
+    {
+        return $this->value;
+    }
+}
