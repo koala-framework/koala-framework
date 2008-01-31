8973cabcaae1d20d2f3ee754ab8030d0323e7c71
Niko Sams <ns@vivid-planet.com>
2008-01-31 10:14:47 +0000

    - mabySave bugfix

--- Vps_js/Panel.js
+++ Vps_js/Panel.js
@@ -2,7 +2,7 @@ Ext.Panel.prototype.mabySubmit = function() {
     var ret = true;
     if (this.items) {
         this.items.each(function(i) {
-            if (i.mabySubmit && !i.mabySubmit()) {
+            if (i.mabySubmit && !i.mabySubmit.apply(i, arguments)) {
                 ret = false;
                 return true;
             }
