d06e66fcc515432069985363190bde4178a15b1d
Niko Sams <ns@vivid-planet.com>
2008-02-07 07:16:29 +0000

    component-icons

--- Vpc/Basic/LinkTag/Abstract/Component.php
+++ Vpc/Basic/LinkTag/Abstract/Component.php
@@ -4,7 +4,7 @@ abstract class Vpc_Basic_LinkTag_Abstract_Component extends Vpc_Abstract
     public static function getSettings()
     {
         return array_merge(parent::getSettings(), array(
-            'componentIcon' => new Vps_Asset('page_white_link')
+            'componentIcon' => new Vps_Asset('page_link')
         ));
     }
 }
