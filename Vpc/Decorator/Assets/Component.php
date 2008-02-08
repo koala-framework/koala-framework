15e7dfb602b3d2c097e5a3770b2637d7742d74b1
Niko Sams <ns@vivid-planet.com>
2008-02-08 14:45:11 +0000

    bugfix: googlemap-assets ist nicht in AllFrontend

--- Vpc/Decorator/Assets/Component.php
+++ Vpc/Decorator/Assets/Component.php
@@ -4,19 +4,11 @@ class Vpc_Decorator_Assets_Component extends Vpc_Decorator_Abstract
     public function getTemplateVars()
     {
         $return = parent::getTemplateVars();
-        
+
         $dep = new Vps_Assets_Dependencies('Frontend');
-        
-        $cfg = Zend_Registry::get('config');
-        if ($cfg->debug->assets) {
-            
-            $jsFiles = $dep->getAssetFiles('js');
-            $cssFiles = $dep->getAssetFiles('css');
-        } else {
-            $v = $cfg->application->version;
-            $jsFiles = array('/assets/AllFrontend.js?v='.$v);
-            $cssFiles = array('/assets/AllFrontend.css?v='.$v);
-        }
+
+        $jsFiles = $dep->getAssetFiles('js');
+        $cssFiles = $dep->getAssetFiles('css');
         $return['assets']['js'] = array_merge($return['assets']['js'], $jsFiles);
         $return['assets']['css'] = array_merge($return['assets']['css'], $cssFiles);
 
