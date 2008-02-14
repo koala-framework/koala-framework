70f96d4634e2de13d872739df059acb989b73317
Niko Sams <ns@vivid-planet.com>
2008-02-14 13:29:12 +0000

    neues autogrid window

---
+++ Vps_js/Auto/Grid/Window.js
@@ -0,0 +1,41 @@
+Ext.namespace('Vps.Auto.Grid');
+
+Vps.Auto.Grid.Window = Ext.extend(Ext.Window, {
+    layout: 'fit',
+    modal: true,
+    closeAction: 'hide',
+    initComponent : function()
+    {
+        this.autoGrid = new Vps.Auto.GridPanel({
+            controllerUrl: this.controllerUrl,
+            autoLoad: false
+        });
+        this.items = [this.autoGrid];
+
+        Vps.Auto.Grid.Window.superclass.initComponent.call(this);
+    },
+
+    showEdit: function(record) {
+        this.show();
+        this.autoGrid.load();
+    },
+
+    getAutoGrid : function()
+    {
+        return this.autoGrid;
+    },
+
+    getGrid : function()
+    {
+        return this.getAutoGrid().getGrid();
+    },
+
+    getBaseParams: function()
+    {
+        return this.getAutoGrid().getBaseParams();
+    },
+    applyBaseParams: function(p)
+    {
+        this.getAutoGrid().applyBaseParams(p);
+    }
+});
