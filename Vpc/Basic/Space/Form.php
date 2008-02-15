ec758fe8e339b2b0d5ce48cb6346938dc4c98857
Markus Hauser <mh@vivid-planet.com>
2008-02-15 09:42:27 +0000

    Pixelangabe bei Space Komponente nur Nummern möglich

--- Vpc/Basic/Space/Form.php
+++ Vpc/Basic/Space/Form.php
@@ -4,8 +4,10 @@ class Vpc_Basic_Space_Form extends Vps_Auto_Vpc_Form
     public function __construct($class, $id = null)
     {
         parent::__construct($class, $id);
-        $this->fields->add(new Vps_Auto_Field_TextField('height'))
+        $this->fields->add(new Vps_Auto_Field_NumberField('height'))
             ->setFieldLabel('Height')
-            ->setWidth(80);
+            ->setWidth(80)
+            ->setAllowNegative(false)
+            ->setAllowDecimals(false);
     }
 }
