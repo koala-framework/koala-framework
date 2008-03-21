450a780724c149c296026adb56a7a0feabfba721
Markus Hauser <mh@vivid-planet.com>
2008-03-21 11:43:11 +0000

    Forum Gruppe ist nicht mehr löschbar wenn sich darin noch themen befinden

--- Vpc/Forum/Controller.php
+++ Vpc/Forum/Controller.php
@@ -10,4 +10,21 @@ class Vpc_Forum_Controller extends Vps_Controller_Action_Auto_Vpc_Tree
     protected $_editDialog = array('controllerUrl'=>'/admin/component/edit/Vpc_Forum_Form',
                                    'width'=>450,
                                    'height'=>200);
+
+    public function jsonDeleteAction()
+    {
+        $id = $this->getRequest()->getParam('id');
+        $row = $this->_table->find($id)->current();
+
+        $threadTable = new Vpc_Forum_Thread_Model();
+        $threadRow = $threadTable->fetchRow(
+            array('component_id = ?' => $row->component_id.'_'.$row->id)
+        );
+
+        if ($threadRow) {
+            $this->view->error = 'Gruppe kann nicht gelöscht werden - es sind noch Themen vorhanden.';
+        } else {
+            parent::jsonDeleteAction();
+        }
+    }
 }
