1e292e371ee033d2be00fafd6349cc1e0b1f5567
Markus Hauser <mh@vivid-planet.com>
2008-03-25 12:42:46 +0000

    trl entfernt

--- Vpc/Basic/Image/Enlarge/Form.php
+++ Vpc/Basic/Image/Enlarge/Form.php
@@ -10,9 +10,9 @@ class Vpc_Basic_Image_Enlarge_Form extends Vpc_Basic_Image_Form
             $classes = Vpc_Abstract::getSetting($class, 'childComponentClasses');
             $image = new Vpc_Basic_Image_Form($classes['smallImage'], $childId);
             $image->fields->getByName('vps_upload_id')->setFileFieldLabel(trlVps('File (optional)'));
-            $this->add(new Vps_Auto_Container_FieldSet('Small Image (optional)'))
+            $this->add(new Vps_Auto_Container_FieldSet(trlVps('Small Image (optional)')))
                 ->setCheckboxToggle(true)
-                ->setCheckboxName(trlVps('enlarge'))
+                ->setCheckboxName('enlarge')
                 ->add($image);
         }
     }
