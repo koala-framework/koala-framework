4695441990af68daba3aef14e1795de49e31e7df
Lorenz Maislinger <lm@vivid-planet.com>
2008-03-26 10:38:23 +0000

    Ausbessern von falscher Vererbung

--- Vps/Auto/Field/ComboBoxText.php
+++ Vps/Auto/Field/ComboBoxText.php
@@ -1,10 +1,10 @@
 <?php
-class Vps_Auto_Field_ComboBoxText extends Vps_Auto_Field_SimpleAbstract
+class Vps_Auto_Field_ComboBoxText extends Vps_Auto_Field_ComboBox
 {
     public function __construct($field_name = null, $field_label = null)
     {
         parent::__construct($field_name, $field_label);
-        $this->setXtype('combobox');
+        $this->setValueField(false);
     }
 
     protected function _addValidators()
@@ -12,38 +12,4 @@ class Vps_Auto_Field_ComboBoxText extends Vps_Auto_Field_SimpleAbstract
         Vps_Auto_Field_SimpleAbstract::_addValidators();
     }
 
-    public function getMetaData()
-    {
-        $ret = parent::getMetaData();
-        if (isset($ret[0]['storeUrl'])) {
-            $ret[0]['store'] = array('url' => $ret[0]['storeUrl']);
-        }
-        return $ret;
-    }
-
-    public function setValues($data)
-    {
-        if (is_string($data)) {
-            return $this->setStore(array('url' => $data));
-        } else if ($data instanceof Vps_Db_Table_Rowset) {
-            $data = $data->toStringDataArray();
-            return $this->setStore(array('data' => $data));
-        } else if (is_array($data)) {
-            $d = array();
-            foreach ($data as $k=>$i) {
-                if (!is_array($i)) {
-                    $d[] = array($k, $i);
-                } else {
-                    $d[] = $i;
-                }
-            }
-            return $this->setStore(array('data' => $d));
-        }
-    }
-    protected function _getValueFromPostData($postData)
-    {
-        $ret = parent::_getValueFromPostData($postData);
-        if ($ret == '' || $ret == 'null') $ret = null;
-        return $ret;
-    }
 }
