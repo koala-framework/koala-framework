7adbebd8b5715b88e49fefad2eff97b9d3f62d99
Niko Sams <ns@vivid-planet.com>
2008-03-26 17:08:20 +0000

    bugfix, forms brauchen einen namen

--- Vps/Controller/Action/Auto/Vpc/Form.php
+++ Vps/Controller/Action/Auto/Vpc/Form.php
@@ -7,7 +7,7 @@ abstract class Vps_Controller_Action_Auto_Vpc_Form extends Vps_Controller_Action
     public function preDispatch()
     {
         if (!isset($this->_form)) {
-            $this->_form = new $this->_formName($this->class, $this->componentId);
+            $this->_form = new $this->_formName('form', $this->class, $this->componentId);
         }
         $this->_form->setBodyStyle('padding: 10px');
         parent::preDispatch();
