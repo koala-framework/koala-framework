b0890b9c8fc90dbd5eb187659c07fc388bf83cdb
Niko Sams <ns@vivid-planet.com>
2008-03-26 07:26:22 +0000

    ImageViewer verwendet jetzt ShowField (ist einfacher und behebt bugs)

--- Vps_js/Form/ImageViewer.js
+++ Vps_js/Form/ImageViewer.js
@@ -1,23 +1,13 @@
-Vps.Form.ImageViewer = Ext.extend(Ext.form.Field,
+Vps.Form.ImageViewer = Ext.extend(Vps.Form.ShowField,
 {
-    onRender : function(ct, position){
-        this.el = ct.createChild('<span class="x-form-field" id="' + this.name + '"></span>', position);
-        this.rendered = true;
-        this.el.dom.name = this.name;
-    },
-
-    setValue : function(value)
-    {
-        var src = '';
-        if (value.previewUrl) {
-            var rand = Math.floor((Math.random()*1000000));
-            if (value.imageUrl) { src += '<a href="' + value.imageUrl + '?' + rand + '" target="_blank">'; }
-            src += '<img src="' + value.previewUrl + '?' + rand + '" />'
-            if (value.imageUrl) { src += '</a>'; }
-        }
-        src += '<input style="visibility:hidden; width:0px;">';
-        Ext.get(this.name).dom.innerHTML = src;
-    }
-
+    tpl: '<tpl if="previewUrl">'+
+            '<tpl if="imageUrl">'+
+                '<a href="{imageUrl}?{[Math.random()]}" target="_blank">'+
+            '</tpl>'+
+            '<img src="{previewUrl}?{[Math.random()]}" />'+
+            '<tpl if="imageUrl">'+
+                '</a>'+
+            '</tpl>'+
+         '</tpl>'
 });
 Ext.reg('imageviewer', Vps.Form.ImageViewer);
