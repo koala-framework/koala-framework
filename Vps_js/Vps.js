d4deac41e4ec95a1c698ca6bef47f3e14f6537bd
Niko Sams <ns@vivid-planet.com>
2008-03-27 08:56:52 +0000

    möglicherweise nützliche hilfs-funktionen

--- Vps_js/Vps.js
+++ Vps_js/Vps.js
@@ -28,6 +28,50 @@ Ext.applyIf(Array.prototype, {
     }
 });
 
+Ext.applyIf(Function.prototype, {
+
+    interceptResult: function(fcn, scope) {
+        if(typeof fcn != "function"){
+            return this;
+        }
+        var method = this;
+        var interception=function() {
+            var retval = method.apply(this || window, arguments);
+            var callArgs = Array.prototype.slice.call(arguments, 0);
+            var args=[retval].concat(callArgs);
+            var newRetval=fcn.apply(scope || this || window, args);
+            return newRetval;
+        };
+        if (this.prototype){
+            Ext.apply(interception.prototype, this.prototype) 
+            if (this.superclass){ interception.superclass=this.superclass; }
+            if (this.override){ interception.override=this.override; }
+        }
+        return interception;
+    }
+});
+
+//http://extjs.com/forum/showthread.php?t=26644
+Vps.clone = function(o) {
+    if('object' !== typeof o) {
+        return o;
+    }
+    var c = 'function' === typeof o.pop ? [] : {};
+    var p, v;
+    for(p in o) {
+        if(o.hasOwnProperty(p)) {
+            v = o[p];
+            if('object' === typeof v) {
+                c[p] = Ext.ux.clone(v);
+            }
+            else {
+                c[p] = v;
+            }
+        }
+    }
+    return c;
+};
+
 Ext.onReady(function()
 {
 //     Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
