629e9a54f2f458221c8ecf7585464be5cf1e06d9
Markus Hauser <mh@vivid-planet.com>
2008-04-02 12:04:19 +0000

    Thumbnails werden auf 100x75 gecropt

--- Vpc/Basic/Image/Thumb/Component.php
+++ Vpc/Basic/Image/Thumb/Component.php
@@ -4,7 +4,8 @@ class Vpc_Basic_Image_Thumb_Component extends Vpc_Basic_Image_Component
     public static function getSettings()
     {
         $settings = parent::getSettings();
-        $settings['dimension'] = array(100, 100);
+        $settings['dimension'] = array(100, 75);
+        $settings['scale'] = array(Vps_Media_Image::SCALE_CROP);
         return $settings;
     }
 }
