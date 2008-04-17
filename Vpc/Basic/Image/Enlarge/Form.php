997e686a4a7540e5f88a72e71d6cf4b5648ec277
Niko Sams <ns@vivid-planet.com>
2008-04-17 07:42:12 +0000

    - fieldset für kleines bild immer anzeigen - größe als pflichtfeld

--- Vpc/Basic/Image/Enlarge/Form.php
+++ Vpc/Basic/Image/Enlarge/Form.php
@@ -4,16 +4,13 @@ class Vpc_Basic_Image_Enlarge_Form extends Vpc_Basic_Image_Form
     public function __construct($name, $class, $id = null)
     {
         parent::__construct($name, $class, $id);
-// d($class);
-// d(Vpc_Abstract::getSetting($class, 'hasSmallImageComponent'));
+
         if (Vpc_Abstract::getSetting($class, 'hasSmallImageComponent')) {
             $classes = Vpc_Abstract::getSetting($class, 'childComponentClasses');
-            $image = new Vpc_Basic_Image_Form('small', $classes['smallImage']);
+            $image = Vps_Auto_Vpc_Form::createComponentForm('smallImage', $classes['smallImage']);
             $image->setComponentIdTemplate('{0}-1');
             $image->fields->getByName('vps_upload_id')->setFileFieldLabel(trlVps('File (optional)'));
-            $this->add(new Vps_Auto_Container_FieldSet(trlVps('Small Image (optional)')))
-                ->setCheckboxToggle(true)
-                ->setCheckboxName('enlarge')
+            $this->add(new Vps_Auto_Container_FieldSet(trlVps('Small Image')))
                 ->add($image);
         }
     }
