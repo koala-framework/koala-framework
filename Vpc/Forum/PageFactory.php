6b2e1a3d6b0804eecbc2bd35713b862eda28927d
Niko Sams <ns@vivid-planet.com>
2008-04-21 06:11:08 +0000

    - Forum-Suche

--- Vpc/Forum/PageFactory.php
+++ Vpc/Forum/PageFactory.php
@@ -4,6 +4,7 @@ class Vpc_Forum_PageFactory extends Vpc_Abstract_PageFactory
     protected $_additionalFactories = array(
         'Vpc_Forum_GroupsPageFactory',
         'Vpc_Forum_UserPageFactory',
-        'Vpc_Forum_FeedPageFactory'
+        'Vpc_Forum_FeedPageFactory',
+        'Vpc_Forum_SearchPageFactory',
     );
 }
