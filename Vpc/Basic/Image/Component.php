17c79170ebdb944d26974de8d2796db8c38490a7
Franz Unger <ufx@vivid-planet.com>
2008-05-19 13:37:00 +0000

    Defaultwerte für Komponenten

--- Vpc/Basic/Image/Component.php
+++ Vpc/Basic/Image/Component.php
@@ -8,7 +8,7 @@ class Vpc_Basic_Image_Component extends Vpc_Abstract
             'componentIcon'     => new Vps_Asset('picture'),
             'tablename'         => 'Vpc_Basic_Image_Model',
             'dimension'         => array(300, 200), // Leeres Array -> freie Wahl, array(width, height), array(array(width, height), null: Bild in Originalgröße)
-            'scale'             => array(Vps_Media_Image::SCALE_BESTFIT),
+            'allow'             => array(Vps_Media_Image::SCALE_BESTFIT),
             'ouputDimensions'   => array('mini'  => array(20, 20, Vps_Media_Image::SCALE_BESTFIT),
                                          'thumb' => array(100, 100, Vps_Media_Image::SCALE_BESTFIT)),
             'editComment'       => false,
@@ -16,7 +16,8 @@ class Vpc_Basic_Image_Component extends Vpc_Abstract
             'allowBlank'        => true,
             'default'           => array(
                 'filename'   => 'filename'
-            )
+            ),
+            'extensions'        => array('jpg')
         ));
         return $settings;
     }
