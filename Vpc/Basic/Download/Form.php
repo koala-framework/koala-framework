03851d25b4217ce090ab09c1c463aa3491727530
Niko Sams <ns@vivid-planet.com>
2008-05-27 08:17:10 +0000

    nur eine textzeile sattt ener TextArea

--- Vpc/Basic/Download/Form.php
+++ Vpc/Basic/Download/Form.php
@@ -9,8 +9,7 @@ class Vpc_Basic_Download_Form extends Vpc_Abstract_Form
         $form->setIdTemplate('{0}-downloadTag');
         $this->add($form);
 
-        $this->add(new Vps_Form_Field_TextArea('infotext', trlVps('Infotext')))
-            ->setWidth(300)
-            ->setGrow(true);
+        $this->add(new Vps_Form_Field_TextField('infotext', trlVps('Link-Text')))
+            ->setWidth(300);
     }
 }
