4d08c195d6ca12894d2a9264f576f986c8befe21
Niko Sams <ns@vivid-planet.com>
2008-05-28 14:38:14 +0000

    exception für bessere fehlermeldung

--- Vpc/Abstract/Form.php
+++ Vpc/Abstract/Form.php
@@ -16,6 +16,9 @@ class Vpc_Abstract_Form extends Vps_Form
     public static function createComponentForm($name, $class)
     {
         $f = Vpc_Admin::getComponentClass($class, 'Form');
+        if (!$f) {
+            throw new Vps_Exception("No Form for Component '$class' found");
+        }
         return new $f($name, $class);
     }
 }
