c97066348f74a7ca66e1cd6305d41a1ff52848a4
Niko Sams <ns@vivid-planet.com>
2008-05-29 14:01:57 +0000

    hilfetexte

--- Vpc/Basic/DownloadTag/Form.php
+++ Vpc/Basic/DownloadTag/Form.php
@@ -8,7 +8,8 @@ class Vpc_Basic_DownloadTag_Form extends Vpc_Abstract_Form
         $this->fields->add(new Vps_Form_Field_TextField('filename', trlVps('Filename')))
             ->setAllowBlank(false)
             ->setVtype('alphanum')
-            ->setIsFilename(true); //um es im JavaScript zu finden
+            ->setIsFilename(true) //um es im JavaScript zu finden
+            ->setHelpText(hlpVps('vps_download_filename'));
         $this->fields->add(new Vps_Form_Field_File('vps_upload_id', trlVps('File')))
             ->setDirectory('BasicDownloadTag')
             ->setAllowBlank(false)
