688e5f422af0c27a2a376689400394b0c6b44178
Niko Sams <ns@vivid-planet.com>
2008-05-30 09:56:46 +0000

    bugfixes, übersetzung etc

--- Vpc/Basic/Image/Form.php
+++ Vpc/Basic/Image/Form.php
@@ -44,7 +44,7 @@ class Vpc_Basic_Image_Form extends Vpc_Abstract_Form
         }
 
         // Fileupload
-        $this->add(new Vps_Form_Field_File('vps_upload_id', trlVps('File')))
+        $this->add(new Vps_Form_Field_File('vps_upload_id', trlVps('Image')))
             ->setExtensions(Vpc_Abstract::getSetting($class, 'extensions'))
             ->setAllowBlank(Vpc_Abstract::getSetting($class, 'allowBlank'))
             ->setAllowOnlyImages(true);
