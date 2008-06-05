4a83279c89748727927334ed6ee4605c384cdda5
Niko Sams <ns@vivid-planet.com>
2008-06-05 12:15:05 +0000

    eigene select klasse, kann array in where verarbeiten

---
+++ Vps/Db/Table/Select.php
@@ -0,0 +1,23 @@
+<?php
+class Vps_Db_Table_Select extends Zend_Db_Table_Select
+{
+    public function where($cond, $value = null, $type = null)
+    {
+        if (is_array($cond)) {
+            foreach ($cond as $key => $val) {
+                // is $key an int?
+                if (is_int($key)) {
+                    // $val is the full condition
+                    $this->where($val);
+                } else {
+                    // $key is the condition with placeholder,
+                    // and $val is quoted into the condition
+                    $this->where($key, $val);
+                }
+            }
+            return $this;
+        } else {
+            return parent::where($cond, $value, $type);
+        }
+    }
+}
