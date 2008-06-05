80939cf63ffa0a6dd4a74b939a47c8401a5d6849
Niko Sams <ns@vivid-planet.com>
2008-06-05 12:17:13 +0000

    cli: stderr verwenden, clientException schön ausgeben

--- Vps/Controller/Action/Error/CliController.php
+++ Vps/Controller/Action/Error/CliController.php
@@ -7,11 +7,16 @@ class Vps_Controller_Action_Error_CliController extends Vps_Controller_Action
         if ($errors->type == Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER ||
             $errors->type == Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION ||
             $errors->exception instanceof Vps_Controller_Action_Web_FileNotFoundException) {
-            echo "ERROR: Controller not found\n\n";
-            $this->_forward('index', 'help', 'vps_controller_action_cli');
+            if ($errors->type == Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER) {
+                file_put_contents('php://stderr', "ERROR: Controller not found\n");
+            } else if ($errors->type == Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION) {
+                file_put_contents('php://stderr', "ERROR: Action not found\n");
+            }
+        } else if ($errors->exception instanceof Vps_ClientException) {
+            file_put_contents('php://stderr', $errors->exception->getMessage()."\n");
         } else {
-            echo $errors->exception->__toString();
-            exit;
+            file_put_contents('php://stderr', $errors->exception->__toString()."\n");
         }
+        exit(1);
     }
 }
