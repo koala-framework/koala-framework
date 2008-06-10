0a20930514f571e20f523cfe9a3119bf5eda0214
Niko Sams <ns@vivid-planet.com>
2008-06-10 13:47:58 +0000

    news im admin

--- Vpc/News/Category/Directory/NewsEditForm.php
+++ Vpc/News/Category/Directory/NewsEditForm.php
@@ -1,35 +1,15 @@
 <?php
-class Vpc_News_Category_Directory_NewsEditForm extends Vps_Auto_Form
+class Vpc_News_Category_Directory_NewsEditForm extends Vps_Form_NonTableForm
 {
     public function __construct($class, $id = null)
     {
         parent::__construct($class, $id);
 
-        $tablename = Vpc_Abstract::getSetting($class, 'tablename');
-        $this->_table = new $tablename(array('componentClass'=>$class));
-
-        $this->add(new Vps_Auto_Field_PoolMulticheckbox('Vpc_News_Categories_NewsToPoolModel', 'Categories'))
-             ->setPool(Vpc_Abstract::getSetting($class, 'pool'));
+        $this->add(new Vps_Form_Field_PoolMulticheckbox('Vpc_News_Category_Directory_NewsToCategoriesModel', trlVps('Categories')))
+             ->setPool(Vpc_Abstract::getSetting($class, 'pool'))
+             ->setReferences(array(
+                'columns' => array('news_id'),
+                'refColumns' => array('id')
+            ));
     }
-
-    public function delete($parentRow)
-    {
-        $this->_row = $parentRow;
-        return parent::delete($parentRow);
-    }
-    public function load($parentRow)
-    {
-        $this->_row = $parentRow;
-        return parent::load($parentRow);
-    }
-    public function prepareSave($parentRow, $postData)
-    {
-        $this->_row = $parentRow;
-        return parent::prepareSave($parentRow, $postData);
-    }
-
-    public function save($parentRow, $postData)
-    {
-        $this->_row = $parentRow;
-        return parent::save($parentRow, $postData);
-    }}
+}
