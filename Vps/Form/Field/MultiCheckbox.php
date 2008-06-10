657199bae6929fa08a5d5c9677335937ca479ca1
Niko Sams <ns@vivid-planet.com>
2008-06-10 13:47:46 +0000

    fix, funktionierte nicht mehr mit models

--- Vps/Form/Field/MultiCheckbox.php
+++ Vps/Form/Field/MultiCheckbox.php
@@ -3,16 +3,41 @@
 class Vps_Form_Field_MultiCheckbox extends Vps_Form_Field_Abstract
 {
     protected $_fields;
+    private $_references;
+    private $_model;
 
     public function __construct($tableName = null, $title = null)
     {
         parent::__construct();
-        $this->setTableName($tableName);
+
+        if (is_object($tableName)) {
+            $model = $tableName;
+        } else if (class_exists($tableName)) {
+            $model = new $tableName();
+        }
+        parent::__construct(get_class($model));
+        if ($model instanceof Zend_Db_Table_Abstract) {
+            $model = new Vps_Model_Db(array(
+                'table' => $model
+            ));
+        }
+        $this->setModel($model);
+
         if ($title) $this->setTitle($title);
         $this->setHideLabels(true);
         $this->setAutoHeight(true);
         $this->setLayout('form');
         $this->setXtype('fieldset');
+    }
+
+    public function setModel($model)
+    {
+        $this->_model = $model;
+    }
+
+    public function getModel()
+    {
+        return $this->_model;
     }
 
     public function getMetaData()
@@ -27,7 +52,7 @@ class Vps_Form_Field_MultiCheckbox extends Vps_Form_Field_Abstract
     protected function _getFields()
     {
         if (!isset($this->_fields)) {
-            $this->_fields = new Vps_Collection();
+            $this->_fields = new Vps_Collection_FormFields();
             $info = $this->getValues()->getTable()->info();
             $pk = $info['primary'][1];
             foreach ($this->getValues() as $i) {
@@ -58,13 +83,47 @@ class Vps_Form_Field_MultiCheckbox extends Vps_Form_Field_Abstract
         }
         return $name;
     }
-
+    protected function _getRowsByRow(Vps_Model_Row_Interface $row)
+    {
+        if ($this->_model instanceof Vps_Model_FieldRows) {
+            $rows = $this->_model->fetchByParentRow($row);
+        } else {
+            $pk = $row->getModel()->getPrimaryKey();
+            if (!$row->$pk) {
+                //neuer eintrag (noch keine id)
+                return array();
+            }
+            $ref = $this->_getReferences($row);
+            $where = array();
+            foreach (array_keys($ref['columns']) as $k) {
+                $where["{$ref['columns'][$k]} = ?"] = $row->{$ref['refColumns'][$k]};
+            }
+            $rows = $this->_model->fetchAll($where);
+        }
+        return $rows;
+    }
+    protected function _getReferences($row)
+    {
+        if ($this->_references) {
+            return $this->_references;
+        } else if ($this->_model instanceof Vps_Model_Db && $row instanceof Vps_Model_Db_Row) {
+            return $this->_model->getTable()
+                        ->getReference(get_class($row->getRow()->getTable()));
+        } else {
+            throw new Vps_Exception('Couldn\'t read references for Multifields. Either use Vps_Model_FieldRows/Vps_Model_Db or set the References by setReferences().');
+        }
+    }
+    public function setReferences($references)
+    {
+        $this->_references = $references;
+    }
+    
     public function load(Vps_Model_Row_Interface $row)
     {
-        if ((array)$row == array()) return array();
+        if (!$row) return array();
 
-        $selected = $row->findDependentRowset($this->getTableName());
-        $ref = $selected->getTable()->getReference(get_class($this->getValues()->getTable()));
+        $selected = $this->_getRowsByRow($row);
+        $ref = $this->_model->getTable()->getReference(get_class($this->getValues()->getTable()));
         $key = $ref['columns'][0];
 
         $selectedIds = array();
@@ -90,12 +149,12 @@ class Vps_Form_Field_MultiCheckbox extends Vps_Form_Field_Abstract
         if ($this->getAllowBlank() === false && $new == array()) {
             throw new Vps_ClientException("Please select at least one ".$this->getTitle().".");
         }
-        $saved = $row->findDependentRowset($this->getTableName());
+        $saved = $this->_getRowsByRow($row);
 
-        $ref = $saved->getTable()->getReference(get_class($row->getTable()));
+        $ref = $this->_getReferences($row);
         $key1 = $ref['columns'][0];
         
-        $ref = $saved->getTable()->getReference(get_class($this->getValues()->getTable()));
+        $ref = $this->_model->getTable()->getReference(get_class($this->getValues()->getTable()));
         $key2 = $ref['columns'][0];
 
         $avaliableKeys = array();
@@ -115,11 +174,9 @@ class Vps_Form_Field_MultiCheckbox extends Vps_Form_Field_Abstract
             }
         }
 
-        $tableName = $this->getTableName();
-        $table = new $tableName();
         foreach ($new as $id) {
             if (in_array($id, $avaliableKeys)) {
-                $i = $table->createRow();
+                $i = $this->_model->createRow();
                 $i->$key1 = $row->id;
                 $i->$key2 = $id;
                 $i->save();
