d1a22be5ad327de66531d150334a0b32be1d826d
Niko Sams <ns@vivid-planet.com>
2008-06-12 07:36:36 +0000

    array damit in unterklassen nochwas dazugespeichert werden kann

--- Vpc/Abstract/Composite/TreeCache.php
+++ Vpc/Abstract/Composite/TreeCache.php
@@ -8,7 +8,11 @@ class Vpc_Abstract_Composite_TreeCache extends Vpc_TreeCache_Static
         parent::_init();
         $cls = Vpc_Abstract::getSetting($this->_class, 'childComponentClasses');
         foreach ($cls as $id=>$class) {
-            if ($class) $this->_classes[$id] = $class;
+            if ($class) {
+                $this->_classes[$id] = array(
+                    'componentClass' => $class
+                );
+            }
         }
     }
 }
