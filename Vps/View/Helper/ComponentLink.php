6e9e1e8458c50679226a68c592957636b639f0be
Niko Sams <ns@vivid-planet.com>
2008-06-12 07:35:59 +0000

    ausgabe von tc-row objekten auch möglich

--- Vps/View/Helper/ComponentLink.php
+++ Vps/View/Helper/ComponentLink.php
@@ -1,8 +1,17 @@
 <?php
 class Vps_View_Helper_ComponentLink
 {
-    public function componentLink($m)
+    public function componentLink($m, $text = null)
     {
-        return '<a href="'.$m['href'].'" rel="'.$m['rel'].'">'.$m['text'].'</a>';
+        if ($m instanceof Vps_Dao_Row_TreeCache) {
+            $si = $m->getTable()->showInvisible();
+            $m = array(
+                'href' => $si ? $m->url_preview : $m->url,
+                'rel' => $si ? $m->rel_preview : $m->rel,
+                'text' => $m->name
+            );
+        }
+        if (!$text) $text = $m['text'];
+        return '<a href="'.$m['href'].'" rel="'.$m['rel'].'">'.$text.'</a>';
     }
 }
