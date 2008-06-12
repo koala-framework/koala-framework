5a82de418109a35217b331c5fe9ce738b09a11ab
Niko Sams <ns@vivid-planet.com>
2008-06-12 07:35:59 +0000

    ausgabe von tc-row objekten auch möglich

--- Vps/View/Helper/Component.php
+++ Vps/View/Helper/Component.php
@@ -3,6 +3,9 @@ class Vps_View_Helper_Component
 {
     public function component($componentId)
     {
+        if ($componentId instanceof Vps_Dao_Row_TreeCache) {
+            $componentId = $componentId->component_id;
+        }
         return '{nocache: ' . $componentId . '}';
     }
 }
