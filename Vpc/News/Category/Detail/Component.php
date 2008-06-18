c89ec2496570674d7398d990d49c9353eded23c5
Niko Sams <ns@vivid-planet.com>
2008-06-18 07:00:29 +0000

    gruppieren damit jede news nur einmal

--- Vpc/News/Category/Detail/Component.php
+++ Vpc/News/Category/Detail/Component.php
@@ -9,6 +9,7 @@ class Vpc_News_Category_Detail_Component extends Vpc_News_List_Abstract_Componen
                       array());
         $categoryId = $this->getTreeCacheRow()->tag;
         $select->where('vpc_news_to_categories.category_id = ?', $categoryId);
+        $select->group('vps_news.id');
         return $select;
     }
 
