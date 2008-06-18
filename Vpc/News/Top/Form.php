6c034bb2c3655c6a5ede0a6929609277204871d0
Niko Sams <ns@vivid-planet.com>
2008-06-18 06:59:19 +0000

    id gibts nimma

--- Vpc/News/Top/Form.php
+++ Vpc/News/Top/Form.php
@@ -1,9 +1,9 @@
 <?php
 class Vpc_News_Top_Form extends Vpc_Abstract_Form
 {
-    public function __construct($name, $class, $id = null)
+    public function __construct($name, $class)
     {
-        parent::__construct($name, $class, $id);
+        parent::__construct($name, $class);
 
         $tc = new Vps_Dao_TreeCache();
         $news = $tc->findComponentsByParentClass('Vpc_News_Directory_Component');
