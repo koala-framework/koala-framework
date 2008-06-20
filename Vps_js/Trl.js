9c3919ce48eaf309de9e04f32c695fa17cbfce49
Lorenz Maislinger <lm@vivid-planet.com>
2008-06-20 09:19:10 +0000

    Änderungen in der Übersetzung - Umstellung auf neue Struktur

--- Vps_js/Trl.js
+++ Vps_js/Trl.js
@@ -4,16 +4,7 @@ function trl (text, values){
     } else {
 			var cnt = 0;
             values.each(function(value) {
-				if (cnt == 0) text = text.replace(/\{(0)\}/g, value);
-				if (cnt == 1) text = text.replace(/\{(1)\}/g, value);
-				if (cnt == 2) text = text.replace(/\{(2)\}/g, value);
-				if (cnt == 3) text = text.replace(/\{(3)\}/g, value);
-				if (cnt == 4) text = text.replace(/\{(4)\}/g, value);
-				if (cnt == 5) text = text.replace(/\{(5)\}/g, value);
-				if (cnt == 6) text = text.replace(/\{(6)\}/g, value);
-				if (cnt == 7) text = text.replace(/\{(7)\}/g, value);
-				if (cnt == 8) text = text.replace(/\{(8)\}/g, value);
-				if (cnt == 9) text = text.replace(/\{(9)\}/g, value);
+				text = text.replace(new RegExp('\\{('+cnt+')\\}', 'g'), value);
 				cnt++;
 			});
            return text;
@@ -31,16 +22,7 @@ function trlp (single, plural, values){
 			}
 			var cnt = 0;
             values.each(function(value) {
-				if (cnt == 0) text = text.replace(/\{(0)\}/g, value);
-				if (cnt == 1) text = text.replace(/\{(1)\}/g, value);
-				if (cnt == 2) text = text.replace(/\{(2)\}/g, value);
-				if (cnt == 3) text = text.replace(/\{(3)\}/g, value);
-				if (cnt == 4) text = text.replace(/\{(4)\}/g, value);
-				if (cnt == 5) text = text.replace(/\{(5)\}/g, value);
-				if (cnt == 6) text = text.replace(/\{(6)\}/g, value);
-				if (cnt == 7) text = text.replace(/\{(7)\}/g, value);
-				if (cnt == 8) text = text.replace(/\{(8)\}/g, value);
-				if (cnt == 9) text = text.replace(/\{(9)\}/g, value);
+				text = text.replace(new RegExp('\\{('+cnt+')\\}', 'g'), value);
 				cnt++;
 			});
            return text;
