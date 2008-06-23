ab9d71b0b7d273a51d6b266716fb8c69dfbc6bd5
Franz Unger <ufx@vivid-planet.com>
2008-06-23 10:51:34 +0000

    Boxenprioritäten und Boxenvererbung

--- Vpc/Master/Box/Component.php
+++ Vpc/Master/Box/Component.php
@@ -4,9 +4,24 @@ class Vpc_Master_Box_Component extends Vpc_Master_Abstract
     public function getTemplateVars()
     {
         $vars = parent::getTemplateVars();
-        $boxes = $this->getData()->getChildComponents(array('treecache' => 'Vpc_Master_Box_TreeCache'));
-        foreach ($boxes as $box) {
-            $vars['boxes'][$box->id] = $box->componentId;
+        $priorities = array();
+        $page = $this->getData();
+        while ($page) { // Aktuelle inkl. aller Überseiten durchlaufen
+            // Boxen für jeweilige Seite holen, falls Überseite nur die mit inherit==true
+            $constraints = array(
+                'treecache' => 'Vpc_TreeCache_StaticBox',
+                'inherit' => $page->componentId != $this->getData()->componentId
+            );
+            $boxes = $this->getData()->getPage()->getChildBoxes($constraints);
+            // Boxen ausgeben, Priorität speichern, wenn gleiche Box mit höher Priorität gefunden
+            foreach ($boxes as $box) {
+                $id = $box->componentId;
+                if (!isset($priorities[$id] ) || $box->priority > $priorities[$id]) {
+                    $vars['boxes'][$box->id] = $id;
+                    $priorities[$box->id] = $box->priority;
+                }
+            }
+            $page = $page->getParentPage();
         }
         return $vars;
     }
--- Vpc/TreeCache/StaticBox.php
+++ Vpc/TreeCache/StaticBox.php
@@ -1,6 +1,16 @@
 <?php
 class Vpc_TreeCache_StaticBox extends Vpc_TreeCache_Static
 {
+    protected function _acceptKey($key, $constraints)
+    {
+        $ret = parent::_acceptKey($key, $constraints);
+        if ($ret && isset($constraints['inherit'])) {
+            $class = $this->_classes[$key];
+            return !isset($class['inherit']) || ($class['inherit'] == $constraints['inherit']);
+        }
+        return $ret;
+    }
+    
     protected function _formatConfig($parentData, $componentKey)
     {
         $ret = parent::_formatConfig($parentData, $componentKey);
