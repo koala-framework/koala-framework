e48d963476baca29e876d9e9ef209ba65871f5c4
Niko Sams <ns@vivid-planet.com>
2008-06-26 14:36:57 +0000

    löschen bugfix

--- Vpc/Paragraphs/Component.php
+++ Vpc/Paragraphs/Component.php
@@ -9,14 +9,15 @@ class Vpc_Paragraphs_Component extends Vpc_Abstract
 
     public static function getSettings()
     {
-        $settings = array_merge(parent::getSettings(), array(
+        $ret = array_merge(parent::getSettings(), array(
             'componentName' => trlVps('Paragraphs'),
             'componentIcon' => new Vps_Asset('page')
         ));
-        $settings['assetsAdmin']['files'][] = 'vps/Vpc/Paragraphs/Panel.js';
-        $settings['childComponentClasses']['text'] = 'Vpc_Basic_Text_Component';
-        $settings['childComponentClasses']['image'] = 'Vpc_Basic_Image_Component';
-        return $settings;
+        $ret['tablename'] = 'Vpc_Paragraphs_Model';
+        $ret['assetsAdmin']['files'][] = 'vps/Vpc/Paragraphs/Panel.js';
+        $ret['childComponentClasses']['text'] = 'Vpc_Basic_Text_Component';
+        $ret['childComponentClasses']['image'] = 'Vpc_Basic_Image_Component';
+        return $ret;
     }
 
     public function getTemplateVars()
