638a71b752bb339258dbe1f83a4c41b7b8e60c15
Franz Unger <ufx@vivid-planet.com>
2008-06-30 09:27:32 +0000

    Bugfix

--- Vpc/Basic/Image/Enlarge/Row.php
+++ Vpc/Basic/Image/Enlarge/Row.php
@@ -20,7 +20,9 @@ class Vpc_Basic_Image_Enlarge_Row extends Vpc_Basic_Image_Row
             $childComponentClass = $childComponentClasses['smallImage'];
             $dimension = Vpc_Abstract::getSetting($childComponentClass, 'dimension');
             $scale = Vpc_Abstract::getSetting($childComponentClass, 'scale');
-            if (is_array($scale)) { $scale = $scale[0]; }
+            if (is_array($scale)) {
+                $scale = isset($scale[0]) ? $scale[0] : null;
+            }
             Vps_Media_Image::scale($source, $target, $dimension, $scale);
         } else {
             parent::_createCacheFile($source, $target, $type);
