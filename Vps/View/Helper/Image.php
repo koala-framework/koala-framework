aa31b7c673ccbbf751b5651b7333b90c5db40b3a
Niko Sams <ns@vivid-planet.com>
2008-07-03 11:29:11 +0000

    css-class argument bei image helper

--- Vps/View/Helper/Image.php
+++ Vps/View/Helper/Image.php
@@ -1,12 +1,20 @@
 <?php
 class Vps_View_Helper_Image
 {
-    public function image($row, $rule = null, $type = 'default', $alt = '')
+    public function image($row, $rule = null, $type = 'default', $alt = '', $cssClass = null)
     {
         $url = $row->getFileUrl($rule, $type);
+        $attr = '';
+        if (is_string($cssClass)) {
+            $attr .= ' class="'.$cssClass.'"';
+        } else if (is_array($cssClass)) {
+            foreach ($cssClass as $k=>$i) {
+                $attr .= ' '.$k.'="'.$i.'"';
+            }
+        }
         $size = $row->getImageDimensions($rule, $type);
         if ($url) {
-            return "<img src=\"$url\" width=\"$size[width]\" height=\"$size[height]\" alt=\"$alt\" />";
+            return "<img src=\"$url\" width=\"$size[width]\" height=\"$size[height]\" alt=\"$alt\"$attr />";
         } else {
             return '';
         }
