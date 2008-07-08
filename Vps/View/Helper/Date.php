844dee77a3c661f24d39b19ff529d043be86fde7
Christian Schindlauer <cs@vivid-planet.com>
2008-07-08 11:30:27 +0000

    leeres datum nicht ausgeben als 1.1.70

--- Vps/View/Helper/Date.php
+++ Vps/View/Helper/Date.php
@@ -3,6 +3,7 @@ class Vps_View_Helper_Date
 {
     public function date($date)
     {
+        if (!$date) return '';
         if (is_string($date)) $date = strtotime($date);
         return date(trlVps('Y-m-d'), $date);
     }
