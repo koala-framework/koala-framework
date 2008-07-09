89f197a762ef505cac9ad207e97afdb916343d30
Franz Unger <ufx@vivid-planet.com>
2008-07-09 10:02:54 +0000

    Header für PDF geändert

--- Vps/Pdf/TcPdf.php
+++ Vps/Pdf/TcPdf.php
@@ -4,24 +4,24 @@ class Vps_Pdf_TcPdf extends TCPDF
 {
 
     public function getRightMargin()
-	{
+    {
         return $this->rMargin;
     }
 
     public function getLeftMargin()
-	{
+    {
         return $this->lMargin;
     }
 
     public function getTopMargin()
-	{
+    {
         return $this->tMargin;
     }
 
     public function Output ($name='',$dest='')
     {
         if ($dest == 'I' || $dest == 'D') {
-            header('Cache-Control: public');
+            header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
             header('Last-Modified: ' . gmdate("D, d M Y H:i:s \G\M\T", time() - 60*60*24));
             header('Accept-Ranges: none');
         }
@@ -29,12 +29,12 @@ class Vps_Pdf_TcPdf extends TCPDF
     }
 
     public function setPageWidth($value)
-	{
+    {
         $this->w = $value;
     }
 
     public function setPageHeight($value)
-	{
+    {
         $this->h = $value;
     }
 
