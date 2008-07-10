68066ee70db4a83923baf0baa8f3df147881dfa2
Franz Unger <ufx@vivid-planet.com>
2008-07-10 14:53:42 +0000

    Treecache -> Generator Teil 1 - geht nicht

--- Vpc/News/Directory/TreeCacheDetail.php
+++ Vpc/News/Directory/TreeCacheDetail.php
@@ -1,7 +1,5 @@
 <?php
 class Vpc_News_Directory_TreeCacheDetail extends Vps_Component_Generator_TablePage
 {
-    protected $_childClassKey = 'detail';
     protected $_nameColumn = 'title';
-    protected $_dbIdShortcut = 'news_';
 }
