a38c8f4deab0a523a6980cf1feb4b5ef5ca88e68
Christoph Eder <ce@vivid-planet.com>
2008-07-23 13:41:09 +0000

    - Composite Form: wenn komponenten keine eigene form haben auch keine verwenden, getComponentForm kann null zurückgeben (niko)

--- Vpc/Abstract/Composite/Form.php
+++ Vpc/Abstract/Composite/Form.php
@@ -5,14 +5,14 @@ class Vpc_Abstract_Composite_Form extends Vpc_Abstract_NonTableForm
     {
         parent::_initFields();
         $classes = Vpc_Abstract::getChildComponentClasses($this->getClass(), 'child');
-
         foreach ($classes as $key => $class) {
             $form = Vpc_Abstract_Form::createChildComponentForm($this->getClass(), "-$key");
-
-            $name = Vpc_Abstract::getSetting($class, 'componentName');
-            $name = str_replace('.', ' ', $name);
-            $this->add(new Vps_Form_Container_FieldSet($name))
-                ->add($form);
+            if ($form) {
+                $name = Vpc_Abstract::getSetting($class, 'componentName');
+                $name = str_replace('.', ' ', $name);
+                $this->add(new Vps_Form_Container_FieldSet($name))
+                    ->add($form);
+            }
         }
     }
 }
