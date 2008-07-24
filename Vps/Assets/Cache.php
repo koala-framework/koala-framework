7b5c62e2849cea94e0acbdf702834bd5c7ce517f
Niko Sams <ns@vivid-planet.com>
2008-07-24 15:43:06 +0000

    assets cache weiter verbessert

--- Vps/Assets/Cache.php
+++ Vps/Assets/Cache.php
@@ -1,7 +1,8 @@
 <?php
 class Vps_Assets_Cache extends Zend_Cache_Core
 {
-    public function __construct()
+    protected $_checkComponentSettings = true;
+    public function __construct(array $options = array())
     {
         parent::__construct(array(
             'lifetime' => null,
@@ -11,20 +12,33 @@ class Vps_Assets_Cache extends Zend_Cache_Core
             'cache_dir' => 'application/cache/assets'
         ));
         $this->setBackend($backend);
+
+        if (isset($options['checkComponentSettings'])) {
+            $this->_checkComponentSettings = $options['checkComponentSettings'];
+        }
     }
 
     public function load($cacheId)
     {
         $ret = parent::load($cacheId);
-        if ($ret && isset($ret['mtimeFiles'])
-            && Vps_Registry::get('config')->debug->componentCache->checkComponentModification)
+        if ($ret && Vps_Registry::get('config')->debug->componentCache->checkComponentModification)
         {
-            $mtime = 0;
-            foreach ($ret['mtimeFiles'] as $f) {
-                if (filemtime($f) > $ret['mtime']) {
-                    $ret = false;
-                    break;
+            if (isset($ret['mtimeFiles'])) {
+                foreach ($ret['mtimeFiles'] as $f) {
+                    if (file_exists($f) && filemtime($f) > $ret['mtime']) {
+                        $ret = false;
+                        break;
+                    }
                 }
+            }
+            if ($ret && $this->_checkComponentSettings)
+            {
+                if ($ret['mtime'] < Vpc_Abstract::getSettingMtime()) {
+                    $ret = false;
+                }
+            }
+            if ($ret && $ret['mtime'] < Vps_Registry::get('configMtime')) {
+                $ret = false;
             }
         }
         return $ret;
@@ -32,12 +46,16 @@ class Vps_Assets_Cache extends Zend_Cache_Core
 
     public function save(&$cacheData, $cacheId)
     {
+        if (!isset($cacheData['mtime'])) $cacheData['mtime'] = 0;
         if (isset($cacheData['mtimeFiles'])) {
-            if (!isset($cacheData['mtime'])) $cacheData['mtime'] = 0;
             foreach ($cacheData['mtimeFiles'] as $f) {
-                $cacheData['mtime'] = max($cacheData['mtime'], filemtime($f));
+                if (file_exists($f)) {
+                    $cacheData['mtime'] = max($cacheData['mtime'], filemtime($f));
+                }
             }
         }
+        $cacheData['mtime'] = max($cacheData['mtime'], Vpc_Abstract::getSettingMtime());
+        $cacheData['mtime'] = max($cacheData['mtime'], Vps_Registry::get('configMtime'));
         return parent::save($cacheData, $cacheId);
     }
 }
