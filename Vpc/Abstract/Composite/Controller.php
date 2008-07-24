000f2e58d1fd5856169a11857bf0a48c78d27de3
Franz Unger <ufx@vivid-planet.com>
2008-07-24 07:21:27 +0000

    Fixes für Komponenten in Seiteneigenschaften

--- Vpc/Abstract/Composite/Controller.php
+++ Vpc/Abstract/Composite/Controller.php
@@ -1,12 +1,5 @@
 <?php
 class Vpc_Abstract_Composite_Controller extends Vps_Controller_Action_Auto_Vpc_Form
 {
-    protected function _initFields()
-    {
-        $classes = Vpc_Abstract::getChildComponentClasses($this->class);
-        foreach ($classes as $k=>$i) {
-            $form = Vpc_Abstract_Form::createChildComponentForm($this->class, '-'.$k);
-            $this->_form->add($form);
-        }
-    }
+    protected $_formName = 'Vpc_Abstract_Composite_Form';
 }
