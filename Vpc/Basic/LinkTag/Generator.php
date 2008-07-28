300aa8e94cfc6a956b2ef6b51d131fddefb88f88
Christoph Eder <ce@vivid-planet.com>
2008-07-28 12:07:01 +0000

    LinkTag repariert

--- Vpc/Basic/LinkTag/Generator.php
+++ Vpc/Basic/LinkTag/Generator.php
@@ -1,6 +1,9 @@
 <?php
-class Vpc_Basic_LinkTag_Generator extends Vps_Component_Generator_Table
+class Vpc_Basic_LinkTag_Generator extends Vps_Component_Generator_Static
 {
+    protected $_loadTableFromComponent = true;
+
+    
     protected function _formatConstraints($parentData, $constraints)
     {
         //es gibt exakt eine unterkomponente mit der id 'link'
@@ -10,13 +13,41 @@ class Vpc_Basic_LinkTag_Generator extends Vps_Component_Generator_Table
             }
             unset($constraints['id']); //contraint nicht weiterreichen
         }
-        $select = parent::_formatConstraints($parentData, $constraints);
-        if (!$select) return null;
-        return $select;
+        if (isset($constraints['componentClass'])) {
+            throw new Vps_Exception("componentClass constraint not supported for LinkTag");
+        }
+        return parent::_formatConstraints($parentData, $constraints);
     }
 
-    protected function _getIdFromRow($row)
+    protected function _fetchKeys($parentData, $constraints)
     {
-        return 'link';
+        //es gibt exakt eine unterkomponente mit der id 'link'
+        $ret = array();
+        $constraints = $this->_formatConstraints($parentData, $constraints);
+        if (is_null($constraints)) return array();
+        $ret[] = 'link';
+        return $ret;
+    }    
+
+    protected function _formatConfig($parentData, $componentKey)
+    {
+        $componentId = '';
+        if ($parentData->componentId) {
+            $componentId = $parentData->componentId . $this->_idSeparator;
+        }
+        $componentId .= $componentKey;
+        $dbId = '';
+        if ($parentData->dbId) {
+            $dbId = $parentData->dbId . $this->_idSeparator;
+        }
+        $dbId .= $componentKey;
+        $row = $this->_table->find($parentData->componentId)->current();
+        return array(
+            'componentId' => $componentId,
+            'dbId' => $dbId,
+            'componentClass' => $this->_settings['component'][$row->component],
+            'parent' => $parentData,
+            'isPage' => false
+        );
     }
 }
