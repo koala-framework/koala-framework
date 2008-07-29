e260f5deeb784efb83dbd457f106c6e112781b45
Christoph Eder <ce@vivid-planet.com>
2008-07-29 10:21:35 +0000

    Übersetzung für images enlarge component

--- Vpc/Composite/ImagesEnlarge/Component.php
+++ Vpc/Composite/ImagesEnlarge/Component.php
@@ -5,7 +5,7 @@ class Vpc_Composite_ImagesEnlarge_Component extends Vpc_Composite_Images_Compone
     {
         $settings = parent::getSettings();
         $ret['generators']['child']['component'] = 'Vpc_Basic_Image_Enlarge_Title_Component';
-        $settings['componentName'] = 'Images Enlarge';
+        $settings['componentName'] = trlVps('Images Enlarge');
         $settings['assets']['files'][] = 'vps/Vpc/Composite/ImagesEnlarge/Component.js';
         $settings['assets']['dep'][] = 'ExtCore';
         return $settings;
