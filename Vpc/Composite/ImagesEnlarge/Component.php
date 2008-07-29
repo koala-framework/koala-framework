f0bb8454558fbd5bc13a1ee0d15b11ecd0fa3348
Markus Hauser <mh@vivid-planet.com>
2008-07-29 10:50:47 +0000

    $settings durch $ret ausgetauscht

--- Vpc/Composite/ImagesEnlarge/Component.php
+++ Vpc/Composite/ImagesEnlarge/Component.php
@@ -3,12 +3,12 @@ class Vpc_Composite_ImagesEnlarge_Component extends Vpc_Composite_Images_Compone
 {
     public static function getSettings()
     {
-        $settings = parent::getSettings();
+        $ret = parent::getSettings();
         $ret['generators']['child']['component'] = 'Vpc_Basic_Image_Enlarge_Title_Component';
-        $settings['componentName'] = trlVps('Images Enlarge');
-        $settings['assets']['files'][] = 'vps/Vpc/Composite/ImagesEnlarge/Component.js';
-        $settings['assets']['dep'][] = 'ExtCore';
-        return $settings;
+        $ret['componentName'] = trlVps('Images Enlarge');
+        $ret['assets']['files'][] = 'vps/Vpc/Composite/ImagesEnlarge/Component.js';
+        $ret['assets']['dep'][] = 'ExtCore';
+        return $ret;
     }
 
     public function getTemplateVars()
