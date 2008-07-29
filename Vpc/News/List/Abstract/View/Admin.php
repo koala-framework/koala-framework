ed1fd36181f5d7a755a4aa49765fd1b4d5efdb56
Franz Unger <ufx@vivid-planet.com>
2008-07-29 07:58:36 +0000

    Bugfixes Cache, Setting 'clearCacheTable'

--- Vpc/News/List/Abstract/View/Admin.php
+++ Vpc/News/List/Abstract/View/Admin.php
@@ -1,13 +1,10 @@
 <?php
 class Vpc_News_List_Abstract_View_Admin extends Vpc_Admin
 {
-    public function clearCache($caller)
+    public function onRowUpdate($row)
     {
-        if ($caller instanceof Vpc_News_Directory_Row) {
-            $components = Vps_Component_Data_Root::getInstance()->getComponentsByClass('Vpc_News_List_Abstract_View_Component');
-            foreach ($components as $c) {
-                Vps_Component_Cache::getInstance()->remove($c->componentId);
-            }
+        if ($row instanceof Vpc_News_Directory_Row) {
+            Vps_Component_Cache::getInstance()->remove($this->_class);
         }
     }
 }
