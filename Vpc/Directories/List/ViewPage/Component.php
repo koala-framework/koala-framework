abc5d651cd905a40f885a63e7c9ff78d8601b955
Markus Hauser <mh@vivid-planet.com>
2008-08-06 15:02:36 +0000

    Item->ItemPage

---
+++ Vpc/Directories/List/ViewPage/Component.php
@@ -0,0 +1,9 @@
+<?php
+class Vpc_Directories_List_ViewPage_Component extends Vpc_Directories_List_View_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        return $ret;
+    }
+}
