f918997aa338347599baddbb90f8c2904279c815
Markus Hauser <mh@vivid-planet.com>
2008-08-14 08:00:05 +0000

    Radio Field hinzu Kleinere Anpassungen an LabelDecorator Show field für Frontend

---
+++ Vps/Form/Field/Radio.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Auswahl wo man nicht reinschreiben kann, so wie eine HTML-Select-Box
+ **/
+class Vps_Form_Field_Radio extends Vps_Form_Field_ComboBox
+{
+    public function __construct($field_name = null, $field_label = null)
+    {
+        parent::__construct($field_name, $field_label);
+
+        $this->setEditable(false);
+        $this->setTriggerAction('all');
+    }
+
+    // $this->setOutputType($type)
+    //    $type = 'vertical', otherwise horizontal
+
+    public function getTemplateVars($values, $fieldNamePostfix = '')
+    {
+        $ret = parent::getTemplateVars($values, $fieldNamePostfix);
+
+        $name = $this->getFieldName();
+        if (isset($values[$name])) {
+            $value = $values[$name];
+        } else {
+            $value = '';
+        }
+        //todo: escapen
+        $ret['id'] = $name.$fieldNamePostfix;
+        $store = $this->_getStoreData();
+        if ($this->getShowNoSelection()) {
+            array_unshift($store['data'], array('', '('.trlVps('no selection').')'));
+        }
+        $ret['html'] = '';
+        $k = 0;
+        foreach ($store['data'] as $i) {
+            $ret['html'] .= '<input type="radio" class="radio" id="'.$ret['id'].$k++.'" '
+                .'name="'.$name.$fieldNamePostfix.'" value="'.$i[0].'"';
+            if ($i[0] == $value) $ret['html'] .= ' checked="checked"';
+            $ret['html'] .= ' /> '.htmlspecialchars($i[1]);
+            if ($this->getOutputType() == 'vertical' && $k < count($store['data'])) {
+                $ret['html'] .= '<br />';
+            }
+        }
+        return $ret;
+    }
+    public static function getSettings()
+    {
+        return array_merge(parent::getSettings(), array(
+            'componentName' => trlVps('Radio Buttons'),
+            'default' => array(
+                'width' => 100
+            )
+        ));
+    }
+}
