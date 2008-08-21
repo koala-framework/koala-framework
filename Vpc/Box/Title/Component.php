1a73568ec36b6492b6f0ebce5545bf890c326781
Niko Sams <ns@vivid-planet.com>
2008-08-21 06:32:21 +0000

    fkt ausgelagert zum einfacheren überschreiben

--- Vpc/Box/Title/Component.php
+++ Vpc/Box/Title/Component.php
@@ -1,11 +1,15 @@
 <?php
 class Vpc_Box_Title_Component extends Vpc_Abstract
 {
+    protected function _getTitle()
+    {
+        return $this->getData()->getTitle() . " - "
+            . Zend_Registry::get('config')->application->name;
+    }
     public function getTemplateVars()
     {
         $ret = parent::getTemplateVars();
-        $ret['title'] = $this->getData()->getTitle() . " - "
-            . Zend_Registry::get('config')->application->name;
+        $ret['title'] = $this->_getTitle();
         return $ret;
     }
 
