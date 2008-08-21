e8aa1f577700e90e9095a1703771b542c1b68c63
Niko Sams <ns@vivid-planet.com>
2008-08-21 06:33:02 +0000

    konfigurieres idTemplate, fixt problem beim speichern von avatar

--- Vpc/Abstract/Composite/Form.php
+++ Vpc/Abstract/Composite/Form.php
@@ -1,6 +1,11 @@
 <?php
 class Vpc_Abstract_Composite_Form extends Vpc_Abstract_Form
 {
+    protected function _getIdTemplateForChild($key)
+    {
+        return '-'.$key;
+    }
+
     protected function _initFields()
     {
         parent::_initFields();
@@ -9,6 +14,7 @@ class Vpc_Abstract_Composite_Form extends Vpc_Abstract_Form
             if (!$class) continue;
             $form = Vpc_Abstract_Form::createChildComponentForm($this->getClass(), "-$key");
             if ($form) {
+                $form->setIdTemplate($this->_getIdTemplateForChild($key));
                 $name = Vpc_Abstract::getSetting($class, 'componentName');
                 $name = str_replace('.', ' ', $name);
                 $this->add(new Vps_Form_Container_FieldSet($name))
