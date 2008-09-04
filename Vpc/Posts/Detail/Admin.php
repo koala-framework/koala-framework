2dac634d1a4dd657f4a9d1b28cce5a6da6fdadc6
Niko Sams <ns@vivid-planet.com>
2008-09-04 12:42:33 +0000

    fix

--- Vpc/Posts/Detail/Admin.php
+++ Vpc/Posts/Detail/Admin.php
@@ -3,7 +3,7 @@ class Vpc_Posts_Detail_Admin extends Vpc_Abstract_Composite_Admin
 {
     public function onRowUpdate($row)
     {
-        parent::onRowUpdate();
+        parent::onRowUpdate($row);
         if ($row instanceof Vpc_Posts_Directory_Row) {
             Vps_Component_Cache::getInstance()->remove($this->_class, $row->component_id.'-'.$row->id);
         }
