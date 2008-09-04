704224566bdadb7745f08259672f4f6c9b1dc5e6
Franz Unger <ufx@vivid-planet.com>
2008-09-04 15:39:54 +0000

    Beziehungen in Forum-Model wegen Administration

--- Vpc/Forum/Group/ModeratorsModel.php
+++ Vpc/Forum/Group/ModeratorsModel.php
@@ -2,4 +2,16 @@
 class Vpc_Forum_Group_ModeratorsModel extends Vps_Db_Table_Abstract
 {
     protected $_name = 'vpc_forum_moderators';
+    protected $_referenceMap = array(
+        'Group' => array(
+            'columns'           => array('group_id'),
+            'refTableClass'     => 'Vpc_Forum_Directory_Model',
+            'refColumns'        => array('id')
+        ),
+        'User' => array(
+            'columns'           => array('user_id'),
+            'refTableClass'     => 'Vps_Model_User_Users',
+            'refColumns'        => array('id')
+        )
+    );
 }
