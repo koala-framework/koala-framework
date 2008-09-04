79cd1cbccdc229d58bb10908d6fcffb714702e75
Franz Unger <ufx@vivid-planet.com>
2008-09-04 14:21:50 +0000

    RSS-Feeds im Forum funktionieren jetzt

--- Vpc/Forum/FeedList/Component.php
+++ Vpc/Forum/FeedList/Component.php
@@ -6,7 +6,7 @@ class Vpc_Forum_FeedList_Component extends Vpc_Forum_AllPostsList_Component
         $ret = parent::getSettings();
         $ret['generators']['feed'] = array(
             'class' => 'Vps_Component_Generator_Page_Static',
-            'component' => 'Vpc_Directories_List_Feed_Component',
+            'component' => 'Vpc_Forum_FeedList_Feed_Component',
             'name' => trlVps('Feed')
         );
         return $ret;
