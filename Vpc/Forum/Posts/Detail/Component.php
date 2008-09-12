404c7e0989c21670eedc8332d6fdd3bbc68cf1a2
Niko Sams <ns@vivid-planet.com>
2008-09-12 13:20:17 +0000

    - Security: nicht jeder eingeloggte user darf gästebuch einträge bearbeiten - was ins forum gehört auch dorthin verschoben

---
+++ Vpc/Forum/Posts/Detail/Component.php
@@ -0,0 +1,10 @@
+<?php
+class Vpc_Forum_Posts_Detail_Component extends Vpc_Posts_Detail_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['generators']['child']['component']['actions'] = 'Vpc_Posts_Detail_Actions_Component';
+        return $ret;
+    }
+}
