c65f6972483b0668b23c000d80eca247b20c2b24
Niko Sams <ns@vivid-planet.com>
2008-09-16 09:08:02 +0000

    fix parse error

--- Vpc/User/Detail/GeneralCommunity/LastPosts/Admin.php
+++ Vpc/User/Detail/GeneralCommunity/LastPosts/Admin.php
@@ -8,7 +8,7 @@ class Vpc_User_Detail_GeneralCommunity_LastPosts_Admin extends Vpc_Abstract_Comp
             $userDetails =  Vps_Component_Data_Root::getInstance()
                 ->getComponentsByClass('Vpc_User_Detail_Component', array('id'=>'_'.$row->user_id));
             foreach ($userDetails as $detail) {
-                Vps_Component_Cache::getInstance()->remove($detail->getRecursiveChildComponents(array('componentClass'=>$this->_class)))
+                Vps_Component_Cache::getInstance()->remove($detail->getRecursiveChildComponents(array('componentClass'=>$this->_class)));
             }
         }
         if ($row instanceof Vpc_Forum_Directory_Row) {
