aa164a5fdef81930efb47c283201f01fc18a7def
Niko Sams <ns@vivid-planet.com>
2008-09-16 08:35:32 +0000

    printcss

--- Vps/View/Helper/Assets.php
+++ Vps/View/Helper/Assets.php
@@ -9,6 +9,9 @@ class Vps_View_Helper_Assets
         foreach ($dep->getAssetUrls($type, 'css') as $file) {
             $ret .= "$indent<link rel=\"stylesheet\" type=\"text/css\" href=\"$file\" />\n";
         }
+        foreach ($dep->getAssetUrls($type, 'printcss') as $file) {
+            $ret .= "$indent<link rel=\"stylesheet\" type=\"text/css\" href=\"$file\" media=\"print\" />\n";
+        }
         foreach ($dep->getAssetUrls($type, 'js') as $file) {
             $ret .= "$indent<script type=\"text/javascript\" src=\"$file\"></script>\n";
         }
