d1ca5f9e94fc7fb43a686de31590b9ff01b52a6d
Franz Unger <ufx@vivid-planet.com>
2008-09-17 08:49:44 +0000

    ImagePosition-Komponente

---
+++ Vpc/Basic/ImagePosition/Component.tpl
@@ -0,0 +1,3 @@
+<div class="<?=$this->cssClass?>">
+    <div class="<?=$this->imagePosition?>"><?= $this->component($this->image) ?></div>
+</div>
---
+++ Vpc/Basic/ImagePosition/Form.php
@@ -0,0 +1,14 @@
+<?php
+class Vpc_Basic_ImagePosition_Form extends Vpc_Abstract_Composite_Form 
+{
+    protected $_createFieldsets = true;
+    
+    public function __construct($name, $class)
+    {
+        parent::__construct($name, $class);
+        $this->add(new Vps_Form_Field_ComboBox('image_position', trlVps('Position of Image')))
+            ->setValues(array('left' => trlVps('Left'), 'right' => trlVps('Right'), 'center' => trlVps('Center')))
+            ->setTriggerAction('all')
+            ->setEditable(false);
+    }
+}
