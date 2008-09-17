f1f79eab7e0fde75ef099b629a35d755be7fcda3
Franz Unger <ufx@vivid-planet.com>
2008-09-17 09:07:24 +0000

    Bug

--- Vpc/Basic/Image/Enlarge/Form.php
+++ Vpc/Basic/Image/Enlarge/Form.php
@@ -6,7 +6,7 @@ class Vpc_Basic_Image_Enlarge_Form extends Vpc_Basic_Image_Form
         parent::__construct($name, $class);
 
         if (Vpc_Abstract::getSetting($class, 'hasSmallImageComponent')) {
-            $image = Vpc_Abstract_Form::createChildComponentForm($class, '-imageEnlarge');
+            $image = Vpc_Abstract_Form::createChildComponentForm($class, '-smallImage');
             $image->fields->getByName('vps_upload_id')->setFileFieldLabel(trlVps('File (optional)'));
             $this->add(new Vps_Form_Container_FieldSet(trlVps('Small Image')))
                 ->add($image);
