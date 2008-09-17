1ee90a639c1a89f68b663defe5488f8e31be2c56
Markus Hauser <mh@vivid-planet.com>
2008-09-17 08:48:09 +0000

    hasContent hinzu

--- Vpc/Advanced/SearchEngineReferer/ViewLatest/Component.php
+++ Vpc/Advanced/SearchEngineReferer/ViewLatest/Component.php
@@ -2,6 +2,8 @@
 class Vpc_Advanced_SearchEngineReferer_ViewLatest_Component
     extends Vpc_Abstract
 {
+    private $_referersCache = null;
+
     public static function getSettings()
     {
         $ret = parent::getSettings();
@@ -13,21 +15,34 @@ class Vpc_Advanced_SearchEngineReferer_ViewLatest_Component
     public function getTemplateVars()
     {
         $ret = parent::getTemplateVars();
-
-        $table = $this->getData()->parent->getComponent()->getTable();
-        $rowset = $table->fetchAll($this->_getWhere(), 'id DESC', $this->_getSetting('limit'));
-
-        $ret['referers'] = array();
-        foreach ($rowset as $row) {
-            $host = parse_url($row->referer_url, PHP_URL_HOST);
-            $ret['referers'][] = array(
-                'component' => Vps_Component_Data_Root::getInstance()->getComponentByDbId($row->component_id),
-                'row'       => $row,
-                'host'      => $host,
-                'query'     => Vpc_Advanced_SearchEngineReferer_Component::getQueryVar($row->referer_url)
-            );
-        }
+        $ret['referers'] = $this->_getReferers();
         return $ret;
+    }
+
+    private function _getReferers()
+    {
+        if (is_null($this->_referersCache)) {
+            $table = $this->getData()->parent->getComponent()->getTable();
+            $rowset = $table->fetchAll($this->_getWhere(), 'id DESC', $this->_getSetting('limit'));
+
+            $this->_referersCache = array();
+            foreach ($rowset as $row) {
+                $host = parse_url($row->referer_url, PHP_URL_HOST);
+                $this->_referersCache[] = array(
+                    'component' => Vps_Component_Data_Root::getInstance()->getComponentByDbId($row->component_id),
+                    'row'       => $row,
+                    'host'      => $host,
+                    'query'     => Vpc_Advanced_SearchEngineReferer_Component::getQueryVar($row->referer_url)
+                );
+            }
+        }
+        return $this->_referersCache;
+    }
+
+    public function hasContent()
+    {
+        $refs = $this->_getReferers();
+        return count($refs) ? true : false;
     }
 
     protected function _getWhere()
