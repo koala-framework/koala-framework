ed2e9b648e9cb091130e3f14f3a31c5a22a1bee8
Franz Unger <ufx@vivid-planet.com>
2008-09-18 13:09:35 +0000

    Cache löschen-Probleme

--- Vpc/Forum/Thread/Admin.php
+++ Vpc/Forum/Thread/Admin.php
@@ -5,7 +5,9 @@ class Vpc_Forum_Thread_Admin extends Vpc_Abstract_Composite_Admin
     {
         parent::onRowUpdate($row);
         if ($row instanceof Vpc_Forum_Group_Row) {
-            Vps_Component_Cache::getInstance()->remove($this->_class, $row->component_id.'_'.$row->id);
+            $components = Vps_Component_Data_Root::getInstance()
+                ->getComponentsByDbId($row->component_id.'-'.$row->id);
+            Vps_Component_Cache::getInstance()->remove($components);
         }
     }
 }
