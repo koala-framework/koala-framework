855842bacafcc2c60da44cc35b93fb3be8f6731a
Niko Sams <ns@vivid-planet.com>
2008-09-18 09:21:35 +0000

    bugfix bei imageenlarge-form: speichern funktioierte nicht da models equal sind

--- Vpc/Basic/Image/Enlarge/Form.php
+++ Vpc/Basic/Image/Enlarge/Form.php
@@ -12,28 +12,4 @@ class Vpc_Basic_Image_Enlarge_Form extends Vpc_Basic_Image_Form
                 ->add($image);
         }
     }
-    protected function _getIdByParentRow($parentRow)
-    {
-        $id = $this->getId();
-        if ($this->getIdTemplate()) {
-            if (!$parentRow) {
-                throw new Vps_Exception("Form has an idTemplate set - so getRow required a parentRow as first argument");
-            }
-            $pk = $parentRow->getModel()->getPrimaryKey();
-            $id = $parentRow->$pk;
-            if (!$id) {
-                return null;
-            }
-            $id = str_replace('{0}', $id, $this->getIdTemplate());
-            if (preg_match_all('#{([a-z0-9_]+)}#', $id, $m)) {
-                foreach ($m[1] as $i) {
-                    if (!isset($parentRow->$i)) {
-                        throw new Vps_Exception("Column '$i' as specified in idTemplate doesn't exist in parentRow");
-                    }
-                    $id = str_replace('{'.$i.'}', $parentRow->$i, $id);
-                }
-            }
-        }
-        return $id;
-    }
 }
