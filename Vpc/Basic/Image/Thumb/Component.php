8d54c31221bc162ccf4f05b94b8c048e6b7e0429
Markus Hauser <mh@vivid-planet.com>
2008-09-18 08:36:41 +0000

    $ret statt $settings

--- Vpc/Basic/Image/Thumb/Component.php
+++ Vpc/Basic/Image/Thumb/Component.php
@@ -3,8 +3,8 @@ class Vpc_Basic_Image_Thumb_Component extends Vpc_Basic_Image_Component
 {
     public static function getSettings()
     {
-        $settings = parent::getSettings();
-        $settings['dimensions'] = array(100, 75, Vps_Media_Image::SCALE_CROP);
-        return $settings;
+        $ret = parent::getSettings();
+        $ret['dimensions'] = array(100, 75, Vps_Media_Image::SCALE_CROP);
+        return $ret;
     }
 }
