cf9dc9c9891a5e656b6fb523075f684f2e7fd095
Markus Hauser <mh@vivid-planet.com>
2008-09-18 08:36:41 +0000

    $ret statt $settings

--- Vpc/News/Detail/PreviewImage/Component.php
+++ Vpc/News/Detail/PreviewImage/Component.php
@@ -3,9 +3,9 @@ class Vpc_News_Detail_PreviewImage_Component extends Vpc_Basic_Image_Component
 {
     public static function getSettings()
     {
-        $settings = array_merge(parent::getSettings(), array(
+        $ret = array_merge(parent::getSettings(), array(
             'dimensions'         => array(30, 20),
         ));
-        return $settings;
+        return $ret;
     }
 }
