fb059f7671c2b4d09fee189eeb257294c7c1797f
Niko Sams <ns@vivid-planet.com>
2008-09-22 14:02:36 +0000

    kommentar

--- Vpc/Menu/BreadCrumbs/Admin.php
+++ Vpc/Menu/BreadCrumbs/Admin.php
@@ -1,9 +1,11 @@
 <?php
-class Vpc_Menu_Breadcrumbs_Admin extends Vpc_Menu_Admin
+class Vpc_Menu_BreadCrumbs_Admin extends Vpc_Menu_Admin
 {
     public function onRowUpdate($row)
     {
         parent::onRowUpdate($row);
+	//bisscen anders als in Menu_Admin, showInMenu nicht berücksichtigen sowie
+        //nur in Update
         foreach (Vpc_Abstract::getComponentClasses() as $componentClass) {
             foreach (Vpc_Abstract::getSetting($componentClass, 'generators') as $generator) {
                 if ((is_instance_of($generator['class'], 'Vps_Component_Generator_Page_Interface') &&
