1ea95df957aaaabb2c187af9f904ce387922bd69
Lorenz Maislinger <lm@vivid-planet.com>
2008-09-22 14:18:34 +0000

    Änderungen in Parser und in PDF Generierung

--- Vpc/Basic/Text/Pdf.php
+++ Vpc/Basic/Text/Pdf.php
@@ -6,9 +6,10 @@ class Vpc_Basic_Text_Pdf extends Vpc_Abstract_Pdf
     {
         $vars = $this->_component->getTemplateVars();
         $contentParts = $vars['contentParts'];
+        $html = '';
         foreach ($contentParts as $content){
             if (is_string($content)){
-                $html = str_replace("", "", $content);
+                $html .= str_replace("", "", $content);
                 $html = preg_replace('# +#', ' ', $html);
                 $html = str_replace('&nbsp;', ' ', $html);
                 $html = preg_replace('#<br[^>]*>[^<]*</li#', '</li', $html);
@@ -31,12 +32,18 @@ class Vpc_Basic_Text_Pdf extends Vpc_Abstract_Pdf
                 $html = preg_replace('#<p[^>]*///>#', '<br/>', $html);
                 $html = str_replace("\n", "", $html);
 
+            } elseif ($content['type'] == 'image') {
                 $this->textAreaHTML($html);
+                $html = '';
+                $content['component']->getComponent()->getPdfWriter($this->_pdf)->writeContent();
+
+            } elseif ($content['type'] == 'link') {
+                $html .= "<a href=\"".$content['component']->url."\">";
             } else {
                 //TODO andere Komponenten aufrufen
-
             }
         }
-    }
 
+        $this->textAreaHTML($html);
+    }
 }
