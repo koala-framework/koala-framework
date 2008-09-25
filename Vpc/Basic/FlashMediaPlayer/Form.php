da6a4843f2c0e4d7dc44dff56e67be2170605e7c
Markus Hauser <mh@vivid-planet.com>
2008-09-25 15:56:59 +0000

    FlashMediaPlayer Absatz hinzugefügt

---
+++ Vpc/Basic/FlashMediaPlayer/Form.php
@@ -0,0 +1,19 @@
+<?php
+class Vpc_Basic_FlashMediaPlayer_Form extends Vpc_Abstract_Form
+{
+    public function __construct($name, $class, $id = null)
+    {
+        parent::__construct($name, $class, $id);
+        $this->fields->add(new Vps_Form_Field_File('vps_upload_id_media', trlVps('Element')))
+            ->setDirectory('BasicFlashMediaPlayer');
+
+        $this->fields->add(new Vps_Form_Field_NumberField('width', trlVps('Width')))
+                ->setMinValue(1)
+                ->setMaxValue(9999);
+        $this->fields->add(new Vps_Form_Field_NumberField('height', trlVps('Height')))
+                ->setMinValue(1)
+                ->setMaxValue(9999);
+        $this->fields->add(new Vps_Form_Field_Checkbox('autostart', trlVps('Start automatically')));
+        $this->fields->add(new Vps_Form_Field_Checkbox('loop', trlVps('Loop')));
+    }
+}
