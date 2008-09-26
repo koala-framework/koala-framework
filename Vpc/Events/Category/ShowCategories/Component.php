4e1aff239567380eef6a364cb602e5f6ab5f3343
Niko Sams <ns@vivid-planet.com>
2008-09-26 09:10:12 +0000

    bugfix, nicht uns selbst aublenden

--- Vpc/Events/Category/ShowCategories/Component.php
+++ Vpc/Events/Category/ShowCategories/Component.php
@@ -6,6 +6,7 @@ class Vpc_Events_Category_ShowCategories_Component extends Vpc_News_Category_Sho
         $ret = parent::getSettings();
         $ret['componentName'] = trlVps('Events.Show categories');
         $ret['showDirectoryClass'] = 'Vpc_Events_Directory_Component';
+        $ret['hideDirectoryClasses'] = array();
         return $ret;
     }
 }
