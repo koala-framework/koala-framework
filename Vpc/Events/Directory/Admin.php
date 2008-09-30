29fe31a6f61f3fd50703cd72efbfa1ac90019e33
Niko Sams <ns@vivid-planet.com>
2008-09-30 07:25:42 +0000

    events bearbeitbar gemacht

---
+++ Vpc/Events/Directory/Admin.php
@@ -0,0 +1,11 @@
+<?php
+class Vpc_Events_Directory_Admin extends Vpc_News_Directory_Admin
+{
+    public function getExtConfig()
+    {
+        $ret = parent::getExtConfig();
+        $ret['idTemplate'] = 'events_{0}-content';
+        return $ret;
+    }
+
+}
