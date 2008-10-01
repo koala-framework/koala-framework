e9cfee3930207fa71a6626bf3eacb668092e068d
Christoph Eder <ce@vivid-planet.com>
2008-10-01 07:47:12 +0000

    - basic->line->component -> standard-style - user-edit -> cssClass -> webStandard - user-lostPwd -> cssClass -> webStandard

--- Vpc/User/Edit/Component.php
+++ Vpc/User/Edit/Component.php
@@ -6,6 +6,7 @@ class Vpc_User_Edit_Component extends Vpc_Abstract_Composite_Component
         $ret = parent::getSettings();
         $ret['generators']['child']['component']['form'] = 'Vpc_User_Edit_Form_Component';
         $ret['forms'] = 'all';
+        $ret['cssClass'] = 'webStandard';
         return $ret;
     }
 }
