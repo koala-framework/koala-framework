ebef577c63bcfc15a9411367b2124b10134aa5f6
Niko Sams <ns@vivid-planet.com>
2008-10-01 15:36:02 +0000

    bugfix wenn unique box direkt unter root liegt

---
+++ tests/Vps/Component/Generator/UniqueRoot/Root.php
@@ -0,0 +1,27 @@
+<?php
+class Vps_Component_Generator_UniqueRoot_Root extends Vpc_Root_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+
+        $ret['generators']['page']['model'] = new Vps_Model_FnF();
+        $ret['generators']['page']['component'] = array();
+        unset($ret['generators']['box']);
+        unset($ret['generators']['title']);
+
+        $ret['generators']['page2'] = array(
+            'class' => 'Vps_Component_Generator_Page_Static',
+            'component' => 'Vpc_Basic_Empty_Component',
+            'name' => 'page1'
+        );
+
+        $ret['generators']['box'] = array(
+            'class' => 'Vps_Component_Generator_Box_Static',
+            'component' => 'Vpc_Basic_Empty_Component',
+            'unique' => true,
+            'inherit' => true
+        );
+        return $ret;
+    }
+}
