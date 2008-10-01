15edf29af1a277c2e8287b76219bfbba623381d0
Niko Sams <ns@vivid-planet.com>
2008-10-01 15:34:03 +0000

    ka was das war

--- Vpc/Basic/Link/Form.php
+++ Vpc/Basic/Link/Form.php
@@ -3,8 +3,7 @@ class Vpc_Basic_Link_Form extends Vpc_Abstract_Composite_Form
 {
     protected function _initFields()
     {
-        $form = new Vpc_Abstract_Form('text', $this->getClass());
-        $form->add(new Vps_Form_Field_TextField('text', trlVps('Linktext')))
+        $this->add(new Vps_Form_Field_TextField('text', trlVps('Linktext')))
             ->setWidth(300)
             ->setAllowBlank(false);
 
