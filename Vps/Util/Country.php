264a7c4c0618f82e8430529a8c7477770e0837cd
Franz Unger <ufx@vivid-planet.com>
2008-10-03 11:24:25 +0000

    Vps_Util_Country-Klasse hinzugefügt

---
+++ Vps/Util/Country.php
@@ -0,0 +1,47 @@
+<?php
+class Vps_Util_Country {
+    
+    const NAME = 'name';
+    const CONTINENT = 'continent';
+    const CAPITAL = 'capital';
+    
+    public static function getValue($key, $what = self::NAME)
+    {
+        $language = Zend_Registry::get('trl')->getTargetLanguage();
+        $masterFile = VPS_PATH . '/Vps/Form/Field/SelectCountry/countries.xml';
+        $cacheId = "countries_{$language}_{$what}";
+        
+        $cache = Zend_Cache::factory(
+            'File',
+            'File',
+            array(
+               'master_file' => $masterFile,
+               'lifetime' => null,
+               'automatic_serialization' => true
+            ),
+            array(
+                'cache_dir' => 'application/cache/config'
+            )
+        );
+        $result = $cache->load($cacheId);
+        if(!$result) {
+            $xml = simplexml_load_file($masterFile);
+            $result = array();
+            $x = 0;
+            foreach ($xml->country as $country) {
+                $value = null;
+                foreach ($country as $k => $c) {
+                    if ($k == $what) {
+                        $attributes = $c->attributes();
+                        if (isset($attributes['language']) && $attributes['language'] == $language) {
+                            $result[(string)$country->iso2] = (string)$c;
+                        }
+                    }
+                }
+            }
+            $cache->save($result, $cacheId);
+        }
+        if (isset($result[$key])) return $result[$key];
+        return null;
+    }
+}
