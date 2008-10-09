b4828d6684326a1cacf61295a1a9d341bb83620b
Niko Sams <ns@vivid-planet.com>
2008-10-09 16:15:45 +0000

    fix löschen

--- Vpc/News/Directory/Row.php
+++ Vpc/News/Directory/Row.php
@@ -1,15 +1,6 @@
 <?php
 class Vpc_News_Directory_Row extends Vps_Db_Table_Row_Abstract
 {
-    protected function _delete()
-    {
-        $class = $this->getTable()->getComponentClass();
-        $admin = Vpc_Admin::getInstance($class);
-        if ($admin) {
-            $admin->delete($this->component_id . '-' . $this->id);
-        }
-    }
-
     public function __toString()
     {
         return $this->title;
