22412bd827b9e4d1b4e340e0405e80ee31a1e2b3
Niko Sams <ns@vivid-planet.com>
2008-10-09 06:36:48 +0000

    ShowCategories abstrakte überklasse + Archiv bei Events

---
+++ Vpc/Directories/Category/ShowCategories/Abstract/Component.php
@@ -0,0 +1,26 @@
+<?php
+abstract class Vpc_Directories_Category_ShowCategories_Abstract_Component extends Vpc_Directories_List_Component
+{
+    abstract public function getCategoryIds();
+
+    public function getSelect()
+    {
+        $select = parent::getSelect();
+        if (!$select) return null;
+
+        $tableName = Vpc_Abstract::getSetting(
+            $this->getItemDirectory()->getChildComponent('_categories')->componentClass,
+            'categoryToItemTableName'
+        );
+        $refData = Vpc_Directories_Category_Detail_List_Component::getTableReferenceData($tableName);
+
+        $select->join($refData['tableName'],
+                      $refData['tableName'].'.'.$refData['itemColumn'].'='
+                        .$refData['refTableName'].'.'.$refData['refItemColumn'],
+                      array());
+        $ids = $this->getCategoryIds();
+        if (!$ids) return null;
+        $select->where($refData['tableName'].'.category_id IN ('.implode(',', $ids).')');
+        return $select;
+    }
+}
