8df7841ab746fc71a1ce60282fa49db674e11beb
Christoph Eder <ce@vivid-planet.com>
2008-10-15 13:23:34 +0000

    - vps shop styling

--- Vpc/Shop/Cart/Checkout/Component.php
+++ Vpc/Shop/Cart/Checkout/Component.php
@@ -4,12 +4,23 @@ class Vpc_Shop_Cart_Checkout_Component extends Vpc_Abstract_Composite_Component
     public static function getSettings()
     {
         $ret = parent::getSettings();
-        $ret['generators']['child']['component'] = 'Vpc_Shop_Cart_Checkout_Form_Component';
+        $ret['generators']['child']['component']['form'] = 'Vpc_Shop_Cart_Checkout_Form_Component';
         $ret['generators']['confirm'] = array(
             'class' => 'Vps_Component_Generator_Page_Static',
             'component' => 'Vpc_Shop_Cart_Checkout_Confirm_Component',
             'name' => trlVps('Send order')
         );
+        $ret['cssClass'] = 'webForm webStandard';
+        return $ret;
+    }
+    public function getTemplateVars()
+    {
+        $ret = parent::getTemplateVars();
+        $ret['keys'] = array();
+        foreach ($this->getData()->getChildComponents(array('generator' => 'child')) as $c) {
+            $ret[$c->id] = $c;
+            $ret['keys'][] = $c->id;
+        }
         return $ret;
     }
 }
---
+++ Vpc/Shop/Cart/Checkout/Component.tpl
@@ -0,0 +1,4 @@
+<div class="<?=$this->cssClass?>">
+    <?=$this->component($this->form);?>
+    <div class="back"><?=$this->componentLink($this->data->getParentPage(),trlVps('Back to cart'));?></div>
+</div>
