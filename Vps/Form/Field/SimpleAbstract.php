330753a57efdf435d6949d7faf865572a4637a5c
Niko Sams <ns@vivid-planet.com>
2008-10-15 07:47:01 +0000

    defaultValue berücksichtigen beim laden (für Vpc_Form)

--- Vps/Form/Field/SimpleAbstract.php
+++ Vps/Form/Field/SimpleAbstract.php
@@ -69,4 +69,13 @@ abstract class Vps_Form_Field_SimpleAbstract extends Vps_Form_Field_Abstract
         if (!isset($postData[$fieldName])) $postData[$fieldName] = null;
         return $postData[$fieldName];
     }
+
+    public function processInput($postData)
+    {
+        $fieldName = $this->getFieldName();
+        if (!isset($postData[$fieldName]) && $this->getDefaultValue()) {
+            $postData[$fieldName] = $this->getDefaultValue();
+        }
+        return $postData;
+    }
 }
