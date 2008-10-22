5578ec04f01308b6331a5b2473da82e157ca8765
Christoph Eder <ce@vivid-planet.com>
2008-10-22 09:00:52 +0000

    - shop änderungen

--- Vpc/Shop/AddToCart/Success/Component.php
+++ Vpc/Shop/AddToCart/Success/Component.php
@@ -4,7 +4,7 @@ class Vpc_Shop_AddToCart_Success_Component extends Vpc_Form_Success_Component
     public static function getSettings()
     {
         $ret = parent::getSettings();
-        $ret['placeholder']['success'] = trlVps('The product has been added the the cart successfully.');
+        $ret['placeholder']['success'] = trlVps('The product has been added the the cart.');
         return $ret;
     }
 
