e113ba5f44f52a2e7e0f5ed23b3c96d0d012b4b4
Lorenz Maislinger <lm@vivid-planet.com>
2008-10-22 10:47:07 +0000

    Ausbesern svn eclipse fehler

---
+++ Vps/Model/ProxyCache/Rowset.php
@@ -0,0 +1,81 @@
+<?php
+class Vps_Model_ProxyCache_Rowset implements Vps_Model_Rowset_Interface
+{
+    protected $_cacheData;
+    protected $_pointer = 0;
+    protected $_model;
+
+    public function __construct($config)
+    {
+        $this->_init();
+        if (isset($config['cacheData'])) $this->_cacheData = $config['cacheData'];
+        $this->_model = $config['model'];
+    }
+
+    public function toArray()
+    {
+        $ret = array();
+        foreach ($this as $row) {
+            $ret[] = $row->toArray();
+        }
+        return $ret;
+    }
+
+    protected function _init()
+    {
+    }
+
+    public function rewind()
+    {
+        $this->_pointer = 0;
+        return $this;
+    }
+
+    public function current()
+    {
+        if (!$this->valid()) {
+            return null;
+        }
+
+        $cacheData = $this->_cacheData[$this->_pointer];
+        if ($cacheData) {
+            return $this->getModel()->getRowByCacheData($cacheData);
+        }
+    }
+
+    public function key()
+    {
+        return $this->_pointer;
+    }
+
+    public function next()
+    {
+        ++$this->_pointer;
+    }
+
+    public function valid()
+    {
+        return $this->_pointer < $this->count();
+    }
+
+    public function count()
+    {
+        return count($this->_cacheData);
+    }
+
+    public function seek($position)
+    {
+        $position = (int) $position;
+        if ($position < 0 || $position > $this->count()) {
+            require_once 'Zend/Db/Table/Rowset/Exception.php';
+            throw new Zend_Db_Table_Rowset_Exception("Illegal index $position");
+        }
+        $this->_pointer = $position;
+        return $this;
+    }
+
+    public function getModel()
+    {
+        return $this->_model;
+    }
+}
