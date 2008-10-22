d693afc30735d9443274add429aaa9dd897a7f23
Lorenz Maislinger <lm@vivid-planet.com>
2008-10-22 10:41:24 +0000

    Erstellung von ProxyCache Bugfixes in Data_Abstract und Xml Model

--- Vps/Model/Proxy/Row.php
+++ Vps/Model/Proxy/Row.php
@@ -1,81 +1,54 @@
 <?php
-class Vps_Model_Proxy_Row extends Vps_Model_Row_Abstract
+class Vps_Model_ProxyCache_Row extends Vps_Model_Proxy_Row
 {
-    protected $_row;
+    protected $_cacheData;
 
     public function __construct(array $config)
     {
-        $this->_row = $config['row'];
+        if (isset($config['cacheData'])) $this->_cacheData = $config['cacheData'];
+        if (!isset($config['row'])) $config['row'] = null;
         parent::__construct($config);
-    }
-
-    public function __isset($name)
-    {
-        if ($this->_row->getModel()->hasColumn($name)) {
-            return isset($this->_row->$name);
-        } else {
-            return parent::__isset($name);
-        }
-    }
-
-    public function __unset($name)
-    {
-        if ($this->_row->getModel()->hasColumn($name)) {
-            $this->_row->__unset($name);
-        } else {
-            parent::__unset($name);
-        }
     }
 
     public function __get($name)
     {
-        if ($this->_row->getModel()->hasColumn($name)) {
-            return $this->_row->$name;
+        if (isset($this->_cacheData[$name])) {
+            return $this->_cacheData[$name];
         } else {
+            $this->_createRowIfNotExists();
             return parent::__get($name);
         }
     }
 
-    public function __set($name, $value)
-    {
-        if ($this->_row->getModel()->hasColumn($name)) {
-            $this->_row->$name = $value;
-        } else {
-            parent::__set($name, $value);
+    private function _createRowIfNotExists() {
+        if (!$this->_row) {
+            $id = $this->_cacheData[$this->_model->getPrimaryKey()];
+            $this->_row = $this->_model->getRowById($id);
         }
-    }
-
-    public function save()
-    {
-        parent::save();
-
-        $id = $this->{$this->_getPrimaryKey()};
-        if (!$id) {
-            $this->_beforeInsert();
-        } else {
-            $this->_beforeUpdate();
-        }
-        $this->_beforeSave();
-        $ret = $this->_row->save();
-        if (!$id) {
-            $this->_afterInsert();
-        } else {
-            $this->_afterUpdate();
-        }
-        $this->_afterSave();
-        return $ret;
-    }
-
-    public function delete()
-    {
-        parent::delete();
-        $this->_beforeDelete();
-        $this->_row->delete();
-        $this->_afterDelete();
     }
 
     public function toArray()
     {
-        return $this->_row->toArray();
+        if (isset($this->_row)) return $this->_row->toArray();
+        else return $this->_cacheData;
+    }
+
+    public function save()
+    {
+
+        $this->_createRowIfNotExists();
+        $ret = parent::save();
+        $this->_model->clearCache();
+        return $ret;
+    }
+
+
+    public function delete()
+    {
+        $this->_createRowIfNotExists();
+        $ret = parent::delete();
+        $this->_model->clearCache();
+        return $ret;
+
     }
 }
