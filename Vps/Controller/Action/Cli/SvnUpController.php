c52f186256a05f951c4ddcdf5783eee352f0c389
Niko Sams <ns@vivid-planet.com>
2008-10-23 08:53:46 +0000

    tag erstellen für web

--- Vps/Controller/Action/Cli/SvnUpController.php
+++ Vps/Controller/Action/Cli/SvnUpController.php
@@ -1,6 +1,11 @@
 <?php
 class Vps_Controller_Action_Cli_SvnUpController extends Vps_Controller_Action_Cli_Abstract
 {
+    public static function getHelp()
+    {
+        return "svn update web+vps";
+    }
+
     public function indexAction()
     {
         echo "updating web\n";
