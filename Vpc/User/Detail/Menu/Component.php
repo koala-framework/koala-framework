7def8d5ac0f5c133f5ce41dada0202888a506257
Niko Sams <ns@vivid-planet.com>
2008-10-29 09:21:11 +0000

    benutzer-menü

---
+++ Vpc/User/Detail/Menu/Component.php
@@ -0,0 +1,28 @@
+<?php
+class Vpc_User_Detail_Menu_Component extends Vpc_Abstract_Composite_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['viewCache'] = false;
+        return $ret;
+    }
+
+    public function getTemplateVars()
+    {
+        $ret = parent::getTemplateVars();
+        $ret['links'] = array();
+
+        $userRow = Vps_Registry::get('userModel')->getAuthedUser();
+        if ($userRow && $userRow->id == $this->getData()->parent->row->id) {
+            $ret['links'] = $this->_getLinks();
+        }
+        return $ret;
+    }
+    protected function _getLinks()
+    {
+        $ret = Vps_Component_Data_Root::getInstance()
+            ->getComponentsByClass('Vpc_User_Edit_Component');
+        return $ret;
+    }
+}
---
+++ Vpc/User/Detail/Menu/Component.tpl
@@ -0,0 +1,5 @@
+<div class="<?=$this->cssClass?>">
+    <? foreach($this->links as $l) { ?>
+        <?=$this->componentLink($l)?>
+    <? } ?>
+</div>
