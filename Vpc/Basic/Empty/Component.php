31a3d9ece23993d9b5418cb95317b458c50c8c63
Niko Sams <ns@vivid-planet.com>
2008-10-29 08:42:38 +0000

    empty hat keinen inhalt :D

--- Vpc/Basic/Empty/Component.php
+++ Vpc/Basic/Empty/Component.php
@@ -7,5 +7,8 @@ class Vpc_Basic_Empty_Component extends Vpc_Abstract
         $ret['componentName'] = trlVps('Empty');
         return $ret;
     }
-
+    public function hasContent()
+    {
+        return false;
+    }
 }
