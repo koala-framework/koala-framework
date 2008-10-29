29425bc9e7f2ad65c60ad5a877b5e548f03dd561
Niko Sams <ns@vivid-planet.com>
2008-10-29 06:16:49 +0000

    bugfix: endlosschleife bei komponenten-settings cache behoben, options wurden nicht durchgeschliffen

--- Vps/Assets/Cache.php
+++ Vps/Assets/Cache.php
@@ -3,10 +3,9 @@ class Vps_Assets_Cache extends Vps_Cache_Core
 {
     public function __construct(array $options = array())
     {
-        parent::__construct(array(
-            'lifetime' => null,
-            'automatic_serialization' => true
-        ));
+        if (!isset($options['lifetime'])) $options['lifetime'] = null;
+        if (!isset($options['automatic_serialization'])) $options['automatic_serialization'] = true;
+        parent::__construct($options);
         $backend = new Zend_Cache_Backend_File(array(
             'cache_dir' => 'application/cache/assets'
         ));
