c92e1889cd65387703ab772c7b549023733321b8
Lorenz Maislinger <lm@vivid-planet.com>
2008-11-03 15:38:22 +0000

    bugfix trl

--- Vps_js/Trl.js
+++ Vps_js/Trl.js
@@ -1,30 +1,41 @@
 function trl (text, values){
-	if (values == null){
+  if (values == null){
            return text;
     } else {
-			var cnt = 0;
+      if (typeof(values) == 'string' || typeof(values) == 'number') {
+        var temp = values;
+        values = new Array();
+        values.push(temp);
+      }
+      var cnt = 0;
             values.each(function(value) {
-				text = text.replace(new RegExp('\\{('+cnt+')\\}', 'g'), value);
-				cnt++;
-			});
+        text = text.replace(new RegExp('\\{('+cnt+')\\}', 'g'), value);
+        cnt++;
+      });
            return text;
        }
 }
 
 function trlp (single, plural, values){
-	if (values == null){
+  if (values == null){
            return '';
     } else {
-			if (values[0] == 1){
-				text = single;
-			} else {
-				text = plural;
-			}
-			var cnt = 0;
+      if (typeof(values) == 'string' || typeof(values) == 'number') {
+        var temp = values;
+        values = new Array();
+        values.push(temp);
+      }
+
+      if (values[0] == 1){
+        text = single;
+      } else {
+        text = plural;
+      }
+      var cnt = 0;
             values.each(function(value) {
-				text = text.replace(new RegExp('\\{('+cnt+')\\}', 'g'), value);
-				cnt++;
-			});
+        text = text.replace(new RegExp('\\{('+cnt+')\\}', 'g'), value);
+        cnt++;
+      });
            return text;
        }
 }
