76a7d5efe81eeb5bce6ef8e5e1b386a46480fa18
Markus Hauser <mh@vivid-planet.com>
2008-11-03 14:12:44 +0000

    spamverdacht im nachhinein schicken geht wieder korrekt mit neuem mail model

--- Vps/Controller/Action/Spam/SetController.php
+++ Vps/Controller/Action/Spam/SetController.php
@@ -9,44 +9,17 @@ class Vps_Controller_Action_Spam_SetController extends Vps_Controller_Action
         if ($value == 0) {
             $row = Vps_Model_Abstract::getInstance('Vps_Model_Mail')->getRow($this->_getParam('id'));
             if (!$row) die('0');
+
+            if (Vps_Model_Mail_Row::getSpamKey($row) != $this->_getParam('key')) {
+                die('0');
+            }
+
             $row->is_spam = 0;
+            $row->mail_sent = 1;
             $row->save();
             $result = $row->sendMail();
-            if (!$result) die('0');
             die('1');
-/*
-            $model = new Vps_Model_Db(array('table' => new Vps_Model_Mail_Table()));
-            $row = $model->find($this->_getParam('id'))->current();
-
-            if ($row->is_spam && !$row->mail_sent) {
-                $varsModel = new Vps_Model_Field(array(
-                    'fieldName'   => 'serialized_mail_vars',
-                    'parentModel' => $model
-                ));
-                $varsRow = $varsModel->getRowByParentRow($row);
-
-                $essentialModel = new Vps_Model_Field(array(
-                    'fieldName'   => 'serialized_mail_essentials',
-                    'parentModel' => $model
-                ));
-                $essentialRow = $essentialModel->getRowByParentRow($row);
-
-                if (Vps_Model_Mail_Row::getSpamKey($row) != $this->_getParam('key')) {
-                    die('0');
-                }
-
-                $row->is_spam = 0;
-                $row->mail_sent = 1;
-                $row->save();
-
-                Vps_Model_Mail_Row::sendMail($essentialRow, $varsRow);
-
-                die('1');
-            }
-*/
         }
         die('0');
     }
-
-
 }
