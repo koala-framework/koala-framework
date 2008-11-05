9b06a5af54942945536406cc0763bd1de91eb855
Franz Unger <ufx@vivid-planet.com>
2008-11-05 09:47:47 +0000

    Bugs von neuem Online-Server behoben

--- Vps/View/Helper/Doctype.php
+++ Vps/View/Helper/Doctype.php
@@ -5,7 +5,7 @@ class Vps_View_Helper_Doctype extends Zend_View_Helper_Doctype
     {
         $return = parent::doctype($doctype);
         if (substr($doctype, 0, 5) == 'XHTML') {
-            $return = '<?xml version="1.0" encoding="utf-8"?>' . "\n" . $return . "\n";
+            $return = '<?xml version="1.0" encoding="utf-8"?>' . "\n" . $return->__toString() . "\n";
         }
         return $return;
     }
