101cd2b0cb29e69db83a7ede0646d4dfc93cb293
Markus Hauser <mh@vivid-planet.com>
2008-11-07 10:13:31 +0000

    CheckIp und SrpcHandlerModel aus Service in Vps und Test geschrieben

---
+++ Vps/Util/Check/Ip/Exception.php
@@ -0,0 +1,4 @@
+<?php
+class Vps_Util_Check_Ip_Exception extends Vps_Exception
+{
+}
---
+++ tests/Vps/Util/CheckIpTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @group Util
+ */
+class Vps_Util_CheckIpTest extends PHPUnit_Framework_TestCase
+{
+    private $_standardChecker = 'Vps_Util_CheckIpMock';
+
+    /**
+     * @expectedException Vps_Util_Check_Ip_Exception
+     */
+    public function testCheckIpFalseIp()
+    {
+        Vps_Util_Check_Ip::getInstance($this->_standardChecker)->checkIp('192.168.0.3');
+    }
+
+    /**
+     * @expectedException Vps_Exception
+     */
+    public function testCheckNoIp()
+    {
+        Vps_Util_Check_Ip::getInstance($this->_standardChecker)->checkIp('intern.vivid-planet.com');
+    }
+
+    /**
+     * @expectedException Vps_Exception
+     */
+    public function testCheckWrongMock()
+    {
+        Vps_Util_Check_Ip::getInstance('Vps_Util_CheckIpMockError')->checkIp('192.168.0.1');
+    }
+
+    public function testCheckIpReturn()
+    {
+        $res = Vps_Util_Check_Ip::getInstance($this->_standardChecker)->checkIp('192.168.0.1', true);
+        $this->assertTrue($res);
+        $res = Vps_Util_Check_Ip::getInstance($this->_standardChecker)->checkIp('192.168.0.1', true);
+        $this->assertTrue($res);
+        $res = Vps_Util_Check_Ip::getInstance($this->_standardChecker)->checkIp('192.168.123.9', true);
+        $this->assertTrue($res);
+        $res = Vps_Util_Check_Ip::getInstance($this->_standardChecker)->checkIp('192.168.0.3', true);
+        $this->assertFalse($res);
+    }
+}
