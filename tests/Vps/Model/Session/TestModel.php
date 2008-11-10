a647076e0d00d470afda308586face0c25d4cb4c
Niko Sams <ns@vivid-planet.com>
2008-11-10 16:40:42 +0000

    selenium tests für session model

---
+++ tests/Vps/Model/Session/TestModel.php
@@ -0,0 +1,8 @@
+<?php
+class Vps_Model_Session_TestModel extends Vps_Model_Session
+{
+    protected $_namespace = 'testModel';
+    protected $_defaultData = array(
+        array('id' => 1, 'foo' => 'bar')
+    );
+}
