bff46debdade9de4da993041456740295790238c
Niko Sams <ns@vivid-planet.com>
2008-11-10 10:57:43 +0000

    vereinfacht, zum besseren überschreiben

--- Vpc/Basic/LinkTag/Intern/Data.php
+++ Vpc/Basic/LinkTag/Intern/Data.php
@@ -2,28 +2,33 @@
 class Vpc_Basic_LinkTag_Intern_Data extends Vps_Component_Data
 {
     private $_data;
-    private function _getData()
+
+    protected function _getRow()
     {
-        if (!isset($this->_data)) {
-            $m = Vpc_Abstract::createModel($this->componentClass);
-            $row = $m->getRow($this->dbId);
-            if ($row) {
-                $this->_data = Vps_Component_Data_Root::getInstance()
-                                                ->getComponentByDbId($row->target);
-            } else {
-                $this->_data = false;
-            }
-        }
-        return $this->_data;
+        $m = Vpc_Abstract::createModel($this->componentClass);
+        $row = $m->getRow($this->dbId);
     }
+
+    protected function _getData()
+    {
+        if ($row = $this->_getRow()) {
+            return Vps_Component_Data_Root::getInstance()
+                                            ->getComponentByDbId($row->target);
+        }
+        return false;
+    }
+
     public function __get($var)
     {
+        if (!isset($this->_data)) {
+            $this->_data = $this->_getData();
+        }
         if ($var == 'url') {
-            $page = $this->_getData();
+            $page = $this->_data;
             if (!$page) return '';
             return $page->url;
         } else if ($var == 'rel') {
-            $page = $this->_getData();
+            $page = $this->_data;
             if (!$page) return '';
             return $page->rel;
         } else {
