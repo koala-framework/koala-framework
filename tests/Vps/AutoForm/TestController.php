c6f6d1cc36a5b21baedfc9d7e37fe90aeacd75a0
Lorenz Maislinger <lm@vivid-planet.com>
2008-11-11 14:12:08 +0000

    endlich der test zum autoform

---
+++ tests/Vps/AutoForm/TestController.php
@@ -0,0 +1,31 @@
+<?php
+class Vps_AutoForm_TestController extends Vps_Controller_Action_Auto_Form
+{
+    protected $_modelName = 'Vps_AutoForm_TestModel';
+    protected $_permissions = array('save', 'add');
+    protected $_buttons = array('save');
+
+    protected function _initFields()
+    {
+        $this->_form->add(new Vps_Form_Field_TextField('foo', 'Foo'));
+        $sessionFormId = new Zend_Session_Namespace('test_avoid_reinsert_id');
+        if (!isset($sessionFormId->count)) {
+            $sessionFormId->count = 0;
+        }
+    }
+
+    public function jsonSaveAction()
+    {
+        parent::jsonSaveAction();
+        $sessionFormId = new Zend_Session_Namespace('test_avoid_reinsert_id');
+        if ($sessionFormId->count == 0) {
+            $sessionFormId->count++;
+            throw new Vps_Exception("error");
+        }
+    }
+
+    protected function _getResourceName()
+    {
+        return 'vps_test';
+    }
+}
