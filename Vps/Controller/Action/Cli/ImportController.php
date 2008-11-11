1ca151aa566438c43768b34c3e684e83acd57b45
Niko Sams <ns@vivid-planet.com>
2008-11-11 15:02:27 +0000

    verbesserungen bei import, nocht nicht fertig

--- Vps/Controller/Action/Cli/ImportController.php
+++ Vps/Controller/Action/Cli/ImportController.php
@@ -1,62 +1,37 @@
 <?php
 class Vps_Controller_Action_Cli_ImportController extends Vps_Controller_Action_Cli_Abstract
 {
-    protected $_mysqlDir = '';
     public function indexAction()
     {
+        if (Vps_Registry::get('config')->server->host != 'vivid') {
+            throw new Vps_ClientException("Import is only possible on vivid server");
+        }
         $dbConfig = new Zend_Config_Ini('application/config.db.ini', 'database');
         $dbConfig = $dbConfig->web;
         $mysqlLocalOptions = "--host={$dbConfig->host} --user={$dbConfig->username} --password={$dbConfig->password} ";
 
         $config = new Zend_Config_Ini('application/config.ini', $this->_getParam('server'));
-
         $this->_sshHost = $config->server->user.'@'.$config->server->host;
-        if ($config->server->host == 'vivid-planet.com') {
-            $this->_mysqlDir = '/usr/local/mysql/bin/';
-        }
+        $this->_sshDir = $config->server->dir;
 
-        $onlineRevision = false;
-        try {
-            $info = new SimpleXMLElement(`ssh {$this->_sshHost} "cd {$config->server->dir} && svn info --xml"`);
-            $onlineRevision = (int)$info->entry['revision'];
-        } catch (Exception $e) {}
-        if (!$onlineRevision) {
-            throw new Vps_ClientException("Can't detect online revision");
-        }
-
-        echo "ermittle db optionen...\n";
-        $dbConfigIni = tempnam('/tmp', 'vpsimport');
-        $this->_systemCheckRet("scp {$this->_sshHost}:{$config->server->dir}/application/config.db.ini $dbConfigIni");
-        $onlineDbConfig = new Zend_Config_Ini($dbConfigIni, 'database');
-        unlink($dbConfigIni);
-        $onlineDbConfig = $onlineDbConfig->web;
-        $mysqlOnlineOptions = "--host={$onlineDbConfig->host} --user={$onlineDbConfig->username} --password={$onlineDbConfig->password} ";
+        $onlineRevision = $this->_systemSshVps('import get-update-revision');
 
         echo "kopiere uploads...\n";
-        $this->_systemCheckRet("rsync --progress --delete --update --exclude=cache/ ".
-            "--recursive {$this->_sshHost}:{$config->uploads}/ ".Vps_Registry::get('config')->uploads."/");
+        $this->_systemSshVps('copy-uploads '.Vps_Registry::get('config')->uploads.'/', $config->uploads);
+        //$this->_systemCheckRet("rsync --progress --delete --update --exclude=cache/ ".
+        //    "--recursive {$this->_sshHost}:{$config->uploads}/ ".Vps_Registry::get('config')->uploads."/");
+
 
         $p = "/var/backups/vpsimport/".date("Y-m-d_H:i:s_U")."_{$dbConfig->dbname}.sql";
         echo "erstelle datenbank-backup in $p...\n";
         $this->_systemCheckRet("mysqldump $mysqlLocalOptions {$dbConfig->dbname} > $p");
 
-        $dumpname = tempnam('/tmp', 'vpsimport');
-        echo "erstelle datenbank dump (online) - $dumpname...\n";
-        exec("ssh {$this->_sshHost} \"echo 'SHOW TABLES' | {$this->_mysqlDir}mysql $mysqlOnlineOptions {$onlineDbConfig->dbname}\"", $tables);
-
-        $this->_systemSsh("{$this->_mysqlDir}mysqldump --ignore-table={$onlineDbConfig->dbname}.cache_component $mysqlOnlineOptions {$onlineDbConfig->dbname} > $dumpname");
-        if (in_array('cache_component', $tables)) {
-            $this->_systemSsh("{$this->_mysqlDir}mysqldump --no-data $mysqlOnlineOptions {$onlineDbConfig->dbname} cache_component >> $dumpname");
-        }
-
-        $this->_systemSsh("bzip2 $dumpname");
-        $this->_systemSsh("ls -lh $dumpname.bz2");
+        echo "erstelle datenbank dump (online)...\n";
+        $dumpname = $this->_systemSshVps('import create-dump');
 
         echo "kopiere datenbank dump...\n";
-        $this->_systemCheckRet("scp {$this->_sshHost}:{$dumpname}.bz2 {$dumpname}.bz2");
-
-        echo "loesche datenbank dump online...\n";
-        $this->_systemSsh("rm $dumpname.bz2");
+        $dumpname = $this->_systemSshVps('copy-dump '.$dumpname);
+        //$this->_systemCheckRet("scp {$this->_sshHost}:{$dumpname} {$dumpname}");
 
         echo "loesche lokale datenbank...\n";
         $this->_systemCheckRet("echo \"DROP DATABASE \`{$dbConfig->dbname}\`\" | mysql $mysqlLocalOptions");
@@ -65,8 +40,8 @@ class Vps_Controller_Action_Cli_ImportController extends Vps_Controller_Action_C
         $this->_systemCheckRet("echo \"CREATE DATABASE \`{$dbConfig->dbname}\`\" | mysql $mysqlLocalOptions");
 
         echo "spiele dump in lokale datenbank ein...\n";
-        $this->_systemCheckRet("rm {$dumpname}");
-        $this->_systemCheckRet("bunzip2 {$dumpname}.bz2");
+        $this->_systemCheckRet("bunzip2 {$dumpname}");
+        $dumpname = substr($dumpname, 0, -4);
         $this->_systemCheckRet("mysql $mysqlLocalOptions {$dbConfig->dbname} < $dumpname");
 
         echo "loesche datebank dump lokal...\n";
@@ -83,7 +58,13 @@ class Vps_Controller_Action_Cli_ImportController extends Vps_Controller_Action_C
     {
         return $this->_systemCheckRet("ssh {$this->_sshHost} \"nice ".$cmd."\"");
     }
-
+    private function _systemSshVps($cmd, $dir = null)
+    {
+        if (!$dir) $dir = $this->_sshDir;
+        $cmd = "sshvps $this->_sshHost $dir $cmd";
+        $cmd = "sudo -u www-data $cmd";
+        return $this->_systemCheckRet($cmd);
+    }
     public static function getHelp()
     {
         return "import uploads+database";
@@ -98,5 +79,50 @@ class Vps_Controller_Action_Cli_ImportController extends Vps_Controller_Action_C
                 'help' => 'what to import'
             )
         );
+    }
+
+    public function createDumpAction()
+    {
+        $dbConfig = new Zend_Config_Ini('application/config.db.ini', 'database');
+        $dbConfig = $dbConfig->web;
+        $mysqlOptions = "--host={$dbConfig->host} --user={$dbConfig->username} --password={$dbConfig->password} ";
+        $config = Zend_Registry::get('config');
+
+        $mysqlDir = '';
+        if ($config->server->host == 'vivid-planet.com') {
+            $mysqlDir = '/usr/local/mysql/bin/';
+        }
+
+        $dumpname = tempnam('/tmp', 'vpsimport');
+
+        $tables = Zend_Registry::get('db')->fetchCol('SHOW TABLES');
+
+
+        $this->_systemCheckRet("{$mysqlDir}mysqldump --ignore-table={$dbConfig->dbname}.cache_component $mysqlOptions {$dbConfig->dbname} > $dumpname");
+        if (in_array('cache_component', $tables)) {
+            $this->_systemCheckRet("{$mysqlDir}mysqldump --no-data $mysqlOptions {$dbConfig->dbname} cache_component >> $dumpname");
+        }
+
+        $this->_systemCheckRet("bzip2 $dumpname");
+
+        echo $dumpname.".bz2";
+
+        $this->_helper->viewRenderer->setNoRender(true);
+    }
+
+    public function getUpdateRevisionAction()
+    {
+        if (file_exists('application/update')) {
+            echo file_get_contents('application/update');
+        } else {
+            try {
+                $info = new SimpleXMLElement(`svn info --xml"`);
+                $onlineRevision = (int)$info->entry['revision'];
+            } catch (Exception $e) {}
+            if (!$onlineRevision) {
+                throw new Vps_ClientException("Can't detect online revision");
+            }
+        }
+        $this->_helper->viewRenderer->setNoRender(true);
     }
 }
