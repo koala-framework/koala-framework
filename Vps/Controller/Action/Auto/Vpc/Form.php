73d658d71d9c29ee055f9fca810d1f711245a3c7
Niko Sams <ns@vivid-planet.com>
2008-11-11 08:38:54 +0000

    paremeter umbenannt

--- Vps/Controller/Action/Auto/Vpc/Form.php
+++ Vps/Controller/Action/Auto/Vpc/Form.php
@@ -22,7 +22,7 @@ abstract class Vps_Controller_Action_Auto_Vpc_Form extends Vps_Controller_Action
     public function indexAction()
     {
         $config = Vpc_Admin::getInstance($this->_getParam('class'))->getExtConfig();
-        $config['baseParams']['component_id'] = $this->_getParam('component_id');
+        $config['baseParams']['componentId'] = $this->_getParam('componentId');
         if ($this->getRequest()->module == 'component_test' && isset($config['controllerUrl'])) {
             $config['controllerUrl'] = str_replace('/admin/component/edit/',
                         '/vps/componentedittest/'.Vps_Component_Data_Root::getComponentClass().'/',
