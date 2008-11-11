708b1db5fbaa30a6ecd4e812303760839142d596
Niko Sams <ns@vivid-planet.com>
2008-11-11 10:52:08 +0000

    bugfix

--- Vpc/Basic/LinkTag/Intern/Data.php
+++ Vpc/Basic/LinkTag/Intern/Data.php
@@ -6,7 +6,7 @@ class Vpc_Basic_LinkTag_Intern_Data extends Vps_Component_Data
     protected function _getRow()
     {
         $m = Vpc_Abstract::createModel($this->componentClass);
-        $row = $m->getRow($this->dbId);
+        return $m->getRow($this->dbId);
     }
 
     protected function _getData()
@@ -22,15 +22,14 @@ class Vpc_Basic_LinkTag_Intern_Data extends Vps_Component_Data
     {
         if (!isset($this->_data)) {
             $this->_data = $this->_getData();
+            if (!$this->_data) $this->_data = false;
         }
         if ($var == 'url') {
-            $page = $this->_data;
-            if (!$page) return '';
-            return $page->url;
+            if (!$this->_data) return '';
+            return $this->_data->url;
         } else if ($var == 'rel') {
-            $page = $this->_data;
-            if (!$page) return '';
-            return $page->rel;
+            if (!$this->_data) return '';
+            return $this->_data->rel;
         } else {
             return parent::__get($var);
         }
