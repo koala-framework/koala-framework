67c7657dcaf1656ddea160325b0a6ba7592dae02
Niko Sams <ns@vivid-planet.com>
2008-11-14 16:12:24 +0000

    fix tests

--- tests/Vps/Update/Test.php
+++ tests/Vps/Update/Test.php
@@ -7,17 +7,22 @@ class Vps_Update_Test extends PHPUnit_Framework_TestCase
     public function testUpdate()
     {
         $actions = array();
-        $actions[0] = $this->getMock('Vps_Update_Action_Abstract', array('preUpdate', 'update'));
+        $actions[0] = $this->getMock('Vps_Update_Action_Abstract', array('preUpdate', 'postUpdate', 'update'));
         $actions[0]->expects($this->once())
                     ->method('update')
                     ->with();
         $actions[0]->expects($this->once())
                     ->method('preUpdate')
                     ->with();
+        $actions[0]->expects($this->once())
+                    ->method('postUpdate')
+                    ->with();
 
-        $update = new Vps_Update_TestUpdate();
+        $update = new Vps_Update_TestUpdate(123);
         $update->setActions($actions);
+        $update->preUpdate();
         $update->update();
+        $update->postUpdate();
     }
 
     public function testGetUpdatesForDir()
