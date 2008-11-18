0a5dd2961df92cfd81f10325eb21e90d3714c304
Lorenz Maislinger <lm@vivid-planet.com>
2008-11-18 11:24:02 +0000

    Connection tests

---
+++ tests/Vps/Connection/ConnectionsError.js
@@ -0,0 +1,86 @@
+Ext.namespace('Vps.Test');
+
+Vps.Test.ConnectionsError = Ext.extend(Ext.Panel, {
+    html: 'test<div id="log" style="color:red"></log>',
+    id: 'blub',
+    initComponent: function()
+    {
+        this.buttons = [];
+        this.buttons.push(
+            new Ext.Button({
+            text:'testA',
+            handler : function(){
+                Ext.Ajax.request({
+                    timeout: 1000,
+                    params: {test:1},
+                    errorText: 'foo1',
+                    url: '/vps/test/vps_connection_test/json-timeout',
+                    failure: function() {
+                        this.el.insertHtml('beforeBegin', "<div id=\"abort\">abort</div>");
+                    },
+                    success: function() {
+                        this.el.overwrite('<div id="testa">success</div>');
+                    },
+                    scope: this
+               });
+            },
+            scope: this
+        }));
+        this.buttons.push(
+            new Ext.Button({
+            text:'testB',
+            handler : function(){
+                Ext.Ajax.request({
+                    timeout: 1000,
+                    params: {test:1},
+                    url: '/vps/test/vps_connection_test/json-success',
+                    failure: function() {
+                        this.el.insertHtml('beforeBegin', "<div id=\"abort\">abort</div>");
+                    },
+                    success: function() {
+                        this.el.insertHtml('beforeBegin', "<div id=\"success\">success</div>");
+                    },
+                    scope: this
+               });
+            },
+            scope: this
+        }));
+        this.buttons.push(
+            new Ext.Button({
+            text:'testC',
+            handler : function(){
+                Ext.Ajax.request({
+                    timeout: 1000,
+                    params: {test:1},
+                    errorText: 'timeoutError',
+                    url: '/vps/test/vps_connection_test/json-timeout',
+                    failure: function() {
+                        this.el.insertHtml('beforeBegin', "<div id=\"aborttimeout\">aborttimeout</div>");
+                    },
+                    success: function() {
+                        this.el.insertHtml('beforeBegin', "<div id=\"success\">success</div>");
+                    },
+                    scope: this
+               });
+                Ext.Ajax.request({
+                    timeout: 1000,
+                    params: {test:2},
+                    errorText: 'exceptionError',
+                    url: '/vps/test/vps_connection_test/json-exception',
+                    failure: function() {
+                        this.el.insertHtml('beforeBegin', "<div id=\"abortexception\">abortexception</div>");
+                    },
+                    success: function() {
+                        this.el.insertHtml('beforeBegin', "<div id=\"success\">success</div>");
+                    },
+                    scope: this
+               });
+            },
+            scope: this
+        }));
+        Vps.Test.ConnectionsError.superclass.initComponent.call(this);
+    }
+});
+
+
+ /* */
--- tests/Vps/Connection/ErrorTest.php
+++ tests/Vps/Connection/ErrorTest.php
@@ -4,9 +4,65 @@
  */
 class Vps_Connection_ErrorTest extends Vps_Test_SeleniumTestCase
 {
-    public function testError()
+    public function setUp()
+    {
+        parent::setUp();
+        $this->setTimeout(120000);
+    }
+    public function testErrorDisplayErrorsFalse()
+    {
+        $this->_testError(false);
+    }
+
+    public function testErrorDisplayErrorsTrue()
+    {
+        $this->_testError(true);
+    }
+
+    private function _testError($errors) {
+        $this->open('/vps/test/vps_connection_test');
+        $this->click("//button[text()='testA']");
+        $this->waitForConnections();
+        if ($errors) $this->runScript('function foo() {Vps.Debug.displayErrors = false; Vps.log("selenium: "+Vps.Debug.displayErrors); }; foo();');
+        $this->click("//button[text()='".trlVps('Retry')."']");
+        $this->waitForConnections();
+        $this->click("//button[text()='".trlVps('Abort')."']");
+        $this->assertEquals("abort", $this->getText('id=abort'));
+
+
+    }
+
+    public function testSuccess()
+    {
+        $this->open('/vps/test/vps_connection_test');
+        $this->click("//button[text()='testB']");
+        $this->waitForConnections();
+        $this->assertEquals("success", $this->getText('id=success'));
+    }
+
+    public function testMoreRequests()
     {
         $this->markTestIncomplete();
+
         $this->open('/vps/test/vps_connection_test');
+        $this->click("//button[text()='testC']");
+        $this->waitForConnections();
+        // $this->click("//button[text()='Retry']");
+        /*$this->click("dom=function foo() {
+        var ret;
+        Ext.ComponentMgr.all.each(function(c) {
+            if (c instanceof Ext.Window && c.title == 'exceptionError') {
+                c.buttons.each(function (b) {
+                    if (b.text == 'Retry') {
+                        ret = b.el.dom;
+                    }
+                });
+            }
+         });
+         return ret;
+         }
+        foo();");
+       // $this->getText(//)
+        //$this->click("////button[text()='Abort']");
     }
 }
