59dfd600125bc81237ed1f9bcf17dc5090226492
Lorenz Maislinger <lm@vivid-planet.com>
2008-11-18 11:24:02 +0000

    Connection tests

--- tests/Vps/Connection/TestController.php
+++ tests/Vps/Connection/TestController.php
@@ -3,12 +3,24 @@ class Vps_Connection_TestController extends Vps_Controller_Action
 {
     public function indexAction()
     {
-        $this->view->ext('Vps.Test.ConnectionError', array(
+        $this->view->ext('Vps.Test.ConnectionsError', array(
             'assetsType' => 'AdminTest'
         ), 'Vps.Test.Viewport');
     }
     protected function _getResourceName()
     {
         return 'vps_test';
+    }
+
+    public function jsonTimeoutAction() {
+        sleep(50);
+    }
+
+    public function jsonExceptionAction() {
+        throw new Vps_Exception("test Exceptions");
+    }
+
+    public function jsonSuccessAction() {
+       //do nothing
     }
 }
