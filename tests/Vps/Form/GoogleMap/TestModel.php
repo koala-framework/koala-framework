2baae9238b303a8c48d5fcc8d6b84f3e4de7166f
Lorenz Maislinger <lm@vivid-planet.com>
2008-11-18 11:45:32 +0000

    Google Maps tests

---
+++ tests/Vps/Form/GoogleMap/TestModel.php
@@ -0,0 +1,8 @@
+<?php
+class Vps_Form_GoogleMap_TestModel extends Vps_Model_FnF
+{
+    protected $_namespace = 'testGoogleMapFnfModel';
+    protected $_data = array(
+        array('id' => 1, 'mapSelected' => '48.015364;13.125755', 'mapEmpty' =>  '')
+    );
+}
