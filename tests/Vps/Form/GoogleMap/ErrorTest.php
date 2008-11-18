296097f95a6ff18422a9915ae1079d4339ff87be
Lorenz Maislinger <lm@vivid-planet.com>
2008-11-18 15:12:35 +0000

    GoogleMap Test

---
+++ tests/Vps/Form/GoogleMap/ErrorTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @group selenium
+ * @group AutoForm
+ */
+class Vps_Form_GoogleMap_ErrorTest extends Vps_Test_SeleniumTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->setTimeout(120000);
+    }
+
+    public function testGoogleNewOpenWindow()
+    {
+        $this->open('/vps/test/vps_form_google-map_test');
+        $this->click("//input[@name='mapEmpty']/../..//img");
+        $this->assertNotNull($this->getText("//span[text()='Adresse eingeben.']"));
+    }
+
+    public function testGoogleOldOpenWindow()
+    {
+        $this->open('/vps/test/vps_form_google-map_test');
+        $this->click("//input[@name='mapSelected']/../..//img");
+        try {
+            //hier muss eine selenium exception geworfen werden
+            $this->getText("//span[text()='Adresse eingeben.']");
+            $this->assertTrue(false);
+        } catch (Exception $e) {
+            $this->assertTrue(true);
+        }
+        sleep(5);
+    }
+
+}
