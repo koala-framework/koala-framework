14cf0e264c6282107298b111de00e668746ff7ae
Niko Sams <ns@vivid-planet.com>
2008-11-27 12:20:34 +0000

    bei link intern berechtigungs fix

--- Vpc/Basic/LinkTag/Intern/PagesController.php
+++ Vpc/Basic/LinkTag/Intern/PagesController.php
@@ -1,6 +1,10 @@
 <?php
 class Vpc_Basic_LinkTag_Intern_PagesController extends Vps_Controller_Action_Component_PagesController
 {
+    protected function _isAllowedComponent()
+    {
+        return true;
+    }
 /*
     private $_pc;
 
