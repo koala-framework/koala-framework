fbaad6d4a51e285498a6c32552fa3863bc044ade
Lorenz Maislinger <lm@vivid-planet.com>
2008-12-02 14:50:02 +0000

    Vps_Model Übergangslösung

--- Vps/Data/Table/Parent.php
+++ Vps/Data/Table/Parent.php
@@ -4,7 +4,12 @@ class Vps_Data_Table_Parent extends Vps_Data_Abstract
     protected $_dataIndex;
     protected $_parentTable;
     protected $_ruleKey;
-    
+
+    /**
+     * @param string $parentTable wenn Zend_Db_Table: table, wenn Vps_Model: rule
+     * @param string $dataIndex row die angezeigt werden soll, wenn null wird __toString verwendet
+     * @param string $ruleKey nur wenn Zend_Db_Table
+     */
     public function __construct($parentTable, $dataIndex = null, $ruleKey = null)
     {
         $this->_parentTable = $parentTable;
@@ -21,7 +26,11 @@ class Vps_Data_Table_Parent extends Vps_Data_Abstract
             $tables = $this->_parentTable;
         }
         foreach ($tables as $t) {
-            $row = $row->findParentRow($t, $this->_ruleKey);
+            if ($t instanceof Zend_Db_Table) {
+                $row = $row->findParentRow($t, $this->_ruleKey);
+            } else {
+                $row = $row->getParentRow($this->_parentTable);
+            }
             if (!$row) return '';
         }
         if (!$this->_dataIndex) {
