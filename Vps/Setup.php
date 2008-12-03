93b359d2f09262300c1f6c042c6975f7335b61d6
Franz Unger <ufx@vivid-planet.com>
2008-12-03 13:35:26 +0000

    demo-Subdomain

--- Vps/Setup.php
+++ Vps/Setup.php
@@ -339,6 +339,11 @@ class Vps_Setup
                    substr($path, 0, 21) == '/docs/vpcms/www.test.' ||
                    substr($path, 0, 25) == '/var/www/html/vpcms/test.') {
             return 'test';
+        } else if (substr($host, 0, 5)=='demo.' ||
+                   substr($path, 0, 17) == '/docs/vpcms/demo.' ||
+                   substr($path, 0, 21) == '/docs/vpcms/www.demo.' ||
+                   substr($path, 0, 25) == '/var/www/html/vpcms/demo.') {
+            return 'demo';
         } else if (substr($host, 0, 8)=='preview.') {
             return 'preview';
         } else {
