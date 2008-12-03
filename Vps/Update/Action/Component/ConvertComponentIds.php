b049f6a1cb5d4cce3b2aa2c259569fb8b70c4d52
Franz Unger <ufx@vivid-planet.com>
2008-12-03 16:30:43 +0000

    Update-Action von DbUpdate-Controller erstellt

---
+++ Vps/Update/Action/Component/ConvertComponentIds.php
@@ -0,0 +1,37 @@
+<?php
+class Vps_Update_Action_Component_ConvertComponentIds extends Vps_Update_Action_Abstract
+{
+    public $search;
+    public $replace;
+
+    public function checkSettings()
+    {
+        parent::checkSettings();
+        if (!$this->search || !$this->replace) {
+            throw new Vps_ClientException("Required parameters: search, replace");
+        }
+    }
+
+    public function update()
+    {
+        $search = $this->search;
+        $replace = $this->replace;
+        $pattern = $this->pattern;
+        if (!$pattern) $pattern = $search . '%';
+
+        $db = Zend_Registry::get('db');
+        foreach ($db->query("SHOW TABLES")->fetchAll() as $table) {
+            $table = array_values($table);
+            $table = $table[0];
+            $hasComponentId = false;
+            foreach ($db->query("SHOW FIELDS FROM $table")->fetchAll() as $field) {
+                if ($field['Field'] == 'component_id') $hasComponentId = true;
+            }
+            if ($hasComponentId) {
+                $db->query("UPDATE $table SET component_id =
+                        REPLACE(component_id, '$search', '$replace')
+                        WHERE component_id LIKE '".str_replace('_', '\_', $pattern)."'");
+            }
+        }
+    }
+}
