bf5a6da3943d7cdcd4082f329fe8cb3bd144dcff
Niko Sams <ns@vivid-planet.com>
2008-12-04 15:54:00 +0000

    einstellbares start-datum

--- Vps/Controller/Action/Component/BenchmarkController.php
+++ Vps/Controller/Action/Component/BenchmarkController.php
@@ -3,19 +3,28 @@ class Vps_Controller_Action_Component_BenchmarkController extends Vps_Controller
 {
     public function indexAction()
     {
+        $start = $this->_getParam('start');
         $graphs = Vps_Controller_Action_Cli_BenchmarkController::getGraphs();
         foreach ($graphs as $gName=>$graph) {
-            echo "<img src=\"/admin/component/benchmark/graph?name=$gName\" />";
+            echo "<img src=\"/admin/component/benchmark/graph?name=$gName&start=$start\" />";
         }
         $this->_helper->viewRenderer->setNoRender(true);
     }
 
     public function graphAction()
     {
+        $start = $this->_getParam('start');
+        if ($start) {
+            $start = strtotime($start);
+        }
+        if (!$start || $start > time() || (time()-$start) < 1*24*60*60) {
+            $start = time()-1*24*60*60;
+        }
+        
         header('Content-Type: image/png');
         echo Vps_Controller_Action_Cli_BenchmarkController::getGraphContent(
             $this->_getParam('name'),
-            max(mktime(18,30,0,11,27,2008), time()-1*24*60*60),
+            $start,
             time()
         );
         $this->_helper->viewRenderer->setNoRender(true);
