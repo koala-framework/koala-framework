6f517d30be491ddb10f02c76711170dc5a15f039
Niko Sams <ns@vivid-planet.com>
2008-12-04 07:56:18 +0000

    exception tests verbessert, mit echtem controller testen

---
+++ tests/Vps/Exception/TestController.php
@@ -0,0 +1,28 @@
+<?php
+class Vps_Exception_TestController extends Vps_Controller_Action
+{
+    public function accessDeniedAction()
+    {
+        throw new Vps_Exception_AccessDenied();
+    }
+
+    public function notFoundAction()
+    {
+        throw new Vps_Exception_NotFound();
+    }
+
+    public function clientAction()
+    {
+        throw new Vps_Exception_Client("client exception");
+    }
+
+    public function exceptionAction()
+    {
+        throw new Vps_Exception("client exception");
+    }
+
+    public function exceptionOtherAction()
+    {
+        throw new Exception();
+    }
+}
