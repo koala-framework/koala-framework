a175dc7576e0daf0e467d1866124cdd85162f6db
Niko Sams <ns@vivid-planet.com>
2008-12-04 08:31:18 +0000

    cache von link-unterkomponente auch löschen weil die löscht sich selbst womöglich nicht wenn sie selbst keine row hat und darauf umgeschalten wurde

--- Vpc/Basic/LinkTag/Admin.php
+++ Vpc/Basic/LinkTag/Admin.php
@@ -11,4 +11,16 @@ class Vpc_Basic_LinkTag_Admin extends Vpc_Admin
         $fields['component'] = "VARCHAR(255) NOT NULL";
         $this->createFormTable('vpc_basic_linktag', $fields);
     }
+    public function onRowUpdate($row)
+    {
+        parent::onRowUpdate($row);
+        if ($row instanceof Vpc_Basic_LinkTag_Row) {
+            Vps_Component_Cache::getInstance()->remove(
+                Vps_Component_Data_Root::getInstance()
+                    ->getComponentsByDbId($row->component_id.'-link', array(
+                        'ignoreVisible' => true)
+                    )
+            );
+        }
+    }
 }
