3c2b7019f5d1c1e2467a0aedd034e7a88a4b1ab2
Niko Sams <ns@vivid-planet.com>
2008-12-04 13:17:37 +0000

    client-exception sollte 200 sein

--- Vps/Exception/Client.php
+++ Vps/Exception/Client.php
@@ -1,6 +1,11 @@
 <?php
 class Vps_Exception_Client extends Vps_Exception_NoMail
 {
+    public function getHeader()
+    {
+        return 'HTTP/1.1 200 OK';
+    }
+
     public function getTemplate()
     {
         return 'ErrorClient';
