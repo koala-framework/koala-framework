851a0f7f2ffbab5ef495e64d04461d552b77b1a8
Niko Sams <ns@vivid-planet.com>
2008-12-04 08:31:27 +0000

    fix id

--- Vpc/Basic/LinkTag/Row.php
+++ Vpc/Basic/LinkTag/Row.php
@@ -14,7 +14,7 @@ class Vpc_Basic_LinkTag_Row extends Vps_Model_Proxy_Row
         $class = Vpc_Abstract::getChildComponentClass($this->getTable()->getComponentClass(), null, $this->component);
         $admin = Vpc_Admin::getInstance($class);
         if ($admin) {
-            $admin->delete($this->component_id.'-1');
+            $admin->delete($this->component_id.'-link');
         }
     }
 }
