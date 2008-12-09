584e7298e69d3c98e19bcf8e6911a6cb2783d101
Lorenz Maislinger <lm@vivid-planet.com>
2008-12-09 14:11:28 +0000

    sleep eingebaut

--- tests/Vps/Form/ShowField/ValueOverlapsTest.php
+++ tests/Vps/Form/ShowField/ValueOverlapsTest.php
@@ -16,6 +16,7 @@ class Vps_Form_ShowField_ValueOverlapsTest extends Vps_Test_SeleniumTestCase
         $this->open('/vps/test/vps_form_show-field_value-overlaps-error');
         $this->click("//button[text()='testA']");
         $this->waitForConnections();
+        sleep(3);
         $posleft = $this->getElementPositionLeft("//div[text()='vorname']");
         $posleft1 = $this->getElementPositionLeft("//label[text()='Vorname:']");
         $this->assertEquals(105, $posleft - $posleft1);
