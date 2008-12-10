5bd34f91ef64100594a7c9ad3a5d28d34abb69e3
Lorenz Maislinger <lm@vivid-planet.com>
2008-12-10 13:40:04 +0000

    Trl Übersetzungcontrollr - ohne filter

---
+++ Vps/Controller/Action/Trl/VpsController.php
@@ -0,0 +1,37 @@
+<?php
+class Vps_Controller_Action_Trl_VpsController extends Vps_Controller_Action_Auto_Grid
+{
+    protected $_modelName = "Vps_Trl_Model_Vps";
+    protected $_buttons = array();
+    protected $_sortable = true;
+    protected $_defaultOrder = 'id';
+    protected $_paging = 30;
+    protected $_editDialog = array('controllerUrl'=>'/vps/trl/vps-edit',
+                                   'width'=>600,
+                                   'height'=>550);
+    protected $_columns;
+
+    protected function _initColumns()
+    {
+       /* $this->_filters['en'] = array(
+            'type'=>'TextField',
+            'width'=>80
+        );*/
+
+        $this->_columns->add(new Vps_Grid_Column_Button('edit'));
+        $this->_columns->add(new Vps_Grid_Column('id', 'Id', 50));
+        $this->_columns->add(new Vps_Grid_Column('context', 'Context', 100));
+        $this->_columns->add(new Vps_Grid_Column('en', 'English Singular', 350));
+        $this->_columns->add(new Vps_Grid_Column('en_plural', 'English Plural', 350));
+
+        parent::_initColumns();
+    }
+
+    public function indexAction ()
+    {
+        $config = array(
+            'controllerUrl' => $this->getRequest()->getPathInfo()
+        );
+        $this->view->ext('Vps.Trl.Grid', $config);
+    }
+}
---
+++ Vps/Controller/Action/Trl/VpsEditController.php
@@ -0,0 +1,40 @@
+<?php
+class Vps_Controller_Action_Trl_VpsEditController extends Vps_Controller_Action_Auto_Form
+{
+    protected $_permissions = array('save', 'add');
+    protected $_modelName = 'Vps_Trl_Model_Vps';
+    protected $_colNames = array();
+
+    protected function _initFields()
+    {
+
+        $this->_form->add(new Vps_Form_Field_ShowField('id', trlVps('Id')));
+        $this->_form->add(new Vps_Form_Field_ShowField('context', trlVps('Context')));
+        $this->_form->add(new Vps_Form_Field_ShowField('en', trlVps('English Singular')));
+        $this->_form->add(new Vps_Form_Field_ShowField('en_plural', trlVps('English Plural')));
+
+        $config = Zend_Registry::get('config');
+        if ($config->languages) {
+            foreach ($config->languages as $lang) {
+                if ($lang != 'en') {
+                    $this->_form->add(new Vps_Form_Field_TextField($lang, trlVps("$lang. Singular")))->setWidth(400);
+                    $this->_colNames[] = $lang;
+
+                    $this->_form->add(new Vps_Form_Field_TextField("$lang._plural", trlVps("$lang. Plural")),300);
+                    $this->_colNames[] = "$lang._plural";
+                }
+            }
+        }
+     }
+
+    protected function _beforeSave(Vps_Model_Row_Interface $row)
+    {
+        parent::_beforeSave($row);
+        foreach ($this->_colNames as $colName)
+        if (!$row->{$colName}) {
+            unset($row->{$colName});
+        }
+    }
+
+
+}
