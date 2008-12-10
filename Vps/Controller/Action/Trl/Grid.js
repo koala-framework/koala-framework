26e5544cf8ade16beae6322f58ddd33723b14029
Lorenz Maislinger <lm@vivid-planet.com>
2008-12-10 13:40:04 +0000

    Trl Übersetzungcontrollr - ohne filter

---
+++ Vps/Controller/Action/Trl/Grid.js
@@ -0,0 +1,19 @@
+Ext.namespace('Vps.Trl');
+
+Vps.Trl.Grid = Ext.extend(Vps.Auto.GridPanel, {
+    initComponent : function()
+    {
+        Vps.Trl.Grid.superclass.initComponent.call(this);
+            this.on('loaded', function() {
+                this.getEditDialog().getAutoForm().on('beforeloadform', function(form, data) {
+                form.items.items.each(function(value, index) {
+                    if (!data[value.name]) {
+                        value.hide();
+
+                    }
+                });
+            })
+        })
+    }
+
+})
--- Vps/Controller/Action/Trl/TrlController.php
+++
@@ -1,6 +0,0 @@
-<?php
-class Vps_Controller_Action_Trl_CountriesController extends Vps_Controller_Action_Auto_Grid
-{
-    protected $_modelName = "Vps_Trl_Model";
-
-}
