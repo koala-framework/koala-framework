ef560a1994327228e96464b329ba92848a5f422a
Franz Unger <ufx@vivid-planet.com>
2008-12-11 09:39:21 +0000

    ComponentOutput liefert jetzt Benchmarkeinträge für rendernoviewcache

--- Vps/Component/Output/NoCache.php
+++ Vps/Component/Output/NoCache.php
@@ -5,7 +5,7 @@ class Vps_Component_Output_NoCache extends Vps_Component_Output_Abstract
     {
         return $this->_processComponent($componentId, $componentClass, $masterTemplate, $plugins);
     }
-    
+
     protected function _processComponent($componentId, $componentClass, $masterTemplate = false, array $plugins = array())
     {
         $ret = $this->_renderContent($componentId, $componentClass, $masterTemplate);
@@ -15,11 +15,11 @@ class Vps_Component_Output_NoCache extends Vps_Component_Output_Abstract
             $p = new $p($componentId);
             $ret = $p->processOutput($ret);
         }
-        
+
         $ret = $this->_parseTemplate($ret);
         return $ret;
     }
-    
+
     protected function _parseTemplate($ret)
     {
         // hasContent-Tags ersetzen
@@ -31,7 +31,7 @@ class Vps_Component_Output_NoCache extends Vps_Component_Output_Abstract
             $replace = $this->_renderHasContent($componentId, $componentClass, $content);
             $ret = str_replace($search, $replace, $ret);
         }
-        
+
         // nocache-Tags ersetzen
         preg_match_all('/{nocache: ([^ }]+) ([^ }]*) ?([^}]*)}/', $ret, $matches);
         foreach ($matches[0] as $key => $search) {
@@ -43,17 +43,25 @@ class Vps_Component_Output_NoCache extends Vps_Component_Output_Abstract
         }
         return $ret;
     }
-    
+
     protected function _renderHasContent($componentId, $componentClass, $content)
     {
-        Vps_Benchmark::count('rendered nocache', $componentId.' (hasContent)');
+        if ($this->_hasViewCache($componentClass)) {
+            Vps_Benchmark::count('rendered nocache', $componentId.' (hasContent)');
+        } else {
+            Vps_Benchmark::count('rendered noviewcache', $componentId.' (hasContent)');
+        }
         $component = $this->_getComponent($componentId);
         return $component->hasContent() ? $content : '';
     }
-    
+
     protected function _renderContent($componentId, $componentClass, $masterTemplate)
     {
-        Vps_Benchmark::count('rendered nocache', $componentId.($masterTemplate?' (master)':''));
+        if ($this->_hasViewCache($componentClass)) {
+            Vps_Benchmark::count('rendered nocache', $componentId.($masterTemplate?' (master)':''));
+        } else {
+            Vps_Benchmark::count('rendered noviewcache', $componentId.($masterTemplate?' (master)':''));
+        }
         if ($masterTemplate) {
             $output = new Vps_Component_Output_Master();
         } else {
