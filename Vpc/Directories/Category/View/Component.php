cc6e0db8c3bbb3850313d5d0c9d1c2535b400f8c
Markus Hauser <mh@vivid-planet.com>
2008-12-17 09:28:37 +0000

    Directory Category hat View bekommen, CategoryTree erbt davon

---
+++ Vpc/Directories/Category/View/Component.php
@@ -0,0 +1,76 @@
+<?php
+class Vpc_Directories_Category_View_Component
+    extends Vpc_Directories_List_ViewPage_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['placeholder']['linkPrefix'] = '';
+        return $ret;
+    }
+
+    public static function getItemCountCacheId($row)
+    {
+        // Row kann von hier (Model) oder von Admin (DB-Row) kommen
+        if ($row instanceof Vps_Model_Row_Interface) $row = $row->getRow();
+        return 'VpcDirectoriesCategoryTreeViewComponent_category'.get_class($row->getTable()).$row->id.'_itemCount';
+    }
+
+    public static function getItemCountCache()
+    {
+        $frontendOptions = array('lifetime' => 3600);
+        $backendOptions = array('cache_dir' => 'application/cache/component/');
+        return Zend_Cache::factory('Core', 'File', $frontendOptions, $backendOptions);
+    }
+
+    protected function _getCountCategoryIds($item)
+    {
+        return array($item->row->id);
+    }
+
+    protected function _getItems()
+    {
+        $items = parent::_getItems();
+
+        $cache = self::getItemCountCache();
+
+        foreach ($items as &$item) {
+            $cacheId = self::getItemCountCacheId($item->row);
+
+            if (($item->listCount = $cache->load($cacheId)) == false) {
+                if (!isset($itemDirectory)) {
+                    $itemDirectory = $this->getData()->parent->getComponent()->getItemDirectory();
+                }
+                if (!isset($connectData)) {
+                    $tableName = Vpc_Abstract::getSetting(
+                        $itemDirectory->componentClass, 'categoryToItemTableName'
+                    );
+                    $connectData = Vpc_Directories_Category_Detail_List_Component::getTableReferenceData(
+                        $tableName
+                    );
+                }
+                if (!isset($directoryComponent)) {
+                    $directoryComponent = $this->getData()->parent->getComponent()
+                        ->getItemDirectory()->parent->getComponent();
+                }
+
+                $categoryIds = $this->_getCountCategoryIds($item);
+
+                $select = $directoryComponent->getSelect();
+                $select->join(
+                    $connectData['tableName'],
+                    "$connectData[refTableName].$connectData[refItemColumn] = $connectData[tableName].$connectData[itemColumn]",
+                    array()
+                );
+                $select->where("$connectData[tableName].category_id IN(".implode(',', $categoryIds).")");
+
+                $item->listCount = $directoryComponent->getData()->countChildComponents($select);
+
+                $cache->save($item->listCount, $cacheId);
+            }
+        }
+
+        return $items;
+    }
+}
+
---
+++ Vpc/Directories/Category/View/Component.tpl
@@ -0,0 +1,13 @@
+<div class="<?=$this->cssClass?>">
+    <? if (isset($this->searchForm)) echo $this->component($this->searchForm); ?>
+    <? if (isset($this->paging)) echo $this->component($this->paging); ?>
+    <ul>
+        <? foreach ($this->items as $item) { ?>
+            <li>
+                <?=$this->componentLink($item, $this->placeholder['linkPrefix'].$item->getPage()->name);?>
+                (<?= $item->listCount; ?>)
+            </li>
+        <? } ?>
+    </ul>
+    <? if (isset($this->paging)) echo $this->component($this->paging); ?>
+</div>
