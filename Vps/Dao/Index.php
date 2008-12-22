cb29c6aa6b32e7b2aea23a1b364f483064e15d7a
Franz Unger <ufx@vivid-planet.com>
2008-12-22 15:10:32 +0000

    getComponentByDbId-Probleme behoben

--- Vps/Dao/Index.php
+++ Vps/Dao/Index.php
@@ -23,31 +23,31 @@ class Vps_Dao_Index extends Vps_Db_Table
         $processedPageIds = array();
         $t = new Vps_Dao_Index();
         foreach (self::$_componentIds as $componentId) {
-            $component = Vps_Component_Data_Root::getInstance()
-                ->getComponentByDbId($componentId);
-            if (!$component) continue; //kann sein wegen visible, wir speichern nur visible komponenten in den index
+            $components = Vps_Component_Data_Root::getInstance()
+                ->getComponentsByDbId($componentId);
+            foreach ($components as $component) {
+                $page = $component->getPage();
+                if (!$page) continue; //zB eine Box die direkt unter der root liegt
+                if (in_array($page->dbId, $processedPageIds)) continue;
+                $processedPageIds[] = $page->dbId;
 
-            $page = $component->getPage();
-            if (!$page) continue; //zB eine Box die direkt unter der root liegt
-            if (in_array($page->dbId, $processedPageIds)) continue;
-            $processedPageIds[] = $page->dbId;
-
-            $select = new Vps_Component_Select();
-            $select->whereFlag('searchContent')
-                   ->whereBox(false);
-            $cc = $page->getRecursiveChildComponents($select);
-            $content = '';
-            foreach ($cc as $c) {
-                $content .= ' '.$c->getComponent()->getSearchContent();
+                $select = new Vps_Component_Select();
+                $select->whereFlag('searchContent')
+                       ->whereBox(false);
+                $cc = $page->getRecursiveChildComponents($select);
+                $content = '';
+                foreach ($cc as $c) {
+                    $content .= ' '.$c->getComponent()->getSearchContent();
+                }
+                $content = trim($content);
+                $row = $t->find($page->dbId)->current();
+                if (!$row) {
+                    $row = $t->createRow();
+                    $row->component_id = $page->dbId;
+                }
+                $row->text = $content;
+                $row->save();
             }
-            $content = trim($content);
-            $row = $t->find($page->dbId)->current();
-            if (!$row) {
-                $row = $t->createRow();
-                $row->component_id = $page->dbId;
-            }
-            $row->text = $content;
-            $row->save();
         }
 
     }
