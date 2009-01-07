012e06261eeb01269301d8a2dfc43e15642ba19d
Niko Sams <ns@vivid-planet.com>
2009-01-07 15:24:24 +0000

    neue assets box

---
+++ Vpc/Box/AssetsSelect/Component.php
@@ -0,0 +1,18 @@
+<?php
+class Vpc_Box_AssetsSelect_Component extends Vpc_Box_Assets_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['modelname'] = 'Vps_Component_FieldModel';
+        $ret['componentName'] = trlVps('Assets Select');
+        return $ret;
+    }
+
+    protected function _getSection()
+    {
+        $ret = $this->getRow()->section;
+        if (!$ret) $ret = 'web';
+        return $ret;
+    }
+}
