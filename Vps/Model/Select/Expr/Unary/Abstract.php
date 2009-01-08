61940a56cc6fb5cbd15f4d8f1ccba7cc22165608
Niko Sams <ns@vivid-planet.com>
2009-01-08 14:27:02 +0000

    cs

--- Vps/Model/Select/Expr/Unary/Abstract.php
+++ Vps/Model/Select/Expr/Unary/Abstract.php
@@ -11,13 +11,13 @@ abstract class Vps_Model_Select_Expr_Unary_Abstract implements Vps_Model_Select_
         return $this->_expressions;
     }
 
-	public function validate ()
-	{
-		if (count($this->_expressions) == 0) {
-			throw new Vps_Exception("'".get_class($this)."' hast to contain at least one Expression");
-		}
-		foreach ($this->_expressions as $expression) {
-			$expression->validate();
-		}
-	}
+    public function validate ()
+    {
+        if (count($this->_expressions) == 0) {
+            throw new Vps_Exception("'".get_class($this)."' hast to contain at least one Expression");
+        }
+        foreach ($this->_expressions as $expression) {
+            $expression->validate();
+        }
+    }
 }
