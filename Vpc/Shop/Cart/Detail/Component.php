291891fabd07b76c779c279db7c698ccbc8b775a
Niko Sams <ns@vivid-planet.com>
2009-01-09 12:46:31 +0000

    bugfix shop warenkorb löschen

--- Vpc/Shop/Cart/Detail/Component.php
+++ Vpc/Shop/Cart/Detail/Component.php
@@ -10,7 +10,7 @@ class Vpc_Shop_Cart_Detail_Component extends Vpc_Abstract_Composite_Component
         return $ret;
     }
 
-    public function processInput($data)
+    public function preProcessInput($data)
     {
         if (isset($data[$this->getData()->componentId]) && $data[$this->getData()->componentId] == 'delete') {
             $this->getData()->row->delete();
