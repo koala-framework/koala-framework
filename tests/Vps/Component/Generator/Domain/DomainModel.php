b4e240a6f0080f5c2f0845f9c87d40ac8b7fbce4
Niko Sams <ns@vivid-planet.com>
2009-01-13 14:35:39 +0000

    fix test: keine ahnung wie der test jemals gehen konnte, model wurde nie wirklich verwendet

--- tests/Vps/Component/Generator/Domain/DomainModel.php
+++ tests/Vps/Component/Generator/Domain/DomainModel.php
@@ -1,14 +1,12 @@
 <?php
-class Vps_Component_Generator_Domain_Model extends Vpc_Root_DomainRoot_Model
+class Vps_Component_Generator_Domain_DomainModel extends Vpc_Root_DomainRoot_Model
 {
     public function __construct($config = array())
     {
-        $config['proxyModel'] = new Vps_Model_FnF(
-            array('data' => array(
-                array('id' => 'at', 'name' => 'Österreich', 'domain' => 'rotary.at', 'component' => 'at'),
-                array('id' => 'ch', 'name' => 'Liechtenstein und Schweiz', 'domain' => 'rotary.ch', 'component' => 'ch')
-            ))
-        );
+        $config['domains'] = array(
+                'at' => array('name' => 'Österreich', 'domain' => 'rotary.at'),
+                'ch' => array('name' => 'Liechtenstein und Schweiz', 'domain' => 'rotary.ch')
+            );
         parent::__construct($config);
     }
 }
