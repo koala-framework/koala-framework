94ea3f1672d8af786f965bec7c014f7d171a1672
Niko Sams <ns@vivid-planet.com>
2009-01-19 10:54:52 +0000

    d durch exception ersetzt

--- Vps/Component/Model.php
+++ Vps/Component/Model.php
@@ -63,7 +63,9 @@ class Vps_Component_Model extends Vps_Model_Abstract
             $rowset = array($root);
         } else if (isset($where['parent_id'])) {
             $page = $root->getComponentById($where['parent_id'], array('ignoreVisible' => true));
-            if (!$page) d($where['parent_id']);
+            if (!$page) {
+                throw new Vps_Exception("Can't find page with parent_id '{$where['parent_id']}'");
+            }
             $rowset = array_values($page->getChildComponents($this->_constraints));
 
             $constraints = $this->_constraints;
