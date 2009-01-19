43c027f70b397230c314df60f2225e66aec0fb75
Niko Sams <ns@vivid-planet.com>
2009-01-19 15:34:12 +0000

    fix test

--- tests/Vps/Component/Generator/RecursiveTable/PageGenerator.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Vps_Component_Generator_RecursiveTable_PageGenerator extends Vps_Component_Generator_Page_Table
-{
-    protected function _getParentDataForRow($row)
-    {
-        d($row);
-    }
-
-}
--- tests/Vps/Component/Generator/RecursiveTable/RecursiveTest.php
+++ tests/Vps/Component/Generator/RecursiveTable/RecursiveTest.php
@@ -13,14 +13,21 @@ class Vps_Component_Generator_RecursiveTable_RecursiveTest extends PHPUnit_Frame
 
     public function testPages()
     {
-        $c = $this->_root->getChildComponent('-1')
-            ->getRecursiveChildComponents(array('filename' => 'bar'));
+        $c = $this->_root->getChildComponent('-1')->getChildComponents();
         $this->assertEquals(count($c), 1);
-        $this->assertEquals(current($c)->componentId, 'root-1_foo');
+
+        $c = $this->_root->getChildComponent('-1')
+            ->getChildComponents(array('filename' => 'bar'));
+        $this->assertEquals(count($c), 1);
+
+        $c = $this->_root->getChildComponent('-1')
+            ->getChildComponents(array('filename' => 'bar'));
+        $this->assertEquals(count($c), 1);
+        $this->assertEquals(current($c)->componentId, 'root-1_1');
 
         $c = $this->_root
             ->getRecursiveChildComponents(array('filename' => 'bar'));
         $this->assertEquals(count($c), 1);
-        $this->assertEquals(current($c)->componentId, 'root-1_foo');
+        $this->assertEquals(current($c)->componentId, 'root-1_1');
     }
 }
