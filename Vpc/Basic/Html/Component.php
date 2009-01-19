82680a7122eb19d3b9ea5f4a88494a70ddece42e
Niko Sams <ns@vivid-planet.com>
2009-01-19 13:17:45 +0000

    exception verbessert

--- Vpc/Basic/Html/Component.php
+++ Vpc/Basic/Html/Component.php
@@ -19,7 +19,7 @@ class Vpc_Basic_Html_Component extends Vpc_Abstract_Composite_Component
     public function getTemplateVars()
     {
         if (Vpc_Abstract::hasSetting(get_class($this), 'default')) {
-            throw new Vps_Exception("Setting 'default' doesn't exist anymore for html components, you need to overwrite the Model.");
+            throw new Vps_Exception("Setting 'default' doesn't exist anymore for ".get_class($this).", you need to overwrite the Model.");
         }
         $ret = parent::getTemplateVars();
         $c = $this->_getRow()->content;
