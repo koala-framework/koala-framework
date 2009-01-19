7fbcc1b7c71ef3a6a6e87c2252ad1bb16f435077
Niko Sams <ns@vivid-planet.com>
2009-01-19 10:55:28 +0000

    schon etwas text ausgeben zum debuggen

--- Vpc/Basic/Html/OfflineHidden/Component.php
+++ Vpc/Basic/Html/OfflineHidden/Component.php
@@ -12,7 +12,7 @@ class Vpc_Basic_Html_OfflineHidden_Component extends Vpc_Basic_Html_Component
     {
         $ret = parent::getTemplateVars();
         if (!Vps_Registry::get('config')->isOnline) {
-            $ret['content'] = '';
+            $ret['content'] = trlVps('(offline hidden)');
         }
         return $ret;
     }
