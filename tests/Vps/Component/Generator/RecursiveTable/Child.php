8f5723a49a5c3e08bf453830cceb2b3bc6b231c7
Niko Sams <ns@vivid-planet.com>
2009-01-19 15:34:12 +0000

    fix test

--- tests/Vps/Component/Generator/RecursiveTable/Child.php
+++ tests/Vps/Component/Generator/RecursiveTable/Child.php
@@ -5,15 +5,16 @@ class Vps_Component_Generator_RecursiveTable_Child extends Vpc_Abstract
     {
         $ret = parent::getSettings();
         $ret['generators']['page'] = array(
-            'class' => 'Vps_Component_Generator_RecursiveTable_PageGenerator',
-            'component' => 'Vps_Component_Generator_RecursiveTable_Page',
+            'class' => 'Vps_Component_Generator_Page_Table',
+            'component' => 'Vpc_Basic_Empty_Component',
             'nameColumn' => 'name',
             'filenameColumn' => 'name',
             'uniqueFilename' => true,
-            'dbIdShortcut' => 'page_',
-            'model' => new Vps_Model_FnF(array('data'=>array(
-                array('id'=>'foo', 'name' => 'bar')
-            )))
+            'model' => new Vps_Model_FnF(array(
+                'columns' => array('id', 'name', 'component_id'),
+                'data'=>array(
+                    array('id'=>1, 'name' => 'bar', 'component_id'=>'root-1')
+                )))
         );
         return $ret;
     }
