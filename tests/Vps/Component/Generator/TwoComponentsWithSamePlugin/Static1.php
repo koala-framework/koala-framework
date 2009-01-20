722b545c635e7ea65c060f0cd5bd8a39e50c9e74
Niko Sams <ns@vivid-planet.com>
2009-01-20 08:19:13 +0000

    test für letzten bugfix

---
+++ tests/Vps/Component/Generator/TwoComponentsWithSamePlugin/Static1.php
@@ -0,0 +1,11 @@
+<?php
+class Vps_Component_Generator_TwoComponentsWithSamePlugin_Static1 extends Vpc_Abstract
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['plugins'] = array('Vps_Component_Plugin_Password_Component');
+        return $ret;
+    }
+
+}
