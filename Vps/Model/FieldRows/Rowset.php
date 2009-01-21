b0a71b5974f231402929921a20491d448cebbc53
Niko Sams <ns@vivid-planet.com>
2009-01-21 09:19:36 +0000

    basis klasse erstellt die wiederverwendet werden kann

--- Vps/Model/FieldRows/Rowset.php
+++ Vps/Model/FieldRows/Rowset.php
@@ -1,19 +1,5 @@
 <?php
-class Vps_Model_FieldRows_Rowset extends Vps_Model_Rowset_Abstract
+class Vps_Model_FieldRows_Rowset extends Vps_Model_Rowset_ParentRow
 {
-    protected $_parentRow;
-    public function __construct($config)
-    {
-        $this->_parentRow = $config['parentRow'];
-        parent::__construct($config);
-    }
 
-    public function current()
-    {
-        if ($this->valid() === false) {
-            return null;
-        }
-        $key = $this->_dataKeys[$this->_pointer];
-        return $this->getModel()->getRowByDataKey($key, $this->_parentRow);
-    }
 }
