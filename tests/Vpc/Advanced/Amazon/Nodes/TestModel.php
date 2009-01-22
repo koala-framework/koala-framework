4042d355bc726da3a343169f63b342cbd51ff4f8
Niko Sams <ns@vivid-planet.com>
2009-01-22 09:39:04 +0000

    addmissingfile

---
+++ tests/Vpc/Advanced/Amazon/Nodes/TestModel.php
@@ -0,0 +1,9 @@
+<?php
+class Vpc_Advanced_Amazon_Nodes_TestModel extends Vps_Model_FnF
+{
+    protected $_primaryKey = 'component_id';
+    protected $_data = array(array(
+        'component_id' => 'root_amazon',
+        'associate_tag' => 'prosalzburgat-21'
+    ));
+}
