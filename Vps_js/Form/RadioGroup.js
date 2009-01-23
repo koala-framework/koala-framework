2f5f337f4681bb2da75f0be4ef8fcca5d714141c
Niko Sams <ns@vivid-planet.com>
2009-01-23 14:17:42 +0000

    - value config unterstützen - setValue/getValue wenn noch nicht gerendert unterstützen

--- Vps_js/Form/RadioGroup.js
+++ Vps_js/Form/RadioGroup.js
@@ -2,24 +2,38 @@ Vps.Form.RadioGroup = Ext.extend(Ext.form.RadioGroup, {
     initComponent: function() {
         Vps.Form.RadioGroup.superclass.initComponent.call(this);
     },
+    afterRender: function() {
+        Vps.Form.RadioGroup.superclass.afterRender.call(this);
+        if (this.value) {
+            this.setValue(this.value);
+        }
+    },
     getValue: function() {
-        var ret = null;
-        this.items.each(function(c) {
-            if (c.getValue()) {
-                ret = c.inputValue;
-                return false;
-            }
-        }, this);
-        return ret;
+        if (!this.rendered) {
+            return this.value;
+        } else {
+            var ret = null;
+            this.items.each(function(c) {
+                if (c.getValue()) {
+                    ret = c.inputValue;
+                    return false;
+                }
+            }, this);
+            return ret;
+        }
     },
     setValue: function(v) {
-        this.items.each(function(c) {
-            if (c.inputValue == v) {
-                c.setValue(true);
-            } else {
-                c.setValue(false);
-            }
-        }, this);
+        if (!this.rendered) {
+            this.value = v;
+        } else {
+            this.items.each(function(c) {
+                if (c.inputValue == v) {
+                    c.setValue(true);
+                } else {
+                    c.setValue(false);
+                }
+            }, this);
+        }
     }
 });
 Ext.reg('radiogroup', Vps.Form.RadioGroup);
