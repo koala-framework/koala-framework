30b3b1587502f5136b960583dbf78b96ca5d65e1
Niko Sams <ns@vivid-planet.com>
2009-01-23 09:57:42 +0000

    settings umformatiert

--- Vpc/Basic/Image/Component.php
+++ Vpc/Basic/Image/Component.php
@@ -4,22 +4,23 @@ class Vpc_Basic_Image_Component extends Vpc_Abstract
 {
     public static function getSettings()
     {
-        $ret = array_merge(parent::getSettings(), array(
-            'componentName'     => trlVps('Image'),
-            'componentIcon'     => new Vps_Asset('picture'),
-            'modelname'         => 'Vpc_Basic_Image_Model',
+        $ret = parent::getSettings();
+        $ret['componentName'] = trlVps('Image');
+        $ret['componentIcon'] = new Vps_Asset('picture');
+        $ret['modelname'] = 'Vpc_Basic_Image_Model';
 
-            'dimensions'        => array(300, 200, Vps_Media_Image::SCALE_BESTFIT), // Leeres Array -> freie Wahl, array(width, height), array(array(width, height), null: Bild in Originalgröße)
-            'editComment'       => false,
-            'editFilename'      => false,
-            'allowBlank'        => true,
-            'pdfMaxWidth'       => 0,
-            'pdfMaxDpi'         => 150,
-            'imgCssClass'       => '',
-            'emptyImage'        => false,
-            'useParentImage'    => false,
-            'showHelpText'      => false
-        ));
+        // Leeres Array -> freie Wahl, array(width, height), array(array(width, height), null: Bild in Originalgröße)
+        $ret['dimensions'] = array(300, 200, Vps_Media_Image::SCALE_BESTFIT);
+
+        $ret['editComment'] = false;
+        $ret['editFilename'] = false;
+        $ret['allowBlank'] = true;
+        $ret['pdfMaxWidth'] = 0;
+        $ret['pdfMaxDpi'] = 150;
+        $ret['imgCssClass'] = '';
+        $ret['emptyImage'] = false;
+        $ret['useParentImage'] = false;
+        $ret['showHelpText'] = false;
         $ret['assetsAdmin']['dep'][] = 'VpsSwfUpload';
         return $ret;
     }
