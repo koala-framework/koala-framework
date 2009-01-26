d9fd6e7d6ff1d97ad77a47a2f76a4ccbafa5a8e6
Niko Sams <ns@vivid-planet.com>
2009-01-26 13:10:56 +0000

    dateiname in seitenbaum kann auf wunsch extra angeg3eben werden

--- Vps/Dao/Pages.php
+++ Vps/Dao/Pages.php
@@ -11,8 +11,7 @@ class Vps_Dao_Pages extends Vps_Db_Table_Abstract
     protected function _setupFilters()
     {
         parent::_setupFilters();
-        $this->_filters['filename'] = new Vps_Filter_Row_UniqueAscii();
-        $this->_filters['filename']->setGroupBy(array('parent_id', 'domain'));
+        $this->_filters['filename'] = new Vps_Dao_Pages_FilenameFilter();
         $this->_filters['pos'] = new Vps_Filter_Row_Numberize();
         $this->_filters['pos']->setGroupBy(array('parent_id', 'category', 'domain'));
     }
