30742f48b5d6c0d5b5b28d202fb68bd1dc334b6c
Niko Sams <ns@vivid-planet.com>
2009-01-30 16:07:26 +0000

    hilfs-fkt zum xtype-tree ausgeben

--- Vps_js/Debug/Activator.js
+++ Vps_js/Debug/Activator.js
@@ -24,3 +24,19 @@ Vps.Debug.showActivator = true;
 //if we have the activator loaded we always want to show debug messages
 //DebugData-Helper doesn't output anything so we set it here
 Vps.Debug.displayErrors = true;
+
+Ext.Container.prototype.logXTypesTree = function() {
+    var level = 0;
+    function foo(c) {
+        if(c.items){
+            var cs = c.items.items;
+            level++;
+            for(var i = 0, len = cs.length; i < len; i++){
+                console.log(new Array(level).join(' ')+cs[i].getXTypes());
+                foo(cs[i]);
+            }
+            level--;
+        }
+    }
+    foo(this);
+};
