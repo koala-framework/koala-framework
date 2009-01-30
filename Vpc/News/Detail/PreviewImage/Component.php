06a7bfe602ccfc6f09c3bd37dedecbf3fd1ee582
Niko Sams <ns@vivid-planet.com>
2009-01-30 15:54:49 +0000

    fix dimensions

--- Vpc/News/Detail/PreviewImage/Component.php
+++ Vpc/News/Detail/PreviewImage/Component.php
@@ -3,9 +3,14 @@ class Vpc_News_Detail_PreviewImage_Component extends Vpc_Basic_Image_Component
 {
     public static function getSettings()
     {
-        $ret = array_merge(parent::getSettings(), array(
-            'dimensions'         => array(30, 20),
-        ));
+        $ret = parent::getSettings();
+        $ret['dimensions'] = array(
+            array(
+                'width'=>30,
+                'height'=>20,
+                'scale'=>Vps_Media_Image::SCALE_BESTFIT
+            )
+        );
         return $ret;
     }
 }
