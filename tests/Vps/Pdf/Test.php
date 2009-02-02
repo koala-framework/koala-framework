4c101f659833f2d8603f207d26d1f5c8c7320370
Lorenz Maislinger <lm@vivid-planet.com>
2009-02-02 07:38:56 +0000

    Umstellung bei Tcpdf -> Umstellung von Pdf fonts pfad bzw. hinzufügen eines zweiten zentralen fontpath wo neue generierte fonts gespeichert werden können.

---
+++ tests/Vps/Pdf/Test.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @group Pdf
+ */
+class Vps_Pdf_Test extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Http host muss gesetzt werden da sonst in der config von tcpdf
+     * in Zeile 65 die Variable nicht gesetzt wird.
+     *
+     */
+    public function setUp()
+    {
+        $_SERVER['HTTP_HOST'] = Zend_Registry::get('testDomain');
+    }
+
+    public function tearDown()
+    {
+        unset($_SERVER['HTTP_HOST']);
+    }
+
+    public function testFonts()
+    {
+        $pdf = new Vps_Pdf_TcPdf();
+        $pdf->addPage();
+        $pdf->Ln(5);
+        $pdf->SetFont('helvetica', '', 10);
+        $pdf->textBox("helvetica");
+        $pdf->Ln(5);
+        $pdf->SetFont('comic', '', 10);
+        $pdf->textBox("comic");
+        $pdf->Ln(5);
+        $pdf->SetFont('helvetican', '', 10);
+        $pdf->textBox("helvetican");
+        $pdf->Ln(5);
+        $pdf->SetFont('arial', '', 10);
+        $pdf->textBox("arial");
+    }
+    public function testNotExistingFonts()
+    {
+        $this->setExpectedException('Vps_Exception');
+        $pdf = new Vps_Pdf_TcPdf();
+        $pdf->addPage();
+        $pdf->Ln(5);
+        $pdf->SetFont('helveticablub', '', 10);
+    }
+}
