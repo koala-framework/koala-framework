0e43aee61013d8fa66dc8c88545a53ca00ba0b05
Niko Sams <ns@vivid-planet.com>
2009-02-13 11:06:03 +0000

    livetime für view, wird ja jetzt auch gecached

--- Vpc/Advanced/Amazon/Nodes/ProductsDirectory/View/Component.php
+++ Vpc/Advanced/Amazon/Nodes/ProductsDirectory/View/Component.php
@@ -1,4 +1,8 @@
 <?php
 class Vpc_Advanced_Amazon_Nodes_ProductsDirectory_View_Component extends Vpc_Directories_List_ViewPage_Component
 {
+    public function getViewCacheLifetime()
+    {
+        return 24*60*60;
+    }
 }
