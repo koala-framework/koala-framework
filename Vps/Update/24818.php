5f270409589171a74cd19cbc78a3992623a54ec2
Franz Unger <ufx@vivid-planet.com>
2009-02-24 15:38:50 +0000

    Update-Script geändert

--- Vps/Update/24818.php
+++ Vps/Update/24818.php
@@ -3,12 +3,7 @@ class Vps_Update_24818 extends Vps_Update
 {
     protected function _init()
     {
-        $this->_actions[] = new Vps_Update_Action_Db_AddField(array(
-            'table' => 'cache_component_meta',
-            'field' => 'callback',
-            'type' => 'varchar(255)',
-            'null' => true,
-            'default' => NULL,
-        ));
+        $sql = "ALTER TABLE `cache_component_meta` ADD `callback` VARCHAR( 255 ) NOT NULL AFTER `cache_id`";
+        Zend_Registry::get('db')->query($sql);
     }
 }
