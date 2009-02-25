dd01b4820d0158240d549aa40566f0b1f2b0795b
Franz Unger <ufx@vivid-planet.com>
2009-02-25 05:29:40 +0000

    Fix

--- Vps/Update/24818.php
+++ Vps/Update/24818.php
@@ -1,7 +1,7 @@
 <?php
 class Vps_Update_24818 extends Vps_Update
 {
-    protected function _init()
+    protected function update()
     {
         $sql = "ALTER TABLE `cache_component_meta` ADD `callback` VARCHAR( 255 ) NOT NULL AFTER `cache_id`";
         Zend_Registry::get('db')->query($sql);
