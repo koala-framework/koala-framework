b310e74502e1d23e594730499ba414df77cad66f
Niko Sams <ns@vivid-planet.com>
2009-03-02 10:05:16 +0000

    es kann auch direkt ein array zurückgegeben werden

--- Vps/Collection/Iterator.php
+++ Vps/Collection/Iterator.php
@@ -6,6 +6,9 @@ class Vps_Collection_Iterator extends ArrayIterator
     public function __construct($collection, $flags = 0)
     {
         $this->_collection = $collection;
-        parent::__construct($collection->getArray(), $flags);
+        if ($collection instanceof Vps_Collection) {
+            $collection = $collection->getArray();
+        }
+        parent::__construct($collection, $flags);
     }
 }
