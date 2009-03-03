5e17d0c9144037db0a5afcaced01f499c07115c5
Niko Sams <ns@vivid-planet.com>
2009-03-03 14:06:51 +0000

    umbau auf partials (hab ich da beim cache löshen eh nix vergessen?)

--- Vpc/Abstract/ListRandom/Component.php
+++ Vpc/Abstract/ListRandom/Component.php
@@ -1,22 +1,35 @@
 <?php
 class Vpc_Abstract_ListRandom_Component extends Vpc_Abstract_List_Component
+    implements Vps_Component_Partial_Interface
 {
     public static function getSettings()
     {
         $ret = parent::getSettings();
-        $ret['viewCache'] = false;
+        $ret['limit'] = 1;
         return $ret;
     }
 
     public function getTemplateVars()
     {
-        $ret = parent::getTemplateVars();
-        if ($ret['children']) {
-            $randKey = array_rand($ret['children']);
-            $ret['child'] = $ret['children'][$randKey];
-            $ret['children'] = array($ret['child']);
-        }
+        $ret = Vpc_Abstract::getTemplateVars();
+        $ret['count'] = $this->getData()->countChildComponents(array('generator' => 'child'));
+        $ret['limit'] = $this->_getSetting('limit');
         return $ret;
     }
+    public function getPartialVars($partial, $nr, $info)
+    {
+        if (!$partial instanceof Vps_Component_Partial_Random)
+            throw new Vps_Exception('Only Vps_Component_Partial_Random supported');
+        $select = new Vps_Component_Select();
+        $select->whereGenerator('child');
+        $select->limit(1, $nr);
+        return array('child' =>
+            $this->getData()->getChildComponent($select)
+        );
+    }
 
+    public function getPartialCacheVars($nr)
+    {
+        return array();
+    }
 }
