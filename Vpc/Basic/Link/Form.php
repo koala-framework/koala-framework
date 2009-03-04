4dda9c0a2815733bf5f2506b092abcd0755ea34b
Christoph Eder <ce@vivid-planet.com>
2009-03-04 16:53:15 +0000

    Titel und Link in Lightbox bugfix (niko)

--- Vpc/Basic/Link/Form.php
+++ Vpc/Basic/Link/Form.php
@@ -1,6 +1,7 @@
 <?php
 class Vpc_Basic_Link_Form extends Vpc_Abstract_Composite_Form
 {
+    protected $_createFieldsets = false;
     protected function _initFields()
     {
         $this->add(new Vps_Form_Field_TextField('text', trlVps('Linktext')))
