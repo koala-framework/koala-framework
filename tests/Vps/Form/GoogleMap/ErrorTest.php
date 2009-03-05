2c4e821976adb20409838223ac66a9e5e72bc428
Niko Sams <ns@vivid-planet.com>
2009-03-05 08:59:21 +0000

    - fix test (waitForConnections fehlte!!) - mache test einfacher (nicht mit seehr strangem try catch :D)

--- tests/Vps/Form/GoogleMap/ErrorTest.php
+++ tests/Vps/Form/GoogleMap/ErrorTest.php
@@ -3,6 +3,7 @@
  * @group selenium
  * @group slow
  * @group AutoForm
+ * @group Form_GoogleMap
  */
 class Vps_Form_GoogleMap_ErrorTest extends Vps_Test_SeleniumTestCase
 {
@@ -15,6 +16,7 @@ class Vps_Form_GoogleMap_ErrorTest extends Vps_Test_SeleniumTestCase
     public function testGoogleNewOpenWindow()
     {
         $this->open('/vps/test/vps_form_google-map_test');
+        $this->waitForConnections();
         $this->click("//input[@name='mapEmpty']/../..//img");
         $this->assertNotNull($this->getText("//span[text()='Adresse eingeben.']"));
     }
@@ -22,15 +24,9 @@ class Vps_Form_GoogleMap_ErrorTest extends Vps_Test_SeleniumTestCase
     public function testGoogleOldOpenWindow()
     {
         $this->open('/vps/test/vps_form_google-map_test');
+        $this->waitForConnections();
         $this->click("//input[@name='mapSelected']/../..//img");
-        try {
-            //hier muss eine selenium exception geworfen werden
-            $this->getText("//span[text()='Adresse eingeben.']");
-            $this->assertTrue(false);
-        } catch (Exception $e) {
-            $this->assertTrue(true);
-        }
-        sleep(5);
+        $this->assertElementNotPresent("//span[text()='Adresse eingeben.']");
     }
 
 }
