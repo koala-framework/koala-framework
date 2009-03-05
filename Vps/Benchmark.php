20df1c8c137bf61e0bd1f21488f9f894ba8197c6
Niko Sams <ns@vivid-planet.com>
2009-03-05 14:34:55 +0000

    bugfix

--- Vps/Benchmark.php
+++ Vps/Benchmark.php
@@ -127,7 +127,11 @@ class Vps_Benchmark
             $load = @file_get_contents('/proc/loadavg');
             $load = explode(' ', $load);
             echo "Load: ". $load[0]."<br />\n";
-            echo "Memory: ".round(memory_get_peak_usage()/1024)." kb<br />\n";
+            if (function_exists('memory_get_peak_usage')) {
+                echo "Memory: ".round(memory_get_peak_usage()/1024)." kb<br />\n";
+            } else {
+                echo "Memory: ".round(memory_get_usage()/1024)." kb<br />\n";
+            }
             if (Zend_Registry::get('db')->getProfiler() && method_exists(Zend_Registry::get('db')->getProfiler(), 'getQueryCount')) {
                 echo "DB-Queries: ".Zend_Registry::get('db')->getProfiler()->getQueryCount()."<br />\n";
             }
