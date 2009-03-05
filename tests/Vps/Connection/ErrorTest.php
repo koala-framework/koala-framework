7bf21285a1e87b8d2877dd579293c500d0b85c3c
Niko Sams <ns@vivid-planet.com>
2009-03-05 09:33:15 +0000

    fix test

--- tests/Vps/Connection/ErrorTest.php
+++ tests/Vps/Connection/ErrorTest.php
@@ -80,6 +80,6 @@ class Vps_Connection_ErrorTest extends Vps_Test_SeleniumTestCase
         $this->click("//button[text()='testD']");
         $this->waitForConnections();
         $text = $this->getText("//div[contains(text(), 'Vps_Exception')]");
-        $this->assertTrue((bool)strpos($text, 'Connection/TestController.php:55'));
+        $this->assertTrue((bool)strpos($text, 'Connection/TestController.php:50'));
     }
 }
