cd089e2f5ba2f804d0dff16f225e19fbb818143b
Niko Sams <ns@vivid-planet.com>
2009-03-06 09:38:02 +0000

    das ist nicht die db id sondern die componentId

--- Vpc/Abstract/Image/Component.php
+++ Vpc/Abstract/Image/Component.php
@@ -151,7 +151,7 @@ class Vpc_Abstract_Image_Component extends Vpc_Abstract_Composite_Component
 
     public static function getMediaOutput($id, $type, $className)
     {
-        $row = Vps_Component_Data_Root::getInstance()->getComponentByDbId($id, array('limit'=>1))
+        $row = Vps_Component_Data_Root::getInstance()->getComponentById($id)
             ->getComponent()->getImageRow();
         if ($row) {
             $fileRow = $row->getParentRow('Image');
