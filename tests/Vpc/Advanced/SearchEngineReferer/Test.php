561e2b71502fab2e327042486f04982b80b29a02
Franz Unger <ufx@vivid-planet.com>
2009-03-06 13:40:25 +0000

    SearchEngineReferer-Test funktioniert wieder

--- tests/Vpc/Advanced/SearchEngineReferer/Test.php
+++ tests/Vpc/Advanced/SearchEngineReferer/Test.php
@@ -9,45 +9,32 @@ class Vpc_Advanced_SearchEngineReferer_Test extends PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        /*
-        $this->_cache = $this->getMock('Vps_Component_Cache', array('remove'), array(), '', false);
-        Vps_Component_Cache::setInstance($this->_cache);
-
         Vps_Component_Data_Root::setComponentClass('Vpc_Advanced_SearchEngineReferer_Root');
         $this->_root = Vps_Component_Data_Root::getInstance();
 
+        Vps_Component_Cache::getInstance()->setModel(new Vps_Component_Cache_CacheModel());
+        Vps_Component_Cache::getInstance()->setMetaModel(new Vps_Component_Cache_CacheMetaModel());
+        Vps_Component_Cache::getInstance()->emptyPreload();
+        Vps_Component_RowObserver::getInstance()->setSkipFnF(false);
         Vps_Component_RowObserver::getInstance()->clear();
-        */
     }
 
     public function tearDown()
     {
-        $this->markTestIncomplete();
-        Vps_Component_Cache::setInstance(null);
         if (isset($_SERVER['HTTP_REFERER'])) unset($_SERVER['HTTP_REFERER']);
     }
 
-    public function testModifyTable()
+    public function testCache()
     {
-        // Vps_Component_Cache wird neu geschrieben, danach kann man das besser testen
-        $this->markTestIncomplete();
-
-        $this->_cache->expects($this->once())
-            ->method('remove')
-            ->with($this->equalTo(array('root-referer'=>$this->_root->getChildComponent('-referer'))));
-
-        $m = Vps_Model_Abstract::getInstance('Vpc_Advanced_SearchEngineReferer_Referer_Model');
-        $r = $m->createRow();
-        $r->name = 'foo12';
-        $r->component_id = 'root';
-        $r->save();
-
-        Vps_Component_RowObserver::getInstance()->process();
+        $cacheVars = $this->_root
+            ->getChildComponent('-referer2')
+            ->getChildComponent('-view')->getComponent()->getCacheVars();
+        $this->assertEquals(1, count($cacheVars));
+        $this->assertEquals('Vpc_Advanced_SearchEngineReferer_Referer2_Model', get_class($cacheVars[0]['model']));
     }
 
     public function testComponentNewEntry()
     {
-        $this->markTestIncomplete();
         $ref2 = $this->_root->getChildComponent('-referer2')->getComponent();
         $model = $ref2->getModel();
         $oldRow = $model->getRow($model->select()
