72924e3a7a1200beb6b06a27eb739344bd288c6e
Niko Sams <ns@vivid-planet.com>
2009-03-09 12:40:46 +0000

    nur dateien öffnen die mit Test.php aufhören => test ausführen schneller

--- Vps/Test/TestSuite.php
+++ Vps/Test/TestSuite.php
@@ -4,11 +4,11 @@ class Vps_Test_TestSuite extends PHPUnit_Framework_TestSuite
     public function __construct($name = 'Vps Framework')
     {
         parent::__construct($name);
-        $this->_addDirectory(VPS_PATH.'/tests');
-        $this->_addDirectory('./tests');
+        $this->_addDirectory(VPS_PATH.'/tests', true);
+        $this->_addDirectory('./tests', false);
     }
 
-    private function _addDirectory($basePath)
+    private function _addDirectory($basePath, $onlyTestPrefix)
     {
         if (!file_exists($basePath)) return;
 
@@ -21,6 +21,9 @@ class Vps_Test_TestSuite extends PHPUnit_Framework_TestSuite
 
         foreach ($dir as $file) {
             $file = substr($file, strlen($basePath)+1);
+            if (substr($file, -8) != 'Test.php' && $onlyTestPrefix) {
+                continue;
+            }
             $className = str_replace(array('./', '.php', '/'), array('', '', '_'), $file);
             if (class_exists($className) && is_instance_of($className, 'PHPUnit_Framework_TestCase')) {
                 require_once($file);
