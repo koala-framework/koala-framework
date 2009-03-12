5b2829e932975831fe9c8d63fd00561c16a2a621
Niko Sams <ns@vivid-planet.com>
2009-03-12 12:03:07 +0000

    online gibts keine test-datenbank

--- tests/Vps/Model/DbWithConnection/ImportExport/Test.php
+++ tests/Vps/Model/DbWithConnection/ImportExport/Test.php
@@ -27,6 +27,10 @@ class Vps_Model_DbWithConnection_ImportExport_Test extends PHPUnit_Framework_Tes
     public function testServiceFormatSql()
     {
         $d = Zend_Registry::get('testDomain');
+        if (substr($d, -6) == '.vivid') {
+            //online gibts keine test-datenbank
+            $this->markTestSkipped();
+        }
 
         $client = new Vps_Srpc_Client(array(
             'serverUrl' => "http://$d/vps/test/vps_model_db-with-connection_import-export_test/export",
