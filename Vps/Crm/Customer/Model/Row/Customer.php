e01f4218e4f4ac75e300f834b67dff3f3c535096
Markus Hauser <mh@vivid-planet.com>
2009-03-13 14:24:43 +0000

    2 schoenheitsfehler im crm

--- Vps/Crm/Customer/Model/Row/Customer.php
+++ Vps/Crm/Customer/Model/Row/Customer.php
@@ -7,7 +7,7 @@ class Vps_Crm_Customer_Model_Row_Customer extends Vps_Model_Proxy_Row
         $r = Vps_Model_Abstract::getInstance('Vps_Crm_Customer_Model_Comments')->createRow();
         $r->customer_id = $this->id;
         $authedUser = Vps_Registry::get('userModel')->getAuthedUser();
-        $r->value = trlVps('Customer added by {0}', $authedUser->__toString());
+        $r->value = trlVps('Customer added by {0}', $authedUser ? $authedUser->__toString() : 'Import');
         $r->save();
     }
 }
