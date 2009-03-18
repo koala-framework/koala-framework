82e6b4299fdeca656c4435a791dc5475d44c5355
Franz Unger <ufx@vivid-planet.com>
2009-03-18 13:38:35 +0000

    Cache löschen Bug: dbId statt componentId AssignedCategories Cache löschen

--- Vpc/Directories/Item/Detail/AssignedCategories/View/Component.php
+++ Vpc/Directories/Item/Detail/AssignedCategories/View/Component.php
@@ -8,4 +8,16 @@ class Vpc_Directories_Item_Detail_AssignedCategories_View_Component
         $ret['generators']['child']['component']['paging'] = false;
         return $ret;
     }
+
+    public function getCacheVars()
+    {
+        $ret = parent::getCacheVars();
+        // TODO: Nur bei eigener ID löschen, dazu müsste man das Feld angeben können
+        $c = $this->getData()->parent->getComponent()->getItemDirectory()->getComponent();
+        $ret[] = array(
+            'model' => Vpc_Abstract::getSetting(get_class($c), 'categoryToItemTableName'),
+            'id' => null
+        );
+        return $ret;
+    }
 }
