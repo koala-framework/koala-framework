b62daab08be85335fe543d84676980d41677c435
Niko Sams <ns@vivid-planet.com>
2009-03-19 14:11:34 +0000

    neuer analytics js-code todo:2431

--- Vps/View/Helper/StatisticCode.php
+++ Vps/View/Helper/StatisticCode.php
@@ -14,9 +14,10 @@ class Vps_View_Helper_StatisticCode
             $ret .= "    document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));\n";
             $ret .= "</script>\n";
             $ret .= "<script type=\"text/javascript\">\n";
-            $ret .= "    var pageTracker = _gat._getTracker(\"".$analyticsCode."\");\n";
-            $ret .= "    pageTracker._initData();\n";
-            $ret .= "    pageTracker._trackPageview();\n";
+            $ret .= "    try {\n";
+            $ret .= "        var pageTracker = _gat._getTracker(\"".$analyticsCode."\");\n";
+            $ret .= "        pageTracker._trackPageview();\n";
+            $ret .= "    } catch(err) {}\n";
             $ret .= "</script>\n";
         }
         return $ret;
