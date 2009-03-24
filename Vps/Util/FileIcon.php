19f98f3a06bb05884ebd44edf66c348808bf87b9
Niko Sams <ns@vivid-planet.com>
2009-03-24 07:09:32 +0000

    fix break

--- Vps/Util/FileIcon.php
+++ Vps/Util/FileIcon.php
@@ -6,29 +6,37 @@ class Vps_Util_FileIcon
         switch ($extension) {
             case 'pdf':
                 $icon = 'page_white_acrobat';
+                break;
             case 'doc':
             case 'docx':
                 $icon = 'page_white_word';
+                break;
             case 'xls':
             case 'xlsx':
                 $icon = 'page_white_excel';
+                break;
             case 'ppt':
             case 'pptx':
                 $icon = 'page_white_powerpoint';
+                break;
             case 'zip':
             case 'rar':
             case 'gz':
             case 'bz2':
                 $icon = 'page_white_compressed';
+                break;
             case 'exe':
                 $icon = 'page_white_gear';
+                break;
             case 'jpg':
             case 'gif':
             case 'png':
             case 'psd':
                 $icon = 'page_white_picture';
+                break;
             default:
                 $icon = 'page_white_get';
+                break;
         }
         return new Vps_Asset($icon);
     }
