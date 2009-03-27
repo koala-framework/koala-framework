fc469d43182a57336f71cec524c0f42b9c0200d3
Niko Sams <ns@vivid-planet.com>
2009-03-27 08:49:01 +0000

    leere fkt entfernt

--- Vpc/Directories/CategoryTree/Directory/Generator.php
+++ Vpc/Directories/CategoryTree/Directory/Generator.php
@@ -2,11 +2,4 @@
 class Vpc_Directories_CategoryTree_Directory_Generator extends Vps_Component_Generator_Page_Table
 {
     protected $_filenameColumn = 'name_path';
-
-    protected function _formatSelect($parentData, array $constraints = array())
-    {
-        $select = parent::_formatSelect($parentData, $constraints);
-        if (!$select) return null;
-        return $select;
-    }
 }
