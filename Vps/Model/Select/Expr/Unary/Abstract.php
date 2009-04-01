fcf1ce7f48af99c8690dd202e2b2302498f0fd27
Niko Sams <ns@vivid-planet.com>
2009-04-01 14:00:08 +0000

    verbesserter zugriff auf einzelne elemente

--- Vps/Model/Select/Expr/Unary/Abstract.php
+++ Vps/Model/Select/Expr/Unary/Abstract.php
@@ -1,5 +1,6 @@
 <?php
-abstract class Vps_Model_Select_Expr_Unary_Abstract implements Vps_Model_Select_Expr_Interface
+abstract class Vps_Model_Select_Expr_Unary_Abstract
+    implements Vps_Model_Select_Expr_Interface, ArrayAccess
 {
     protected $_expressions;
     public function __construct(array $expressions)
@@ -9,6 +10,32 @@ abstract class Vps_Model_Select_Expr_Unary_Abstract implements Vps_Model_Select_
     public function getExpressions()
     {
         return $this->_expressions;
+    }
+    public function addExpression($x)
+    {
+        $this->_expressions[] = $x;
+        return $this;
+    }
+
+    public function offsetExists($offset)
+    {
+        throw new Vps_Exception_NotYetImplemented();
+    }
+    public function offsetGet($offset)
+    {
+        throw new Vps_Exception_NotYetImplemented();
+    }
+    public function offsetSet($offset, $value)
+    {
+        if (!$offset) {
+            $this->_expressions[] = $value;
+        } else {
+            throw new Vps_Exception_NotYetImplemented();
+        }
+    }
+    public function offsetUnset($offset)
+    {
+        throw new Vps_Exception_NotYetImplemented();
     }
 
     public function validate ()
