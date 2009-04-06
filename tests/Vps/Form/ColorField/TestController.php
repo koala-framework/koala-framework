f84ac79d974f7e1ab44be04bb4388a53c743b77a
Niko Sams <ns@vivid-planet.com>
2009-04-06 09:11:48 +0000

    ColorField: kstal mit farbe daneben anzeigen todo:2484

---
+++ tests/Vps/Form/ColorField/TestController.php
@@ -0,0 +1,26 @@
+<?php
+class Vps_Form_ColorField_TestController extends Vps_Controller_Action_Auto_Form
+{
+    protected $_modelName = 'Vps_Form_ColorField_TestModel';
+    protected $_permissions = array('save', 'add');
+    protected $_buttons = array('save');
+
+    protected function _initFields()
+    {
+        $this->_form->add(new Vps_Form_Field_ColorField('color', 'color'));
+    }
+
+    protected function _getResourceName()
+    {
+        return 'vps_test';
+    }
+    public function indexAction()
+    {
+        $config = array();
+        $config['baseParams']['id'] = 1;
+        $config['controllerUrl'] = $this->getRequest()->getPathInfo();
+        $config['assetsType'] = 'Vps_Form_ColorField:Test';
+        $this->view->ext('Vps.Auto.FormPanel', $config, 'Vps.Test.Viewport');
+    }
+}
+
---
+++ tests/Vps/Form/ColorField/TestModel.php
@@ -0,0 +1,7 @@
+<?php
+class Vps_Form_ColorField_TestModel extends Vps_Model_FnF
+{
+    protected $_data = array(
+        array('id'=>1, 'color'=>'ff0000')
+    );
+}
