a5345bb0c24eefcbe901b7713029b36c50c07375
Markus Hauser <mh@vivid-planet.com>
2009-04-08 08:41:40 +0000

    merge: fix fuer scrollprobleme im ie7

--- Vps_js/Panel.js
+++ Vps_js/Panel.js
@@ -10,3 +10,24 @@ Ext.Panel.prototype.mabySubmit = function(cb, options) {
     }
     return ret;
 };
+
+/*
+ * Fix von: http://extjs.com/forum/showthread.php?p=224621#post224621
+ * Fehler trat auf im IE7 in Stargate bei folgender Kombination:
+ * Panel -> TabPanel -> FormPanel -> Columns -> Column -> TextArea
+ * Wenn die Textarea scrollbar ist und man hin und her scrollt kommt es
+ * vor, dass man bei der äußeren scrollbar nicht nach ganz oben scrollen
+ * kann und somit auch nicht mehr speichern kann.
+ */
+Ext.Panel.override({
+    setAutoScroll: function() {
+        if (this.rendered && this.autoScroll) {
+            var el = this.body || this.el;
+            if (el) {
+                el.setOverflow('auto');
+                // Following line required to fix autoScroll
+                el.dom.style.position = 'relative';
+            }
+        }
+    }
+});
