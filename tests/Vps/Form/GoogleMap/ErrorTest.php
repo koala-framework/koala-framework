707609948ec3880ce666c734e24a0c5891946692
Niko Sams <ns@vivid-planet.com>
2009-04-14 12:55:15 +0000

    test ein einen zusammengeführt -> sollte schneller sein

--- tests/Vps/Form/GoogleMap/ErrorTest.php
+++ tests/Vps/Form/GoogleMap/ErrorTest.php
@@ -7,26 +7,17 @@
  */
 class Vps_Form_GoogleMap_ErrorTest extends Vps_Test_SeleniumTestCase
 {
-    public function setUp()
-    {
-        parent::setUp();
-        $this->setTimeout(300000);
-    }
-
     public function testGoogleNewOpenWindow()
     {
         $this->open('/vps/test/vps_form_google-map_test');
         $this->waitForConnections();
         $this->click("//input[@name='mapEmpty']/../..//img");
-        $this->assertNotNull($this->getText("//span[text()='Adresse eingeben.']"));
-    }
+        $this->assertNotNull($this->getText("//span[text()='".trlVps('enter address')."']"));
 
-    public function testGoogleOldOpenWindow()
-    {
         $this->open('/vps/test/vps_form_google-map_test');
         $this->waitForConnections();
         $this->click("//input[@name='mapSelected']/../..//img");
-        $this->assertElementNotPresent("//span[text()='Adresse eingeben.']");
+        $this->assertElementNotPresent("//span[text()='".trlVps('enter address')."']");
     }
 
 }
