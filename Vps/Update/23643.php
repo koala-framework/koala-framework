b16227e597fe2e9e49d59f6aebe48ea466671c82
Franz Unger <ufx@vivid-planet.com>
2009-05-05 06:36:06 +0000

    Klasse von statischer Methode in Update-Scripts hat sich geändert

--- Vps/Update/23643.php
+++ Vps/Update/23643.php
@@ -6,8 +6,8 @@ class Vps_Update_23643 extends Vps_Update
         foreach (array('content', 'media', 'admin', 'asset', 'cli', 'unkown') as $t) {
             $this->_actions[] = new Vps_Update_Action_Rrd_RenameDs(array(
                 'file' => 'benchmark.rrd',
-                'name' => Vps_Controller_Action_Cli_BenchmarkController::escapeField($t.'-rendered cache (preloaded)'),
-                'newName' => Vps_Controller_Action_Cli_BenchmarkController::escapeField($t.'-rendered cache'),
+                'name' => Vps_Util_Rrd_Field::escapeField($t.'-rendered cache (preloaded)'),
+                'newName' => Vps_Util_Rrd_Field::escapeField($t.'-rendered cache'),
             ));
         }
     }
--- Vps/Update/23644.php
+++ Vps/Update/23644.php
@@ -6,7 +6,7 @@ class Vps_Update_23644 extends Vps_Update
         foreach (array('content', 'media', 'admin', 'asset', 'cli', 'unkown') as $t) {
             $this->_actions[] = new Vps_Update_Action_Rrd_AddDs(array(
                 'file' => 'benchmark.rrd',
-                'name' => Vps_Controller_Action_Cli_BenchmarkController::escapeField($t.'-rendered noviewcache'),
+                'name' => Vps_Util_Rrd_Field::escapeField($t.'-rendered noviewcache'),
                 'max'=>(2^31)
             ));
         }
