0c61589c4c296c2d3c528e8e337ddab7a8d6a32b
Niko Sams <ns@vivid-planet.com>
2009-05-06 15:23:01 +0000

    Event LinkTag

---
+++ Vpc/Basic/LinkTag/Event/Component.php
@@ -0,0 +1,23 @@
+<?php
+class Vpc_Basic_LinkTag_Event_Component extends Vpc_Basic_LinkTag_Abstract_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['dataClass'] = 'Vpc_Basic_LinkTag_Event_Data';
+        $ret['componentName'] = trlVps('Link.to Event');
+        $ret['modelname'] = 'Vps_Component_FieldModel';
+        return $ret;
+    }
+
+    public function getCacheVars()
+    {
+        $ret = parent::getCacheVars();
+        //ein event von der der status geändert wird oder der titel geändert wird
+        if ($this->getData()->getLinkedData()) {
+            $ret = array_merge($ret, $this->getData()->getLinkedData()
+                                                ->getComponent()->getCacheVars());
+        }
+        return $ret;
+    }
+}
