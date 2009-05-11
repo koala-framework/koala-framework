6e73a1b49f41a118b18bd513da9ebb294fa60a7e
Niko Sams <ns@vivid-planet.com>
2009-05-11 11:08:46 +0000

    - web-config eigene klasse - config immer im Setup::setUp ganz oben erstellen, nicht lazy in registry - Setup::setUp() kann die konfig-klasse übergeben werden

--- Vps/Registry.php
+++ Vps/Registry.php
@@ -12,22 +12,6 @@ class Vps_Registry extends Zend_Registry
             $v = Vps_Setup::createDao();
             $this->offsetSet('dao', $v);
             return $v;
-        } else if ($index == 'config' && !parent::offsetExists($index)) {
-            require_once 'Vps/Config/Cache.php';
-            $cache = new Vps_Config_Cache;
-            $cacheId = 'config_'.Vps_Setup::getConfigSection();
-            require_once 'Zend/Config/Ini.php';
-            $mtime = $cache->test($cacheId);
-            if(!$mtime) {
-                $v = Vps_Setup::createConfig();
-                $mtime = time();
-                $cache->save($v, $cacheId);
-            } else {
-                $v = $cache->load($cacheId);
-            }
-            $this->offsetSet('configMtime', $mtime);
-            $this->offsetSet('config', $v);
-            return $v;
         } else if ($index == 'acl' && !parent::offsetExists($index)) {
             $v = new Vps_Acl();
             $this->offsetSet('acl', $v);
@@ -50,7 +34,7 @@ class Vps_Registry extends Zend_Registry
 
     public function offsetExists($index)
     {
-        if (in_array($index, array('db', 'dao', 'config', 'acl', 'userModel', 'trl', 'hlp'))) {
+        if (in_array($index, array('db', 'dao', 'acl', 'userModel', 'trl', 'hlp'))) {
             return true;
         }
         return parent::offsetExists($index);
