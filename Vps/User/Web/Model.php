e8bad049467b440031f595161a79d1944163c5b6
Markus Hauser <mh@vivid-planet.com>
2009-05-12 12:48:43 +0000

    Fix bei vps_users model, reference ist immer das in der config eingestellte model

--- Vps/User/Web/Model.php
+++ Vps/User/Web/Model.php
@@ -1,1 +1,19 @@
-<?phpclass Vps_User_Web_Model extends Vps_Model_Db{    protected $_table = 'vps_users';    protected $_referenceMap = array(        'User' => array(            'column' => 'id',            'refModelClass' => 'Vps_User_Model'        )    );    protected $_default = array('role' => 'guest');}
+<?php
+class Vps_User_Web_Model extends Vps_Model_Db
+{
+    protected $_table = 'vps_users';
+    protected $_referenceMap = array(
+        'User' => array(
+            'column' => 'id',
+            'refModelClass' => ''
+        )
+    );
+    protected $_default = array('role' => 'guest');
+
+    protected function _init()
+    {
+        $userModelClass = get_class(Vps_Registry::get('userModel'));
+        $this->_referenceMap['User']['refModelClass']  = $userModelClass;
+        parent::_init();
+    }
+}
