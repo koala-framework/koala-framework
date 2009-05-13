4baf6bda7cf97e43d8a630ffadb7230a1a64f0ea
Niko Sams <ns@vivid-planet.com>
2009-05-13 08:49:21 +0000

    update auch ausführen wenn kein mysql binary da ist

--- Vps/Update/Sql.php
+++ Vps/Update/Sql.php
@@ -7,30 +7,42 @@ class Vps_Update_Sql extends Vps_Update
         $dbConfig = Zend_Registry::get('db')->getConfig();
         $mysqlOptions = "--host={$dbConfig['host']} --user={$dbConfig['username']} --password={$dbConfig['password']} {$dbConfig['dbname']} ";
         $mysqlBinary = 'mysql';
-        if (Zend_Registry::get('config')->server->host == 'vivid-planet.com') {
-            $mysqlDir = '/usr/local/mysql/bin/mysql';
-        }
 
-        $descriptorspec = array(
-            0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
-            1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
-            2 => array("pipe", "w"),  // stderr is a pipe that the child will write to
-        );
-        $process = proc_open($mysqlBinary.' '.$mysqlOptions, $descriptorspec, $pipes);
-        if (!is_resource($process)) {
-            throw new Vps_Exception("Can't execute mysql");
-        }
-        fwrite($pipes[0], $this->sql);
-        fclose($pipes[0]);
+        exec("which $mysqlBinary", $out, $ret);
+        if (!$ret) {
 
-        $output = stream_get_contents($pipes[1]);
-        fclose($pipes[1]);
+            $descriptorspec = array(
+                0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
+                1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+                2 => array("pipe", "w"),  // stderr is a pipe that the child will write to
+            );
+            $process = proc_open($mysqlBinary.' '.$mysqlOptions, $descriptorspec, $pipes);
+            if (!is_resource($process)) {
+                throw new Vps_Exception("Can't execute mysql");
+            }
+            fwrite($pipes[0], $this->sql);
+            fclose($pipes[0]);
 
-        $output .= stream_get_contents($pipes[2]);
-        fclose($pipes[2]);
+            $output = stream_get_contents($pipes[1]);
+            fclose($pipes[1]);
 
-        if (proc_close($process) != 0) {
-            throw new Vps_Exception("Executing sql statement failed: ".$output);
+            $output .= stream_get_contents($pipes[2]);
+            fclose($pipes[2]);
+
+            if (proc_close($process) != 0) {
+                throw new Vps_Exception("Executing sql statement failed: ".$output);
+            }
+        } else {
+
+            //fallback falls kein mysql-binary vorhanden
+            //regexp von http://www.dev-explorer.com/articles/multiple-mysql-queries
+            $queries = preg_split("/;+(?=([^'|^\\\']*['|\\\'][^'|^\\\']*['|\\\'])*[^'|^\\\']*[^'|^\\\']$)/", $this->sql); 
+            foreach ($queries as $query){ 
+                if (trim($query) > 0) {
+                    Vps_Registry::get('db')->getConnection()->query($query);
+                }
+            }
+
         }
     }
 }
