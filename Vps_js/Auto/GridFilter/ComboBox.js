85468d906f0f541d5e9823a1b226f8bee232659a
Niko Sams <ns@vivid-planet.com>
2009-05-15 11:56:18 +0000

    möglichkeit eingebaut um bei cmobobox-filter eigenes feld verwenden zu können

--- Vps_js/Auto/GridFilter/ComboBox.js
+++ Vps_js/Auto/GridFilter/ComboBox.js
@@ -2,24 +2,28 @@ Vps.Auto.GridFilter.ComboBox = function(config)
 {
     Vps.Auto.GridFilter.ComboBox.superclass.constructor.call(this, config);
 
-    var record = Ext.data.Record.create(['id', 'name'])
-    var filterStore = new Ext.data.Store({
-        reader: new Ext.data.ArrayReader({}, record),
-        data: config.data
-    });
-    if (!config['default'] && filterStore.find('id', 0) == -1) {
-        filterStore.insert(0, [new record({id: 0, name: config['defaultText'] ? config['defaultText'] : trlVps('all')})]);
-		config['default'] = 0;
-    }
-    this.combo = new Ext.form.ComboBox({
-            store: filterStore,
-            displayField: 'name',
-            valueField: 'id',
-            mode: 'local',
-            triggerAction: 'all',
-            editable: false,
-            width: config.width || 200
+    if (config.field) {
+        this.combo = Ext.ComponentMgr.create(config.field);
+    } else {
+        var record = Ext.data.Record.create(['id', 'name'])
+        var filterStore = new Ext.data.Store({
+            reader: new Ext.data.ArrayReader({}, record),
+            data: config.data
         });
+        if (!config['default'] && filterStore.find('id', 0) == -1) {
+            filterStore.insert(0, [new record({id: 0, name: config['defaultText'] ? config['defaultText'] : trlVps('all')})]);
+            config['default'] = 0;
+        }
+        this.combo = new Ext.form.ComboBox({
+                store: filterStore,
+                displayField: 'name',
+                valueField: 'id',
+                mode: 'local',
+                triggerAction: 'all',
+                editable: false,
+                width: config.width || 200
+            });
+    }
     this.combo.setValue(config['default']);
     this.combo.on('select', function() {
         this.fireEvent('filter', this, this.getParams());
