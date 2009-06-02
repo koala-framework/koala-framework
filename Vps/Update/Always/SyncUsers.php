740bd166f658fdde529307290974cfd423daea47
Niko Sams <ns@vivid-planet.com>
2009-06-02 07:50:28 +0000

    nicht immer ausführen

--- Vps/Update/Always/SyncUsers.php
+++ Vps/Update/Always/SyncUsers.php
@@ -3,6 +3,11 @@ class Vps_Update_Always_SyncUsers extends Vps_Update
 {
     public function postClearCache()
     {
+        if (!Vps_Registry::get('db')) return;
+        $tables = Vps_Registry::get('db')->fetchCol('SHOW TABLES');
+        if (!in_array('vps_users', $tables)) return;
+        if (!in_array('cache_users', $tables)) return;
+
         Vps_Registry::get('userModel')->synchronize(true);
     }
 }
