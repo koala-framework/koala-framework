524d5d2e9e6cdd4f17ea8fbf3c5ad02fef5d2d8b
Franz Unger <ufx@vivid-planet.com>
2009-06-10 08:24:43 +0000

    Bugfix: componentEditUrl wurde bei PagesController nicht übergeben

--- Vps/Controller/Action/Component/PagesController.php
+++ Vps/Controller/Action/Component/PagesController.php
@@ -20,7 +20,8 @@ class Vps_Controller_Action_Component_PagesController extends Vps_Controller_Act
 
     public function indexAction()
     {
-        $this->view->ext('Vps.Component.Pages');
+        $this->view->xtype = 'vps.component.pages';
+        $this->view->componentEditUrl = '/admin/component/edit';
     }
 
     public function init()
