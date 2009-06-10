154931ea29b8c9fdf73c032016a8c36c8aeefdf8
Niko Sams <ns@vivid-planet.com>
2009-06-10 09:40:14 +0000

    Toolbar eine fixe hÃ¶he geben - weil am anfang ist ja noch nix drinnen. Fixt most-annoying-bug-ever: in paragraphs kann man jetzt ganz runter scrollen!

--- Vps_js/Component/ComponentPanel.js
+++ Vps_js/Component/ComponentPanel.js
@@ -4,7 +4,7 @@ Vps.Component.ComponentPanel = Ext.extend(Vps.Binding.AbstractPanel, {
     mainComponentId: '{0}',
     mainComponentText: 'Content',
     mainComponentIcon: '/assets/vps/images/paragraph_page.gif',
-
+    componentEditUrl: '/admin/component/edit',
     initComponent: function() {
         if (this.autoLoad !== false) {
             this.autoLoad = true;
@@ -13,7 +13,7 @@ Vps.Component.ComponentPanel = Ext.extend(Vps.Binding.AbstractPanel, {
         }
         this.contentPanel = new Ext.Panel();
         Ext.apply(this, {
-            tbar        : [],
+            tbar        : new Ext.Toolbar({ height: 25 }),
             items       : this.contentPanel,
             currentItem : this.contentPanel
         });
