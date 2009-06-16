6fa2509b1ff91bac1d550dc2a121b83ca512fea7
Markus Hauser <mh@vivid-planet.com>
2009-06-16 11:23:22 +0000

    TreeSelect form button hinzugefuegt

---
+++ Vps/Form/Field/TreeSelect.php
@@ -0,0 +1,9 @@
+<?php
+class Vps_Form_Field_TreeSelect extends Vps_Form_Field_SimpleAbstract
+{
+    public function __construct($field_name = null, $field_label = null)
+    {
+        parent::__construct($field_name, $field_label);
+        $this->setXtype('treeselect');
+    }
+}
