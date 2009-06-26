ce92593009b92e8de42813d1704fd03f6cffa835
Christoph Eder <ce@vivid-planet.com>
2009-06-26 07:46:04 +0000

    Fix wenn shop-seite inivisible

--- Vpc/Shop/Products/Directory/Generator.php
+++ Vpc/Shop/Products/Directory/Generator.php
@@ -1,10 +1,18 @@
 <?php
 class Vpc_Shop_Products_Directory_Generator extends Vps_Component_Generator_Page_Table
 {
-    protected function _getParentDataByRow($row)
+    protected function _getParentDataByRow($row, $select)
     {
+        $constraints = array();
+        if ($select->hasPart(Vps_Component_Select::WHERE_SUBROOT)) {
+            $constraints['subroot'] = $select->getPart(Vps_Component_Select::WHERE_SUBROOT);
+        }
+        if ($select->hasPart(Vps_Component_Select::IGNORE_VISIBLE)) {
+            $constraints['ignoreVisible'] = $select->getPart(Vps_Component_Select::IGNORE_VISIBLE);
+        }
         return Vps_Component_Data_Root::getInstance()->getComponentsByClass(
-            'Vpc_Shop_Products_Directory_Component'
+            'Vpc_Shop_Products_Directory_Component',
+            $constraints
         );
     }
 }
