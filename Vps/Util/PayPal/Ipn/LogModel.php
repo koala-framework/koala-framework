6b5f56e345e38703b3ff8418f5e881bf09855729
Niko Sams <ns@vivid-planet.com>
2009-07-03 13:26:52 +0000

    ipn kann callback fkt übergeben bekommen

--- Vps/Util/PayPal/Ipn/LogModel.php
+++ Vps/Util/PayPal/Ipn/LogModel.php
@@ -1,6 +1,7 @@
 <?php
 class Vps_Util_PayPal_Ipn_LogModel extends Vps_Model_Db
 {
+    const HASH_CODE = '34sdkfakewrieif';
     protected $_table = 'paypal_ipn_log';
     protected function _init()
     {
@@ -8,5 +9,22 @@ class Vps_Util_PayPal_Ipn_LogModel extends Vps_Model_Db
         $this->_siblingModels[] = new Vps_Model_Field(array(
             'fieldName' => 'data'
         ));
+    }
+
+    /**
+     * Muss in "custom" von der bezahlung gespeichert werden
+     */
+    public static function getEncodedCallback($class, $method, $params = array())
+    {
+        $ret = 'vpsProcessIpnEntry:';
+        $data = array(
+            'class' => $class,
+            'method' => $method,
+            'params' => $params
+        );
+        $data = serialize($data);
+        $ret .= substr(md5($data.self::HASH_CODE), 0, 10);
+        $ret .= $data;
+        return $ret;
     }
 }
