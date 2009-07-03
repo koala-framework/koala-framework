7b1cdc88549f6fdb59cefb69e0f193bc1e273ceb
Niko Sams <ns@vivid-planet.com>
2009-07-03 06:52:11 +0000

    korrekter tabellenname

--- Vpc/Mail/Admin.php
+++ Vpc/Mail/Admin.php
@@ -4,6 +4,6 @@ class Vpc_Mail_Admin extends Vpc_Admin
     public function setup()
     {
         $fields['subject'] = 'VARCHAR(255) NOT NULL';
-        $this->createFormTable('vpc_basic_text', $fields);
+        $this->createFormTable('vpc_mail', $fields);
     }
 }
