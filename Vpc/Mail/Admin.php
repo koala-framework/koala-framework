a579c296f8d48d2d861fac1d8fe39fbef11ae02e
Franz Unger <ufx@vivid-planet.com>
2009-07-03 06:55:47 +0000

    Vpc_Mail bearbeitet jetzt auch Absenderadresse...

--- Vpc/Mail/Admin.php
+++ Vpc/Mail/Admin.php
@@ -4,6 +4,9 @@ class Vpc_Mail_Admin extends Vpc_Admin
     public function setup()
     {
         $fields['subject'] = 'VARCHAR(255) NOT NULL';
+        $fields['from_email'] = 'VARCHAR(255) NOT NULL';
+        $fields['from_name'] = 'VARCHAR(255) NOT NULL';
+        $fields['reply_email'] = 'VARCHAR(255) NOT NULL';
         $this->createFormTable('vpc_mail', $fields);
     }
 }
--- Vpc/Mail/Form.php
+++ Vpc/Mail/Form.php
@@ -8,5 +8,14 @@ class Vpc_Mail_Form extends Vpc_Abstract_Form
         $this->add(new Vps_Form_Field_TextField('subject', trlVps('Subject')))
             ->setAllowBlank(false)
             ->setWidth(300);
+        $this->add(new Vps_Form_Field_TextField('from_email', trlVps('From Address')))
+            ->setAllowBlank(false)
+            ->setVtype('email')
+            ->setWidth(300);
+        $this->add(new Vps_Form_Field_TextField('from_name', trlVps('From Name')))
+            ->setWidth(300);
+        $this->add(new Vps_Form_Field_TextField('reply_email', trlVps('Reply Address')))
+            ->setVtype('email')
+            ->setWidth(300);
     }
 }
