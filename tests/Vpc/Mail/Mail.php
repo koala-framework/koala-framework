9f879d9eff48ae516fa1c46e35eea5b69bf7b93b
Franz Unger <ufx@vivid-planet.com>
2009-07-07 08:55:54 +0000

    Mail-Test war nicht ganz korrekt

--- tests/Vpc/Mail/Mail.php
+++ tests/Vpc/Mail/Mail.php
@@ -4,7 +4,7 @@ class Vpc_Mail_Mail extends Vpc_Mail_Component
     public static function getSettings()
     {
         $ret = parent::getSettings();
-        $ret['generators']['content']['component'] = 'Vpc_Mail_Mail_Component';
+        $ret['generators']['content']['component'] = 'Vpc_Mail_Content_Component';
         $ret['modelname'] = 'Vpc_Mail_MailModel';
         return $ret;
     }
