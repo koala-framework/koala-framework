cef7550efeb19767c784805b64ddc1d7acc6c476
Markus Hauser <mh@vivid-planet.com>
2009-07-07 13:49:57 +0000

    fix wegen row observer bei user loeschung

--- Vps/User/Relation/Model.php
+++ Vps/User/Relation/Model.php
@@ -1,1 +1,17 @@
-<?phpclass Vps_User_Relation_Model extends Vps_Model_Proxy{    public function __construct(array $config = array())    {        if (!isset($config['proxyModel'])) {            $client = new Vps_Srpc_Client(array(                'serverUrl' => Vps_Registry::get('config')->service->usersRelation->url,                'extraParams' => array('applicationId' => Vps_Registry::get('config')->application->id)            ));            $config['proxyModel'] = new Vps_Model_Service(array('client' => $client));        }        parent::__construct($config);    }}
+<?php
+class Vps_User_Relation_Model extends Vps_Model_Proxy
+{
+    protected $_rowClass = 'Vps_User_Relation_Row';
+
+    public function __construct(array $config = array())
+    {
+        if (!isset($config['proxyModel'])) {
+            $client = new Vps_Srpc_Client(array(
+                'serverUrl' => Vps_Registry::get('config')->service->usersRelation->url,
+                'extraParams' => array('applicationId' => Vps_Registry::get('config')->application->id)
+            ));
+            $config['proxyModel'] = new Vps_User_Relation_ServiceModel(array('client' => $client));
+        }
+        parent::__construct($config);
+    }
+}
