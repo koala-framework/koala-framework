7a143df17cf077e0a5dd54b0a6cfabf0654ae43e
Niko Sams <ns@vivid-planet.com>
2009-07-07 08:22:24 +0000

    fix tests

--- tests/Vpc/Advanced/Amazon/Nodes/Test.php
+++ tests/Vpc/Advanced/Amazon/Nodes/Test.php
@@ -21,12 +21,12 @@ class Vpc_Advanced_Amazon_Nodes_Test extends Vps_Test_SeleniumTestCase
         $this->assertContainsText("css=.vpcAdvancedAmazonNodesTestComponent", "Php");
         $this->assertContainsText("css=.vpcAdvancedAmazonNodesTestComponent", "JavaScript");
         $this->clickAndWait('link=Php');
-        $this->clickAndWait('link=PHP 5 / MySQL 5. Studienausgabe');
-        $this->assertContainsText("css=.vpcAdvancedAmazonNodesProductsDirectoryDetail .bookInfos h1", "PHP 5 / MySQL 5. Studienausgabe");
-        $this->assertContainsText("css=.vpcAdvancedAmazonNodesProductsDirectoryDetail .bookInfos h2", "Caroline Kannengiesser, Matthias Kannengiesser");
+        $this->clickAndWait('link=PHP Design Patterns');
+        $this->assertContainsText("css=.vpcAdvancedAmazonNodesProductsDirectoryDetail .bookInfos h1", "PHP Design Patterns");
+        $this->assertContainsText("css=.vpcAdvancedAmazonNodesProductsDirectoryDetail .bookInfos h2", "Stephan Schmidt");
         $href = $this->getAttribute('link='.trlVps('order now at amazon').'@href');
         $this->assertEquals('http://www.amazon.de', substr($href, 0, 20));
-        $this->assertContains('3772369197', $href);
+        $this->assertContains('389721864X', $href);
         $this->assertContains('prosalzburgat-21', $href);
     }
 }
