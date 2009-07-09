e28dfbc42c87f1715833cd5676e933b5eadb8b8d
Niko Sams <ns@vivid-planet.com>
2009-07-09 12:18:40 +0000

    sicherheit im shop

--- Vpc/Shop/Cart/Checkout/Payment/PayPal/Confirm/Component.php
+++ Vpc/Shop/Cart/Checkout/Payment/PayPal/Confirm/Component.php
@@ -4,9 +4,12 @@ class Vpc_Shop_Cart_Checkout_Payment_PayPal_Confirm_Component extends Vpc_Shop_C
     public function processInput($data)
     {
         $order = $this->_getOrder();
-
-        $this->_order->status = 'ordered';
-        $this->_order->date = new Zend_Db_Expr('NOW()');
-        $this->_order->save();
+        if ($order) { //order kann möglicherweise bereits mittels ipn bestätigt worden sein
+            //abfrage manuell machen damit nicht status auf ordered zurückgesetzt wird wenn ipn bereits auf payed gesetzt hat
+            Vps_Registry::get('db')->query("
+                UPDATE vpc_shop_orders SET status='ordered', date=NOW()
+                WHERE id='$order->id' AND status='cart'
+            ");
+        }
     }
 }
