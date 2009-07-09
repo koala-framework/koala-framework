c739e2e64b710d21e0fff8db20a239d0aef1bb02
Niko Sams <ns@vivid-planet.com>
2009-07-09 13:18:49 +0000

    paypal fixed

--- Vpc/Shop/Cart/Checkout/Payment/PayPal/Confirm/Component.php
+++ Vpc/Shop/Cart/Checkout/Payment/PayPal/Confirm/Component.php
@@ -3,8 +3,10 @@ class Vpc_Shop_Cart_Checkout_Payment_PayPal_Confirm_Component extends Vpc_Shop_C
 {
     public function processInput($data)
     {
-        $order = $this->_getOrder();
-        if ($order) { //order kann möglicherweise bereits mittels ipn bestätigt worden sein
+        $data = Vps_Util_PayPal_Ipn_LogModel::decodeCallback($data['custom']);
+        if ($data) {
+            $order = Vps_Model_Abstract::getInstance('Vpc_Shop_Cart_Orders')->getRow($data['data']['orderId']);
+
             //abfrage manuell machen damit nicht status auf ordered zurückgesetzt wird wenn ipn bereits auf payed gesetzt hat
             Vps_Registry::get('db')->query("
                 UPDATE vpc_shop_orders SET status='ordered', date=NOW()
