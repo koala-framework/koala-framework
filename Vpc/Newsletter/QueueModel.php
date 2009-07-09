694d2a5c40b9cd8ade53109d7fb97e817c665ece
Franz Unger <ufx@vivid-planet.com>
2009-07-09 14:58:19 +0000

    Newsletter Updates, ziemlich fertig jetzt

--- Vpc/Newsletter/QueueModel.php
+++ Vpc/Newsletter/QueueModel.php
@@ -2,4 +2,10 @@
 class Vpc_Newsletter_QueueModel extends Vps_Model_Db_Proxy
 {
     protected $_table = 'vpc_newsletter_queue';
+    protected $_referenceMap = array(
+        'Newsletter' => array(
+            'column' => 'newsletter_id',
+            'refModelClass' => 'Vpc_Newsletter_Model'
+        )
+    );
 }
