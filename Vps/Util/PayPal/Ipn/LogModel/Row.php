113b7de2c4a0efce8845271c77dbbde947407b6d
Niko Sams <ns@vivid-planet.com>
2009-07-10 07:21:26 +0000

    bugfix, mitloggen ob callback erfolgreich ausgefuert wurde

--- Vps/Util/PayPal/Ipn/LogModel/Row.php
+++ Vps/Util/PayPal/Ipn/LogModel/Row.php
@@ -6,15 +6,18 @@ class Vps_Util_PayPal_Ipn_LogModel_Row extends Vps_Model_Db_Row
         parent::_afterInsert();
         $c = Vps_Util_PayPal_Ipn_LogModel::decodeCallback($this->custom);
         if ($c && $c['cb']) {
+            $ret = false;
             if (class_exists($c['cb'])) {
-                call_user_func(array($c['cb'], 'processIpn'), $this, $c['data']);
+                $ret = call_user_func(array($c['cb'], 'processIpn'), $this, $c['data']);
             } else if (Vps_Component_Data_Root::getComponentClass()) {
                 $component = Vps_Component_Data_Root::getInstance()
                                                 ->getComponentById($c['cb']);
                 if ($component) {
-                    $component->getComponent()->processIpn($this, $c['data']);
+                    $ret = $component->getComponent()->processIpn($this, $c['data']);
                 }
             }
+            $this->callback_success = $ret;
+            $this->save();
         }
     }
 }
