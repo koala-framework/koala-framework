2d93e469687b30af129a1fef199a7a18517bf26a
Niko Sams <ns@vivid-planet.com>
2009-07-10 07:10:06 +0000

    bugfix

--- Vps/Util/PayPal/Ipn/LogModel/Row.php
+++ Vps/Util/PayPal/Ipn/LogModel/Row.php
@@ -9,9 +9,10 @@ class Vps_Util_PayPal_Ipn_LogModel_Row extends Vps_Model_Db_Row
             if (class_exists($c['cb'])) {
                 call_user_func(array($c['cb'], 'processIpn'), $this, $c['data']);
             } else if (Vps_Component_Data_Root::getComponentClass()) {
-                $c = Vps_Component_Data_Root::getInstance()->getComponentById($c['cb']);
-                if ($c) {
-                    $c->getComponent()->processIpn($this, $c['data']);
+                $component = Vps_Component_Data_Root::getInstance()
+                                                ->getComponentById($c['cb']);
+                if ($component) {
+                    $component->getComponent()->processIpn($this, $c['data']);
                 }
             }
         }
