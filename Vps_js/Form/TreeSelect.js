ff713d544d62bc0019ea30d6f1bd4a826e674573
Markus Hauser <mh@vivid-planet.com>
2009-07-14 14:48:15 +0000

    TreeSelect field kann eine Reference gesetzt werden, damit der wert direkt nach dem laden stimmt. Bisher musste hierfuer ein neues Data gesetzt werden.

--- Vps_js/Form/TreeSelect.js
+++ Vps_js/Form/TreeSelect.js
@@ -38,9 +38,24 @@ Vps.Form.TreeSelect = Ext.extend(Ext.form.TriggerField,
                     controllerUrl: this.controllerUrl,
                     listeners: {
                         click: function(node) {
+                            if (!this.displayField && node) {
+                                var nodeTexts = [];
+                                nodeTexts.push(node.text);
+                                var nd = node.parentNode;
+                                while (nd) {
+                                    nodeTexts.push(nd.text);
+                                    nd = nd.parentNode;
+                                }
+                                var nodeText = '';
+                                for (var i = 0; i < nodeTexts.length - (typeof this.cutNodes != 'undefined' ? this.cutNodes : 1); i++) {
+                                    nodeText = nodeTexts[i] + ' » ' + nodeText;
+                                }
+                                if (nodeText) nodeText = nodeText.substr(0, nodeText.length-3);
+                            }
+
                             this.selectWin.value = {
                                 id: node.id,
-                                name: this.displayField ? node.attributes.data[this.displayField] : node.text
+                                name: this.displayField ? node.attributes.data[this.displayField] : nodeText
                             };
                         },
                         scope: this
