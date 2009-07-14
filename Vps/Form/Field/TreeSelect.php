a135334b1aa4c421d2ca0b9ed3d5d33e48248059
Markus Hauser <mh@vivid-planet.com>
2009-07-14 14:48:15 +0000

    TreeSelect field kann eine Reference gesetzt werden, damit der wert direkt nach dem laden stimmt. Bisher musste hierfuer ein neues Data gesetzt werden.

--- Vps/Form/Field/TreeSelect.php
+++ Vps/Form/Field/TreeSelect.php
@@ -7,10 +7,15 @@ class Vps_Form_Field_TreeSelect extends Vps_Form_Field_SimpleAbstract
         $this->setXtype('treeselect');
     }
 
-    // setReference('Category') muss gesetzt werden, damit der inhalt beim
+    // -> setReference('Category') muss gesetzt werden, damit der inhalt beim
     // laden stimmt.
-    // setDisplayField setzt ein Feld, das beim Laden und auch nach dem
+    // -> setDisplayField setzt ein Feld, das beim Laden und auch nach dem
     // Auswählen eines Eintrags verwendet wird. (standard ist der .js nodeText)
+
+    // -> setCutNodes(2) kann verwendet werden, um die ersten X ebenen im Text
+    // abzuschneiden (nachdem ein wert gewählt wurde). Standard ist das auf 1,
+    // was im normalfall den Root ausblendet.
+    // Muss auch im model korrekt implementiert sein
 
     // wenn man zu faul ist das model zu erben damit man die korrekte reference
     // einstellt, kann man auch ein Data erstellen, dass den wert nach dem laden
@@ -25,21 +30,28 @@ class Vps_Form_Field_TreeSelect extends Vps_Form_Field_SimpleAbstract
             $id = $ret[$this->getFieldName()];
             $parentRow = $row->getParentRow($ref);
 
-            $displayField = $this->getDisplayField();
-            if ($displayField) {
-                $ret[$this->getFieldName()] = array(
-                    'id' => $id,
-                    'name' => $parentRow->{$displayField}
-                );
-            } else if ($parentRow instanceof Vps_Model_Tree_Row_Interface) {
-                $ret[$this->getFieldName()] = array(
-                    'id' => $id,
-                    'name' => $parentRow->getTreePath()
-                );
+            if ($parentRow) {
+                $displayField = $this->getDisplayField();
+                if ($displayField) {
+                    $ret[$this->getFieldName()] = array(
+                        'id' => $id,
+                        'name' => $parentRow->{$displayField}
+                    );
+                } else if ($parentRow instanceof Vps_Model_Tree_Row_Interface) {
+                    $ret[$this->getFieldName()] = array(
+                        'id' => $id,
+                        'name' => $parentRow->getTreePath()
+                    );
+                } else {
+                    $ret[$this->getFieldName()] = array(
+                        'id' => $id,
+                        'name' => $parentRow->__toString()
+                    );
+                }
             } else {
                 $ret[$this->getFieldName()] = array(
                     'id' => $id,
-                    'name' => $parentRow->__toString()
+                    'name' => ''
                 );
             }
         }
