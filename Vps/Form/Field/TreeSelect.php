8d5cf80eccd2451ebfde093d3bfb5d6b5ded52ba
Markus Hauser <mh@vivid-planet.com>
2009-07-14 14:08:30 +0000

    TreeSelect field kann eine Reference gesetzt werden, damit der wert direkt nach dem laden stimmt. Bisher musste hierfuer ein neues Data gesetzt werden.

--- Vps/Form/Field/TreeSelect.php
+++ Vps/Form/Field/TreeSelect.php
@@ -6,4 +6,44 @@ class Vps_Form_Field_TreeSelect extends Vps_Form_Field_SimpleAbstract
         parent::__construct($field_name, $field_label);
         $this->setXtype('treeselect');
     }
+
+    // setReference('Category') muss gesetzt werden, damit der inhalt beim
+    // laden stimmt.
+    // setDisplayField setzt ein Feld, das beim Laden und auch nach dem
+    // Auswählen eines Eintrags verwendet wird. (standard ist der .js nodeText)
+
+    // wenn man zu faul ist das model zu erben damit man die korrekte reference
+    // einstellt, kann man auch ein Data erstellen, dass den wert nach dem laden
+    // korrekt darstellt
+
+    public function load($row, $postData = array())
+    {
+        $ret = parent::load($row, $postData);
+
+        $ref = $this->getReference();
+        if ($ref && $row && !empty($ret[$this->getFieldName()])) {
+            $id = $ret[$this->getFieldName()];
+            $parentRow = $row->getParentRow($ref);
+
+            $displayField = $this->getDisplayField();
+            if ($displayField) {
+                $ret[$this->getFieldName()] = array(
+                    'id' => $id,
+                    'name' => $parentRow->{$displayField}
+                );
+            } else if ($parentRow instanceof Vps_Model_Tree_Row_Interface) {
+                $ret[$this->getFieldName()] = array(
+                    'id' => $id,
+                    'name' => $parentRow->getTreePath()
+                );
+            } else {
+                $ret[$this->getFieldName()] = array(
+                    'id' => $id,
+                    'name' => $parentRow->__toString()
+                );
+            }
+        }
+
+        return $ret;
+    }
 }
