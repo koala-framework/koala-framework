58946950e9c7626a41c36d893e27546b5e11a8e4
Niko Sams <ns@vivid-planet.com>
2009-07-15 06:39:56 +0000

    inherits eine statische eigenschaft von Generators geamcht. Wird in der praxis nie dynamisch geändert

--- Vps/Component/Generator/Page/Static.php
+++ Vps/Component/Generator/Page/Static.php
@@ -3,12 +3,12 @@ class Vps_Component_Generator_Page_Static extends Vps_Component_Generator_Pseudo
     implements Vps_Component_Generator_Page_Interface, Vps_Component_Generator_PseudoPage_Interface
 {
     protected $_idSeparator = '_';
+    protected $_inherits = true;
 
     protected function _formatConfig($parentData, $componentKey)
     {
         $data = parent::_formatConfig($parentData, $componentKey);
         $data['isPage'] = true;
-        $data['inherits'] = true;
 
         $c = $this->_settings;
         $data['name'] = isset($c['name']) ? $c['name'] : $componentKey;
