59106216e7446b07c7c79eb2f9ccc116199128a7
Niko Sams <ns@vivid-planet.com>
2009-07-20 11:30:59 +0000

    neue confirm komponente verwenden

---
+++ Vpc/Shop/Cart/Checkout/Payment/Abstract/Confirm/Paragraphs/Component.php
@@ -0,0 +1,9 @@
+<?php
+class Vpc_Shop_Cart_Checkout_Payment_Abstract_Confirm_Paragraphs_Component extends Vpc_Paragraphs_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        return $ret;
+    }
+}
--- Vpc/Shop/Cart/Checkout/Payment/CashOnDelivery/Component.php
+++ Vpc/Shop/Cart/Checkout/Payment/CashOnDelivery/Component.php
@@ -6,6 +6,7 @@ class Vpc_Shop_Cart_Checkout_Payment_CashOnDelivery_Component extends Vpc_Shop_C
         $ret = parent::getSettings();
         $ret['componentName'] = trlVps('Cash on Delivery');
         $ret['generators']['mail']['component'] = 'Vpc_Shop_Cart_Checkout_Payment_CashOnDelivery_Mail_Component';
+        $ret['generators']['confirm']['component'] = 'Vpc_Shop_Cart_Checkout_Payment_CashOnDelivery_Confirm_Component';
         $ret['cashOnDeliveryCharge'] = 6.5;
         return $ret;
     }
