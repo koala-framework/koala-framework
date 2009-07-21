aee136c6a6e6ce6ab10e9b26d49b92103a1233dc
Franz Unger <ufx@vivid-planet.com>
2009-07-21 05:44:56 +0000

    ExecutionTime für Plugins inkl. Tests

---
+++ tests/Vps/Component/Output/Plugin/Component.php
@@ -0,0 +1,15 @@
+<?php
+class Vps_Component_Output_Plugin_Component extends Vpc_Abstract_Composite_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['generators']['child']['component'] = array(
+            'child' => 'Vps_Component_Output_C1_Child_Component'
+        );
+        $ret['plugins'] = array('Vps_Component_Output_PluginAfter');
+        
+        return $ret;
+    }
+}
+?>
