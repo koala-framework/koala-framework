2b15caf5efebc8b31be5a16f72b023c4774044ea
Markus Hauser <mh@vivid-planet.com>
2009-07-24 11:55:18 +0000

    dimesions angepasst, altes format funzt da nimmer

--- Vpc/Composite/LinkImage/Image/Component.php
+++ Vpc/Composite/LinkImage/Image/Component.php
@@ -4,7 +4,13 @@ class Vpc_Composite_LinkImage_Image_Component extends Vpc_Basic_Image_Component
     public static function getSettings()
     {
         $ret = parent::getSettings();
-        $ret['dimensions'] = array(150, 0, Vps_Media_Image::SCALE_BESTFIT);
+        $ret['dimensions'] = array(
+            'default' => array(
+                'width' => 150,
+                'height' => 0,
+                'scale' => Vps_Media_Image::SCALE_BESTFIT
+            )
+        );
         return $ret;
     }
 }
