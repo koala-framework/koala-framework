a545f2aedef379c6fa6036ec9f279ca73b0a7e00
Franz Unger <ufx@vivid-planet.com>
2009-07-29 08:58:19 +0000

    Images als PDF: man kann Bilder jetzt in mehreren Spalten ausgeben

--- Vpc/Abstract/Image/Pdf.php
+++ Vpc/Abstract/Image/Pdf.php
@@ -1,22 +1,81 @@
 <?php
 class Vpc_Abstract_Image_Pdf extends Vpc_Abstract_Pdf
 {
-    public function writeContent()
+    private $_size;
+
+    public function writeContent($setCoordinates = true)
     {
-        $maxWidth = Vpc_Abstract::getSetting(get_class($this->_component), 'pdfMaxWidth');
         $image = $this->_component->getImageRow();
         $file = $image->getParentRow('Image');
-        $dimension = $this->_component->getImageDimensions();
-        $area = $this->getMaxTextWidth();
 
         if ($file && is_file($file->getFileSource())) {
             $source = $file->getFileSource();
+            $size = $this->getSize();
+
+            $imageSize = array(
+                'scale' => Vps_Media_Image::SCALE_DEFORM,
+                'width' => $this->_calculateDpi($size['width']),
+                'height' => $this->_calculateDpi($size['height'])
+            );
+            $content = Vps_Media_Image::scale($source, $imageSize);
+            $filter = new Vps_Filter_Ascii();
+            $tempFilename = 'temp_'.$filter->filter($file->filename);
+            $handle = fopen($tempFilename, 'wb');
+            fwrite($handle, $content);
+            fclose($handle);
+            $data = getimagesize($tempFilename);
+            if ($data[2] == 2) { // nur jpgs ausgeben
+                if ($setCoordinates && $this->addPageIfNecessary()) {
+                    $x = $this->getX();
+                    $this->SetY($this->getTopMargin());
+                    $this->SetX($x);
+                }
+                $this->_pdf->Image(
+                    $tempFilename, $this->getX(), $this->getY(),
+                    $size['width'], $size['height'], $file->extension
+                );
+                if ($setCoordinates) {
+                    $this->SetY($this->getY() + $size['height'] + 2);
+                }
+            }
+            unlink($tempFilename);
+            return true;
+        }
+        return false;
+    }
+
+    public function addPageIfNecessary()
+    {
+        $size = $this->getSize();
+        $pageExclBorders = $this->getPageHeight() - $this->getBottomMargin();
+        if (($this->getY() + $size['height']) > $pageExclBorders) {
+            if ($this->PageNo() == $this->getNumPages()) {
+                $this->AddPage();
+            } else {
+                $this->SetPage($this->PageNo() + 1);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public function getSize()
+    {
+        if (!$this->_size) {
+            $file = $this->_component->getImageRow()->getParentRow('Image');
+            if (!$file || !is_file($file->getFileSource())) {
+                return null;
+            }
+
+            $dimension = $this->_component->getImageDimensions();
             if (!$dimension){
                 $dimensions = getimagesize($file->getFileSource());
                 $dimension["width"] = $dimensions[0];
                 $dimension["height"] = $dimensions[1];
             }
 
+            $maxWidth = Vpc_Abstract::getSetting(get_class($this->_component), 'pdfMaxWidth');
+            $area = $this->getMaxTextWidth();
             if ($maxWidth > $area || $maxWidth == 0) {
                 $height = $dimension["height"] / $dimension["width"] * $area;
                 $width = $area;
@@ -25,36 +84,9 @@ class Vpc_Abstract_Image_Pdf extends Vpc_Abstract_Pdf
                 $width = $maxWidth;
             }
 
-            $size = array();
-            $size['scale'] = Vps_Media_Image::SCALE_DEFORM;
-            $size['width'] = $this->_calculateDpi($width);
-            $size['height'] = $this->_calculateDpi($height);
-
-            $content = Vps_Media_Image::scale($source, $size);
-            $filter = new Vps_Filter_Ascii();
-            $tempFilename = 'temp_'.$filter->filter($file->filename);
-            $handle = fopen($tempFilename, 'wb');
-            fwrite($handle, $content);
-            fclose($handle);
-            $data = getimagesize($tempFilename);
-            if ($data[2] == 2) { // nur jpgs ausgeben
-                $pageExclBorders = $this->getPageHeight()
-                                    - $this->getBottomMargin();
-
-                if (($this->getY() + $height) > ($pageExclBorders)) {
-                    if ($this->PageNo() == $this->getNumPages()) {
-                        $this->AddPage();
-                    } else {
-                        $this->SetPage($this->PageNo() + 1);
-                    }
-                    $this->SetY($this->getTopMargin());
-                }
-                $this->_pdf->Image($tempFilename, $this->getLeftMargin(),
-                                        $this->getY(), $width, $height, $file->extension);
-                $this->SetY($this->getY() + $height + 2);
-            }
-            unlink($tempFilename);
+            $this->_size = array('width' => $width, 'height' => $height);
         }
+        return $this->_size;
     }
 
     private function _calculateDpi ($mm)
