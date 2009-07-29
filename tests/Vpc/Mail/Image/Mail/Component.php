1e06ce26a88ecbe692f22c76000dafc1638b1e6b
Franz Unger <ufx@vivid-planet.com>
2009-07-29 10:49:10 +0000

    Vpc_Mail Tests gefixt

---
+++ tests/Vpc/Mail/Image/Mail/Component.php
@@ -0,0 +1,16 @@
+<?php
+class Vpc_Mail_Image_Mail_Component extends Vpc_Mail_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['generators']['content']['component'] = 'Vpc_Mail_Image_Content_Component';
+        $ret['modelname'] = 'Vpc_Mail_Image_Mail_Model';
+        return $ret;
+    }
+
+    public function getImages()
+    {
+        return $this->_images;
+    }
+}
