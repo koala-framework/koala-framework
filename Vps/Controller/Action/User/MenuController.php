e2e48b419c8545604775cbed20343a6be8330894
Markus Hauser <mh@vivid-planet.com>
2009-07-29 08:26:06 +0000

    entscheidung ob web ein frontend hat oder nicht liegt jetzt an der acl-ressource des seitenbaums

--- Vps/Controller/Action/User/MenuController.php
+++ Vps/Controller/Action/User/MenuController.php
@@ -35,7 +35,8 @@ class Vps_Controller_Action_User_MenuController extends Vps_Controller_Action
 
         $role = Zend_Registry::get('userModel')->getAuthedChangedUserRole();
         $this->view->changeUser = $acl->isAllowed($role, 'vps_user_changeuser', 'view');
-        if (Vps_Registry::get('config')->vpc->rootComponent) {
+
+        if (Vps_Registry::get('acl')->has('vps_component_pages')) {
             $this->view->hasFrontend = true;
         } else {
             $this->view->hasFrontend = false;
