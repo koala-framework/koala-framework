ded4ef6e86622809490e0e48f27cb32c5ead7cdb
Franz Unger <ufx@vivid-planet.com>
2009-07-29 08:58:19 +0000

    Images als PDF: man kann Bilder jetzt in mehreren Spalten ausgeben

--- Vpc/Composite/Images/Component.php
+++ Vpc/Composite/Images/Component.php
@@ -7,6 +7,7 @@ class Vpc_Composite_Images_Component extends Vpc_Abstract_List_Component
         $ret['componentName'] = trlVps('Images');
         $ret['componentIcon'] = new Vps_Asset('pictures');
         $ret['generators']['child']['component'] = 'Vpc_Basic_Image_Component';
+        $ret['pdfColumns'] = 1;
         return $ret;
     }
 }
