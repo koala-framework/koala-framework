bc21ba20f60611db4c46b2f7ceb77290219d2fae
Markus Hauser <mh@vivid-planet.com>
2009-07-31 11:40:02 +0000

    Numberfield wird getestet und wurde gefixt wenn zahl = 0

---
+++ tests/Vps/Form/NumberField/TestModel.php
@@ -0,0 +1,8 @@
+<?php
+class Vps_Form_NumberField_TestModel extends Vps_Model_FnF
+{
+    public $_data = array(
+        array('id' => 1, 'nr' => 5)
+    );
+    protected $_toStringField = 'nr';
+}
