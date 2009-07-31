176c278dea0624c568ca8950e33f43cb987755fd
Niko Sams <ns@vivid-planet.com>
2009-07-31 13:50:00 +0000

    kein exception wenn nix da

--- Vpc/Shop/Cart/Checkout/Payment/Abstract/Confirm/Component.php
+++ Vpc/Shop/Cart/Checkout/Payment/Abstract/Confirm/Component.php
@@ -17,19 +17,23 @@ class Vpc_Shop_Cart_Checkout_Payment_Abstract_Confirm_Component extends Vpc_Edit
     {
         $ret = Vps_Model_Abstract::getInstance('Vpc_Shop_Cart_Orders')->getCartOrder();
         if (!$ret || !$ret->data) {
-            throw new Vps_Exception_AccessDenied("No Order exists");
+            return null;
         }
         return $ret;
     }
 
     public function processInput($data)
     {
-        $this->getData()->parent->getComponent()->confirmOrder($this->_getOrder());
+        $o = $this->_getOrder();
+        if ($o) {
+            $this->getData()->parent->getComponent()->confirmOrder($o);
+        }
     }
 
     public function getPlaceholders()
     {
         $o = $this->_getOrder();
+        if (!$o) return array();
         $ret = $o->toArray();
         $m = new Vps_View_Helper_Money();
         $ret['total'] = $m->money($o->getTotal());
