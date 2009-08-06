f92c3b328d4eda5a1f9c23d71a8284657c55a353
Niko Sams <ns@vivid-planet.com>
2009-08-06 06:26:07 +0000

    Bei allen caches mit backend "File" die option für dateiberechtigungen korrekt setzen, damit bugs wenn http einen cache erstellt und cli ihn ned lesen kann behoben sind.
    Test dafür mit MirrorCache.

---
+++ tests/Vps/Model/MirrorCache/DelayWithHttpAndCliTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Testet ob cli und http zugriffe eh auch nur alle x minuten syncen
+ * es gab ein problem mit berechtigungen
+ * @group Model_MirrorCache
+ * @group Model_MirrorCache_Delay
+ * @group slow
+ */
+class Vps_Model_MirrorCache_DelayWithHttpAndCliTest extends PHPUnit_Framework_TestCase
+{
+    public function testSyncWithCli()
+    {
+        Vps_Benchmark::enable();
+        Vps_Benchmark::reset();
+
+        Vps_Model_MirrorCache_TestController::setup();
+        Vps_Model_MirrorCache_TestController::$proxyModel->synchronize(Vps_Model_MirrorCache::SYNC_ONCE);
+        $this->assertEquals(1, (int)Vps_Benchmark::getCounterValue('mirror sync'));
+
+        $url = 'http://'.Vps_Registry::get('testDomain').'/vps/test/vps_model_mirror-cache_test';
+        $this->assertEquals(0, file_get_contents($url));
+
+        sleep(6); //sync delay ist 5 sec
+        $this->assertEquals(1, file_get_contents($url));
+        $this->assertEquals(0, file_get_contents($url));
+
+        Vps_Benchmark::reset();
+        Vps_Model_MirrorCache_TestController::$proxyModel->countRows();
+        $this->assertEquals(0, (int)Vps_Benchmark::getCounterValue('mirror sync'));
+
+        sleep(6); //sync delay ist 5 sec
+        Vps_Benchmark::reset();
+        Vps_Model_MirrorCache_TestController::$proxyModel->synchronize(Vps_Model_MirrorCache::SYNC_ALWAYS);
+        $this->assertEquals(1, (int)Vps_Benchmark::getCounterValue('mirror sync'));
+        $this->assertEquals(0, file_get_contents($url));
+    }
+}
