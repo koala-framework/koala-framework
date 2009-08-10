81c91b41e07a402dd4f1b1697eb8111cec5e8c94
Niko Sams <ns@vivid-planet.com>
2009-08-10 15:22:43 +0000

    interface erweitert

--- Vps/Http/Requestor/Response/Interface.php
+++ Vps/Http/Requestor/Response/Interface.php
@@ -4,4 +4,6 @@ interface Vps_Http_Requestor_Response_Interface
     public function getBody();
     public function getStatusCode();
     public function getContentType();
+    public function getHeader($h);
+    public function __toString();
 }
