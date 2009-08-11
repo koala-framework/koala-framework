6a0ffde1610e9cb5f064c34dd63307835bb20faf
Niko Sams <ns@vivid-planet.com>
2009-08-11 08:10:36 +0000

    umask fuer cache datei

--- Vps/Config/Cache.php
+++ Vps/Config/Cache.php
@@ -17,7 +17,9 @@ class Vps_Config_Cache extends Zend_Cache_Frontend_File {
         
         require_once 'Zend/Cache/Backend/File.php';
         $backend = new Zend_Cache_Backend_File(array(
-            'cache_dir' => 'application/cache/config'
+            'cache_dir' => 'application/cache/config',
+            'hashed_directory_umask' => 0777,
+            'cache_file_umask' => 0666,
         ));
         
         $this->setBackend($backend);
