1567ed4fbcec3bf55c97d6b8c5785dc08f098f89
Niko Sams <ns@vivid-planet.com>
2009-08-12 10:25:14 +0000

    fix progress

---
+++ Vps/Test/TestRunner.php
@@ -0,0 +1,8 @@
+<?php
+class Vps_Test_TestRunner extends PHPUnit_TextUI_TestRunner
+{
+    public function handleConfiguration(array &$arguments)
+    {
+        parent::handleConfiguration($arguments);
+    }
+}
