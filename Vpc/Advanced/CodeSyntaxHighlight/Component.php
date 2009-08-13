4b562af20327a46733f4024499da77c32328a850
Niko Sams <ns@vivid-planet.com>
2009-08-13 12:16:05 +0000

    syntax highligted code komponente

---
+++ Vpc/Advanced/CodeSyntaxHighlight/Component.php
@@ -0,0 +1,33 @@
+<?php
+class Vpc_Advanced_CodeSyntaxHighlight_Component extends Vpc_Abstract
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['componentName'] = trlVps('Code Syntax Highlighted');
+        $ret['componentIcon'] = new Vps_Asset('page_code');
+        $ret['modelname'] = 'Vps_Component_FieldModel';
+        return $ret;
+    }
+
+    public function getTemplateVars()
+    {
+        $ret = parent::getTemplateVars();
+        require_once 'geshi.php';
+
+        $row = $this->getRow();
+        if ($row->language) {
+            $geshi = new GeSHi($row->code, $row->language);
+            if ($row->line_numbers) {
+                $geshi->enable_line_numbers(GESHI_NORMAL_LINE_NUMBERS);
+            } else {
+                $geshi->enable_line_numbers(GESHI_NO_LINE_NUMBERS);
+            }
+            $ret['html'] = $geshi->parse_code();
+        } else {
+            $ret['html'] = '<code>'.htmlspecialchars($row->code).'</code>';
+        }
+        return $ret;
+    }
+
+}
---
+++ Vpc/Advanced/CodeSyntaxHighlight/Form.php
@@ -0,0 +1,29 @@
+<?php
+class Vpc_Advanced_CodeSyntaxHighlight_Form extends Vpc_Abstract_Form
+{
+    protected function _initFields()
+    {
+        parent::_initFields();
+        $this->add(new Vps_Form_Field_TextArea('code'))
+            ->setFieldLabel(trlVps('Code'))
+            ->setHeight(225)
+            ->setWidth(450);
+
+        $values = array();
+        require_once 'geshi.php';
+        $geshi = new GeSHi();
+        foreach (new DirectoryIterator($geshi->language_path) as $i) {
+            if ($i->isDir()) continue;
+            if (substr($i, -4) != '.php') continue;
+            $i = substr($i->__toString(), 0, -4);
+            $values[$i] = $i;
+        }
+        $this->add(new Vps_Form_Field_Select('language'))
+            ->setFieldLabel(trlVps('Language'))
+            ->setValues($values);
+
+
+        $this->add(new Vps_Form_Field_Checkbox('line_numbers'))
+            ->setFieldLabel(trlVps('Line Numbers'));
+    }
+}
