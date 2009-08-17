d783f333b021977b5252d6c6adee06ff79679ddf
Niko Sams <ns@vivid-planet.com>
2009-08-17 13:33:07 +0000

    fix toArray fÃ¼r siblingModels in proxy model

--- Vps/Model/Proxy/Row.php
+++ Vps/Model/Proxy/Row.php
@@ -81,6 +81,10 @@ class Vps_Model_Proxy_Row extends Vps_Model_Row_Abstract
 
     public function toArray()
     {
-        return $this->_row->toArray();
+        $ret = $this->_row->toArray();
+        foreach ($this->_getSiblingRows() as $row) {
+            $ret = array_merge($row->toArray(), $ret);
+        }
+        return $ret;
     }
 }
