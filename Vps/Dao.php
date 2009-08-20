6e7d635c11cd069752519c407e6f5468e2fef278
Niko Sams <ns@vivid-planet.com>
2009-08-20 06:24:18 +0000

    persistente db-verbindung aufbauen

--- Vps/Dao.php
+++ Vps/Dao.php
@@ -38,6 +38,7 @@ class Vps_Dao
         if (!isset($dbConfig['username']) && isset($dbConfig['user'])) $dbConfig['username'] = $dbConfig['user'];
         if (!isset($dbConfig['password']) && isset($dbConfig['pass'])) $dbConfig['password'] = $dbConfig['pass'];
         if (!isset($dbConfig['dbname']) && isset($dbConfig['name'])) $dbConfig['dbname'] = $dbConfig['name'];
+        $dbConfig['persistent'] = true;
         return $dbConfig;
     }
 
