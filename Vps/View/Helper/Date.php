4dd3f231ffc28ced1bf092f7834d6e70066ac0a0
Franz Unger <ufx@vivid-planet.com>
2009-08-25 07:42:45 +0000

    Date jetzt nicht mehr per Zend_Date, sondern mit PHP-eigener Klasse DateTime berechnen

--- Vps/View/Helper/Date.php
+++ Vps/View/Helper/Date.php
@@ -4,7 +4,7 @@ class Vps_View_Helper_Date
     public function date($date)
     {
         if (!$date) return '';
-        $date = new Zend_Date($date);
-        return $date->toString(trlVps('Y-m-d'));
+        $datetime = new DateTime($date);
+        return $datetime->format(trlVps('Y-m-d'));
     }
 }
