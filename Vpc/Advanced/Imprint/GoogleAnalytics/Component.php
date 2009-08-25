f37d68a8b65b4721e2cbd0d28ed85703c6d897f2
Niko Sams <ns@vivid-planet.com>
2009-08-25 07:19:55 +0000

    nicht von composite erben und nicht unnötig text-komponenten erstellen

--- Vpc/Advanced/Imprint/GoogleAnalytics/Component.php
+++ Vpc/Advanced/Imprint/GoogleAnalytics/Component.php
@@ -1,14 +1,12 @@
 <?php
-class Vpc_Advanced_Imprint_GoogleAnalytics_Component extends Vpc_Abstract_Composite_Component
+class Vpc_Advanced_Imprint_GoogleAnalytics_Component extends Vpc_Abstract
 {
     public static function getSettings()
     {
         $ret = array_merge(parent::getSettings(), array(
             'componentName' => trlVps('Imprint.Google-Analytics'),
-            'modelname' => 'Vps_Component_FieldModel',
             'cssClass' => 'webStandard'
         ));
-        $ret['generators']['child']['component']['text'] = 'Vpc_Basic_Text_Component';
         return $ret;
     }
 }
