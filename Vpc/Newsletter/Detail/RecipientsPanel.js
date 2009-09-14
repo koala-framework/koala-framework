50e2d1d233bcba9004f5002cd1b8ba615a6a2ab4
Markus Hauser <mh@vivid-planet.com>
2009-09-14 13:34:01 +0000

    newsletter fix wenns bei recipients kein edit dialog gibt

--- Vpc/Newsletter/Detail/RecipientsPanel.js
+++ Vpc/Newsletter/Detail/RecipientsPanel.js
@@ -3,11 +3,13 @@ Vpc.Newsletter.Detail.RecipientsPanel = Ext.extend(Vps.Auto.GridPanel, {
 
     initComponent: function() {
         this.actions.saveRecipients = new Vpc.Newsletter.Detail.RecipientsAction({scope: this});
-        this.editDialog = {
-            controllerUrl: this.formControllerUrl,
-            width: 500,
-            height: 450
-        };
+        if (this.formControllerUrl) {
+            this.editDialog = {
+                controllerUrl: this.formControllerUrl,
+                width: 500,
+                height: 450
+            };
+        }
         Vpc.Newsletter.Detail.RecipientsPanel.superclass.initComponent.call(this);
     }
 });
