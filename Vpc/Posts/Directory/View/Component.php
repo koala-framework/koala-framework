a68538aea783a1a9c95ac44d54c02b11610ef7de
Markus Hauser <mh@vivid-planet.com>
2009-09-15 15:38:20 +0000

    partial id gefixt und bei posts wieder eingeschaltet.

--- Vpc/Posts/Directory/View/Component.php
+++ Vpc/Posts/Directory/View/Component.php
@@ -1,8 +1,8 @@
 <?php
 class Vpc_Posts_Directory_View_Component extends Vpc_Directories_List_View_Component
-{/*
+{
     public function getPartialClass()
     {
         return 'Vps_Component_Partial_Id';
-    }*/
+    }
 }
