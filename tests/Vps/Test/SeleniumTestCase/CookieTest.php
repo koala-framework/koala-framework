8ec6499148f8ee06d2faef43e7630cd28cfd9b50
Niko Sams <ns@vivid-planet.com>
2009-09-17 10:17:46 +0000

    cookie test

---
+++ tests/Vps/Test/SeleniumTestCase/CookieTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @group 
+ */
+class Vps_Test_SeleniumTestCase_CookieTest extends Vps_Test_SeleniumTestCase
+{
+    public function testCookie()
+    {
+        $this->open('/');
+        $this->assertEquals($this->_unitTestCookie, $this->getCookieByName('unitTest'));
+    }
+}
