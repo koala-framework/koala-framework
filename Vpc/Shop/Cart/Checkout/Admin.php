484f51fede528cff8f16fa4ffcdf3e51748f817b
Niko Sams <ns@vivid-planet.com>
2009-09-24 12:23:52 +0000

    fehler korrigiert damit vpc_shop genau einmal da ist

--- Vpc/Shop/Cart/Checkout/Admin.php
+++ Vpc/Shop/Cart/Checkout/Admin.php
@@ -9,8 +9,10 @@ class Vpc_Shop_Cart_Checkout_Admin extends Vpc_Admin
     public function addResources(Vps_Acl $acl)
     {
         parent::addResources($acl);
-        $acl->add(new Vps_Acl_Resource_MenuDropdown('vpc_shop',
+        if (!$acl->has('vpc_shop')) {
+            $acl->add(new Vps_Acl_Resource_MenuDropdown('vpc_shop',
                     array('text'=>trlVps('Shop'), 'icon'=>'cart.png')), 'vps_component_root');
+        }
         $components = Vps_Component_Data_Root::getInstance()
                 ->getComponentsBySameClass($this->_class, array('ignoreVisible'=>true));
         $icon = Vpc_Abstract::getSetting($this->_class, 'componentIcon');
