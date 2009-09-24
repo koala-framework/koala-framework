2053082cd7a03820490126ace0cf51a0a9e1d417
Christoph Eder <ce@vivid-planet.com>
2009-09-24 13:32:28 +0000

    update

--- Vpc/Newsletter/Update/30401.sql
+++ Vpc/Newsletter/Update/30401.sql
@@ -1,1 +1,3 @@
-UPDATE `vpc_newsletter_queue` SET `recipient_model` = 'Vpc_Newsletter_Subscribe_Model';
+UPDATE `vpc_newsletter_queue` SET `recipient_model` = 'Vpc_Newsletter_Subscribe_Model'
+	WHERE recipient_model='Vpc_Berlot_Newsletter_Subscribe_Model';
+	
