ffaf0ee4a34b9314f0426622556942e557dc1a67
Niko Sams <ns@vivid-planet.com>
2009-09-29 10:49:05 +0000

    pflichtfeld

--- Vpc/Events/Detail/Form.php
+++ Vpc/Events/Detail/Form.php
@@ -6,7 +6,8 @@ class Vpc_Events_Detail_Form extends Vpc_News_Detail_Abstract_Form
         parent::_initFields();
         unset($this->fields['publish_date']);
         if (isset($this->fields['expiry_date'])) unset($this->fields['expiry_date']);
-        $this->add(new Vps_Form_Field_DateTimeField('start_date', trlVps('From')));
+        $this->add(new Vps_Form_Field_DateTimeField('start_date', trlVps('From')))
+            ->setAllowBlank(false);
         $this->add(new Vps_Form_Field_DateTimeField('end_date', trlVps('To')));
         $this->add(new Vps_Form_Field_TextField('place', trlVps('Place (City)')));
     }
