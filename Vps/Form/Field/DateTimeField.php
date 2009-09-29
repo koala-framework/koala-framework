6c18ff7c0fea107558aa5250a4036027bb4cc9da
vivid <vivid@localhost>
2009-09-29 10:44:52 +0000

    fix wenn falsches format

--- Vps/Form/Field/DateTimeField.php
+++ Vps/Form/Field/DateTimeField.php
@@ -10,8 +10,11 @@ class Vps_Form_Field_DateTimeField extends Vps_Form_Field_SimpleAbstract
     protected function _getValueFromPostData($postData)
     {
         $ret = parent::_getValueFromPostData($postData);
-        preg_match('#^"([0-9]{4}-[0-9]{2}-[0-9]{2})T([0-9]{2}:[0-9]{2}:[0-9]{2})"$#', $ret, $m);
-        $ret = "$m[1] $m[2]";
+        if (preg_match('#^"([0-9]{4}-[0-9]{2}-[0-9]{2})T([0-9]{2}:[0-9]{2}:[0-9]{2})"$#', $ret, $m)) {
+            $ret = "$m[1] $m[2]";
+	} else {
+	    $ret = null;
+	}
         return $ret;
     }
 
