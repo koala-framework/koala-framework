b9cdfc5a5589af52cad0a50b401fa4929b829d44
Christoph Eder <ce@vivid-planet.com>
2009-09-29 08:30:20 +0000

    1

--- Vpc/Events/Detail/Component.php
+++ Vpc/Events/Detail/Component.php
@@ -1,4 +1,10 @@
 <?php
 class Vpc_Events_Detail_Component extends Vpc_News_Detail_Abstract_Component
 {
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['assetsAdmin']['dep'][] = 'VpsFormDateTimeField';
+        return $ret;
+    }
 }
