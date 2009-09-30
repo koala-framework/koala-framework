2821f2f83d684689993465c57eba814f4507c0b9
Markus Hauser <mh@vivid-planet.com>
2009-09-30 13:39:15 +0000

    Models kÃ¶nnen sich dupliieren

--- Vps/Model/Row/Abstract.php
+++ Vps/Model/Row/Abstract.php
@@ -391,4 +391,17 @@ abstract class Vps_Model_Row_Abstract implements Vps_Model_Row_Interface, Serial
         }
         return false;
     }
+
+    public function duplicate()
+    {
+        $primaryKey = $this->getModel()->getPrimaryKey();
+        $newRow = $this->getModel()->createRow();
+
+        foreach ($this->toArray() as $k => $v) {
+            if ($k == $primaryKey) continue;
+            $newRow->$k = $v;
+        }
+
+        return $newRow;
+    }
 }
