83e5ed65ab355bfcd228537ae032803851a0c586
Niko Sams <ns@vivid-planet.com>
2009-10-01 07:33:15 +0000

    standard wert

--- Vps/Model/Row/Interface.php
+++ Vps/Model/Row/Interface.php
@@ -7,7 +7,7 @@ interface Vps_Model_Row_Interface
     public function __set($name, $value);
     public function save();
     public function delete();
-    public function duplicate(array $data);
+    public function duplicate(array $data = array());
     public function toArray();
     public function setSiblingRows(array $rows);
     public function getChildRows($rule, $select = array());
