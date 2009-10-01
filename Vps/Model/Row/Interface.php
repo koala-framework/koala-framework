9daf39f9be92f0acbd11acbe031e82a863835e67
Niko Sams <ns@vivid-planet.com>
2009-10-01 07:32:03 +0000

    remove duplicated duplicate

--- Vps/Model/Row/Interface.php
+++ Vps/Model/Row/Interface.php
@@ -6,10 +6,9 @@ interface Vps_Model_Row_Interface
     public function __get($name);
     public function __set($name, $value);
     public function save();
-    public function delete();
+    public function delete(array $data);
     public function duplicate();
     public function toArray();
-    public function duplicate();
     public function setSiblingRows(array $rows);
     public function getChildRows($rule, $select = array());
     public function createChildRow($rule, array $data = array());
