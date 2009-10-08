1b1812bca0e659198cf4926ae99bc33963293da3
Niko Sams <ns@vivid-planet.com>
2009-10-08 15:19:41 +0000

    merged fieldset in frontend mit checkbox

---
+++ tests/Vps/Form/FieldSet/Frontend/Test.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @group selenium
+ * @group Vps_Form_FieldSet
+ */
+class Vps_Form_FieldSet_Frontend_Test extends Vps_Test_SeleniumTestCase
+{
+    private $_root;
+
+    public function setUp()
+    {
+        Vps_Component_Data_Root::setComponentClass('Vps_Form_FieldSet_Frontend_Root');
+        $this->_root = Vps_Component_Data_Root::getInstance();
+        parent::setUp();
+    }
+
+    public function testRemembersValueOnSubmit()
+    {
+        $this->openVpc('/form');
+        $this->assertFalse($this->isChecked("css=fieldset legend input"));
+        $this->click("css=fieldset legend input");
+        $this->clickAndWait("css=button.submit");
+        $this->assertTrue($this->isChecked("css=fieldset legend input"));
+    }
+
+    public function testNoValidationInFieldset()
+    {
+        $this->openVpc('/form');
+        $this->type("css=#foo1", 'blah');
+        $this->click("css=fieldset legend input");
+        $this->clickAndWait("css=button.submit");
+        $this->assertTextPresent(trlVps('Ein Fehler ist aufgetreten'));
+        $this->assertTrue($this->isChecked("css=fieldset legend input"));
+        $this->click("css=fieldset legend input");
+        $this->clickAndWait("css=button.submit");
+        $this->assertTextNotPresent(trlVps('Ein Fehler ist aufgetreten'));
+    }
+}
