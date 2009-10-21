378c9d172794768d95956bdb6cf1a7a8f51388af
Niko Sams <ns@vivid-planet.com>
2009-10-21 06:58:45 +0000

    merged random wirklich random gemacht

--- Vps/Filter/Row/Random.php
+++ Vps/Filter/Row/Random.php
@@ -9,9 +9,21 @@ class Vps_Filter_Row_Random extends Vps_Filter_Row_Abstract
     public function filter($row)
     {
         if (!$row->{$this->_field}) {
-            return substr(uniqid('', true), 0, $this->_length);
+            return $this->_generateRandStr($this->_length);
         } else {
             return $row->{$this->_field};
         }
+    }
+
+    private function _generateRandStr($length)
+    {
+        $letters = '1234567890qwertyuiopasdfghjklzxcvbnm';
+
+        $s = '';
+        $lettersLength = strlen($letters)-1;
+        for($i = 0 ; $i < $length ; $i++) {
+            $s .= $letters[rand(0,$lettersLength)];
+        }
+        return $s;
     }
 }
