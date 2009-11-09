d513e28ee5bbbb388839449580e7aef5b598e823
Markus Hauser <mh@vivid-planet.com>
2009-11-09 15:26:44 +0000

    proxy fuer services

--- Vps/User/Mirror.php
+++ Vps/User/Mirror.php
@@ -6,13 +6,28 @@ final class Vps_User_Mirror extends Vps_Model_MirrorCache
 
     protected function _init()
     {
-        $client = new Vps_Srpc_Client(array(
+        $clientConfig = array(
             'serverUrl' => Vps_Registry::get('config')->service->users->url,
             'extraParams' => array(
                 'applicationId' => Vps_Registry::get('config')->application->id,
                 'version'       => Vps_User_Model::version()
             )
-        ));
+        );
+        $cfg = Vps_Registry::get('config');
+        if (!empty($cfg->service->users->proxy->host)) {
+            $clientConfig['proxy_host'] = $cfg->service->users->proxy->host;
+        }
+        if (!empty($cfg->service->users->proxy->port)) {
+            $clientConfig['proxy_port'] = $cfg->service->users->proxy->port;
+        }
+        if (!empty($cfg->service->users->proxy->user)) {
+            $clientConfig['proxy_user'] = $cfg->service->users->proxy->user;
+        }
+        if (!empty($cfg->service->users->proxy->pass)) {
+            $clientConfig['proxy_pass'] = $cfg->service->users->proxy->pass;
+        }
+
+        $client = new Vps_Srpc_Client($clientConfig);
         $this->_sourceModel = new Vps_Model_Service(array('client' => $client));
         $this->_proxyModel = new Vps_Model_Db(array('table' => 'cache_users'));
 
