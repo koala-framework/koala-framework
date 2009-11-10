17aae9c2b83790bda440dd18f598598a525e4af8
Niko Sams <ns@vivid-planet.com>
2009-11-10 08:06:33 +0000

    web kann auch in vw-projekte liegen

--- Vps/Controller/Action/Cli/TagController.php
+++ Vps/Controller/Action/Cli/TagController.php
@@ -168,6 +168,8 @@ class Vps_Controller_Action_Cli_TagController extends Vps_Controller_Action_Cli_
             return $m[1];
         } else if (preg_match("#branches/(.*)\n#", `svn info`, $m)) {
             return $m[1];
+        } else if (preg_match("#trunk/vw-projekte/(.*)\n#", `svn info`, $m)) {
+            return $m[1];
         }
         return false;
     }
