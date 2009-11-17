6521bcca6808071c2818b1de2ed46894f07fa41c
Franz Unger <ufx@vivid-planet.com>
2009-11-17 22:41:39 +0000

    Newsletter: Falls Empfänger keine Adresse, beim Senden überspringen und Fehler merken

---
+++ Vps/Update/31621.sql
@@ -0,0 +1,1 @@
+ ALTER TABLE `vpc_newsletter_queue` CHANGE `status` `status` ENUM( 'queued', 'sending', 'userNotFound', 'noAddress', 'sent', 'sendingError' ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'queued';
