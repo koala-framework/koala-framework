cf45006d1a3af8986db3a03ef5b815f8615e282f
Niko Sams <ns@vivid-planet.com>
2009-11-19 15:29:10 +0000

    loadAfterSave für htmleditor etwas sauberer gelöst

--- Vps/Form/Field/HtmlEditor.php
+++ Vps/Form/Field/HtmlEditor.php
@@ -4,6 +4,7 @@ class Vps_Form_Field_HtmlEditor extends Vps_Form_Field_SimpleAbstract
     public function __construct($field_name = null, $field_label = null)
     {
         parent::__construct($field_name, $field_label);
+        $this->setLoadAfterSave(true);
         $this->setXtype('htmleditor');
     }
 }
