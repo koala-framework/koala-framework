4018b87de3404e9f20276894a14f622104c1cdfe
Niko Sams <ns@vivid-planet.com>
2009-11-24 07:22:43 +0000

    limit von rrd cache berücksichtigen

---
+++ Vps/Update/31736.php
@@ -0,0 +1,13 @@
+<?php
+class Vps_Update_31736 extends Vps_Update
+{
+    protected function _init()
+    {
+        $this->_actions[] = new Vps_Update_Action_Rrd_AddDs(array(
+            'file' => 'benchmark.rrd',
+            'name' => 'memcache-limit-maxbytes',
+            'type' => 'GAUGE',
+            'max' => pow(2, 64),
+        ));
+    }
+}
