a56251414cc6944c241d55c9390645befeaf4233
Markus Hauser <mh@vivid-planet.com>
2009-11-24 16:02:10 +0000

    merged test fixes

--- tests/Vpc/TextImage/ImageEnlarge/LinkTag/TestComponent.php
+++ tests/Vpc/TextImage/ImageEnlarge/LinkTag/TestComponent.php
@@ -5,7 +5,11 @@ class Vpc_TextImage_ImageEnlarge_LinkTag_TestComponent extends Vpc_TextImage_Ima
     {
         $ret = parent::getSettings();
         $ret['ownModel'] = 'Vpc_TextImage_ImageEnlarge_LinkTag_TestModel';
-        $ret['generators']['link']['component']['enlarge'] = 'Vpc_TextImage_ImageEnlarge_LinkTag_EnlargeTag_TestComponent';
+        $ret['generators']['link']['component'] = array(
+            'none' => 'Vpc_Basic_LinkTag_Empty_Component',
+            'download' => 'Vpc_Basic_LinkTag_Empty_Component',
+            'enlarge' => 'Vpc_TextImage_ImageEnlarge_LinkTag_EnlargeTag_TestComponent'
+        );
         return $ret;
     }
 }
