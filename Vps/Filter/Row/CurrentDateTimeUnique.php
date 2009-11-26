77c705b6977643871cb1f550a711489f7b5cf28e
Markus Hauser <mh@vivid-planet.com>
2009-11-26 13:00:53 +0000

    merge von 1.7: neuer filter fuer service + test fuer ein problem im service (user in selber sekunde anlegen)

---
+++ Vps/Filter/Row/CurrentDateTimeUnique.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Generates the current date for a model, but makes it unique.
+ * If a row already exists with the current date, it adds one second.
+ * Is used for instance in the service
+ */
+class Vps_Filter_Row_CurrentDateTimeUnique extends Vps_Filter_Row_CurrentDateTime
+{
+    public function filter($row)
+    {
+        $ret = parent::filter($row);
+
+        $m = $row->getModel();
+        $highestRow = $m->getRow($m->select()
+            ->where(new Vps_Model_Select_Expr_Or(array(
+                new Vps_Model_Select_Expr_HigherDate($this->_field, $ret),
+                new Vps_Model_Select_Expr_Equals($this->_field, $ret)
+            )))
+            ->order($this->_field, 'DESC')
+        );
+        if ($highestRow) {
+            $ret = date($this->_dateFormat, strtotime($highestRow->{$this->_field}) + 1);
+        }
+        return $ret;
+    }
+}
