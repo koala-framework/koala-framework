6923106d01ab01276a7537459dfaee1cea177b47
Niko Sams <ns@vivid-planet.com>
2009-11-26 09:04:46 +0000

    Zend unterstützt bei Cache_Frontend_File jetzt auch mehrere master-dateien

--- Vps/Config/Cache.php
+++ Vps/Config/Cache.php
@@ -1,38 +1,20 @@
 <?php
-require_once 'Zend/Cache.php';
-require_once 'Zend/Cache/Core.php';
 require_once 'Zend/Cache/Frontend/File.php';
-class Vps_Config_Cache extends Zend_Cache_Frontend_File {
-    
-    private $_masterFile2_mtime = null;
-    
+class Vps_Config_Cache extends Zend_Cache_Frontend_File
+{
     public function __construct()
     {
         parent::__construct(array(
-            'master_file' => 'application/config.ini',
+            'master_files' => array(
+                'application/config.ini',
+                VPS_PATH . '/config.ini'
+            ),
             'automatic_serialization' => true
         ));
-        
-        $this->_masterFile2_mtime = filemtime(VPS_PATH . '/config.ini');
-        
-        require_once 'Zend/Cache/Backend/File.php';
-        $backend = new Zend_Cache_Backend_File(array(
-            'cache_dir' => 'application/cache/config',
-            'hashed_directory_umask' => 0777,
-            'cache_file_umask' => 0666,
-        ));
-        
-        $this->setBackend($backend);
+        require_once 'Vps/Cache/Backend/File.php';
+        $this->setBackend(new Vps_Cache_Backend_File());
     }
-    
-    public function test($id)
-    {
-        $lastModified = parent::test($id);
-        if ($lastModified && $lastModified > $this->_masterFile2_mtime) {
-            return $lastModified;
-        }
-        return false;
-    }
+
 
     public static function getInstance()
     {
