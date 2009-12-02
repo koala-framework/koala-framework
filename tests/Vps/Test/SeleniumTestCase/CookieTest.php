92380da9fd7013ab58670f83f892be4ee9798d74
Niko Sams <ns@vivid-planet.com>
2009-12-02 10:39:19 +0000

    komponenten abgeschalten, nicht frontend verwenden zum testenn

--- tests/Vps/Test/SeleniumTestCase/CookieTest.php
+++ tests/Vps/Test/SeleniumTestCase/CookieTest.php
@@ -6,7 +6,7 @@ class Vps_Test_SeleniumTestCase_CookieTest extends Vps_Test_SeleniumTestCase
 {
     public function testCookie()
     {
-        $this->open('/');
+        $this->open('/vps/test/vps_test_selenium-test-case_test/index');
         $this->assertEquals($this->_unitTestCookie, $this->getCookieByName('unitTest'));
     }
 }
