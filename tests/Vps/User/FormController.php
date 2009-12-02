a89c43c0438c8c1edfb7c0ba28b66864dcf075db
Niko Sams <ns@vivid-planet.com>
2009-12-02 11:16:08 +0000

    eigene config, kein menü

--- tests/Vps/User/FormController.php
+++ tests/Vps/User/FormController.php
@@ -3,5 +3,12 @@ class Vps_User_FormController extends Vps_Controller_Action_Auto_Form
 {
     protected $_buttons = array('save', 'add');
     protected $_formName = 'Vps_User_UserForm';
+    
+    public function indexAction()
+    {
+        parent::indexAction();
+        $this->view->assetsType = 'Vps_User:Test';
+        $this->view->viewport = 'Vps.Test.Viewport';
+    }
 }
 
