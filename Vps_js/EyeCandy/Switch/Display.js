5b14b5e42af8193eff523df8b91c8d5ccb885aa1
Markus Hauser <mh@vivid-planet.com>
2009-12-10 17:00:46 +0000

    switchDisplay flacker-fix

--- Vps_js/EyeCandy/Switch/Display.js
+++ Vps_js/EyeCandy/Switch/Display.js
@@ -1,3 +1,6 @@
+// um flackern zu unterbinden
+document.write('<style type="text/css"> div.vpsSwitchDisplay div.switchContent { display: none; } </style>');
+
 Vps.onContentReady(function() {
     var els = Ext.query('div.vpsSwitchDisplay');
     els.forEach(function(el) {
@@ -17,8 +20,12 @@ Vps.Switch.Display = function(el) {
     this.switchLink = Ext.get(Ext.query('a.switchLink', this.el.dom)[0]);
     this.switchContent = Ext.get(Ext.query('div.switchContent', this.el.dom)[0]);
 
+    // durch unterbinden von flackern (ganz oben) muss das auf block
+    // gesetzt werden, damit die hoehe gemessen werden kann
+    this.switchContent.setStyle('display', 'block');
     this.switchContent.scaleHeight = this.switchContent.getHeight();
     this.switchContent.setHeight(0);
+    // und schnell wieder auf 'none' bevors wer merkt :)
     this.switchContent.setStyle('display', 'none');
 
     if (this.switchLink && this.switchContent) {
