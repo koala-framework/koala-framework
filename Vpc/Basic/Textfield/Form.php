5a5bd58361960d0a62b941561948c359eedb272e
Markus Hauser <mh@vivid-planet.com>
2009-12-10 13:00:06 +0000

    textfield komponente form: _initFields verwenden statt konstruktor

--- Vpc/Basic/Textfield/Form.php
+++ Vpc/Basic/Textfield/Form.php
@@ -1,10 +1,11 @@
 <?php
 class Vpc_Basic_Textfield_Form extends Vpc_Abstract_Form
 {
-    public function __construct($name, $class, $id = null)
+    protected function _initFields()
     {
-        parent::__construct($name, $class, $id);
+        parent::_initFields();
         $this->fields->add(new Vps_Form_Field_TextField('content', trlVps('Content')))
             ->setWidth(400);
     }
+
 }
