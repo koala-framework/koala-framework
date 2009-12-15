1afd8b796dde0a755571643c74021e16ff6374cf
Niko Sams <ns@vivid-planet.com>
2009-12-15 14:12:37 +0000

    toStringField statt überschriebenem __toString in row

--- Vpc/Forum/Directory/Model.php
+++ Vpc/Forum/Directory/Model.php
@@ -3,6 +3,7 @@ class Vpc_Forum_Directory_Model extends Vps_Model_Db
 {
     protected $_table = 'vpc_forum_groups';
     protected $_rowClass = 'Vpc_Forum_Directory_Row';
+    protected $_toStringField = 'name';
 
     protected function _setupFilters()
     {
