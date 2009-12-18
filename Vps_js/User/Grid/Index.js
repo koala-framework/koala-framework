a3c8d9404441a5342cf61576072e05cc6ad6dcba
Markus Hauser <mh@vivid-planet.com>
2009-12-18 09:30:11 +0000

    bei user kann mail sending unterdrueckt werden + comments + logs

--- Vps_js/User/Grid/Index.js
+++ Vps_js/User/Grid/Index.js
@@ -1,122 +1,61 @@
 
 Ext.namespace("Vps.User.Grid");
 
-Vps.User.Grid.Index = Ext.extend(Vps.Auto.GridPanel,
-{
+Vps.User.Grid.Index = Ext.extend(Ext.Panel, {
+
     initComponent: function() {
+        var bindings = [];
+        var southPanelItems = [ ];
+
+        if (this.commentsControllerUrl) {
+            var commentsGrid = new Vps.Auto.GridPanel({
+                controllerUrl: this.commentsControllerUrl,
+                region: 'center',
+                title: trlVps('Comments')
+            });
+            bindings.push({ item: commentsGrid, queryParam: 'user_id' });
+            southPanelItems.push(commentsGrid);
+        }
+
+        if (this.logControllerUrl) {
+            logGridConfig = {
+                controllerUrl: this.logControllerUrl ,
+                region: 'center',
+                split: true,
+                title: trlVps('Log')
+            };
+            if (this.commentsControllerUrl) {
+                logGridConfig.width = 550;
+                logGridConfig.region = 'east';
+            }
+            var logGrid = new Vps.Auto.GridPanel(logGridConfig);
+            bindings.push({ item: logGrid, queryParam: 'user_id' });
+            southPanelItems.push(logGrid);
+        }
+
+        if (typeof commentsGrid != 'undefined' || typeof logGrid != 'undefined') {
+            var southPanel = new Ext.Panel({
+                layout: 'border',
+                items: southPanelItems,
+                region: 'south',
+                split: true,
+                height: 240
+            });
+        }
+
+        var userGrid = new Vps.User.Grid.Grid({
+            controllerUrl: this.controllerUrl,
+            region: 'center',
+            bindings: bindings
+        });
+
+        this.layout = 'border';
+        this.items = [ userGrid ];
+
+        if (typeof southPanel != 'undefined') {
+            this.items.push(southPanel);
+        }
+
         Vps.User.Grid.Index.superclass.initComponent.call(this);
-        if (!this.columnsConfig) this.columnsConfig = { };
-        this.columnsConfig['resend_mails'] = {
-            clickHandler: function(grid, rowIndex, col, e) {
-                var r = grid.getStore().getAt(rowIndex);
-                var win = new Vps.User.Grid.SendMailWindow({
-                    controllerUrl: this.controllerUrl,
-                    baseParams: { user_id: r.data.id }
-                });
-                win.show();
-            },
-            scope: this
-        };
-
-        this.actions.userdelete = new Ext.Action({
-            text    : trlVps('Delete user'),
-            icon    : '/assets/silkicons/delete.png',
-            cls     : 'x-btn-text-icon',
-            handler : this.onDelete,
-            scope: this
-        });
-        this.actions.userlock = new Ext.Action({
-            text    : trlVps('Lock / unlock user'),
-            icon    : '/assets/silkicons/lock.png',
-            cls     : 'x-btn-text-icon',
-            handler : this.onUnLock,
-            scope: this
-        });
-    },
-
-    onUnLock: function() {
-        Ext.Msg.show({
-            title: trlVps('Lock user'),
-            msg: trlVps('Do you really wish to (un)lock this user?'),
-            buttons: Ext.Msg.YESNO,
-            scope: this,
-            fn: function(button) {
-                if (button == 'yes') {
-                    var selectedRows = this.getGrid().getSelectionModel().getSelections();
-                    if (!selectedRows.length) return;
-
-                    var ids = [];
-                    var params = this.getBaseParams() || {};
-                    var newNewRecords = [];
-                    selectedRows.each(function(selectedRow) {
-                        ids.push(selectedRow.id);
-                    }, this);
-                    if (!ids.length) return;
-
-                    params[this.store.reader.meta.id] = ids.join(';');
-
-                    this.el.mask(trlVps('Locking / unlocking...'));
-                    Ext.Ajax.request({
-                        url: this.controllerUrl+'/json-user-lock',
-                        params: params,
-                        success: function(response, options, r) {
-                            this.reload();
-                            this.fireEvent('datachange', r);
-                        },
-                        callback: function() {
-                            this.el.unmask();
-                        },
-                        scope : this
-                    });
-                }
-            }
-        });
-    },
-
-    onDelete : function() {
-        Ext.Msg.show({
-            title: trlVps('Delete user'),
-            msg: trlVps('Do you really wish to delete this user?'),
-            buttons: Ext.Msg.YESNO,
-            scope: this,
-            fn: function(button) {
-                if (button == 'yes') {
-                    var selectedRows = this.getGrid().getSelectionModel().getSelections();
-                    if (!selectedRows.length) return;
-
-                    var ids = [];
-                    var params = this.getBaseParams() || {};
-                    var newNewRecords = [];
-                    selectedRows.each(function(selectedRow) {
-                        ids.push(selectedRow.id);
-                    }, this);
-                    if (!ids.length) return;
-
-                    params[this.store.reader.meta.id] = ids.join(';');
-
-                    this.el.mask(trlVps('Deleting...'));
-                    Ext.Ajax.request({
-                        url: this.controllerUrl+'/json-user-delete',
-                        params: params,
-                        success: function(response, options, r) {
-                            this.reload();
-                            this.fireEvent('deleterow', this.grid);
-                            this.fireEvent('datachange', r);
-
-                            this.activeId = null;
-                            //wenn gelöscht alle anderen disablen
-                            this.bindings.each(function(i) {
-                                i.item.disable();
-                                i.item.reset();
-                            }, this);
-                        },
-                        callback: function() {
-                            this.el.unmask();
-                        },
-                        scope : this
-                    });
-                }
-            }
-        });
     }
 });
