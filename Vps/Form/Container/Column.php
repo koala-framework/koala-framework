7c5660c69f1a2c8f94af99275bbbda8ab3b6c84d
Franz Unger <ufx@vivid-planet.com>
2009-12-21 17:03:30 +0000

    merged Form_Column wird jetzt auch in Frontend ausgegeben

--- Vps/Form/Container/Column.php
+++ Vps/Form/Container/Column.php
@@ -7,4 +7,15 @@ class Vps_Form_Container_Column extends Vps_Form_Container_Abstract
         $this->setBaseCls('x-plain');
         $this->setStyle('margin: 0px 10px;');
     }
+
+    public function getTemplateVars($values, $fieldNamePostfix='')
+    {
+        static $nr;
+        $nr++;
+        $ret = parent::getTemplateVars($values, $fieldNamePostfix);
+        $ret['preHtml'] = ''; // damit ein div ausgegeben wird
+        $ret['postHtml'] = '';
+        $ret['id'] = 'Column' . $nr;
+        return $ret;
+    }
 }
