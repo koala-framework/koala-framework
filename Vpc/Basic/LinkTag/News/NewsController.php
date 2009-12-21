38963c945f77765c8a8387b4b19b112d0d8c4684
Niko Sams <ns@vivid-planet.com>
2009-12-21 08:48:45 +0000

    merged das ist mittlerweile ein model

--- Vpc/Basic/LinkTag/News/NewsController.php
+++ Vpc/Basic/LinkTag/News/NewsController.php
@@ -1,7 +1,7 @@
 <?php
 class Vpc_Basic_LinkTag_News_NewsController extends Vps_Controller_Action_Auto_Grid
 {
-    protected $_tableName = 'Vpc_News_Directory_Model';
+    protected $_modelName = 'Vpc_News_Directory_Model';
     protected $_defaultOrder = array('field'=>'publish_date', 'direction'=>'DESC');
     protected $_paging = 20;
 
