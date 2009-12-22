adf025c7bc0d8b96cc1e0bc04029b6c8d3ed41ac
Niko Sams <ns@vivid-planet.com>
2009-12-22 11:16:48 +0000

    statistik entfernt, machte probleme

--- Vps/Loader/Benchmark.php
+++ Vps/Loader/Benchmark.php
@@ -5,11 +5,6 @@ class Vps_Loader_Benchmark extends Vps_Loader
     public static function loadClass($class)
     {
         parent::loadClass($class);
-        if (substr($class, 0, 4) == 'Vpc_') {
-            if (is_subclass_of($class, 'Vpc_Abstract')) {
-                Vps_Benchmark::count('component classes included', $class);
-            }
-        }
         Vps_Benchmark::count('classes included', $class);
     }
 }
