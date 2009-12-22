5fa5fe95c302b236b2b5f4c980b6f22cf0f6319e
Niko Sams <ns@vivid-planet.com>
2009-12-22 16:04:54 +0000

    bugfix wenn nicht gefunden

--- Vps/Component/Abstract/ToStringData.php
+++ Vps/Component/Abstract/ToStringData.php
@@ -5,6 +5,7 @@ class Vps_Component_Abstract_ToStringData extends Vps_Data_Abstract
     {
         //$row ist die von der parent, also zB der List
         $c = Vps_Component_Data_Root::getInstance()->getComponentById($row->component_id.'-'.$row->id);
+        if (!$c) return '';
         $admin = Vpc_Admin::getInstance($c->componentClass);
         return $admin->componentToString($c);
     }
