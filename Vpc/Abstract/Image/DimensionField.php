808dc2f9b657af8c425d27a7b29ccc953d32f5a5
Niko Sams <ns@vivid-planet.com>
2010-01-11 09:57:02 +0000

    merged fix notice wenn keine dimension gesetzt

--- Vpc/Abstract/Image/DimensionField.php
+++ Vpc/Abstract/Image/DimensionField.php
@@ -53,7 +53,7 @@ class Vpc_Abstract_Image_DimensionField extends Vps_Form_Field_Abstract
             $dimensions = $this->getDimensions();
             reset($dimensions);
 
-            if ($data['dimension']) {
+            if (!empty($data['dimension'])) {
                 $dimension = $dimensions[$data['dimension']];
             } else {
                 $dimension = current($dimensions);
