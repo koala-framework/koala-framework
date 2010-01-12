3b5a0a0cc34ead8d4ee26a01d703e2d32d5f326f
Niko Sams <ns@vivid-planet.com>
2010-01-12 09:47:34 +0000

    cleanup

--- Vpc/User/ChangePassword/Component.php
+++ Vpc/User/ChangePassword/Component.php
@@ -5,16 +5,6 @@ class Vpc_User_ChangePassword_Component extends Vpc_Abstract_Composite_Component
     {
         $ret = parent::getSettings();
         $ret['generators']['child']['component']['form'] = 'Vpc_User_Edit_Form_Component';
-        $ret['forms'] = 'all';
-        $ret['cssClass'] = 'webStandard';
-        $ret['viewCache'] = false;
-        return $ret;
-    }
-
-    public function getTemplateVars()
-    {
-        $ret = parent::getTemplateVars();
-        $ret['authedUser'] = Vps_Registry::get('userModel')->getAuthedUser();
         return $ret;
     }
 }
