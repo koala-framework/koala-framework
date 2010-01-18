582e826e1988dbb61e3a5601dbcc3393593a5ec4
Markus Hauser <mh@vivid-planet.com>
2010-01-18 10:15:58 +0000

    fix newsletter user anlegen im backend

--- Vpc/Newsletter/Subscribe/RecipientController.php
+++ Vpc/Newsletter/Subscribe/RecipientController.php
@@ -3,4 +3,12 @@ class Vpc_Newsletter_Subscribe_RecipientController extends Vps_Controller_Action
 {
     protected $_permissions = array('save', 'add');
     protected $_formName = 'Vpc_Newsletter_Subscribe_Form';
+
+    protected function _beforeInsert(Vps_Model_Row_Interface $row)
+    {
+        $row->subscribe_date = date('Y-m-d H:i:s');
+        if ($row->getModel()->hasColumn('activated')) {
+            $row->activated = 1;
+        }
+    }
 }
