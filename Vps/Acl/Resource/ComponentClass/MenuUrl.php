3e6bab1524c216dca2bb0bf8f0de179edbd1e6db
Franz Unger <ufx@vivid-planet.com>
2010-01-22 12:24:53 +0000

    merged Menüeintrag für statische Komponente kann jetzt eine andere resourceId als ComponentClass haben, damit man mehrere Menüeinträge für eine Komponente machen kann. Ist Abwärtskompatibel.

--- Vps/Acl/Resource/ComponentClass/MenuUrl.php
+++ Vps/Acl/Resource/ComponentClass/MenuUrl.php
@@ -4,10 +4,11 @@ class Vps_Acl_Resource_ComponentClass_MenuUrl extends Vps_Acl_Resource_MenuUrl
 {
     protected $_componentClass;
 
-    public function __construct($componentClass, $menuConfig = null, $menuUrl = null)
+    public function __construct($resourceId, $menuConfig = null, $menuUrl = null, $componentClass = null)
     {
+        if (!$componentClass) $componentClass = $resourceId;
         $this->_componentClass = $componentClass;
-        parent::__construct('vpc_'.$componentClass, $menuConfig, $menuUrl);
+        parent::__construct('vpc_'.$resourceId, $menuConfig, $menuUrl);
     }
 
     public function getComponentClass()
