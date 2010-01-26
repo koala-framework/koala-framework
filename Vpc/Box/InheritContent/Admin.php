535aaba14a6e6307d3ef52033d13de6a3d10faed
Niko Sams <ns@vivid-planet.com>
2010-01-26 14:25:26 +0000

    editComponents zum komponenten bearbeiten verwenden, nicht eigene form; funktioniert damit auch mit paragraphs und columns

---
+++ Vpc/Box/InheritContent/Admin.php
@@ -0,0 +1,8 @@
+<?php
+class Vpc_Box_InheritContent_Admin extends Vpc_Admin
+{
+    public function getExtConfig()
+    {
+        return array();
+    }
+}
--- Vpc/Box/InheritContent/Controller.php
+++
@@ -1,4 +0,0 @@
-<?php
-class Vpc_Box_InheritContent_Controller extends Vps_Controller_Action_Auto_Vpc_Form
-{
-}
