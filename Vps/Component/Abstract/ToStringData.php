4a2821ab590b57674b6dee9ec1f31f837c07d858
Niko Sams <ns@vivid-planet.com>
2010-01-26 15:17:52 +0000

    ignoreVisible da es sonst in invisible paragraphs ned ged

--- Vps/Component/Abstract/ToStringData.php
+++ Vps/Component/Abstract/ToStringData.php
@@ -4,7 +4,8 @@ class Vps_Component_Abstract_ToStringData extends Vps_Data_Abstract
     public function load($row)
     {
         //$row ist die von der parent, also zB der List
-        $c = Vps_Component_Data_Root::getInstance()->getComponentById($row->component_id.'-'.$row->id);
+        $c = Vps_Component_Data_Root::getInstance()
+            ->getComponentById($row->component_id.'-'.$row->id, array('ignoreVisible'=>true));
         if (!$c) return '';
         $admin = Vpc_Admin::getInstance($c->componentClass);
         return $admin->componentToString($c);
