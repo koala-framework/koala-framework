b9ccdde17c8e11a8572841d79ca9941ec2873795
Christoph Eder <ce@vivid-planet.com>
2010-01-29 10:16:54 +0000

    fix test, auskommentierten code entfernt

--- tests/Vps/Assets/Dynamic/Test.php
+++ tests/Vps/Assets/Dynamic/Test.php
@@ -28,9 +28,9 @@ class Vps_Assets_Dynamic_Test extends PHPUnit_Framework_TestCase
         $type = 'Vps_Assets_Dynamic:Test';
         $files = $dep->getAssetUrls($type, 'css', 'web', false);
         $this->assertEquals(1, count($files));
-        $this->assertContains('/assets/dynamic/Vps_Assets_Dynamic_Asset/Vps_Assets_Dynamic:Test', $files[0]);
+        $this->assertContains('/assets/dynamic/Vps_Assets_Dynamic:Test/Vps_Assets_Dynamic_Asset', $files[0]);
 
-        $c = $loader->getFileContents('dynamic/Vps_Assets_Dynamic_Asset/Vps_Assets_Dynamic:Test');
+        $c = $loader->getFileContents('dynamic/Vps_Assets_Dynamic:Test/Vps_Assets_Dynamic_Asset');
         $this->assertEquals('a { color: red; }', $c['contents']);
     }
 
@@ -77,47 +77,4 @@ class Vps_Assets_Dynamic_Test extends PHPUnit_Framework_TestCase
         $this->assertEquals(1, Vps_Benchmark::getCounterValue('processing dependencies miss'));
         $this->assertEquals(2, Vps_Benchmark::getCounterValue('load asset all'));
     }
-/*
-    public function testNoDebug()
-    {
-        Vps_Assets_Cache::getInstance()->clean();
-        Vps_Benchmark::enable();
-        Vps_Benchmark::reset();
-        $this->_testNoDebug();
-        $this->assertEquals(1, Vps_Benchmark::getCounterValue('processing dependencies miss'));
-        $this->assertEquals(1, Vps_Benchmark::getCounterValue('load asset all'));
-        $this->assertEquals(3, Vps_Benchmark::getCounterValue('load asset'));
-        $this->_testNoDebug();
-        $this->_testNoDebug();
-        $this->assertEquals(1, Vps_Benchmark::getCounterValue('processing dependencies miss'));
-        $this->assertEquals(1, Vps_Benchmark::getCounterValue('load asset all'));
-        $this->assertEquals(3, Vps_Benchmark::getCounterValue('load asset'));
-    }
-    private function _testNoDebug()
-    {
-        $this->assertEquals('none', Vps_Media_Output::getEncoding());
-        $config = clone Zend_Registry::get('config');
-        $config->application->version = '1.0';
-        $config->debug->menu = false;
-        $config->debug->assets->js = false;
-        $config->debug->assets->css = false;
-        $config->debug->assets->printcss = false;
-        $dep = new Vps_Assets_Dependencies($config);
-
-        $type = 'Vps_Assets_OwnConfig:Test';
-        $files = $dep->getAssetUrls($type, 'js', 'web', false);
-        $expected = array(
-            '/assets/all/web/'.Zend_Registry::get('trl')->getTargetLanguage().'/Vps_Assets_OwnConfig:Test.js?v=1.0',
-        );
-        $this->assertEquals($expected, $files);
-
-        $loader = new Vps_Assets_Loader($config);
-
-        $c = $loader->getFileContents('all/web/'.Zend_Registry::get('trl')->getTargetLanguage().'/Vps_Assets_OwnConfig:Test.js?v=1.0');
-        $this->assertContains("file2\nfile1\n", $c['contents']);
-
-        $c = $loader->getFileContents('all/web/'.Zend_Registry::get('trl')->getTargetLanguage().'/Vps_Assets_OwnConfig:Test.js?v=1.0');
-        $this->assertContains("file2\nfile1\n", $c['contents']);
-    }
-*/
 }
