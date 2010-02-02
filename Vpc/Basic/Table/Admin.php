0d98a01a144dca52dd149298ebe7826c6c30c306
Markus Hauser <mh@vivid-planet.com>
2010-02-02 14:14:37 +0000

    kommentar hinzu

--- Vpc/Basic/Table/Admin.php
+++ Vpc/Basic/Table/Admin.php
@@ -7,6 +7,7 @@ class Vpc_Basic_Table_Admin extends Vpc_Admin
         $fields['data'] = 'text NOT NULL';
         $this->createFormTable('vpc_basic_table', $fields);
 
+        // die hier braucht id als primary
         $fields['pos'] = 'int NOT NULL';
         $fields['data'] = 'text NOT NULL';
         $this->createFormTable('vpc_basic_table_data', $fields);
