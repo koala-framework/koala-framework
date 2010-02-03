dea92805f7e3bf0f42f70cff008aaef37f8dc0f8
Niko Sams <ns@vivid-planet.com>
2010-02-03 13:50:54 +0000

    merged fix downloads

--- Vpc/Basic/Download/Admin.php
+++ Vpc/Basic/Download/Admin.php
@@ -7,4 +7,11 @@ class Vpc_Basic_Download_Admin extends Vpc_Abstract_Composite_Admin
         $fields['infotext'] = 'text';
         $this->createFormTable('vpc_basic_download', $fields);
     }
+
+    public function gridColumns()
+    {
+        $ret = parent::gridColumns();
+        unset($ret['string']);
+        return $ret;
+    }
 }
