bb1d85723ffe3af91e851ce500262d946fc13011
Niko Sams <ns@vivid-planet.com>
2010-02-03 10:26:29 +0000

    bearbeitbare meta-tags

---
+++ Vpc/Box/MetaTagsContent/Component.php
@@ -0,0 +1,19 @@
+<?php
+class Vpc_Box_MetaTagsContent_Component extends Vpc_Box_MetaTags_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['componentName'] = trlVps('Meta Tags');
+        $ret['ownModel'] = 'Vps_Component_FieldModel';
+        return $ret;
+    }
+    protected function _getMetaTags()
+    {
+        $ret = parent::_getMetaTags();
+        $row = $this->_getRow();
+        if ($row->keywords) $ret['keywords'] = $row->keywords;
+        if ($row->description) $ret['description'] = $row->description;
+        return $ret;
+    }
+}
