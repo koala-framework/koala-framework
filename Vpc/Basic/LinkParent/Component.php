3eb6c87efc30604cd901a28ed08c353c47808dc0
Christoph Eder <ce@vivid-planet.com>
2010-02-09 13:55:16 +0000

    - trl mit trlVps ersetzt

--- Vpc/Basic/LinkParent/Component.php
+++ Vpc/Basic/LinkParent/Component.php
@@ -4,7 +4,7 @@ class Vpc_Basic_LinkParent_Component extends Vpc_Basic_Link_Component
     public static function getSettings()
     {
         $ret = parent::getSettings();
-        $ret['componentName'] = trl('Link to parent page');
+        $ret['componentName'] = trlVps('Link to parent page');
         $ret['generators']['child']['component']['linkTag'] =
             'Vpc_Basic_LinkTag_ParentPage_Component';
         $ret['ownModel'] = 'Vpc_Basic_LinkParent_Model';
