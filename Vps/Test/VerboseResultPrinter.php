4834bad5b01c392798258f7b8e7a85f77fbb9a3a
Niko Sams <ns@vivid-planet.com>
2010-02-09 08:21:45 +0000

    fehler gleich ausgeben wenn verbose

--- Vps/Test/VerboseResultPrinter.php
+++ Vps/Test/VerboseResultPrinter.php
@@ -15,4 +15,15 @@ class Vps_Test_VerboseResultPrinter extends PHPUnit_TextUI_ResultPrinter
         echo "\n".date('Y-m-d H:i:s').': starting '.$test->toString()."\n";
         return parent::startTest($test);
     }
+
+    public function addError(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+        echo $e;
+        return parent::addError($test, $e, $time);
+    }
+    public function addFailure(PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time)
+    {
+        echo $e;
+        return parent::addFailure($test, $e, $time);
+    }
 }
