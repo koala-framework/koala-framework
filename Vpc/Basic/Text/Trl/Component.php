756e462460d434c175549236059cb81d5d9a093b
Niko Sams <ns@vivid-planet.com>
2010-02-11 08:44:46 +0000

    nur child generator entfernen, andere im web hinzugefügte lassen

--- Vpc/Basic/Text/Trl/Component.php
+++ Vpc/Basic/Text/Trl/Component.php
@@ -4,7 +4,7 @@ class Vpc_Basic_Text_Trl_Component extends Vpc_Chained_Trl_Component
     public static function getSettings($masterComponentClass)
     {
         $ret = parent::getSettings($masterComponentClass);
-        $ret['generators'] = array();
+        unset($ret['generators']['child']);
         $ret['generators']['text'] = array(
             'class' => 'Vps_Component_Generator_Static',
             'component' => $masterComponentClass,
