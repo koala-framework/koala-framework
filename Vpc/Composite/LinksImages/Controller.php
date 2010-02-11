b683c0bb5ed4dfaaa6bd1e8e40f88cedc56f9d37
Christoph Eder <ce@vivid-planet.com>
2010-02-11 09:07:36 +0000

    - wird von der composite schon erledigt

--- Vpc/Composite/LinksImages/Controller.php
+++ Vpc/Composite/LinksImages/Controller.php
@@ -1,18 +1,4 @@
 <?php
 class Vpc_Composite_LinksImages_Controller extends Vpc_Abstract_List_Controller
 {
-    protected function _initColumns()
-    {
-        $class = Vpc_Abstract::getChildComponentClass($this->_getParam('class'), 'child');
-        $classes = Vpc_Abstract::getChildComponentClasses($class, 'child');
-
-
-        $this->_columns->add(new Vps_Grid_Column('image', trlVps('Image'), 100))
-            ->setData(new Vpc_Composite_LinksImages_ImageData($classes['image'], 'gridRow'))
-            ->setRenderer('mouseoverPic');
-        $this->_columns->add(new Vps_Grid_Column('pic_large'))
-            ->setData(new Vpc_Composite_LinksImages_ImageData($classes['image'], 'gridRowLarge'));
-
-        parent::_initColumns();
-    }
 }
