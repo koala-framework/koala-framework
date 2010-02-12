339408e7b698aaa8a3a5160cbfa49400909b2850
Niko Sams <ns@vivid-planet.com>
2010-02-12 12:54:16 +0000

    mehrsprachigkeit (fast) fertig

--- tests/Vpc/Trl/Simple/Test.php
+++ tests/Vpc/Trl/Simple/Test.php
@@ -1,6 +1,21 @@
-<?
+<?php
+/**
+ * @group Vpc_Trl
+ * @group Vpc_UrlResolve
+ */
+class Vpc_Trl_Simple_Text extends Vpc_TestAbstract
+{
+    public function setUp()
+    {
+        parent::setUp('Vpc_Trl_Simple_Root');
+    }
+    public function testIt()
+    {
+        //$this->assertEquals('root-de_test', $this->_root->getPageByUrl('/de/test')->componentId);
+    }
 /*
-http://xlink.vps.niko.vivid/vps/vpctest/Vpc_Trl_Simple_Root/de/test
-http://xlink.vps.niko.vivid/vps/vpctest/Vpc_Trl_Simple_Root/en/test
+http://doleschal.vps.niko.vivid/vps/vpctest/Vpc_Trl_Simple_Root/de/test
+http://doleschal.vps.niko.vivid/vps/vpctest/Vpc_Trl_Simple_Root/en/test
 */
+}
 ?>
