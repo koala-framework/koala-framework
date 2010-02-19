13221c8227bb9179e8d42b1bda4dc2378ed7a8ee
Christoph Eder <ce@vivid-planet.com>
2010-02-19 13:42:58 +0000

    - updateScript bugfix

--- Vpc/Basic/Table/Admin.php
+++ Vpc/Basic/Table/Admin.php
@@ -8,9 +8,18 @@ class Vpc_Basic_Table_Admin extends Vpc_Admin
         $this->createFormTable('vpc_basic_table', $fields);
 
         // die hier braucht id als primary
-        $fields['pos'] = 'int NOT NULL';
-        $fields['data'] = 'text NOT NULL';
-        $this->createFormTable('vpc_basic_table_data', $fields);
+        $tablename = 'vpc_basic_table_data';
+        if (!$this->_tableExists($tablename)) {
+            $sql = "CREATE TABLE IF NOT EXISTS `$tablename` (
+              `id` int(11) NOT NULL auto_increment,
+              `component_id` varchar(255) NOT NULL,
+              `data` text NOT NULL,
+              `pos` int(11) NOT NULL,
+              PRIMARY KEY  (`id`),
+              KEY `component_id` (`component_id`)
+            )";
+            Vps_Registry::get('db')->query($sql);
+        }
     }
     public function getExtConfig()
     {
