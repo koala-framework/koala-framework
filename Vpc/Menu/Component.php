af59348bdb07b1ed09a867ef56426efcdce4c6dd
Christoph Eder <ce@vivid-planet.com>
2010-03-03 13:09:00 +0000

    - expandendMenu eingebaut

--- Vpc/Menu/Component.php
+++ Vpc/Menu/Component.php
@@ -16,7 +16,6 @@ class Vpc_Menu_Component extends Vpc_Menu_Abstract
         );
         $ret['separator'] = '';
         $ret['linkPrefix'] = '';
-        $ret['showParentPage'] = false;
         $ret['cssClass'] = 'webStandard printHidden';
         return $ret;
     }
@@ -28,21 +27,6 @@ class Vpc_Menu_Component extends Vpc_Menu_Abstract
         $ret['subMenu'] = $this->getData()->getChildComponent('-subMenu');
         $ret['separator'] = $this->_getSetting('separator');
         $ret['linkPrefix'] = $this->_getSetting('linkPrefix');
-        $ret['parentPage'] = null;
-        if ($this->_getSetting('showParentPage')) {
-            $currentPages = array_reverse($this->_getCurrentPages());
-            if (isset($this->getData()->level)) {
-                $level = $this->getData()->level;
-            } else {
-                $level = $this->_getSetting('level');
-            }
-            if (is_string($level)) {
-                throw new Vps_Exception("You can't use showParentMenu for MainMenus (what should that do?)");
-            }
-            if (isset($currentPages[$level-2])) {
-                $ret['parentPage'] = $currentPages[$level-2];
-            }
-        }
         return $ret;
     }
     public function hasContent()
