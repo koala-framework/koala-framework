719873397affdf3886f4e586057b966555502c4b
Markus Hauser <mh@vivid-planet.com>
2010-03-04 15:01:25 +0000

    trl() uebersetzung fuer mehrsprachige komponenten. tests folgen noch

--- Vpc/Abstract/Image/Trl/Component.php
+++ Vpc/Abstract/Image/Trl/Component.php
@@ -1,4 +1,10 @@
 <?php
 class Vpc_Abstract_Image_Trl_Component extends Vpc_Abstract_Composite_Trl_Component
 {
+    public function getTemplateVars()
+    {
+        $ret = parent::getTemplateVars();
+        $ret['data'] = $ret['chained'];
+        return $ret;
+    }
 }
