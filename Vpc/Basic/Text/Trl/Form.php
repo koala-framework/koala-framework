af10509c529432856691e72e4269b42121916741
Niko Sams <ns@vivid-planet.com>
2010-03-15 07:26:00 +0000

    fix Text Trl form nachdem die unterkomponente umbenannt wurde

--- Vpc/Basic/Text/Trl/Form.php
+++ Vpc/Basic/Text/Trl/Form.php
@@ -16,7 +16,7 @@ class Vpc_Basic_Text_Trl_Form extends Vpc_Abstract_Form
     protected function _initFields()
     {
         parent::_initFields();
-        $this->add(Vpc_Abstract_Form::createChildComponentForm($this->getClass(), "-text"));
+        $this->add(Vpc_Abstract_Form::createChildComponentForm($this->getClass(), "-child"));
 
         $fs = $this->add(new Vps_Form_Container_FieldSet(trlVps("Original")));
 
