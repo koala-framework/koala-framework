91409dafeba4985796beddcc053cd7c37edb45b9
Markus Hauser <mh@vivid-planet.com>
2010-03-16 10:12:36 +0000

    guestbook fixes und neuer menupunkt zum editieren oben im menu

--- Vpc/Guestbook/Admin.php
+++ Vpc/Guestbook/Admin.php
@@ -21,4 +21,22 @@ class Vpc_Guestbook_Admin extends Vpc_Directories_Item_Directory_Admin
         ));
         return array_merge($arr, $ret);
     }
+
+    public function addResources(Vps_Acl $acl)
+    {
+        parent::addResources($acl);
+        $components = Vps_Component_Data_Root::getInstance()
+                ->getComponentsBySameClass($this->_class, array('ignoreVisible'=>true));
+        $name = Vpc_Abstract::getSetting($this->_class, 'componentName');
+        $icon = Vpc_Abstract::getSetting($this->_class, 'componentIcon');
+        if (strpos($name, '.') !== false) $name = substr($name, strrpos($name, '.') + 1);
+        $c = $components[0];
+
+        if (!$acl->has('guestbook_entries')) {
+            $acl->add(new Vps_Acl_Resource_MenuUrl('guestbook_entries',
+                array('text'=>$name, 'icon'=>$icon),
+                Vpc_Admin::getInstance($c->componentClass)->getControllerUrl('Comments').'?componentId='.$c->dbId), 'vps_component_root');
+        }
+
+    }
 }
