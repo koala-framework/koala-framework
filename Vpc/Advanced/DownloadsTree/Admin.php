578e9a425903d64d42662476061bb27f83f9a5a8
Markus Hauser <mh@vivid-planet.com>
2010-03-16 10:39:36 +0000

    Advanced/DownloadsTree funktioniert jetzt wieder in seitenbaum und im menuepunkt oben

--- Vpc/Advanced/DownloadsTree/Admin.php
+++ Vpc/Advanced/DownloadsTree/Admin.php
@@ -10,7 +10,12 @@ class Vpc_Advanced_DownloadsTree_Admin extends Vpc_Admin
     public function getExtConfig()
     {
         $ret = parent::getExtConfig();
-        $ret['xtype'] = 'vpc.advanced.downloadstree';
+        $ret['customerarea'] = $ret['form'];
+        unset($ret['form']);
+        $ret['customerarea']['xtype'] = 'vpc.advanced.downloadstree';
+        $ret['customerarea']['projectsUrl'] = Vpc_Admin::getInstance($this->_class)->getControllerUrl('Projects');
+        $ret['customerarea']['projectUrl'] = Vpc_Admin::getInstance($this->_class)->getControllerUrl('Project');
+        $ret['customerarea']['downloadsUrl'] = Vpc_Admin::getInstance($this->_class)->getControllerUrl('Downloads');
         return $ret;
     }
 
