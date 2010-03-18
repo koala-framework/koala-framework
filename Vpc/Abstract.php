5fa60192c31da0d2e32c281886f0c40cf34cd16b
Stephan W. <sw@vivid-planet.com>
2010-03-18 12:28:44 +0000

    Reihenfolge Link -> Image in Vpc_Composite_LinkImage_Component umgekehrt

--- Vpc/Abstract.php
+++ Vpc/Abstract.php
@@ -167,7 +167,7 @@ abstract class Vpc_Abstract extends Vps_Component_Abstract
         $constraints = array(
             'generator' => $generator,
         );
-        if ($componentKey) $constrains['componeontKey'] = $componentKey;
+        if ($componentKey) $constrains['componentKey'] = $componentKey;
         $classes = array_values(self::getChildComponentClasses($class, $constraints));
         if (!isset($classes[0])) {
             throw new Vps_Exception("childComponentClass '$componentKey' for generator '$generator' not set for '$class'");
--- Vpc/Composite/LinkImage/Component.php
+++ Vpc/Composite/LinkImage/Component.php
@@ -7,8 +7,8 @@ class Vpc_Composite_LinkImage_Component extends Vpc_Abstract_Composite_Component
             'componentName'     => trlVps('Link Image'),
             'componentIcon'     => new Vps_Asset('image')
         ));
-        $ret['generators']['child']['component']['link'] = 'Vpc_Basic_LinkTag_Component';
         $ret['generators']['child']['component']['image'] = 'Vpc_Basic_Image_Component';
+        $ret['generators']['child']['component']['link'] = 'Vpc_Basic_LinkTag_Component';
         return $ret;
     }
 }
