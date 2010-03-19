34948fc336555dba468459bb74bac5acffcac3cc
Niko Sams <ns@vivid-planet.com>
2010-03-19 12:46:00 +0000

    link zu news bei news typ für jede news seite einen eintrag anzeigen

--- Vpc/Basic/LinkTag/Empty/Admin.php
+++ Vpc/Basic/LinkTag/Empty/Admin.php
@@ -1,5 +1,5 @@
 <?php
-class Vpc_Basic_LinkTag_Empty_Admin extends Vpc_Abstract_Admin
+class Vpc_Basic_LinkTag_Empty_Admin extends Vpc_Basic_LinkTag_Abstract_Admin
 {
     public function componentToString(Vps_Component_Data $data)
     {
--- Vpc/Basic/LinkTag/News/Form.php
+++ Vpc/Basic/LinkTag/News/Form.php
@@ -14,6 +14,14 @@ class Vpc_Basic_LinkTag_News_Form extends Vpc_Abstract_Form
             ->setStoreUrl(
                 Vpc_Admin::getInstance($class)->getControllerUrl('News').'/json-data'
             )
-            ->setListWidth(210);
+            ->setListWidth(210)
+            ->setAllowBlank(false);
+    }
+
+    public function getIsCurrentLinkTag($parentRow)
+    {
+        $row = $this->getRow($parentRow);
+        $c = Vps_Component_Data_Root::getInstance()->getComponentByDbId('news_'.$row->news_id);
+        return 'news_'.$c->parent->dbId = $this->getName();
     }
 }
