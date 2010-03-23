9e5ba43da7928e489fa9139afa66b72d1db3b69a
Niko Sams <ns@vivid-planet.com>
2010-03-23 06:36:19 +0000

    fix test

--- tests/Vpc/Trl/News/News/TestModel.php
+++ tests/Vpc/Trl/News/News/TestModel.php
@@ -5,8 +5,8 @@ class Vpc_Trl_News_News_TestModel extends Vps_Model_FnF
     {
         $config = array(
             'data'=> array(
-                array('id'=>'1', 'component_id'=>'root-master_test', 'visible'=>true, 'title'=>'lipsum', 'teaser'=>'blablub'),
-                array('id'=>'2', 'component_id'=>'root-master_test', 'visible'=>true, 'title'=>'lipsum2', 'teaser'=>'blablub2'),
+                array('id'=>'1', 'component_id'=>'root-master_test', 'visible'=>true, 'title'=>'lipsum', 'teaser'=>'blablub', 'publish_date'=>'2010-03-01'),
+                array('id'=>'2', 'component_id'=>'root-master_test', 'visible'=>true, 'title'=>'lipsum2', 'teaser'=>'blablub2', 'publish_date'=>'2010-03-01'),
             )
         );
         parent::__construct($config);
