f50e0a964282ca499b751d8a035c570626ffc7aa
Niko Sams <ns@vivid-planet.com>
2010-03-29 08:30:59 +0000

    setup entfernt, wird nicht mehr verwendet

--- Vpc/Abstract/List/Admin.php
+++ Vpc/Abstract/List/Admin.php
@@ -1,5 +1,5 @@
 <?php
-class Vpc_Abstract_List_Admin extends Vpc_Admin
+class Vpc_Abstract_List_Trl_Admin extends Vpc_Admin
 {
     public function getExtConfig()
     {
@@ -20,22 +20,4 @@ class Vpc_Abstract_List_Admin extends Vpc_Admin
             )
         );
     }
-    public function setup()
-    {
-        $class = Vpc_Abstract::getChildComponentClass($this->_class, 'child');
-        Vpc_Admin::getInstance($class)->setup();
-
-        if (!$this->_tableExists('vpc_composite_list')) {
-            $sql = 'CREATE TABLE IF NOT EXISTS `vpc_composite_list` (
-              `id` int(10) unsigned NOT NULL auto_increment,
-              `component_id` varchar(255) NOT NULL,
-              `component_class` varchar(255) NOT NULL,
-              `pos` tinyint(4) NOT NULL,
-              `visible` tinyint(4) NOT NULL,
-              PRIMARY KEY  (`id`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ;';
-            Vps_Registry::get('db')->query($sql);
-        }
-    }
-
 }
