d776f41ed703a9c7ad0622479bf6c0cefeb323e0
Niko Sams <ns@vivid-planet.com>
2010-03-30 15:33:56 +0000

    overrides in eigene datei verschoben damit die assets abhängigkeiten felxibler gemacht werden können

--- Vps_js/Vps.js
+++ Vps_js/Vps.js
@@ -84,19 +84,6 @@ Vps.clone = function(o) {
     return c;
 };
 
-//workaround für Permission denied to access property 'dom' from non-chrome context
-//siehe http://www.extjs.com/forum/showthread.php?t=74765
-//fixed in Ext 3
-if (Ext.Element) {
-Ext.Element.prototype.contains = function(el) {
-    try {
-        return !el ? false : Ext.lib.Dom.isAncestor(this.dom, el.dom ? el.dom : el);
-    } catch(e) {
-        return false;
-    }
-};
-}
-
 Ext.onReady(function()
 {
 //     Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
