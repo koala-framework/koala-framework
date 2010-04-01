d1739572830112081518821765bead97ba2dca52
Franz Unger <ufx@vivid-planet.com>
2010-04-01 07:13:30 +0000

    Upload-Dateinamenfilter durch urlencode beim Ausliefern ersetzt

--- Vps/Filter/FileUpload.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Vps_Filter_FileUpload implements Zend_Filter_Interface
-{
-    public function filter($value)
-    {
-        $value = str_replace('#', '-', $value);
-        return $value;
-    }
-}
