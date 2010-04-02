b65961007b7c905051f6194bf51939bda4ef2080
Markus Hauser <mh@vivid-planet.com>
2010-04-02 13:11:21 +0000

    show similar videos aus/einschaltbar fuer youtube community video

--- Vpc/Advanced/CommunityVideo/Component.php
+++ Vpc/Advanced/CommunityVideo/Component.php
@@ -24,6 +24,11 @@ class Vpc_Advanced_CommunityVideo_Component extends Vpc_Abstract_Flash_Component
             if ($urlParts && !empty($urlParts['host'])) {
                 if (preg_match('/youtube\.com$/i', $urlParts['host'])) {
                     $url = str_replace('/watch?v=', '/v/', $url);
+                    if ($this->getRow()->show_similar_videos) {
+                        if (strpos($url, 'rel=0') === false) {
+                            $url .= '&rel=0';
+                        }
+                    }
                 } else if (preg_match('/vimeo\.com$/i', $urlParts['host'])) {
                     $clipId = substr($urlParts['path'], 1);
                     $url = 'http://vimeo.com/moogaloop.swf?clip_id='.$clipId.'&server=vimeo.com&show_title=1&show_byline=1&show_portrait=0&color=&fullscreen=1';
@@ -34,6 +39,9 @@ class Vpc_Advanced_CommunityVideo_Component extends Vpc_Abstract_Flash_Component
         $ret['url'] = $url;
         $ret['width'] = $this->getRow()->width;
         $ret['height'] = $this->getRow()->height;
+        $ret['params'] = array(
+            'allowfullscreen' => 'true'
+        );
         return $ret;
     }
 
