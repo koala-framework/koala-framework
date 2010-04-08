04b5bd08551c834c4f7f9ce671943569e3e08679
Franz Unger <ufx@vivid-planet.com>
2010-04-08 11:44:36 +0000

    UpdateScript zum ids ändern kann jetzt auch ids überschreiben

--- Vps/Update/Action/Component/ConvertComponentIds.php
+++ Vps/Update/Action/Component/ConvertComponentIds.php
@@ -17,6 +17,7 @@ class Vps_Update_Action_Component_ConvertComponentIds extends Vps_Update_Action_
         $search = $this->search;
         $replace = $this->replace;
         $pattern = isset($this->pattern) ? $this->pattern : $search . '%';
+        $overwrite = isset($this->overwrite) && $this->overwrite;
 
         $db = Zend_Registry::get('db');
         foreach ($db->query("SHOW TABLES")->fetchAll() as $table) {
@@ -27,9 +28,18 @@ class Vps_Update_Action_Component_ConvertComponentIds extends Vps_Update_Action_
                 if ($field['Field'] == 'component_id') $hasComponentId = true;
             }
             if ($hasComponentId) {
+                $dbPattern = str_replace('_', '\_', $pattern);
+                if ($overwrite) {
+                    $sql = "(SELECT REPLACE(component_id, '$search', '$replace')
+                            FROM $table WHERE component_id LIKE '$dbPattern')";
+                    $ids = $db->fetchCol($sql);
+                    $sql = "DELETE FROM $table
+                        WHERE component_id IN ('" . implode("', '", $ids) . "')";
+                    $db->query($sql);
+                }
                 $db->query("UPDATE $table SET component_id =
                         REPLACE(component_id, '$search', '$replace')
-                        WHERE component_id LIKE '".str_replace('_', '\_', $pattern)."'");
+                        WHERE component_id LIKE '$dbPattern'");
             }
         }
         $db->query("UPDATE vpc_basic_text SET content =
