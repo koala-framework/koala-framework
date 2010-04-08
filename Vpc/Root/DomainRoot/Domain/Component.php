006e391001df915cbeaec9ecce2fbb19bf786e74
Niko Sams <ns@vivid-planet.com>
2010-04-08 07:44:43 +0000

    DomainRoot CategoryGenerator generalisiert, es ist jetzt kein eigener CategoryGenerator mehr nötig. Dieser funktioniert auch bei mehrsprachigken webs

--- Vpc/Root/DomainRoot/Domain/Component.php
+++ Vpc/Root/DomainRoot/Domain/Component.php
@@ -5,7 +5,7 @@ class Vpc_Root_DomainRoot_Domain_Component extends Vpc_Abstract
     {
         $ret = parent::getSettings();
         $ret['generators']['category'] = array(
-            'class' => 'Vpc_Root_DomainRoot_Domain_CategoryGenerator',
+            'class' => 'Vpc_Root_CategoryGenerator',
             'component' => 'Vpc_Root_Category_Component',
             'model' => 'Vpc_Root_CategoryModel'
         );
