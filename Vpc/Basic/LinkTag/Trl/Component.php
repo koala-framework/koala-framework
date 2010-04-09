35efd50ae8c126fbd10f16153740c72f6b202cc8
Franz Unger <ufx@vivid-planet.com>
2010-04-09 11:13:54 +0000

    Trl: LinkTag-Komponente hat Link-Data nicht verwendet

--- Vpc/Basic/LinkTag/Trl/Component.php
+++ Vpc/Basic/LinkTag/Trl/Component.php
@@ -4,6 +4,7 @@ class Vpc_Basic_LinkTag_Trl_Component extends Vpc_Chained_Trl_Component
     public static function getSettings($masterComponent)
     {
         $ret = parent::getSettings($masterComponent);
+        $ret['dataClass'] = 'Vpc_Basic_LinkTag_Data';
         return $ret;
     }
 
