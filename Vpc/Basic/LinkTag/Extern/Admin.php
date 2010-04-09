f433aa68d54f2183ea242156b441d9c9032d810e
Niko Sams <ns@vivid-planet.com>
2010-04-09 07:44:21 +0000

    setup gibts nicht mehr -> entfernt

--- Vpc/Basic/LinkTag/Extern/Admin.php
+++ Vpc/Basic/LinkTag/Extern/Admin.php
@@ -1,20 +1,4 @@
 <?php
 class Vpc_Basic_LinkTag_Extern_Admin extends Vpc_Basic_LinkTag_Abstract_Admin
 {
-    public function setup()
-    {
-        $fields['target']       = "varchar(255) NOT NULL";
-        $fields['rel']          = "varchar(255) DEFAULT NULL";
-        $fields['param']        = "varchar(255) DEFAULT NULL";
-        $fields['open_type']    = "enum('self','popup','blank') NOT NULL DEFAULT 'self'";
-        $fields['width']        = "mediumint DEFAULT NULL";
-        $fields['height']       = "mediumint DEFAULT NULL";
-        $fields['menubar']      = "tinyint(4) NOT NULL";
-        $fields['toolbar']      = "tinyint(4) NOT NULL";
-        $fields['locationbar']  = "tinyint(4) NOT NULL";
-        $fields['statusbar']    = "tinyint(4) NOT NULL";
-        $fields['scrollbars']   = "tinyint(4) NOT NULL";
-        $fields['resizable']    = "tinyint(4) NOT NULL";
-        $this->createFormTable('vpc_basic_link_extern', $fields);
-    }
 }
--- Vpc/Basic/LinkTag/Intern/Admin.php
+++ Vpc/Basic/LinkTag/Intern/Admin.php
@@ -24,12 +24,6 @@ class Vpc_Basic_LinkTag_Intern_Admin extends Vpc_Basic_LinkTag_Abstract_Admin
         return array();
     }
 
-    public function setup()
-    {
-        $fields['target']   = "varchar(255) NOT NULL";
-        $this->createFormTable('vpc_basic_link_intern', $fields);
-    }
-
     public function componentToString(Vps_Component_Data $data)
     {
         if (!$data->getLinkedData()) return '';
