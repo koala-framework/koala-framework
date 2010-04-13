dedbd9f807ec4119d685bfd53a39f890ede4cd42
Niko Sams <ns@vivid-planet.com>
2010-04-13 07:41:49 +0000

    standard associateTag

--- Vpc/Advanced/Amazon/Product/Component.php
+++ Vpc/Advanced/Amazon/Product/Component.php
@@ -6,14 +6,19 @@ class Vpc_Advanced_Amazon_Product_Component extends Vpc_Abstract
         $ret = parent::getSettings();
         $ret['componentName'] = trlVps('Amazon.Product');
         $ret['ownModel'] = 'Vps_Component_FieldModel';
+
+        $ret['associateTag'] = 'vps-21';
         return $ret;
     }
 
     public function getTemplateVars()
     {
         $ret = parent::getTemplateVars();
+        $select = new Vps_Model_Select();
+        $select->whereEquals('asin', $this->getRow()->asin);
+        $select->whereEquals('AssociateTag', $this->_getSetting('associateTag'));
         $ret['product'] = Vps_Model_Abstract::getInstance('Vps_Util_Model_Amazon_Products')
-            ->getRow($this->getRow()->asin);
+            ->getRow($select);
         return $ret;
     }
 
