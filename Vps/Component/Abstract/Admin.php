fbd229218b196d6e9fa08e9bdc5dea1cc8ba4850
Niko Sams <ns@vivid-planet.com>
2010-04-19 15:14:19 +0000

    andere exceptions auch abfangen um bessere fehlermeldung zu haben

--- Vps/Component/Abstract/Admin.php
+++ Vps/Component/Abstract/Admin.php
@@ -193,6 +193,8 @@ class Vps_Component_Abstract_Admin
             return $data->getComponent()->getRow()->__toString();
         } catch (Zend_Db_Table_Row_Exception $e) {
             throw new Vps_Exception("__toString failed for component ".$data->componentClass.' you might want to set _toStringField or override componentToString');
+        } catch (Vps_Exception $e) {
+            throw new Vps_Exception("__toString failed for component ".$data->componentClass.' you might want to set _toStringField or override componentToString');
         }
     }
 
