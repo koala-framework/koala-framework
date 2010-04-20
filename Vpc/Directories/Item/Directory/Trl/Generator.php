114510de2f8f0ff71fe8f137bdff065ed80a2e75
Niko Sams <ns@vivid-planet.com>
2010-04-20 13:55:33 +0000

    generator geht auch ohne eigenem model im trl

--- Vpc/Directories/Item/Directory/Trl/Generator.php
+++ Vpc/Directories/Item/Directory/Trl/Generator.php
@@ -12,7 +12,7 @@ class Vpc_Directories_Item_Directory_Trl_Generator extends Vpc_Chained_Trl_Gener
         }
         $m = Vpc_Abstract::createChildModel($this->_class);
         $ret = parent::_getChainedChildComponents($parentData, $select);
-        if ($select->getPart(Vps_Component_Select::IGNORE_VISIBLE) !== true) {
+        if ($m && $select->getPart(Vps_Component_Select::IGNORE_VISIBLE) !== true) {
             foreach ($ret as $k=>$c) {
                 $r = $m->getRow($parentData->dbId.$this->getIdSeparator().$this->_getIdFromRow($c));
                 if (!$r || !$r->visible) {
@@ -33,18 +33,27 @@ class Vpc_Directories_Item_Directory_Trl_Generator extends Vpc_Chained_Trl_Gener
     {
         $ret = parent::_formatConfig($parentData, $row);
         $m = Vpc_Abstract::createChildModel($this->_class);
-        $id = $parentData->dbId.$this->getIdSeparator().$this->_getIdFromRow($row);
-        $ret['row'] = $m->getRow($id);
-        if (!$ret['row']) {
-            $ret['row'] = $m->createRow();
-            $ret['row']->component_id = $id;
+        if ($m) {
+            $id = $parentData->dbId.$this->getIdSeparator().$this->_getIdFromRow($row);
+            $ret['row'] = $m->getRow($id);
+            if (!$ret['row']) {
+                $ret['row'] = $m->createRow();
+                $ret['row']->component_id = $id;
+            }
+        } else {
+            $ret['row'] = $ret['chained']->row;
         }
 
         //TODO: nicht mit settings direkt arbeiten, besser das echte generator objekt holen
         $masterCC = Vpc_Abstract::getSetting($this->_class, 'masterComponentClass');
         $masterGen = Vpc_Abstract::getSetting($masterCC, 'generators');
         $detailGen = $masterGen['detail'];
-        $ret['name'] = $ret['row']->{$detailGen['nameColumn']};
+        if (isset($detailGen['nameColumn'])) {
+            $ret['name'] = $ret['row']->{$detailGen['nameColumn']};
+        } else {
+            $ret['name'] = $ret['chained']->name;
+        }
+
         if (isset($detailGen['filenameColumn'])) {
             $fn = $ret['row']->{$detailGen['filenameColumn']};
         } else {
