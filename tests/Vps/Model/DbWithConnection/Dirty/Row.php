9c0d6df558e69e8c3ca44bf341fe4f6c0dc07f43
Markus Hauser <mh@vivid-planet.com>
2010-04-22 13:16:55 +0000

    klassenname(n) korrigiert

--- tests/Vps/Model/DbWithConnection/Dirty/Row.php
+++ tests/Vps/Model/DbWithConnection/Dirty/Row.php
@@ -1,15 +1,15 @@
 <?php
-class Vps_Model_DbWithConnection_Row extends Vps_Db_Table_Row
+class Vps_Model_DbWithConnection_Dirty_Row extends Vps_Db_Table_Row
 {
     public static $saveCount = 0;
 
     public static function resetMock()
     {
-        Vps_Model_DbWithConnection_Row::$saveCount = 0;
+        self::$saveCount = 0;
     }
 
     public function save()
     {
-        Vps_Model_DbWithConnection_Row::$saveCount += 1;
+        self::$saveCount += 1;
     }
 }
