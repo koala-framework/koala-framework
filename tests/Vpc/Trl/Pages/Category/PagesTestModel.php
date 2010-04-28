c47b1a9e4e50345ca6f5e9ac21c33187db2d693e
Niko Sams <ns@vivid-planet.com>
2010-04-28 10:01:17 +0000

    test der testet ob seiten die im master visible sind am slave auch gehen

--- tests/Vpc/Trl/Pages/Category/PagesTestModel.php
+++ tests/Vpc/Trl/Pages/Category/PagesTestModel.php
@@ -6,5 +6,7 @@ class Vpc_Trl_Pages_Category_PagesTestModel extends Vps_Model_FnF
                   'parent_id'=>'root-master-main', 'component'=>'test', 'is_home'=>true, 'hide'=>false),
             array('id'=>2, 'pos'=>1, 'visible'=>true, 'name'=>'Test', 'filename' => 'test',
                   'parent_id'=>1, 'component'=>'test', 'is_home'=>false, 'hide'=>false),
+            array('id'=>3, 'pos'=>2, 'visible'=>false, 'name'=>'Test2', 'filename' => 'test2',
+                  'parent_id'=>1, 'component'=>'test', 'is_home'=>false, 'hide'=>false),
     );
 }
