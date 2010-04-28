1eabeb6b9715712ef936f27193ad0a591ddf6a0a
Niko Sams <ns@vivid-planet.com>
2010-04-28 14:47:33 +0000

    merged select objekt derm parent mitübergeben

--- Vps/Model/Select/Expr/Child/Sum.php
+++ Vps/Model/Select/Expr/Child/Sum.php
@@ -1,8 +1,8 @@
 <?php
 class Vps_Model_Select_Expr_Child_Sum extends Vps_Model_Select_Expr_Child
 {
-    public function __construct($child, $field)
+    public function __construct($child, $field, Vps_Model_Select $select=null)
     {
-        parent::__construct($child, new Vps_Model_Select_Expr_Sum($field));
+        parent::__construct($child, new Vps_Model_Select_Expr_Sum($field), $select);
     }
 }
