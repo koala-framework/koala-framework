a8fecabbfcc9157231e34bade6bdb0aa6efd832e
Markus Hauser <mh@vivid-planet.com>
2010-05-07 15:40:00 +0200

    ListChildPages und ListSwitch engültig gelöscht, sind jetzt in List/ unterordner.

--- Vpc/ListChildPages/Teaser/Trl/Component.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @deprecated
- * @since 04.05.2010
- * @see Vpc_List_ChildPages_Teaser_Trl_Component
- */
-class Vpc_ListChildPages_Teaser_Trl_Component extends Vpc_List_ChildPages_Teaser_Trl_Component
-{
-}
