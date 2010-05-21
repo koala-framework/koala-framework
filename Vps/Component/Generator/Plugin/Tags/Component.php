5ec92fa59c1edc927fb1d6b396e7ad866425e165
Niko Sams <ns@vivid-planet.com>
2010-05-21 14:36:55 +0200

    erstes Generator Plugin: Tags

--- Vps/Component/Generator/Plugin/Tags/Component.php
+++ Vps/Component/Generator/Plugin/Tags/Component.php
@@ -1,4 +1,11 @@
 <?php
 class Vps_Component_Generator_Plugin_Tags_Component extends Vps_Component_Generator_Plugin_Abstract
 {
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['componentName'] = trlVps('Tags');
+        $ret['assetsAdmin']['dep'][] = 'VpsAutoAssignGrid';
+        return $ret;
+    }
 }
