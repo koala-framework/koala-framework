92d3926aaa6e8373e1b4e781416e8b0dd096995b
Niko Sams <ns@vivid-planet.com>
2010-05-21 14:36:55 +0200

    erstes Generator Plugin: Tags

---
+++ Vps/Component/Generator/Plugin/Tags/Controller.php
@@ -0,0 +1,21 @@
+<?php
+class Vps_Component_Generator_Plugin_Tags_Controller extends Vps_Controller_Action_Auto_AssignGrid
+{
+    protected $_modelName = 'Vps_Component_Generator_Plugin_Tags_ComponentsToTagsModel';
+    protected $_assignFromReference = 'Tag';
+
+    protected function _createAssignRow($id)
+    {
+        $row = $this->_getModel()->createRow();
+        $row->tag_id = $id;
+        $row->component_id = $this->_getParam('componentId');
+        return $row;
+    }
+
+    
+    protected function _initColumns()
+    {
+        parent::_initColumns();
+        $this->_columns->add(new Vps_Grid_Column('tag_text', trlVps('Tag')));
+    }
+}
