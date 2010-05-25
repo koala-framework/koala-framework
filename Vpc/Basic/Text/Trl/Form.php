1e50f98f92f93225649e36979021c661d6b93425
Niko Sams <ns@vivid-planet.com>
2010-05-25 11:28:23 +0200

    Text Uebersetzung: Inhalt uebernehmen Button

--- Vpc/Basic/Text/Trl/Form.php
+++ Vpc/Basic/Text/Trl/Form.php
@@ -4,9 +4,13 @@ class Vpc_Basic_Text_Trl_Form extends Vpc_Abstract_Form
     protected function _initFields()
     {
         parent::_initFields();
-        $this->add(Vpc_Abstract_Form::createChildComponentForm($this->getClass(), "-child"));
+        $this->add(Vpc_Abstract_Form::createChildComponentForm($this->getClass(), "-child", 'child'));
 
         $fs = $this->add(new Vps_Form_Container_FieldSet(trlVps("Original")));
+
+        $fs->add(new Vps_Form_Field_Panel('copy'))
+            ->setHideLabel(true)
+            ->setXtype('vpc.basic.text.trl.copybutton');
 
         $fs->add(new Vps_Form_Field_ShowField('content'))
             ->setHideLabel(true)
