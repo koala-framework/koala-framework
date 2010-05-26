f04b981b0ea7d4c8db5e349ec6b5671474fba85f
Niko Sams <ns@vivid-planet.com>
2010-05-26 14:26:02 +0200

    hilfsfunktionen zum tags auslesen

--- Vps/Component/Generator/Plugin/Tags/Component.php
+++ Vps/Component/Generator/Plugin/Tags/Component.php
@@ -8,4 +8,36 @@ class Vps_Component_Generator_Plugin_Tags_Component extends Vps_Component_Genera
         $ret['assetsAdmin']['dep'][] = 'VpsAutoAssignGrid';
         return $ret;
     }
+
+    public function getTags(Vps_Component_Data $data)
+    {
+        if ($data->generator != $this->_generator) {
+            throw new Vps_Exception("invalid data, must be from same generator as the generator plugin");
+        }
+        $m = Vps_Model_Abstract::getInstance('Vps_Component_Generator_Plugin_Tags_ComponentsToTagsModel');
+        $s = $m->select()
+            ->whereEquals('component_id', $data->dbId);
+        $ret = array();
+        foreach ($m->getRows($s) as $row) {
+            $ret[] = $row->getParentRow('Tag');
+        }
+        return $ret;
+    }
+
+    public function getComponentsWithSameTags(Vps_Component_Data $data)
+    {
+        $ret = array();
+        $ids = array();
+        foreach ($this->getTags($data) as $tag) {
+            foreach ($tag->getChildRows('ComponentsToTags') as $row) {
+                $d = Vps_Component_Data_Root::getInstance()
+                    ->getComponentByDbId($row->component_id);
+                if ($d && $d->componentId != $data->componentId && !in_array($d->componentId, $ids)) {
+                    $ids[] = $d->componentId;
+                    $ret[] = $d;
+                }
+            }
+        }
+        return $ret;
+    }
 }
