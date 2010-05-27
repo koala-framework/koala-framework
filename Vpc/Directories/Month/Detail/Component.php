844cd3c5933534058d25414932c3a3d6f98426a2
Franz Unger <ufx@vivid-planet.com>
2010-05-27 14:39:46 +0200

    News und Events nach Jahre gruppiert

--- Vpc/Directories/Month/Detail/Component.php
+++ Vpc/Directories/Month/Detail/Component.php
@@ -12,6 +12,12 @@ class Vpc_Directories_Month_Detail_Component extends Vpc_Directories_List_Compon
     {
         $select = parent::getSelect();
         $dateColumn = Vpc_Abstract::getSetting($this->getData()->parent->componentClass, 'dateColumn');
+        $select = $this->_getDateSelect($select, $dateColumn);
+        return $select;
+    }
+
+    protected function _getDateSelect($select, $dateColumn)
+    {
         $monthDate = substr($this->getData()->row->$dateColumn, 0, 7);
         $select->where($dateColumn.' >= ?', "$monthDate-01 00:00:00");
         $select->where($dateColumn.' <= ?', "$monthDate-31 23:59:59");
