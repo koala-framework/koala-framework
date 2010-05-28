0f23a3f474d0818d7db7d865bfd65da24fdd9105
Markus Hauser <mh@vivid-planet.com>
2010-05-28 11:01:58 +0200

    Columns_Trl test fix

--- tests/Vpc/Trl/Columns/Columns/ColumnsModel.php
+++ tests/Vpc/Trl/Columns/Columns/ColumnsModel.php
@@ -1,19 +1,26 @@
 <?php
-class Vpc_Trl_Columns_Columns_ColumnsModel extends Vpc_Columns_ColumnsModel
+class Vpc_Trl_Columns_Columns_ColumnsModel extends Vpc_Abstract_List_Model
 {
     public function __construct($config = array())
     {
         $this->_referenceMap['Component']['refModelClass'] = 'Vpc_Trl_Columns_Columns_Model';
 
         $config['proxyModel'] = new Vps_Model_FnF(array(
-                'columns' => array('id', 'component_id', 'width'),
-                'primaryKey' => 'id',
-                'data'=> array(
-                    array('id' => 1, 'component_id'=>'root-master_test', 'width'=>'100'),
-                    array('id' => 2, 'component_id'=>'root-master_test', 'width'=>'100'),
-                    array('id' => 3, 'component_id'=>'root-master_test', 'width'=>'50'),
-                )
-            ));
+            'columns' => array('id', 'component_id', 'pos', 'visible', 'data'),
+            'primaryKey' => 'id',
+            'siblingModels' => array(
+                new Vps_Model_Field(array('fieldName'=>'data'))
+            )
+        ));
+        $config['proxyModel']
+            ->createRow(array('id' => 1, 'component_id'=>'root-master_test', 'pos' => 1, 'visible' => 1, 'width'=>'100'))
+            ->save();
+        $config['proxyModel']
+            ->createRow(array('id' => 2, 'component_id'=>'root-master_test', 'pos' => 1, 'visible' => 1, 'width'=>'100'))
+            ->save();
+        $config['proxyModel']
+            ->createRow(array('id' => 3, 'component_id'=>'root-master_test', 'pos' => 1, 'visible' => 1, 'width'=>'50'))
+            ->save();
         parent::__construct($config);
     }
 }
