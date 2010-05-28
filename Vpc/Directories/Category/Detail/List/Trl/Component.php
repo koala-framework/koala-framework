6dec3d40917fb17879bc8e38a61b5ec4c6f3317f
Niko Sams <ns@vivid-planet.com>
2010-05-28 15:00:14 +0200

    Auch wie im master nur die eine Kategorie anzeigen

--- Vpc/Directories/Category/Detail/List/Trl/Component.php
+++ Vpc/Directories/Category/Detail/List/Trl/Component.php
@@ -6,4 +6,13 @@ class Vpc_Directories_Category_Detail_List_Trl_Component extends Vpc_Directories
         $ret = parent::getTemplateVars();
         return $ret;
     }
+
+    public function getSelect()
+    {
+        $select = parent::getSelect();
+        $s = new Vps_Model_Select();
+        $s->whereEquals('category_id', $this->getData()->parent->id);
+        $select->where(new Vps_Model_Select_Expr_Child_Contains('Categories', $s));
+        return $select;
+    }
 }
