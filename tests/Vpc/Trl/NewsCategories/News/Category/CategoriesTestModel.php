cc83ae826b348889ca8e4062d545d6ca92ce3ce2
Niko Sams <ns@vivid-planet.com>
2010-05-28 09:44:13 +0200

    test an neues model angepasst

--- tests/Vpc/Trl/NewsCategories/News/Category/CategoriesTestModel.php
+++ tests/Vpc/Trl/NewsCategories/News/Category/CategoriesTestModel.php
@@ -5,8 +5,8 @@ class Vpc_Trl_NewsCategories_News_Category_CategoriesTestModel extends Vps_Model
     public function __construct()
     {
         $data = array(
-            array('id'=>'1', 'name'=>'Lorem'),
-            array('id'=>'2', 'name'=>'Ipsum'),
+            array('id'=>'1', 'name'=>'Lorem', 'component_id' => 'root-master_test-categories', 'visible'=>1),
+            array('id'=>'2', 'name'=>'Ipsum', 'component_id' => 'root-master_test-categories', 'visible'=>1),
         );
         $config = array(
             'data' => $data
