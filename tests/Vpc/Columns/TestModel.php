62e2c7f85b78e38174e7d4932cbfb00636ae837f
Markus Hauser <mh@vivid-planet.com>
2010-05-31 12:52:45 +0200

    Vpc_Columns test bugfix

--- tests/Vpc/Columns/TestModel.php
+++ tests/Vpc/Columns/TestModel.php
@@ -1,14 +1,14 @@
 <?php
-class Vpc_Columns_TestModel extends Vpc_Columns_Model
+class Vpc_Columns_TestModel extends Vpc_Abstract_List_OwnModel
 {
     public function __construct($config = array())
     {
-        $this->_dependentModels['Columns'] = 'Vpc_Columns_TestColumnsModel';
+        $this->_dependentModels['Children'] = 'Vpc_Columns_TestColumnsModel';
 
         $config['proxyModel'] = new Vps_Model_FnF(array(
                 'primaryKey' => 'component_id',
                 'data'=> array(
-                    array('component_id'=>3000, 'data'=>''),
+                    array('component_id'=>'3000', 'data'=>''),
                 )
             ));
         parent::__construct($config);
