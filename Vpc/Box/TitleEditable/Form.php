38af97334c47a04311c8c984699679bdaf23f676
Niko Sams <ns@vivid-planet.com>
2010-06-01 15:40:37 +0200

    Backport: Bearbeitbarer titel

---
+++ Vpc/Box/TitleEditable/Form.php
@@ -0,0 +1,10 @@
+<?php
+class Vpc_Box_TitleEditable_Form extends Vpc_Abstract_Form
+{
+    protected function _initFields()
+    {
+        parent::_initFields();
+        $this->add(new Vps_Form_Field_TextField('title', trlVps('Title')))
+            ->setWidth(400);
+    }
+}
