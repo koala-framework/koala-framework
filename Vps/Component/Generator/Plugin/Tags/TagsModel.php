fd6933d2742c0056c379006534b2f368683cde78
Niko Sams <ns@vivid-planet.com>
2010-06-01 08:21:37 +0200

    toSTringField gesetzt

--- Vps/Component/Generator/Plugin/Tags/TagsModel.php
+++ Vps/Component/Generator/Plugin/Tags/TagsModel.php
@@ -5,4 +5,5 @@ class Vps_Component_Generator_Plugin_Tags_TagsModel extends Vps_Model_Db
     protected $_dependentModels = array(
         'ComponentsToTags' => 'Vps_Component_Generator_Plugin_Tags_ComponentsToTagsModel'
     );
+    protected $_toStringField = 'text';
 }
