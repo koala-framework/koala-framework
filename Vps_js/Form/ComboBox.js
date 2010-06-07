0d3072ee4f7caf2d961fd8741ab81cface6bb861
Markus Hauser <mh@vivid-planet.com>
2010-06-07 13:57:27 +0200

    fix fuer combobox wenn value 0 ist

--- Vps_js/Form/ComboBox.js
+++ Vps_js/Form/ComboBox.js
@@ -152,7 +152,7 @@ Vps.Form.ComboBox = Ext.extend(Ext.form.ComboBox,
 
     setValue : function(v)
     {
-        if (v == '') v = null;
+        if (v === '') v = null;
         if (v == this.emptyText) v = null;
         if (v && this.store.proxy && this.valueField && this.mode == 'remote') {
             //wenn proxy vorhanden können daten nachgeladen werden
