182071ebf92b45b27e4b825764a29afbaa749e83
Franz Unger <ufx@vivid-planet.com>
2010-06-07 08:35:56 +0200

    Nochmal CLI-Controller umbenannt

--- Vps/Controller/Action/Cli/Web/ScpVpsController.php
+++ Vps/Controller/Action/Cli/Web/ScpVpsController.php
@@ -1,5 +1,5 @@
 <?php
-class Vps_Controller_Action_Cli_ScpVpsController extends Vps_Controller_Action_Cli_Abstract
+class Vps_Controller_Action_Cli_Web_ScpVpsController extends Vps_Controller_Action_Cli_Abstract
 {
     public static function getHelp()
     {
