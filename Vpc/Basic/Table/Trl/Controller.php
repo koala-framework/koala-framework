b917f79d7f6e8d9d2b471b688282a1df386419ce
Franz Unger <ufx@vivid-planet.com>
2010-06-10 14:29:42 +0200

    Uebersetzung Table-Komponente neu

--- Vpc/Basic/Table/Trl/Controller.php
+++ Vpc/Basic/Table/Trl/Controller.php
@@ -1,20 +1,32 @@
 <?php
-class Vpc_Basic_Table_Trl_Controller extends Vpc_Basic_Table_Controller
+class Vpc_Basic_Table_Trl_Controller extends Vps_Controller_Action_Auto_Vpc_Grid
 {
-    protected function _getClass()
+    protected $_buttons = array('save');
+    protected $_defaultOrder = 'pos';
+
+    public function preDispatch()
     {
-        return $this->_getChainedComponent()->componentClass;
+        $this->setModel($this->_getComponent()->getComponent()->getChildModel());
+        parent::preDispatch();
     }
 
-    protected function _getComponentId()
+    protected function _beforeSave($row)
     {
-        return $this->_getChainedComponent()->componentId;
     }
 
-    private function _getChainedComponent()
+    protected function _initColumns()
+    {
+        $this->_columns->add(new Vps_Grid_Column('pos'));
+        for ($i = 1; $i <= $this->_getComponent()->chained->getComponent()->getColumnCount(); $i++) {
+            $this->_columns->add(new Vps_Grid_Column("column$i", trlVps('Column {0}', $i), 150))
+                ->setEditor(new Vps_Form_Field_TextField());
+        }
+        $this->_columns->add(new Vps_Grid_Column_Visible());
+    }
+
+    private function _getComponent()
     {
         return Vps_Component_Data_Root::getInstance()
-            ->getComponentById($this->_getParam('componentId'), array('ignoreVisible' => true))
-            ->chained;
+            ->getComponentById($this->_getParam('componentId'), array('ignoreVisible' => true));
     }
 }
---
+++ Vpc/Basic/Table/Trl/Update/1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `vpc_basic_table_data_trl` (
+  `id` int(11) NOT NULL,
+  `data` text NOT NULL,
+  `visible` tinyint(4) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ;
