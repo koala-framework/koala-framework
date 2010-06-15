73f159bea70af3c87526850659e3206a639bd373
Niko Sams <ns@vivid-planet.com>
2010-06-15 10:59:43 +0200

    Fixes fuer neuen cli controllern

--- Vps/Controller/Action/Cli/Web/SvnUpController.php
+++ Vps/Controller/Action/Cli/Web/SvnUpController.php
@@ -58,7 +58,7 @@ class Vps_Controller_Action_Cli_Web_SvnUpController extends Vps_Controller_Actio
             exit;
 
         } else {
-            $this->_forward('update', 'git', 'vps_controller_action_cli');
+            $this->_forward('update', 'git', 'vps_controller_action_cli_web');
         }
     }
 
