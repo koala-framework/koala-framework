e497bc8adcf516c62412bd719a6b1d66dbcc2770
Niko Sams <ns@vivid-planet.com>
2010-06-15 08:47:30 +0200

    Position auch in Übersetztem Paragraph anzeigen

--- Vpc/Paragraphs/Trl/Controller.php
+++ Vpc/Paragraphs/Trl/Controller.php
@@ -47,6 +47,7 @@ class Vpc_Paragraphs_Trl_Controller extends Vps_Controller_Action_Auto_Vpc_Grid
         $this->_columns->add(new Vps_Grid_Column('component_class'));
         $this->_columns->add(new Vps_Grid_Column('component_name'));
         $this->_columns->add(new Vps_Grid_Column('component_icon'));
+        $this->_columns->add(new Vps_Grid_Column('pos'));
 
         $this->_columns->add(new Vps_Grid_Column('preview'))
             ->setData(new Vps_Data_Vpc_Frontend($this->_getParam('class')))
