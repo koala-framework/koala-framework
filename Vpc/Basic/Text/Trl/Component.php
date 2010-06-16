5141f2bb5a118ca40ab136a4c47b898af2c8d562
Niko Sams <ns@vivid-planet.com>
2010-06-16 14:13:39 +0200

    editComponents geleert da Text einen eigenen Controller hat

--- Vpc/Basic/Text/Trl/Component.php
+++ Vpc/Basic/Text/Trl/Component.php
@@ -5,6 +5,7 @@ class Vpc_Basic_Text_Trl_Component extends Vpc_Chained_Trl_MasterAsChild_Compone
     {
         $ret = parent::getSettings($masterComponentClass);
         $ret['assetsAdmin']['files'][] = 'vps/Vpc/Basic/Text/Trl/CopyButton.js';
+        $ret['editComponents'] = array();
         return $ret;
     }
 }
