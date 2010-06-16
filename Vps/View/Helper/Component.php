be5d8e189507b89d2e2729525d599c3814fc2a9d
Franz Unger <ufx@vivid-planet.com>
2010-06-16 11:37:17 +0200

    Component-Helper greift auf Output_Component zu

--- Vps/View/Helper/Component.php
+++ Vps/View/Helper/Component.php
@@ -4,9 +4,11 @@ class Vps_View_Helper_Component
     public function component(Vps_Component_Data $component = null)
     {
         if (!$component) return '';
-        $plugins = $component->getPlugins('Vps_Component_Plugin_Interface_View');
-        $plugins = implode(' ', $plugins);
-        if ($plugins) $plugins = ' '.$plugins;
-        return "{nocache: {$component->componentClass} {$component->componentId} {$plugins}}";
+
+        if (isset($component->box) && $component->box) {
+            return Vps_Component_Output_Box::getHelperOutput($component);
+        } else {
+            return Vps_Component_Output_Component::getHelperOutput($component);
+        }
     }
 }
