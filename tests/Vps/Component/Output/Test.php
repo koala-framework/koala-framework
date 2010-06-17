86fc6cda63daf4cd380f87b74f65a8d2a1d387ec
Franz Unger <ufx@vivid-planet.com>
2010-06-17 12:07:26 +0200

    Jetzt funktionieren Partials

--- tests/Vps/Component/Output/Test.php
+++ tests/Vps/Component/Output/Test.php
@@ -8,15 +8,15 @@ class Vps_Component_Output_Test extends PHPUnit_Framework_TestCase
     {
         Vps_Component_Data_Root::setComponentClass('Vps_Component_Output_C3_Root_Component');
         $root = Vps_Component_Data_Root::getInstance();
-        $output = new Vps_Component_View();
+        $view = new Vps_Component_View();
 
-        $value = $output->renderMaster($root);
+        $value = $view->renderMaster($root);
         $this->assertEquals('c3_rootmaster c1_box c3_root', $value);
 
-        $value = $output->renderMaster($root->getChildComponent('_childpage'));
+        $value = $view->renderMaster($root->getChildComponent('_childpage'));
         $this->assertEquals('c3_rootmaster c3_box c3_childpagemaster c3_childpage', $value);
 
-        $value = $output->renderMaster($root->getChildComponent('_childpage')->getChildComponent('_childpage'));
+        $value = $view->renderMaster($root->getChildComponent('_childpage')->getChildComponent('_childpage'));
         $this->assertEquals('c3_rootmaster c3_box c3_childpagemaster c3_childpage2', $value);
     }
 
@@ -24,15 +24,15 @@ class Vps_Component_Output_Test extends PHPUnit_Framework_TestCase
     {
         Vps_Component_Data_Root::setComponentClass('Vps_Component_Output_C1_Root_Component');
         $root = Vps_Component_Data_Root::getInstance();
-        $output = new Vps_Component_View();
+        $view = new Vps_Component_View();
 
-        $value = $output->render($root->getChildComponent('-child'));
+        $value = $view->render($root->getChildComponent('-child'));
         $this->assertEquals('c1_childmaster c1_child c1_childchild', $value);
 
-        $value = $output->renderMaster($root);
+        $value = $view->renderMaster($root);
         $this->assertEquals('c1_rootmaster c1_box c1_root plugin(plugin(c1_childmaster c1_child c1_childchild))', $value);
 
-        $value = $output->render($root);
+        $value = $view->render($root);
         $this->assertEquals('c1_root plugin(plugin(c1_childmaster c1_child c1_childchild))', $value);
     }
 
@@ -40,18 +40,18 @@ class Vps_Component_Output_Test extends PHPUnit_Framework_TestCase
     {
         Vps_Component_Data_Root::setComponentClass('Vps_Component_Output_C3_Root_Component');
         $root = Vps_Component_Data_Root::getInstance();
-        $output = new Vps_Component_View();
+        $view = new Vps_Component_View();
 
-        $value = $output->renderMaster($root);
+        $value = $view->renderMaster($root);
         $this->assertEquals('c3_rootmaster c1_box c3_root', $value);
 
-        $value = $output->renderMaster($root->getChildComponent('_childpage'));
+        $value = $view->renderMaster($root->getChildComponent('_childpage'));
         $this->assertEquals('c3_rootmaster c3_box c3_childpagemaster c3_childpage', $value);
 
-        $value = $output->render($root->getChildComponent('_childpage'));
+        $value = $view->render($root->getChildComponent('_childpage'));
         $this->assertEquals('c3_childpagemaster c3_childpage', $value);
 
-        $value = $output->renderMaster($root->getChildComponent('_childpage')->getChildComponent('_childpage'));
+        $value = $view->renderMaster($root->getChildComponent('_childpage')->getChildComponent('_childpage'));
         $this->assertEquals('c3_rootmaster c3_box c3_childpagemaster c3_childpage2', $value);
     }
 
@@ -59,26 +59,26 @@ class Vps_Component_Output_Test extends PHPUnit_Framework_TestCase
     {
         Vps_Component_Data_Root::setComponentClass('Vps_Component_Output_Plugin_Component');
         $root = Vps_Component_Data_Root::getInstance();
-        $output = new Vps_Component_View();
+        $view = new Vps_Component_View();
 
-        $value = $output->renderMaster($root);
+        $value = $view->renderMaster($root);
         // Eigentlicher Code zur Kontrolle in PluginAfter!
         $this->assertEquals('rootmaster pluginChild rootmasterend', $value);
     }
 
     public function testPartial()
     {
-        $this->markTestIncomplete();
-        $output = new Vps_Component_Output_NoCache();
-
         Vps_Component_Data_Root::setComponentClass('Vps_Component_Output_Partial_Random_Component');
-        $value = $output->renderMaster(Vps_Component_Data_Root::getInstance());
+        $root = Vps_Component_Data_Root::getInstance();
+        $view = new Vps_Component_View();
+
+        $value = $view->renderMaster($root);
         $this->assertTrue(in_array($value, array(
             'bar0bar1', 'bar0bar2', 'bar1bar2', 'bar1bar0', 'bar2bar0', 'bar2bar1'
         )));
 
         Vps_Component_Data_Root::setComponentClass('Vps_Component_Output_Partial_Paging_Component');
-        $value = $output->renderMaster(Vps_Component_Data_Root::getInstance());
+        $value = $view->renderMaster(Vps_Component_Data_Root::getInstance());
         $this->assertEquals('bar2', $value);
     }
 
