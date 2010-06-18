bc0aab77d6f12d87563d2afc03475cc12d6992d1
Niko Sams <ns@vivid-planet.com>
2010-06-18 10:36:35 +0200

    Fix test, generator wurde umbenannt
    ist noch von der list umstellung

--- tests/Vpc/Columns/TestComponent.php
+++ tests/Vpc/Columns/TestComponent.php
@@ -4,8 +4,8 @@ class Vpc_Columns_TestComponent extends Vpc_Columns_Component
     public static function getSettings()
     {
         $ret = parent::getSettings();
-        $ret['generators']['columns']['model'] = 'Vpc_Columns_TestColumnsModel';
-        $ret['generators']['columns']['component'] = 'Vpc_Columns_TestComponent_Column';
+        $ret['generators']['child']['model'] = 'Vpc_Columns_TestColumnsModel';
+        $ret['generators']['child']['component'] = 'Vpc_Columns_TestComponent_Column';
         $ret['ownModel'] = 'Vpc_Columns_TestModel';
         return $ret;
     }
