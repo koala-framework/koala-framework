37049ae1fe045d8d252395be14f6cf99498e8c39
Niko Sams <ns@vivid-planet.com>
2010-07-09 09:46:31 +0200

    Bildgallerie MultiFileUpload: vergroessern standard

--- Vpc/List/Gallery/Image/LinkTag/Component.php
+++ Vpc/List/Gallery/Image/LinkTag/Component.php
@@ -1,4 +1,10 @@
 <?php
 class Vpc_List_Gallery_Image_LinkTag_Component extends Vpc_TextImage_ImageEnlarge_LinkTag_Component
 {
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['ownModel'] = 'Vpc_List_Gallery_Image_LinkTag_Model';
+        return $ret;
+    }
 }
