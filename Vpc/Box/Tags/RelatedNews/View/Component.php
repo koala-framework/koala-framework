a32f3cc7686268d1c1e4338247d8b3ff11ae4ffc
Niko Sams <ns@vivid-planet.com>
2010-07-13 16:45:37 +0200

    standardmaessig nur 10 news anzeigen

--- Vpc/Box/Tags/RelatedNews/View/Component.php
+++ Vpc/Box/Tags/RelatedNews/View/Component.php
@@ -1,6 +1,13 @@
 <?php
 class Vpc_Box_Tags_RelatedNews_View_Component extends Vpc_Directories_List_ViewPage_Component
 {
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['limit'] = 10;
+        return $ret;
+    }
+
     public static function getStaticCacheVars($componentClass)
     {
         $ret = parent::getStaticCacheVars($componentClass);
