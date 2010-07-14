7dd38854f08bd546a9199ab974ddff205872f099
Niko Sams <ns@vivid-planet.com>
2010-07-14 08:37:28 +0200

    tags zuweisung: filter+paging aktiviert

--- Vps/Component/Generator/Plugin/Tags/TagsController.php
+++ Vps/Component/Generator/Plugin/Tags/TagsController.php
@@ -2,6 +2,8 @@
 class Vps_Component_Generator_Plugin_Tags_TagsController extends Vps_Controller_Action_Auto_Grid
 {
     protected $_modelName = 'Vps_Component_Generator_Plugin_Tags_TagsModel';
+    protected $_filters = array('text'=>true);
+    protected $_paging = 25;
 
     protected function _initColumns()
     {
