a9c481232d341bc6916e001d38adefd41248dcf0
Niko Sams <ns@vivid-planet.com>
2010-07-14 14:38:37 +0200

    Width Field: einheit, schmaeler

--- Vpc/Form/Field/TextField/Form.php
+++ Vpc/Form/Field/TextField/Form.php
@@ -5,6 +5,8 @@ class Vpc_Form_Field_TextField_Form extends Vpc_Form_Field_Abstract_Form
     {
         parent::_initFields();
         $this->fields->add(new Vps_Form_Field_NumberField('width', trlVps('Width')))
+            ->setComment('px')
+            ->setWidth(50)
             ->setAllowNegative(false)
             ->setAllowDecimal(false);
         $this->fields->add(new Vps_Form_Field_TextField('default_value', trlVps('Default Value')));
