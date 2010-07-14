12b982d85bce387a436d63e8c22278b03449788b
Niko Sams <ns@vivid-planet.com>
2010-07-14 11:32:16 +0200

    fix getChildComponentClass call

--- Vpc/Form/Dynamic/Admin.php
+++ Vpc/Form/Dynamic/Admin.php
@@ -4,9 +4,11 @@ class Vpc_Form_Dynamic_Admin extends Vpc_Form_Admin
     //TODO 1.10: kann dur editComponents ersetzt werden
     public function getExtConfig()
     {
-        $ret = Vpc_Admin::getInstance(Vpc_Abstract::getChildComponentClass($this->_class, 'child', 'paragraphs'))
+        $ret = parent::getExtConfig();
+        $parConfig = Vpc_Admin::getInstance(Vpc_Abstract::getChildComponentClass($this->_class, 'child', 'paragraphs'))
                 ->getExtConfig();
-        $ret['paragraphs']['componentIdSuffix'] = '-paragraphs';
+        $parConfig['paragraphs']['componentIdSuffix'] = '-paragraphs';
+        $ret = array_merge($ret, $parConfig);
         return $ret;
     }
 }
