8168adbf5fe5dfb61f7b21b839a885aa01c3110f
Niko Sams <ns@vivid-planet.com>
2010-07-23 11:36:17 +0200

    feldnamen validieren
    punkt od. leerzeichen koennen zu problemen fuehren

--- Vps/Form/Field/Abstract.php
+++ Vps/Form/Field/Abstract.php
@@ -44,6 +44,14 @@ abstract class Vps_Form_Field_Abstract extends Vps_Component_Abstract
         }
     }
 
+    public function setName($name)
+    {
+        if ($name && !preg_match('#^[a-z0-9_-]+$#i', $name)) {
+            throw new Vps_Exception("Invalid field name '$name'");
+        }
+        $this->__call('setName', array($name));
+    }
+
     protected function _getTrlProperties()
     {
         return array('fieldLabel');
