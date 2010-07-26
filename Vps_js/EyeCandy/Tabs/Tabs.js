54c419567915806afdbbbb35a8ba3629cbcd3e03
Markus Hauser <mh@vivid-planet.com>
2010-07-26 16:09:52 +0200

    gmap wird in vpsTabs korrekt initialisiert

--- Vps_js/EyeCandy/Tabs/Tabs.js
+++ Vps_js/EyeCandy/Tabs/Tabs.js
@@ -8,6 +8,11 @@ Vps.onContentReady(function() {
 
 
 Vps.Tabs = function(el) {
+    this.addEvents({
+        'beforeTabActivate': true,
+        'tabActivate': true
+    });
+
     this.el = el;
     this._activeTabIdx = null;
     this.switchEls = Ext.query('.vpsTabsLink', this.el.dom);
@@ -37,6 +42,9 @@ Vps.Tabs = function(el) {
 
 Ext.extend(Vps.Tabs, Ext.util.Observable, {
     activateTab: function(idx) {
+        // passed arguments are: tabsObject, newIndex, oldIndex
+        this.fireEvent('beforeTabActivate', this, idx, this._activeTabIdx);
+
         if (this._activeTabIdx !== null) {
             Ext.get(this.switchEls[this._activeTabIdx]).removeClass('vpsTabsLinkActive');
             Ext.get(this.contentEls[this._activeTabIdx]).removeClass('vpsTabsContentActive');
@@ -44,6 +52,22 @@ Ext.extend(Vps.Tabs, Ext.util.Observable, {
         Ext.get(this.switchEls[idx]).addClass('vpsTabsLinkActive');
         Ext.get(this.contentEls[idx]).addClass('vpsTabsContentActive');
 
+        // passed arguments are: tabsObject, newIndex, oldIndex
+        this.fireEvent('tabActivate', this, idx, this._activeTabIdx);
+
         this._activeTabIdx = idx;
+    },
+
+    getIdxByContentEl: function(el) {
+        if (el.dom) el = el.dom;
+        for (var i = 0; i < this.contentEls.length; i++) {
+            if (this.contentEls[i] === el) return i;
+        }
+        return (-1);
+    },
+
+    getContentElByIdx: function(idx) {
+        if (this.contentEls[idx]) return this.contentEls[idx];
+        return null;
     }
 });
