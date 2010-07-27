a668138c8fef7b65a71cf7da53827b022b491650
Niko Sams <ns@vivid-planet.com>
2010-07-27 18:06:51 +0200

    weitere performanceverbesserungen
    maxFileMTime neur fuer Admin type berechnen, das sollte reichen in der praxis und ist 4x so schnell

--- bootstrap.php
+++ bootstrap.php
@@ -9,6 +9,34 @@ set_include_path($include_path);
 require_once 'Vps/Setup.php';
 Vps_Setup::setUp();
 
+
+$d = "<?php
+\$dependencies = array(
+";
+$c = new Zend_Config_Ini('config.ini', 'dependencies');
+foreach ($c->toArray() as $k=>$i) {
+    p($i);
+    $d .= "    '".$k."' => array(\n";
+    if (isset($i['files'])) {
+    $d .= "        'files' => array(\n";
+    foreach ($i['files'] as $f) {
+        $d .= "            '$f',\n";
+    }
+    $d .= "        ),\n";
+    }
+    if (isset($i['dep'])) {
+    $d .= "        'dep' => array(\n";
+    foreach ($i['dep'] as $f) {
+        $d .= "            '$f',\n";
+    }
+    $d .= "        ),\n";
+    }
+    $d .= "    ),\n";
+}
+$d .= "
+);";
+file_put_contents('dependencies.php', $d);
+
 Vps_Assets_Loader::load();
 
 $front = Vps_Controller_Front::getInstance();
