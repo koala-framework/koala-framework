18d6d07868ad2c8279cd8d6a698e9c233e716009
Niko Sams <ns@vivid-planet.com>
2010-07-30 11:14:35 +0200

    form trl test

---
+++ tests/Vpc/Trl/FormDynamic/Form/Paragraphs/TextField/TestModel.php
@@ -0,0 +1,11 @@
+<?php
+class Vpc_Trl_FormDynamic_Form_Paragraphs_TextField_TestModel extends Vps_Model_FnF
+{
+    protected $_primaryKey = 'component_id';
+    protected $_data = array(
+        array('component_id'=>'root-master_test1-paragraphs-1', 'field_label'=>'Label'),
+        array('component_id'=>'root-master_test1-paragraphs-2', 'field_label'=>'Required', 'required'=>true),
+        array('component_id'=>'root-master_test1-paragraphs-3', 'field_label'=>'EMail', 'vtype'=>'email'),
+        array('component_id'=>'root-master_test1-paragraphs-4', 'field_label'=>'Default', 'default_value'=>'Def'),
+    );
+}
---
+++ tests/Vpc/Trl/FormDynamic/Test.php
@@ -0,0 +1,1 @@
+http://vps.vps.niko.vivid/vps/vpctest/Vpc_Trl_FormDynamic_Root/de/test1
