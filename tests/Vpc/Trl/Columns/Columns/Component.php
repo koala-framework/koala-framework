22e0395debb6fc7bbb6617cd0a7a541b1ca33970
Niko Sams <ns@vivid-planet.com>
2010-07-30 08:17:19 +0200

    fix test

--- tests/Vpc/Trl/Columns/Columns/Component.php
+++ tests/Vpc/Trl/Columns/Columns/Component.php
@@ -1,9 +1,9 @@
 <?php
 class Vpc_Trl_Columns_Columns_Component extends Vpc_Columns_Component
 {
-    public static function getSettings()
+    public static function getSettings($parentComponentClass)
     {
-        $ret = parent::getSettings();
+        $ret = parent::getSettings($parentComponentClass);
         $ret['generators']['child']['component'] = 'Vpc_Trl_Columns_Columns_Column_Component';
         $ret['ownModel'] = 'Vpc_Trl_Columns_Columns_Model';
         $ret['childModel'] = 'Vpc_Trl_Columns_Columns_ColumnsModel';
