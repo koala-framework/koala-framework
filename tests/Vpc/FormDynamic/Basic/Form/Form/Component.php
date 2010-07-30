d9887662b0f13838d644047f1c630a104d672415
Niko Sams <ns@vivid-planet.com>
2010-07-30 14:54:32 +0200

    file uploads

--- tests/Vpc/FormDynamic/Basic/Form/Form/Component.php
+++ tests/Vpc/FormDynamic/Basic/Form/Form/Component.php
@@ -1,9 +1,18 @@
 <?php
 class Vpc_FormDynamic_Basic_Form_Form_Component extends Vpc_Form_Dynamic_Form_Component
 {
-    protected function _initForm()
+    protected function _createModel($referenceMap)
     {
-        parent::_initForm();
-        $this->_form->setModel(new Vps_Model_FnF());
+        foreach ($referenceMap as $k=>$i) {
+            if ($i['refModelClass'] == 'Vps_Uploads_Model') {
+                $referenceMap[$k]['refModelClass'] = 'Vpc_FormDynamic_Basic_Form_Form_UploadsModel';
+            }
+        }
+        return new Vps_Model_Mail(array(
+            'componentClass' => get_class($this),
+            'proxyModel' => new Vps_Model_FnF(),
+            'referenceMap' => $referenceMap
+        ));
     }
+
 }
