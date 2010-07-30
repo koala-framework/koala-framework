1ffe28bb535138d4bda7ec4f9935a00b69aef5a7
Franz Unger <ufx@vivid-planet.com>
2010-07-30 13:57:06 +0200

    UploadController: maxResolution holen verbessert

--- Vps/Controller/Action/Media/UploadController.php
+++ Vps/Controller/Action/Media/UploadController.php
@@ -22,12 +22,12 @@ class Vps_Controller_Action_Media_UploadController extends Vps_Controller_Action
         $fileRow = Vps_Model_Abstract::getInstance('Vps_Uploads_Model')
             ->createRow();
 
-        $maxResolution = $this->_getParam('maxResolution');
+        $maxResolution = (int)$this->_getParam('maxResolution');
         if ($this->_getParam('maxResolution')) {
             $image = getimagesize($file['tmp_name']);
-            if (substr($image['mime'], 0, 6) != 'image/') $maxResolution = null;
+            if (substr($image['mime'], 0, 6) != 'image/') $maxResolution = 0;
         }
-        if ($maxResolution) {
+        if ($maxResolution > 0) {
             $fileData = Vps_Media_Image::scale($file['tmp_name'], array('width' => $maxResolution, 'height' => $maxResolution, 'scale' => Vps_Media_Image::SCALE_BESTFIT));
             $filename = substr($file['name'], 0, strrpos($file['name'], '.'));
             $extension = substr(strrchr($file['name'], '.'), 1);
