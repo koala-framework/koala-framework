fc2cd09cb33584412fd64d2a8902833176b99d55
Franz Unger <ufx@vivid-planet.com>
2010-07-30 09:06:29 +0200

    Update-Scripts fuer Events und News verbessert

--- Vpc/News/Category/Directory/Update/34541.sql
+++ Vpc/News/Category/Directory/Update/34541.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `vpc_news_to_categories` CHANGE `news_id` `news_id` SMALLINT NOT NULL DEFAULT '0';
+DELETE FROM `vpc_news_to_categories` WHERE news_id NOT IN (SELECT id FROM vpc_news);
 ALTER TABLE `vpc_news_to_categories` ADD FOREIGN KEY ( `news_id` ) REFERENCES `vpc_news` (`id`);
 ALTER TABLE `vpc_news_to_categories` ADD FOREIGN KEY ( `category_id` ) REFERENCES `vpc_directories_categories` (`id`);
