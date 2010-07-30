e3703b9220d17cf9e4cce18227cce2b8164455d6
Niko Sams <ns@vivid-planet.com>
2010-07-30 11:47:45 +0200

    vpc_form ist jetzt unterkomponente
    vorbereitung fuer trl

--- tests/Vpc/FormDynamic/Basic/Form/Component.php
+++ tests/Vpc/FormDynamic/Basic/Form/Component.php
@@ -5,13 +5,8 @@ class Vpc_FormDynamic_Basic_Form_Component extends Vpc_Form_Dynamic_Component
     {
         $ret = parent::getSettings();
         $ret['generators']['child']['component']['paragraphs'] = 'Vpc_FormDynamic_Basic_Form_Paragraphs_Component';
+        $ret['generators']['child']['component']['form'] = 'Vpc_FormDynamic_Basic_Form_Form_Component';
         $ret['ownModel'] = 'Vpc_FormDynamic_Basic_Form_TestModel';
         return $ret;
-    }
-
-    protected function _initForm()
-    {
-        parent::_initForm();
-        $this->_form->setModel(new Vps_Model_FnF());
     }
 }
