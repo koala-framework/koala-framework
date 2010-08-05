0cdce16232ff5ff8e79dcc483cf8b0c5faaeab87
Niko Sams <ns@vivid-planet.com>
2010-08-05 14:49:27 +0200

    extConfig verwenden statt admin

--- Vpc/Composite/TwoColumns/Component.php
+++ Vpc/Composite/TwoColumns/Component.php
@@ -8,6 +8,8 @@ class Vpc_Composite_TwoColumns_Component extends Vpc_Abstract_Composite_Componen
         $ret['componentIcon'] = new Vps_Asset('application_tile_horizontal');
         $ret['generators']['child']['component']['leftColumn'] = 'Vpc_Composite_TwoColumns_Left_Component';
         $ret['generators']['child']['component']['rightColumn'] = 'Vpc_Composite_TwoColumns_Right_Component';
+
+        $ret['extConfig'] = 'Vpc_Abstract_Composite_ExtConfigTabs';
         return $ret;
     }
 }
