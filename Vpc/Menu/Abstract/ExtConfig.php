f6e880977d49fb8fdf4be5e8b7c94c618a5b56da
Niko Sams <ns@vivid-planet.com>
2010-08-05 14:00:51 +0200

    fehlende controllerUrl

--- Vpc/Menu/Abstract/ExtConfig.php
+++ Vpc/Menu/Abstract/ExtConfig.php
@@ -4,6 +4,7 @@ class Vpc_Menu_Abstract_ExtConfig extends Vps_Component_Abstract_ExtConfig_Abstr
     protected function _getConfig()
     {
         $config = $this->_getStandardConfig('vpc.menu.panel');
+        $config['formControllerUrl'] = $this->getControllerUrl('Form');
         return array(
             'form' => $config
         );
