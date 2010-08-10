673454a3a52d0125b05528c7ff27e34a2eb683b6
Markus Hauser <mh@vivid-planet.com>
2010-08-10 17:27:51 +0200

    temporaere csv export ordner loeschen wenn man sie nicht mehr braucht + update controller der die alten loescht

--- Vps/Model/Db.php
+++ Vps/Model/Db.php
@@ -826,6 +826,7 @@ class Vps_Model_Db extends Vps_Model_Abstract
 
                 $ret = file_get_contents($filename.'.gz');
                 unlink($filename.'.gz');
+                rmdir($tmpExportFolder);
                 return $ret;
             } else {
                 return '';
