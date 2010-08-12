ccc64e57c1c5f06183dd79e845a5e59324d4cf35
Niko Sams <ns@vivid-planet.com>
2010-08-12 10:35:57 +0200

    mail content testen

--- tests/Vpc/FormDynamic/Basic/Test.php
+++ tests/Vpc/FormDynamic/Basic/Test.php
@@ -1,67 +1,35 @@
 <?php
 /**
- * @group slow
- * @group selenium
  * @group Vpc_FormDynamic
- *
- * http://vps.vps.niko.vivid/vps/vpctest/Vpc_FormDynamic_Basic_Root/form
- * http://vps.vps.niko.vivid/vps/vpctest/Vpc_FormDynamic_Basic_Root/form2
  */
-class Vpc_FormDynamic_Basic_Test extends Vps_Test_SeleniumTestCase
+class Vpc_FormDynamic_Basic_Test extends Vpc_TestAbstract
 {
     public function setUp()
     {
-        parent::setUp();
-        Vps_Component_Data_Root::setComponentClass('Vpc_FormDynamic_Basic_Root');
+        parent::setUp('Vpc_FormDynamic_Basic_Root');
     }
 
-    public function testTextField()
+    public function testIt()
     {
-        //default value
-        $this->openVpc('/form');
-        $this->assertElementValueEquals('css=#form_root_form-paragraphs-4', 'Def');
-        $this->clickAndWait('css=button');
-        $this->assertElementValueEquals('css=#form_root_form-paragraphs-4', 'Def');
+        $c = $this->_root->getComponentById('root_form-form')->getComponent();
+        $postData = array(
+            'form_root_form-paragraphs-1' => '',
+            'form_root_form-paragraphs-2' => 'asdfasdf',
+            'form_root_form-paragraphs-3' => '',
+            'form_root_form-paragraphs-4' => 'Def',
+            'form_root_form-paragraphs-5-post' => '1',
+            'form_root_form-paragraphs-6' => 'on',
+            'form_root_form-paragraphs-6-post' => '1',
+            'root_form-form' => 'submit',
+            'root_form-form-post' => 'post',
+        );
+        $c->processInput($postData);
+        $this->assertEquals($c->getErrors(), array());
+        $row = $c->getFormRow();
 
-        //required
-        $this->openVpc('/form');
-        $this->assertElementPresent('css=#form_root_form-paragraphs-2');
-        $this->assertElementNotPresent('css=.vpsFieldError #form_root_form-paragraphs-2');
-        $this->clickAndWait('css=button');
-        $this->assertTextPresent('Required: Please fill out');
-        $this->assertElementPresent('css=.vpsFieldError #form_root_form-paragraphs-2');
-
-        //vtype email
-        $this->openVpc('/form');
-        $this->type('css=#form_root_form-paragraphs-3', 'foo');
-        $this->clickAndWait('css=button');
-        $this->assertTextPresent('EMail: \'foo\' is not a valid');
-        $this->assertElementPresent('css=.vpsFieldError #form_root_form-paragraphs-3');
+        $text = $row->body_text;
+        $this->assertContains('Required: asdfasdf', $text);
+        $this->assertContains('Check: off', $text);
+        $this->assertContains('CheckDefault: on', $text);
     }
-
-    public function testCheckbox()
-    {
-        //default value
-        $this->openVpc('/form');
-        $this->assertNotChecked('css=#form_root_form-paragraphs-5');
-        $this->assertChecked('css=#form_root_form-paragraphs-6');
-    }
-
-    public function testFile()
-    {
-        //required
-        $this->openVpc('/form2');
-        $this->markTestIncomplete('file ist noch nicht fertig implementiert');
-        $this->clickAndWait('css=button');
-        $this->assertTextPresent('Required: Please fill out');
-        $this->assertElementPresent('css=.vpsFieldError #form_root_form-paragraphs-8');
-    }
-
-    public function testAdmin()
-    {
-        $this->openVpcEdit('Vpc_FormDynamic_Basic_Form_Paragraphs_Component', 'root_form-paragraphs');
-        $this->waitForConnections();
-        //test könnte natürlich verbessert werden, aber zumindest testen ob kein fehler kommt
-    }
-
 }
