866a725f602c8126b2a3de096e46203e2861db5b
Niko Sams <ns@vivid-planet.com>
2010-08-17 14:31:55 +0200

    voucher admin verbessert

--- Vpc/Shop/Cart/Plugins/Voucher/VouchersController.php
+++ Vpc/Shop/Cart/Plugins/Voucher/VouchersController.php
@@ -4,6 +4,7 @@ class Vpc_Shop_Cart_Plugins_Voucher_VouchersController extends Vps_Controller_Ac
     protected $_modelName = 'Vpc_Shop_Cart_Plugins_Voucher_Vouchers';
     protected $_buttons = array('add', 'delete');
     protected $_paging = 25;
+    protected $_filters = array('text'=>true);
 
     public function indexAction()
     {
@@ -16,13 +17,13 @@ class Vpc_Shop_Cart_Plugins_Voucher_VouchersController extends Vps_Controller_Ac
     protected function _initColumns()
     {
         parent::_initColumns();
+        $this->_columns->add(new Vps_Grid_Column_Date('date', trlVps('Date')));
         $this->_columns->add(new Vps_Grid_Column('code', trlVps('Code')));
         $this->_columns->add(new Vps_Grid_Column('amount', trlcVps('Amount of Money', 'Amount'), 50))
             ->setRenderer('euroMoney');
         $this->_columns->add(new Vps_Grid_Column('used_amount', trlcVps('Amount of Money', 'Used Amount'), 50))
             ->setType('float')
             ->setRenderer('euroMoney');
-        $this->_columns->add(new Vps_Grid_Column_Date('date', trlVps('Date')));
         $this->_columns->add(new Vps_Grid_Column('comment', trlVps('Comment')));
     }
 }
