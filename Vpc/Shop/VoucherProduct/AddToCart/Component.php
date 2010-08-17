88bc8ec327fc1698653f9f5d3056282cfe5c07cb
Niko Sams <ns@vivid-planet.com>
2010-08-17 18:11:42 +0200

    orderProductData klasse

--- Vpc/Shop/VoucherProduct/AddToCart/Component.php
+++ Vpc/Shop/VoucherProduct/AddToCart/Component.php
@@ -4,42 +4,7 @@ class Vpc_Shop_VoucherProduct_AddToCart_Component extends Vpc_Shop_AddToCartAbst
     public static function getSettings()
     {
         $ret = parent::getSettings();
+        $ret['orderProductData'] = 'Vpc_Shop_VoucherProduct_AddToCart_OrderProductData';
         return $ret;
-    }
-
-    public function getPrice(Vpc_Shop_Cart_OrderProduct $orderProduct)
-    {
-        return $orderProduct->amount;
-    }
-
-    public function getAmount(Vpc_Shop_Cart_OrderProduct $orderProduct)
-    {
-        return 1;
-    }
-
-    public function getAdditionalOrderData(Vpc_Shop_Cart_OrderProduct $row)
-    {
-        $ret = parent::getAdditionalOrderData($row);
-        $ret[] = array(
-            'class' => 'amount',
-            'name' => trlcVps('Amount of Money', 'Amount'),
-            'value' => Vps_View_Helper_Money::money($row->amount)
-        );
-        return $ret;
-    }
-
-    public function orderConfirmed(Vpc_Shop_Cart_OrderProduct $orderProduct)
-    {
-        //gutschein erstellen
-        $row = Vps_Model_Abstract::getInstance('Vpc_Shop_Cart_Plugins_Voucher_Vouchers')->createRow();
-        $row->amount = $orderProduct->amount;
-        $row->date = date('Y-m-d H:i:s');
-        $row->comment = trlVps('Order').' '.$orderProduct->getParentRow('Order')->order_number;
-        $row->save();
-    }
-
-    public function getProductText(Vpc_Shop_Cart_OrderProduct $orderProduct)
-    {
-        return trlVps('Voucher');
     }
 }
