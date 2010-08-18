320505c1b61d727ed6abba0357a98fe6a27b1028
Niko Sams <ns@vivid-planet.com>
2010-08-18 16:20:46 +0200

    changes-eintrag feur neuen branch

--- Vps/Date.php
+++ Vps/Date.php
@@ -1,12 +1,4 @@
 <?php
-Zend_Date::setOptions(array(
-    'format_type' => 'php'
-));
-class Vps_Date extends Zend_Date
+class Vps_Date extends DateTime
 {
-    public function __construct($date = null, $part = null, $locale = null)
-    {
-        if (!$locale) $locale = Zend_Registry::get('trl')->getTargetLanguage();
-        parent::__construct($date, $part, $locale);
-    }
 }
