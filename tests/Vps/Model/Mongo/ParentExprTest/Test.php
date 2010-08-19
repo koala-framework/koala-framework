dca0c89a65147cad2d6c40fb83f779d37cabb3fe
Niko Sams <ns@vivid-planet.com>
2010-08-19 13:03:29 +0200

    test umbenannt

--- tests/Vps/Model/Mongo/ParentExprTest/Test.php
+++ tests/Vps/Model/Mongo/ParentExprTest/Test.php
@@ -3,44 +3,43 @@
  * @group Mongo
  * @group slow
  */
-class Vps_Model_Mongo_ExprTest_Test extends PHPUnit_Framework_TestCase
+class Vps_Model_Mongo_ParentExprTest_Test extends PHPUnit_Framework_TestCase
 {
+    private $_model;
     public function setUp()
     {
         Vps_Model_Abstract::clearInstances();
 
-        $mongoModel = Vps_Model_Abstract::getInstance('Vps_Model_Mongo_ExprTest_MongoModel');
-        $mongoModel->getCollection()->insert(
+        $this->_model = Vps_Model_Abstract::getInstance('Vps_Model_Mongo_ParentExprTest_MongoModel');
+        $this->_model->getCollection()->insert(
             array('id'=>100, 'a'=>'a', 'parent_id'=>1, 'parent_name'=>'one') //TODO id sollte nicht nötig sein
         , array('safe'=>true));
     }
 
     protected function tearDown()
     {
-        Vps_Model_Abstract::getInstance('Vps_Model_Mongo_ExprTest_MongoModel')->cleanUp();
+        $this->_model->cleanUp();
         Vps_Model_Abstract::clearInstances();
     }
 
     public function testInitial()
     {
-        $mongoModel = Vps_Model_Abstract::getInstance('Vps_Model_Mongo_ExprTest_MongoModel');
-        $row = $mongoModel->getRow(array());
+        $row = $this->_model->getRow(array());
         $this->assertEquals('one', $row->parent_name);
         $this->assertEquals('one', $row->getParentRow('Parent')->name);
     }
 
     public function testChangedName()
     {
-        $parentModel = Vps_Model_Abstract::getInstance('Vps_Model_Mongo_ExprTest_ParentModel');
+        $parentModel = Vps_Model_Abstract::getInstance('Vps_Model_Mongo_ParentExprTest_ParentModel');
         $parentRow = $parentModel->getRow(1);
         $parentRow->name = 'onex';
         $parentRow->save();
 
-        $mongoModel = Vps_Model_Abstract::getInstance('Vps_Model_Mongo_ExprTest_MongoModel');
-        $row = $mongoModel->getRow(array());
+        $row = $this->_model->getRow(array());
         $this->assertEquals('onex', $row->parent_name);
         $this->assertEquals('onex', $row->getParentRow('Parent')->name);
-        $r = $mongoModel->getCollection()->findOne();
+        $r = $this->_model->getCollection()->findOne();
         $this->assertEquals('onex', $r['parent_name']);
     }
 }
