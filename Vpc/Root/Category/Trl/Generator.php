40b80439b29fd8a31789e53f1de66eca9b00db52
Franz Unger <ufx@vivid-planet.com>
2010-08-19 13:41:01 +0200

    Category Generator fuer CC und Trl angepasst

--- Vpc/Root/Category/Trl/Generator.php
+++ Vpc/Root/Category/Trl/Generator.php
@@ -1,81 +1,18 @@
 <?php
-class Vpc_Root_Category_Trl_Generator extends Vpc_Chained_Trl_Generator
+class Vpc_Root_Category_Trl_Generator extends Vpc_Root_Category_Cc_Generator
 {
     public function getPagesControllerConfig($component)
     {
         $ret = parent::getPagesControllerConfig($component);
-
-        foreach ($ret['actions'] as &$a) $a = false;
         $ret['actions']['properties'] = true;
         $ret['actions']['visible'] = true;
-
-        return $ret;
-    }
-
-    public function getChildData($parentData, $select = array())
-    {
-
-        $filename = null;
-        $limit = null;
-        $ignoreVisible = $select->hasPart(Vps_Component_Select::IGNORE_VISIBLE) ?
-            $select->getPart(Vps_Component_Select::IGNORE_VISIBLE) : false;
-            static $showInvisible;
-        if (is_null($showInvisible)) {
-            $showInvisible = Vps_Registry::get('config')->showInvisible;
-        }
-        if ($showInvisible) $ignoreVisible = true;
-
-        // Nach Filename selbst suchen, da ja andere Sprache
-        if ($select->hasPart(Vps_Component_Select::WHERE_FILENAME)) {
-            $filename = $select->getPart(Vps_Component_Select::WHERE_FILENAME);
-            $select->unsetPart(Vps_Component_Select::WHERE_FILENAME);
-        }
-        // Limit auch selbst beachten, da in slave eigenes visible gesetzt ist
-        if ($select->hasPart(Vps_Component_Select::LIMIT_COUNT)) {
-            $limit = $select->getPart(Vps_Component_Select::LIMIT_COUNT);
-            $select->unsetPart(Vps_Component_Select::LIMIT_COUNT);
-        }
-        $select->ignoreVisible();
-        $ret = array();
-        foreach (parent::getChildData($parentData, $select) as $key => $c) {
-            if (($ignoreVisible || $c->visible) &&
-                (!$filename || $c->filename == $filename)
-            ){
-                $ret[$key] = $c;
-            }
-            if ($limit && count($ret) == $limit) {
-                return $ret;
-            }
-        }
         return $ret;
     }
 
     protected function _formatConfig($parentData, $row)
     {
         $ret = parent::_formatConfig($parentData, $row);
-
-        //im pages generator fangen die ids immer von vorne an
-        $id = $this->_getIdFromRow($row);
-        if (!is_numeric($id)) throw new Vps_Exception("Id must be numeric");
-        $idParent = $parentData;
-        while ($idParent->componentClass != $this->_class) {
-            $idParent = $idParent->parent;
-        }
-        $id = $this->_getIdFromRow($row);
-        $ret['componentId'] = $idParent->componentId.$this->getIdSeparator().$id;
-        $ret['dbId'] = $idParent->dbId.$this->getIdSeparator().$id;
-
-        //parent geradebiegen
-        if (!$parentData || ($parentData->componentClass == $this->_class && is_numeric($ret['chained']->parent->componentId))) {
-            $c = new Vps_Component_Select();
-            $c->ignoreVisible(true);
-            $c->whereId('_'.$ret['chained']->parent->componentId);
-            $parentData = $parentData->getChildComponent($c);
-        }
-        $ret['parent'] = $parentData;
-
         $dbRow = $this->_getModel()->getRow($ret['componentId']);
-
         if (!$dbRow) {
             $dbRow = $this->_getModel()->createRow(array(
                 'component_id' => $ret['componentId'],
@@ -91,14 +28,5 @@ class Vpc_Root_Category_Trl_Generator extends Vpc_Chained_Trl_Generator
         $ret['visible'] = $row->isHome ? true : $dbRow->visible;
         $ret['isHome'] = $row->isHome;
         return $ret;
-    }
-
-    protected function _getDataClass($config, $id)
-    {
-        if (isset($config['isHome']) && $config['isHome']) {
-            return 'Vps_Component_Data_Home';
-        } else {
-            return parent::_getDataClass($config, $id);
-        }
     }
 }
