e0ab26a2d2c503e072c4a279a0728c76c192c5ea
Niko Sams <ns@vivid-planet.com>
2010-08-19 16:49:16 +0200

    numberfield stat combo

--- Vpc/Shop/VoucherProduct/AddToCart/Form.php
+++ Vpc/Shop/VoucherProduct/AddToCart/Form.php
@@ -8,8 +8,11 @@ class Vpc_Shop_VoucherProduct_AddToCart_Form extends Vpc_Shop_AddToCartAbstract_
         for($i=1;$i<=10;$i++) {
             $values[$i*10] = Vps_View_Helper_Money::money($i*10);
         }
-        $this->add(new Vps_Form_Field_Select('amount', trlcVps('Amount of Money', 'Amount')))
+        $this->add(new Vps_Form_Field_NumberField('amount', trlcVps('Amount of Money', 'Amount')))
+            ->setAllowNegative(false)
+            ->setWidth(50)
             ->setValues($values)
-            ->setAllowBlank(false);
+            ->setAllowBlank(false)
+            ->setComment('EUR');
     }
 }
