6d1bd01183a328bb58a13e4164c813f3a2e5ec39
Franz Unger <ufx@vivid-planet.com>
2010-08-19 13:41:01 +0200

    Category Generator fuer CC und Trl angepasst

--- Vpc/Root/Category/Cc/Generator.php
+++ Vpc/Root/Category/Cc/Generator.php
@@ -1,20 +1,15 @@
 <?php
-class Vpc_Root_Category_Cc_Generator extends Vpc_Chained_Cc_Generator
+class Vpc_Root_Category_Cc_Generator extends Vpc_Chained_Abstract_Generator
 {
     public function getPagesControllerConfig($component)
     {
         $ret = parent::getPagesControllerConfig($component);
-
         foreach ($ret['actions'] as &$a) $a = false;
-        $ret['actions']['properties'] = true;
-        $ret['actions']['visible'] = true;
-
         return $ret;
     }
 
     public function getChildData($parentData, $select = array())
     {
-
         $filename = null;
         $limit = null;
         $ignoreVisible = $select->hasPart(Vps_Component_Select::IGNORE_VISIBLE) ?
@@ -73,23 +68,6 @@ class Vpc_Root_Category_Cc_Generator extends Vpc_Chained_Cc_Generator
             $parentData = $parentData->getChildComponent($c);
         }
         $ret['parent'] = $parentData;
-
-        $dbRow = $this->_getModel()->getRow($ret['componentId']);
-
-        if (!$dbRow) {
-            $dbRow = $this->_getModel()->createRow(array(
-                'component_id' => $ret['componentId'],
-                'name' => $row->getRow()->name,
-                'filename' => $row->getRow()->filename,
-                'visible' => $row->isHome, //home ist standardmäßig immer sichtbar, andere seiten nicht
-                'custom_filename' => $row->getRow()->custom_filename
-            ));
-        }
-        $ret['row'] = $dbRow;
-        $ret['name'] = $dbRow->name;
-        $ret['filename'] = $dbRow->filename;
-        $ret['visible'] = $row->isHome ? true : $dbRow->visible;
-        $ret['isHome'] = $row->isHome;
         return $ret;
     }
 
