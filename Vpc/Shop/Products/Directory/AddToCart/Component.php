c3cea244b986b650d1df30fe70e1bfd661141bc9
Christoph Eder <ce@vivid-planet.com>
2010-08-20 09:46:42 +0200

    - bugfix (niko)

--- Vpc/Shop/Products/Directory/AddToCart/Component.php
+++ Vpc/Shop/Products/Directory/AddToCart/Component.php
@@ -24,7 +24,7 @@ class Vpc_Shop_Products_Directory_AddToCart_Component extends Vpc_Form_Component
     protected function _beforeInsert(Vps_Model_Row_Interface $row)
     {
         parent::_beforeInsert($row);
-        $id = $this->_getProduct()->id;
+        $id = $this->getData()->row->id;
         $addToCart = $this->getData()->parent->getComponent()->getItemDirectory()
             ->getChildComponent('_'.$id)
             ->getChildComponent('-addToCart');
