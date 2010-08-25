064eb989aa93f60d67a965d0c6316743389638a8
Niko Sams <ns@vivid-planet.com>
2010-08-25 14:08:38 +0200

    nicht existierenden, nicht verwendetes model entfernt

--- Vpc/Root/Category/Cc/Component.php
+++ Vpc/Root/Category/Cc/Component.php
@@ -5,7 +5,6 @@ class Vpc_Root_Category_Cc_Component extends Vpc_Chained_Cc_Component
     {
         $ret = parent::getSettings($masterComponentClass);
         $ret['generators']['page']['class'] = 'Vpc_Root_Category_Cc_Generator';
-        $ret['generators']['page']['model'] = 'Vpc_Root_Category_Cc_GeneratorModel';
         return $ret;
     }
 }
