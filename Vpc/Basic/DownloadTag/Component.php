3ec2347f9156730747b52ae0bc21b702d59d6046
Niko Sams <ns@vivid-planet.com>
2010-08-27 11:02:41 +0200

    DownloadTag der Passwortgeschuetzt ist die Downloads auch schuetzen
    benutezr muss eingeloggt sein wenn er sich die datei runterladen will

--- Vpc/Basic/DownloadTag/Component.php
+++ Vpc/Basic/DownloadTag/Component.php
@@ -57,23 +57,34 @@ class Vpc_Basic_DownloadTag_Component extends Vpc_Basic_LinkTag_Abstract_Compone
 
     public static function isValidMediaOutput($id, $type, $className)
     {
-        if (Vps_Component_Data_Root::getInstance()->getComponentById($id)) {
-            return self::VALID;
-        }
-        if (Vps_Registry::get('config')->showInvisible) {
-            //preview im frontend
-            if (Vps_Component_Data_Root::getInstance()->getComponentById($id, array('ignoreVisible'=>true))) {
-                return self::VALID_DONT_CACHE;
+        $retValid = self::VALID;
+        $c = Vps_Component_Data_Root::getInstance()->getComponentById($id);
+        if (!$c) {
+            $c = Vps_Component_Data_Root::getInstance()->getComponentById($id, array('ignoreVisible'=>true));
+            if (!$c) return self::INVALID;
+            if (Vps_Registry::get('config')->showInvisible) {
+                //preview im frontend
+                $retValid = self::VALID_DONT_CACHE;
+            } else if (Vps_Registry::get('acl')->isAllowedComponentById($id, $className, Vps_Registry::get('userModel')->getAuthedUser())) {
+                //paragraphs vorschau im backend
+                $retValid = self::VALID_DONT_CACHE;
             }
         }
-
-        //paragraphs vorschau im backend
-        $authData = Vps_Registry::get('userModel')->getAuthedUser();
-        if (Vps_Registry::get('acl')->isAllowedComponentById($id, $className, $authData)) {
-            return self::VALID_DONT_CACHE;
+        while ($c) {
+            foreach (Vpc_Abstract::getSetting($c->componentClass, 'plugins') as $plugin) {
+                if (is_instance_of($plugin, 'Vps_Component_Plugin_Interface_Login')) {
+                    $plugin = new $plugin($id);
+                    if ($plugin->isLoggedIn()) {
+                        return self::VALID_DONT_CACHE;
+                    } else {
+                        return self::ACCESS_DENIED;
+                    }
+                }
+            }
+            if ($c->isPage) break;
+            $c = $c->parent;
         }
-
-        return self::INVALID;
+        return $retValid;
     }
 
     public static function getMediaOutput($id, $type, $className)
