0668f042dbcafa3e7ac2c5491b58c7c5b572f2c2
Franz Unger <ufx@vivid-planet.com>
2010-08-30 13:43:30 +0200

    Test fuer Flash-Komponente funktioniert, noch mit altem swfobject

---
+++ tests/Vpc/Flash/Component.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * http://vps11.franz.vivid/vps/vpctest/Vpc_Flash_Component
+ *
+ */class Vpc_Flash_Component extends Vpc_Abstract_Flash_Component
+{
+    protected function _getFlashData()
+    {
+        $ret = parent::_getFlashData();
+        $ret['url'] = '/assets/vps/tests/Vpc/Flash/demo.swf';
+        $ret['width'] = 558;
+        $ret['height'] = 168;
+        $ret['params'] = array(
+            'allowfullscreen' => 'true'
+        );
+        return $ret;
+    }
+
+    protected function _getFlashVars()
+    {
+        $ret = parent::_getFlashVars();
+        $ret['allowfullscreen'] = true;
+        return $ret;
+    }
+}
+/*
+<embed height="168" width="558"
+flashvars="loc=de_DE&amp;required_version=9,0,124,0"
+wmode="opaque"
+quality="high"
+bgcolor="#FFFFFF"
+name="shell.swf"
+id="shell_object-embed"
+src="demo"
+type="application/x-shockwave-flash">
+*/
---
+++ tests/Vpc/Flash/Root.php
@@ -0,0 +1,16 @@
+<?php
+class Vpc_Flash_Root extends Vps_Component_NoCategoriesRoot
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['generators']['flash'] = array(
+            'component' => 'Vpc_Flash_Component',
+            'class' => 'Vps_Component_Generator_Page_Static',
+        );
+        unset($ret['generators']['page']);
+        unset($ret['generators']['title']);
+        unset($ret['generators']['box']);
+        return $ret;
+    }
+}
