8ee5472920f656dcdb287ffd8666f0c6ec679c11
Niko Sams <ns@vivid-planet.com>
2010-08-31 14:32:44 +0200

    diese methode auch ins interface

--- Vps/Model/RowsSubModel/Interface.php
+++ Vps/Model/RowsSubModel/Interface.php
@@ -2,4 +2,5 @@
 interface Vps_Model_RowsSubModel_Interface
 {
     public function getRowsByParentRow(Vps_Model_Row_Interface $parentRow, $select = array());
+    public function createRowByParentRow(Vps_Model_Row_Interface $parentRow, array $data = array());
 }
