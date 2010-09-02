b014e086a5bab6e00ca49c71bb73670e389b89ef
Markus Hauser <mh@vivid-planet.com>
2010-09-02 14:38:54 +0200

    neuer auto filter TextField um in einem bestimmten feld zu suchen

--- Vps/Controller/Action/Auto/Filter/Query.php
+++ Vps/Controller/Action/Auto/Filter/Query.php
@@ -1,6 +1,9 @@
 <?php
 class Vps_Controller_Action_Auto_Filter_Query extends Vps_Controller_Action_Auto_Filter_Abstract
 {
+    const SELECT_TYPE_EQUALS = 'equals';
+    const SELECT_TYPE_CONTAINS = 'contains';
+
     protected function _init()
     {
         parent::_init();
@@ -10,10 +13,15 @@ class Vps_Controller_Action_Auto_Filter_Query extends Vps_Controller_Action_Auto
 
     public function formatSelect($select, $params = array()) {
         $column = $this->getFieldName();
-        if (!$this->getModel()->hasColumn($column))
+        if (!$this->getModel()->hasColumn($column)) {
             throw new Vps_Exception("Model has to have column \"$column\" to filter");
+        }
         if (isset($params[$this->getParamName()]) && $params[$this->getParamName()]) {
-            $select->whereEquals($column, $params[$this->getParamName()]);
+            if (!$this->getSelectType() || $this->getSelectType() == self::SELECT_TYPE_EQUALS) {
+                $select->whereEquals($column, $params[$this->getParamName()]);
+            } else if ($this->getSelectType() == self::SELECT_TYPE_CONTAINS) {
+                $select->where(new Vps_Model_Select_Expr_Contains($column, $params[$this->getParamName()]));
+            }
         }
         return $select;
     }
