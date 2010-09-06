ce9d0087b3f1570730dc6c7b6c7a9f96ea84c92a
Franz Unger <ufx@vivid-planet.com>
2010-09-06 17:38:26 +0200

    ChainedModel ist jetzt flexibler damit man auch Db-Model verwenden kann

--- Vpc/Chained/Abstract/ChainedModel.php
+++ Vpc/Chained/Abstract/ChainedModel.php
@@ -1,22 +1,20 @@
 <?php
 class Vpc_Chained_Abstract_ChainedModel extends Vps_Model_FnF
 {
-    protected $_columns = array('component_id', 'filename', 'name', 'master');
-    protected $_primaryKey = 'component_id';
+    protected $_columns = array('id', 'filename', 'name');
+    protected $_primaryKey = 'id';
+    protected $_toStringField = 'name';
 
     public function __construct(array $values = array())
     {
         $config['data'] = array();
-        $master = true;
         foreach ($values as $key => $value) {
             $config['data'][] = array(
-                'component_id' => 'root-' . $key,
+                'id' => $key,
                 'filename' => $key,
                 'name' => $value,
-                'master' => $master,
                 'visible' => 1
             );
-            $master = false;
         }
         $config['toStringField'] = 'name';
         parent::__construct($config);
