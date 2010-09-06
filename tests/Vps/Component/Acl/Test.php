55f38fe1086525d20c801b41f07b3f5c20c68f1c
Franz Unger <ufx@vivid-planet.com>
2010-09-06 08:55:23 +0200

    allowTag entfernt

--- tests/Vps/Component/Acl/Test.php
+++ tests/Vps/Component/Acl/Test.php
@@ -56,36 +56,4 @@ class Vps_Component_Acl_Test extends PHPUnit_Framework_TestCase
         $this->assertTrue($this->_acl->isAllowed('test', 'Vps_Component_Acl_Empty2'));
         $this->assertTrue($this->_acl->isAllowed('test', 'Vps_Component_Acl_TestComponent'));
     }
-
-    public function testAllowTag()
-    {
-        $this->_acl->allowComponent('test', null);
-        $this->_acl->allowTag('test', 'test');
-        $this->assertFalse($this->_acl->isAllowed(null, $this->_root->getComponentById(1)));
-        $this->assertTrue($this->_acl->isAllowed('test', $this->_root->getComponentById(1)));
-    }
-
-    public function testAllowAllTag()
-    {
-        $this->_acl->allowComponent('test', null);
-        $this->_acl->allowTag('test', null);
-        $this->assertTrue($this->_acl->isAllowed('test', $this->_root->getComponentById(2)));
-        $this->assertTrue($this->_acl->isAllowed('test', $this->_root->getComponentById(1)));
-    }
-
-    public function testAllowTagChildComponent()
-    {
-        $this->_acl->allowComponent('test', null);
-        $this->_acl->allowTag('test', 'blub');
-        $this->assertTrue($this->_acl->isAllowed('test', $this->_root->getComponentById(3)));
-        $this->assertTrue($this->_acl->isAllowed('test', $this->_root->getComponentById('3-blub')));
-    }
-
-    public function testAllowTagChildPage()
-    {
-        $this->_acl->allowComponent('test', null);
-        $this->_acl->allowTag('test', 'blub');
-        $this->assertTrue($this->_acl->isAllowed('test', $this->_root->getComponentById(3)));
-        $this->assertFalse($this->_acl->isAllowed('test', $this->_root->getComponentById(4)));
-    }
 }
