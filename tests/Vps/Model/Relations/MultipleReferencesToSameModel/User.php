9acf3ad53ad21baf32290b4c2011c345782b9306
Niko Sams <ns@vivid-planet.com>
2010-09-07 16:30:51 +0200

    getChildRows mit mehrere relationen zu einem model unterstuetzen

--- tests/Vps/Model/Relations/MultipleReferencesToSameModel/User.php
+++ tests/Vps/Model/Relations/MultipleReferencesToSameModel/User.php
@@ -6,19 +6,13 @@ class Vps_Model_Relations_MultipleReferencesToSameModel_User extends Vps_Model_F
         array('id'=>101, 'name'=>'hias'),
     );
     protected $_dependentModels = array(
-        //getChildRows('Todo', 'Creator');
-        //getChildRows('Todo:Creator');
-        //getChildRows('Todo->Creator');
-        'Todo' => 'Vps_Model_Relations_MultipleReferencesToSameModel_Todo',
-
-        //getChildRows('TodoCreator');
-        'TodoCreator' => 'Vps_Model_Relations_MultipleReferencesToSameModel_Todo:Creator',
-        'TodoAssignee' => 'Vps_Model_Relations_MultipleReferencesToSameModel_Todo:Assignee',
-
-        'TodoCreator' => 'Vps_Model_Relations_MultipleReferencesToSameModel_Todo->Creator',
-        'TodoAssignee' => 'Vps_Model_Relations_MultipleReferencesToSameModel_Todo->Assignee',
-
-        'Creator' => 'Vps_Model_Relations_MultipleReferencesToSameModel_Todo',
-        'Assignee' => 'Vps_Model_Relations_MultipleReferencesToSameModel_Todo',
+        'TodoCreator' => array(
+            'model' => 'Vps_Model_Relations_MultipleReferencesToSameModel_Todo',
+            'rule' => 'Creator'
+        ),
+        'TodoAssignee' => array(
+            'model' => 'Vps_Model_Relations_MultipleReferencesToSameModel_Todo',
+            'rule' => 'Assignee'
+        ),
     );
 }
