68615e32ddd7db1f92fe762db3bd3418e2c4d21b
Niko Sams <ns@vivid-planet.com>
2010-09-08 13:48:08 +0200

    volltextsuche meta-model

---
+++ Vpc/FulltextSearch/Search/Component.php
@@ -0,0 +1,9 @@
+<?php
+class Vpc_FulltextSearch_Search_Component extends Vpc_Abstract
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        return $ret;
+    }
+}
