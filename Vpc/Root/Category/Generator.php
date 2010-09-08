f6f4c51bd62b742665274afecf0c69d0c69f207f
Franz Unger <ufx@vivid-planet.com>
2010-09-08 10:36:41 +0200

    componentIds fuer PagesController wurden nicht korrekt evaluiert

--- Vpc/Root/Category/Generator.php
+++ Vpc/Root/Category/Generator.php
@@ -292,6 +292,16 @@ class Vpc_Root_Category_Generator extends Vps_Component_Generator_Abstract
         $ret['actions']['visible'] = true;
         $ret['actions']['makeHome'] = true;
 
+        // Bei Pages muss nach oben gesucht werden, weil Klasse von Generator
+        // mit Komponentklasse übereinstimmen muss
+        $c = $component;
+        while ($c && $c->componentClass != $this->getClass()) {
+            $c = $c->parent;
+        }
+        if ($c) { //TODO warum tritt das auf?
+            $ret['editControllerComponentId'] = $c->componentId;
+        }
+
         $ret['icon'] = 'page';
         if ($component->isHome) {
             $ret['iconEffects'][] = 'home';
