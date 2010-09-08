63514e7d0a655af1e8799e0f41df22d558d76cbd
Niko Sams <ns@vivid-planet.com>
2010-09-08 13:19:32 +0200

    aus chainedType ein Flag gemacht
    mit franz besprochen

--- tests/Vpc/Cc/Paragraphs/Master/Component.php
+++ tests/Vpc/Cc/Paragraphs/Master/Component.php
@@ -8,7 +8,7 @@ class Vpc_Cc_Paragraphs_Master_Component extends Vpc_Abstract
             'class' => 'Vps_Component_Generator_Page_Static',
             'component' => 'Vpc_Cc_Paragraphs_Master_Paragraphs_Component',
         );
-        $ret['chainedType'] = 'Cc';
+        $ret['flags']['chainedType'] = 'Cc';
         return $ret;
     }
 }
