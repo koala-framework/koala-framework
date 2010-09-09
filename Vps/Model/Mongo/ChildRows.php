b3e8fa58a3566518af5e82ac509108c529f8f165
Niko Sams <ns@vivid-planet.com>
2010-09-09 16:35:44 +0200

    const war in falschem interface, oopos

--- Vps/Model/Mongo/ChildRows.php
+++ Vps/Model/Mongo/ChildRows.php
@@ -158,7 +158,7 @@ class Vps_Model_Mongo_ChildRows extends Vps_Model_Data_Abstract
         foreach ($models as $model) {
             foreach ($model->_exprs as $column=>$expr) {
                 if ($expr instanceof Vps_Model_Select_Expr_Parent) {
-                    if ($model->getReference($expr->getParent()) === Vps_Model_SubModel_Interface::SUBMODEL_PARENT) {
+                    if ($model->getReference($expr->getParent()) === Vps_Model_RowsSubModel_Interface::SUBMODEL_PARENT) {
                         //nothing to do in that case
                     } else if ($model->getReferencedModel($expr->getParent()) === $row->getModel()) {
 
--- tests/Vps/Model/Mongo/ChildRowsWithParentExpr/MongoModel.php
+++ tests/Vps/Model/Mongo/ChildRowsWithParentExpr/MongoModel.php
@@ -12,7 +12,7 @@ class Vps_Model_Mongo_ChildRowsWithParentExpr_MongoModel extends Vps_Model_Mongo
                     'refModelClass' => 'Vps_Model_Mongo_ChildRowsWithParentExpr_ParentModel',
                     'column' => 'parent_id'
                 ),
-                'Mongo' => Vps_Model_SubModel_Interface::SUBMODEL_PARENT,
+                'Mongo' => Vps_Model_RowsSubModel_Interface::SUBMODEL_PARENT,
             ),
             'exprs' => array(
                 'parent_name' => new Vps_Model_Select_Expr_Parent('Parent', 'name'),
