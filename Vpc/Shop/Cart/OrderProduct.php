0e69b6ded038658b8d5a09fc967a269b3d63d22d
Niko Sams <ns@vivid-planet.com>
2010-09-09 10:02:33 +0200

    add_component_id darf leer sein, passiert wenn eine bestellung im backend angelegt wird

--- Vpc/Shop/Cart/OrderProduct.php
+++ Vpc/Shop/Cart/OrderProduct.php
@@ -4,10 +4,7 @@ class Vpc_Shop_Cart_OrderProduct extends Vps_Model_Db_Row
     protected function _beforeSave()
     {
         parent::_beforeSave();
-        if (!$this->add_component_id) {
-            $e = new Vps_Exception("add_component_id is required");
-            $e->logOrThrow();
-        }
+        //add_component_id darf leer sein, passiert wenn eine bestellung im backend angelegt wird
         if (!$this->add_component_class) {
             $e = new Vps_Exception("add_component_class is required");
             $e->logOrThrow();
