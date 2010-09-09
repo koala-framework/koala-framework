2df6bc76a3ac178a0d9168b735d95b1edc861e97
Niko Sams <ns@vivid-planet.com>
2010-09-09 17:47:18 +0200

    fix test

--- tests/Vps/Model/Mongo/ChildRowsTest/MongoModel.php
+++ tests/Vps/Model/Mongo/ChildRowsTest/MongoModel.php
@@ -5,7 +5,8 @@ class Vps_Model_Mongo_ChildRowsTest_MongoModel extends Vps_Model_Mongo_TestModel
     {
         parent::_init();
         $this->_dependentModels['Foo'] = new Vps_Model_Mongo_RowsSubModel(array(
-            'fieldName' => 'foo'
+            'fieldName' => 'foo',
+            'parentModel' => $this
         ));
     }
 }
