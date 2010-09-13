c74fd0dc68fcca7c0448944b50c4a7d2d7aaf28c
Niko Sams <ns@vivid-planet.com>
2010-09-13 16:51:40 +0200

    submodel_parent exprs nicht cachen

---
+++ tests/Vps/Model/Mongo/ChildRowsWithMirrorCacheSimple/Test.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @group Model
+ * @group Mongo
+ * @group Mongo_ChildRowsWithMirrorCacheSimple
+ */
+class Vps_Model_Mongo_ChildRowsWithMirrorCacheSimple_Test extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        Vps_Model_Abstract::clearInstances();
+    }
+
+    public function testIt()
+    {
+        $m = Vps_Model_Abstract::getInstance('Vps_Model_Mongo_ChildRowsWithMirrorCacheSimple_MongoModel');
+
+        $m->initialSync();
+
+        $this->assertEquals(1, $m->getProxyModel()->getCollection()->find()->count());
+        $row = $m->getProxyModel()->getCollection()->findOne();
+        $this->assertEquals('bar', $row['foo']);
+        $this->assertEquals(1, count($row['children']));
+        $this->assertEquals('blub', $row['children'][0]['blub']);
+    }
+
+    public function testParentExpr()
+    {
+        $m = Vps_Model_Abstract::getInstance('Vps_Model_Mongo_ChildRowsWithMirrorCacheSimple_MongoModel');
+        $m->initialSync();
+        $row = $m->getRow(1);
+        $this->assertEquals('bar', $row->getChildRows('Children')->current()->parent_foo);
+    }
+
+    public function testParentExprIsntCached()
+    {
+        $m = Vps_Model_Abstract::getInstance('Vps_Model_Mongo_ChildRowsWithMirrorCacheSimple_MongoModel');
+
+        $m->initialSync();
+        $row = $m->getProxyModel()->getCollection()->findOne();
+        $this->assertTrue(!isset($row['children'][0]['parent_foo']));
+    }
+}
