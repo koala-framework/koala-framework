a09c694cefaea433bd9968014d3fc8a1e899fdcc
Markus Hauser <mh@vivid-planet.com>
2010-09-13 18:12:21 +0200

    Text Component Cc gefixt, hat link components falsch geholt

--- Vpc/Basic/Text/Cc/Component.php
+++ Vpc/Basic/Text/Cc/Component.php
@@ -19,24 +19,10 @@ class Vpc_Basic_Text_Cc_Component extends Vpc_Chained_Cc_Component
         $childs = $this->getData()->getChildComponents();
         foreach ($ret['contentParts'] as $kPart=>$part) {
             if (is_array($part)) {
-                if ($part['type'] == 'image') {
-                    $part['nr'] = 'i'.$part['nr'];
-                } else if ($part['type'] == 'link') {
-                    $part['nr'] = 'l'.$part['nr'];
-                } else if ($part['type'] == 'download') {
-                    $part['nr'] = 'd'.$part['nr'];
-                } else {
-                    continue;
-                }
-                foreach ($childs as $child) {
-                    if ($child->dbId == $this->getData()->dbId.'-'.$part['nr']) {
-                        $ret['contentParts'][$kPart] = array(
-                            'type' => $part['type'],
-                            'component'=>$row
-                        );
-                        break;
-                    }
-                }
+                $component = self::getChainedByMaster($part['component'], $this->getData(), array(
+                    'ignoreVisible' => true
+                ));
+                $ret['contentParts'][$kPart]['component'] = $component;
             }
         }
         return $ret;
