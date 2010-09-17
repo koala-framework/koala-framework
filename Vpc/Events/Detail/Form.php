ad480762040ae239a683a458cb90ae0305a3c663
Franz Unger <ufx@vivid-planet.com>
2010-09-17 16:09:48 +0200

    Newskomponente: Breite von Feld in Admin geaendert

--- Vpc/Events/Detail/Form.php
+++ Vpc/Events/Detail/Form.php
@@ -9,6 +9,7 @@ class Vpc_Events_Detail_Form extends Vpc_News_Detail_Abstract_Form
         $this->add(new Vps_Form_Field_DateTimeField('start_date', trlVps('From')))
             ->setAllowBlank(false);
         $this->add(new Vps_Form_Field_DateTimeField('end_date', trlVps('To')));
-        $this->add(new Vps_Form_Field_TextField('place', trlVps('Place (City)')));
+        $this->add(new Vps_Form_Field_TextField('place', trlVps('Place (City)')))
+            ->setWidth(300);
     }
 }
