cce7742bc31e0c00692faaf562e6d17699d8e329
Niko Sams <ns@vivid-planet.com>
2010-09-21 17:12:04 +0200

    refactoring: kopierten code endlich vereinheitlicht

--- Vpc/Paragraphs/EditComponentsData.php
+++ Vpc/Paragraphs/EditComponentsData.php
@@ -9,53 +9,6 @@ class Vpc_Paragraphs_EditComponentsData extends Vps_Data_Abstract
         $this->_componentClass = $componentClass;
     }
 
-    //teilw. übernommen von Vpc_Directories_Item_Directory_ExtConfigAbstract
-    //TODO: code sharen
-    private function _getEditConfigs($componentClass, Vps_Component_Generator_Abstract $gen, $idTemplate, $componentIdSuffix)
-    {
-        $ret = array();
-        $cfg = Vpc_Admin::getInstance($componentClass)->getExtConfig();
-        foreach ($cfg as $k=>$c) {
-            if (!isset($this->_componentConfigs[$componentClass.'-'.$k])) {
-                $this->_componentConfigs[$componentClass.'-'.$k] = $c;
-            }
-            $ret[] = array(
-                'componentClass' => $componentClass,
-                'type' => $k,
-                'idTemplate' => $idTemplate,
-                'componentIdSuffix' => $componentIdSuffix
-            );
-        }
-        foreach ($gen->getGeneratorPlugins() as $plugin) {
-            $cls = get_class($plugin);
-            $cfg = Vpc_Admin::getInstance($cls)->getExtConfig();
-            foreach ($cfg as $k=>$c) {
-                if (!isset($this->_componentConfigs[$cls.'-'.$k])) {
-                    $this->_componentConfigs[$cls.'-'.$k] = $c;
-                }
-                $ret[] = array(
-                    'componentClass' => $cls,
-                    'type' => $k,
-                    'idTemplate' => $idTemplate,
-                    'componentIdSuffix' => $componentIdSuffix
-                );
-            }
-        }
-        if (Vpc_Abstract::hasSetting($componentClass, 'editComponents')) {
-            $editComponents = Vpc_Abstract::getSetting($componentClass, 'editComponents');
-            foreach ($editComponents as $c) {
-                $childGen = Vps_Component_Generator_Abstract::getInstances($componentClass, array('componentKey'=>$c));
-                $childGen = $childGen[0];
-                $cls = Vpc_Abstract::getChildComponentClass($componentClass, null, $c);
-                $edit = $this->_getEditConfigs($cls, $childGen,
-                                               $idTemplate,
-                                               $componentIdSuffix.$childGen->getIdSeparator().$c);
-                $ret = array_merge($ret, $edit);
-            }
-        }
-        return $ret;
-    }
-
     protected function _getComponentClassByRow($row)
     {
         $classes = Vpc_Abstract::getChildComponentClasses($this->_componentClass, 'paragraphs');
@@ -65,7 +18,11 @@ class Vpc_Paragraphs_EditComponentsData extends Vps_Data_Abstract
     public function load($row)
     {
         $gen = Vps_Component_Generator_Abstract::getInstance($this->_componentClass, 'paragraphs');
-        $ret = $this->_getEditConfigs($this->_getComponentClassByRow($row), $gen, '{componentId}-{0}', '');
+
+        $edit = Vps_Component_Abstract_ExtConfig_Abstract::getEditConfigs($this->_getComponentClassByRow($row), $gen, '{componentId}-{0}', '');
+        $this->_componentConfigs = array_merge($this->_componentConfigs, $edit['componentConfigs']);
+        $ret = $edit['contentEditComponents'];
+
         $component = Vps_Component_Data_Root::getInstance()->getComponentByDbId($row->component_id.'-'.$row->id, array('ignoreVisible'=>true));
         foreach (Vps_Controller_Action_Component_PagesController::getSharedComponents($component) as $cls=>$cmp) {
             $cfg = Vpc_Admin::getInstance($cls)->getExtConfig(Vps_Component_Abstract_ExtConfig_Abstract::TYPE_SHARED);
