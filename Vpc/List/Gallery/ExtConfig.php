a0132ab3575368402c9749d7e4ea28affac8d92a
Niko Sams <ns@vivid-planet.com>
2010-09-21 14:15:00 +0200

    settings an 2. stelle aber beim anlegen aufmagen

--- Vpc/List/Gallery/ExtConfig.php
+++ Vpc/List/Gallery/ExtConfig.php
@@ -3,9 +3,13 @@ class Vpc_List_Gallery_ExtConfig extends Vpc_Abstract_List_ExtConfigListUpload
 {
     protected function _getConfig()
     {
-        $ret = array(
-            'settings' => $this->_getStandardConfig('vps.autoform', 'Settings', trlVps('Settings'))
-        );
-        return array_merge($ret, parent::_getConfig());
+        $ret = parent::_getConfig();
+        $ret['settings'] = $this->_getStandardConfig('vps.autoform', 'Settings', trlVps('Settings'), new Vps_Asset('wrench'));
+        return $ret;
+    }
+
+    public function getEditAfterCreateConfigKey()
+    {
+        return 'settings';
     }
 }
