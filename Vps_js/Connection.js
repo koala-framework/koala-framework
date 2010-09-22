890176061bf78783c2ab97ff910bd51b8d5efbb9
Niko Sams <ns@vivid-planet.com>
2010-09-22 09:55:50 +0200

    comma und seimocolon fixes

--- Vps_js/Connection.js
+++ Vps_js/Connection.js
@@ -149,7 +149,7 @@ Vps.Connection = Ext.extend(Ext.data.Connection, {
             plain:true,
             footer:true,
             closable:false
-        });;
+        });
         var dlg = new Ext.Window(cfg);
 
         if (typeof cfg.showCancel == 'undefined' || cfg.showCancel) {
@@ -314,7 +314,7 @@ Vps.Connection = Ext.extend(Ext.data.Connection, {
             if (options.vpsCallback.success) {
                 options.vpsCallback.success.call(options.vpsCallback.scope, response, options, r);
             }
-        };
+        }
     },
     vpsNoJsonSuccess: function(response, options)
     {
