d34f862fa81aa3e664abc5a36f536ddc3b0a4628
Niko Sams <ns@vivid-planet.com>
2010-09-24 08:34:33 +0200

    fix test

--- tests/Vpc/Columns/TestComponent.php
+++ tests/Vpc/Columns/TestComponent.php
@@ -4,11 +4,9 @@ class Vpc_Columns_TestComponent extends Vpc_Columns_Component
     public static function getSettings($parentComponentClass)
     {
         $ret = parent::getSettings($parentComponentClass);
-        $ret['generators']['child']['model'] = 'Vpc_Columns_TestColumnsModel';
         $ret['generators']['child']['component'] = 'Vpc_Columns_TestComponent_Column';
         $ret['ownModel'] = 'Vpc_Columns_TestModel';
+        $ret['childModel'] = 'Vpc_Columns_TestColumnsModel';
         return $ret;
     }
-
-
 }
