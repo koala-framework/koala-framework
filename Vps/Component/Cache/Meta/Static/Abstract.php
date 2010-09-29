f1bb608ee046376cd5e10d974ed54a8db6a962a0
Franz Unger <ufx@vivid-planet.com>
2010-09-29 17:26:18 +0200

    Callback-Cache loeschen Bug

--- Vps/Component/Cache/Meta/Static/Abstract.php
+++ Vps/Component/Cache/Meta/Static/Abstract.php
@@ -1,12 +1,16 @@
 <?php
 abstract class Vps_Component_Cache_Meta_Static_Abstract extends Vps_Component_Cache_Meta_Abstract
 {
-    protected $_model;
-    protected $_pattern = '{component_id}';
+    protected $_pattern;
+
+    public function __construct($pattern)
+    {
+        $this->_pattern = $pattern;
+    }
 
     public function getModelname($componentClass)
     {
-        return $this->_getModelName($this->_model);
+        return null;
     }
 
     public function getPattern()
