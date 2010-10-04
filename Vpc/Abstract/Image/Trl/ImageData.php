98a9768d99ed3cd6347831cb27cc7cc7608a3107
Niko Sams <ns@vivid-planet.com>
2010-10-04 17:20:11 +0200

    fix trl text-image mit download

--- Vpc/Abstract/Image/Trl/ImageData.php
+++ Vpc/Abstract/Image/Trl/ImageData.php
@@ -35,6 +35,10 @@ class Vpc_Abstract_Image_Trl_ImageData extends Vps_Data_Abstract implements Vps_
     protected function _getMasterImageUrl($componentId)
     {
         $c = Vps_Component_Data_Root::getInstance()->getComponentById($componentId, array('ignoreVisible'=>true));
+        if (!is_instance_of($c->componentClass, 'Vpc_Abstract_Image_Trl_Component')) {
+            //kann vorkommen bei TextImage->LinkTag->EnlargeTag wenn was anderes als EnlergeTag einstellt ist
+            return null;
+        }
         $row = $c->chained->getComponent()->getRow();
         if (!$row) return null;
         $row = $row->getParentRow('Image');
