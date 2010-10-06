cacbd38dca6720ef2ceb254165f8b3c6f241ceda
Niko Sams <ns@vivid-planet.com>
2010-10-06 16:08:49 +0200

    check-for-invalid in eigenem prozess aufrufen, damit keine db verbindung emacht wird was beim forken probleme macht

--- Vps/Controller/Action/Cli/Web/FulltextController.php
+++ Vps/Controller/Action/Cli/Web/FulltextController.php
@@ -63,7 +63,7 @@ class Vps_Controller_Action_Cli_Web_FulltextController extends Vps_Controller_Ac
 
     public function rebuildAction()
     {
-        $this->_checkForInvalid();
+        system("php bootstrap.php check-for-invalid");
 
         $queueFile = 'application/temp/fulltextRebuildQueue';
 
