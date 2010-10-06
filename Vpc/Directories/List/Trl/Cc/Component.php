403e9ec6611e39257af5b6dbc9c2a11241a8679c
Niko Sams <ns@vivid-planet.com>
2010-10-06 16:29:20 +0200

    korrektes select holen

--- Vpc/Directories/List/Trl/Cc/Component.php
+++ Vpc/Directories/List/Trl/Cc/Component.php
@@ -1,4 +1,20 @@
 <?php
 class Vpc_Directories_List_Trl_Cc_Component extends Vpc_Directories_List_Cc_Component
 {
+    public function getSelect()
+    {
+        $itemDirectory = $this->getItemDirectory();
+        if (!$itemDirectory) return null;
+        if (is_string($itemDirectory)) {
+            throw new Vps_Exception_NotYetImplemented();
+        } else {
+            if (Vpc_Abstract::getSetting($this->getData()->chained->chained->componentClass, 'useDirectorySelect')) {
+                $ret = $itemDirectory->getComponent()->getSelect();
+            } else {
+                $ret = $itemDirectory->getGenerator('detail')
+                    ->select($this->getItemDirectory());
+            }
+        }
+        return $ret;
+    }
 }
