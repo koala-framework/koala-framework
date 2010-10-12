60b54ed348cb2d020bedc0d76b430d1ea74c8b19
Niko Sams <ns@vivid-planet.com>
2010-10-12 15:37:33 +0200

    MasterAsChild generell brauchbar gemacht

---
+++ Vpc/Chained/Abstract/MasterAsChild/Component.php
@@ -0,0 +1,44 @@
+<?php
+class Vpc_Chained_Abstract_MasterAsChild_Component extends Vpc_Abstract
+{
+    public static function getSettings($masterComponentClass)
+    {
+        $ret = parent::getSettings();
+        if (!$masterComponentClass) {
+            throw new Vps_Exception("This component requires a parameter");
+        }
+        $ret['masterComponentClass'] = $masterComponentClass;
+        $ret['generators']['child'] = array(
+            'class' => 'Vps_Component_Generator_Static',
+            'component' => $masterComponentClass,
+        );
+        $ret['editComponents'] = array('child');
+        try {
+            $ret['componentName'] = Vpc_Abstract::getSetting($masterComponentClass, 'componentName');
+        } catch (Exception $e) {}
+        try {
+            $ret['componentIcon'] = Vpc_Abstract::getSetting($masterComponentClass, 'componentIcon');
+        } catch (Exception $e) {}
+        return $ret;
+    }
+
+    /*
+    public function sendContent()
+    {
+        d('lala');
+        $this->getData()->getChildComponent('-child')->getComponent()->sendContent();
+    }
+    */
+
+    public function getTemplateVars()
+    {
+        $ret = parent::getTemplateVars();
+        $ret['child'] = $this->getData()->getChildComponent('-child');
+        return $ret;
+    }
+
+    public function hasContent()
+    {
+        return $this->getData()->getChildComponent('-child')->hasContent();
+    }
+}
--- Vpc/Chained/Trl/MasterAsChild/Component.php
+++ Vpc/Chained/Trl/MasterAsChild/Component.php
@@ -1,41 +1,4 @@
 <?php
-class Vpc_Chained_Trl_MasterAsChild_Component extends Vpc_Abstract
+class Vpc_Chained_Trl_MasterAsChild_Component extends Vpc_Chained_Abstract_MasterAsChild_Component
 {
-    public static function getSettings($masterComponentClass)
-    {
-        $ret = parent::getSettings();
-        if (!$masterComponentClass) {
-            throw new Vps_Exception("This component requires a parameter");
-        }
-        $ret['masterComponentClass'] = $masterComponentClass;
-        $ret['generators']['child'] = array(
-            'class' => 'Vps_Component_Generator_Static',
-            'component' => $masterComponentClass,
-        );
-        $ret['editComponents'] = array('child');
-        try {
-            $ret['componentName'] = Vpc_Abstract::getSetting($masterComponentClass, 'componentName');
-        } catch (Exception $e) {}
-        try {
-            $ret['componentIcon'] = Vpc_Abstract::getSetting($masterComponentClass, 'componentIcon');
-        } catch (Exception $e) {}
-        return $ret;
-    }
-
-    public function sendContent()
-    {
-        $this->getData()->getChildComponent('-child')->getComponent()->sendContent();
-    }
-
-    public function getTemplateVars()
-    {
-        $ret = parent::getTemplateVars();
-        $ret['child'] = $this->getData()->getChildComponent('-child');
-        return $ret;
-    }
-
-    public function hasContent()
-    {
-        return $this->getData()->getChildComponent('-child')->hasContent();
-    }
 }
