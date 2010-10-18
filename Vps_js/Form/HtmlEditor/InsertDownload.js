4e086c7c3db22561b57b7d2335120d55398463c6
Niko Sams <ns@vivid-planet.com>
2010-10-18 15:02:32 +0200

    InsertDownload dialog

---
+++ Vps_js/Form/HtmlEditor/InsertDownload.js
@@ -0,0 +1,73 @@
+Vps.Form.HtmlEditor.InsertDownload = function(config) {
+    Ext.apply(this, config);
+
+    var panel = Ext.ComponentMgr.create(Ext.applyIf(this.componentConfig, {
+        baseCls: 'x-plain',
+        formConfig: {
+            tbar: false
+        }
+    }));
+    this.downloadDialog = new Vps.Auto.Form.Window({
+        autoForm: panel,
+        width: 450,
+        height: 400,
+        autoLoad: false
+    });
+};
+Ext.extend(Vps.Form.HtmlEditor.InsertDownload, Ext.util.Observable, {
+    init: function(cmp){
+        this.cmp = cmp;
+        this.cmp.on('afterCreateToolbar', this.afterCreateToolbar, this);
+    },
+
+    // private
+    afterCreateToolbar: function(tb) {
+        tb.insert(8, {
+            icon: '/assets/silkicons/folder_link.png',
+            handler: this.onInsertDownload,
+            scope: this,
+            tooltip: {
+                cls: 'x-html-editor-tip',
+                title: trlVps('Download'),
+                text: trlVps('Create new Download for the selected text or edit selected Download.')
+            },
+            cls: 'x-btn-icon',
+            clickEvent: 'mousedown',
+            tabIndex: -1
+        });
+    },
+
+    onInsertDownload: function() {
+        var a = this.cmp.getFocusElement('a');
+        if (a && a.tagName && a.tagName.toLowerCase() == 'a') {
+            var expr = new RegExp(this.componentId+'-d([0-9]+)');
+            var m = a.href.match(expr);
+            if (m) {
+                var nr = parseInt(m[1]);
+            }
+            if (nr) {
+                this.downloadDialog.un('datachange', this._insertDownloadLink, this);
+                this.downloadDialog.showEdit({
+                    componentId: this.componentId+'-d'+nr
+                });
+                return;
+            }
+        }
+        Ext.Ajax.request({
+            params: {componentId: this.componentId},
+            url: this.controllerUrl+'/json-add-download',
+            success: function(response, options, r) {
+                this.downloadDialog.un('datachange', this._insertDownloadLink, this);
+                this.downloadDialog.showEdit({
+                    componentId: r.componentId
+                });
+                this.downloadDialog.on('datachange', this._insertDownloadLink, this, { single: true });
+            },
+            scope: this
+        });
+    },
+    _insertDownloadLink : function() {
+        var params = this.downloadDialog.getAutoForm().getBaseParams();
+        this.relayCmd('createlink', params.componentId);
+    }
+});
