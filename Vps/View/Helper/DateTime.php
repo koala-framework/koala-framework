6f82f0c08cd9266c16295f0cda0c09e634617346
Niko Sams <ns@vivid-planet.com>
2010-10-18 16:26:36 +0200

    Date-View-Helper: fallback wenn keine view gesetzt
    passiert zB bei excel export

--- Vps/View/Helper/DateTime.php
+++ Vps/View/Helper/DateTime.php
@@ -4,9 +4,9 @@ class Vps_View_Helper_DateTime extends Vps_View_Helper_Date
     public function dateTime($date, $format = null)
     {
         if (!$format) {
-            if ($this->_view->item && $this->_view->item instanceof Vps_Component_Data) {
+            if ($this->_view && $this->_view->item && $this->_view->item instanceof Vps_Component_Data) {
                 $format = $this->_view->item->trlVps('Y-m-d H:i');
-            } else if ($this->_view->data && $this->_view->data instanceof Vps_Component_Data) {
+            } else if ($this->_view && $this->_view->data && $this->_view->data instanceof Vps_Component_Data) {
                 $format = $this->_view->data->trlVps('Y-m-d H:i');
             } else {
                 $format = trlVps('Y-m-d H:i');
