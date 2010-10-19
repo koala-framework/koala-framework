76b2880978d8faba3c2f70b15bbc44932f018286
Franz Unger <ufx@vivid-planet.com>
2010-10-19 16:24:59 +0200

    OutputPlaceholdersPlugin/Test geht wieder

--- tests/Vps/Component/OutputPlaceholdersPlugin/Test.php
+++ tests/Vps/Component/OutputPlaceholdersPlugin/Test.php
@@ -2,32 +2,15 @@
 /**
  * @group Component_OutputPlaceholder
  */
-class Vps_Component_OutputPlaceholdersPlugin_Test extends PHPUnit_Framework_TestCase
+class Vps_Component_OutputPlaceholdersPlugin_Test extends Vpc_TestAbstract
 {
-    protected $_output;
-    protected static $_templates = array();
-    protected static $_expectedCalls = 0;
-    protected static $_calls = 0;
-
     public function setUp()
     {
-        $this->markTestIncomplete();
-        Vps_Component_Data_Root::setComponentClass('Vps_Component_OutputPlaceholdersPlugin_Root_Component');
-        $this->_root = Vps_Component_Data_Root::getInstance();
-
-        $this->_output = new Vps_Component_Output_Cache();
-        Vps_Component_Cache::setBackend(Vps_Component_Cache::CACHE_BACKEND_FNF);
+        parent::setUp('Vps_Component_OutputPlaceholdersPlugin_Root_Component');
     }
 
     public function testCached()
     {
-        $this->_output->renderMaster($this->_root); //caches it
-
-        $this->assertEquals('xLorem child bar bar Ipsumy', $this->_output->renderMaster($this->_root));
-    }
-
-    public function testUncached()
-    {
-        $this->assertEquals('xLorem child bar bar Ipsumy', $this->_output->renderMaster($this->_root));
+        $this->assertEquals('xLorem child bar bar Ipsumy', $this->_root->render(true, true));
     }
 }
