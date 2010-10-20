2c2e4e0bf7ee1e6f7f2bae289ba99c3a4e018a48
Niko Sams <ns@vivid-planet.com>
2010-10-20 14:18:48 +0200

    block styles test, alter bugreport durch tinymce behoben

--- tests/Vpc/Basic/TextSessionModel/SeleniumTest.php
+++ tests/Vpc/Basic/TextSessionModel/SeleniumTest.php
@@ -129,4 +129,62 @@ class Vpc_Basic_TextSessionModel_SeleniumTest extends Vps_Test_SeleniumTestCase
         $this->selectFrame('relative=top');
     }
 
+    public function testBlockStyle()
+    {
+        $this->openVpcEdit('Vpc_Basic_TextSessionModel_TestComponent', 'root_text');
+        $this->waitForConnections();
+
+        //insert some text
+        $this->typeKeys("document.getElementsByTagName('iframe')[0].contentDocument.body", 'bar baz');
+        usleep(100*1000);
+        $this->select('css=select.x-block-style-select', 'label=Test1');
+
+        //assert inserted tag
+        $this->selectFrame('//iframe');
+        $this->assertAttribute('//h1/@class', 'style1');
+        $this->selectFrame('relative=top');
+
+        $this->select('css=select.x-block-style-select', 'label=Default');
+
+        //assert inserted tag
+        $this->selectFrame('//iframe');
+        $this->assertElementNotPresent('//h1');
+        $this->assertElementPresent('//p');
+        $this->selectFrame('relative=top');
+    }
+
+    public function testBlockStyleOnTwoParagraphs()
+    {
+        $this->openVpcEdit('Vpc_Basic_TextSessionModel_TestComponent', 'root_text');
+        $this->waitForConnections();
+
+        //insert some text
+        $this->typeKeys("document.getElementsByTagName('iframe')[0].contentDocument.body", "bar baz\n");
+        $this->keyPress("document.getElementsByTagName('iframe')[0].contentDocument.body", 13);
+
+        $this->selectFrame('//iframe');
+        $this->assertXpathCount('//body/p', 2);
+        $this->selectFrame('relative=top');
+
+        //select some text
+        $this->runScript('var editor = null;
+            Ext.ComponentMgr.all.each(function(c) { if (c instanceof Vps.Form.HtmlEditor) { editor = c; return false; } });
+            var bookmark = {
+                start: [0,0,0],
+                end: [3,0,1]
+            };
+            editor.tinymceEditor.selection.moveToBookmark(bookmark);
+            editor.onEditorEvent();
+        ');
+
+        $this->select('css=select.x-block-style-select', 'label=Test1');
+
+        $this->selectFrame('//iframe');
+        $this->assertXpathCount('//body/p', 0);
+        $this->assertXpathCount('//body/h1', 2);
+        $this->selectFrame('relative=top');
+
+        $this->assertSelectedLabel('css=select.x-block-style-select', 'Test1');
+    }
+
 }
