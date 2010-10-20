e4e05d324f0f37facf34ff25b8a7164cf30f3200
Niko Sams <ns@vivid-planet.com>
2010-10-20 14:47:24 +0200

    fix styles editor im test

--- tests/Vpc/Basic/TextSessionModel/TestStylesModel.php
+++ tests/Vpc/Basic/TextSessionModel/TestStylesModel.php
@@ -3,10 +3,11 @@ class Vpc_Basic_TextSessionModel_TestStylesModel extends Vpc_Basic_Text_StylesMo
 {
     public function __construct($config = array())
     {
-        $config['proxyModel'] = new Vps_Model_FnF(array(
+        $config['proxyModel'] = new Vps_Model_Session(array(
+                'namespace' => 'TextSessionModel_TestStylesModel',
                 'uniqueIdentifier' => 'Vpc_Basic_Text_TestStylesModel_Proxy',
                 'columns' => array('id', 'pos', 'name', 'tag', 'ownStyles', 'styles'),
-                'data'=> array(
+                'defaultData'=> array(
                     array('id'=>1, 'pos'=>1, 'name'=>'Test1', 'tag'=>'h1', 'ownStyles'=>'', 'styles'=>serialize(array('font_weight'=>'bold', 'font_size'=>'10', 'text_align'=>'center'))),
                     array('id'=>2, 'pos'=>2, 'name'=>'Test2', 'tag'=>'p', 'ownStyles'=>'', 'styles'=>serialize(array('font_size'=>'10', 'color'=>'ff0000'))),
                     array('id'=>3, 'pos'=>3, 'name'=>'Test3', 'tag'=>'span', 'ownStyles'=>'', 'styles'=>serialize(array('font_size'=>'8', 'color'=>'00ff00'))),
@@ -14,6 +15,7 @@ class Vpc_Basic_TextSessionModel_TestStylesModel extends Vpc_Basic_Text_StylesMo
             ));
         parent::__construct($config);
     }
+
     public function getMasterStyles()
     {
         return array();
