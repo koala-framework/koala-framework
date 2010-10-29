b34e68aa678596f9f85bc3b99899b884a5f672aa
Niko Sams <ns@vivid-planet.com>
2010-10-29 09:26:18 +0200

    type parameter entfernt, gibts scheinbar nicht mehr

--- Vps/Component/View/Helper/ImageUrl.php
+++ Vps/Component/View/Helper/ImageUrl.php
@@ -1,8 +1,8 @@
 <?php
 class Vps_Component_View_Helper_ImageUrl extends Vps_Component_View_Helper_ImageParam
 {
-    public function imageUrl($image, $type = 'default')
+    public function imageUrl($image)
     {
-        return $this->_getImageUrl($image, $type);
+        return $this->_getImageUrl($image);
     }
 }
