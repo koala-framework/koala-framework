fe0f606bb7caf33aadcdce9a0549444390af9fa3
Niko Sams <ns@vivid-planet.com>
2010-10-29 09:26:35 +0200

    imageParam helper repariert

--- Vps/Component/View/Helper/ImageParam.php
+++ Vps/Component/View/Helper/ImageParam.php
@@ -1,10 +1,15 @@
 <?php
 class Vps_Component_View_Helper_ImageParam extends Vps_Component_View_Helper_Image
 {
-    public function imageParam($image, $param, $type = 'default')
+    public function imageParam($image, $param)
     {
-        $data = $this->_getImageParams($image, $type);
-        if (!$data) return '';
-        return $data[$param];
+        if ($param == 'url') {
+            return $this->_getImageUrl($image);
+        } else if ($param == 'width' || $param == 'height') {
+            $size = $this->_getImageSize($image);
+            return $size[$param];
+        } else {
+            throw new Vps_Exception_NotYetImplemented();
+        }
     }
 }
