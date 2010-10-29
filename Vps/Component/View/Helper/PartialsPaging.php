2804bad3f2dcd2d05f4f89df8aed99ca1f8d1135
Niko Sams <ns@vivid-planet.com>
2010-10-29 11:20:54 +0200

    helper der gar nicht mehr funktionieren kann entfernt

--- Vps/Component/View/Helper/PartialsPaging.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Vps_Component_View_Helper_PartialsPaging extends Vps_Component_View_Helper_Partials
-{
-    public function partialsPaging($component)
-    {
-        return $this->partials($component, 'Vps_Component_Partial_Paging');
-    }
-}
