e2b54e9080057cae03c52d2ba9f2ecceb9a856ea
Niko Sams <ns@vivid-planet.com>
2010-11-05 09:42:06 +0100

    row kann auch null sein

--- Vps/Form/Field/MultiFields.php
+++ Vps/Form/Field/MultiFields.php
@@ -106,7 +106,7 @@ class Vps_Form_Field_MultiFields extends Vps_Form_Field_Abstract
         }
         return $rows;
     }
-    public function load(Vps_Model_Row_Interface $row, $postData = array())
+    public function load($row, $postData = array())
     {
         if (isset($postData[$this->getFieldName()])) {
             $postData = $postData[$this->getFieldName()];
