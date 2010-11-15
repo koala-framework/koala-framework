9e3ee91ef71809dceeb981b3b90077ab7a404180
Franz Unger <ufx@vivid-planet.com>
2010-11-15 09:08:24 +0100

    getCacheMeta hat da nicht richtig funktioniert

--- Vpc/Forum/Thread/Preview/Component.php
+++ Vpc/Forum/Thread/Preview/Component.php
@@ -13,12 +13,6 @@ class Vpc_Forum_Thread_Preview_Component extends Vpc_Abstract
     {
         $ret = parent::getCacheMeta();
         $ret[] = new Vps_Component_Cache_Meta_Static_Model($row->getModel(), '{component_id}-moderate-close');
-        return $ret;
-    }
-
-    public function getCacheMeta()
-    {
-        $ret = parent::getCacheMeta();
         $threadVars = $this->getData()->parent->getComponent()->getThreadVars();
         if ($threadVars['lastPost']->user) {
             $ret[] = new Vps_Component_Cache_Meta_Row($threadVars['lastPost']->user->row);
