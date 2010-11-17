10ccd4d6d4a7302b4354859fa1d45cf9cb335f49
Niko Sams <ns@vivid-planet.com>
2010-11-17 14:35:09 +0100

    fix test

--- tests/Vpc/Basic/Image/BestFitWithZeroHeightComponent.php
+++ tests/Vpc/Basic/Image/BestFitWithZeroHeightComponent.php
@@ -4,7 +4,7 @@ class Vpc_Basic_Image_BestFitWithZeroHeightComponent extends Vpc_Basic_Image_Tes
     public static function getSettings()
     {
         $ret = parent::getSettings();
-        $ret['dimensions'] = array(array('width'=>100, 'height'=>null, 'scale'=>Vps_Media_Image::SCALE_BESTFIT));
+        $ret['dimensions'] = array(array('width'=>100, 'height'=>null, 'scale'=>Vps_Media_Image::SCALE_DEFORM));
         return $ret;
     }
 
