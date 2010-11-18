58325f5855272ca3138ef622aafe4a9b9c096396
Franz Unger <ufx@vivid-planet.com>
2010-11-18 09:46:42 +0100

    Cache Meta schreiben mit DbId bei Directories und GeneratorRow verbessert

--- Vps/Component/Cache/Meta/Static/GeneratorRow.php
+++ Vps/Component/Cache/Meta/Static/GeneratorRow.php
@@ -8,9 +8,14 @@ class Vps_Component_Cache_Meta_Static_GeneratorRow extends Vps_Component_Cache_M
             foreach (Vpc_Abstract::getSetting($class, 'generators') as $key => $gData) {
                 if ($gData['component'] == $componentClass) {
                     $generator = Vps_Component_Generator_Abstract::getInstance($class, $key);
+                    if (isset($gData['dbIdShortcut'])) {
+                        $pattern = $gData['dbIdShortcut'];
+                    } else {
+                        $pattern = '%' . $generator->getIdSeparator();
+                    }
+                    $pattern .= '{id}';
                     $ret[] = new Vps_Component_Cache_Meta_Static_Model(
-                        $generator->getModel(),
-                        '%' . $generator->getIdSeparator() . '{id}'
+                        $generator->getModel(), $pattern
                     );
                 }
             }
