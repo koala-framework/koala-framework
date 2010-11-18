d635ca1613df9298382d2108537740c61c53bcb0
Markus Hauser <mh@vivid-planet.com>
2010-11-18 14:21:32 +0100

    communityVideo trl

---
+++ Vpc/Advanced/CommunityVideo/Trl/Component.php
@@ -0,0 +1,25 @@
+<?php
+class Vpc_Advanced_CommunityVideo_Trl_Component extends Vpc_Chained_Trl_Component
+{
+    public static function getSettings($masterComponentClass)
+    {
+        $ret = parent::getSettings($masterComponentClass);
+        $ret['extConfig'] = 'Vps_Component_Abstract_ExtConfig_Form';
+        $ret['ownModel'] = 'Vps_Component_FieldModel';
+        return $ret;
+    }
+
+    public function getTemplateVars()
+    {
+        $ret = parent::getTemplateVars();
+
+        $url = $this->getRow()->url;
+        if ($url) {
+            $ret['flash']['data']['url'] = Vpc_Advanced_CommunityVideo_Component::replaceUrl($this->getRow());
+            if ($this->getRow()->width) $ret['flash']['data']['width'] = $this->getRow()->width;
+            if ($this->getRow()->height) $ret['flash']['data']['height'] = $this->getRow()->height;
+        }
+
+        return $ret;
+    }
+}
---
+++ Vpc/Advanced/CommunityVideo/Trl/Controller.php
@@ -0,0 +1,5 @@
+<?php
+class Vpc_Advanced_CommunityVideo_Trl_Controller extends Vps_Controller_Action_Auto_Vpc_Form
+{
+    protected $_formName = 'Vpc_Advanced_CommunityVideo_Trl_Form';
+}
