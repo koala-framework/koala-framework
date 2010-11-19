8ddc9cb819bccebc415da7c4590bebb0f8418f8e
Markus Hauser <mh@vivid-planet.com>
2010-11-19 13:52:04 +0100

    form dynamic test fix

--- tests/Vpc/FormDynamic/Basic/Form/TestModel.php
+++ tests/Vpc/FormDynamic/Basic/Form/TestModel.php
@@ -3,6 +3,6 @@ class Vpc_FormDynamic_Basic_Form_TestModel extends Vps_Model_FnF
 {
     protected $_primaryKey = 'component_id';
     protected $_data = array(
-        array('component_id'=>'root_form')
+        array('component_id'=>'root_form', 'recipient' => 'vivid@vivid.vps')
     );
 }
