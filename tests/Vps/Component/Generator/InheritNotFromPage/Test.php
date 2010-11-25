c4b7e95bcdff899502d71f225fc833c69b55261a
Franz Unger <ufx@vivid-planet.com>
2010-11-25 11:22:54 +0100

    Weitere Tests an PHP 5.3 angepasst

--- tests/Vps/Component/Generator/InheritNotFromPage/Test.php
+++ tests/Vps/Component/Generator/InheritNotFromPage/Test.php
@@ -3,13 +3,11 @@
  * @group Generator
  * @group GeneratorInherit
  */
-class Vps_Component_Generator_InheritNotFromPage_Test extends PHPUnit_Framework_TestCase
+class Vps_Component_Generator_InheritNotFromPage_Test extends Vpc_TestAbstract
 {
-    private $_root;
     public function setUp()
     {
-        Vps_Component_Data_Root::setComponentClass('Vps_Component_Generator_InheritNotFromPage_Root');
-        $this->_root = Vps_Component_Data_Root::getInstance();
+        parent::setUp('Vps_Component_Generator_InheritNotFromPage_Root');
     }
 
     public function testTitleBox()
