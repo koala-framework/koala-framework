6670f8e9f7394a07b07392fba40e5c1940b50666
Markus Hauser <mh@vivid-planet.com>
2010-12-03 09:13:23 +0100

    wollt ich gestern eigentlich nicht umbenennen...

--- Vpc/Form/Dynamic/Component.php
+++ Vpc/Form/Dynamic/Component.php
@@ -4,7 +4,7 @@ class Vpc_Form_Dynamic_Component extends Vpc_Abstract_Composite_Component
     public static function getSettings()
     {
         $ret = parent::getSettings();
-        $ret['componentName'] = trlVps('Form.Dynamic');
+        $ret['componentName'] = trlVps('Form');
         $ret['componentIcon'] = new Vps_Asset('application_form');
         $ret['generators']['child']['component']['paragraphs'] = 'Vpc_Form_Dynamic_Paragraphs_Component';
         $ret['generators']['child']['component']['form'] = 'Vpc_Form_Dynamic_Form_Component';
