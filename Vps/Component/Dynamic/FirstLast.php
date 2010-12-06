b3d29484827bf3d65b0fd7b55816af07c2cf804e
Christoph Eder <ce@vivid-planet.com>
2010-12-06 17:03:32 +0100

    - first und last fuer partials

---
+++ Vps/Component/Dynamic/FirstLast.php
@@ -0,0 +1,20 @@
+<?php
+class Vps_Component_Dynamic_FirstLast extends Vps_Component_Dynamic_Abstract
+{
+    public function setArguments()
+    {
+    }
+
+    public function getContent()
+    {
+        $ret = '';
+        $info = $this->_info['partial'];
+        if ($info['number'] == 0) {
+            $ret .= ' first';
+        }
+        if ($info['number'] == $info['total']-1) {
+            $ret .= ' last';
+        }
+        return trim($ret);
+    }
+}
