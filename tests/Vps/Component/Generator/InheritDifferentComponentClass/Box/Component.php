3f1569f7516fab8587b7f1f523ed8fc64419556e
Niko Sams <ns@vivid-planet.com>
2010-12-09 08:45:34 +0100

    Test an neue technik angepasst, inheritComponentClass setting gibts nicht mehr

--- tests/Vps/Component/Generator/InheritDifferentComponentClass/Box/Component.php
+++ tests/Vps/Component/Generator/InheritDifferentComponentClass/Box/Component.php
@@ -4,7 +4,27 @@ class Vps_Component_Generator_InheritDifferentComponentClass_Box_Component exten
     public static function getSettings()
     {
         $ret = parent::getSettings();
-        $ret['inheritComponentClass'] = 'Vps_Component_Generator_InheritDifferentComponentClass_Box_Inherit_Component';
+        $ret['flags']['alternativeComponent'] = 'Vps_Component_Generator_InheritDifferentComponentClass_Box_Inherit_Component';
         return $ret;
+    }
+
+    public static function useAlternativeComponent($componentClass, $parentData, $generator)
+    {
+        $c = $parentData;
+        while (!$c->inherits) $c = $c->parent;
+
+        $c = $c->parent;
+        if (!$c) return false;
+        while (!$c->inherits) $c = $c->parent;
+
+        $instances = Vps_Component_Generator_Abstract::getInstances($c, array(
+                'inherit' => true
+        ));
+        if (in_array($generator, $instances, true)) {
+            //wir wurden geerbt weils über uns ein parentData mit dem gleichen generator gibt
+            return true;
+        } else {
+            return false;
+        }
     }
 }
