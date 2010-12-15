b873083430339e2d333e7779b3741572291960b0
Franz Unger <ufx@vivid-planet.com>
2010-12-15 18:11:44 +0100

    Update-Scripts haben teilw. nicht gepasst

--- Vps/Update/33044.sql
+++ Vps/Update/33044.sql
@@ -1,4 +1,5 @@
 #tags: vpc
+DROP TABLE IF EXISTS `cache_component_url_parents`;
 CREATE TABLE `cache_component_url_parents` (
 `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
 `page_id` VARCHAR( 200 ) NOT NULL ,
--- Vps/Update/33046.sql
+++ Vps/Update/33046.sql
@@ -1,6 +1,8 @@
 #tags: vpc
-CREATE TABLE `cache_component_url` (
-`url` VARCHAR( 255 ) NOT NULL ,
-`page_id` VARCHAR( 200 ) NOT NULL ,
-PRIMARY KEY ( `url` )
+DROP TABLE IF EXISTS `cache_component_url`;
+CREATE TABLE IF NOT EXISTS `cache_component_url` (
+  `url` varchar(255) NOT NULL,
+  `page_id` varchar(200) NOT NULL,
+  `page` text NOT NULL,
+  PRIMARY KEY (`url`)
 ) ENGINE = MYISAM;
