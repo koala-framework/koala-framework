cf1457b4c063860e7d85dc8be8c8a0594a93c8be
Niko Sams <ns@vivid-planet.com>
2010-12-15 10:06:52 +0100

    Renderer::getPlugins auch entfernt, jippie

--- Vps/Component/Renderer.php
+++ Vps/Component/Renderer.php
@@ -19,26 +19,4 @@ class Vps_Component_Renderer extends Vps_Component_Renderer_Abstract
         $this->_isRenderMaster = $renderMaster;
         return parent::renderComponent($component);
     }
-
-    /**
-     * Gibt die Plugins für die Komponente *genau einmal* zurück
-     *
-     * Der Helper fragt einmal beim Master (falls es einen gibt) und einmal
-     * bei der Komponente
-     */
-    public function getPlugins(Vps_Component_Data $component)
-    {
-        $ret = array();
-        $componentId = $component->componentId;
-
-        if (!isset($this->_plugins[$componentId])) {
-            $this->_plugins[$componentId] = $component->getPlugins();
-        }
-
-        if (count($this->_plugins[$componentId])) {
-            $ret = $this->_plugins[$componentId];
-            $this->_plugins[$componentId] = array();
-        }
-        return $ret;
-    }
 }
