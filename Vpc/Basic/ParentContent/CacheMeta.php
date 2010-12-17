66b5c959a33569e73a314d19bf03a8f8b6c19017
Franz Unger <ufx@vivid-planet.com>
2010-12-17 15:38:14 +0100

    ParentContent-Cache loeschen: nur bei Aenderung von parent_id

--- Vpc/Basic/ParentContent/CacheMeta.php
+++ Vpc/Basic/ParentContent/CacheMeta.php
@@ -1,12 +1,9 @@
 <?php
-/**
- * @package Vpc
- * @subpackage Basic
- */
 class Vpc_Basic_ParentContent_CacheMeta extends Vps_Component_Cache_Meta_Static_Model
 {
     public static function getDeleteWhere($pattern, $row, $dirtyColumns, $params)
     {
+        if (!$dirtyColumns || !in_array('parent_id', $dirtyColumns)) return null;
         $ret = parent::getDeleteWhere($pattern, $row, $dirtyColumns, $params);
         $ret['db_id'] = array();
         foreach (self::_getRecursiveChildIds($row->id, $row->getModel()) as $id) {
