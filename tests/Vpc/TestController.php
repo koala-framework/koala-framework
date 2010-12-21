7a32d08a5f6489a03d668447adb991bc1df088ee
Markus Hauser <mh@vivid-planet.com>
2010-12-21 10:10:24 +0100

    apc cache bei tests loeschen

--- tests/Vpc/TestController.php
+++ tests/Vpc/TestController.php
@@ -15,6 +15,7 @@ class Vpc_TestController extends Vps_Controller_Action
         //FnF models setzen damit tests nicht in echte tabellen schreiben
         Vps_Component_Cache::setInstance(Vps_Component_Cache::CACHE_BACKEND_FNF);
 
+        apc_clear_cache('user');
         Vps_Component_Data_Root::setComponentClass($this->_getParam('root'));
         $root = Vps_Component_Data_Root::getInstance();
         $root->setFilename('vps/vpctest/'.$this->_getParam('root'));
