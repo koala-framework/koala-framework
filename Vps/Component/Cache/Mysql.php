71a478f3ae1c718fac52cee9d5fb2e881c0cea38
Niko Sams <ns@vivid-planet.com>
2010-12-22 13:58:07 +0100

    benchmark wenn von mysql geladen wurde

--- Vps/Component/Cache/Mysql.php
+++ Vps/Component/Cache/Mysql.php
@@ -67,6 +67,7 @@ class Vps_Component_Cache_Mysql extends Vps_Component_Cache
         $cacheId = $this->_getCacheId($componentId, $type, $value);
         $content = apc_fetch($cacheId);
         if ($content === false) {
+            Vps_Benchmark::count('comp cache mysql');
             $select = $this->getModel('cache')->select()
                 ->whereEquals('component_id', $componentId)
                 ->whereEquals('type', $type)
