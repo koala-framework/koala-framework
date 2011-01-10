fe56b8e9f1054bffe02ce96163ffd377ac6e8b14
Franz Unger <ufx@vivid-planet.com>
2011-01-10 12:02:05 +0100

    UrlField und EMailField hinzugefuegt, verwenden beide Vps_Util_Punycode

--- Vpc/Basic/LinkTag/Mail/Form.php
+++ Vpc/Basic/LinkTag/Mail/Form.php
@@ -5,9 +5,8 @@ class Vpc_Basic_LinkTag_Mail_Form extends Vpc_Abstract_Form
     {
         parent::__construct($name, $class, $id);
 
-        $this->add(new Vps_Form_Field_TextField('mail', 'E-Mail Address'))
-            ->setWidth(300)
-            ->setAllowBlank(false);
+        $this->add(new Vps_Form_Field_EMailField('mail', 'E-Mail Address'))
+            ->setWidth(300);
         $this->add(new Vps_Form_Field_TextField('subject', 'Predefined Subject for Mail'))
             ->setWidth(300);
         $this->add(new Vps_Form_Field_TextArea('text', 'Predefined Text for Mail'))
