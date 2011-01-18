80cbf165dc3373fbe27bd7d3e66fc3965f1f7a70
Franz Unger <ufx@vivid-planet.com>
2011-01-18 13:47:09 +0100

    Tests gefixt, gingen nicht mehr wegen Abschalten vom Vorrendern der Bilder

--- tests/Vpc/Trl/Image/Test.php
+++ tests/Vpc/Trl/Image/Test.php
@@ -102,6 +102,14 @@ class Vpc_Trl_Image_Test extends Vpc_TestAbstract
 
     private function _checkTheSizes($html, $smallImageNum, $smallWidth, $smallHeight)
     {
+        // getMediaOutput aufrufen, damit Cache-Meta geschrieben wird (wegen d0cf3812b20fa19c40617ac5b08ed08a18ff808d)
+        // muss so gemacht werden, weil der request über getimagesize weiter unten
+        // nicht das FnF-Cache Model dieses Request schreiben kann
+        preg_match('/.*\/media\/([\w\.]+)\/([\w\-]+)\/(\w+)\/.*/', $html, $matches);
+        $class = $matches[1];
+        $classWithoutDot = strpos($class, '.') ? substr($class, 0, strpos($class, '.')) : $class;
+        call_user_func(array($classWithoutDot, 'getMediaOutput'), $matches[2], $matches[3], $class);
+
         $this->assertRegExp('#<img.+?src=".+?'.$smallImageNum.'\.jpg.+width="'.$smallWidth.'".+height="'.$smallHeight.'"#ms', $html);
 
         preg_match('#src="(.+?)"#ms', $html, $matches);
