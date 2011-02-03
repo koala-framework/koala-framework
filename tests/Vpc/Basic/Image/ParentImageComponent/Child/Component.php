86485ecacca759de16f60a7117b338bf55141ae8
Franz Unger <ufx@vivid-planet.com>
2011-02-03 14:07:49 +0100

    Media-Cache wurde nicht geleert, wenn useParentImage verwendet wurde

--- tests/Vpc/Basic/Image/ParentImageComponent/Child/Component.php
+++ tests/Vpc/Basic/Image/ParentImageComponent/Child/Component.php
@@ -2,6 +2,8 @@
 class Vpc_Basic_Image_ParentImageComponent_Child_Component
     extends Vpc_Basic_Image_Component
 {
+    public static $getMediaOutputCalled = 0;
+
     public static function getSettings()
     {
         $ret = parent::getSettings();
@@ -9,5 +11,11 @@ class Vpc_Basic_Image_ParentImageComponent_Child_Component
         $ret['dimensions'] = array(array('width'=>16, 'height'=>16, 'scale'=>Vps_Media_Image::SCALE_DEFORM));
         $ret['useParentImage'] = true;
         return $ret;
+    }
+
+    public static function getMediaOutput($id, $type, $className)
+    {
+        self::$getMediaOutputCalled++;
+        return parent::getMediaOutput($id, $type, $className);
     }
 }
