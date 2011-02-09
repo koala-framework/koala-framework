3ed139f58c8806668a53a445a0cd77efe7f77639
Markus Hauser <mh@vivid-planet.com>
2011-02-09 17:44:13 +0100

    table bugfix: wenn 2 nacheinander geaendert werden, uebernimmt die zweite nicht die columns der ersten

--- Vpc/Basic/Table/ExtConfig.php
+++ Vpc/Basic/Table/ExtConfig.php
@@ -5,6 +5,7 @@ class Vpc_Basic_Table_ExtConfig extends Vps_Component_Abstract_ExtConfig_Abstrac
     {
         $settings = $this->_getStandardConfig('vps.autoform', 'Settings', trlVps('Settings'), new Vps_Asset('wrench_orange'));
         $table = $this->_getStandardConfig('vps.autogrid', 'Index', trlVps('Table'), new Vps_Asset('wrench'));
+        $table['renderAlways'] = true;
         return array(
             'settings' => $settings,
             'table' => $table
