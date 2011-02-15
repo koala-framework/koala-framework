4722160b2e1986da5da1e8652b14ba92c5f63875
Markus Hauser <mh@vivid-planet.com>
2011-02-15 12:52:43 +0100

    test fuer cards form mit real models, weils bei kitepower dahingehend ein problem gab

---
+++ tests/Vps/Form/CardsRealModels/TestController.php
@@ -0,0 +1,29 @@
+<?php
+class Vps_Form_CardsRealModels_TestController extends Vps_Controller_Action_Auto_Form
+{
+    protected $_permissions = array('save', 'add');
+
+    public function init()
+    {
+        if ($this->_getParam('testDb')) {
+            Vps_Test_SeparateDb::setDbAndCreateCookie($this->_getParam('testDb'));
+        } else {
+            Vps_Test_SeparateDb::setDbFromCookie();
+        }
+        parent::init();
+    }
+
+    protected function _initFields()
+    {
+        $this->_form = new Vps_Form_CardsRealModels_Form_Wrapper();
+    }
+
+    public function indexAction()
+    {
+        $config = array();
+        $config['baseParams']['id'] = $this->_getParam('id');
+        $config['controllerUrl'] = $this->getRequest()->getPathInfo();
+        $config['assetsType'] = 'Vps_Form_CardsRealModels:Test';
+        $this->view->ext('Vps.Auto.FormPanel', $config, 'Vps.Test.Viewport');
+    }
+}
