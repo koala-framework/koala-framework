5c70fa5ac6431bec511230a2dc2da6155512fbff
Markus Hauser <mh@vivid-planet.com>
2011-02-22 12:56:13 +0100

    CardsRealModels test erweitert

--- tests/Vps/Form/CardsRealModels/Form/Firstname.php
+++ tests/Vps/Form/CardsRealModels/Form/Firstname.php
@@ -8,10 +8,4 @@ class Vps_Form_CardsRealModels_Form_Firstname extends Vps_Form_CardsRealModels_F
         parent::_init();
         $this->add(new Vps_Form_Field_TextField('firstname', 'Firstname'));
     }
-
-    public function prepareSave($parentRow, $postData)
-    {
-        parent::prepareSave($parentRow, $postData);
-        $row = $this->_getRowByParentRow($parentRow);
-    }
 }
