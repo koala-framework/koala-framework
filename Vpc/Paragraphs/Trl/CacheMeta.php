44f9da2278d96618aadc00cf6c2c3abb29246292
Franz Unger <ufx@vivid-planet.com>
2011-03-04 10:50:28 +0100

    Paragraphs_Trl: Cache loeschen hatte Bug

---
+++ Vpc/Paragraphs/Trl/CacheMeta.php
@@ -0,0 +1,12 @@
+<?php
+class Vpc_Paragraphs_Trl_CacheMeta extends Vps_Component_Cache_Meta_Static_ChildModel
+{
+    public static function getDeleteWhere($pattern, $row, $dirtyColumns, $params)
+    {
+        $ret = parent::getDeleteWhere($pattern, $row, $dirtyColumns, $params);
+        $dbId = $row->component_id;
+        $dbId = substr($dbId, 0, max(strrpos($dbId, '-'), strrpos($dbId, '_')));
+        $ret['db_id'] = $dbId;
+        return $ret;
+    }
+}
