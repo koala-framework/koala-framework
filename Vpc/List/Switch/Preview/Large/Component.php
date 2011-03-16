d6c5e848be896513ef2b3bb970a91b6364fafae2
Christoph Eder <ce@vivid-planet.com>
2011-03-16 15:47:01 +0100

    - groesse auf 800x600 erhoeht

--- Vpc/List/Switch/Preview/Large/Component.php
+++ Vpc/List/Switch/Preview/Large/Component.php
@@ -6,8 +6,8 @@ class Vpc_List_Switch_Preview_Large_Component extends Vpc_Basic_Image_Component
         $ret = parent::getSettings();
         $ret['dimensions'] = array(
         'default'=>array(
-            'width' => 480,
-            'height' => 360,
+            'width' => 800,
+            'height' => 600,
             'scale' => Vps_Media_Image::SCALE_CROP
         ));
         $ret['useParentImage'] = true;
