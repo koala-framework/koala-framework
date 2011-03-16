c2890184867e195e74716cb6daaacc63647f67d2
Christoph Eder <ce@vivid-planet.com>
2011-03-16 15:50:33 +0100

    - man sollte vorher lesen/testen, bevor man was eincheckt ...

--- Vpc/List/Switch/Preview/Large/Component.php
+++ Vpc/List/Switch/Preview/Large/Component.php
@@ -6,8 +6,8 @@ class Vpc_List_Switch_Preview_Large_Component extends Vpc_Basic_Image_Component
         $ret = parent::getSettings();
         $ret['dimensions'] = array(
         'default'=>array(
-            'width' => 800,
-            'height' => 600,
+            'width' => 480,
+            'height' => 360,
             'scale' => Vps_Media_Image::SCALE_CROP
         ));
         $ret['useParentImage'] = true;
