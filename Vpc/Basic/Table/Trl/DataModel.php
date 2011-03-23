1e00e5d0bd27a255bda180f9c35f6bc0b32cbb04
Christoph Eder <ce@vivid-planet.com>
2011-03-23 12:18:48 +0100

    - markus: ein siblingModel reicht. in dem werden alle tabellendaten gespeichert. ka, warum das vorher so drinnen war ...

--- Vpc/Basic/Table/Trl/DataModel.php
+++ Vpc/Basic/Table/Trl/DataModel.php
@@ -3,15 +3,9 @@ class Vpc_Basic_Table_Trl_DataModel extends Vps_Model_Db
 {
     protected $_table = 'vpc_basic_table_data_trl';
 
-    public function __construct(array $config = array())
+    protected function _init()
     {
-        parent::__construct($config);
-        if (!isset($config['columnCount'])) throw new Vps_Exception('columnCount not given');
-        $columns = array();
-        for ($x = 1; $x <= $config['columnCount']; $x++) $columns[] = 'column' . $x;
-        $this->_siblingModels[] = new Vps_Model_Field(array(
-            'fieldName'=>'data',
-            'columns' => $columns
-        ));
+        $this->_siblingModels[] = new Vps_Model_Field(array('fieldName'=>'data'));
+        parent::_init();
     }
 }
