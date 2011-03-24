8a034a1b270f27d8b57d0e19b60805bb250481f9
Franz Unger <ufx@vivid-planet.com>
2011-03-24 12:11:05 +0100

    Ups, vorigen commit falsch eingecheckt

--- Vps/Update/35001.sql
+++ Vps/Update/35001.sql
@@ -1,3 +1,1 @@
-ALTER TABLE `vps_enquiries` ADD `sent_mail_content_text` TEXT NOT NULL ;
-ALTER TABLE `vps_enquiries` ADD `sent_mail_content_html` TEXT NOT NULL ;
-ALTER TABLE `vps_enquiries` ADD `mail_attachments` TEXT NOT NULL AFTER `serialized_mail_essentials` ;
+ALTER TABLE `vps_enquiries` CHANGE `sent_mail_content_html` `sent_mail_content_html` TEXT NULL ;
