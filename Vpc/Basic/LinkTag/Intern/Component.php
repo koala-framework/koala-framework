be97bf3ec78396c45e663573a70747b3a6cd7867
Markus Hauser <mh@vivid-planet.com>
2011-03-24 14:42:15 +0100

    cache fix 1.11

--- Vpc/Basic/LinkTag/Intern/Component.php
+++ Vpc/Basic/LinkTag/Intern/Component.php
@@ -20,29 +20,26 @@ class Vpc_Basic_LinkTag_Intern_Component extends Vpc_Basic_LinkTag_Abstract_Comp
         $ret = parent::getCacheVars();
         $linkedData = $this->getData()->getLinkedData();
         if ($linkedData && isset($linkedData->row) && $linkedData->row) {
+            // wenns kein chained gibt, hat man in der variable $chainedData
+            // das normale $linkedData drin.
+            $chainedData = $linkedData;
+            while ($chainedData->row instanceof Vpc_Root_Category_Trl_GeneratorRow) {
+                $chainedData = $chainedData->chained;
+            }
             if ($linkedData->row instanceof Vpc_Root_Category_Trl_GeneratorRow) {
-                $ret[] = array(
-                    'model' => 'Vps_Component_Model',
-                    'id' => $linkedData->chained->row->id
-                );
-                $ret[] = array(
-                    'model' => 'Vpc_Root_Category_GeneratorModel',
-                    'id' => $linkedData->chained->row->id
-                );
                 $ret[] = array(
                     'model' => 'Vpc_Root_Category_Trl_GeneratorModel',
                     'id' => $linkedData->row->component_id
                 );
-            } else {
-                $ret[] = array(
-                    'model' => 'Vps_Component_Model',
-                    'id' => $linkedData->row->id
-                );
-                $ret[] = array(
-                    'model' => 'Vpc_Root_Category_GeneratorModel',
-                    'id' => $linkedData->row->id
-                );
-            }
+            } 
+            $ret[] = array(
+                'model' => 'Vps_Component_Model',
+                'id' => $chainedData->row->id
+            );
+            $ret[] = array(
+                'model' => 'Vpc_Root_Category_GeneratorModel',
+                'id' => $chainedData->row->id
+            );
             if ($linkedData instanceof Vpc_Basic_LinkTag_FirstChildPage_Data) {
                 $childData = $linkedData->_getFirstChildPage();
                 $ret = array_merge($ret, $childData->getComponent()->getCacheVars());
