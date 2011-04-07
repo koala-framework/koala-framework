91b3666ec9c847c7e358294adfe429730b9f1716
Christoph Eder <ce@vivid-planet.com>
2011-04-07 11:09:12 +0200

    - wheres bei den events entfernt

--- Vpc/Events/Directory/Generator.php
+++ Vpc/Events/Directory/Generator.php
@@ -5,8 +5,6 @@ class Vpc_Events_Directory_Generator extends Vps_Component_Generator_Page_Table
     {
         $ret = parent::_formatSelect($parentData, $select);
         if (!$ret) return $ret;
-        $ret->where('start_date <= NOW()');
-        $ret->where('end_date >= NOW() OR ISNULL(end_date)');
         $ret->order('start_date', 'ASC');
         return $ret;
     }
