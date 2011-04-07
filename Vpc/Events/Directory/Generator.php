4e95be008115395ba541ef41543d7da9fb6f0de9
Christoph Eder <ce@vivid-planet.com>
2011-04-07 11:03:34 +0200

    - events generator wurde nicht verwendet. dadurch entstand ein error

--- Vpc/Events/Directory/Generator.php
+++ Vpc/Events/Directory/Generator.php
@@ -1,10 +1,12 @@
 <?php
-class Vpc_Events_Directory_Generator extends Vpc_News_Directory_Generator
+class Vpc_Events_Directory_Generator extends Vps_Component_Generator_Page_Table
 {
     protected function _formatSelect($parentData, $select)
     {
-        $ret = Vpc_Directories_ItemPage_Directory_Component::_formatSelect($parentData, $select);
+        $ret = parent::_formatSelect($parentData, $select);
         if (!$ret) return $ret;
+        $ret->where('start_date <= NOW()');
+        $ret->where('end_date >= NOW() OR ISNULL(end_date)');
         $ret->order('start_date', 'ASC');
         return $ret;
     }
