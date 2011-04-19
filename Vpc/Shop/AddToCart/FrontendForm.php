5866fd44a1e12ed399cdf6c00156d189a50aa046
Franz Unger <ufx@vivid-planet.com>
2011-04-19 16:49:52 +0200

    Shop: Wenn das gleiche Produkt nochmal in den Warenkorb gelegt wird, wird es beim bestehenden dazugezaehlt und nicht als neue Zeile angelegt

--- Vpc/Shop/AddToCart/FrontendForm.php
+++ Vpc/Shop/AddToCart/FrontendForm.php
@@ -24,4 +24,21 @@ class Vpc_Shop_AddToCart_FrontendForm extends Vpc_Shop_AddToCartAbstract_Fronten
     {
         $this->_product = $product;
     }
+
+    protected function _beforeInsert(&$row)
+    {
+        $select = $row->getModel()->select()
+            ->whereEquals('shop_order_id', $row->shop_order_id)
+            ->whereEquals('shop_product_price_id', $row->shop_product_price_id)
+            ->whereEquals('add_component_id', $row->add_component_id)
+            ->whereEquals('add_component_class', $row->add_component_class);
+        foreach ($row->getSiblingRow(0)->toArray() as $key => $val) {
+            $select->whereEquals($key, $val);
+        }
+        $existingRow = $row->getModel()->getRow($select);
+        if ($existingRow) {
+            $existingRow->amount += $row->amount;
+            $row = $existingRow;
+        }
+    }
 }
