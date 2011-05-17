a32ef138d862af6c7f41bc36f59a3506231cbbd0
Markus Hauser <mh@vivid-planet.com>
2011-05-17 16:43:13 +0200

    test fuer home in level weiter unten und start der behebung

---
+++ tests/Vps/Component/Generator/HomeNotTopLevel/Test.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @group HomeNotTopLevel
+ */
+class Vps_Component_Generator_HomeNotTopLevel_Test extends Vpc_TestAbstract
+{
+    public function setUp()
+    {
+        parent::setUp('Vps_Component_Generator_HomeNotTopLevel_Root');
+        $this->_root->setFilename(null);
+    }
+
+    public function testFromRoot()
+    {
+        $c = $this->_root->getChildPages(array('home'=>true), array());
+        $this->assertEquals(count($c), 1);
+        $this->assertEquals(current($c)->componentId, 3);
+
+        $c = $this->_root->getChildPages(array('home'=>true));
+        $this->assertEquals(count($c), 0);
+    }
+
+    public function testFromPage1()
+    {
+        $page = $this->_root->getComponentById('1');
+        $c = $page->getChildPages(array('home'=>true), array());
+        $this->assertEquals(count($c), 1);
+        $this->assertEquals(current($c)->componentId, 3);
+
+
+// Eigentlich dürfte nichts zurückkommen, dz. ist es im Vpc_Root_Category_Generator
+// so implementiert, dass es schon zurückkommt. Da es nur die Home betrifft nicht weiter
+// schlimm, sollte es mal ganz korrekt implementiert werden, müsste dieser test funzen
+
+//         $page1 = $this->_root->getComponentById('1');
+//         $c = $page1->getChildPages(array('home' => true));
+//         $this->assertEquals(count($c), 0);
+    }
+
+    public function testFromPage2()
+    {
+        $page = $this->_root->getComponentById('2');
+        $c = $page->getChildPages(array('home'=>true), array());
+        $this->assertEquals(count($c), 1);
+        $this->assertEquals(current($c)->componentId, 3);
+
+        $page = $this->_root->getComponentById('2');
+        $c = $page->getChildPages(array('home'=>true));
+        $this->assertEquals(count($c), 1);
+        $this->assertEquals(current($c)->componentId, 3);
+    }
+}
