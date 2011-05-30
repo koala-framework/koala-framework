f34e3802659565dd96b4e702156735e2b90436ba
Markus Hauser <mh@vivid-planet.com>
2011-05-30 10:00:42 +0200

    Table hat keine Spaltenbegrenzung mehr, sondern zeigt standardmäßig immer 26 an.

--- Vpc/Basic/Table/Controller.php
+++ Vpc/Basic/Table/Controller.php
@@ -6,9 +6,9 @@ class Vpc_Basic_Table_Controller extends Vps_Controller_Action_Auto_Vpc_Grid
 
     protected function _initColumns()
     {
-        $columnCount = Vps_Component_Data_Root::getInstance()
-            ->getComponentById($this->_getParam('componentId'), array('ignoreVisible'=>true))
-            ->getComponent()->getColumnCount();
+        $component = Vps_Component_Data_Root::getInstance()
+            ->getComponentById($this->_getParam('componentId'), array('ignoreVisible'=>true));
+        $maxColumns = Vpc_Abstract::getSetting($component->componentClass, 'maxColumns');
 
         $sel = new Vps_Form_Field_Select();
         $rowStyles = Vpc_Abstract::getSetting($this->_getParam('class'), 'rowStyles');
@@ -21,7 +21,7 @@ class Vpc_Basic_Table_Controller extends Vps_Controller_Action_Auto_Vpc_Grid
         $this->_columns->add(new Vps_Grid_Column('css_style', trlVps('Style'), 100))
             ->setEditor($sel);
         $this->_columns->add(new Vps_Grid_Column_Visible());
-        for ($i = 1; $i <= $columnCount; $i++) {
+        for ($i = 1; $i <= $maxColumns; $i++) {
             $this->_columns->add(new Vps_Grid_Column("column$i", $this->_getColumnLetterByIndex($i-1), 150))
                 ->setEditor(new Vps_Form_Field_TextField());
         }
