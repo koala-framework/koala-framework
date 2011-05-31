8152140f745ec15938357b6d11f1932908032e50
Markus Hauser <mh@vivid-planet.com>
2011-05-31 12:55:23 +0200

    events trl und cc und trlcc

--- Vpc/Events/Directory/Generator.php
+++ Vpc/Events/Directory/Generator.php
@@ -1,9 +1,9 @@
 <?php
-class Vpc_Events_Directory_Generator extends Vpc_News_Directory_Generator
+class Vpc_Events_Directory_Generator extends Vps_Component_Generator_Page_Table
 {
     protected function _formatSelect($parentData, $select)
     {
-        $ret = Vpc_Directories_ItemPage_Directory_Component::_formatSelect($parentData, $select);
+        $ret = parent::_formatSelect($parentData, $select);
         if (!$ret) return $ret;
         $ret->order('start_date', 'ASC');
         return $ret;
