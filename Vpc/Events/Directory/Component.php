fb71f6feb30a9a32229eb2eb583c6bf1f4ae8ad8
Markus Hauser <mh@vivid-planet.com>
2011-05-31 12:55:23 +0200

    events trl und cc und trlcc

--- Vpc/Events/Directory/Component.php
+++ Vpc/Events/Directory/Component.php
@@ -7,7 +7,7 @@ class Vpc_Events_Directory_Component extends Vpc_News_Directory_Component
         $ret['componentName'] = trlVps('Events.Events');
         $ret['childModel'] = 'Vpc_Events_Directory_Model';
 
-        $ret['generators']['detail']['class'] = 'Vpc_News_Directory_Generator';
+        $ret['generators']['detail']['class'] = 'Vpc_Events_Directory_Generator';
         $ret['generators']['detail']['component'] = 'Vpc_Events_Detail_Component';
         $ret['generators']['detail']['dbIdShortcut'] = 'events_';
 
