a268f8b8bc4b35eb9c0b726acc39747721cab7d2
Niko Sams <ns@vivid-planet.com>
2011-06-22 10:11:53 +0200

    kommentiert wofuer das da ist

--- Vps/Component/Plugin/Interface/Login.php
+++ Vps/Component/Plugin/Interface/Login.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Dieses Interface wird von Downloads Komponenten die in einem geschützten Bereich liegen
+ * verwendet um zu überprüfen ob der Benutzer eingeloggt ist.
+ */
 interface Vps_Component_Plugin_Interface_Login
 {
     public function isLoggedIn();
