5c05777e0d3cf28e9d01f766b45595ab5084a87e
Niko Sams <ns@vivid-planet.com>
2011-06-29 11:27:11 +0200

    LoggedIn verschnellern, profil wird nicht immer verwendet

--- Vpc/User/BoxWithoutLogin/LoggedIn/Component.php
+++ Vpc/User/BoxWithoutLogin/LoggedIn/Component.php
@@ -1,5 +1,5 @@
 <?php
-class Vpc_User_BoxWithoutLogin_LoggedIn_Component extends Vpc_Abstract_Composite_Component
+class Vpc_User_BoxWithoutLogin_LoggedIn_Component extends Vpc_Abstract
 {
     public static function getSettings()
     {
@@ -14,14 +14,19 @@ class Vpc_User_BoxWithoutLogin_LoggedIn_Component extends Vpc_Abstract_Composite
     {
         $ret = parent::getTemplateVars();
         $ret['authedUser'] = Vps_Registry::get('userModel')->getAuthedUser();
+
+        /*
         $userDir = Vps_Component_Data_Root::getInstance()
             ->getComponentByClass(
                 'Vpc_User_Directory_Component',
                 array('subroot' => $this->getData())
             );
         if ($userDir) {
+            //falls das doch mal benötigt wird, über ein setting ein/ausschaltbar machen
             $ret['myProfile'] = $userDir->getChildComponent('_' . $ret['authedUser']->id);
         }
+        */
+
         $ret['links'] = $this->_getLinks();
         $ret['linkPostfix'] = $this->_getSetting('linkPostfix');
         return $ret;
