1cf326746c24c0b3747675a2764bef5631ce7c0e
Niko Sams <ns@vivid-planet.com>
2011-06-29 16:23:15 +0200

    RowCache model bei Usern verwenden
    cached role und email fuer user box

--- Vps/User/Mirror.php
+++ Vps/User/Mirror.php
@@ -6,31 +6,37 @@ final class Vps_User_Mirror extends Vps_Model_MirrorCache
 
     protected function _init()
     {
-        $clientConfig = array(
-            'serverUrl' => Vps_Registry::get('config')->service->users->url,
-            'extraParams' => array(
-                'applicationId' => Vps_Registry::get('config')->application->id,
-                'version'       => Vps_User_Model::version()
-            )
-        );
-        $cfg = Vps_Registry::get('config');
-        if (!empty($cfg->service->users->proxy->host)) {
-            $clientConfig['proxyHost'] = $cfg->service->users->proxy->host;
-        }
-        if (!empty($cfg->service->users->proxy->port)) {
-            $clientConfig['proxyPort'] = $cfg->service->users->proxy->port;
-        }
-        if (!empty($cfg->service->users->proxy->user)) {
-            $clientConfig['proxyUser'] = $cfg->service->users->proxy->user;
-        }
-        if (!empty($cfg->service->users->proxy->password)) {
-            $clientConfig['proxyPassword'] = $cfg->service->users->proxy->password;
-        }
-
-        $client = new Vps_Srpc_Client($clientConfig);
-        $this->_sourceModel = new Vps_Model_Service(array('client' => $client));
         $this->_proxyModel = new Vps_Model_Db(array('table' => 'cache_users'));
-
         parent::_init();
+    }
+
+    public function getSourceModel()
+    {
+        if (!$this->_sourceModel) {
+            $clientConfig = array(
+                'serverUrl' => Vps_Registry::get('config')->service->users->url,
+                'extraParams' => array(
+                    'applicationId' => Vps_Registry::get('config')->application->id,
+                    'version'       => Vps_User_Model::version()
+                )
+            );
+            $cfg = Vps_Registry::get('config');
+            if (!empty($cfg->service->users->proxy->host)) {
+                $clientConfig['proxyHost'] = $cfg->service->users->proxy->host;
+            }
+            if (!empty($cfg->service->users->proxy->port)) {
+                $clientConfig['proxyPort'] = $cfg->service->users->proxy->port;
+            }
+            if (!empty($cfg->service->users->proxy->user)) {
+                $clientConfig['proxyUser'] = $cfg->service->users->proxy->user;
+            }
+            if (!empty($cfg->service->users->proxy->password)) {
+                $clientConfig['proxyPassword'] = $cfg->service->users->proxy->password;
+            }
+            $client = new Vps_Srpc_Client($clientConfig);
+            $this->_sourceModel = new Vps_Model_Service(array('client' => $client));
+        }
+
+        return $this->_sourceModel;
     }
 }
---
+++ tests/Vps/Model/RowCache/Test.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @group Model
+ * @group Model_RowCache
+ */
+class Vps_Model_RowCache_Test extends PHPUnit_Framework_TestCase
+{
+    
+    protected function setUp()
+    {
+        apc_clear_cache('user');
+        parent::setUp();
+    }
+
+    private function _createModelInstance()
+    {
+        Vps_Model_Abstract::clearInstances();
+        return new Vps_Model_RowCache(array(
+            'proxyModel' => Vps_Model_Abstract::getInstance('Vps_Model_RowCache_SourceModel'),
+            'cacheColumns' => array('foo')
+        ));
+    }
+
+    public function testRead()
+    {
+        $m = $this->_createModelInstance();
+        $source = Vps_Model_Abstract::getInstance('Vps_Model_RowCache_SourceModel');
+        $this->assertEquals(1, $m->getRow(1)->foo);
+        $this->assertEquals(1, $source->called['getRows']);
+        $this->assertEquals(1, $m->getRow(1)->foo);
+        $this->assertEquals(1, $source->called['getRows']);
+        $this->assertEquals('x1', $m->getRow(1)->bar);
+        $this->assertEquals(1, $source->called['getRows']);
+
+        $m = $this->_createModelInstance();
+        $source = Vps_Model_Abstract::getInstance('Vps_Model_RowCache_SourceModel');
+        $this->assertEquals(1, $m->getRow(1)->foo); //gecached, hier nicht laden
+        $this->assertEquals(0, $source->called['getRows']);
+        $this->assertEquals(1, $m->getRow(1)->foo);
+        $this->assertEquals(0, $source->called['getRows']);
+        $this->assertEquals('x1', $m->getRow(1)->bar); //feld nicht gecached, hier wird laden
+        $this->assertEquals(1, $source->called['getRows']);
+    }
+
+    public function testSave()
+    {
+        $m = $this->_createModelInstance();
+        $source = Vps_Model_Abstract::getInstance('Vps_Model_RowCache_SourceModel');
+        $this->assertEquals(1, $m->getRow(1)->foo);
+        $this->assertEquals(1, $source->called['getRows']);
+
+
+        $m = $this->_createModelInstance();
+        $source = Vps_Model_Abstract::getInstance('Vps_Model_RowCache_SourceModel');
+        $r = $m->getRow(1);
+        $this->assertEquals(0, $source->called['getRows']);
+        $r->foo = 'asdf';
+        $this->assertEquals(1, $source->called['getRows']);
+        $r->save(); //muss cache löschen
+
+        /*
+        //kann nicht gesetet werden, weil innerhalb eines requests der apc cache nicht gelöscht werden kann
+        $m = $this->_createModelInstance();
+        $source = Vps_Model_Abstract::getInstance('Vps_Model_RowCache_SourceModel');
+        $this->assertEquals(1, $source->called['getRows']);
+        $this->assertEquals($m->getRow(1)->foo, 'asdf'); //nicht mehr gecached, da cache gelöscht wurde
+        */
+    }
+}
