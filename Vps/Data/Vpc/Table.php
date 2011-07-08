af6e686fab26c4c009b6449e8e283735c50ebe6b
Franz Unger <ufx@vivid-planet.com>
2011-07-08 17:46:29 +0200

    Data_Vpc_Table jetzt mit einstellbarem idSeparator

--- Vps/Data/Vpc/Table.php
+++ Vps/Data/Vpc/Table.php
@@ -3,21 +3,23 @@ class Vps_Data_Vpc_Table extends Vps_Data_Table_Parent
 {
     private $_componentClass;
     private $_tagSuffix;
+    private $_idSeparator;
 
-    public function __construct($parentTable, $dataIndex = null, $componentClass, $tagSuffix = '')
+    public function __construct($parentTable, $dataIndex = null, $componentClass, $tagSuffix = '', $idSeparator = '-')
     {
         parent::__construct($parentTable, $dataIndex);
         $this->_componentClass = $componentClass;
         $this->_tagSuffix = $tagSuffix;
+        $this->_idSeparator = $idSeparator;
     }
 
     private function _getParentRow($row)
     {
         $table = new $this->_parentTable(array('componentClass' => $this->_componentClass));
         if ($this->_tagSuffix) {
-            $componentId = $row->component_id . '-' . $row->id . '-' . $this->_tagSuffix;
+            $componentId = $row->component_id . $this->_idSeparator . $row->id . '-' . $this->_tagSuffix;
         } else {
-            $componentId = $row->component_id . '-' . $row->id;
+            $componentId = $row->component_id . $this->_idSeparator . $row->id;
         }
         $where = array(
             'component_id = ?' => $componentId
