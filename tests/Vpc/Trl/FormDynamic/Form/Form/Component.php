9b31c44903e66b295861b4dd214a055a7cb50cdb
Niko Sams <ns@vivid-planet.com>
2011-07-18 09:36:22 +0200

    fix test

--- tests/Vpc/Trl/FormDynamic/Form/Form/Component.php
+++ tests/Vpc/Trl/FormDynamic/Form/Form/Component.php
@@ -1,9 +1,4 @@
 <?php
-class Vpc_Trl_FormDynamic_Form_Form_Component extends Vpc_Form_Dynamic_Form_Component
+class Vpc_Trl_FormDynamic_Form_Form_Component extends Vpc_FormDynamic_Basic_Form_Form_Component
 {
-    protected function _initForm()
-    {
-        parent::_initForm();
-        $this->_form->setModel(new Vps_Model_FnF());
-    }
 }
