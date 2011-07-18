c4fe6e3eceb9f7cbc93bb381efb92398d5196c73
Niko Sams <ns@vivid-planet.com>
2011-07-18 09:29:22 +0200

    previewWidth entfernt

--- Vpc/Paragraphs/Trl/ExtConfig.php
+++ Vpc/Paragraphs/Trl/ExtConfig.php
@@ -4,7 +4,6 @@ class Vpc_Paragraphs_Trl_ExtConfig extends Vps_Component_Abstract_ExtConfig_Abst
     protected function _getConfig()
     {
         $config = $this->_getStandardConfig('vpc.paragraphs');
-        $config['previewWidth'] = $this->_getSetting('previewWidth');
         $config['showDelete'] = false;
         $config['showPosition'] = false;
         return array(
