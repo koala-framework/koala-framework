254759155c16cd7c2c809fc3b27e07b7eae205b5
Niko Sams <ns@vivid-planet.com>
2011-07-22 09:57:08 +0200

    file backend versteht jetzt auch compression

--- Vps/Cache/Backend/File.php
+++ Vps/Cache/Backend/File.php
@@ -10,6 +10,21 @@ class Vps_Cache_Backend_File extends Zend_Cache_Backend_File
         if (!isset($options['hashed_directory_umask'])) {
             $options['hashed_directory_umask'] = 0777;
         }
+        $this->_options['compression'] = isset($options['compression']) ? (bool)$options['compression'] : false;
         parent::__construct($options);
+    }
+
+    
+    public function load($id, $doNotTestCacheValidity = false)
+    {
+        $ret = parent::load($id, $doNotTestCacheValidity);
+        if ($this->_options['compression']) $ret = gzinflate($ret);
+        return $ret;
+    }
+
+    public function save($data, $id, $tags = array(), $specificLifetime = false)
+    {
+        if ($this->_options['compression']) $data = gzdeflate($data);
+        return parent::save($data, $id, $tags, $specificLifetime);
     }
 }
