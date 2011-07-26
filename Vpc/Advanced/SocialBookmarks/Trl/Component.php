865508cd4fbc3dfb079c5134a1378f63a018b0e4
Markus Hauser <mh@vivid-planet.com>
2011-07-26 11:45:01 +0200

    inheritComponentClass gibts nicht mehr

--- Vpc/Advanced/SocialBookmarks/Trl/Component.php
+++ Vpc/Advanced/SocialBookmarks/Trl/Component.php
@@ -5,7 +5,6 @@ class Vpc_Advanced_SocialBookmarks_Trl_Component extends Vpc_Chained_Trl_MasterA
     {
         $ret = parent::getSettings($masterComponentClass);
         $ret['editComponents'][] = 'child'; //kann das vielleicht im parent gemacht werden?
-        $ret['inheritComponentClass'] = 'Vpc_Advanced_SocialBookmarks_Trl_Inherit_Component';
         return $ret;
     }
 }
