30a0b2a9caf9986aa0f7d4ec084458ae6a3a5482
Niko Sams <ns@vivid-planet.com>
2011-08-02 10:30:29 +0200

    sibling models bei geaenderten columns auch beruecksichtigen
    sonst ist last_modified nicht geaendert wenn premium_until geaendert wurde

--- Vps/Filter/Row/CurrentDateTimeIfColumnChanged.php
+++ Vps/Filter/Row/CurrentDateTimeIfColumnChanged.php
@@ -13,9 +13,25 @@ class Vps_Filter_Row_CurrentDateTimeIfColumnChanged extends Vps_Filter_Row_Curre
         if (!$row->$column) return false;
 
         //TODO: $row->getDirtyColumns direkt verwenden!
-        while ($row instanceof Vps_Model_Proxy_Row) $row = $row->getProxiedRow();
-        if (!$row instanceof Vps_Model_Db_Row) return false;
-        $dc = $row->getRow()->___getDirtyColumns();
+        //TODO: wenn darauf umgestellt wird, testen ob die siblings eh auch mit dabei sind
+        //      (bsp rssinclude Users premium_until)
+        $rows = array();
+        $rows[] = $row;
+        foreach ($row->_getSiblingRows() as $row) {
+            $rows[] = $row;
+        }
+        while ($row instanceof Vps_Model_Proxy_Row) {
+            $row = $row->getProxiedRow();
+            foreach ($row->_getSiblingRows() as $row) {
+                $rows[] = $row;
+            }
+        }
+        $dc = array();
+        foreach ($rows as $r) {
+            if ($r instanceof Vps_Model_Db_Row) {
+                $dc = array_merge($dc, $row->getRow()->___getDirtyColumns());
+            }
+        }
         if (!array_intersect($dc, $this->_columns)) {
             return true;
         }
