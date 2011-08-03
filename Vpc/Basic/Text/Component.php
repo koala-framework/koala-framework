94d35ac812650d0c459140fe77f7e3b13d6e66ac
Franz Unger <ufx@vivid-planet.com>
2011-08-03 11:50:13 +0200

    Text-Komponente: Standardtext (Lorem Ipsum) jetzt ueber Setting einstellbar)

--- Vpc/Basic/Text/Component.php
+++ Vpc/Basic/Text/Component.php
@@ -26,6 +26,7 @@ class Vpc_Basic_Text_Component extends Vpc_Abstract
             'enableStyles'      => true,
             'enableStylesEditor'=> true,
             'enableTagsWhitelist'=> true,
+            'defaultText'       => Vpc_Abstract::LOREM_IPSUM,
 
             //veraltert NICHT VERWENDEN!! (in der Vpc_Mail komponente ist ein besserer ersatz)
             'emailStyles'       => array()
