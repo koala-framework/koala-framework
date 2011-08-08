1e2ff36464ff8a2cd93960447039559711f4f337
Franz Unger <ufx@vivid-planet.com>
2011-08-08 11:27:48 +0200

    Fur ExtConfig::getEditConfigs() ist idTemplate jetzt optional, Standardimplementierung wird automatisch erstellt

--- Vpc/Abstract/List/ExtConfigListEditButton.php
+++ Vpc/Abstract/List/ExtConfigListEditButton.php
@@ -8,10 +8,7 @@ class Vpc_Abstract_List_ExtConfigListEditButton extends Vps_Component_Abstract_E
     {
         $detail = Vpc_Abstract::getChildComponentClass($this->_class, 'child');
         $gen = Vps_Component_Generator_Abstract::getInstance($this->_class, 'child');
-        $generators = Vpc_Abstract::getSetting($this->_class, 'generators');
-        $idTemplate = '{componentId}'.$gen->getIdSeparator().'{0}';
-        $edit = Vps_Component_Abstract_ExtConfig_Abstract::getEditConfigs($detail, $gen, $idTemplate, '');
-
+        $edit = Vps_Component_Abstract_ExtConfig_Abstract::getEditConfigs($detail, $gen);
         $config = $this->_getStandardConfig('vpc.list.listEditButton', 'ListEditButton');
         $config['componentConfigs'] = $edit['componentConfigs'];
         $config['contentEditComponents'] = $edit['contentEditComponents'];
