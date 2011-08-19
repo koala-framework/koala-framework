c23b55cd014b6dcf351d7d6cb239076f1eb63ea4
Niko Sams <ns@vivid-planet.com>
2011-08-19 15:14:41 +0200

    Columns fuer frontend formulare
    - floaten jetzt
    - Columns erstellt ein div
    - kaputte Column1 klasse wird dafuer nicht benoetigt

--- Vps/Form/Container/Columns.php
+++ Vps/Form/Container/Columns.php
@@ -9,4 +9,12 @@ class Vps_Form_Container_Columns extends Vps_Form_Container_Abstract
         $this->setBorder(false);
         $this->setBaseCls('x-plain');
     }
+
+    public function getTemplateVars($values, $fieldNamePostfix='')
+    {
+        $ret = parent::getTemplateVars($values, $fieldNamePostfix);
+        $ret['preHtml'] = ''; // damit ein div ausgegeben wird
+        $ret['postHtml'] = '<div class="clear"></div>';
+        return $ret;
+    }
 }
