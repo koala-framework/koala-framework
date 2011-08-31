c7b1edd5da79cb2fd1d2b7e3d8ce384da7c04355
Markus Hauser <mh@vivid-planet.com>
2011-08-31 15:22:48 +0200

    List.Plugins.CenterItems now sets the padding to the parent of the items and understands subtractOffset config

--- Vps_js/EyeCandy/List/Plugins/CenterItems.js
+++ Vps_js/EyeCandy/List/Plugins/CenterItems.js
@@ -2,14 +2,21 @@ Vps.EyeCandy.List.Plugins.CenterItems = Ext.extend(Vps.EyeCandy.List.Plugins.Abs
     init: function() {
         var listWidth = this.list.el.getWidth();
         var itemsWidth = 0;
+        var listItemWrapper = null;
         this.list.items.forEach(function(it) {
             itemsWidth += it.el.getWidth();
+            if (!listItemWrapper) {
+                listItemWrapper = it.el.parent();
+            }
         }, this);
 
         var fullWidth = this.list.el.getWidth();
-
         var paddingLeft = (listWidth - itemsWidth) / 2;
-        this.list.el.setStyle('padding-left', paddingLeft+'px');
-        this.list.el.setWidth(fullWidth);
+
+        if (listItemWrapper) {
+            if (this.substractOffset) paddingLeft -= this.substractOffset;
+            listItemWrapper.setStyle('padding-left', paddingLeft+'px');
+            this.list.el.setWidth(fullWidth);
+        }
     }
 });
