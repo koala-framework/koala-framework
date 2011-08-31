d74d30d1a71062711d4198c226812b106a1ac784
Markus Hauser <mh@vivid-planet.com>
2011-08-31 14:56:35 +0200

    made EyeCandy.List.Plugins.CenterItems working

--- Vps_js/EyeCandy/List/Plugins/CenterItems.js
+++ Vps_js/EyeCandy/List/Plugins/CenterItems.js
@@ -1,4 +1,15 @@
 Vps.EyeCandy.List.Plugins.CenterItems = Ext.extend(Vps.EyeCandy.List.Plugins.Abstract, {
     init: function() {
+        var listWidth = this.list.el.getWidth();
+        var itemsWidth = 0;
+        this.list.items.forEach(function(it) {
+            itemsWidth += it.el.getWidth();
+        }, this);
+
+        var fullWidth = this.list.el.getWidth();
+
+        var paddingLeft = (listWidth - itemsWidth) / 2;
+        this.list.el.setStyle('padding-left', paddingLeft+'px');
+        this.list.el.setWidth(fullWidth);
     }
 });
