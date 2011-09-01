1f95aa5b744f0fea45cbb377fc8f1eb1f9a4039c
Niko Sams <ns@vivid-planet.com>
2011-09-01 06:39:40 +0200

    remove now unneeded code

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,5 @@
 <?php
 chdir(dirname(__FILE__));
-define('VPS_PATH', dirname(__FILE__));
-
-$include_path  = get_include_path();
-$include_path .= PATH_SEPARATOR . VPS_PATH;
-set_include_path($include_path);
 
 require_once 'Vps/Setup.php';
 Vps_Setup::setUp();
