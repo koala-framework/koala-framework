95b6ccafe07a7287a720d5398322f1c221dc614b
Niko Sams <ns@vivid-planet.com>
2011-09-01 07:57:44 +0200

    don't allow setting trl home invisible
    like we do for master

--- Vpc/Root/Category/Trl/GeneratorRow.php
+++ Vpc/Root/Category/Trl/GeneratorRow.php
@@ -11,4 +11,13 @@ class Vpc_Root_Category_Trl_GeneratorRow extends Vps_Model_Proxy_Row
     {
         throw new Vps_ClientException(trlVps("Can't delete translated pages."));
     }
+
+    protected function _beforeUpdate()
+    {
+        parent::_beforeUpdate();
+        $c = Vps_Component_Data_Root::getInstance()->getComponentByDbId($this->component_id, array('ignoreVisible'=>true, 'limit'=>1));
+        if ($c->isHome && !$this->visible) {
+            throw new Vps_ClientException(trlVps('Cannot set Home Page invisible'));
+        }
+    }
 }
