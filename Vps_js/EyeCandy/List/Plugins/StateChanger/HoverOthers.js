290df585deeece38474d9137266246b68741990a
Niko Sams <ns@vivid-planet.com>
2011-09-01 10:16:41 +0200

    make items that get skipped configurable

--- Vps_js/EyeCandy/List/Plugins/StateChanger/HoverOthers.js
+++ Vps_js/EyeCandy/List/Plugins/StateChanger/HoverOthers.js
@@ -1,14 +1,22 @@
 Vps.EyeCandy.List.Plugins.StateChanger.HoverOthers = Ext.extend(Vps.EyeCandy.List.Plugins.Abstract, {
     state: 'tiny',
+    skipItems: 0,
     init: function() {
         this.list.on('childMouseEnter', function(item) {
-            i = item.getNextSibling(); //skip one
+            var i = item;
+            for(var j=0; j<this.skipItems && i; ++j) {
+                i = i.getNextSibling(); //skip
+            }
             if (i) {
                 while (i = i.getNextSibling()) {
                     i.pushState(this.state, this);
                 }
             }
-            i = item.getPreviousSibling(); //skip one
+
+            i = item;
+            for(var j=0; j<this.skipItems && i; ++j) {
+                i = i.getPreviousSibling(); //skip
+            }
             if (i) {
                 while (i = i.getPreviousSibling()) {
                     i.pushState(this.state, this);
@@ -16,13 +24,20 @@ Vps.EyeCandy.List.Plugins.StateChanger.HoverOthers = Ext.extend(Vps.EyeCandy.Lis
             }
         }, this);
         this.list.on('childMouseLeave', function(item) {
-            i = item.getNextSibling(); //skip one
+            var i = item;
+            for(var j=0; j<this.skipItems && i; ++j) {
+                i = i.getNextSibling(); //skip
+            }
             if (i) {
                 while (i = i.getNextSibling()) {
                     i.removeState(this.state, this);
                 }
             }
-            i = item.getPreviousSibling(); //skip one
+
+            i = item;
+            for(var j=0; j<this.skipItems && i; ++j) {
+                i = i.getPreviousSibling(); //skip
+            }
             if (i) {
                 while (i = i.getPreviousSibling()) {
                     i.removeState(this.state, this);
