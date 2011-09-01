e35e62ab9a1229631c6d2ba2f5a00877b0fd919a
Niko Sams <ns@vivid-planet.com>
2011-09-01 10:17:13 +0200

    correctly remove state

--- Vps_js/EyeCandy/List/Plugins/StateChanger/Click.js
+++ Vps_js/EyeCandy/List/Plugins/StateChanger/Click.js
@@ -5,7 +5,7 @@ Vps.EyeCandy.List.Plugins.StateChanger.Click = Ext.extend(Vps.EyeCandy.List.Plug
         this.list.on('childClick', function(item, ev) {
             if (this.stateUnique) {
                 this.list.getItems().forEach(function(i) {
-                    if (i!= item) item.removeState(this.state, this);
+                    if (i != item) i.removeState(this.state, this);
                 }, this);
             }
             item.pushState(this.state, this);
