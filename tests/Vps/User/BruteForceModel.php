76b9046273c87e22f45737ddd1798fcf760e039e
Niko Sams <ns@vivid-planet.com>
2011-09-05 08:40:33 +0200

    use service model in test

--- tests/Vps/User/BruteForceModel.php
+++ tests/Vps/User/BruteForceModel.php
@@ -1,5 +1,5 @@
 <?php
-class Vps_User_BruteForceModel extends Vps_User_Model
+class Vps_User_BruteForceModel extends Vps_User_Model_Service
 {
     protected $_maxLockTime = 60; // in seconds. for brute force tests
 }
