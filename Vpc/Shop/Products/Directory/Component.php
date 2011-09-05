1a6a85ede7569eb8de81a5aeadbcf6843e0c71d3
Franz Unger <ufx@vivid-planet.com>
2011-09-05 07:55:22 +0200

    Vpc_Abstract_ExtConfigNone doesn't exists anymore since there is a Vps_Component_Abstract_ExtConfig_None

--- Vpc/Shop/Products/Directory/Component.php
+++ Vpc/Shop/Products/Directory/Component.php
@@ -20,7 +20,7 @@ class Vpc_Shop_Products_Directory_Component extends Vpc_Directories_ItemPage_Dir
         $ret['componentName'] = trlVps('Shop.Products');
         $ret['flags']['hasResources'] = true;
 
-        $ret['extConfig'] = 'Vpc_Abstract_ExtConfigNone';
+        $ret['extConfig'] = 'Vps_Component_Abstract_ExtConfig_None';
         $ret['extConfigControllerIndex'] = 'Vpc_Directories_Item_Directory_ExtConfigEditButtons';
         return $ret;
     }
