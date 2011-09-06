7cf8f6472971af7961ca82beb3228b0ebef52dd9
Niko Sams <ns@vivid-planet.com>
2011-09-06 15:07:51 +0200

    show pagePropertiesForm from generator in PageProperties form

--- Vpc/Root/Category/GeneratorController.php
+++ Vpc/Root/Category/GeneratorController.php
@@ -21,7 +21,7 @@ class Vpc_Root_Category_GeneratorController extends Vps_Controller_Action_Auto_F
         }
     }
 
-    private function _getComponentId()
+    private function _getComponentOrParentId()
     {
         if ($this->_getParam('id')) {
             $id = $this->_getParam('id');
@@ -34,8 +34,9 @@ class Vpc_Root_Category_GeneratorController extends Vps_Controller_Action_Auto_F
     protected function _initFields()
     {
         $componentClasses = array();
+        $componentNames = array();
         $component = Vps_Component_Data_Root::getInstance()
-            ->getComponentById($this->_getComponentId(), array('ignoreVisible' => true));
+            ->getComponentById($this->_getComponentOrParentId(), array('ignoreVisible' => true));
         while (empty($componentClasses) && $component) {
             foreach (Vpc_Abstract::getSetting($component->componentClass, 'generators') as $key => $generator) {
                 if (is_instance_of($generator['class'], 'Vpc_Root_Category_Generator')) {
@@ -43,7 +44,8 @@ class Vpc_Root_Category_GeneratorController extends Vps_Controller_Action_Auto_F
                         $name = Vpc_Abstract::getSetting($class, 'componentName');
                         if ($name) {
                             $name = str_replace('.', ' ', $name);
-                            $componentClasses[$k] = $name;
+                            $componentNames[$k] = $name;
+                            $componentClasses[$k] = $class;
                         }
                     }
                 }
@@ -64,23 +66,78 @@ class Vpc_Root_Category_GeneratorController extends Vps_Controller_Action_Auto_F
             ->setVtype('alphanum');
 
         $fields->add(new Vps_Form_Field_Select('component',  trlVps('Pagetype')))
-            ->setValues($componentClasses)
+            ->setValues($componentNames)
             ->setTpl('<tpl for="."><div class="x-combo-list-item">{name}</div></tpl>')
             ->setAllowBlank(false);
         $fields->add(new Vps_Form_Field_Checkbox('hide',  trlVps('Hide in Menu')));
 
         $component = Vps_Component_Data_Root::getInstance()
-            ->getComponentById($this->_getComponentId(), array('ignoreVisible' => true));
+            ->getComponentById($this->_getComponentOrParentId(), array('ignoreVisible' => true));
+        if ($this->_getParam('id')) {
+            //$component is the component, just get inheritClasses
+            $inheritClasses = $component->inheritClasses;
+        } else {
+            //$component is the parent component (we are adding)
+            //this code is very similar to Vps_Component_Data::__get inheritClasses
+            $inheritClasses = array();
+            $page = $component;
+            while (($page = $page->parent)) {
+                foreach (Vpc_Abstract::getSetting($page->componentClass, 'generators') as $gKey=> $g) {
+                    if (isset($g['inherit']) && $g['inherit']) {
+                        if (isset($g['unique']) && $g['unique']) continue; //ignore, not edited
+                        if (!in_array($page->componentClass, $inheritClasses)) {
+                            $inheritClasses[] = $page->componentClass;
+                        }
+                    }
+                }
+                if ($page->inherits) {
+                    //wenn page selbst erbt einfach von da übernehmen (rekursiver aufruf)
+                    $inheritClasses = array_merge($inheritClasses, $page->inheritClasses);
+                    break; //aufhören, rest kommt durch rekursion daher
+                }
+            }
+        }
+        unset($component);
+        $generators = array();
+        $generatorForms = array();
+        $formsForComponent = array();
+        foreach ($componentClasses as $key=>$componentClass) {
+            $component = array(
+                'componentClass' => $componentClass,
+                'inheritClasses' => $inheritClasses
+            );
+            $formsForComponent[$key] = array();
+            foreach (Vps_Component_Generator_Abstract::getInstances($component) as $g) {
+                if (!isset($generators[$g->getClass().'.'.$g->getGeneratorKey()])) {
+                    $generators[$g->getClass().'.'.$g->getGeneratorKey()] = $g;
+                    $f = $g->getPagePropertiesForm();
+                    if ($f) {
+                        $f->setName('gen_'.$g->getGeneratorKey());
+                        $f->setIdTemplate('{0}-'.$g->getGeneratorKey());
+                        $f->setCreateMissingRow(true);
+                        $fields->add($f);
+                    }
+                    $generatorForms[$g->getClass().'.'.$g->getGeneratorKey()] = $f;
+                }
+                if ($generatorForms[$g->getClass().'.'.$g->getGeneratorKey()]) {
+                    $formsForComponent[$key][] = 'gen_'.$g->getGeneratorKey();
+                }
+            }
+        }
+        $this->_form->setFormsForComponent($formsForComponent);
 
-        foreach (Vps_Component_Generator_Abstract::getInstances($component) as $g) {
-            $f = $g->getPagePropertiesForm();
+        /*
+        foreach ($componentClasses as $key=>$componentClass) {
+            $f = Vpc_Admin::getInstance($componentClass)->getPagePropertiesForm();
             if ($f) {
-                $f->setName($g->getGeneratorKey());
+                $f->setName($key);
                 $f->setIdTemplate('{0}');
                 $f->setCreateMissingRow(true);
                 $fields->add($f);
             }
         }
+        + the same for child components (using editComponents?)
+        */
     }
 
     protected function _beforeInsert(Vps_Model_Row_Interface $row)
