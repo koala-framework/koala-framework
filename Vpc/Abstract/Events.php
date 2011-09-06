cc68dea8c91e9475fbb93c2acab9f1d82d6febea
Franz Unger <ufx@vivid-planet.com>
2011-09-06 14:54:29 +0200

    Test Component_Cache_OwnModel is working

--- Vpc/Abstract/Events.php
+++ Vpc/Abstract/Events.php
@@ -7,20 +7,21 @@ class Vpc_Abstract_Events extends Vps_Component_Abstract_Events
         if (Vpc_Abstract::hasSetting($this->_class, 'ownModel')) {
             $ret[] = array(
                 'class' => Vpc_Abstract::getSetting($this->_class, 'ownModel'),
-                'event' => Vps_Component_Abstract_Events::EVENT_ROW_UPDATE,
+                'event' => Vps_Component_Events::EVENT_ROW_UPDATE,
                 'callback' => 'onOwnRowUpdate'
             );
         }
         return $ret;
     }
 
-    public function onOwnRowUpdate($event, $class, $data)
+    public function onOwnRowUpdate($event, $data)
     {
-        if ($data['dirtyColumns']) {
-            foreach ($this->_getComponentsByDbIdOwnClass($data['row']->component_id) as $c) {
-                self::fireEvent(Vps_Component_Abstract_Events::EVENT_COMPONENT_CONTENT_CHANGE, $c->componentClass, $c->componentId);
-                self::fireEvent(Vps_Component_Abstract_Events::EVENT_COMPONENT_HAS_CONTENT_CHANGE, $c->componentClass, $c->componentId);
-            }
+        foreach ($this->_getComponentsByDbIdOwnClass($data['row']->component_id) as $c) {
+            self::fireEvent(Vps_Component_Events::EVENT_COMPONENT_CONTENT_CHANGE, $c->componentClass, $c->componentId);
+            //$hc = $c->getComponent()->hasContent(); //todo: make this more selective
+            //if ($hc != getFromCache($c->componentId)) {
+                self::fireEvent(Vps_Component_Events::EVENT_COMPONENT_HAS_CONTENT_CHANGE, $c->componentClass, array('componentId'=>$c->componentId, 'hasContent'=>$hc));
+            //}
         }
     }
 }
--- Vps/Component/Abstract/Events.php
+++ Vps/Component/Abstract/Events.php
@@ -1,71 +1,9 @@
 <?php
-class Vps_Component_Abstract_Events
+class Vps_Component_Abstract_Events extends Vps_Component_Events
 {
-    protected $_class;
-
-    const EVENT_ROW_UPDATE = 'rowUpdate';
-    const EVENT_COMPONENT_CONTENT_CHANGE = 'componentContentChange';
-    const EVENT_COMPONENT_HAS_CONTENT_CHANGE = 'componentHasContentChange';
-
-
-    protected function __construct($class)
+    public static function getGetInstanceClass($componentClass)
     {
-        $this->_class = $class;
-        $this->_init();
-    }
-
-    protected function _init()
-    {
-    }
-
-    /**
-     * @return $this
-     */
-    public static function getInstance($componentClass)
-    {
-        static $instances = array();
-        if (!isset($instances[$componentClass])) {
-            $c = Vpc_Admin::getComponentClass($componentClass, 'Events');
-            if (!$c) { return null; }
-            $instances[$componentClass] = new $c($componentClass);
-        }
-        return $instances[$componentClass];
-    }
-
-    public static function getAllListeners()
-    {
-        static $listeners; // todo: cache in apc
-        if (!isset($listeners)) {
-            $listeners = array();
-            $eventObjects = array();
-            foreach (Vpc_Abstract::getComponentClasses() as $componentClass) {
-                $eventObjects[] = Vps_Component_Abstract_Events::getInstance($componentClass);
-            }
-            $eventObjects[] = Vps_Component_Cache::getInstance();
-            foreach ($eventObjects as $eventObject)
-                foreach ($eventObject->getListeners() as $listener) {
-                    // todo: make it failproof
-                    $class = $listener['class'];
-                    $event = $listener['event'];
-                    $callbackClass = get_class($eventObject);
-                    $callbackMethod = $listener['callback'];
-                    if (!isset($listeners[$event][$class][$callbackClass])) {
-                        $listeners[$event][$class][$callbackClass] = array();
-                    }
-                    $listeners[$event][$class][$callbackClass][] = $callbackMethod;
-                }
-        }
-        d($listeners);
-        return $listeners;
-    }
-
-    public static function fireEvent($event, $class, $data)
-    {
-        foreach (self::getAllListeners() as $listener) {
-            if ($listener['event'] == $event && $listener['class'] == $class) {
-                $ev->{$listener['callback']}($event, $class, $data);
-            }
-        }
+        return Vpc_Admin::getComponentClass($componentClass, 'Events');
     }
 
     protected function _getComponentsByDbIdOwnClass($dbId)
