dd2195f22d6f6f5adb2824b9de6285c4c6c005d6
Niko Sams <ns@vivid-planet.com>
2011-09-07 10:55:40 +0200

    add fieldset when edited in pageProperties

--- Vpc/Basic/LinkTag/Admin.php
+++ Vpc/Basic/LinkTag/Admin.php
@@ -18,6 +18,12 @@ class Vpc_Basic_LinkTag_Admin extends Vpc_Admin
     public function getPagePropertiesForm()
     {
         $ret = new Vpc_Basic_LinkTag_Form(null, $this->_class);
+        $fs = new Vps_Form_Container_FieldSet(trlVps('Link'));
+        foreach ($ret as $f) {
+            $ret->fields->remove($f);
+            $fs->add($f);
+        }
+        $ret->add($fs);
         return $ret;
     }
 }
