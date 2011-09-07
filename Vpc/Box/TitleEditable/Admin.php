e97f99a4678fdd97a441a8f41f0c86ce1406cb9d
Niko Sams <ns@vivid-planet.com>
2011-09-07 08:32:47 +0200

    reuse existing form

--- Vpc/Box/TitleEditable/Admin.php
+++ Vpc/Box/TitleEditable/Admin.php
@@ -3,9 +3,6 @@ class Vpc_Box_TitleEditable_Admin extends Vpc_Abstract_Admin
 {
     public function getPagePropertiesForm()
     {
-        $form = new Vpc_Abstract_Form(null, $this->_class);
-        $form->add(new Vps_Form_Field_TextField('title', trlVps('Title')))
-            ->setWidth(450);
-        return $form;
+        return new Vpc_Box_TitleEditable_Form(null, $this->_class);
     }
 }
