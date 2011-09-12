63db2b77a3d4b0620aa8ced9c28f85dc9effa2e6
Franz Unger <ufx@vivid-planet.com>
2011-09-12 17:06:31 +0200

    Generator-Events for Table_Generator, with tests

--- Vps/Component/Generator/Events.php
+++ Vps/Component/Generator/Events.php
@@ -1,5 +1,10 @@
 <?php
-abstract class Vps_Component_Generator_Events extends Vps_Component_Events
+class Vps_Component_Generator_Events extends Vps_Component_Events
 {
-
+    protected function _getGenerator()
+    {
+        return Vps_Component_Generator_Abstract::getInstance(
+            $this->_config['componentClass'], $this->_config['generatorKey']
+        );
+    }
 }
--- Vps/Component/Generator/Events/Table.php
+++ Vps/Component/Generator/Events/Table.php
@@ -1,4 +1,58 @@
 <?php
 class Vps_Component_Generator_Events_Table extends Vps_Component_Generator_Events
 {
+    public function getListeners()
+    {
+        $ret = parent::getListeners();
+        $ret[] = array(
+            'class' => get_class($this->_getGenerator()->getModel()),
+            'event' => Vps_Component_Events::EVENT_ROW_UPDATE,
+            'callback' => 'onRowUpdate'
+        );
+        $ret[] = array(
+            'class' => get_class($this->_getGenerator()->getModel()),
+            'event' => Vps_Component_Events::EVENT_ROW_INSERT,
+            'callback' => 'onRowAdd'
+        );
+        $ret[] = array(
+            'class' => get_class($this->_getGenerator()->getModel()),
+            'event' => Vps_Component_Events::EVENT_ROW_DELETE,
+            'callback' => 'onRowDelete'
+        );
+        return $ret;
+    }
+
+    public function onRowUpdate($event, $row)
+    {
+        if (in_array('visible', $row->getDirtyColumns())) {
+            if ($row->visible) {
+                $event = Vps_Component_Events::EVENT_COMPONENT_ADD;
+            } else {
+                $event = Vps_Component_Events::EVENT_COMPONENT_REMOVE;
+            }
+            $this->fireEvent($event, $this->_config['componentClass'], $row);
+        }
+        if (in_array('pos', $row->getDirtyColumns()) && $row->visible) {
+            $event = Vps_Component_Events::EVENT_COMPONENT_MOVE;
+            $this->fireEvent($event, $this->_config['componentClass'], $row);
+        }
+        if (in_array('component', $row->getDirtyColumns()) && $row->visible) {
+            $event = Vps_Component_Events::EVENT_COMPONENT_CLASS_CHANGE;
+            $this->fireEvent($event, $this->_config['componentClass'], $row);
+        }
+    }
+
+    public function onRowAdd($event, $row)
+    {
+        if ($row->visible) {
+            $this->fireEvent(Vps_Component_Events::EVENT_COMPONENT_ADD, $this->_config['componentClass'], $row);
+        }
+    }
+
+    public function onRowDelete($event, $row)
+    {
+        if ($row->visible) {
+            $this->fireEvent(Vps_Component_Events::EVENT_COMPONENT_REMOVE, $this->_config['componentClass'], $row);
+        }
+    }
 }
