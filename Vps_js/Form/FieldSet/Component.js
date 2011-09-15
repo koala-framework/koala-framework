8aaa5e0a224ade6a4b58bce21103648506865ee2
Niko Sams <ns@vivid-planet.com>
2011-09-15 17:50:48 +0200

    set collapsed class on startup

--- Vps_js/Form/FieldSet/Component.js
+++ Vps_js/Form/FieldSet/Component.js
@@ -3,6 +3,9 @@ Vps.onContentReady(function()
     var checkboxes = Ext.query('div.vpsFormContainerFieldSet fieldset legend input');
     Ext.each(checkboxes, function(c) {
         c = Ext.get(c);
+        if (!c.dom.checked) {
+            c.up('fieldset').addClass('vpsFormContainerFieldSetCollapsed');
+        }
         c.on('click', function() {
             if (this.dom.checked) {
                 this.up('fieldset').removeClass('vpsFormContainerFieldSetCollapsed');
