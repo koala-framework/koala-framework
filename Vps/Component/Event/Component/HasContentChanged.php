d795476097cd0966e584e4192e2002bbcbb3a619
Franz Unger <ufx@vivid-planet.com>
2011-09-20 07:34:24 +0200

    HasContent-Test

--- Vps/Component/Event/Component/HasContentChanged.php
+++ Vps/Component/Event/Component/HasContentChanged.php
@@ -1,4 +1,4 @@
 <?php
-class Vps_Component_Event_Component_HasContendChanged extends Vps_Component_Event_Component_Abstract
+class Vps_Component_Event_Component_HasContentChanged extends Vps_Component_Event_Component_Abstract
 {
 }
