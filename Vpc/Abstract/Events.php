f6cb9c761c14f83b1a38d69dd38c190a90859c85
Franz Unger <ufx@vivid-planet.com>
2011-09-20 07:35:39 +0200

    Events for Cards_Component

--- Vpc/Abstract/Events.php
+++ Vpc/Abstract/Events.php
@@ -19,15 +19,5 @@ class Vpc_Abstract_Events extends Vps_Component_Abstract_Events
         $this->fireEvent(new Vps_Component_Event_Component_ContentChanged(
             $this->_class, $event->row->component_id
         ));
-        /*
-        foreach (Vps_Component_Data_Root::getInstance()->getComponentsByDbId($row->component_id, array('componentClass'=>$this->_class)) as $c) {
-            self::fireEvent('Vps_Component_Event_Component_ContentChanged', $c->componentClass, $c->componentId);
-            self::fireEvent('Vps_Component_Event_Component_HasContentChanged', $c->componentClass, $c->componentId);
-            //$hc = $c->getComponent()->hasContent(); //todo: make this more selective
-            //if ($hc != getFromCache($c->componentId)) {
-                //self::fireEvent(Vps_Component_Events::EVENT_COMPONENT_HAS_CONTENT_CHANGE, $c->componentClass, array('componentId'=>$c->componentId, 'hasContent'=>$hc));
-            //}
-        }
-        */
     }
 }
