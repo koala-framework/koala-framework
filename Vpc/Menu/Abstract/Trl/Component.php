0d9612964fca3fab385150f3518b53284f32c099
Niko Sams <ns@vivid-planet.com>
2011-09-23 15:23:27 +0200

    current and class is not anymore in data (thankfully)
    so this hack isn't needed anymore

--- Vpc/Menu/Abstract/Trl/Component.php
+++ Vpc/Menu/Abstract/Trl/Component.php
@@ -8,16 +8,6 @@ abstract class Vpc_Menu_Abstract_Trl_Component extends Vpc_Chained_Trl_Component
         return $ret;
     }
 
-    protected function _getChainedComponent($component)
-    {
-        $ret = Vpc_Chained_Trl_Component::getChainedByMaster($component, $this->getData());
-        if ($ret) {
-            if (isset($component->current)) $ret->current = $component->current;
-            if (isset($component->class)) $ret->class = $component->class;
-        }
-        return $ret;
-    }
-
     public static function getStaticCacheMeta($componentClass)
     {
         $ret = parent::getStaticCacheMeta($componentClass);
