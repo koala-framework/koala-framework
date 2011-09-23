d5ed69f7e1be188f876dd4c74fb4a0817b94c98b
Niko Sams <ns@vivid-planet.com>
2011-09-23 15:20:11 +0200

    implement alternativeComponents correctly for chained
    also slightly modify masterComponentsMap

--- Vpc/Chained/Abstract/Generator.php
+++ Vpc/Chained/Abstract/Generator.php
@@ -190,7 +190,7 @@ class Vpc_Chained_Abstract_Generator extends Vps_Component_Generator_Abstract
 
     protected function _formatConfig($parentData, $row)
     {
-        $componentClass = $this->_settings['component'][$this->_settings['masterComponentsMap'][$row->componentClass]];
+        $componentClass = $this->_settings['masterComponentsMap'][$row->componentClass];
 
         $alternativeComponent = Vpc_Abstract::getFlag($componentClass, "alternativeComponent");
         if ($alternativeComponent && call_user_func(array($componentClass, 'useAlternativeComponent'), $componentClass, $parentData, $this)) {
