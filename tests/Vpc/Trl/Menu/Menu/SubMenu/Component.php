fd7c70752f9418a744cd906efd659d215877d6ce
Niko Sams <ns@vivid-planet.com>
2011-09-26 13:33:11 +0200

    remove showAsEditComponent setting, this was removed with the new menu editing
    and add a check that verifies it is not set in a web

--- tests/Vpc/Trl/Menu/Menu/SubMenu/Component.php
+++ tests/Vpc/Trl/Menu/Menu/SubMenu/Component.php
@@ -4,7 +4,6 @@ class Vpc_Trl_Menu_Menu_SubMenu_Component extends Vpc_Menu_Component
     public static function getSettings()
     {
         $ret = parent::getSettings();
-        $ret['showAsEditComponent'] = true;
         unset($ret['dataModel']);
         $ret['level'] = 2;
         return $ret;
