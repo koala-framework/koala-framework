d8fb00975f0801383086858cef5eafdd2e840057
Niko Sams <ns@vivid-planet.com>
2011-09-30 08:57:40 +0200

    enable view cache in tests in setUp
    so tests don't depend on a specific execution order

--- tests/Vpc/TestAbstract.php
+++ tests/Vpc/TestAbstract.php
@@ -14,6 +14,8 @@ abstract class Vpc_TestAbstract extends Vps_Test_TestCase
         $this->_root->setFilename('vps/vpctest/'.$componentClass);
         apc_clear_cache('user');
         Vps_Component_Cache::saveStaticMeta();
+        Vps_Registry::get('config')->debug->componentCache->disable = false;
+        Vps_Config::deleteValueCache('debug.componentCache.disable');
     }
 
     protected final function _process()
