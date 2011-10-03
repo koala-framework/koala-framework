7623d5d300ecb0bb64dedbf32b6238d7c46c362f
Niko Sams <ns@vivid-planet.com>
2011-10-03 10:32:30 +0200

    use query, has less dependencies

--- Vpc/Basic/ImageEnlarge/EnlargeTag/Component.js
+++ Vpc/Basic/ImageEnlarge/EnlargeTag/Component.js
@@ -1,6 +1,6 @@
 Vps.onContentReady(function() {
-    Ext.select('.vpcEnlargeTag').each(function(el) {
-        if (!el.child('.webZoom')) {
+    Ext.query('.vpcEnlargeTag').each(function(el) {
+        if (!Ext.fly(el).child('.webZoom')) {
             Ext.DomHelper.append(el,
                 { tag: 'span', cls: 'webZoom' }
             );
