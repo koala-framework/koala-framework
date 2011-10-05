9848d804fc0f2c217c54b8557ba1de58f30963c7
Franz Unger <ufx@vivid-planet.com>
2011-10-05 15:16:10 +0200

    Event_Model_Updated still had wrong implementation

--- Vps/Component/Event/Model/Updated.php
+++ Vps/Component/Event/Model/Updated.php
@@ -3,6 +3,6 @@ class Vps_Component_Event_Model_Updated extends Vps_Component_Event_Abstract
 {
     public function __construct(Vps_Model_Abstract $model)
     {
-        $this->setClass(get_class($model));
+        $this->class = get_class($model);
     }
 }
