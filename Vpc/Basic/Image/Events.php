40b7760a3cc0fb91f34e820416f2f698ab714436
Christoph Eder <ce@vivid-planet.com>
2011-10-06 14:32:03 +0200

    workaround component not found, this should not happen
    by niko

--- Vpc/Basic/Image/Events.php
+++ Vpc/Basic/Image/Events.php
@@ -15,9 +15,9 @@ class Vpc_Basic_Image_Events extends Vpc_Abstract_Image_Events
 
     public function onMediaChanged(Vps_Component_Event_Media_Changed $event)
     {
-        $components = Vps_Component_Data_Root::getInstance()
-            ->getComponentById($event->componentId)
-            ->getChildComponents(array('componentClass' => $this->_class));
+        $c = Vps_Component_Data_Root::getInstance()->getComponentById($event->componentId);
+        if (!$c) return;
+        $components = $c->getChildComponents(array('componentClass' => $this->_class));
         foreach ($components as $component) {
             $this->fireEvent(new Vps_Component_Event_Media_Changed(
                 $this->_class, $component->componentId
