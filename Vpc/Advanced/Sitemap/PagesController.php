9e2cc245885dbda22731b4f6df41ce621a354c81
Niko Sams <ns@vivid-planet.com>
2011-10-10 15:30:59 +0200

    add sitemap component

---
+++ Vpc/Advanced/Sitemap/PagesController.php
@@ -0,0 +1,8 @@
+<?php
+class Vpc_Advanced_Sitemap_PagesController extends Vps_Controller_Action_Component_PagesController
+{
+    protected function _isAllowedComponent()
+    {
+        return true;
+    }
+}
