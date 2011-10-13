2357a12affa749c8d7fc4b4efe8b93cb62c79be5
Niko Sams <ns@vivid-planet.com>
2011-10-13 08:31:19 +0200

    fix dynamic in partialId
    info must not be cached, as it would not be dynamic then
    
    Conflicts:
    
    	tests/Vps/Component/Cache/DynamicWithPartialId/Test.php

--- Vps/Component/View/Helper/Partial.php
+++ Vps/Component/View/Helper/Partial.php
@@ -24,6 +24,24 @@ class Vps_Component_View_Helper_Partial extends Vps_Component_View_Renderer
         return $view->render($template);
     }
 
+    
+    public function renderCached($cachedContent, $componentId, $config)
+    {
+        //add info to dynamic config, has to be done here because config is dynamic (from partials)
+
+                                      //componentId   config
+        preg_match_all('/{cc dynamic: [a-zA-Z0-9_-]+ ([^}]*)}/i', $cachedContent, $matches);
+        foreach ($matches[1] as $match) {
+            $dynamicConfig = $match;
+            $dynamicConfig = $dynamicConfig != '' ? unserialize(base64_decode($dynamicConfig)) : array();
+            $dynamicConfig['info'] = $config['info'];
+            $dynamicConfig = base64_encode(serialize($dynamicConfig));
+            $cachedContent = str_replace($match, $dynamicConfig, $cachedContent);
+        }
+        return $cachedContent;
+    }
+
+
     protected function _getTemplate($componentClass, $config)
     {
         return Vpc_Abstract::getTemplateFile($componentClass, 'Partial');
