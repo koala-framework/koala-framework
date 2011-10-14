10dee17ada43c21b52f7419bd1ca3e13480f07e0
Niko Sams <ns@vivid-planet.com>
2011-10-14 13:54:21 +0200

    clear menu cache when page was added

--- tests/Kwf/Component/Cache/Menu/Test.php
+++ tests/Kwf/Component/Cache/Menu/Test.php
@@ -46,26 +46,23 @@ class Kwf_Component_Cache_Menu_Test extends Kwc_TestAbstract
 
     public function testAddPage()
     {
-        $this->markTestIncomplete('eventscache');
-
         $page = $this->_root->getComponentById(1);
         $page->render(true, true);
 
         $row = $this->_root->getGenerator('page')->getModel()->createRow(array(
-            'id'=>5, 'pos'=>3, 'visible'=>true, 'name'=>'f5', 'filename' => 'f5',
+            'pos'=>3, 'visible'=>true, 'name'=>'f5', 'filename' => 'f5',
                   'parent_id'=>'root', 'component'=>'empty', 'is_home'=>false, 'hide'=>false, 'custom_filename' => null
         ));
         $row->save();
         $this->_process();
         $html = $page->render(true, true);
-        //d($html);
         $this->assertEquals(3, substr_count($html, '<li'));
-        $this->assertEquals(2, substr_count($html, 'g1'));
+        $this->assertEquals(2, substr_count($html, 'f1'));
         $this->assertEquals(2, substr_count($html, 'f4'));
         $this->assertEquals(2, substr_count($html, 'f5'));
     }
 
-    public function testMovePage()
+    public function testChangeParent()
     {
         $this->markTestIncomplete('eventscache');
 
@@ -93,5 +90,15 @@ class Kwf_Component_Cache_Menu_Test extends Kwc_TestAbstract
         $this->assertEquals(2, substr_count($html, '<li'));
         $this->assertEquals(2, substr_count($html, 'f3'));
         $this->assertEquals(2, substr_count($html, 'f4'));
+    }
+
+    public function testRemovePage()
+    {
+        $this->markTestIncomplete('eventscache');
+    }
+
+    public function testPositionChange()
+    {
+        $this->markTestIncomplete('eventscache');
     }
 }
