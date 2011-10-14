1935efab488bfa87c56b175ac4bbdef8fbd16050
Niko Sams <ns@vivid-planet.com>
2011-10-14 13:54:21 +0200

    clear menu cache when page was added

---
+++ Kwc/Menu/Abstract/Events.php
@@ -0,0 +1,40 @@
+<?php
+class Kwc_Menu_Abstract_Events extends Kwc_Abstract_Events
+{
+    public function getListeners()
+    {
+        $ret = parent::getListeners();
+        $ret[] = array(
+            'class' => null,
+            'event' => 'Kwf_Component_Event_Page_Added',
+            'callback' => 'onPageAdd'
+        );
+        return $ret;
+    }
+
+    public function onPageAdd(Kwf_Component_Event_Page_Added $event)
+    {
+        $menuLevel = Kwc_Abstract::getSetting($this->_class, 'level');
+        foreach (Kwf_Component_Data_Root::getInstance()->getComponentsByDbId($event->dbId) as $data) {
+            $level = 0;
+            while ($data && !Kwc_Abstract::getFlag($data->componentClass, 'menuCategory')) {
+                if ($data->isPage) $level++;
+                $data = $data->parent;
+            }
+            $cat = Kwc_Abstract::getFlag($data->componentClass, 'menuCategory');
+            if (is_int($menuLevel)) {
+                if ($menuLevel == $level+1) {
+                    $this->fireEvent(new Kwf_Component_Event_ComponentClass_ContentChanged(
+                        $this->_class
+                    ));
+                }
+            } else {
+                if ($level == 1 && $cat == $menuLevel) {
+                    $this->fireEvent(new Kwf_Component_Event_ComponentClass_ContentChanged(
+                        $this->_class
+                    ));
+                }
+            }
+        }
+    }
+}
