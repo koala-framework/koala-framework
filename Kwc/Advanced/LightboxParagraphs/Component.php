a2b0a9be93c7fb7d4946441c9ff3ded83b764016
Christoph Eder <ce@vivid-planet.com>
2011-10-20 15:20:59 +0200

    fix renaming

--- Kwc/Advanced/LightboxParagraphs/Component.php
+++ Kwc/Advanced/LightboxParagraphs/Component.php
@@ -1,10 +1,10 @@
 <?php
-class Kwc_Advanced_LightboxParagraphs_Component extends Vpc_Paragraphs_Component
+class Kwc_Advanced_LightboxParagraphs_Component extends Kwc_Paragraphs_Component
 {
     public static function getSettings()
     {
         $ret = parent::getSettings();
-        $ret['componentName'] = trlVps('Paragraphs in Lightbox');
+        $ret['componentName'] = trlKwf('Paragraphs in Lightbox');
         $ret['contentSender'] = 'Kwf_Component_Abstract_ContentSender_Lightbox';
         return $ret;
     }
