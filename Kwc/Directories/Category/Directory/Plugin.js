8fdbd5bbaad445d123a1d3f21568a02eb1b903a3
Niko Sams <ns@vivid-planet.com>
2011-10-24 17:27:35 +0200

    fix directory plugin in TabsPanel

--- Kwc/Directories/Category/Directory/Plugin.js
+++ Kwc/Directories/Category/Directory/Plugin.js
@@ -6,6 +6,9 @@ Ext.extend(Kwc.Directories.Category.Directory.Plugin, Ext.util.Observable,
 {
     init: function(newsPanel)
     {
+        if (newsPanel instanceof Kwc.Directories.Item.Directory.TabsPanel) {
+            newsPanel = newsPanel.grid;
+        }
         newsPanel.on('beforerendergrid', function(grid) {
             grid.getTopToolbar().add({
                 text    : trlKwf('Categories'),
