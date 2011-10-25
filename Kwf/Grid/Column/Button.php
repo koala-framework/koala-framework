47dafbd95967e1c1eacb9b681a940f47cc823125
Niko Sams <ns@vivid-planet.com>
2011-10-25 08:51:11 +0200

    a button loads nothing, so set empty data

--- Kwf/Grid/Column/Button.php
+++ Kwf/Grid/Column/Button.php
@@ -11,6 +11,7 @@ class Kwf_Grid_Column_Button extends Kwf_Grid_Column
         $this->setRenderer('cellButton');
         $this->setSortable(false);
         $this->setColumnType('button');
+        $this->setData(new Kwf_Data_Empty());
     }
 
     public function load($row, $role)
