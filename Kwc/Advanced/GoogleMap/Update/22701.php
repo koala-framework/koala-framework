1d7d721d21e6a663096b19850225d9986ce6272f
Niko Sams <ns@vivid-planet.com>
2011-10-25 11:26:18 +0200

    only execute this update script when old table exists

--- Kwc/Advanced/GoogleMap/Update/22701.php
+++ Kwc/Advanced/GoogleMap/Update/22701.php
@@ -3,12 +3,19 @@ class Kwc_Advanced_GoogleMap_Update_22701 extends Kwf_Update
 {
     protected function _init()
     {
-        $this->_actions[] = new Kwf_Update_Action_Component_ConvertTableToFieldModel(array(
-            'table'=>'kwc_advanced_googlemap',
-        ));
-        $this->_actions[] = new Kwf_Update_Action_Db_DropTable(array(
-            'table'=>'kwc_advanced_googlemap',
-        ));
+        $db = Kwf_Registry::get('db');
+        $q = $db->query("SHOW TABLES");
+        $tables = array();
+        while($t = $q->fetchColumn()) {
+            $tables[] = $t;
+        }
+        if (in_array('kwc_advanced_googlemap', $tables)) {
+            $this->_actions[] = new Kwf_Update_Action_Component_ConvertTableToFieldModel(array(
+                'table'=>'kwc_advanced_googlemap',
+            ));
+            $this->_actions[] = new Kwf_Update_Action_Db_DropTable(array(
+                'table'=>'kwc_advanced_googlemap',
+            ));
+        }
     }
-
 }
