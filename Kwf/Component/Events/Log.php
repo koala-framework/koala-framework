baf40d0026fd976ea5b849162796ea3f202d64c5
Niko Sams <ns@vivid-planet.com>
2011-11-02 17:20:49 +0100

    fix log

--- Kwf/Component/Events/Log.php
+++ Kwf/Component/Events/Log.php
@@ -18,7 +18,7 @@ class Kwf_Component_Events_Log extends Zend_Log
     public function logEvent($indent, $callback, Kwf_Component_Event_Abstract $event)
     {
         $message =
-            str_repeat(' ', ($indent - 1) * 2) .
+            str_repeat(' ', $indent * 2) .
             $event->__toString() . ': ' .
             ( $callback ? ($callback['class'] . '::' . $callback['method'] . '(' . _btArgsString($callback['config']) . ')') : '(none)' );
         $this->log($message, Zend_Log::INFO);
