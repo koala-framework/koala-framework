0c1c448961725337d0dc0c0982ea40d428e7d550
Franz Unger <ufx@vivid-planet.com>
2011-11-03 14:44:46 +0100

    composite-events for hascontent: only throw if there is a parentDbId

--- Kwc/Abstract/Composite/Events.php
+++ Kwc/Abstract/Composite/Events.php
@@ -19,8 +19,11 @@ class Kwc_Abstract_Composite_Events extends Kwc_Abstract_Events
 
     public function onChildHasContentChange(Kwf_Component_Event_Component_HasContentChanged $event)
     {
-        $this->fireEvent(new Kwf_Component_Event_Component_HasContentChanged(
-            $this->_class, $event->getParentDbId()
-        ));
+        $parentDbId = $event->getParentDbId();
+        if ($parentDbId) {
+            $this->fireEvent(new Kwf_Component_Event_Component_HasContentChanged(
+                $this->_class, $parentDbId
+            ));
+        }
     }
 }
