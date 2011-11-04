c3c02257d3d1dd39c228e0036f6d71fbcc75d53d
Niko Sams <ns@vivid-planet.com>
2011-11-04 13:44:54 +0100

    the whole point of ParentContent is that clearing the content cache is not necessary
    so just don't do it

--- Kwc/Menu/ParentContent/Events.php
+++ Kwc/Menu/ParentContent/Events.php
@@ -1,35 +1,4 @@
 <?php
 class Kwc_Menu_ParentContent_Events extends Kwc_Basic_ParentContent_Events
 {
-    public function getListeners()
-    {
-        $ret = parent::getListeners();
-        $ret[] = array(
-            'class' => Kwc_Abstract::getSetting($this->_class, 'menuComponentClass'),
-            'event' => 'Kwf_Component_Event_ComponentClass_ContentChanged',
-            'callback' => 'onMenuComponentClassContentChanged'
-        );
-        $ret[] = array(
-            'class' => Kwc_Abstract::getSetting($this->_class, 'menuComponentClass'),
-            'event' => 'Kwf_Component_Event_Component_ContentChanged',
-            'callback' => 'onMenuContentChanged'
-        );
-        return $ret;
-    }
-
-    public function onMenuComponentClassContentChanged(Kwf_Component_Event_ComponentClass_ContentChanged $event)
-    {
-        $this->fireEvent(new Kwf_Component_Event_ComponentClass_ContentChanged(
-            $this->_class
-        ));
-    }
-
-    public function onMenuContentChanged(Kwf_Component_Event_Component_ContentChanged $event)
-    {
-        foreach (Kwf_Component_Data_Root::getInstance()->getComponentsByDbId($event->dbId) as $c) {
-            $this->fireEvent(new Kwf_Component_Event_Component_RecursiveContentChanged(
-                $this->_class, $c->getPageOrRoot()->componentId
-            ));
-        }
-    }
 }
