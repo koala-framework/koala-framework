c5798ad987ddf457ce6423ed3c14003e1c67538c
Niko Sams <ns@vivid-planet.com>
2011-11-07 15:45:44 +0100

    clear cache for List_ChildPages_Teaser

--- Kwc/List/ChildPages/Teaser/Component.php
+++ Kwc/List/ChildPages/Teaser/Component.php
@@ -26,14 +26,4 @@ class Kwc_List_ChildPages_Teaser_Component extends Kwc_Abstract
         $ret['children'] = $this->getData()->getChildComponents(array('generator' => 'child'));
         return $ret;
     }
-
-    public static function getStaticCacheMeta($componentClass)
-    {
-        $ret = parent::getStaticCacheMeta($componentClass);
-        // Ist ziemlich grob, sonst müsste man sich eigenes Meta schreiben
-        // Wenn eine Unterseite zB. offline genommen wird, muss der Cache gelöscht werden
-        $ret[] = new Kwf_Component_Cache_Meta_Static_Model('Kwf_Component_Model');
-        $ret[] = new Kwf_Component_Cache_Meta_Static_Model('Kwc_Root_Category_GeneratorModel');
-        return $ret;
-    }
 }
---
+++ Kwc/List/ChildPages/Teaser/Events.php
@@ -0,0 +1,34 @@
+<?php
+class Kwc_List_ChildPages_Teaser_Events extends Kwc_Abstract_Events
+{
+    public function getListeners()
+    {
+        $ret = parent::getListeners();
+        $g = Kwc_Abstract::getSetting($this->_class, 'generators');
+        $ret[] = array(
+            'class' => $g['child']['component'],
+            'event' => 'Kwf_Component_Event_Component_HasContentChanged',
+            'callback' => 'onChildHasContentChanged'
+        );
+        $ret[] = array(
+            'event' => 'Kwf_Component_Event_Page_Removed',
+            'callback' => 'onPageAddedOrRemoved'
+        );
+        $ret[] = array(
+            'event' => 'Kwf_Component_Event_Page_Added',
+            'callback' => 'onPageAddedOrRemoved'
+        );
+        return $ret;
+    }
+
+    public function onChildHasContentChanged(Kwf_Component_Event_Component_HasContentChanged $ev)
+    {
+        $dbId = $ev->getParentDbId();
+        $this->fireEvent(new Kwf_Component_Event_Component_ContentChanged($this->_class, $dbId));
+    }
+
+    public function onPageAddedOrRemoved(Kwf_Component_Event_Page_Removed $ev)
+    {
+        $this->fireEvent(new Kwf_Component_Event_ComponentClass_ContentChanged($this->_class));
+    }
+}
