bed75320abf361e5879b2f32faf10f7320723136
Franz Unger <ufx@vivid-planet.com>
2011-11-09 10:32:30 +0100

    refine composite-events and let cards-events base on it

--- Kwc/Abstract/Composite/Events.php
+++ Kwc/Abstract/Composite/Events.php
@@ -4,25 +4,26 @@ class Kwc_Abstract_Composite_Events extends Kwc_Abstract_Events
     public function getListeners()
     {
         $ret = parent::getListeners();
-        $generators = Kwc_Abstract::getSetting($this->_class, 'generators');
-        if (is_array($generators['child']['component'])) {
-            foreach ($generators['child']['component'] as $component) {
-                $ret[] = array(
-                    'class' => $component,
-                    'event' => 'Kwf_Component_Event_Component_HasContentChanged',
-                    'callback' => 'onChildHasContentChange'
-                );
-            }
+        foreach (Kwc_Abstract::getChildComponentClasses($this->_class, 'child') as $class) {
+            $ret[] = array(
+                'class' => $class,
+                'event' => 'Kwf_Component_Event_Component_HasContentChanged',
+                'callback' => 'onChildHasContentChange'
+            );
         }
         return $ret;
     }
 
     public function onChildHasContentChange(Kwf_Component_Event_Component_HasContentChanged $event)
     {
-        $parentDbId = $event->getParentDbId();
-        if ($parentDbId) {
+        $classes = Kwc_Abstract::getChildComponentClasses($this->_class, 'child');
+        $key = array_search($event->class, $classes);
+        if ($key && 
+            substr($event->dbId, -strlen($key)-1) == '-' . $key && 
+            $event->getParentDbId()
+        ) {
             $this->fireEvent(new Kwf_Component_Event_Component_HasContentChanged(
-                $this->_class, $parentDbId
+                $this->_class, $event->getParentDbId()
             ));
         }
     }
