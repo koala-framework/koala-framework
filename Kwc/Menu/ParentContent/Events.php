e7eeb304dfe44e37e15b84ed0d1912f27156a902
Niko Sams <ns@vivid-planet.com>
2011-11-11 14:15:50 +0100

    clear Menu_ParentContent cache

--- Kwc/Menu/ParentContent/Events.php
+++ Kwc/Menu/ParentContent/Events.php
@@ -1,4 +1,34 @@
 <?php
-class Kwc_Menu_ParentContent_Events extends Kwc_Basic_ParentContent_Events
+class Kwc_Menu_ParentContent_Events extends Kwc_Abstract_Events //not Kwc_Basic_ParentContent_Events
 {
+    public function getListeners()
+    {
+        $ret = parent::getListeners();
+        $ret[] = array(
+            'event' => 'Kwf_Component_Event_Page_ParentChanged',
+            'callback' => 'onPageParentChanged'
+        );
+        $ret[] = array(
+            'class' => Kwc_Abstract::getSetting($this->_class, 'menuComponentClass'),
+            'event' => 'Kwf_Component_Event_Component_HasContentChanged',
+            'callback' => 'onMenuHasContentChanged'
+        );
+        return $ret;
+    }
+
+    public function onPageParentChanged(Kwf_Component_Event_Page_ParentChanged $event)
+    {
+        $this->fireEvent(new Kwf_Component_Event_ComponentClass_ContentChanged(
+            $this->_class
+        ));
+    }
+
+    public function onMenuHasContentChanged(Kwf_Component_Event_Component_HasContentChanged $event)
+    {
+        foreach (Kwf_Component_Data_Root::getInstance()->getComponentsByDbId($event->dbId) as $c) {
+            $this->fireEvent(new Kwf_Component_Event_Component_RecursiveHasContentChanged(
+                $this->_class, $c->getPageOrRoot()->componentId
+            ));
+        }
+    }
 }
