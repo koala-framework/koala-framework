934818c63fb8740a73b46de265bff2aac75e97c8
Niko Sams <ns@vivid-planet.com>
2011-11-14 09:56:39 +0100

    new List plugin: changes list item class on state change

---
+++ Kwf_js/EyeCandy/List/Plugins/StateListener/ItemClass.js
@@ -0,0 +1,12 @@
+Kwf.EyeCandy.List.Plugins.StateListener.ItemClass = Ext.extend(Kwf.EyeCandy.List.Plugins.Abstract, {
+    init: function() {
+        this.list.on('childStateChanged', this._change, this);
+    },
+    _change: function(item) {
+        this.list.states.forEach(function(s) {
+            item.el.removeClass('itemState'+s.substr(0, 1).toUpperCase()+s.substr(1));
+        }, this);
+        var s = item.getState();
+        item.el.addClass('itemState'+s.substr(0, 1).toUpperCase()+s.substr(1));
+    }
+});
