e234d8e0ffca8f05eb2ea29ba54c47c94a2e79bd
Niko Sams <ns@vivid-planet.com>
2011-11-15 17:23:38 +0100

    fix Box_SwitchLanguage

---
+++ Vpc/Box/SwitchLanguage/Trl/Component.php
@@ -0,0 +1,9 @@
+<?php
+class Vpc_Box_SwitchLanguage_Trl_Component extends Vpc_Chained_Trl_MasterAsChild_Component
+{
+    public static function getSettings($masterComponentClass)
+    {
+        $ret = parent::getSettings($masterComponentClass);
+        return $ret;
+    }
+}
