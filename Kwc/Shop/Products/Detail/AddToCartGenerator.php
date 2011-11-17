630d41feb8e342dfde629de418e606ebf12c6c28
Niko Sams <ns@vivid-planet.com>
2011-11-17 11:10:36 +0100

    fix addToCart generator

--- Kwc/Shop/Products/Detail/AddToCartGenerator.php
+++ Kwc/Shop/Products/Detail/AddToCartGenerator.php
@@ -3,7 +3,13 @@ class Kwc_Shop_Products_Detail_AddToCartGenerator extends Kwf_Component_Generato
 {
     protected function _getChildComponentClass($key, $parentData)
     {
+        if ($key != 'product') {
+            throw new Kwf_Exception("invalid key '$key'");
+        }
         $generators = Kwc_Abstract::getSetting($this->getClass(), 'generators');
+        if (count($generators['addToCart']['component']) <= 1) {
+            return $generators['addToCart']['component']['product'];
+        }
         if ($parentData) {
             foreach ($generators['addToCart']['component'] as $component => $class) {
                 if ($component == $parentData->row->component) {
