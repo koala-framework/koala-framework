b2373fff1a73c42bdc61b93b3e9bc4a993112534
Niko Sams <ns@vivid-planet.com>
2011-11-18 13:43:12 +0100

    fix shop, adapt to generator changes

--- Kwc/Shop/Products/Detail/AddToCartGenerator.php
+++ Kwc/Shop/Products/Detail/AddToCartGenerator.php
@@ -3,7 +3,7 @@ class Kwc_Shop_Products_Detail_AddToCartGenerator extends Kwf_Component_Generato
 {
     protected function _getChildComponentClass($key, $parentData)
     {
-        if ($key != 'product') {
+        if ($key != $this->getGeneratorKey()) {
             throw new Kwf_Exception("invalid key '$key'");
         }
         $generators = Kwc_Abstract::getSetting($this->getClass(), 'generators');
@@ -18,5 +18,41 @@ class Kwc_Shop_Products_Detail_AddToCartGenerator extends Kwf_Component_Generato
             }
         }
         return null;
+    }
+
+    public function getStaticChildComponentIds()
+    {
+        return array(
+            $this->getIdSeparator().$this->getGeneratorKey()
+        );
+        foreach (array_keys($this->getChildComponentClasses()) as $c) {
+            $childComponentIds[] = $this->getIdSeparator().$c;
+        }
+        return $childComponentIds;
+    }
+
+    protected function _formatConfig($parentData, $key)
+    {
+        $ret = array(
+            'componentId' => $parentData->componentId . $this->_idSeparator . $this->getGeneratorKey(),
+            'dbId' => $parentData->dbId . $this->_idSeparator . $this->getGeneratorKey(),
+            'parent' => $parentData,
+            'isPage' => false,
+            'isPseudoPage' => false,
+            'inherit' => false
+        );        
+        $ret['componentClass'] = $this->_getChildComponentClass($key, $parentData);
+        return $ret;
+    }
+
+    public function getChildData($parentData, $select = array())
+    {
+        Kwf_Benchmark::count('GenStaticSelect::getChildData');
+
+        if (is_array($select)) {
+            $select = new Kwf_Component_Select($select);
+        }
+        $data = $this->_createData($parentData, $this->getGeneratorKey(), $select);
+        return array($data);
     }
 }
