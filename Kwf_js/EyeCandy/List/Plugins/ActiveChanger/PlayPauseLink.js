e5e1aed25273cd9ec0f07130dbf1b3711130ee57
Markus Hauser <mh@vivid-planet.com>
2011-11-22 16:21:51 +0100

    added playPauseLink as activeChanger list plugin

---
+++ Kwf_js/EyeCandy/List/Plugins/ActiveChanger/PlayPauseLink.js
@@ -0,0 +1,46 @@
+Kwf.EyeCandy.List.Plugins.ActiveChanger.PlayPauseLink = Ext.extend(Kwf.EyeCandy.List.Plugins.Abstract, {
+    init: function() {
+        if (!this.interval) this.interval = 5000;
+
+        this.playPauseLink = this.list.el.createChild({
+            tag: 'a',
+            cls: 'listPlayPause',
+            href: '#'
+        });
+        this._isPlaying = false;
+
+        this.playPauseLink.on('click', function(ev) {
+            ev.stopEvent();
+            if (!this._isPlaying) {
+                this.play();
+                this.playPauseLink.removeClass('listIsPausing');
+                this.playPauseLink.addClass('listIsPlaying');
+                this._isPlaying = true;
+            } else {
+                this.pause();
+                this.playPauseLink.removeClass('listIsPlaying');
+                this.playPauseLink.addClass('listIsPausing');
+                this._isPlaying = false;
+            }
+        }, this);
+
+        if (this.autoPlay) {
+            this.play.defer(this.interval, this);
+        }
+    },
+
+    play: function() {
+        this.next();
+        this.play.defer(this.interval, this);
+    },
+
+    next: function() {
+        var item;
+        if (this.list.getActiveItem() === this.list.getLastItem()) {
+            item = this.list.getFirstItem();
+        } else {
+            item = this.list.getItem(this.list.getActiveItem().listIndex+1);
+        }
+        if (item) this.list.setActiveItem(item);
+    }
+});
