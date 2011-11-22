9ff7063a5c89ff878915ff6a4de652aa8721d2bb
Niko Sams <ns@vivid-planet.com>
2011-11-22 17:38:39 +0100

    use DateField in Dynamic Form

--- Kwc/Form/Dynamic/Paragraphs/Component.php
+++ Kwc/Form/Dynamic/Paragraphs/Component.php
@@ -11,6 +11,7 @@ class Kwc_Form_Dynamic_Paragraphs_Component extends Kwc_Paragraphs_Component
                 'textArea' => 'Kwc_Form_Field_TextArea_Component',
                 'fieldSet' => 'Kwc_Form_Container_FieldSet_Component',
                 'select' => 'Kwc_Form_Field_Select_Component',
+                'dateField' => 'Kwc_Form_Field_DateField_Component',
                 'radio' => 'Kwc_Form_Field_Radio_Component',
                 'multiCheckbox' => 'Kwc_Form_Field_MultiCheckbox_Component',
                 'file' => 'Kwc_Form_Field_File_Component',
