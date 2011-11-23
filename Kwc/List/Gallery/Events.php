1e03ba2b7778a0763736fe451a9f480659518318
Niko Sams <ns@vivid-planet.com>
2011-11-23 10:33:25 +0100

    when number of columns changed, the content with changes
    so the images get a new width

---
+++ Kwc/List/Gallery/Events.php
@@ -0,0 +1,14 @@
+<?php
+class Kwc_List_Gallery_Events extends Kwc_Abstract_List_Events
+{
+    protected function _onOwnRowUpdate(Kwf_Component_Data $c, Kwf_Component_Event_Row_Abstract $event)
+    {
+        parent::_onOwnRowUpdate($c, $event);
+        if ($event->isDirty('columns')) {
+            $this->fireEvent(new Kwf_Component_Event_Component_ContentWidthChanged(
+                $this->_class, $c->componentId
+            ));
+        }
+    }
+
+}
