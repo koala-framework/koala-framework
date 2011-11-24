cbf0f5fb3f415d928e1b804a92c8c1467cb4defd
Franz Unger <ufx@vivid-planet.com>
2011-11-24 13:24:46 +0100

    events for LinkTag (must throw UrlChanged when child changes and itself is a page)

---
+++ Kwc/Basic/LinkTag/Events.php
@@ -0,0 +1,30 @@
+<?php
+class Kwc_Basic_LinkTag_Events extends Kwc_Abstract_Events
+{
+    public function getListeners()
+    {
+        $ret = parent::getListeners();
+        foreach (Kwc_Abstract::getChildComponentClasses($this->_class, 'child') as $class) {
+            $ret[] = array(
+                'class' => $class,
+                'event' => 'Kwf_Component_Event_Component_ContentChanged',
+                'callback' => 'onChildContentChanged'
+            );
+        }
+        return $ret;
+    }
+
+    public function onChildContentChanged(Kwf_Component_Event_Component_ContentChanged $event)
+    {
+        $components = Kwf_Component_Data_Root::getInstance()
+            ->getComponentsByDbId($event->dbId);
+        foreach ($components as $component) {
+            $parent = $component->parent;
+            if ($parent->componentClass == $this->_class && $parent->isPage) {
+                $this->fireEvent(
+                    new Kwf_Component_Event_Page_UrlChanged($this->_class, $parent->dbId)
+                );
+            }
+        }
+    }
+}
