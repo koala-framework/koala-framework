f649c3fe17167338f70e084fcef7b99fd135e984
Niko Sams <ns@vivid-planet.com>
2011-11-24 08:56:18 +0100

    don't use all paragraphs used in web, only those that make sense
    as in the other Mail component

--- Kwc/Shop/Cart/Checkout/Payment/Abstract/ShippedMail/Paragraphs/Component.php
+++ Kwc/Shop/Cart/Checkout/Payment/Abstract/ShippedMail/Paragraphs/Component.php
@@ -5,8 +5,11 @@ class Kwc_Shop_Cart_Checkout_Payment_Abstract_ShippedMail_Paragraphs_Component
     public static function getSettings()
     {
         $ret = parent::getSettings();
-        $ret['generators']['paragraphs']['component']['address'] = 'Kwc_Shop_Cart_Checkout_Payment_Abstract_Mail_Paragraphs_Address_Component';
-        $ret['generators']['paragraphs']['component']['products'] = 'Kwc_Shop_Cart_Checkout_Payment_Abstract_Mail_Paragraphs_Products_Component';
+        $ret['generators']['paragraphs']['component'] = array(
+            'textImage' => 'Kwc_TextImage_Component',
+            'address' => 'Kwc_Shop_Cart_Checkout_Payment_Abstract_Mail_Paragraphs_Address_Component',
+            'products' => 'Kwc_Shop_Cart_Checkout_Payment_Abstract_Mail_Paragraphs_Products_Component'
+        );
         return $ret;
     }
 }
