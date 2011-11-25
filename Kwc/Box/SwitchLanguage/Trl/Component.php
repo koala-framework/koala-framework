81599a7d5113b5f8247d671fe48f61df5d02b2b3
Niko Sams <ns@vivid-planet.com>
2011-11-25 09:18:58 +0100

    fix merge error: rename Vpc to Kwc

--- Kwc/Box/SwitchLanguage/Trl/Component.php
+++ Kwc/Box/SwitchLanguage/Trl/Component.php
@@ -1,5 +1,5 @@
 <?php
-class Vpc_Box_SwitchLanguage_Trl_Component extends Vpc_Chained_Trl_MasterAsChild_Component
+class Kwc_Box_SwitchLanguage_Trl_Component extends Kwc_Chained_Trl_MasterAsChild_Component
 {
     public static function getSettings($masterComponentClass)
     {
