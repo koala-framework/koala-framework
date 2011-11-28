d2ebc766cc80d75e06e9ff4dec49aa8b57007bad
Niko Sams <ns@vivid-planet.com>
2011-11-28 11:38:43 +0100

    rerender box cart after a form was submitted sucessfully
    so the link appears when something was put into the cart

---
+++ Kwc/Shop/Box/CartLink/Component.js
@@ -0,0 +1,20 @@
+Kwf.onContentReady(function() {
+    Ext.select('.kwcForm', true).each(function(form) {
+        form.kwcForm.on('submitSuccess', function(r) {
+            Ext.select('.kwcShopBoxCartLink').each(function(el) {
+                var url = '/kwf/util/kwc/render';
+                if (Kwf.Debug.rootFilename) url = Kwf.Debug.rootFilename + url;
+                Ext.Ajax.request({
+                    params: { componentId: el.dom.id },
+                    url: url,
+                    success: function(response, options) {
+                        this.replaceWith({ html: response.responseText });
+                        Kwf.callOnContentReady();
+                    },
+                    scope: el
+                });
+
+            }, this);
+        }, this);
+    });
+});
