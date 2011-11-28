ef372a4b2d7b3e841f36821887cd8847868cf0cd
Niko Sams <ns@vivid-planet.com>
2011-11-28 11:36:50 +0100

    Kwc_Form: create Form class
    - simpler form code as there is an form object
    - inherits Observable so can have events
    - fires submitSuccess event

--- Kwc/Form/Component.js
+++ Kwc/Form/Component.js
@@ -1,116 +1,133 @@
 Kwf.onContentReady(function() {
     Ext.select('.kwcForm', true).each(function(form) {
-        var config = form.parent().down('.config', true);
-        if (!config) return;
-        config = Ext.decode(config.value);
-        if (!config) return;
+        form.kwcForm = new Kwc.Form.Component(form);
+    });
+});
+Ext.ns('Kwc.Form');
+Kwc.Form.Component = function(form)
+{
+    this.addEvents('submitSuccess');
+    this.el = form;
+    var config = form.parent().down('.config', true);
+    if (!config) return;
+    config = Ext.decode(config.value);
+    if (!config) return;
+    this.config = config;
 
-        //TODO there should be a proper Form class and object
-        //but for now this should do...
-        //TODO move into own file, think about the name
-        var fields = [];
-        form.select('.kwfField', true).each(function(fieldEl) {
-            var classes = fieldEl.dom.className.split(' ');
-            var fieldConstructor = false;
-            classes.each(function (c) {
-                if (Kwf.FrontendForm.fields[c]) {
-                    fieldConstructor = Kwf.FrontendForm.fields[c];
-                }
-            }, this);
-            if (fieldConstructor) {
-                var field = new fieldConstructor(fieldEl);
-                fields.push(field);
+    this.fields = [];
+    form.select('.kwfField', true).each(function(fieldEl) {
+        var classes = fieldEl.dom.className.split(' ');
+        var fieldConstructor = false;
+        classes.each(function (c) {
+            if (Kwf.FrontendForm.fields[c]) {
+                fieldConstructor = Kwf.FrontendForm.fields[c];
             }
         }, this);
-        var findField = function(fieldName) {
-            var ret = null;
-            fields.each(function(f) {
-                if (f.getFieldName() == fieldName) {
-                    ret = f;
-                    return true;
-                }
-            }, this);
-            return ret;
-        };
-        if (config.hideForValue) {
-            config.hideForValue.each(function(hideForValue) {
-                var field = findField(hideForValue.field);
-                field.on('change', function(v, f) {
-                    if (v == hideForValue.value) {
-                        findField(hideForValue.hide).hide();
-                    } else {
-                        findField(hideForValue.hide).show();
-                    }
-                }, this);
-
-                //initialize on startup
-                if (field.getValue() == hideForValue.value) {
-                    findField(hideForValue.hide).hide();
-                }
-            }, this);
+        if (fieldConstructor) {
+            var field = new fieldConstructor(fieldEl);
+            this.fields.push(field);
         }
+    }, this);
 
+    if (this.config.hideForValue) {
+        this.config.hideForValue.each(function(hideForValue) {
+            var field = this.findField(hideForValue.field);
+            field.on('change', function(v, f) {
+                if (v == hideForValue.value) {
+                    this.findField(hideForValue.hide).hide();
+                } else {
+                    this.findField(hideForValue.hide).show();
+                }
+            }, this);
 
-        form.child('form button.submit').on('click', function(e) {
-            if (form.dontUseAjaxRequest) return;
-
-            var button = form.child('.button');
-            button.down('.saving').show();
-            button.down('.submit').hide();
-            
-            Ext.Ajax.request({
-                url: config.controllerUrl + '/json-save',
-                ignoreErrors: true,
-                params: {
-                    componentId: config.componentId
-                },
-                form: form.down('form'),
-                failure: function() {
-                    //on failure try a plain old post of the form
-                    form.dontUseAjaxRequest = true; //avoid endless recursion
-                    button.down('.submit').dom.click();
-                },
-                success: function(response, options, r) {
-                    
-                    button.down('.saving').hide();
-                    button.down('.submit').show();
-
-                    // remove and set error classes for fields
-                    fields.each(function(field) {
-                        field.hideError();
-                    }, this);
-                    for(var fieldName in r.errorFields) {
-                        var field = findField(fieldName);
-                        field.showError(r.errorFields[fieldName]);
-                    }
-
-                    // remove and add error messages
-                    var error = form.parent().down('.webFormError');
-                    if (error) error.remove();
-
-                    if (r.errorMessages && r.errorMessages.length) {
-                        var html = '<div class="webStandard kwcFormError webFormError">';
-                        html += '<p class="error">' + r.errorPlaceholder + ':</p>';
-                        html += '<ul>';
-                        for (var i=0; i<r.errorMessages.length; i++) {
-                            html += '<li>' + r.errorMessages[i] + '</li>';
-                        }
-                        html += '</ul>';
-                        html += '</div>';
-                        form.parent().createChild(html, form);
-                    }
-                    
-                    // show success content
-                    if (r.successContent) {
-                        form.parent().createChild(r.successContent);
-                        form.remove();
-                    }
-                },
-                scope: this
-            });
-
-            e.stopEvent();
+            //initialize on startup
+            if (field.getValue() == hideForValue.value) {
+                this.findField(hideForValue.hide).hide();
+            }
         }, this);
+    }
 
-    });
+
+    form.child('form button.submit').on('click', this.onSubmit, this);
+    
+};
+Ext.extend(Kwc.Form.Component, Ext.util.Observable, {
+    findField: function(fieldName) {
+        var ret = null;
+        this.fields.each(function(f) {
+            if (f.getFieldName() == fieldName) {
+                ret = f;
+                return true;
+            }
+        }, this);
+        return ret;
+    },
+    onSubmit: function(e) {
+        if (this.dontUseAjaxRequest) return;
+
+        var button = this.el.child('.button');
+        button.down('.saving').show();
+        button.down('.submit').hide();
+
+        Ext.Ajax.request({
+            url: this.config.controllerUrl + '/json-save',
+            ignoreErrors: true,
+            params: {
+                componentId: this.config.componentId
+            },
+            form: this.el.down('form'),
+            failure: function() {
+                //on failure try a plain old post of the form
+                this.dontUseAjaxRequest = true; //avoid endless recursion
+                button.down('.submit').dom.click();
+            },
+            success: function(response, options, r) {
+
+                button.down('.saving').hide();
+                button.down('.submit').show();
+
+                var hasErrors = false;
+
+                // remove and set error classes for fields
+                this.fields.each(function(field) {
+                    field.hideError();
+                }, this);
+                for(var fieldName in r.errorFields) {
+                    var field = this.findField(fieldName);
+                    field.showError(r.errorFields[fieldName]);
+                    hasErrors= true;
+                }
+
+                // remove and add error messages
+                var error = this.el.parent().down('.webFormError');
+                if (error) error.remove();
+
+                if (r.errorMessages && r.errorMessages.length) {
+                    hasErrors= true;
+                    var html = '<div class="webStandard kwcFormError webFormError">';
+                    html += '<p class="error">' + r.errorPlaceholder + ':</p>';
+                    html += '<ul>';
+                    for (var i=0; i<r.errorMessages.length; i++) {
+                        html += '<li>' + r.errorMessages[i] + '</li>';
+                    }
+                    html += '</ul>';
+                    html += '</div>';
+                    this.el.parent().createChild(html, form);
+                }
+
+                // show success content
+                if (r.successContent) {
+                    this.el.parent().createChild(r.successContent);
+                    this.el.remove();
+                }
+
+                if (!hasErrors) {
+                    this.fireEvent('submitSuccess', this, r);
+                }
+            },
+            scope: this
+        });
+
+        e.stopEvent();
+    }
 });
