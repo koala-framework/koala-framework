ca935e985e36c7c7909e3a8676becd4d9ad77bf0
Franz Unger <ufx@vivid-planet.com>
2011-11-29 08:22:28 +0100

    component class should be known, was implemented the wrong way

--- Kwf/Controller/Dispatcher.php
+++ Kwf/Controller/Dispatcher.php
@@ -30,7 +30,7 @@ class Kwf_Controller_Dispatcher extends Zend_Controller_Dispatcher_Standard
                 $controller = substr($class, $pos + 1) . 'Controller';
                 $class = substr($class, 0, $pos);
             }
-            if (in_array($class, Kwc_Abstract::getComponentClasses())) {
+            if (!in_array($class, Kwc_Abstract::getComponentClasses())) {
                 //unknown component class
                 return false;
             }
