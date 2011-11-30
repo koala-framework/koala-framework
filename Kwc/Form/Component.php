f8224a5377d3a9eca5db51acf78274fe1b8f342c
Niko Sams <ns@vivid-planet.com>
2011-11-30 09:45:54 +0100

    Kwc_Form: use correct format for error message

--- Kwc/Form/Component.php
+++ Kwc/Form/Component.php
@@ -135,7 +135,9 @@ class Kwc_Form_Component extends Kwc_Abstract_Composite_Component
     {
         if (!$e instanceof Kwf_Exception_Abstract) $e = new Kwf_Exception_Other($e);
         $e->logOrThrow();
-        $this->_errors[] = trlKwf('An error occured while processing the form. Please try to submit again later.');
+        $this->_errors[] = array(
+            'message' => trlKwf('An error occured while processing the form. Please try to submit again later.')
+        );
     }
 
     public function getPostData()
