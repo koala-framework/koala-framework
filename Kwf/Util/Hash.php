e86650908cd32e20565c02776a440437d987a2b5
Niko Sams <ns@vivid-planet.com>
2011-11-30 17:41:10 +0100

    cache hash salt in apc
    two stat calls less

--- Kwf/Util/Hash.php
+++ Kwf/Util/Hash.php
@@ -3,10 +3,15 @@ class Kwf_Util_Hash
 {
     public function hash($str)
     {
-        $hashFile = 'cache/hashprivatepart';
-        if (!file_exists($hashFile)) {
-            file_put_contents($hashFile, time().rand(100000, 1000000));
+        $salt = Kwf_Cache_Simple::fetch('hashpp-');
+        if (!$salt) {
+            $hashFile = 'cache/hashprivatepart';
+            if (!file_exists($hashFile)) {
+                file_put_contents($hashFile, time().rand(100000, 1000000));
+            }
+            $salt = file_get_contents($hashFile);
+            Kwf_Cache_Simple::add('hashpp-', $salt);
         }
-        return md5(file_get_contents($hashFile).$str);
+        return md5($salt.$str);
     }
 }
