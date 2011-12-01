d327171e77617ed9ecaa78e7aa0f4a49905c73af
Niko Sams <ns@vivid-planet.com>
2011-12-01 18:30:07 +0100

    fix test, bool was casted to string

--- Kwf/Form/Container/FieldSet/Hidden.php
+++ Kwf/Form/Container/FieldSet/Hidden.php
@@ -5,4 +5,11 @@ class Kwf_Form_Container_FieldSet_Hidden extends Kwf_Form_Field_Hidden
     {
         return 'hidden';
     }
+
+    protected function _getValueFromPostData($postData)
+    {
+        $fieldName = $this->getFieldName();
+        if (!isset($postData[$fieldName])) $postData[$fieldName] = $this->getDefaultValue();
+        return (bool)$postData[$fieldName];
+    }
 }
