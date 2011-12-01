a600f6c7404d587ee3cb30a92985a09739e8481d
Franz Unger <ufx@vivid-planet.com>
2011-12-01 15:53:22 +0100

    text-generator has to use whereChildOfSamePage more strictly
    there can be more child components with the same id on the same page

--- Kwc/Basic/Text/Generator.php
+++ Kwc/Basic/Text/Generator.php
@@ -28,6 +28,12 @@ class Kwc_Basic_Text_Generator extends Kwf_Component_Generator_Table
             }
             $select->whereEquals('nr', substr($id, 2));
         }
+        if ($p = $select->getPart(Kwf_Component_Select::WHERE_CHILD_OF_SAME_PAGE)) {
+            $select->where(new Kwf_Model_Select_Expr_Or(array(
+                new Kwf_Model_Select_Expr_StartsWith('component_id', $p->dbId.'-'),
+                new Kwf_Model_Select_Expr_Equal('component_id', $p->dbId),
+            )));
+        }
         return $select;
     }
 
