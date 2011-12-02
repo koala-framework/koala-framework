377561942c6af5c24ada70e773d53ead14e90aef
Franz Unger <ufx@vivid-planet.com>
2011-12-02 13:20:04 +0100

    Flash and GoogleMap only render if element is visible

--- Kwc/Advanced/GoogleMapView/Component.js
+++ Kwc/Advanced/GoogleMapView/Component.js
@@ -30,34 +30,14 @@ Kwc.Advanced.GoogleMap.renderMap = function(map) {
     return myMap;
 };
 
-Kwf.onContentReady(function() {
-    var maps = Ext.DomQuery.select('div.kwcAdvancedGoogleMapView');
-    Ext.each(maps, function(map) {
-        // wenn in kwfSwitchDisplay (Klappbox)
-        var switchDisplayUp = Ext.get(map).up('div.kwfSwitchDisplay');
-        // wenn in kwfTabs
-        var tabsContentUp = Ext.get(map).up('div.kwfTabsContent');
-
-        // TODO: wenn da noch mehr so ausnahmen kommen, könnte man sich eine generelle
-        // lösung mit einer 'kwfGmapDelayedRender' cssClass o.Ä. überlegen
-        if (switchDisplayUp) {
-            (function(switchDisplayUp, map) {
-                Ext.get(switchDisplayUp).switchDisplayObject.on('opened', function() {
-                    map.gmapObject = Kwc.Advanced.GoogleMap.renderMap(map);
-                });
-            }).defer(1, this, [switchDisplayUp, map]);
-        } else if (tabsContentUp && !tabsContentUp.hasClass('kwfTabsContentActive')) {
-            (function(tabsContentUp, map) {
-                var tabsUp = Ext.get(tabsContentUp).up('div.kwfTabs');
-                Ext.get(tabsUp).tabsObject.on('tabActivate', function(tabs, newIdx, oldIdx) {
-                    if (tabsContentUp.dom === tabs.getContentElByIdx(newIdx)) {
-                        map.gmapObject = Kwc.Advanced.GoogleMap.renderMap(map);
-                    }
-                }, Ext.get(tabsUp).tabsObject);
-            }).defer(1, this, [tabsContentUp, map]);
-        } else {
-            map.gmapObject = Kwc.Advanced.GoogleMap.renderMap(map);
-        }
-    });
+Kwf.onContentReady(function(el, options) {
+    (function(el) {
+        var maps = Ext.DomQuery.select('div.kwcAdvancedGoogleMapView', el);
+        Ext.each(maps, function(map) {
+            if (!map.gmapObject && Ext.get(map).isVisible(true)) {
+                map.gmapObject = Kwc.Advanced.GoogleMap.renderMap(map);
+            }
+        });
+    }).defer(1, this, [el]);
 });
 
