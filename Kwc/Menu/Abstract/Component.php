c2c03621ca1f9baae2dd17c93801532101be40e9
Niko Sams <ns@vivid-planet.com>
2011-12-14 09:03:49 +0100

    remove old deprecated Cache_Meta classes

--- Kwc/Menu/Abstract/Component.php
+++ Kwc/Menu/Abstract/Component.php
@@ -190,23 +190,4 @@ abstract class Kwc_Menu_Abstract_Component extends Kwc_Abstract
         }
         return $ret;
     }
-
-    public static function getStaticCacheMeta($componentClass)
-    {
-        $ret = parent::getStaticCacheMeta($componentClass);
-        foreach (Kwc_Abstract::getComponentClasses() as $class) {
-            foreach (Kwc_Abstract::getSetting($class, 'generators') as $key => $generator) {
-                if (!isset($generator['showInMenu']) || !$generator['showInMenu']) continue;
-                $generator = current(Kwf_Component_Generator_Abstract::getInstances(
-                    $class, array('generator' => $key))
-                );
-                if (!$generator->getGeneratorFlag('page') || !$generator->getGeneratorFlag('table')) continue;
-                $ret[] = new Kwf_Component_Cache_Meta_Static_Model($generator->getModel());
-            }
-        }
-
-        $ret[] = new Kwf_Component_Cache_Meta_Static_Model('Kwf_Component_Model');
-
-        return $ret;
-    }
 }
