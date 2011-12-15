498c1411b1106d0d43875cbee5e0472b79cc2bf8
Niko Sams <ns@vivid-planet.com>
2011-12-15 08:02:31 +0100

    add method to get the id from post data

--- Kwc/Form/Component.php
+++ Kwc/Form/Component.php
@@ -73,6 +73,18 @@ class Kwc_Form_Component extends Kwc_Abstract_Composite_Component
         $this->_processInput($postData);
     }
 
+    protected final function _getIdFromPostData($postData)
+    {
+        if (!empty($postData[$this->getData()->componentId.'-id']) && !empty($postData[$this->getData()->componentId.'-idHash'])) {
+            if ($postData[$this->getData()->componentId.'-idHash'] == Kwf_Util_Hash::hash($postData[$this->getData()->componentId.'-id'])) {
+                //TODO: if hash doesn't match -> exception
+                //TODO: remove component id from field name
+                return $postData[$this->getData()->componentId.'-id'];
+            }
+        }
+        return null;
+    }
+
     protected function _processInput($postData)
     {
         if ($this->_processed) {
@@ -82,11 +94,7 @@ class Kwc_Form_Component extends Kwc_Abstract_Composite_Component
 
         if (!$this->getForm()) return;
 
-        if (!empty($postData[$this->getData()->componentId.'-id']) && !empty($postData[$this->getData()->componentId.'-idHash'])) {
-            if ($postData[$this->getData()->componentId.'-idHash'] == Kwf_Util_Hash::hash($postData[$this->getData()->componentId.'-id'])) {
-                $this->getForm()->setId($postData[$this->getData()->componentId.'-id']);
-            }
-        }
+        $this->getForm()->setId($this->_getIdFromPostData($postData));
 
         $m = $this->getForm()->getModel();
         while ($m instanceof Kwf_Model_Proxy) {
