d6994d93ed8fb6b18f4b538a236b5040a250bc19
Niko Sams <ns@vivid-planet.com>
2011-12-22 11:53:53 +0100

    skip update if table already exists

--- Kwc/Composite/TextImageLink/Update/1.sql
+++ Kwc/Composite/TextImageLink/Update/1.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `kwc_composite_text_image_link` (
+CREATE TABLE IF NOT EXISTS `kwc_composite_text_image_link` (
   `component_id` varchar(255) NOT NULL,
   `title` text NOT NULL,
   `teaser` text NOT NULL,
