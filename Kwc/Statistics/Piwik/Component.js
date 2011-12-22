7506d0fd7005cd8160e73f2777f5208c0198b333
Niko Sams <ns@vivid-planet.com>
2011-12-22 16:35:43 +0100

    avoid JS errors if Piwik is not loaded (blocked) or not set up

--- Kwc/Statistics/Piwik/Component.js
+++ Kwc/Statistics/Piwik/Component.js
@@ -3,12 +3,15 @@ Ext.ns('Kwc.Statistics.Piwik');
 Kwc.Statistics.Piwik.getTracker = function()
 {
     if (!Kwc.Statistics.Piwik.url || !Kwc.Statistics.Piwik.idSite) return null;
+    if (!Piwik) return null;
     return Piwik.getTracker(Kwc.Statistics.Piwik.url, Kwc.Statistics.Piwik.idSite);
 };
 
 Kwf.Statistics.onCount(function(url) {
     var piwikTracker = Kwc.Statistics.Piwik.getTracker();
-    piwikTracker.setCustomUrl(url);
-    piwikTracker.setReferrerUrl(location.href);
-    piwikTracker.trackPageView();
+    if (piwikTracker) {
+        piwikTracker.setCustomUrl(url);
+        piwikTracker.setReferrerUrl(location.href);
+        piwikTracker.trackPageView();
+    }
 });
