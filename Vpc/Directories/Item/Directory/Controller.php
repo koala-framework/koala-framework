59fcecb2ae333d5fa3cf80692fd208bd0ac18951
Niko Sams <ns@vivid-planet.com>
2012-01-19 12:49:20 +0100

    make editDialog width and height setting optional

--- Vpc/Directories/Item/Directory/Controller.php
+++ Vpc/Directories/Item/Directory/Controller.php
@@ -21,8 +21,12 @@ class Vpc_Directories_Item_Directory_Controller extends Vps_Controller_Action_Au
                 'autoForm' => 'Vpc.Directories.Item.Directory.EditFormPanel',
                 'controllerUrl' => $url
             );
-            if ($this->_editDialog['width']) $editDialog['width'] = $this->_editDialog['width'];
-            if ($this->_editDialog['height']) $editDialog['height'] = $this->_editDialog['height'];
+            if (isset($this->_editDialog['width']) && $this->_editDialog['width']) {
+                $editDialog['width'] = $this->_editDialog['width'];
+            }
+            if (isset($this->_editDialog['height']) && $this->_editDialog['height']) {
+                $editDialog['height'] = $this->_editDialog['height'];
+            }
             $this->_editDialog = $editDialog;
         }
     }
