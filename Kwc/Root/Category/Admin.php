b5884f34a03b8839388c8dddba5d36b1a2e6db49
Niko Sams <ns@vivid-planet.com>
2012-01-20 15:14:11 +0100

    support duplicating categories

---
+++ Kwc/Root/Category/Admin.php
@@ -0,0 +1,33 @@
+<?php
+class Kwc_Root_Category_Admin extends Kwc_Abstract_Admin
+{
+    public function getDuplicateProgressSteps($source)
+    {
+        $ret = parent::getDuplicateProgressSteps($source);
+        //pages are not duplicated because they are not returned by 'inherit'=>false
+        //so duplicate them here
+        $s = array(
+            'whereGeneratorFlags' => array('pageGenerator'=>true),
+            'ignoreVisible'=>true
+        );
+        foreach ($source->getChildComponents($s) as $c) {
+            $ret += $c->generator->getDuplicateProgressSteps($c);
+        }
+        return $ret;
+    }
+
+    public function duplicate($source, $target, Zend_ProgressBar $progressBar = null)
+    {
+        parent::duplicate($source, $target, $progressBar);
+
+        //pages are not duplicated because they are not returned by 'inherit'=>false
+        //so duplicate them here
+        $s = array(
+            'whereGeneratorFlags' => array('pageGenerator'=>true),
+            'ignoreVisible'=>true
+        );
+        foreach ($source->getChildComponents($s) as $c) {
+            $c->generator->duplicateChild($c, $target, $progressBar);
+        }
+    }
+}
