b9cbeed95f7e504dc8cb340e80b86d5030b019d1
Niko Sams <ns@vivid-planet.com>
2012-02-02 14:22:50 +0100

    collapse dropdown menu if only one menu entry is shown

--- Kwf/Acl/Resource/MenuDropdown.php
+++ Kwf/Acl/Resource/MenuDropdown.php
@@ -1,4 +1,15 @@
 <?php
 class Kwf_Acl_Resource_MenuDropdown extends Kwf_Acl_Resource_Abstract
 {
+    private $_collapseIfSingleChild = false;
+
+    public function setCollapseIfSingleChild($v)
+    {
+        $this->_collapseIfSingleChild = $v;
+    }
+
+    public function getCollapseIfSingleChild()
+    {
+        return $this->_collapseIfSingleChild;
+    }
 }
