829b406b8fe4a40f94bac404eed2bc397045a3dc
Franz Unger <ufx@vivid-planet.com>
2012-02-06 14:10:25 +0100

    load piwik in try-catch because it might be that the object does not exist

--- Kwc/Statistics/Piwik/Component.js
+++ Kwc/Statistics/Piwik/Component.js
@@ -3,8 +3,11 @@ Ext.ns('Kwc.Statistics.Piwik');
 Kwc.Statistics.Piwik.getTracker = function()
 {
     if (!Kwc.Statistics.Piwik.url || !Kwc.Statistics.Piwik.idSite) return null;
-    if (!Piwik) return null;
-    return Piwik.getTracker(Kwc.Statistics.Piwik.url, Kwc.Statistics.Piwik.idSite);
+    try {
+        return Piwik.getTracker(Kwc.Statistics.Piwik.url, Kwc.Statistics.Piwik.idSite);
+    } catch( err ) {
+        return null;
+    }
 };
 
 Kwf.Statistics.onCount(function(url) {
