cba76067ae4f038bc4d203cae0dd5a44aa29e3a3
Franz Unger <ufx@vivid-planet.com>
2012-02-16 08:47:25 +0100

    new newsletter importToQueue-method with speed improvements

--- Kwc/Newsletter/Subscribe/AbstractRecipientsController.php
+++ Kwc/Newsletter/Subscribe/AbstractRecipientsController.php
@@ -23,29 +23,49 @@ abstract class Kwc_Newsletter_Subscribe_AbstractRecipientsController extends Kwf
 
         $count = $this->_model->countRows($select);
         $progressBar = new Zend_ProgressBar(
-            new Kwf_Util_ProgressBar_Adapter_Cache(
-                $this->_getParam('progressNum')
-            ), 0, $count * 1.1
+            new Kwf_Util_ProgressBar_Adapter_Cache($this->_getParam('progressNum')),
+            0,
+            $count + 1
         );
 
-        $limit = 1000;
+        if ($this->_model->hasColumnMappings('Kwc_Mail_Recipient_Mapping')) {
+            $limit = 1000;
+            $offset = 0;
+            $this->view->before = $component->getComponent()->countQueue();
+            $this->view->rtrExcluded = array();
+            do {
+                $select->limit($limit, $offset);
+                $result = $component->getComponent()->importToQueue($this->_model, $select);
+                $this->view->rtrExcluded = array_merge(
+                    $this->view->rtrExcluded, $result['rtrExcluded']
+                );
+                $select->unsetPart('limit');
+                $offset += $limit;
+                $progressBar->next($limit, $offset." / $count");
+            } while ($count > $offset);
+            $progressBar->next(1, "$count / $count");
+            $this->view->after = $component->getComponent()->countQueue();
+            $this->view->added = $this->view->after - $this->view->before;
+        } else {
+            $limit = 1000;
 
-        $offset = 0;
-        do {
-            $select->limit($limit, $offset);
-            $rowset = $this->_model->getRows($select);
-            $x = 0;
-            foreach ($rowset as $row) {
-                $x++;
-                $component->getComponent()->addToQueue($row);
-                $progressBar->next(1, ($offset+$x)." / $count");
-            }
-            $offset += $limit;
-        } while (count($rowset));
-        unset($rowset);
+            $offset = 0;
+            do {
+                $select->limit($limit, $offset);
+                $rowset = $this->_model->getRows($select);
+                $x = 0;
+                foreach ($rowset as $row) {
+                    $x++;
+                    $component->getComponent()->addToQueue($row);
+                    $progressBar->next(1, ($offset+$x)." / $count");
+                }
+                $offset += $limit;
+            } while (count($rowset));
+            unset($rowset);
+            $progressBar->next(1, trlKwf('RTR-ECG-Check and saving data: please wait...'));
+            $this->view->assign($component->getComponent()->saveQueue());
+        }
 
-        $progressBar->next(1, trlKwf('RTR-ECG-Check and saving data: please wait...'));
-        $this->view->assign($component->getComponent()->saveQueue());
         $progressBar->finish();
     }
 }
