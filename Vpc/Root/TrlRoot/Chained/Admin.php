f6ef30f111febd53b5c4ca72528183c45e05c2b7
Niko Sams <ns@vivid-planet.com>
2012-02-16 11:34:56 +0100

    correctly duplicate translated components when duplicating master across domains
    required for webs that use domains plus trl

--- Vpc/Root/TrlRoot/Chained/Admin.php
+++ Vpc/Root/TrlRoot/Chained/Admin.php
@@ -6,10 +6,56 @@ class Vpc_Root_TrlRoot_Chained_Admin extends Vpc_Abstract_Admin
     {
         $chained = Vps_Component_Data_Root::getInstance()
             ->getComponentsByClass('Vpc_Root_TrlRoot_Chained_Component', array('ignoreVisible'=>true)); //bySameClass wenn fkt nicht mehr static (todo oben erledigt)
+
+        $sourceChained = array();
         foreach ($chained as $c) {
-            $sourceChained = Vpc_Chained_Trl_Component::getChainedByMaster($source, $c, array('ignoreVisible'=>true));
-            $newChained = Vpc_Chained_Trl_Component::getChainedByMaster($new, $c, array('ignoreVisible'=>true));
-            if (!$sourceChained || $source->componentId==$sourceChained->componentId) continue; //wenn sourceChained nicht gefunden handelt es sich zB um ein MasterAsChild - was ignoriert werden muss
+            $subRootAboveTrl = $c; //eg. domain
+            while($subRootAboveTrl = $subRootAboveTrl->parent) {
+                if (Vpc_Abstract::getFlag($subRootAboveTrl->componentClass, 'subroot')) {
+                    break;
+                }
+            }
+            if (!$subRootAboveTrl) {
+                $subRootAboveTrl = Kwf_Component_Data_Root::getInstance();
+            }
+            $d = $source;
+            while($d = $d->parent) {
+                if ($d->componentId == $subRootAboveTrl->componentId) {
+                    $sourceChained[$c->getLanguage()] = $c;
+                }
+            }
+        }
+
+        $targetChained = array();
+        foreach ($chained as $c) {
+            $subRootAboveTrl = $c; //eg. domain
+            while($subRootAboveTrl = $subRootAboveTrl->parent) {
+                if (Vpc_Abstract::getFlag($subRootAboveTrl->componentClass, 'subroot')) {
+                    break;
+                }
+            }
+            if (!$subRootAboveTrl) {
+                $subRootAboveTrl = Kwf_Component_Data_Root::getInstance();
+            }
+            $d = $new;
+            while($d = $d->parent) {
+                if ($d->componentId == $subRootAboveTrl->componentId) {
+                    if (isset($sourceChained[$c->getLanguage()])) { //only if there is a source language
+                        $targetChained[] = array(
+                            'targetChained' => $c,
+                            'sourceChained' => $sourceChained[$c->getLanguage()],
+                        );
+                    }
+                }
+            }
+        }
+
+        foreach ($targetChained as $c) {
+            $sourceChained = Vpc_Chained_Trl_Component::getChainedByMaster($source, $c['sourceChained'], array('ignoreVisible'=>true));
+            $newChained = Vpc_Chained_Trl_Component::getChainedByMaster($new, $c['targetChained'], array('ignoreVisible'=>true));
+            if (!$sourceChained || $source->componentId==$sourceChained->componentId) {
+                continue; //wenn sourceChained nicht gefunden handelt es sich zB um ein MasterAsChild - was ignoriert werden muss
+            }
             if (!$newChained) {
                 throw new Vps_Exception("can't find chained components");
             }
