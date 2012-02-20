e12fb80dfa0e3468ad67447169acade15c5abf5d
Franz Unger <ufx@vivid-planet.com>
2012-02-20 11:36:52 +0100

    newsletter: statistics panel now also shows %
    Conflicts:
    
    	Kwc/Newsletter/Detail/StatisticsController.php

--- Kwc/Newsletter/Detail/StatisticsController.php
+++ Kwc/Newsletter/Detail/StatisticsController.php
@@ -6,11 +6,38 @@ class Kwc_Newsletter_Detail_StatisticsController extends Kwf_Controller_Action_A
         parent::_initColumns();
 
         $this->_columns->add(new Kwf_Grid_Column('link', trlKwf('Link'), 600));
-        $this->_columns->add(new Kwf_Grid_Column('count', trlKwf('Count'), 50));
+        $this->_columns->add(new Kwf_Grid_Column('count', trlKwf('Count'), 50))
+            ->setCssClass('kwf-renderer-decimal');
+        $this->_columns->add(new Kwf_Grid_Column('percent', trlKwf('[%]'), 50));
     }
 
     protected function _fetchData()
     {
+        $db = Kwf_Registry::get('db');
+        $pos = 1;
+
+        $ret = array();
+        $newsletterId = substr(strrchr($this->_getParam('componentId'), '_'), 1);
+        $total = $db->fetchOne("SELECT count_sent FROM kwc_newsletter WHERE id=$newsletterId");
+        if ($total) {
+            $sql = "
+                SELECT count(distinct (concat (recipient_id,recipient_model_shortcut)))
+                FROM kwc_mail_redirect_statistics s, kwc_mail_redirect r
+                WHERE s.redirect_id=r.id AND mail_component_id='" . $this->_getParam('componentId') . "-mail'";
+            $count = $db->fetchOne($sql);
+            $ret[] = array(
+                'pos' => $pos++,
+                'link' => '<b>' . trlKwf('click rate') . '</b> (' . trlKwf('percentage of users which clicked at least one link in newsletter') . ')',
+                'count' => $count,
+                'percent' => number_format(($count / $total)*100, 2) . '%'
+            );
+            $ret[] = array(
+                'pos' => $pos++,
+                'link' => ' ',
+                'count' => '',
+                'percent' => '',
+            );
+        }
         $sql = "
             SELECT r.value, r.type, count(*) c
             FROM kwc_mail_redirect_statistics s, kwc_mail_redirect r
@@ -36,7 +63,8 @@ class Kwc_Newsletter_Detail_StatisticsController extends Kwf_Controller_Action_A
             $row['value'] = $link;
             $ret[] = array(
                 'link' => $link,
-                'count' => $row['c']
+                'count' => $row['c'],
+                'percent' => number_format(($row['c'] / $total)*100, 2) . '%'
             );
         }
         return $ret;
