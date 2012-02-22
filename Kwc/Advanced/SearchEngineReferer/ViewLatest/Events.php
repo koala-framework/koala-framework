2c4028d29c667e99f423bf7e6f8f08dad423e73c
Michael Freudenthaler <mf@vivid-planet.com>
2012-02-22 14:20:23 +0100

    Kwc_Advanced_SearchEngineReferer_Component::processInput saves not dbIb but componentId

--- Kwc/Advanced/SearchEngineReferer/ViewLatest/Events.php
+++ Kwc/Advanced/SearchEngineReferer/ViewLatest/Events.php
@@ -14,11 +14,16 @@ class Kwc_Advanced_SearchEngineReferer_ViewLatest_Events extends Kwc_Abstract_Ev
 
     public function onRowInsert(Kwf_Component_Event_Row_Inserted $event)
     {
-        $this->fireEvent(new Kwf_Component_Event_Component_ContentChanged(
-            $this->_class, $event->row->component_id
-        ));
-        $this->fireEvent(new Kwf_Component_Event_Component_HasContentChanged(
-            $this->_class, $event->row->component_id
-        ));
+        // Kwc_Advanced_SearchEngineReferer_Component::processInput saves not dbIb but componentId
+        $component = Kwf_Component_Data_Root::getInstance()
+            ->getComponentById($event->row->component_id);
+        if ($component) {
+            $this->fireEvent(new Kwf_Component_Event_Component_ContentChanged(
+                $this->_class, $component->dbId . '-view'
+            ));
+            $this->fireEvent(new Kwf_Component_Event_Component_HasContentChanged(
+                $this->_class, $component->dbId . '-view'
+            ));
+        }
     }
 }
