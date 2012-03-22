0cebbe0286c75e56909f414fb80865a88a4e1e6e
Niko Sams <ns@vivid-planet.com>
2012-03-22 15:11:10 +0100

    fix tests

--- tests/Kwf/Component/Fulltext/BasicHtml/Root.php
+++ tests/Kwf/Component/Fulltext/BasicHtml/Root.php
@@ -10,6 +10,12 @@ class Kwf_Component_Fulltext_BasicHtml_Root extends Kwf_Component_NoCategoriesRo
             'html' => 'Kwf_Component_Fulltext_BasicHtml_Html_Component',
             'htmlChild' => 'Kwf_Component_Fulltext_BasicHtml_HtmlChild_Component',
         );
+
+        //required so fulltext events will be processed
+        $ret['generators']['search'] = array(
+            'class' => 'Kwf_Component_Generator_Static',
+            'component' => 'Kwf_Component_Fulltext_BasicHtml_FulltextSearch_Component',
+        );
         return $ret;
     }
 }
