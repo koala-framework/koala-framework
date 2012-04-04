6cded3a7fc3204b1873026ad5b0fd1728c2892e5
Niko Sams <ns@vivid-planet.com>
2012-04-04 17:34:32 +0200

    return the same values as ZendSearch backend does

--- Kwf/Util/Fulltext/Backend/Solr.php
+++ Kwf/Util/Fulltext/Backend/Solr.php
@@ -142,14 +142,16 @@ class Kwf_Util_Fulltext_Backend_Solr extends Kwf_Util_Fulltext_Backend_Abstract
     public function userSearch(Kwf_Component_Data $subroot, $queryString, $offset, $limit, $params = array())
     {
         $ret = array();
-        $params['fl'] = 'componentId,content,title';
+        $params['fl'] = 'componentId,content';
         $res = $this->_getSolrService($subroot)->search($queryString, $offset, $limit, $params);
         foreach ($res->response->docs as $doc) {
-            $ret[] = array(
-                'componentId' => $doc->componentId,
-                'title' => $doc->title,
-                'content' => $doc->content,
-            );
+            $data = Kwf_Component_Data_Root::getInstance()->getComponentById($doc->componentId);
+            if ($data) {
+                $ret[] = array(
+                    'data' => $data,
+                    'content' => $doc->content,
+                );
+            }
         }
         //TODO: error handling
         return array(
