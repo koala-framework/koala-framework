038e1e0add454b067a23a23ab1d0c952d1c81b52
Niko Sams <ns@vivid-planet.com>
2012-05-14 17:45:10 +0200

    add setting to disable history state
    useful for webs that are incompatible with that

--- Kwf_js/Utils/HistoryState.js
+++ Kwf_js/Utils/HistoryState.js
@@ -1,9 +1,11 @@
 Ext.ns('Kwf.Utils');
 Kwf.Utils.HistoryStateCls = function() {
     this.addEvents('popstate');
+    this.disabled = false; //functionality can be disabled, so it behaves like a browser that doesn't support history states
     this.initialState = {};
     this.currentState = {};
     Ext.EventManager.on(window, 'popstate', function(event) {
+        if (this.disabled) return;
         if (event.browserEvent.state) {
             this.currentState = event.browserEvent.state;
         } else {
@@ -14,12 +16,12 @@ Kwf.Utils.HistoryStateCls = function() {
 };
 Ext.extend(Kwf.Utils.HistoryStateCls, Ext.util.Observable, {
     pushState: function(title, href) {
-        if (window.history.pushState) {
+        if (window.history.pushState && !this.disabled) {
             window.history.pushState(this.currentState, title, href);
         }
     },
     replaceState: function(title, href) {
-        if (window.history.replaceState) {
+        if (window.history.replaceState && !this.disabled) {
             window.history.replaceState(this.currentState, title, href);
         }
     }
