5bedaacdeb08c66c423c0dc6e01a0cd81eb98e57
Niko Sams <ns@vivid-planet.com>
2012-05-18 08:32:54 +0200

    fix cmyk->rgb conversion by using icc profiles
    also move the _processCommonImagickSettings before the scaling else it won't work correctly

--- Kwf/Media/Image.php
+++ Kwf/Media/Image.php
@@ -225,6 +225,7 @@ class Kwf_Media_Image
                     $im = new Imagick();
                     $im->readImage($source);
                 }
+                $im = self::_processCommonImagickSettings($im);
                 if (isset($size['rotate']) && $size['rotate']) {
                     $im->rotateImage('#FFF', $size['rotate']);
                 }
@@ -232,7 +233,6 @@ class Kwf_Media_Image
                 $im->cropImage($size['width'], $size['height'], $size['x'], $size['y']);
                 $im->setImagePage(0, 0, 0, 0);
     //             $im->unsharpMaskImage(1, 0.5, 1.0, 0.05);
-                $im = self::_processCommonImagickSettings($im);
                 $ret = $im->getImageBlob();
                 $im->destroy();
             }
@@ -246,11 +246,11 @@ class Kwf_Media_Image
                     $im = new Imagick();
                     $im->readImage($source);
                 }
+                $im = self::_processCommonImagickSettings($im);
                 if (isset($size['rotate']) && $size['rotate']) {
                     $im->rotateImage('#FFF', $size['rotate']);
                 }
                 $im->thumbnailImage($size['width'], $size['height']);
-                $im = self::_processCommonImagickSettings($im);
                 $ret = $im->getImageBlob();
                 $im->destroy();
             } else {
@@ -299,6 +299,21 @@ class Kwf_Media_Image
 
     private function _processCommonImagickSettings($im)
     {
+        if ($im->getImageColorspace() == Imagick::COLORSPACE_CMYK) {
+            $profiles = $im->getImageProfiles('icc', false);
+            $hasIccProfile = in_array('icc', $profiles);
+            // if it doesnt have a CMYK ICC profile, we add one
+            if ($hasIccProfile === false) {
+                $iccCmyk = file_get_contents(Kwf_Config::getValue('libraryPath').'/icc/ISOuncoated.icc');
+                $im->profileImage('icc', $iccCmyk);
+                unset($iccCmyk);
+            }
+            // then we add an RGB profile
+            $iccRgb = file_get_contents(Kwf_Config::getValue('libraryPath').'/icc/sRGB_v4_ICC_preference.icc');
+            $im->profileImage('icc', $iccRgb);
+            unset($iccRgb);
+        }
+
         $im->setImageColorspace(Imagick::COLORSPACE_RGB);
         $im->stripImage();
         $im->setImageCompressionQuality(90);
