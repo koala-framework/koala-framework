12fc25d1caf4d2037267f07fdfa1ac2ac38c67c5
Niko Sams <ns@vivid-planet.com>
2012-05-21 17:43:48 +0200

    add a cache for isValid for components that are behind a login or something
    add this cache inside the isValid method

--- Kwf/Media/Output/Component.php
+++ Kwf/Media/Output/Component.php
@@ -7,32 +7,45 @@ class Kwf_Media_Output_Component
      */
     public static function isValid($id)
     {
-        $ret = Kwf_Media_Output_IsValidInterface::VALID;
-        $c = Kwf_Component_Data_Root::getInstance()->getComponentByDbId($id);
-        if (!$c) {
-            $c = Kwf_Component_Data_Root::getInstance()->getComponentByDbId($id, array('ignoreVisible'=>true));
-            if (!$c) return Kwf_Media_Output_IsValidInterface::INVALID;
-            if (Kwf_Component_Data_Root::getShowInvisible()) {
-                //preview im frontend
-                $ret = Kwf_Media_Output_IsValidInterface::VALID_DONT_CACHE;
-            } else if (Kwf_Registry::get('acl')->isAllowedComponentById($id, $c->componentClass, Kwf_Registry::get('userModel')->getAuthedUser())) {
-                //paragraphs vorschau im backend
-                $ret = Kwf_Media_Output_IsValidInterface::VALID_DONT_CACHE;
-            } else {
-                return Kwf_Media_Output_IsValidInterface::ACCESS_DENIED;
-            }
-        }
-        //$ret can be VALID or VALID_DONT_CACHE at this point
-
-        $plugins = array();
-        while ($c) {
-            foreach (Kwc_Abstract::getSetting($c->componentClass, 'plugins') as $plugin) {
-                if (is_instance_of($plugin, 'Kwf_Component_Plugin_Interface_Login')) {
-                    $plugins[] = $plugin;
+        $writeCache = false;
+        $cacheId = 'media-isvalid-component-'.$id;
+        $plugins = Kwf_Cache_Simple::fetch($cacheId, $success);
+        if ($success) {
+            //success means it's VALID and we successfully fetched the $plugins
+            $ret = Kwf_Media_Output_IsValidInterface::VALID;
+        } else {
+            $ret = Kwf_Media_Output_IsValidInterface::VALID;
+            $c = Kwf_Component_Data_Root::getInstance()->getComponentByDbId($id);
+            if (!$c) {
+                $c = Kwf_Component_Data_Root::getInstance()->getComponentByDbId($id, array('ignoreVisible'=>true));
+                if (!$c) return Kwf_Media_Output_IsValidInterface::INVALID;
+                if (Kwf_Component_Data_Root::getShowInvisible()) {
+                    //preview im frontend
+                    $ret = Kwf_Media_Output_IsValidInterface::VALID_DONT_CACHE;
+                } else if (Kwf_Registry::get('acl')->isAllowedComponentById($id, $c->componentClass, Kwf_Registry::get('userModel')->getAuthedUser())) {
+                    //paragraphs vorschau im backend
+                    $ret = Kwf_Media_Output_IsValidInterface::VALID_DONT_CACHE;
+                } else {
+                    return Kwf_Media_Output_IsValidInterface::ACCESS_DENIED;
                 }
             }
-            if ($c->isPage) break;
-            $c = $c->parent;
+            //$ret can be VALID or VALID_DONT_CACHE at this point
+
+            $plugins = array();
+            while ($c) {
+                foreach (Kwc_Abstract::getSetting($c->componentClass, 'plugins') as $plugin) {
+                    if (is_instance_of($plugin, 'Kwf_Component_Plugin_Interface_Login')) {
+                        $plugins[] = $plugin;
+                    }
+                }
+                if ($c->isPage) break;
+                $c = $c->parent;
+            }
+
+            if ($ret == Kwf_Media_Output_IsValidInterface::VALID) {
+                //only cache VALID, VALID_DONT_CACHE can't be cached
+                $writeCache = true;
+            }
         }
 
         foreach ($plugins as $plugin) {
@@ -43,6 +56,12 @@ class Kwf_Media_Output_Component
                 $ret = Kwf_Media_Output_IsValidInterface::ACCESS_DENIED;
                 break;
             }
+        }
+
+        if ($writeCache && $ret == Kwf_Media_Output_IsValidInterface::VALID_DONT_CACHE) {
+            //only cache VALID_DONT_CACHE, not VALID as it will be cached in Kwf_Media::getOutput
+            //this cache doesn't need to be cleared, because of it's lifetime
+            Kwf_Cache_Simple::add($cacheId, $plugins, 60*60);
         }
         return $ret;
     }
