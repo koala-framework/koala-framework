2e9a2b7172a09510feecefc7a849b51b00174358
Niko Sams <ns@vivid-planet.com>
2012-05-21 17:16:38 +0200

    simplify variable name, add comment

--- Kwf/Media/Output/Component.php
+++ Kwf/Media/Output/Component.php
@@ -7,29 +7,31 @@ class Kwf_Media_Output_Component
      */
     public static function isValid($id)
     {
-        $retValid = Kwf_Media_Output_IsValidInterface::VALID;
+        $ret = Kwf_Media_Output_IsValidInterface::VALID;
         $c = Kwf_Component_Data_Root::getInstance()->getComponentByDbId($id);
         if (!$c) {
             $c = Kwf_Component_Data_Root::getInstance()->getComponentByDbId($id, array('ignoreVisible'=>true));
             if (!$c) return Kwf_Media_Output_IsValidInterface::INVALID;
             if (Kwf_Component_Data_Root::getShowInvisible()) {
                 //preview im frontend
-                $retValid = Kwf_Media_Output_IsValidInterface::VALID_DONT_CACHE;
+                $ret = Kwf_Media_Output_IsValidInterface::VALID_DONT_CACHE;
             } else if (Kwf_Registry::get('acl')->isAllowedComponentById($id, $c->componentClass, Kwf_Registry::get('userModel')->getAuthedUser())) {
                 //paragraphs vorschau im backend
-                $retValid = Kwf_Media_Output_IsValidInterface::VALID_DONT_CACHE;
+                $ret = Kwf_Media_Output_IsValidInterface::VALID_DONT_CACHE;
             } else {
                 return Kwf_Media_Output_IsValidInterface::ACCESS_DENIED;
             }
         }
+
+        //$ret can be VALID or VALID_DONT_CACHE at this point
         while ($c) {
             foreach (Kwc_Abstract::getSetting($c->componentClass, 'plugins') as $plugin) {
                 if (is_instance_of($plugin, 'Kwf_Component_Plugin_Interface_Login')) {
                     $plugin = new $plugin($id);
                     if ($plugin->isLoggedIn()) {
-                        $retValid = Kwf_Media_Output_IsValidInterface::VALID_DONT_CACHE;
+                        $ret = Kwf_Media_Output_IsValidInterface::VALID_DONT_CACHE;
                     } else {
-                        $retValid = Kwf_Media_Output_IsValidInterface::ACCESS_DENIED;
+                        $ret = Kwf_Media_Output_IsValidInterface::ACCESS_DENIED;
                         break 2;
                     }
                 }
@@ -37,6 +39,6 @@ class Kwf_Media_Output_Component
             if ($c->isPage) break;
             $c = $c->parent;
         }
-        return $retValid;
+        return $ret;
     }
 }
