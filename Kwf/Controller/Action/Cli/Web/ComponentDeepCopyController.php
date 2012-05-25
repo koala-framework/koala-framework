049eefb0917a0c149137ec552ce804063ffccb56
Niko Sams <ns@vivid-planet.com>
2012-05-25 08:47:43 +0200

    increase memory limit

--- Kwf/Controller/Action/Cli/Web/ComponentDeepCopyController.php
+++ Kwf/Controller/Action/Cli/Web/ComponentDeepCopyController.php
@@ -23,7 +23,7 @@ class Kwf_Controller_Action_Cli_Web_ComponentDeepCopyController extends Kwf_Cont
 
     public function indexAction()
     {
-        ini_set('memory_limit', '512M');
+        ini_set('memory_limit', '1024M');
         set_time_limit(0);
 
         $source = Kwf_Component_Data_Root::getInstance()->getComponentByDbId($this->_getParam('source'), array('ignoreVisible'=>true));
