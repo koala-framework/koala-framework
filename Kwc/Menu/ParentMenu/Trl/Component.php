40304b487a42a9af0c22a962ff54ea3a4702ed35
Niko Sams <ns@vivid-planet.com>
2012-06-01 12:42:59 +0200

    fix ParentMenu_Trl hasContent

--- Kwc/Menu/ParentMenu/Trl/Component.php
+++ Kwc/Menu/ParentMenu/Trl/Component.php
@@ -1,4 +1,16 @@
 <?php
 class Kwc_Menu_ParentMenu_Trl_Component extends Kwc_Menu_Trl_Component
 {
+    public function hasContent()
+    {
+        $tvars = $this->getTemplateVars();
+        $c = count($tvars['menu']);
+        
+        if (Kwc_Abstract::getSetting(Kwc_Abstract::getSetting($this->getData()->chained->componentClass, 'menuComponentClass'), 'emptyIfSingleEntry')) {
+            if ($c > 1) return true;
+        } else if ($c > 0) {
+            return true;
+        }
+        return false;
+    }
 }
