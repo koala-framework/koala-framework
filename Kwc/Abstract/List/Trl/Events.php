e55ec4d945e6de7a41dcfd2d5b4508a96e8f72eb
Niko Sams <ns@vivid-planet.com>
2012-06-08 08:56:51 +0200

    List Trl: first impl. of cache clearing

--- Kwc/Abstract/List/Trl/Events.php
+++ Kwc/Abstract/List/Trl/Events.php
@@ -9,6 +9,17 @@ class Kwc_Abstract_List_Trl_Events extends Kwc_Abstract_Events
             'event' => 'Kwf_Component_Event_Row_Updated',
             'callback' => 'onRowUpdate'
         );
+        $masterComponentClass = Kwc_Abstract::getSetting($this->_class, 'masterComponentClass');
+        $ret[] = array(
+            'class' => Kwc_Abstract::getSetting($masterComponentClass, 'childModel'),
+            'event' => 'Kwf_Component_Event_Row_Updated',
+            'callback' => 'onMasterRowUpdate'
+        );
+        $ret[] = array(
+            'class' => Kwc_Abstract::getSetting($masterComponentClass, 'childModel'),
+            'event' => 'Kwf_Component_Event_Row_Deleted',
+            'callback' => 'onMasterRowDelete'
+        );
         return $ret;
     }
 
@@ -18,6 +29,40 @@ class Kwc_Abstract_List_Trl_Events extends Kwc_Abstract_Events
             $this->fireEvent(new Kwf_Component_Event_Component_HasContentChanged(
                 $this->_class, $event->row->component_id
             ));
+        }
+    }
+
+    protected function onMasterRowUpdate(Kwf_Component_Event_Row_Abstract $event)
+    {
+        if ($event->isDirty('pos')) {
+
+            $chainedType = 'Trl';
+
+            foreach (Kwf_Component_Data_Root::getInstance()->getComponentsByDbId($event->row->component_id) as $c) {
+                $chained = Kwc_Chained_Abstract_Component::getAllChainedByMaster($c, $chainedType);
+                foreach ($chained as $c) {
+                    $this->fireEvent(
+                        new Kwf_Component_Event_Component_ContentChanged($this->_class, $c->dbId)
+                    );
+                }
+            }
+        }
+    }
+
+    protected function onMasterRowDelete(Kwf_Component_Event_Row_Abstract $event)
+    {
+        $chainedType = 'Trl';
+
+        foreach (Kwf_Component_Data_Root::getInstance()->getComponentsByDbId($event->row->component_id) as $c) {
+            $chained = Kwc_Chained_Abstract_Component::getAllChainedByMaster($c, $chainedType);
+            foreach ($chained as $c) {
+                $this->fireEvent(
+                    new Kwf_Component_Event_Component_ContentChanged($this->_class, $c->dbId)
+                );
+                $this->fireEvent(
+                    new Kwf_Component_Event_Component_HasContentChanged($this->_class, $c->dbId)
+                );
+            }
         }
     }
 }
