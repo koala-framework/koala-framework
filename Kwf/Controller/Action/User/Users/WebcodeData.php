17ee8632d134191318d6c4c3e85dc28a4d6c2aab
Michael Freudenthaler <mf@vivid-planet.com>
2012-06-13 18:03:27 +0200

    don't save webcode, the model does that in createUserRow

--- Kwf/Controller/Action/User/Users/WebcodeData.php
+++ Kwf/Controller/Action/User/Users/WebcodeData.php
@@ -1,7 +1,6 @@
 <?php
 class Kwf_Controller_Action_User_Users_WebcodeData extends Kwf_Data_Abstract
 {
-
     public function load($row)
     {
         if (empty($row->webcode)) {
@@ -13,16 +12,5 @@ class Kwf_Controller_Action_User_Users_WebcodeData extends Kwf_Data_Abstract
 
     public function save(Kwf_Model_Row_Interface $row, $data)
     {
-        $authedRole = Zend_Registry::get('userModel')->getAuthedUserRole();
-        $acl = Zend_Registry::get('acl');
-        if ($acl->getRole($authedRole) instanceof Kwf_Acl_Role_Admin) {
-            if (!$data) {
-                $row->webcode = '';
-            } else if ($data && $row->webcode !== '') {
-                // webcode holen
-                $row->webcode = Zend_Registry::get('userModel')->getWebcode();
-            }
-        }
     }
-
 }
