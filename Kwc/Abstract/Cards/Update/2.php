63ffd451fa55ff7182322c088f5fb1a48a613a06
Niko Sams <ns@vivid-planet.com>
2012-06-20 13:30:50 +0200

    improve performance of update script if thate are a lot of link tags

--- Kwc/Abstract/Cards/Update/2.php
+++ Kwc/Abstract/Cards/Update/2.php
@@ -12,13 +12,52 @@ class Kwc_Abstract_Cards_Update_2 extends Kwf_Update
             }
             Kwf_Registry::get('db')->query('DROP TABLE kwc_basic_linktag');
         }
+        $ids = array();
+        $search = array();
+        $replace = array();
         foreach (Kwf_Registry::get('db')->fetchAll('SELECT component_id, component FROM kwc_basic_cards') as $row) {
-            $config = array(
-                'search' => $row['component_id'] . '-link',
-                'replace' => $row['component_id'] . '-child',
-            );
-            $this->_actions[] = new Kwf_Update_Action_Component_ConvertComponentIds($config);
+            $ids[] = $row['component_id'];
+            $search[] = $row['component_id'] . '-link';
+            $replace[] = $row['component_id'] . '-child';
+
         }
-        parent::update();
+
+        $db = Zend_Registry::get('db');
+        foreach ($db->query("SHOW TABLES")->fetchAll() as $table) {
+            $table = array_values($table);
+            $table = $table[0];
+            $hasComponentId = false;
+            $column = 'component_id';
+            foreach ($db->query("SHOW FIELDS FROM $table")->fetchAll() as $field) {
+                if ($table == 'kwf_pages') {
+                    $column = 'parent_id';
+                    $hasComponentId = true;
+                } else if ($field['Field'] == 'component_id') {
+                    $hasComponentId = true;
+                }
+            }
+            if ($hasComponentId) {
+                echo "\n\n".$table.":\n";
+                $sql = "SELECT $column FROM $table WHERE 0 ";
+                foreach ($ids as $id) {
+                    $sql .= " OR $column LIKE ".$db->quote(str_replace('_', '\_', $id.'-link').'%');
+                }
+                file_put_contents('up.sql', "\n\n\n".$sql, FILE_APPEND);
+                foreach ($db->query($sql)->fetchAll() AS $row) {
+                    $newId = str_replace($search, $replace, $row[$column]);
+                    $sql = "UPDATE $table SET $column=".$db->quote($newId)." WHERE $column=".$db->quote($row[$column]);
+                    echo $sql."\n";
+                    $db->query($sql);
+                }
+            }
+        }
+        echo "\n\nkwc_basic_text:\n";
+        foreach ($search as $k=>$s) {
+            $r = $replace[$k];
+            $db->query("UPDATE kwc_basic_text SET content =
+                    REPLACE(content, 'href=\"$s-', 'href=\"$r-') WHERE content LIKE '$s'");
+            $db->query("UPDATE kwc_basic_text SET content =
+                    REPLACE(content, 'href=\n  \"$s-', 'href=\n  \"$r-') WHERE content LIKE '$s'");
+        }
     }
 }
