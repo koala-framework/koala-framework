f004ed31a98b15269cf5a94b2f818377d75cc369
Niko Sams <ns@vivid-planet.com>
2012-06-25 16:47:59 +0200

    fire correct event

--- Kwf/Component/Generator/Page/Events/Static.php
+++ Kwf/Component/Generator/Page/Events/Static.php
@@ -4,7 +4,7 @@ class Kwf_Component_Generator_Page_Events_Static extends Kwf_Component_Generator
     protected function _fireComponentEvent($eventType, Kwf_Component_Event_Component_AbstractFlag $ev)
     {
         parent::_fireComponentEvent($eventType, $ev);
-        $cls = 'Kwf_Component_Event_Component_'.$eventType;
+        $cls = 'Kwf_Component_Event_Page_'.$eventType;
         $g = $this->_getGenerator();
         foreach ($ev->component->getChildComponents(array('generator'=>$g->getGeneratorKey())) as $c) {
             $this->fireEvent(new $cls($c->componentClass, $c, $ev->flag));
