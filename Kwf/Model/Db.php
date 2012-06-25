c2171c948821ddefb603f4c32fc3a0559646d4c1
Niko Sams <ns@vivid-planet.com>
2012-06-25 11:38:42 +0200

    correctly use tableNameAlias for siblings and Expr_Parent

--- Kwf/Model/Db.php
+++ Kwf/Model/Db.php
@@ -205,15 +205,14 @@ class Kwf_Model_Db extends Kwf_Model_Abstract
                         return $m->getTableName().'.'.$field;
                     }
                     if (is_string($field)) {
-                        $ret = $m->_formatFieldInternal($field, $dbSelect);
+                        $ret = $m->_formatFieldInternal($field, $dbSelect, $tableNameAlias);
                     } else {
-                        $ret = $this->_createDbSelectExpression($field, $dbSelect, $m);
+                        $ret = $this->_createDbSelectExpression($field, $dbSelect, $m, $tableNameAlias);
                     }
                     if ($ret) return $ret;
                 }
             }
         }
-
         return $this->_formatFieldExpr($field, $dbSelect, $tableNameAlias);
     }
 
@@ -597,7 +596,7 @@ class Kwf_Model_Db extends Kwf_Model_Abstract
             if ($ref === Kwf_Model_RowsSubModel_Interface::SUBMODEL_PARENT) {
                 $ref = $dbDepOf->getReferenceByModelClass($depOf->getParentModel(), null);
             }
-            $col1 = $dbDepOf->_formatField($ref['column'], null /* select fehlt - welches sollte das sein? */ );
+            $col1 = $dbDepOf->_formatField($ref['column'], null /* select fehlt - welches sollte das sein? */ , $tableNameAlias);
             $col2 = $dbRefM->transformColumnName($dbRefM->getPrimaryKey());
 
             $refSelect->where("$refTableName.$col2=$col1");
