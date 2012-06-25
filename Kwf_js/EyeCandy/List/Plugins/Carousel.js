c584974c922a13de32de94ccf6c2869c5a2c830d
Niko Sams <ns@vivid-planet.com>
2012-06-25 09:50:44 +0200

    improve carousel
    - avoid flickering
    - make it possible to disable fading
    - individual fade config (optional)

--- Kwf_js/EyeCandy/List/Plugins/Carousel.js
+++ Kwf_js/EyeCandy/List/Plugins/Carousel.js
@@ -1,13 +1,21 @@
 Kwf.EyeCandy.List.Plugins.Carousel = Ext.extend(Kwf.EyeCandy.List.Plugins.Abstract, {
     numberShown: 5,
-    //animationConfig: { concurrent: true, duration: 0.25 },
+    //animationConfig: { duration: 0.25 },
+    //fadeAnimationConfig: { duration: 0.25 }, //optional, by default animationConfig will be used
+    useFade: true, //if elements will be faded in/out in addition to the moving
     moveElementSelector: false, //if false list.el, else list.child(moveElementSelector)
 
     _moveActive: false,
     init: function() {
         Ext.applyIf(this, {
-            animationConfig: { concurrent: true, duration: 0.25 }
+            animationConfig: { duration: 0.25 }
         });
+        if (!this.fadeAnimationConfig) {
+            this.fadeAnimationConfig = this.animationConfig;
+        }
+        this.animationConfig.concurrent = true;
+        this.fadeAnimationConfig.concurrent = true;
+
         this.moveElement = this.list.el;
         if (this.moveElementSelector) {
             this.moveElement = this.moveElement.child(this.moveElementSelector);
@@ -45,18 +53,33 @@ Kwf.EyeCandy.List.Plugins.Carousel = Ext.extend(Kwf.EyeCandy.List.Plugins.Abstra
         this._moveActive = true;
 
         // fade out first one
-        var cfg = Ext.applyIf({
-            useDisplay: true
-        }, this.animationConfig);
-        this.list.getItem(0).el.fadeOut(cfg);
+        if (this.useFade) {
+            var cfg = Ext.applyIf({
+                endOpacity: 0.01
+            }, this.fadeAnimationConfig);
+            this.list.getItem(0).el.fadeOut(cfg);
+        }
 
         // fade in new one
-        this.list.getItem(this.numberShown).el.fadeIn(cfg);
+        if (this.useFade) {
+            var cfg = Ext.applyIf({
+                endOpacity: 0.99
+            }, this.fadeAnimationConfig);
+            this.list.getItem(this.numberShown).el.fadeIn(cfg);
+        } else {
+            this.list.getItem(this.numberShown).el.show();
+        }
 
         var firstElWidth = this.list.getItem(0).getWidthIncludingMargin();
 
         var cfg = Ext.applyIf({
             callback: function() {
+
+                if (this.useFade) {
+                    //fully hide & show (as we have endOpacity)
+                    this.list.getItem(0).el.hide();
+                    this.list.getItem(this.numberShown).el.show();
+                }
 
                 // push moved left element to back
                 this.list.getItem(0).el.insertAfter(this.list.getLastItem().el); //move element
@@ -97,19 +120,36 @@ Kwf.EyeCandy.List.Plugins.Carousel = Ext.extend(Kwf.EyeCandy.List.Plugins.Abstra
         this.moveElement.move('left', this.list.getItem(0).getWidthIncludingMargin(), false);
 
         // fade in first
-        var cfg = Ext.applyIf({
-            useDisplay: true
-        }, this.animationConfig);
-        this.list.getItem(0).el.fadeIn(cfg);
+        if (this.useFade) {
+            var cfg = Ext.applyIf({
+                useDisplay: false,
+                endOpacity: 0.99
+            }, this.fadeAnimationConfig);
+            this.list.getItem(0).el.fadeIn(cfg);
+        } else {
+            this.list.getItem(0).el.show();
+        }
 
         // fade out last
-        this.list.getItem(this.numberShown).el.fadeOut(cfg);
+        if (this.useFade) {
+            var cfg = Ext.applyIf({
+                useDisplay: false,
+                endOpacity: 0.01
+            }, this.fadeAnimationConfig);
+            this.list.getItem(this.numberShown).el.fadeOut(cfg);
+        }
+
         var cfg = Ext.applyIf({
             callback: function() {
                 this._moveActive = false;
+                if (this.useFade) {
+                    //fully hide & show (as we have endOpacity)
+                    this.list.getItem(this.numberShown).el.hide();
+                    this.list.getItem(0).el.show();
+                }
             },
             scope: this
-        });
+        }, this.animationConfig);
         this.moveElement.move('right', this.list.getItem(0).getWidthIncludingMargin(), cfg);
     }
 
