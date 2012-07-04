be9b6ed666816588a511128d23790e5c0b866c7e
Niko Sams <ns@vivid-planet.com>
2012-07-04 09:04:42 +0200

    translate validation messages

--- Vps/Validate/Digits.php
+++ Vps/Validate/Digits.php
@@ -3,9 +3,10 @@ class Vps_Validate_Digits extends Zend_Validate_Digits
 {
     public function __construct()
     {
-        $this->_allowEmpty = $allowEmpty;
+        $this->_messageTemplates[self::NOT_DIGITS] = trlVps("'%value%' contains characters which are not digits; but only digits are allowed");
+        $this->_messageTemplates[self::STRING_EMPTY] = trlVps("'%value%' is an empty string");
+        $this->_messageTemplates[self::INVALID] = trlVps("Invalid type given, value should be string, integer or float");
     }
-
     public function isValid($value)
     {
         $ret = parent::isValid($value);
