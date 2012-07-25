5c3aad98a2852a406e71276139c01fe0c73c3424
Franz Unger <ufx@vivid-planet.com>
2012-07-25 14:08:18 +0200

    comment for trl-menu change commit

--- Kwc/Menu/OtherCategory/Trl/Component.php
+++ Kwc/Menu/OtherCategory/Trl/Component.php
@@ -1,4 +1,9 @@
 <?php
+/*
+ * Doesn't extends Kwc_Menu_Abstract_Trl_Component like all other trl-menus.
+ * Reason is that target menu can extend expandable-menu or menu but we must extend either one.
+ * If this breaks something rethink and test it.
+ */
 class Kwc_Menu_OtherCategory_Trl_Component extends Kwc_Menu_OtherCategory_Component
 {
     protected function _getMenuComponentClass()
