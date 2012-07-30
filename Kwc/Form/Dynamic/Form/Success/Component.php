c3a59580ee9b078304f20b25a836b128b3e183e6
Franz Unger <ufx@vivid-planet.com>
2012-07-30 16:41:12 +0200

    default text for success component in dynamic form

--- Kwc/Form/Dynamic/Form/Success/Component.php
+++ Kwc/Form/Dynamic/Form/Success/Component.php
@@ -1,11 +1,12 @@
 <?php
-class Kwc_Form_Dynamic_Form_Success_Component extends Kwc_TextImage_Component
+class Kwc_Form_Dynamic_Form_Success_Component extends Kwc_Basic_Text_Component
 {
     public static function getSettings()
     {
         $ret = parent::getSettings();
         $ret['cssClass'] = 'webStandard webSuccess';
         $ret['componentName'] = trlKwfStatic('Text on successful submit');
+        $ret['defaultText'] = trlKwf('The form has been submitted successfully.');
         return $ret;
     }
 
