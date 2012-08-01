161e481ff7057084fa1419cebd11ae96677fc71a
Michael Freudenthaler <mf@vivid-planet.com>
2012-08-01 08:35:36 +0200

    fixed a typingmistake

--- Kwf_js/Utils/Slider.js
+++ Kwf_js/Utils/Slider.js
@@ -1,9 +1,9 @@
 //override to allow snapping to maxValue
 //properly fixed in Ext 4
-Ext.Slider.prototpye.originalDoSnap = Ext.Slider.prototpye.doSnap;
-Ext.Slider.prototpye.doSnap = function(value) {
+Ext.Slider.prototype.originalDoSnap = Ext.Slider.prototype.doSnap;
+Ext.Slider.prototype.doSnap = function(value) {
     if (this.maxValue - value < this.increment / 2) {
         return this.maxValue;
     }
-    return Ext.Slider.prototpye.originalDoSnap.call(this, value);
+    return Ext.Slider.prototype.originalDoSnap.call(this, value);
 };
