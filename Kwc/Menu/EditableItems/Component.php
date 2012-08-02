76145f0385725122f0341f69af6057b734c7b730
Niko Sams <ns@vivid-planet.com>
2012-08-02 09:03:22 +0200

    add documentation

--- Kwc/Menu/EditableItems/Component.php
+++ Kwc/Menu/EditableItems/Component.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * This component can be added as child to a Kwc_Menu_Abstract_Component and provides
+ * an instance of 'child' component per menu item.
+ *
+ * Usually the menu template has to be overwritten and the associated child for the menu item
+ * is found in 'editable' index.
+ *
+ * Example (from Menu Component.ptl):
+ *     <?=$this->componentLink($m['data'], $this->component($m['editable']->getChildComponent('-image')).$m['text']);?>
+ *
+ */
 class Kwc_Menu_EditableItems_Component extends Kwc_Abstract
 {
     public static function getSettings()
