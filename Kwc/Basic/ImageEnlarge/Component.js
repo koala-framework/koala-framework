fbf83b048dd3e222f175c62ee2214a265ffc70ba
Mario Kemetinger <mk@vivid-planet.com>
2012-08-09 09:53:56 +0200

    Styling Galleryhover

--- Kwc/Basic/ImageEnlarge/Component.js
+++ Kwc/Basic/ImageEnlarge/Component.js
@@ -1,14 +1,5 @@
-Kwf.onContentReady(function() {
-    Ext.query('.kwcBasicImageEnlarge').each(function(el) {
-        if (!Ext.fly(el).hasClass('showHoverIcon')) return;
-        var el = Ext.fly(el).child('a');
-        if (el && !Ext.fly(el).child('.webZoom')) {
-            if (el.getWidth() > 100 && el.getHeight() > 100) {
-                Ext.DomHelper.append(el,
-                        { tag: 'span', cls: 'outerHoverIcon', html: '<span class="innerHoverIcon"></span>'}
-                );
-            }
-        }
-    });
+Kwf.onElementReady('.kwcBasicImageEnlarge.showHoverIcon > a', function(el) {
+    if (el.getWidth() > 100 && el.getHeight() > 100) {
+        el.createChild({ tag: 'span', cls: 'outerHoverIcon', html: '<span class="innerHoverIcon"></span>'});
+    }
 });
-
