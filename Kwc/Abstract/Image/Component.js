e2ca279e14414b2e686d9f2c3a86383d2d9a01c8
Niko Sams <ns@vivid-planet.com>
2012-08-10 11:15:59 +0200

    move if around onElementReady so it doesn't need to be executed for non-dpr dispays

--- Kwc/Abstract/Image/Component.js
+++ Kwc/Abstract/Image/Component.js
@@ -1,9 +1,9 @@
-Kwf.onElementReady('.kwcAbstractImage', function(el) {
-    if (window.devicePixelRatio && window.devicePixelRatio > 1) {
+if (window.devicePixelRatio && window.devicePixelRatio > 1) {
+    Kwf.onElementReady('.kwcAbstractImage', function(el) {
         var dpr2src = el.dom.getAttribute('data-dpr2src');
         if (dpr2src) {
             var img = el.child('img');
             img.dom.src = dpr2src;
         }
-    }
-});
+    });
+}
