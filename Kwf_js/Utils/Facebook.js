d2384e5a4dac37a0ae958ee826fd4978e8eecce6
Michael Freudenthaler <mf@vivid-planet.com>
2012-09-27 17:07:37 +0200

    use of german facebook sdk version

--- Kwf_js/Utils/Facebook.js
+++ Kwf_js/Utils/Facebook.js
@@ -14,6 +14,6 @@ window.fbAsyncInit = function() {
 (function(d){
     var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
     js = d.createElement('script'); js.id = id; js.async = true;
-    js.src = "//connect.facebook.net/en_US/all.js";
+    js.src = "//connect.facebook.net/de_DE/all.js";
     d.getElementsByTagName('head')[0].appendChild(js);
 }(document));
