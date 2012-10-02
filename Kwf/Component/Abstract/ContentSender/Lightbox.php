c74ed3d99abd512c7b423741ecccdd92e391d10d
Niko Sams <ns@vivid-planet.com>
2012-10-02 13:15:13 +0200

    fix lightbox ContentSender searching for home

--- Kwf/Component/Abstract/ContentSender/Lightbox.php
+++ Kwf/Component/Abstract/ContentSender/Lightbox.php
@@ -20,7 +20,7 @@ class Kwf_Component_Abstract_ContentSender_Lightbox extends Kwf_Component_Abstra
             $parent = $parent->parent;
         }
         if (!$parent) {
-            $parent = Kwf_Component_Data_Root::getInstance()->getChildComponent(array('home' => true)); //TODO support home under categories, domains and language
+            $parent = Kwf_Component_Data_Root::getInstance()->getChildPage(array('home' => true), array());
         }
 
         //TODO: the proper solution would be to restructure List_Switch so that the page is our parent
