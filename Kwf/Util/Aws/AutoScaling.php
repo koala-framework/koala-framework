6f51f800ca873f7d916726572a5c2e545111163b
Niko Sams <ns@vivid-planet.com>
2012-10-03 13:47:56 +0200

    add possibility to refresh autoScaling instances dns names
    can be called when a instance gets added or removed

--- Kwf/Util/Aws/AutoScaling.php
+++ Kwf/Util/Aws/AutoScaling.php
@@ -10,38 +10,71 @@ class Kwf_Util_Aws_AutoScaling extends AmazonAS
         parent::__construct($options);
     }
 
+    private static function _getInstanceDnsNamesCache()
+    {
+        static $i;
+        if (!$i) {
+            $i = Kwf_Cache::factory(
+                'Core',
+                'File',
+                array('lifetime' => null, 'automatic_serialization' => true),
+                array('cache_dir' => 'cache/aws')
+            );
+        }
+        return $i;
+    }
+
+    private static function _getInstanceDnsNamesCacheId($autoScalingGroup)
+    {
+        return 'aws_as_inst_dns_'.str_replace('-', '_', $autoScalingGroup);
+    }
+
+    //uncached, use getInstanceDnsNamesCached to use cache
     public static function getInstanceDnsNames($autoScalingGroup)
     {
-        $cacheId = 'aws-as-inst-dns-'.$autoScalingGroup;
-        $servers = Kwf_Cache_Simple::fetch($cacheId);
-        if ($servers === false) {
-            $ac = new Kwf_Util_Aws_AutoScaling();
-            $r = $ac->describe_auto_scaling_groups(array(
-                'AutoScalingGroupNames' => $autoScalingGroup,
-            ));
-            if (!$r->isOK()) {
-                throw new Kwf_Exception($r->body->asXml());
-            }
-            $instanceIds = array();
-            foreach ($r->body->DescribeAutoScalingGroupsResult->AutoScalingGroups->member->Instances->member as $member) {
-                $instanceIds[] = (string)$member->InstanceId;
-            }
+        $ac = new Kwf_Util_Aws_AutoScaling();
+        $r = $ac->describe_auto_scaling_groups(array(
+            'AutoScalingGroupNames' => $autoScalingGroup,
+        ));
+        if (!$r->isOK()) {
+            throw new Kwf_Exception($r->body->asXml());
+        }
+        $instanceIds = array();
+        foreach ($r->body->DescribeAutoScalingGroupsResult->AutoScalingGroups->member->Instances->member as $member) {
+            $instanceIds[] = (string)$member->InstanceId;
+        }
 
-            $ec2 = new Kwf_Util_Aws_Ec2();
-            $r = $ec2->describe_instances(array(
-                'InstanceId' => $instanceIds
-            ));
-            if (!$r->isOK()) {
-                throw new Kwf_Exception($r->body->asXml());
+        $ec2 = new Kwf_Util_Aws_Ec2();
+        $r = $ec2->describe_instances(array(
+            'InstanceId' => $instanceIds
+        ));
+        if (!$r->isOK()) {
+            throw new Kwf_Exception($r->body->asXml());
+        }
+        $servers = array();
+        foreach ($r->body->reservationSet->item as $reservaionSet) {
+            foreach ($reservaionSet->instancesSet->item as $item) {
+                $dnsName = (string)$item->dnsName;
+                if ($dnsName) $servers[] = $dnsName;
             }
-            $servers = array();
-            foreach ($r->body->reservationSet->item as $reservaionSet) {
-                foreach ($reservaionSet->instancesSet->item as $item) {
-                    $dnsName = (string)$item->dnsName;
-                    if ($dnsName) $servers[] = $dnsName;
-                }
-            }
-            Kwf_Cache_Simple::add($cacheId, $servers, 60*10);
+        }
+        return $servers;
+    }
+
+    public static function refreshInstanceDnsNamesCache($autoScalingGroup)
+    {
+        $servers = self::getInstanceDnsNames($autoScalingGroup);
+        self::_getInstanceDnsNamesCache()->save($servers, self::_getInstanceDnsNamesCacheId($autoScalingGroup));
+        return $servers;
+    }
+
+    //if used you need to refresh this cache yourself
+    public static function getInstanceDnsNamesCached($autoScalingGroup)
+    {
+        $cacheId = self::_getInstanceDnsNamesCacheId($autoScalingGroup);
+        $servers = self::_getInstanceDnsNamesCache()->load($cacheId);
+        if ($servers === false) {
+            $servers = self::refreshInstanceDnsNamesCache($autoScalingGroup);
         }
         return $servers;
     }
