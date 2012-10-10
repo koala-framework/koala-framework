6e9e28f2d7852bd7621d2046eadb5448a981d80f
Niko Sams <ns@vivid-planet.com>
2012-10-10 10:55:58 +0200

    call correct method

--- Kwf/Util/Aws/ElastiCache/CacheClusterEndpoints.php
+++ Kwf/Util/Aws/ElastiCache/CacheClusterEndpoints.php
@@ -59,7 +59,7 @@ class Kwf_Util_Aws_ElastiCache_CacheClusterEndpoints
         $cacheId = self::_getCacheId($cacheClusterId);
         $servers = self::_getCache()->load($cacheId);
         if ($servers === false) {
-            $servers = self::refresh($cacheClusterId);
+            $servers = self::refreshCache($cacheClusterId);
         }
         return $servers;
     }
