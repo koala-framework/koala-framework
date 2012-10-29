36ea9518a440a7e83ef87ba31cb43457f581d32c
Niko Sams <ns@vivid-planet.com>
2012-10-29 16:50:10 +0100

    add possibility to not include pages from page geneartor in CopyTarget

--- Kwc/Chained/CopyTarget/Component.php
+++ Kwc/Chained/CopyTarget/Component.php
@@ -1,17 +1,20 @@
 <?php
 abstract class Kwc_Chained_CopyTarget_Component extends Kwc_Abstract
 {
-    public static function getSettings()
+    public static function getSettings($includePageGenerator = true)
     {
         $ret = parent::getSettings();
         $ret['generators']['target'] = array(
             'class' => 'Kwc_Chained_CopyTarget_TargetGenerator',
             'component' => null
         );
-        $pageGenerator = Kwc_Chained_Cc_Component::createChainedGenerator('Kwc_Root_Category_Component', 'page');
-        if ($pageGenerator) {
-            $ret['generators']['page'] = $pageGenerator;
-            $ret['generators']['page']['class'] = 'Kwc_Chained_CopyTarget_PagesGenerator';
+
+        if ($includePageGenerator) {
+            $pageGenerator = Kwc_Chained_Cc_Component::createChainedGenerator('Kwc_Root_Category_Component', 'page');
+            if ($pageGenerator) {
+                $ret['generators']['page'] = $pageGenerator;
+                $ret['generators']['page']['class'] = 'Kwc_Chained_CopyTarget_PagesGenerator';
+            }
         }
         return $ret;
     }
