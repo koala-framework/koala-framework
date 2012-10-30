0c8b2597ef6f32eeee3aee652d887660fa60f3e5
Niko Sams <ns@vivid-planet.com>
2012-10-30 14:06:58 +0100

    adapt HtmlParser to changes

--- tests/Kwc/Mail/HtmlParser/Test.php
+++ tests/Kwc/Mail/HtmlParser/Test.php
@@ -46,15 +46,15 @@ class Kwc_Mail_HtmlParser_Test extends Kwf_Test_TestCase
         $p = new Kwc_Mail_HtmlParser($styles);
         $html = '<p>Lorem Ipsum</p>';
         $html = $p->parse($html);
-        $this->_assertHtmlEquals('<p><font face="Verdana" size="3">Lorem Ipsum</font></p>', $html);
+        $this->_assertHtmlEquals('<p style="font-size: 18px; "><font face="Verdana">Lorem Ipsum</font></p>', $html);
 
         $html = '<p class="red">Lorem Ipsum</p>';
         $html = $p->parse($html);
-        $this->_assertHtmlEquals('<p class="red"><font face="Verdana" size="3" color="red">Lorem Ipsum</font></p>', $html);
+        $this->_assertHtmlEquals('<p class="red" style="font-size: 18px; "><font face="Verdana" color="red">Lorem Ipsum</font></p>', $html);
 
         $html = '<h1>Lorem Ipsum</h1>';
         $html = $p->parse($html);
-        $this->_assertHtmlEquals('<h1><font face="Verdana" size="4"><b>Lorem Ipsum</b></font></h1>', $html);
+        $this->_assertHtmlEquals('<h1 style="font-size: 24px; "><font face="Verdana"><b>Lorem Ipsum</b></font></h1>', $html);
 
         $html = '<div>Lorem</div><div>Ipsum</div>';
         $html = $p->parse($html);
@@ -62,11 +62,11 @@ class Kwc_Mail_HtmlParser_Test extends Kwf_Test_TestCase
 
         $html = '<strong foo="bar">Lorem Ipsum</strong>';
         $html = $p->parse($html);
-        $this->_assertHtmlEquals('<p><font face="Verdana" size="3"><b>Lorem Ipsum</b></font></p>', $html);
+        $this->_assertHtmlEquals('<p style="font-size: 18px; "><font face="Verdana"><b>Lorem Ipsum</b></font></p>', $html);
 
         $html = 'Lorem<br />Ipsum';
         $html = $p->parse($html);
-        $this->_assertHtmlEquals('<p><font face="Verdana" size="3">Lorem<br />Ipsum</font></p>', $html);
+        $this->_assertHtmlEquals('<p style="font-size: 18px; "><font face="Verdana">Lorem<br />Ipsum</font></p>', $html);
 
         // wenn man ein nicht geschlossenes <br> rein gibt, macht das xml einen fehler,
         // geht aber normal weiter, deshalb dieser teil auskommentiert. siehe auch den kommentar beim HtmlParser
@@ -87,7 +87,7 @@ class Kwc_Mail_HtmlParser_Test extends Kwf_Test_TestCase
             )
         );
         $html = '<table><tr><td>Guten Tag, &NBSP; <strong>Frau Staterau!</strong></td><td>&nbsp;</td><td>Testtext</td></tr></table>';
-        $expected= '<table><tr><td><font face="Verdana" size="2">Guten Tag, &NBSP; <strong>Frau Staterau!</strong></font></td><td><font face="Verdana" size="2">&nbsp;</font></td><td><font face="Verdana" size="2">Testtext</font></td></tr></table>';
+        $expected= '<table><tr><td style="font-size: 12px; "><font face="Verdana">Guten Tag, &NBSP; <strong>Frau Staterau!</strong></font></td><td style="font-size: 12px; "><font face="Verdana">&nbsp;</font></td><td style="font-size: 12px; "><font face="Verdana">Testtext</font></td></tr></table>';
         $p = new Kwc_Mail_HtmlParser($styles);
         $html = $p->parse($html);
         $this->_assertHtmlEquals($expected, $html);
@@ -112,8 +112,8 @@ class Kwc_Mail_HtmlParser_Test extends Kwf_Test_TestCase
         );
         $html  = '<table><tr><td><p>Blu bla Bli</p></td></tr></table>';
         $html .= '<table class="foo"><tr><td><p>Blu bla Bli</p></td></tr></table>';
-        $expected  = '<table><tr><td><p><font size="3">Blu bla Bli</font></p></td></tr></table>';
-        $expected .= '<table class="foo"><tr><td><p><font size="2">Blu bla Bli</font></p></td></tr></table>';
+        $expected  = '<table><tr><td><p style="font-size: 18px; ">Blu bla Bli</p></td></tr></table>';
+        $expected .= '<table class="foo"><tr><td><p style="font-size: 12px; ">Blu bla Bli</p></td></tr></table>';
         $p = new Kwc_Mail_HtmlParser($styles);
         $html = $p->parse($html);
         $this->_assertHtmlEquals($expected, $html);
