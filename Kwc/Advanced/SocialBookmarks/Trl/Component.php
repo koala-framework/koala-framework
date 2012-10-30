2412b1e5d69944382e1770db2fb658363404aaa0
Niko Sams <ns@vivid-planet.com>
2012-10-30 11:00:10 +0100

    fix SocialBookmarks trl

--- Kwc/Advanced/SocialBookmarks/Trl/Component.php
+++ Kwc/Advanced/SocialBookmarks/Trl/Component.php
@@ -5,6 +5,20 @@ class Kwc_Advanced_SocialBookmarks_Trl_Component extends Kwc_Chained_Trl_MasterA
     {
         $ret = parent::getSettings($masterComponentClass);
         $ret['editComponents'][] = 'child'; //kann das vielleicht im parent gemacht werden?
+        $ret['flags']['hasAlternativeComponent'] = true;
         return $ret;
+    }
+
+
+    public static function getAlternativeComponents()
+    {
+        return array(
+            'inherit'=>'Kwc_Advanced_SocialBookmarks_Inherit_Trl_Component.Kwc_Advanced_SocialBookmarks_Inherit_Component'
+        );
+    }
+
+    public static function useAlternativeComponent($componentClass, $parentData, $generator)
+    {
+        return false;
     }
 }
