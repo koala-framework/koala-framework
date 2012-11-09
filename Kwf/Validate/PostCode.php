9b771ae788475a6dc654dc5882c6c13c504619c0
Alexander Kaufmann <ak@vivid-planet.com>
2012-11-09 07:57:09 +0100

    messageTemplates translated with trlKwfStatic

--- Kwf/Validate/PostCode.php
+++ Kwf/Validate/PostCode.php
@@ -4,8 +4,8 @@ class Kwf_Validate_PostCode extends Zend_Validate_PostCode
     // $options has to set as locale string for example for Austria PostCode set "de_AT"
     public function __construct($options = null)
     {
-        $this->_messageTemplates[self::INVALID] = trlKwf("Invalid type given. The value should be a string or a integer");
-        $this->_messageTemplates[self::NO_MATCH] = trlKwf("'%value%' does not appear to be a postal code");
+        $this->_messageTemplates[self::INVALID] = trlKwfStatic("Invalid type given. The value should be a string or a integer");
+        $this->_messageTemplates[self::NO_MATCH] = trlKwfStatic("'%value%' does not appear to be a postal code");
         parent::__construct($options);
     }
 }
