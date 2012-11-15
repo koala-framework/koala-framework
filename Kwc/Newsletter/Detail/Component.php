ea287e0db27f567b7214d885c26029099d4bb43c
Franz Unger <ufx@vivid-planet.com>
2012-11-15 09:20:38 +0100

    newsletter: subscribe model has to implement mapping
    prevents two code path which do the same

--- Kwc/Newsletter/Detail/Component.php
+++ Kwc/Newsletter/Detail/Component.php
@@ -71,7 +71,7 @@ class Kwc_Newsletter_Detail_Component extends Kwc_Directories_Item_Detail_Compon
         );
 
         if (!$model->hasColumnMappings('Kwc_Mail_Recipient_Mapping')) {
-            throw new Kwf_Exception('importToQueue must only be called for model that have column mapping "Kwc_Mail_Recipient_Mapping"');
+            throw new Kwf_Exception(get_class($this->_model) . ' has to implement column mapping "Kwc_Mail_Recipient_Mapping"');
         }
 
         if ($model->hasColumnMappings('Kwc_Mail_Recipient_UnsubscribableMapping')) {
@@ -116,92 +116,6 @@ class Kwc_Newsletter_Detail_Component extends Kwc_Directories_Item_Detail_Compon
         // add to model
         $queueModel = $this->getData()->parent->getComponent()->getChildModel()->getDependentModel('Queue');
         $queueModel->import(Kwf_Model_Db::FORMAT_ARRAY, $import, array('ignore' => true));
-        return $ret;
-    }
-
-    public function addToQueue(Kwc_Mail_Recipient_Interface $recipient)
-    {
-        $newsletter = $this->getData()->row;
-        if (in_array($newsletter->status, array('start', 'stop', 'finished', 'sending'))) {
-            throw new Kwf_ClientException(trlKwf('Can only add users to a paused newsletter'));
-        }
-
-        if ($recipient instanceof Zend_Db_Table_Row_Abstract) {
-            $class = get_class($recipient->getTable());
-        } else if ($recipient instanceof Kwf_Model_Row_Abstract) {
-            $class = get_class($recipient->getModel());
-        } else {
-            throw new Kwf_Exception('Only models or tables are supported.');
-        }
-
-        // check if the necessary modelShortcut is set in 'mail' childComponent
-        $generators = $this->_getSetting('generators');
-        // this function checks if everything neccessary is set
-        Kwc_Mail_Redirect_Component::getRecipientModelShortcut(
-            $generators['mail']['component'],
-            $class
-        );
-
-        // break here if the receiver has unsubscribed
-        if ($recipient instanceof Kwc_Mail_Recipient_UnsubscribableInterface) {
-            if ($recipient->getMailUnsubscribe()) return false;
-        }
-        // break if the account has not been activated
-        if ($recipient instanceof Kwf_Model_Abstract &&
-            $recipient->getModel()->hasColumn('activated') &&
-            !$recipient->activated
-        ) {
-            return false;
-        }
-
-        // add to senders list import
-        $this->_toImport[] = array(
-            'newsletter_id' => $newsletter->id,
-            'recipient_model' => $class,
-            'recipient_id' => $recipient->id,
-            'status' => 'queued',
-            'searchtext' =>
-                $recipient->getMailFirstname() . ' ' .
-                $recipient->getMailLastname() . ' ' .
-                $recipient->getMailEmail()
-        );
-
-        // if this receiver should be checked against the rtr-ecg
-        if (count($this->_toImport) && $this->_getSetting('checkRtrList')) {
-            $this->_rtrCheck[count($this->_toImport) - 1] = $recipient->getMailEmail();
-        }
-
-        return true;
-    }
-
-    public function saveQueue()
-    {
-        $ret = array();
-        $ret['rtrExcluded'] = array();
-
-        // check against rtr-ecg list
-        if (count($this->_rtrCheck)) {
-            $badKeys = Kwf_Util_RtrList::getBadKeys($this->_rtrCheck);
-
-            // remove the bad rtr entries from the list
-            if ($badKeys) {
-                foreach ($badKeys as $badKey) {
-                    $ret['rtrExcluded'][] = $this->_rtrCheck[$badKey];
-                    unset($this->_toImport[$badKey]);
-                }
-                // assign new keys
-                $this->_toImport = array_values($this->_toImport);
-            }
-        }
-
-        // add to model
-        $newsletter = $this->getData()->row;
-        $model = $this->getData()->parent->getComponent()->getChildModel()->getDependentModel('Queue');
-        $select = $model->select()->whereEquals('newsletter_id', $newsletter->id);
-        $ret['before'] = $model->countRows($select);
-        $model->import(Kwf_Model_Db::FORMAT_ARRAY, $this->_toImport, array('ignore' => true));
-        $ret['after'] = $model->countRows($select);
-        $ret['added'] = $ret['after'] - $ret['before'];
         return $ret;
     }
 }
--- Kwc/Newsletter/Subscribe/AbstractRecipientsController.php
+++ Kwc/Newsletter/Subscribe/AbstractRecipientsController.php
@@ -73,46 +73,25 @@ abstract class Kwc_Newsletter_Subscribe_AbstractRecipientsController extends Kwf
         $progressBar = new Zend_ProgressBar(
             new Kwf_Util_ProgressBar_Adapter_Cache($this->_getParam('progressNum')),
             0,
-            $count + 1
+            $count
         );
 
-        if ($this->_model->hasColumnMappings('Kwc_Mail_Recipient_Mapping')) {
-            $limit = 1000;
-            $offset = 0;
-            $this->view->before = $component->getComponent()->countQueue();
-            $this->view->rtrExcluded = array();
-            do {
-                $select->limit($limit, $offset);
-                $result = $component->getComponent()->importToQueue($this->_model, $select);
-                $this->view->rtrExcluded = array_merge(
-                    $this->view->rtrExcluded, $result['rtrExcluded']
-                );
-                $select->unsetPart('limit');
-                $offset += $limit;
-                $progressBar->next($limit, $offset." / $count");
-            } while ($count > $offset);
-            $progressBar->next(1, "$count / $count");
-            $this->view->after = $component->getComponent()->countQueue();
-            $this->view->added = $this->view->after - $this->view->before;
-        } else {
-            $limit = 1000;
-
-            $offset = 0;
-            do {
-                $select->limit($limit, $offset);
-                $rowset = $this->_model->getRows($select);
-                $x = 0;
-                foreach ($rowset as $row) {
-                    $x++;
-                    $component->getComponent()->addToQueue($row);
-                    $progressBar->next(1, ($offset+$x)." / $count");
-                }
-                $offset += $limit;
-            } while (count($rowset));
-            unset($rowset);
-            $progressBar->next(1, trlKwf('RTR-ECG-Check and saving data: please wait...'));
-            $this->view->assign($component->getComponent()->saveQueue());
-        }
+        $limit = 1000;
+        $offset = 0;
+        $this->view->before = $component->getComponent()->countQueue();
+        $this->view->rtrExcluded = array();
+        do {
+            $select->limit($limit, $offset);
+            $result = $component->getComponent()->importToQueue($this->_model, $select);
+            $this->view->rtrExcluded = array_merge(
+                $this->view->rtrExcluded, $result['rtrExcluded']
+            );
+            $select->unsetPart('limit');
+            $offset += $limit;
+            $progressBar->next($limit, $offset." / $count");
+        } while ($count > $offset);
+        $this->view->after = $component->getComponent()->countQueue();
+        $this->view->added = $this->view->after - $this->view->before;
 
         $progressBar->finish();
     }
