04b1103e32d1c2d575190785c605ed94e0fa0d9e
Alexander Kaufmann <ak@vivid-planet.com>
2012-11-29 10:40:32 +0100

    resourceId from strucute is lowerCase

--- Kwf/Acl/Resource/Component/Structure.php
+++ Kwf/Acl/Resource/Component/Structure.php
@@ -3,6 +3,6 @@ class Kwf_Acl_Resource_Component_Structure extends Zend_Acl_Resource
 {
     public function __construct($componentClass)
     {
-        parent::__construct('kwc_structure_' . $componentClass);
+        parent::__construct('kwc_structure_' . strtolower($componentClass));
     }
 }
