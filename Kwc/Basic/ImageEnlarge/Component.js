b0c5297447218a82445b396bbe1a73969d9c4b47
Mario Kemetinger <mk@vivid-planet.com>
2012-12-06 15:04:03 +0100

    HoverIcons bei ImageEnlarge ueberarbeitet

--- Kwc/Basic/ImageEnlarge/Component.js
+++ Kwc/Basic/ImageEnlarge/Component.js
@@ -1,5 +1,5 @@
 Kwf.onElementReady('.kwcBasicImageEnlarge.showHoverIcon > a', function(el) {
-    if (el.getWidth() > 100 && el.getHeight() > 100) {
+    if (el.getWidth() > 50 && el.getHeight() > 50) {
         el.createChild({ tag: 'span', cls: 'outerHoverIcon', html: '<span class="innerHoverIcon"></span>'});
     }
 });
