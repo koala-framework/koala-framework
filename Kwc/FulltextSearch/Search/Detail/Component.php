60008a28156cca175008f440212fb205b9c05559
Niko Sams <ns@vivid-planet.com>
2012-12-17 11:10:54 +0100

    highlight terms in search

--- Kwc/FulltextSearch/Search/Detail/Component.php
+++ Kwc/FulltextSearch/Search/Detail/Component.php
@@ -4,6 +4,15 @@ class Kwc_FulltextSearch_Search_Detail_Component extends Kwc_Directories_Item_De
     public static function getSettings()
     {
         $ret = parent::getSettings();
+        $ret['viewCache'] = false;
+        return $ret;
+    }
+
+    public function getTemplateVars()
+    {
+        $ret = parent::getTemplateVars();
+        $ret['queryParts'] = $this->getData()->parent->getChildComponent('-view')
+            ->getComponent()->getSearchForm()->getComponent()->getFormRow()->query;
         return $ret;
     }
 }
--- Kwc/FulltextSearch/Search/Detail/Component.tpl
+++ Kwc/FulltextSearch/Search/Detail/Component.tpl
@@ -1,4 +1,4 @@
 <div class="<?=$this->cssClass?>">
-    <?=$this->componentLink($this->data->row->data)?>
-    <?=$this->data->row->content?>
+    <?=$this->componentLink($this->data->row->data, $this->highlightTerms($this->queryParts, $this->data->row->data->name));?>
+    <span class="preview"><?=$this->highlightTerms($this->queryParts, $this->data->row->content);?></span>
 </div>
