2f7dc95ddede972161b29cc9bdd99a51036d78ff
Franz Unger <ufx@vivid-planet.com>
2013-01-09 12:29:26 +0100

    add possibility to hide sender fields for mail component

--- Kwc/Mail/Form.php
+++ Kwc/Mail/Form.php
@@ -9,15 +9,19 @@ class Kwc_Mail_Form extends Kwc_Abstract_Form
         $this->add(new Kwf_Form_Field_TextField('subject', trlKwf('Subject')))
             ->setAllowBlank(false)
             ->setWidth(300);
-        $this->add(new Kwf_Form_Field_TextField('from_email', trlKwf('From Address')))
-            ->setVtype('email')
-            ->setWidth(300)
-            ->setDefaultValue($default['from_email']);
-        $this->add(new Kwf_Form_Field_TextField('from_name', trlKwf('From Name')))
-            ->setWidth(300)
-            ->setDefaultValue($default['from_name']);
-        $this->add(new Kwf_Form_Field_TextField('reply_email', trlKwf('Reply Address')))
-            ->setVtype('email')
-            ->setWidth(300);
+        if (Kwc_Abstract::getSetting($this->getClass(), 'editFrom')) {
+            $this->add(new Kwf_Form_Field_TextField('from_email', trlKwf('From Address')))
+                ->setVtype('email')
+                ->setWidth(300)
+                ->setDefaultValue($default['from_email']);
+            $this->add(new Kwf_Form_Field_TextField('from_name', trlKwf('From Name')))
+                ->setWidth(300)
+                ->setDefaultValue($default['from_name']);
+        }
+        if (Kwc_Abstract::getSetting($this->getClass(), 'editReplyTo')) {
+            $this->add(new Kwf_Form_Field_TextField('reply_email', trlKwf('Reply Address')))
+                ->setVtype('email')
+                ->setWidth(300);
+        }
     }
 }
