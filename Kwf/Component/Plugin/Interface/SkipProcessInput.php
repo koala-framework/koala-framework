a2bcda93e1423ee607e1dbf1ab7c2a0a351847c1
Niko Sams <ns@vivid-planet.com>
2013-01-17 16:47:04 +0100

    document plugin interfaces

--- Kwf/Component/Plugin/Interface/SkipProcessInput.php
+++ Kwf/Component/Plugin/Interface/SkipProcessInput.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * Component Plugins that implement this interface can skip the processInput call.
+ *
+ * gets called also for cached contents
  */
 interface Kwf_Component_Plugin_Interface_SkipProcessInput
 {
--- Kwf/Component/Plugin/Interface/ViewBeforeChildRender.php
+++ Kwf/Component/Plugin/Interface/ViewBeforeChildRender.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Plugin Interface that can process the view output *before* all child components are rendered
+ *
+ * gets called also for cached contents
+ */
 interface Kwf_Component_Plugin_Interface_ViewBeforeChildRender
 {
     public function processOutput($output);
