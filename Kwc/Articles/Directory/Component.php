cad31abe1ce702c007c07c98f9c820297aa0fa2a
Alexander Kaufmann <ak@vivid-planet.com>
2013-01-21 10:41:39 +0100

    add ajax view for articles directory

--- Kwc/Articles/Directory/Component.php
+++ Kwc/Articles/Directory/Component.php
@@ -4,7 +4,7 @@ class Kwc_Articles_Directory_Component extends Kwc_Directories_ItemPage_Director
     public static function getSettings()
     {
         $ret = parent::getSettings();
-
+        $ret['generators']['child']['component']['view'] = 'Kwc_Articles_Directory_View_Component';
         return $ret;
     }
 }
