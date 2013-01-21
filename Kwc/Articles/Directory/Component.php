75236de03cfda48c9cdc28fdf2b49ae791d99569
Alexander Kaufmann <ak@vivid-planet.com>
2013-01-21 10:39:48 +0100

    added articles directory for VI's

---
+++ Kwc/Articles/Directory/Component.php
@@ -0,0 +1,10 @@
+<?php
+class Kwc_Articles_Directory_Component extends Kwc_Directories_ItemPage_Directory_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+
+        return $ret;
+    }
+}
