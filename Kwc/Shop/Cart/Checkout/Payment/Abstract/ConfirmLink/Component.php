0876b3243123e0764daee8e32acf1ab84be2ef7a
Mario Kemetinger <mk@vivid-planet.com>
2013-01-21 11:02:54 +0100

    The placeholder has to be in getSettings - not in getTemplateVars

--- Kwc/Shop/Cart/Checkout/Payment/Abstract/ConfirmLink/Component.php
+++ Kwc/Shop/Cart/Checkout/Payment/Abstract/ConfirmLink/Component.php
@@ -1,11 +1,17 @@
 <?php
 class Kwc_Shop_Cart_Checkout_Payment_Abstract_ConfirmLink_Component extends Kwc_Abstract
 {
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['placeholder']['confirm'] = trlKwfStatic('Send order');
+        return $ret;
+    }
+
     public function getTemplateVars()
     {
         $ret = parent::getTemplateVars();
         $ret['confirm'] = $this->getData()->parent->getChildComponent('_confirm');
-        $ret['placeholder']['confirm'] = 'Send order';
         return $ret;
     }
 }
