7ba519ac726a28c218e822af760cb6b0ede4b358
Alexander Kaufmann <ak@vivid-planet.com>
2013-01-22 13:18:07 +0100

    added expressions for articles model to check - logged in user is allowed to see article - show year from date field - check if article is top and not expired

--- Kwc/Articles/Directory/Model.php
+++ Kwc/Articles/Directory/Model.php
@@ -10,4 +10,30 @@ class Kwc_Articles_Directory_Model extends Kwf_Model_Db
      protected $_referenceMap = array(
          'Author' => 'author_id->Kwc_Articles_Directory_AuthorsModel',
      );
+
+    protected function _init()
+    {
+        parent::_init();
+        $authedUser = Kwf_Registry::get('userModel')->getAuthedUser();
+        if ($authedUser) {
+            $s = new Kwf_Model_Select();
+            $s->whereEquals('user_id', $authedUser->id);
+            if ($authedUser->role == 'external') {
+                $this->_exprs['autheduser_visible'] = new Kwf_Model_Select_Expr_Not(new Kwf_Model_Select_Expr_Field('only_intern'));
+            } else {
+                $this->_exprs['autheduser_visible'] = new Kwf_Model_Select_Expr_Boolean(true);
+            }
+        } else {
+            $this->_exprs['autheduser_visible'] = new Kwf_Model_Select_Expr_Boolean(false);
+        }
+
+        $this->_exprs['date_year'] = new Kwf_Model_Select_Expr_Date_Year('date');
+        $this->_exprs['is_top_and_not_expired'] = new Kwf_Model_Select_Expr_And(array(
+            new Kwf_Model_Select_Expr_Field('is_top'),
+            new Kwf_Model_Select_Expr_Or(array(
+                new Kwf_Model_Select_Expr_Higher('expire_top_read_required', new Kwf_Date('now')),
+                new Kwf_Model_Select_Expr_IsNull('expire_top_read_required'),
+            ))
+        ));
+    }
 }
