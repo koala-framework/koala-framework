341be4422c53d363d75dfee2b128576c9edd1f88
Alexander Kaufmann <ak@vivid-planet.com>
2013-01-22 13:25:41 +0100

    added model for db table kwc_articles_tags with correct dependencies/references for articlesToTag

--- Kwc/Articles/Directory/Model.php
+++ Kwc/Articles/Directory/Model.php
@@ -5,6 +5,7 @@ class Kwc_Articles_Directory_Model extends Kwf_Model_Db
     protected $_toStringField = 'title';
 
     protected $_dependentModels = array(
+        'ArticleToTag' => 'Kwc_Articles_Directory_ToTagModel',
         'Categories' => 'Kwc_Articles_Directory_ToCategoryModel'
     );
      protected $_referenceMap = array(
---
+++ Kwc/Articles/Directory/TagsModel.php
@@ -0,0 +1,23 @@
+<?php
+class Kwc_Articles_Directory_TagsModel extends Kwf_Model_Db
+{
+    protected $_table = 'kwc_article_tags';
+    protected $_toStringField = 'name';
+
+    protected $_dependentModels = array(
+        'ArticleToTag' => 'Kwc_Articles_Directory_ToTagModel'
+    );
+
+    protected function _setupFilters()
+    {
+        parent::_setupFilters();
+        $this->_filters['pos'] = new Kwf_Filter_Row_Numberize();
+        $this->_filters['pos']->setGroupBy('type');
+    }
+
+    protected function _init()
+    {
+        parent::_init();
+        $this->_exprs['count_used'] = new Kwf_Model_Select_Expr_Child_Count('ArticleToTag');
+    }
+}
