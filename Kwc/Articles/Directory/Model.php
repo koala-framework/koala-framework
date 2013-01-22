68d399907b03e8fdda0635afe4ea3bb431a1d70b
Alexander Kaufmann <ak@vivid-planet.com>
2013-01-22 13:23:43 +0100

    bugfix for articles model, added correct dependentModel for categories

--- Kwc/Articles/Directory/Model.php
+++ Kwc/Articles/Directory/Model.php
@@ -5,7 +5,7 @@ class Kwc_Articles_Directory_Model extends Kwf_Model_Db
     protected $_toStringField = 'title';
 
     protected $_dependentModels = array(
-        'Categories' => 'ArticleToCategory'
+        'Categories' => 'Kwc_Articles_Directory_ToCategoryModel'
     );
      protected $_referenceMap = array(
          'Author' => 'author_id->Kwc_Articles_Directory_AuthorsModel',
