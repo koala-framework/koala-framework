62c28c37add3c405c63a804397b28868a360c131
Niko Sams <ns@vivid-planet.com>
2013-01-28 16:18:43 +0100

    javascript implementation of fieldset checkbox in frontend

--- Kwf_js/FrontendForm/FieldSet.js
+++ Kwf_js/FrontendForm/FieldSet.js
@@ -1,8 +1,7 @@
 Kwf.onContentReady(function()
 {
-    var checkboxes = Ext.query('div.kwfFormContainerFieldSet fieldset legend input');
-    Ext.each(checkboxes, function(c) {
-        c = Ext.get(c);
+    Ext.select('div.kwfFormContainerFieldSet').each(function(fs) {
+        var c = fs.child('fieldset > legend > input');
         if (!c.dom.checked) {
             c.up('fieldset').addClass('kwfFormContainerFieldSetCollapsed');
         }
@@ -16,3 +15,33 @@ Kwf.onContentReady(function()
     });
 });
 
+Kwf.FrontendForm.FieldSet = Ext.extend(Kwf.FrontendForm.Field, {
+    initField: function() {
+        var inp = this.el.child('fieldset > legend > input');
+        if (inp) {
+            inp.on('click', function() {
+                this.fireEvent('change', this.getValue());
+            }, this);
+        }
+    },
+    getFieldName: function() {
+        var inp = this.el.child('fieldset > legend > input');
+        if (!inp) return null;
+        return inp.dom.name;
+    },
+    getValue: function() {
+        var inp = this.el.child('fieldset > legend > input');
+        if (!inp) return null;
+        return inp.dom.value;
+    },
+    clearValue: function() {
+        var inp = this.el.child('fieldset > legend > input');
+        inp.dom.value = '';
+    },
+    setValue: function(value) {
+        var inp = this.el.child('fieldset > legend > input');
+        inp.dom.value = value;
+    }
+});
+
+Kwf.FrontendForm.fields['kwfFormContainerFieldSet'] = Kwf.FrontendForm.FieldSet;
