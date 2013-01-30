0d9a7db211fbe4e588013612452babf3acde5fc3
Franz Unger <ufx@vivid-planet.com>
2013-01-30 12:24:43 +0100

    removed console.log

--- Kwc/User/Activate/Form/Success/Component.js
+++ Kwc/User/Activate/Form/Success/Component.js
@@ -1,7 +1,5 @@
 Kwf.onContentReady(function() {
-    console.log('foo');
     Ext.query('.kwcUserFormSuccess').each(function(el) {
-        console.log('bar');
         window.setTimeout("window.location.href = '/'", 3000);
     }, this);
 });
