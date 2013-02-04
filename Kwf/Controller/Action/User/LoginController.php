5b511f4b03cb2d376fee550112c20e7ae5aa7a5f
Niko Sams <niko.sams@gmail.com>
2013-02-04 06:27:00 +0100

    only check for production branch if git repository

--- Kwf/Controller/Action/User/LoginController.php
+++ Kwf/Controller/Action/User/LoginController.php
@@ -42,9 +42,10 @@ class Kwf_Controller_Action_User_LoginController extends Kwf_Controller_Action
         } else {
             $this->view->image = false;
         }
-        if (Kwf_Util_Git::web()->getActiveBranch() != 'production'
-            || Kwf_Util_Git::kwf()->getActiveBranch() != 'production/'.Kwf_Registry::get('config')->application->id
-        ) {
+        if (file_exists('.git') && Kwf_Util_Git::web()->getActiveBranch() != 'production') {
+            $this->view->untagged = true;
+        }
+        if (file_exists(KWF_PATH.'/.git') && Kwf_Util_Git::kwf()->getActiveBranch() != 'production/'.Kwf_Registry::get('config')->application->id) {
             $this->view->untagged = true;
         }
         $this->view->application = Zend_Registry::get('config')->application->toArray();
