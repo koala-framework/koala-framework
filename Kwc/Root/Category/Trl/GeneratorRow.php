e7ca4d4be42e73afb4bf7847cf26a2c3daf78f34
Niko Sams <ns@vivid-planet.com>
2013-02-04 14:18:15 +0100

    fix saving of trl home
    the visible column for home is ignored, don't check it

--- Kwc/Root/Category/Trl/GeneratorRow.php
+++ Kwc/Root/Category/Trl/GeneratorRow.php
@@ -11,13 +11,4 @@ class Kwc_Root_Category_Trl_GeneratorRow extends Kwf_Model_Proxy_Row
     {
         throw new Kwf_ClientException(trlKwf("Can't delete translated pages."));
     }
-
-    protected function _beforeUpdate()
-    {
-        parent::_beforeUpdate();
-        $c = Kwf_Component_Data_Root::getInstance()->getComponentByDbId($this->component_id, array('ignoreVisible'=>true, 'limit'=>1));
-        if ($c->isHome && !$this->visible) {
-            throw new Kwf_ClientException(trlKwf('Cannot set Home Page invisible'));
-        }
-    }
 }
