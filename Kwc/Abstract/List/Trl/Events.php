f3a536773d46bff9e152a2eeb7ee83296efbf87e
Niko Sams <ns@vivid-planet.com>
2013-02-04 14:56:55 +0100

    clear view cache of trl list

--- Kwc/Abstract/List/Trl/Events.php
+++ Kwc/Abstract/List/Trl/Events.php
@@ -15,9 +15,17 @@ class Kwc_Abstract_List_Trl_Events extends Kwc_Abstract_Events
     public function onRowUpdate(Kwf_Component_Event_Row_Updated $event)
     {
         if ($event->isDirty('visible')) {
-            $this->fireEvent(new Kwf_Component_Event_Component_HasContentChanged(
-                $this->_class, $event->row->component_id
-            ));
+            //component_id is the child component id, not as in master the list component id
+            $c = Kwf_Component_Data_Root::getInstance()
+                ->getComponentsByDbId($event->row->component_id, array('ignoreVisible'=>true));
+            foreach ($c as $i) {
+                $this->fireEvent(new Kwf_Component_Event_Component_HasContentChanged(
+                    $this->_class, $i->parent->dbId
+                ));
+                $this->fireEvent(new Kwf_Component_Event_Component_ContentChanged(
+                    $this->_class, $i->parent->dbId
+                ));
+            }
         }
     }
 }
