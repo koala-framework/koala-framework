4dd4052d732efd3c54f67be82fbc81db17c5161d
Niko Sams <ns@vivid-planet.com>
2013-02-05 16:21:45 +0100

    Trl: fire Page_Added events

--- Kwc/Chained/Trl/GeneratorEvents/Table/Page.php
+++ Kwc/Chained/Trl/GeneratorEvents/Table/Page.php
@@ -1,4 +1,10 @@
 <?php
 class Kwc_Chained_Trl_GeneratorEvents_Table_Page extends Kwc_Chained_Trl_GeneratorEvents_Table_PseudoPage
 {
+    protected function _fireComponentEvent($eventType, Kwf_Component_Data $c, $flag)
+    {
+        parent::_fireComponentEvent($eventType, $c, $flag);
+        $cls = 'Kwf_Component_Event_Page_'.$eventType;
+        $this->fireEvent(new $cls($c->componentClass, $c, $flag));
+    }
 }
