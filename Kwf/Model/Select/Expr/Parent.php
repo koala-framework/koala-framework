f43e48402657b62211afa6649f6f271bb752dded
Benjamin Hohenwarter <bh@vivid-planet.com>
2013-02-11 09:14:44 +0100

    Change to correct concatination operator

--- Kwf/Model/Select/Expr/Parent.php
+++ Kwf/Model/Select/Expr/Parent.php
@@ -27,13 +27,13 @@ class Kwf_Model_Select_Expr_Parent implements Kwf_Model_Select_Expr_Interface
     public function validate()
     {
         if (!$this->_field) {
-            throw new Kwf_Exception("No Field set for '"+get_class($this)+"'");
+            throw new Kwf_Exception("No Field set for '".get_class($this)."'");
         }
         if (!is_string($this->_field)) {
-            throw new Kwf_Exception("Field must be of type string in '"+get_class($this)+"'");
+            throw new Kwf_Exception("Field must be of type string in '".get_class($this)."'");
         }
         if (!$this->_parent) {
-            throw new Kwf_Exception("No parent rule set for '"+get_class($this)+"'");
+            throw new Kwf_Exception("No parent rule set for '".get_class($this)."'");
         }
     }
 
