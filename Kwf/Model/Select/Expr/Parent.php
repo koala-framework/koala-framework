18cd7d3b3fb09225737e01d40959db93bfd2f581
Benjamin Hohenwarter <bh@vivid-planet.com>
2013-02-11 14:23:35 +0100

    Change check in Kwf_Model_Select_Expr_Parent to && relation

--- Kwf/Model/Select/Expr/Parent.php
+++ Kwf/Model/Select/Expr/Parent.php
@@ -29,7 +29,7 @@ class Kwf_Model_Select_Expr_Parent implements Kwf_Model_Select_Expr_Interface
         if (!$this->_field) {
             throw new Kwf_Exception("No Field set for '".get_class($this)."'");
         }
-        if (!is_string($this->_field) || !($this->_field instanceof Kwf_Model_Select_Expr_Interface)) {
+        if (!is_string($this->_field) && !($this->_field instanceof Kwf_Model_Select_Expr_Interface)) {
             throw new Kwf_Exception("Field must be of type string or Kwf_Model_Select_Expr_Interface
                              in '".get_class($this)."'");
         }
