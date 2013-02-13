26bde926d8e585545b953fd3388b1aebea3043e5
Benjamin Hohenwarter <bh@vivid-planet.com>
2013-02-13 11:33:30 +0100

    Improve ProxyReference-Test and fix test

--- tests/Kwf/Model/DbWithConnection/ProxyReferences/Test.php
+++ tests/Kwf/Model/DbWithConnection/ProxyReferences/Test.php
@@ -4,6 +4,7 @@ class Kwf_Model_DbWithConnection_ProxyReferences_Test extends Kwf_Test_TestCase
     private $_modelParent;
     private $_modelParent2;
     private $_modelChild;
+    private $_modelStartChild;
     public function setUp()
     {
         $this->_modelParent = Kwf_Model_Abstract::
@@ -17,6 +18,10 @@ class Kwf_Model_DbWithConnection_ProxyReferences_Test extends Kwf_Test_TestCase
         $this->_modelChild = Kwf_Model_Abstract::
             getInstance('Kwf_Model_DbWithConnection_ProxyReferences_ChildModel');
         $this->_modelChild->setUp();
+
+        $this->_modelStartChild = Kwf_Model_Abstract::
+            getInstance('Kwf_Model_DbWithConnection_ProxyReferences_StartChildModel');
+        $this->_modelStartChild->setUp();
     }
 
     public function tearDown()
@@ -24,21 +29,21 @@ class Kwf_Model_DbWithConnection_ProxyReferences_Test extends Kwf_Test_TestCase
         $this->_modelParent->dropTable();
         $this->_modelParent2->dropTable();
         $this->_modelChild->dropTable();
+        $this->_modelStartChild->dropTable();
     }
 
     public function testChildContainsReference()
     {
         $childSelect = new Kwf_Model_Select();
         $childSelect->where(new Kwf_Model_Select_Expr_Parent('Parent2',
-                            new Kwf_Model_Select_Expr_Equal('foo2', 3)));
+                                new Kwf_Model_Select_Expr_Equal('foo2', 3)));
         $select = new Kwf_Model_Select();
-        $select->where(new Kwf_Model_Select_Expr_Parent('Parent',
-                            new Kwf_Model_Select_Expr_Child_Contains('Childs'), $childSelect));
-        $rows = $this->_modelChild->getRows($select);
-        $this->markTestIncomplete();
-        $this->assertEquals(count($rows), 3);
+        $select->where(new Kwf_Model_Select_Expr_Parent('StartParent',
+                            new Kwf_Model_Select_Expr_Child_Contains('Childs', $childSelect)));
+        $rows = $this->_modelStartChild->getRows($select);
+        $this->assertEquals(count($rows), 1);
         foreach ($rows as $row) {
-            $this->assertEquals($row->getParentRow('Parent2')->foo2, 3);
+            $this->assertEquals($row->start_child, 'Child #1');
         }
     }
 
