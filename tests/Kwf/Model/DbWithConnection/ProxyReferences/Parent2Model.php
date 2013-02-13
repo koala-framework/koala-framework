e032c2a8bb4107a06ec5cb85c24179d6f8ad8dfd
Benjamin Hohenwarter <bh@vivid-planet.com>
2013-02-13 10:09:04 +0100

    Add test for model_db_proxy references

---
+++ tests/Kwf/Model/DbWithConnection/ProxyReferences/Parent2Model.php
@@ -0,0 +1,30 @@
+<?php
+class Kwf_Model_DbWithConnection_ProxyReferences_Parent2Model extends Kwf_Model_Db
+{
+    private $_tableName;
+    public function __construct($config = array())
+    {
+        $this->_tableName = uniqid('dbtest_parent2');
+        $config['table'] = $this->_tableName;
+
+        $this->_dependentModels['Childs'] = 'Kwf_Model_DbWithConnection_ProxyReferences_ChildModel';
+
+        parent::__construct($config);
+    }
+
+    public function setUp()
+    {
+        Kwf_Registry::get('db')->query("CREATE TABLE IF NOT EXISTS {$this->_tableName} (
+            `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            `foo2` INT NOT NULL
+        ) ENGINE = INNODB");
+        Kwf_Registry::get('db')->query("TRUNCATE TABLE {$this->_tableName}");
+        Kwf_Registry::get('db')->query("INSERT INTO {$this->_tableName}
+                        (id, foo2) VALUES (1, 3)");
+    }
+
+    public function dropTable()
+    {
+        Kwf_Registry::get('db')->query("DROP TABLE IF EXISTS {$this->_tableName}");
+    }
+}
---
+++ tests/Kwf/Model/DbWithConnection/ProxyReferences/Test.php
@@ -0,0 +1,57 @@
+<?php
+class Kwf_Model_DbWithConnection_ProxyReferences_Test extends Kwf_Test_TestCase
+{
+    private $_modelParent;
+    private $_modelParent2;
+    private $_modelChild;
+    public function setUp()
+    {
+        $this->_modelParent = Kwf_Model_Abstract::
+            getInstance('Kwf_Model_DbWithConnection_ProxyReferences_ParentModel');
+        $this->_modelParent->setUp();
+
+        $this->_modelParent2 = Kwf_Model_Abstract::
+            getInstance('Kwf_Model_DbWithConnection_ProxyReferences_Parent2Model');
+        $this->_modelParent2->setUp();
+
+        $this->_modelChild = Kwf_Model_Abstract::
+            getInstance('Kwf_Model_DbWithConnection_ProxyReferences_ChildModel');
+        $this->_modelChild->setUp();
+    }
+
+    public function tearDown()
+    {
+        $this->_modelParent->dropTable();
+        $this->_modelParent2->dropTable();
+        $this->_modelChild->dropTable();
+    }
+
+    public function testChildContainsReference()
+    {
+        $childSelect = new Kwf_Model_Select();
+        $childSelect->where(new Kwf_Model_Select_Expr_Parent('Parent2',
+                            new Kwf_Model_Select_Expr_Equal('foo2', 3)));
+        $select = new Kwf_Model_Select();
+        $select->where(new Kwf_Model_Select_Expr_Parent('Parent',
+                            new Kwf_Model_Select_Expr_Child_Contains('Child'), $childSelect));
+        $this->markTestIncomplete();
+        $rows = $this->_modelChild->getRows($select);
+        $this->assertEquals(count($rows), 1);
+        foreach ($rows as $row) {
+            $this->assertEquals($row->foo, 5);
+        }
+    }
+
+    public function testChildReference()
+    {
+        $select = new Kwf_Model_Select();
+        $select->where(new Kwf_Model_Select_Expr_Parent('Parent',
+                            new Kwf_Model_Select_Expr_Equal('foo', 5)));
+        $this->markTestIncomplete();
+        $rows = $this->_modelChild->getRows($select);
+        $this->assertEquals(count($rows), 1);
+        foreach ($rows as $row) {
+            $this->assertEquals($row->foo, 5);
+        }
+    }
+}
