90ffa851f4cf8987b5e57c0dae20a1c80cd26b3d
Tobias Asen <ta@vivid-planet.com>
2013-02-15 08:24:04 +0100

    Combobox is positionated correctly if there is a label added but no box label

--- Kwc/Form/Decorator/Label.php
+++ Kwc/Form/Decorator/Label.php
@@ -46,13 +46,13 @@ class Kwc_Form_Decorator_Label extends Kwc_Form_Decorator_Abstract
                 $class .= ' '.$item['item']->getFieldName();
             }
             $preHtml = '<div class="'.$class.'">';
-            if ($item['item'] && !$item['item']->getHideLabel() && $item['item']->getFieldLabel()) {
+            if ($item['item'] && !$item['item']->getHideLabel()) {
                 $preHtml .= '<label for="'
                     .(isset($item['id']) ? $item['id'] : $item['item']->getFieldName()).'"'
                     .($item['item']->getLabelWidth() ? ' style="width:'.$item['item']->getLabelWidth().'px"' : '')
                 .'>';
                 $preHtml .= $item['item']->getFieldLabel();
-                if ($item['item']->getAllowBlank()===false) {
+                if ($item['item']->getAllowBlank()===false && $item['item']->getFieldLabel()) {
                     /* TODO: wenn wir hier einmal andere Zeichen benötigen oder an einer anderen
                      * Position, dann machen wir eine reine CSS-Lösung:
                      * für alles ausser <IE8 .foo:after { content: '*'; }
@@ -62,8 +62,10 @@ class Kwc_Form_Decorator_Label extends Kwc_Form_Decorator_Abstract
                      */
                     $preHtml .= '<span class="requiredSign">*</span>';
                 }
-                $preHtml .= $item['item']->getLabelSeparator();
-                $preHtml .= '</label>';
+                if ($item['item']->getFieldLabel()) {
+                    $preHtml .= $item['item']->getLabelSeparator();
+                }
+                $preHtml .= '&nbsp;</label>';
             }
             $postHtml = '';
             if ($item['item'] && $item['item']->getComment()) {
