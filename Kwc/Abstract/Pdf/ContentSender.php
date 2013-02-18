1fe0631b830d25a97741ec1b1d0d2e1598fad583
Franz Unger <ufx@vivid-planet.com>
2013-02-18 09:44:54 +0100

    make pdf-content sender working

--- Kwc/Abstract/Pdf/ContentSender.php
+++ Kwc/Abstract/Pdf/ContentSender.php
@@ -6,20 +6,15 @@ class Kwc_Abstract_Pdf_ContentSender extends Kwf_Component_Abstract_ContentSende
         return $this->_data->parent;
     }
 
-    public function sendContent($name = '', $dest = 'I')
+    public function sendContent($includeMaster)
     {
-        $plugins = $this->_data->getPlugins('Kwf_Component_Plugin_Interface_View');
-        if ($plugins) {
-            if (count($plugins) > 1 || !is_instance_of($plugins[0], 'Kwf_Component_Plugin_Password_Component')) {
-                throw new Kwf_Exception("For pdf only one plugin of type 'Kwf_Component_Plugin_Password_Component' is allowed.");
-            }
-            $p = new $plugins[0]($this->_data->componentId);
-            if ($p->processOutput('')) {
-                parent::sendContent($includeMaster);
-                return false;
-            }
+        if ($this->checkAllowed()) {
+            $this->outputPdf();
         }
+    }
 
+    public function outputPdf($name = '', $dest = 'I')
+    {
         $masterClass = Kwc_Admin::getComponentFile($this->_data->componentClass, 'PdfMaster', 'php', true);
         if (!$masterClass) { $masterClass = 'Kwf_Pdf_TcPdf'; }
         $pdfComponent = $this->_getPdfComponent();
@@ -29,5 +24,18 @@ class Kwc_Abstract_Pdf_ContentSender extends Kwf_Component_Abstract_ContentSende
         $pdf = new $masterClass($pdfComponent);
         $pdfComponent->getPdfWriter($pdf)->writeContent();
         return $pdf->output($name, $dest);
+    }
+
+    protected function checkAllowed()
+    {
+        $valid = Kwf_Media_Output_Component::isValid($this->_data->componentId);
+        if ($valid == Kwf_Media_Output_IsValidInterface::ACCESS_DENIED) { // send non pdf content to show login-plugin
+            $contentSender = new Kwf_Component_Abstract_ContentSender_Default($this->_data);
+            $contentSender->sendContent(true);
+            return false;
+        } else if ($valid == Kwf_Media_Output_IsValidInterface::INVALID) {
+            throw new Kwf_Exception_NotFound();
+        }
+        return $valid;
     }
 }
