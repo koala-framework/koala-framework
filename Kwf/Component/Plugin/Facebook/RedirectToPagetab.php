5491cbe2e3768678e49edbd34e9da1527a51cb37
Michael Freudenthaler <mf@vivid-planet.com>
2013-02-20 13:32:51 +0100

    simplyfied the plugin
    this plugin should only redirect - see the comments inside for more info

--- Kwf/Component/Plugin/Facebook/RedirectToPagetab.php
+++ Kwf/Component/Plugin/Facebook/RedirectToPagetab.php
@@ -1,4 +1,8 @@
 <?php
+/*
+// * this plugin allways redirects the user to the pagetab
+* e.g. we use this plugin for a fangate through the signed request on the pagetab
+*/
 class Kwf_Component_Plugin_Facebook_RedirectToPagetab extends Kwf_Component_Plugin_Abstract
     implements Kwf_Component_Plugin_Interface_ViewReplace
 {
@@ -12,34 +16,12 @@ class Kwf_Component_Plugin_Facebook_RedirectToPagetab extends Kwf_Component_Plug
         $signedRequest = $facebook->getSignedRequest();
         // check, if user is authed, or if we are on the right pagetab
         //(that information is provided by the signedRequest)
-        if (!$userId) {
-            if (!isset($signedRequest['page']['id'])
-                || $signedRequest['page']['id'] != Kwf_Config::getValue('kwc.fbAppData.siteId')) {
-                return '<script>top.location.href = "'.Kwf_Config::getValue('kwc.fbAppData.pageTabUrl').'";</script>"';
-            }
+        if (!isset($signedRequest['page']['id'])
+            || $signedRequest['page']['id'] != Kwf_Config::getValue('kwc.fbAppData.siteId')) {
+            echo '<script>top.location.href = "'.Kwf_Config::getValue('kwc.fbAppData.pageTabUrl').'";</script>"';
+            exit;
         } else {
-            // We have a user ID, so probably a logged in user.
-            // If not, we'll get an exception, which we handle below.
-            try {
-                $likes = $facebook->api('/'.$userId.'/likes');
-                $fanSiteId = trim(Kwf_Config::getValue('kwc.fbAppData.siteId'));
-                foreach ($likes['data'] as $l) {
-                    if ($l['id'] == $fanSiteId) {
-                        return false;
-                    }
-                }
-                if (!isset($signedRequest['page']['id'])
-                    || $signedRequest['page']['id'] != Kwf_Config::getValue('kwc.fbAppData.siteId')) {
-                    return '<script>top.location.href = "'.Kwf_Config::getValue('kwc.fbAppData.pageTabUrl').'";</script>"';
-                }
-            } catch(FacebookApiException $e) {
-                // If the user is logged out, you can have a
-                // user ID even though the access token is invalid.
-                // In this case, we'll get an exception, so we'll
-                // send the user back to the pagetab
-                return '<script>top.location.href = "'.Kwf_Config::getValue('kwc.fbAppData.pageTabUrl').'";</script>"';
-            }
+            return false;
         }
-        return false;
     }
 }
