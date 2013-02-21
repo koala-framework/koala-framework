45055d462ef608c0c6c13982fcc447024922d295
Tobias Asen <ta@vivid-planet.com>
2013-02-21 12:42:08 +0100

    remove unused code

--- Kwc/List/ChildPages/PageNameOnly/Component.php
+++ Kwc/List/ChildPages/PageNameOnly/Component.php
@@ -16,12 +16,4 @@ class Kwc_List_ChildPages_PageNameOnly_Component extends Kwc_Abstract
         $ret['childPages'] = $page->getChildPages();
         return $ret;
     }
-
-    public static function getStaticCacheMeta($componentClass)
-    {
-        $ret = parent::getStaticCacheMeta($componentClass);
-        $ret[] = new Kwf_Component_Cache_Meta_Static_Model('Kwf_Component_Model', '{componentId}');
-        $ret[] = new Kwf_Component_Cache_Meta_Static_Model('Kwc_Root_Category_GeneratorModel', '{id}');
-        return $ret;
-    }
 }
