5f0d40e5b911a41232ce9eb9e5d5a5260799cd97
Benjamin Hohenwarter <bh@vivid-planet.com>
2013-02-25 14:36:48 +0100

    Add additional event to listen to favourite-component
    This events may occure and effect the results of favourites counts and lists

--- Kwc/Favourites/Events.php
+++ Kwc/Favourites/Events.php
@@ -10,6 +10,20 @@ class Kwc_Favourites_Events extends Kwf_Component_Abstract_Events
             'callback' => 'onComponentRemove'
         );
         $ret[] = array(
+            'class' => $this->_class,
+            'event' => 'Kwf_Component_Event_Component_Added',
+            'callback' => 'onComponentAdd'
+        );
+        $ret[] = array(
+            'event' => 'Kwf_Component_Event_Component_RecursiveRemoved',
+            'callback' => 'onComponentRecursiveRemoveOrAdd'
+        );
+        $ret[] = array(
+            'event' => 'Kwf_Component_Event_Component_RecursiveAdded',
+            'callback' => 'onComponentRecursiveRemoveOrAdd'
+        );
+
+        $ret[] = array(
             'class' => Kwc_Abstract::getSetting($this->_class, 'favouritesModel'),
             'event' => 'Kwf_Component_Event_Row_Deleted',
             'callback' => 'onFavouriteRemove'
@@ -33,12 +47,54 @@ class Kwc_Favourites_Events extends Kwf_Component_Abstract_Events
         $userIds = $model->export(Kwf_Model_Abstract::FORMAT_ARRAY, $select, $options);
         if ($userIds) {
             foreach ($userIds as $userId) {
-                $componentIds = Kwf_Cache_Simple::fetch('favCIds'.$userId, $success);
+                $componentIds = Kwf_Cache_Simple::fetch('favCIds'.$userId['user_id'], $success);
                 if ($success) {
-                    Kwf_Cache_Simple::delete('favCIds'.$userId);
+                    Kwf_Cache_Simple::delete('favCIds'.$userId['user_id']);
                     $key = array_search($componentId, $componentIds);
                     unset($componentIds[$key]);
-                    Kwf_Cache_Simple::add('favCIds'.$userId, $componentIds);
+                    Kwf_Cache_Simple::add('favCIds'.$userId['user_id'], $componentIds);
+                    $log = Kwf_Component_Events_Log::getInstance();
+                    if ($log) {
+                        $log->log("favourites cache clear $componentId", Zend_Log::INFO);
+                    }
+                }
+            }
+        }
+    }
+
+    public function onComponentRecursiveRemoveOrAdd(Kwf_Component_Event_Component_RecursiveAbstract $event)
+    {
+        $componentId = $event->component->componentId;
+        $model = Kwf_Model_Abstract::getInstance(Kwc_Abstract::
+                        getSetting($this->_class, 'favouritesModel'));
+        $select = new Kwf_Model_Select();
+        $select->where(new Kwf_Model_Select_Expr_Like('component_id', $componentId.'%'));
+        $options = array('columns' => array('user_id'));
+        $userIds = $model->export(Kwf_Model_Abstract::FORMAT_ARRAY, $select, $options);
+        if ($userIds) {
+            foreach ($userIds as $userId) {
+                Kwf_Cache_Simple::delete('favCIds'.$userId['user_id']);
+            }
+        }
+    }
+
+    public function onComponentAdd(Kwf_Component_Event_Component_Added $event)
+    {
+        $componentId = $event->component->componentId;
+        $model = Kwf_Model_Abstract::getInstance(Kwc_Abstract::
+                        getSetting($this->_class, 'favouritesModel'));
+        $select = new Kwf_Model_Select();
+        $select->where(new Kwf_Model_Select_Expr_Equal('component_id', $componentId));
+        $options = array('columns' => array('user_id'));
+        $userIds = $model->export(Kwf_Model_Abstract::FORMAT_ARRAY, $select, $options);
+
+        if ($userIds) {
+            foreach ($userIds as $userId) {
+                $componentIds = Kwf_Cache_Simple::fetch('favCIds'.$userId['user_id'], $success);
+                if ($success) {
+                    Kwf_Cache_Simple::delete('favCIds'.$userId['user_id']);
+                    $componentIds[] = $componentId;
+                    Kwf_Cache_Simple::add('favCIds'.$userId['user_id'], $componentIds);
                     $log = Kwf_Component_Events_Log::getInstance();
                     if ($log) {
                         $log->log("favourites cache clear $componentId", Zend_Log::INFO);
