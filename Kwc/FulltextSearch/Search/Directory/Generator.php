597fd4f8c01bda490ad0a57f476196999ffde038
Benjamin Hohenwarter <bh@vivid-planet.com>
2013-03-01 11:49:03 +0100

    Make searchpage-subroot to be dynamic

--- Kwc/FulltextSearch/Search/Directory/Generator.php
+++ Kwc/FulltextSearch/Search/Directory/Generator.php
@@ -2,4 +2,14 @@
 class Kwc_FulltextSearch_Search_Directory_Generator extends Kwf_Component_Generator_Table
 {
     protected $_hasNumericIds = false;
+
+    protected function _formatSelect($parentData, $select)
+    {
+        $select = parent::_formatSelect($parentData, $select);
+        if (!$parentData) {
+            throw new Kwf_Exception('ParentData is null, this should not happen');
+        }
+        $select->whereEquals('subroot', $parentData);
+        return $select;
+    }
 }
