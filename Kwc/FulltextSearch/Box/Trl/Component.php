cd6ba69c9ddc90e30bef5f328aa5e4b6e7d9ba44
Benjamin Hohenwarter <bh@vivid-planet.com>
2013-03-04 08:45:39 +0100

    Fix searchBox-trl getting the correct form
    The old searchBox-trl code was deprecated. Now it's searching for the search-trl component and getting it's form to display.

--- Kwc/FulltextSearch/Box/Trl/Component.php
+++ Kwc/FulltextSearch/Box/Trl/Component.php
@@ -1,10 +1,27 @@
 <?php
 class Kwc_FulltextSearch_Box_Trl_Component extends Kwc_Chained_Trl_Component
 {
+    public function processInput($postData)
+    {
+        Kwf_Component_Data_Root::getInstance()
+            ->getComponentByClass('Kwc_FulltextSearch_Search_Trl_Component',
+                                   array('subroot'=>$this->getData()))
+            ->getChildComponent('-child')
+            ->getChildComponent('-view')->getChildComponent('-searchForm')
+            ->getComponent()->processInput($postData);
+    }
+
     public function getTemplateVars()
     {
         $ret = parent::getTemplateVars();
-        $ret['searchPage'] = Kwc_Chained_Trl_Component::getChainedByMaster($ret['searchPage'], $this->getData(), array('ignoreVisible'=>true));
+        $searchPage = Kwf_Component_Data_Root::getInstance()
+            ->getComponentByClass('Kwc_FulltextSearch_Search_Trl_Component',
+                                   array('subroot'=>$this->getData()))
+            ->getChildComponent('-child');
+        $ret['searchForm'] = $searchPage->getChildComponent('-view')
+            ->getChildComponent('-searchForm');
+        $ret['config']['searchTitle'] = $searchPage->getTitle();
+        $ret['config']['searchUrl'] = $searchPage->getAbsoluteUrl();
         return $ret;
     }
 }
