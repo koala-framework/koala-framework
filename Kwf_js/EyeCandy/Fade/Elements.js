7b2a367f2aea7011557fb64eb65ff61f0e7cfc88
Alexander Kaufmann <ak@vivid-planet.com>
2013-03-07 16:08:47 +0100

    check for this._template to override it easily in extended class

--- Kwf_js/EyeCandy/Fade/Elements.js
+++ Kwf_js/EyeCandy/Fade/Elements.js
@@ -236,8 +236,16 @@ Kwf.Fade.Elements.prototype = {
                 template += '<a class="elementAccessNext" href="#"></a>\n';
             }
 
-            this.template = new Ext.XTemplate(template);
-            this.template.append(this.selectorRoot);
+            if (this._template) {
+                if (typeof(this._template)=='string') {
+                    this._template = new Ext.XTemplate(this._template);
+                } else if (!(this._template instanceof Ext.XTemplate)) {
+                    throw trlKwf('Template has to be an Ext.XTemplate');
+                }
+            } else {
+                this._template = new Ext.XTemplate(template);
+            }
+            this._template.append(this.selectorRoot);
 
             var elementAccessLinks = Ext.get(this.selectorRoot).select('a.elementAccessLink', true);
             if (elementAccessLinks) {
