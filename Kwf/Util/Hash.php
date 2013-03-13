152e4a00d319ab55cb219ba8acc446ebe3dbc6e7
Niko Sams <ns@vivid-planet.com>
2013-03-13 10:05:40 +0100

    add hashPrivatePart setting to share salt across multiple web servers

--- Kwf/Util/Hash.php
+++ Kwf/Util/Hash.php
@@ -5,12 +5,16 @@ class Kwf_Util_Hash
     {
         $salt = Kwf_Cache_SimpleStatic::fetch('hashpp-');
         if (!$salt) {
-            $hashFile = 'cache/hashprivatepart';
-            if (!file_exists($hashFile)) {
-                file_put_contents($hashFile, time().rand(100000, 1000000));
+            if ($salt = Kwf_Config::getSetting('hashprivatepart')) {
+                //defined in config, required if multiple webservers should share the same salt
+            } else {
+                $hashFile = 'cache/hashprivatepart';
+                if (!file_exists($hashFile)) {
+                    file_put_contents($hashFile, time().rand(100000, 1000000));
+                }
+                $salt = file_get_contents($hashFile);
+                Kwf_Cache_SimpleStatic::add('hashpp-', $salt);
             }
-            $salt = file_get_contents($hashFile);
-            Kwf_Cache_SimpleStatic::add('hashpp-', $salt);
         }
         return md5($salt.$str);
     }
