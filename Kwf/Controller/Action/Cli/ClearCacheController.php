6a036391de2a8991c397c6f3e6c74e25777fb925
Niko Sams <ns@vivid-planet.com>
2013-03-13 14:13:18 +0100

    ClearCache: remove unused code and server option

--- Kwf/Controller/Action/Cli/ClearCacheController.php
+++ Kwf/Controller/Action/Cli/ClearCacheController.php
@@ -8,25 +8,7 @@ class Kwf_Controller_Action_Cli_ClearCacheController extends Kwf_Controller_Acti
 
     public function indexAction()
     {
-        if ($this->_getParam('server')) {
-            $config = Kwf_Config_Web::getInstance($this->_getParam('server'));
-
-            if ($config->server->useKwfForUpdate) {
-                $sshHost = $config->server->user.'@'.$config->server->host;
-                $sshDir = $config->server->dir;
-                $cmd = "sshvps $sshHost $sshDir clear-cache";
-                $cmd = "sudo -u vps $cmd";
-                $this->_systemCheckRet($cmd);
-            } else {
-                Kwf_Util_ClearCache::getInstance()->clearCache(
-                    $this->_getParam('type'),
-                    true,
-                    true,
-                    $config->server);
-            }
-        } else {
-            Kwf_Util_ClearCache::getInstance()->clearCache($this->_getParam('type'), true);
-        }
+        Kwf_Util_ClearCache::getInstance()->clearCache($this->_getParam('type'), true);
         exit;
     }
 
@@ -39,11 +21,6 @@ class Kwf_Controller_Action_Cli_ClearCacheController extends Kwf_Controller_Acti
                 'value'=> $types,
                 'valueOptional' => true,
                 'help' => 'what to clear'
-            ),
-            array(
-                'param'=> 'server',
-                'help' => 'server',
-                'allowBlank' => true
             )
         );
     }
