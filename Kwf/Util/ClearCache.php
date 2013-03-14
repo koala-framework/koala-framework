761691e133249aca05cf01210a7e7a2ed6c62b68
Niko Sams <ns@vivid-planet.com>
2013-03-14 13:08:28 +0100

    refactor getting other ec2 instances into resuable method

--- Kwf/Util/ClearCache.php
+++ Kwf/Util/ClearCache.php
@@ -264,35 +264,7 @@ class Kwf_Util_ClearCache
             } else {
                 $otherHostsTypes = array_intersect($otherHostsTypes, $types);
             }
-            $ec2 = new Kwf_Util_Aws_Ec2();
-            $r = $ec2->describe_instances(array(
-                'Filter' => array(
-                    array(
-                        'Name' => 'tag:application.id',
-                        'Value' => Kwf_Config::getValue('application.id'),
-                    ),
-                    array(
-                        'Name' => 'tag:config_section',
-                        'Value' => Kwf_Setup::getConfigSection(),
-                    )
-                )
-            ));
-            if (!$r->isOK()) {
-                throw new Kwf_Exception($r->body->asXml());
-            }
-
-            $ownHostname = file_get_contents('http://169.254.169.254/latest/meta-data/public-hostname');
-            $domains = array();
-            foreach ($r->body->reservationSet->item as $resItem) {
-                foreach ($resItem->instancesSet->item as $item) {
-                    $dnsName = (string)$item->dnsName;
-                    if ($dnsName && $dnsName != $ownHostname) {
-                        $domains[] = array(
-                            'domain'=>$dnsName,
-                        );
-                    }
-                }
-            }
+            $domains = Kwf_Util_Aws_Ec2_InstanceDnsNames::getOther();
             foreach ($domains as $domain) {
                 if ($output) {
                     echo "executing clear-cache on $domain:\n";
