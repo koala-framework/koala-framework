7075d8f2c7e3501d18cda03689252563ca6f04eb
Niko Sams <ns@vivid-planet.com>
2013-03-14 15:23:34 +0000

    read config setting correctly

--- Kwf/Util/Hash.php
+++ Kwf/Util/Hash.php
@@ -5,7 +5,7 @@ class Kwf_Util_Hash
     {
         $salt = Kwf_Cache_SimpleStatic::fetch('hashpp-');
         if (!$salt) {
-            if ($salt = Kwf_Config::getSetting('hashPrivatePart')) {
+            if ($salt = Kwf_Config::getValue('hashPrivatePart')) {
                 //defined in config, required if multiple webservers should share the same salt
             } else {
                 $hashFile = 'cache/hashprivatepart';
