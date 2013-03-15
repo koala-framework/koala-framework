f9ee818778ef64f4dbc97306f4ebe85e0b5df336
Niko Sams <ns@vivid-planet.com>
2013-03-15 10:41:29 +0100

    add Advanced_DynamicContent component that allows output of uncached content without having to disable viewCache

---
+++ Kwc/Advanced/DyamicContent/Component.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Component that allows dynamic content without having to disable view cache.
+ */
+abstract class Kwc_Advanced_DyamicContent_Component extends Kwc_Abstract
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['plugins'][] = 'Kwc_Advanced_DyamicContent_Plugin';
+        return $ret;
+    }
+
+    public function getTemplateVars()
+    {
+        $ret = parent::getTemplateVars();
+        $ret['dynamicPlaceholder'] = '{dynamicContent '.$this->getData()->componentClass.'}';
+        return $ret;
+    }
+
+    abstract public static function getDynamicContent($componentId, $componentClass);
+}
