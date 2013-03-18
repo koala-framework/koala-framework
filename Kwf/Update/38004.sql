0a101020e45ec7d084919fd67303455db36c2569
Niko Sams <ns@vivid-planet.com>
2013-03-18 15:59:37 +0100

    fix typo in update script

--- Kwf/Update/38004.sql
+++ Kwf/Update/38004.sql
@@ -1,4 +1,4 @@
 CREATE TABLE  `kwf_update` (
-    `date` TEXT CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
+    `data` TEXT CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
 ) ENGINE = INNODB;
 INSERT INTO  `kwf_update` (`data`) VALUES ('');
