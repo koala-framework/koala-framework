0a6a9c7e1f2fcd447a442d8de1b4bc90b620c509
Mario Kemetinger <mk@vivid-planet.com>
2013-03-19 14:02:01 +0100

    Class "Small" for imageEnlargeIcon when imageWidth is under 200px

--- Kwc/Basic/ImageEnlarge/Component.js
+++ Kwc/Basic/ImageEnlarge/Component.js
@@ -2,5 +2,8 @@ Kwf.onElementReady('.kwcBasicImageEnlarge.showHoverIcon > a', function(el) {
     var img = el.child('img').dom;
     if (img.width > 50 && img.height > 50) {
         el.createChild({ tag: 'span', cls: 'outerHoverIcon', html: '<span class="innerHoverIcon"></span>'});
+        if (img.width < 200) {
+            el.addClass('small');
+        }
     }
 });
