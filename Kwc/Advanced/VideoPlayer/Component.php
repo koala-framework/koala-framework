74619cc52c939e11575d55467f218b6067b002f2
Michael Freudenthaler <mf@vivid-planet.com>
2013-03-28 16:07:13 +0100

    initial commit for videoplayer component

---
+++ Kwc/Advanced/VideoPlayer/Component.php
@@ -0,0 +1,27 @@
+<?php
+class Kwc_Advanced_VideoPlayer_Component extends Kwc_Abstract_Composite_Component
+    implements Kwf_Media_Output_IsValidInterface
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret = array_merge(parent::getSettings(), array(
+            'ownModel'     => 'Kwc_Advanced_VideoPlayer_Model',
+            'componentName' => trlKwfStatic('Video'),
+            'componentIcon' => new Kwf_Asset('images'),
+            'extConfig' => 'Kwf_Component_Abstract_ExtConfig_Form'
+        ));
+//         $ret['dataClass'] = 'Kwc_Basic_DownloadTag_Data';
+        $ret['assetsAdmin']['dep'][] = 'KwfFormFile';
+        return $ret;
+    }
+    public static function isValidMediaOutput($id, $type, $className)
+    {
+        return true;
+    }
+
+    public static function getMediaOutput($id, $type, $className)
+    {
+        return true;
+    }
+}
