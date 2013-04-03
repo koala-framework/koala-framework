edba8fb79a7c36a871dbdd4b952374751cfbe02b
Franz Unger <ufx@vivid-planet.com>
2013-04-03 11:08:13 +0200

    Support trailing whitespaces in Filter ComboBox

--- Kwf_js/Auto/Filter/ComboBox.js
+++ Kwf_js/Auto/Filter/ComboBox.js
@@ -5,26 +5,42 @@ Kwf.Auto.Filter.ComboBox = function(config)
     if (config.field) {
         this.combo = Ext.ComponentMgr.create(config.field);
     } else {
-        var record = Ext.data.Record.create(['id', 'name']);
+        var record = Ext.data.Record.create(['id', 'name', 'displayname']);
+        var filterData = config.data;
+        for (var i in filterData) {
+            filterData[i][2] = filterData[i][1];
+            if (filterData[i][1]) {
+                var a = [];
+                while (filterData[i][1].substr(a.length, 1) == ' ') {
+                    a.push('&nbsp;');
+                }
+                filterData[i][1] = a.join('') + filterData[i][1].substr(a.length);
+            }
+        }
         var filterStore = new Ext.data.Store({
             reader: new Ext.data.ArrayReader({}, record),
-            data: config.data
+            data: filterData
         });
         if (!config['default'] && filterStore.find('id', 0) == -1) {
             filterStore.insert(0, [new record({id: 0, name: config['defaultText'] ? config['defaultText'] : trlKwf('all')})]);
             config['default'] = 0;
         }
         this.combo = new Ext.form.ComboBox({
-                store: filterStore,
-                displayField: 'name',
-                valueField: 'id',
-                mode: 'local',
-                triggerAction: 'all',
-                editable: config.editable || false,
-                forceSelection: true,
-                width: config.width || 200,
-                listWidth: config.listWidth
-            });
+            store: filterStore,
+            displayField: 'displayname',
+            valueField: 'id',
+            mode: 'local',
+            triggerAction: 'all',
+            editable: config.editable || false,
+            forceSelection: true,
+            width: config.width || 200,
+            listWidth: config.listWidth,
+            tpl: new Ext.XTemplate(
+                '<tpl for=".">',
+                    '<div class="x-combo-list-item">{name}</div>',
+                '</tpl>'
+            )
+        });
     }
     this.combo.setValue(config['default']);
     this.combo.on('select', function() {
