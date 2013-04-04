62d9d3cfb2c9e7ad5406e3f8a785d79a787b3bfe
Benjamin Hohenwarter <bh@vivid-planet.com>
2013-04-04 15:11:19 +0200

    Fix setup for table trl test

--- tests/Kwc/Trl/Table/Root/Master.php
+++ tests/Kwc/Trl/Table/Root/Master.php
@@ -1,5 +1,5 @@
 <?php
-class Kwc_Trl_Table_Root_Master extends Kwc_Abstract
+class Kwc_Trl_Table_Root_Master extends Kwc_Root_TrlRoot_Master_Component
 {
     public static function getSettings()
     {
@@ -9,6 +9,9 @@ class Kwc_Trl_Table_Root_Master extends Kwc_Abstract
             'component' => 'Kwc_Trl_Table_Table_Component',
             'name' => 'table',
         );
+        unset($ret['generators']['flag']);
+        unset($ret['generators']['box']);
+        unset($ret['generators']['category']);
         return $ret;
     }
 }
