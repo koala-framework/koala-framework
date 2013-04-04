f0d2acfcca59cda7c5588e4cdc9952be9d38d448
Benjamin Hohenwarter <bh@vivid-planet.com>
2013-04-04 15:24:16 +0200

    Remove unused function, return correct id
    If id is requested master_id is needed

--- Kwc/Basic/Table/Trl/RowData.php
+++ Kwc/Basic/Table/Trl/RowData.php
@@ -1,16 +1,12 @@
 <?php
 class Kwc_Basic_Table_Trl_RowData extends Kwf_Model_Db_Row
 {
-    public function getReplacedContent($field)
-    {
-        return Kwc_Basic_Table_RowData::replaceContent($this->$field);
-    }
-
     public function __get($name)
     {
-        $value = parent::__get($name);
-        if (!$value) {
-            //FIXME load from master-row
+        if ($name == 'id') {
+            $value = parent::__get('master_id');
+        } else {
+            $value = parent::__get($name);
         }
         return $value;
     }
