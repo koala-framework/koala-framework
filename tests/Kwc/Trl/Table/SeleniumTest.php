d1fad34bf12709e4feed2c8a56c19bcad76c53c1
Benjamin Hohenwarter <bh@vivid-planet.com>
2013-04-05 11:57:48 +0200

    Add selenium test to check if admin opens
    Dependencies have to be set correctly

---
+++ tests/Kwc/Trl/Table/SeleniumTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @group slow
+ * @group selenium
+ * @group Table
+ */
+class Kwc_Trl_Table_SeleniumTest extends Kwf_Test_SeleniumTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        Kwf_Component_Data_Root::setComponentClass('Kwc_Trl_Table_Root_Root');
+    }
+
+    public function testAdmin()
+    {
+        $this->markTestIncomplete('Dependencies not correctly set for selenium test');
+        $this->openKwcEdit('Kwc_Trl_Table_Table_Trl_Component.Kwc_Trl_Table_Table_Component', 'root-en_table');
+        $this->waitForConnections();
+    }
+}
