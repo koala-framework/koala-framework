22c69a979479e339825697e0916a9dbbecf2a129
Benjamin Hohenwarter <bh@vivid-planet.com>
2013-04-08 08:10:24 +0200

    Set correct dependencies

--- tests/Kwc/Trl/Table/SeleniumTest.php
+++ tests/Kwc/Trl/Table/SeleniumTest.php
@@ -14,7 +14,6 @@ class Kwc_Trl_Table_SeleniumTest extends Kwf_Test_SeleniumTestCase
 
     public function testAdmin()
     {
-        $this->markTestIncomplete('Dependencies not correctly set for selenium test');
         $this->openKwcEdit('Kwc_Trl_Table_Table_Trl_Component.Kwc_Trl_Table_Table_Component', 'root-en_table');
         $this->waitForConnections();
     }
