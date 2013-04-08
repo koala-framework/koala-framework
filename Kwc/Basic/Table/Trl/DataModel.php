d2bd91617bd8184665d2a950ceeb9cce718fae82
Benjamin Hohenwarter <bh@vivid-planet.com>
2013-04-08 11:56:53 +0200

    Remove unused function

--- Kwc/Basic/Table/Trl/DataModel.php
+++ Kwc/Basic/Table/Trl/DataModel.php
@@ -9,9 +9,4 @@ class Kwc_Basic_Table_Trl_DataModel extends Kwf_Model_Db
         $this->_siblingModels[] = new Kwf_Model_Field(array('fieldName'=>'data'));
         parent::_init();
     }
-
-    public function getRows($where = array(), $order=null, $limit=null, $start=null)
-    {
-        return parent::getRows($where, $order, $limit, $start);
-    }
 }
