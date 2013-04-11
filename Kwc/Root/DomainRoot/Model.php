7e1998c0d66e0556f9636cef61c844a95cf5f55b
Alexander Kaufmann <ak@vivid-planet.com>
2013-04-11 09:03:30 +0200

    deleted previewDomain code

--- Kwc/Root/DomainRoot/Model.php
+++ Kwc/Root/DomainRoot/Model.php
@@ -2,7 +2,7 @@
 class Kwc_Root_DomainRoot_Model extends Kwf_Model_Data_Abstract
 {
     private $_domains;
-    protected $_columns = array('id', 'name', 'domain', 'preview_domain', 'component', 'pattern');
+    protected $_columns = array('id', 'name', 'domain', 'component', 'pattern');
     protected $_toStringField = 'name';
 
     public function __construct($config = array())
