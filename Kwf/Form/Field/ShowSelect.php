f0e158197d25473877190dd05ad9f839d4b622ed
Michael Freudenthaler <mf@vivid-planet.com>
2013-04-11 13:04:52 +0200

    prevent error if field is not set

--- Kwf/Form/Field/ShowSelect.php
+++ Kwf/Form/Field/ShowSelect.php
@@ -18,7 +18,11 @@ class Kwf_Form_Field_ShowSelect extends Kwf_Form_Field_ShowField
                     }
                 }
             } else if (is_array($data)) {
-                $ret[$this->getName()] = $data[$ret[$this->getName()]];
+                if (!$ret[$this->getName()]) {
+                    $ret[$this->getName()] = null;
+                } else {
+                    $ret[$this->getName()] = $data[$ret[$this->getName()]];
+                }
             }
         } else {
             $reference = $this->getReference();
