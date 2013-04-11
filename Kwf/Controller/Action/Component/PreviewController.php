873ceb06bf8a9fa04049911e58527c6ae562a7fa
Alexander Kaufmann <ak@vivid-planet.com>
2013-04-11 08:25:35 +0200

    added redirectAction for frontend preview requests to check if logged in

--- Kwf/Controller/Action/Component/PreviewController.php
+++ Kwf/Controller/Action/Component/PreviewController.php
@@ -5,4 +5,11 @@ class Kwf_Controller_Action_Component_PreviewController extends Kwf_Controller_A
     {
         $this->view->xtype = 'kwf.component.preview';
     }
+
+    public function redirectAction()
+    {
+        if (!$this->_getParam('url')) throw new Kwf_Exception('No redirect url param found');
+        header('Location: ' . urldecode($this->_getParam('url')));
+        exit;
+    }
 }
