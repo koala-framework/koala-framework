3ebc89f0455122b58f229705066a4ea29c7c5c4e
Niko Sams <ns@vivid-planet.com>
2013-04-12 08:24:32 +0200

    increase cache setup version because of Kwf_Util_SessionHandler

--- Kwf/Setup.php
+++ Kwf/Setup.php
@@ -24,7 +24,7 @@ class Kwf_Setup
 {
     public static $configClass;
     public static $configSection;
-    const CACHE_SETUP_VERSION = 1; //increase version if incompatible changes to generated file are made
+    const CACHE_SETUP_VERSION = 2; //increase version if incompatible changes to generated file are made
 
     public static function setUp($configClass = 'Kwf_Config_Web')
     {
