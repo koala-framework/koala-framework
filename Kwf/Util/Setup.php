9eb667c6ed7b403ca2a2ca6c2dce952675bd2b7e
Niko Sams <ns@vivid-planet.com>
2013-04-19 07:37:55 +0200

    don't initialize multibyte and iconv extensions if they are not loaded

--- Kwf/Util/Setup.php
+++ Kwf/Util/Setup.php
@@ -125,8 +125,12 @@ class Kwf_Util_Setup
         $ret .= "ini_set('memory_limit', '128M');\n";
         $ret .= "error_reporting(E_ALL);\n";
         $ret .= "date_default_timezone_set('Europe/Berlin');\n";
-        $ret .= "mb_internal_encoding('UTF-8');\n";
-        $ret .= "iconv_set_encoding('internal_encoding', 'utf-8');\n";
+        if (function_exists('mb_internal_encoding')) {
+            $ret .= "mb_internal_encoding('UTF-8');\n";
+        }
+        if (function_exists('iconv_set_encoding')) {
+            $ret .= "iconv_set_encoding('internal_encoding', 'utf-8');\n";
+        }
         $ret .= "set_error_handler(array('Kwf_Debug', 'handleError'), E_ALL);\n";
         $ret .= "set_exception_handler(array('Kwf_Debug', 'handleException'));\n";
         $ret .= "umask(000); //nicht 002 weil wwwrun und kwcms in unterschiedlichen gruppen\n";
