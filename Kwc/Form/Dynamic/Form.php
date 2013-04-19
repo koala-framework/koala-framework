87ca5730b8b6662e93dd9c6eb87c36edf1a9d0bd
Niko Sams <ns@vivid-planet.com>
2013-04-19 07:51:08 +0200

    set width for inputs, they are too narrow currently

--- Kwc/Form/Dynamic/Form.php
+++ Kwc/Form/Dynamic/Form.php
@@ -6,10 +6,13 @@ class Kwc_Form_Dynamic_Form extends Kwc_Abstract_Form
         parent::_initFields();
         $fs = new Kwf_Form_Container_FieldSet(trlKwf('Form Properties'));
         $fs->add(new Kwf_Form_Field_TextField('recipient', trlKwf('E-Mail Recipient')))
-            ->setVtype('email');
+            ->setVtype('email')
+            ->setWidth(400);
         $fs->add(new Kwf_Form_Field_TextField('recipient_cc', trlKwf('E-Mail CC')))
-            ->setVtype('email');
-        $fs->add(new Kwf_Form_Field_TextField('subject', trlKwf('E-Mail Subject')));
+            ->setVtype('email')
+            ->setWidth(400);
+        $fs->add(new Kwf_Form_Field_TextField('subject', trlKwf('E-Mail Subject')))
+            ->setWidth(400);
         $this->add($fs);
         $this->add(Kwc_Abstract_Form::createChildComponentForm($this->getClass(), '-form'));
     }
