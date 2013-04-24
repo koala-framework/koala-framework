f9fd56dca63742c5b159732f2b282745ef706973
Alexander Kaufmann <ak@vivid-planet.com>
2013-04-24 17:28:00 +0200

    added new item directory Kwc/Directories/Item/DirectoryNoAdmin/Component
    if you want to use an directory only for frontend
    moved some code into the new component(directory detail, view, getDirectory functions)

---
+++ Kwc/Directories/Item/DirectoryNoAdmin/Component.php
@@ -0,0 +1,25 @@
+<?php
+abstract class Kwc_Directories_Item_DirectoryNoAdmin_Component extends Kwc_Directories_List_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['generators']['detail'] = array(
+            'class' => 'Kwf_Component_Generator_Table',
+            'component' => 'Kwc_Directories_Item_Detail_Component'
+        );
+        $ret['generators']['child']['component']['view'] = 'Kwc_Directories_List_View_Component';
+        $ret['useDirectorySelect'] = false;
+        return $ret;
+    }
+
+    public static function getItemDirectoryClasses($directoryClass)
+    {
+        return array($directoryClass);
+    }
+
+    protected function _getItemDirectory()
+    {
+        return $this->getData();
+    }
+}
