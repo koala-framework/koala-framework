737ca9eb3a294c2234c05b725c1a7e2fb62cf5f3
Alexander Kaufmann <ak@vivid-planet.com>
2013-04-25 09:01:11 +0200

    added new Directory List View -> Kwc/Directories/List/ViewGrouping/Component
    now it is possible to group every partial

---
+++ Kwc/Directories/List/ViewGrouping/Component.php
@@ -0,0 +1,21 @@
+<?php
+class Kwc_Directories_List_ViewGrouping_Component extends Kwc_Directories_Item_DirectoryNoAdmin_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['generators']['detail']['component'] = 'Kwc_Directories_List_ViewGrouping_Group_Component';
+        $ret['dependentModel'] = false;
+        return $ret;
+    }
+
+    public function getSelect()
+    {
+        $ret = parent::getSelect();
+        $select = $this->getData()->parent->getGenerator('detail')->getFormattedSelect($this->getData()->parent);
+        if (!$this->_getSetting('dependentModel')) throw new Kwf_Exception('Set dependentModel for correct grouping');
+        $ret->where(new Kwf_Model_Select_Expr_Child_Contains($this->_getSetting('dependentModel'), $select));
+        $ret->order('pos', 'ASC');
+        return $ret;
+    }
+}
