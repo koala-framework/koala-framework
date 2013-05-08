14b461bbb8b64b04bcc6f6afb7075849cb31d6b3
Niko Sams <ns@vivid-planet.com>
2013-05-08 13:43:34 +0200

    make placeholder text changeable and correclty implement trl

--- Kwf/Form/Field/DateField.php
+++ Kwf/Form/Field/DateField.php
@@ -12,10 +12,18 @@ class Kwf_Form_Field_DateField extends Kwf_Form_Field_SimpleAbstract
     public function __construct($field_name = null, $field_label = null)
     {
         parent::__construct($field_name, $field_label);
+        $this->setPlaceholderText(trlKwfStatic('yyyy-mm-dd'));
         $this->setXtype('datefield');
     }
 
     //setHideDatePicker in Frontend only
+
+    protected function _getTrlProperties()
+    {
+        $ret = parent::_getTrlProperties();
+        $ret[] = 'placeholderText';
+        return $ret;
+    }
 
     protected function _processLoaded($v)
     {
@@ -34,7 +42,7 @@ class Kwf_Form_Field_DateField extends Kwf_Form_Field_SimpleAbstract
     protected function _getValueFromPostData($postData)
     {
         $ret = parent::_getValueFromPostData($postData);
-        if ($ret == trlKwf('yyyy-mm-dd')) return null;
+        if ($ret == $this->getPlaceholderText()) return null;
         if (!$ret) return null;
 
         //format das von ext kommt
@@ -65,11 +73,11 @@ class Kwf_Form_Field_DateField extends Kwf_Form_Field_SimpleAbstract
     {
         $name = $this->getFieldName();
         $value = $this->_getOutputValueFromValues($values);
-        if (!$value) $value = trlKwf('yyyy-mm-dd');
+        if (!$value) $value = $this->getPlaceholderText();
         $ret = parent::getTemplateVars($values, $fieldNamePostfix, $idPrefix);
 
         $class = '';
-        if ($value != trlKwf('yyyy-mm-dd')) {
+        if ($value != $this->getPlaceholderText()) {
             $v = strtotime($value);
             if ($v) $value = date(trlKwf('Y-m-d'), $v);
         } else {
