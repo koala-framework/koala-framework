b9533abdab49deb15b61776246d796071d62ea2b
Niko Sams <ns@vivid-planet.com>
2013-05-13 13:43:39 +0200

    fix news trl form

--- Kwc/News/Detail/Trl/Form.php
+++ Kwc/News/Detail/Trl/Form.php
@@ -4,7 +4,7 @@ class Kwc_News_Detail_Trl_Form extends Kwc_News_Detail_Abstract_Trl_Form
     protected function _init()
     {
         parent::_init();
-        $detail = Kwc_Abstract::getChildComponentClass($this->getDirectoryClass(), 'detail');
+        $detail = $this->getClass();
         $this->add(Kwc_Abstract_Form::createChildComponentForm($detail, '-image', 'image'));
     }
 }
