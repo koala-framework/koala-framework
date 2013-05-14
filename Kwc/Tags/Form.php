a4d007d587bc4580eba5bc2a580ad75d053659eb
Franz Unger <ufx@vivid-planet.com>
2013-05-14 07:23:55 +0200

    Form extends Kwc_Abstract_Form to use correct component class

--- Kwc/Tags/Form.php
+++ Kwc/Tags/Form.php
@@ -1,12 +1,12 @@
 <?php
-class Kwc_Tags_Form extends Kwf_Form
+class Kwc_Tags_Form extends Kwc_Abstract_Form
 {
     protected $_model = 'Kwc_Tags_ComponentModel';
     protected function _initFields()
     {
         $this->setCreateMissingRow(true);
         parent::_initFields();
-        $tagsControllerUrl = Kwc_Abstract_Admin::getInstance('Kwc_Tags_Component')->getControllerUrl();
+        $tagsControllerUrl = Kwc_Abstract_Admin::getInstance($this->getClass())->getControllerUrl();
         $this->add(new Kwf_Form_Field_SuperBoxSelect('ComponentToTag', 'Tag', trlKwf('Tags')))
             ->setWidth(300)
             ->setListWidth(300)
