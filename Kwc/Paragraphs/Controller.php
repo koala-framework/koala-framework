0f5255a86d8a51abf90f166d40ae65eea170f177
Niko Sams <ns@vivid-planet.com>
2013-05-14 11:17:14 +0200

    fix pasting whole paragraph that is not a page

--- Kwc/Paragraphs/Controller.php
+++ Kwc/Paragraphs/Controller.php
@@ -139,7 +139,11 @@ class Kwc_Paragraphs_Controller extends Kwf_Controller_Action_Auto_Kwc_Grid
             $c = $c->parent;
         }
 
-        if ($source->isPage && is_instance_of($source->componentClass, 'Kwc_Paragraphs_Component')) {
+        $sourceIsParagraphs = is_instance_of($source->componentClass, 'Kwc_Paragraphs_Component');
+        if ($source->isPage && $sourceIsParagraphs) {
+            //a whole paragraphs component is in clipboard
+            $sources = $source->getChildComponents(array('generator'=>'paragraphs', 'ignoreVisible'=>true));
+        } else if (!$source->isPage && !is_instance_of($source->parent->componentClass, 'Kwc_Paragraphs_Component')) {
             //a whole paragraphs component is in clipboard
             $sources = $source->getChildComponents(array('generator'=>'paragraphs', 'ignoreVisible'=>true));
         } else {
