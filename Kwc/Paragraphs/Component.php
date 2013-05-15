1d3e20224d38c8a9e83689b7d6c47cc7f6f27d68
Alexander Kaufmann <ak@vivid-planet.com>
2013-05-15 11:36:40 +0200

    reverted commit 5d93a24bb37c1750aeaa9df23c2e9e7b2606d58b, config setting name is ok

--- Kwc/Paragraphs/Component.php
+++ Kwc/Paragraphs/Component.php
@@ -32,7 +32,7 @@ class Kwc_Paragraphs_Component extends Kwc_Abstract
         }
         $ret['showCopyPaste'] = true;
         $ret['extConfig'] = 'Kwc_Paragraphs_ExtConfig';
-        $ret['useMobileBreakpoints'] = Kwf_Config::getValue('kwc.useMobileBreakpoints');
+        $ret['useMobileBreakpoints'] = Kwf_Config::getValue('kwc.mobileBreakpoints');
         return $ret;
     }
 
