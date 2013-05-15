645c5794ed39f157bb773206d7fef4d072df4155
Niko Sams <ns@vivid-planet.com>
2013-05-15 09:40:01 +0200

    only remove submit if exists

--- Kwf_js/FrontendForm/Cards.js
+++ Kwf_js/FrontendForm/Cards.js
@@ -2,7 +2,7 @@ Kwf.FrontendForm.Cards = Ext.extend(Kwf.FrontendForm.Field, {
     initField: function() {
         var config = this.form.getFieldConfig(this.getFieldName());
         var combobox = this.form.findField(config.combobox);
-        combobox.el.child('input.submit').remove(); //remove non-js fallback
+        combobox.el.select('input.submit').remove(); //remove non-js fallback
         combobox.on('change', function(value) {
             this.el.select('.kwfFormContainerCard .kwfFormCard').addClass('inactive');
             this.el.select('.kwfFormContainerCard.'+value+' .kwfFormCard').removeClass('inactive');
