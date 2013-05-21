903fb88c1a7ad583ac503810ec1c26afc0e0d0fa
Franz Unger <ufx@vivid-planet.com>
2013-05-21 11:40:54 +0200

    Set feAutologin-cookie for domain
    Was broken since using Ajax-Request for login because was /admin/component/edit/Login_Form_Component/FrontendForm/ used as cookie-directory

--- Kwc/User/Login/Form/Component.php
+++ Kwc/User/Login/Form/Component.php
@@ -56,7 +56,7 @@ class Kwc_User_Login_Form_Component extends Kwc_Form_Component
             $authedUser = Kwf_Registry::get('userModel')->getKwfModel()->getAuthedUser();
             if ($row->auto_login) {
                 $cookieValue = $authedUser->id.'.'.md5($authedUser->password);
-                setcookie('feAutologin', $cookieValue, time() + (100*24*60*60));
+                setcookie('feAutologin', $cookieValue, time() + (100*24*60*60), '/');
             }
             $this->_afterLogin($authedUser);
         } else {
