bee58cb0231507da8a6899c746149649093ec355
Niko Sams <niko.sams@gmail.com>
2013-05-21 21:47:32 +0200

    call update-downloader in background process

--- Kwf/Controller/Action/Cli/Web/MaintenanceController.php
+++ Kwf/Controller/Action/Cli/Web/MaintenanceController.php
@@ -66,4 +66,88 @@ class Kwf_Controller_Action_Cli_Web_MaintenanceController extends Kwf_Controller
         echo json_encode($out);
         exit;
     }
+
+    public function downloadUpdatesAction()
+    {
+        $urls = array();
+        if ($this->_getParam('libraryUrl')) {
+            $urls['library.tar.gz'] = $this->_getParam('libraryUrl');
+        }
+        $urls['kwf.tar.gz'] = $this->_getParam('kwfUrl');
+        $urls['app.tar.gz'] = $this->_getParam('appUrl');
+
+        $c = new Kwf_Util_ProgressBar_Adapter_Cache($this->_getParam('progressNum'));
+        $progress = new Zend_ProgressBar($c, 0, 6);
+
+        foreach ($urls as $target=>$url) {
+            $progress->next(1, 'Downloading '.$target);
+            exec("wget -O ".escapeshellarg($target)." ".escapeshellarg($url)." 2>&1", $out, $ret);
+            if ($ret) {
+                throw new Kwf_Exception_Client("Download Failed:".implode("\n", $out));
+            }
+        }
+
+        foreach ($urls as $target=>$url) {
+            $progress->next(1, 'Extracting '.$target);
+
+            $dir = tempnam('.', 'downloader');
+            unlink($dir);
+            mkdir($dir);
+            exec("tar xfz $target -C $dir"." 2>&1", $out, $ret);
+            if ($ret) {
+                throw new Kwf_Exception_Client("Extraction failed");
+            }
+            $dirs = glob("$dir/*");
+            if (count($dirs) != 1) {
+                throw new Kwf_Exception_Client("more than one directory extracted");
+            }
+            if (!is_dir($dirs[0])) {
+                throw new Kwf_Exception_Client("no directory extracted");
+            }
+            if ($target == 'app.tar.gz') {
+                //move away current files to backup
+                $backupDir = uniqid("backup").'app';
+                mkdir($backupDir);
+                $it = new DirectoryIterator(getcwd());
+                foreach($it as $file) {
+                    if($file->isDot()) continue;
+                    if (substr($file->getFilename(), -4) == '-lib' || $file->getFilename() == 'library') continue;
+                    if (substr($file->getFilename(), 0, 6) == 'backup') continue;
+                    if ($file->getFilename() == 'config.local.ini') continue;
+                    if ($file->getFilename() == $backupDir) continue;
+                    if ($file->getPathname() == $dir) continue;
+                    if ($file->getFilename() == 'app.tar.gz') continue;
+                    rename($file->getPathname(), $backupDir.'/'.$file->getFilename());
+                }
+
+                //move in new files
+                $it = new DirectoryIterator($dirs[0]);
+                foreach ($it as $file) {
+                    if($file->isDot()) continue;
+                    rename($file->getPathname(), './'.$file->getFilename());
+                }
+                rmdir("$dirs[0]");
+
+            } else {
+                $targetDir = false;
+                if ($target == 'library.tar.gz') {
+                    $targetDir = 'library';
+                } else if ($target == 'kwf.tar.gz') {
+                    $targetDir = 'kwf-lib';
+                    copy($targetDir.'/include_path', $dirs[0].'/include_path');
+                }
+                rename($targetDir, uniqid("backup").$targetDir);
+                rename("$dirs[0]",  $targetDir);
+            }
+            rmdir($dir);
+            unlink($target);
+        }
+
+
+        $out = array(
+            'success' => true
+        );
+        echo json_encode($out);
+        exit;    
+    }
 }
--- Kwf/Controller/Action/Maintenance/UpdateDownloaderController.php
+++ Kwf/Controller/Action/Maintenance/UpdateDownloaderController.php
@@ -25,78 +25,14 @@ class Kwf_Controller_Action_Maintenance_UpdateDownloaderController extends Kwf_C
             throw new Kwf_Exception_Client("Following directories are not writeable for ".`whoami`." user<br />".implode("<br />", $errors));
         }
 
-        $urls = array();
+        $cmd = "php bootstrap.php maintenance download-updates ";
+        $cmd .= " --progressNum=".escapeshellarg($this->_getParam('progressNum'));
         if ($this->_getParam('libraryUrl')) {
-            $urls['library.tar.gz'] = $this->_getParam('libraryUrl');
+            $cmd .= " --libraryUrl=".escapeshellarg($this->_getParam('libraryUrl'));
         }
-        $urls['kwf.tar.gz'] = $this->_getParam('kwfUrl');
-        $urls['app.tar.gz'] = $this->_getParam('appUrl');
-        
-        $c = new Kwf_Util_ProgressBar_Adapter_Cache($this->_getParam('progressNum'));
-        $progress = new Zend_ProgressBar($c, 0, 6);
-
-        foreach ($urls as $target=>$url) {
-            $progress->next(1, 'Downloading '.$target);
-            exec("wget -O ".escapeshellarg($target)." ".escapeshellarg($url)." 2>&1", $out, $ret);
-            if ($ret) {
-                throw new Kwf_Exception_Client("Download Failed:".implode("\n", $out));
-            }
-        }
-
-        foreach ($urls as $target=>$url) {
-            $progress->next(1, 'Extracting '.$target);
-
-            $dir = tempnam('.', 'downloader');
-            unlink($dir);
-            mkdir($dir);
-            exec("tar xfz $target -C $dir"." 2>&1", $out, $ret);
-            if ($ret) {
-                throw new Kwf_Exception_Client("Extraction failed");
-            }
-            $dirs = glob("$dir/*");
-            if (count($dirs) != 1) {
-                throw new Kwf_Exception_Client("more than one directory extracted");
-            }
-            if (!is_dir($dirs[0])) {
-                throw new Kwf_Exception_Client("no directory extracted");
-            }
-            if ($target == 'app.tar.gz') {
-                //move away current files to backup
-                $backupDir = uniqid("backup").'app';
-                mkdir($backupDir);
-                $it = new DirectoryIterator(getcwd());
-                foreach($it as $file) {
-                    if($file->isDot()) continue;
-                    if (substr($file->getFilename(), -4) == '-lib' || $file->getFilename() == 'library') continue;
-                    if (substr($file->getFilename(), 0, 6) == 'backup') continue;
-                    if ($file->getFilename() == 'config.local.ini') continue;
-                    if ($file->getFilename() == $backupDir) continue;
-                    if ($file->getPathname() == $dir) continue;
-                    if ($file->getFilename() == 'app.tar.gz') continue;
-                    rename($file->getPathname(), $backupDir.'/'.$file->getFilename());
-                }
-
-                //move in new files
-                $it = new DirectoryIterator($dirs[0]);
-                foreach ($it as $file) {
-                    if($file->isDot()) continue;
-                    rename($file->getPathname(), './'.$file->getFilename());
-                }
-                rmdir("$dirs[0]");
-
-            } else {
-                $targetDir = false;
-                if ($target == 'library.tar.gz') {
-                    $targetDir = 'library';
-                } else if ($target == 'kwf.tar.gz') {
-                    $targetDir = 'kwf-lib';
-                    copy($targetDir.'/include_path', $dirs[0].'/include_path');
-                }
-                rename($targetDir, uniqid("backup").$targetDir);
-                rename("$dirs[0]",  $targetDir);
-            }
-            rmdir($dir);
-            unlink($target);
-        }
+        $cmd .= " --kwfUrl=".escapeshellarg($this->_getParam('kwfUrl'));
+        $cmd .= " --appUrl=".escapeshellarg($this->_getParam('appUrl'));
+        $procData = Kwf_Util_BackgroundProcess::start($cmd, $this->view);
+        $this->view->assign($procData);
     }
 }
