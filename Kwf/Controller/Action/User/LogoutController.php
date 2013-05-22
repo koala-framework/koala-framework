714cc963f4715f775c43d9a883cfe39df9b0749e
Franz Unger <ufx@vivid-planet.com>
2013-05-22 12:27:16 +0200

    Set autologin-cookie always for whole domain

--- Kwf/Controller/Action/User/LogoutController.php
+++ Kwf/Controller/Action/User/LogoutController.php
@@ -4,7 +4,7 @@ class Kwf_Controller_Action_User_LogoutController extends Kwf_Controller_Action
     public function indexAction()
     {
         Kwf_Auth::getInstance()->clearIdentity();
-        setcookie('feAutologin', '', time() - 3600);
+        setcookie('feAutologin', '', time() - 3600, '/');
         $url = $this->_getParam('redirect') ? $this->_getParam('redirect') : '/';
         header('Location: ' . $url);
         exit;
