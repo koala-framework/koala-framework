9196344804de9cf01ae0bbff21e72b89fb32978c
Niko Sams <ns@vivid-planet.com>
2013-05-22 15:43:59 +0200

    TitleEditable: dynamic form based on position in component tree

--- Kwc/Box/TitleEditable/Admin.php
+++ Kwc/Box/TitleEditable/Admin.php
@@ -1,8 +1,21 @@
 <?php
 class Kwc_Box_TitleEditable_Admin extends Kwc_Abstract_Admin
 {
-    public function getPagePropertiesForm()
+    public function getPagePropertiesForm($config)
     {
-        return new Kwc_Box_TitleEditable_Form(null, $this->_class);
+        $c = $config['component'];
+        if ($config['mode'] == 'add' || $c->isPage) {
+            //pages can have their own title
+            return new Kwc_Box_TitleEditable_Form(null, $this->_class);
+        } else if (Kwc_Abstract::getFlag($c->componentClass, 'subroot') || $c->componentId == 'root') {
+            //subroots/root can have title for child pages
+            $ret = new Kwc_Box_TitleEditable_Form(null, $this->_class);
+            $ret->fields['title']->setHelpText(null);
+            $ret->fields['title']->setComment(trlKwf('for child pages'));
+            return $ret;
+        }
+
+        //others have no title
+        return null;
     }
 }
