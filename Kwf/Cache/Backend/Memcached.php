5b51a7c5ab0851ae6e6f1db9009e2226eca5882f
Niko Sams <ns@vivid-planet.com>
2013-05-23 16:29:05 +0200

    don't md5 all cache ids, no real reason for that

--- Kwf/Cache/Backend/Memcached.php
+++ Kwf/Cache/Backend/Memcached.php
@@ -186,7 +186,7 @@ class Kwf_Cache_Backend_Memcached extends Zend_Cache_Backend_Memcached
 
     protected function _processId($id)
     {
-        $id = md5($this->_getCacheIdPrefix().$id);
+        $id = $this->_getCacheIdPrefix().$id;
         return $id;
     }
 
