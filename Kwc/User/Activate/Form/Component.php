c2c6d740e150fcf778d86f8ec2ea6ba1be2e47c2
Franz Unger <ufx@vivid-planet.com>
2013-05-24 09:53:39 +0200

    Deactivate viewCache for User-Activation-Form

--- Kwc/User/Activate/Form/Component.php
+++ Kwc/User/Activate/Form/Component.php
@@ -14,6 +14,7 @@ class Kwc_User_Activate_Form_Component extends Kwc_Form_Component
         $ret['placeholder']['submitButton'] = trlKwfStatic('Activate Account');
         $ret['generators']['child']['component']['success'] = 'Kwc_User_Activate_Form_Success_Component';
         $ret['useAjaxRequest'] = true;
+        $ret['viewCache'] = false;
         return $ret;
     }
 
