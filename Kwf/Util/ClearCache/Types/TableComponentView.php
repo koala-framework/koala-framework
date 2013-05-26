d4509c6b2686b7394b89451700854c894c20b6cf
Niko Sams <niko.sams@gmail.com>
2013-05-26 18:48:10 +0200

    fix clear-cache

--- Kwf/Util/ClearCache/Types/TableComponentView.php
+++ Kwf/Util/ClearCache/Types/TableComponentView.php
@@ -9,13 +9,13 @@ class Kwf_Util_ClearCache_Types_TableComponentView extends Kwf_Util_ClearCache_T
     protected function _clearCache($options)
     {
         try {
-            $cnt = Zend_Registry::get('db')->query("SELECT COUNT(*) FROM $t WHERE deleted=0")->fetchColumn();
+            $cnt = Zend_Registry::get('db')->query("SELECT COUNT(*) FROM cache_component WHERE deleted=0")->fetchColumn();
             if ($cnt > 5000) {
                 $this->_output("skipped: (won't delete $cnt entries, use clear-view-cache to clear)\n");
                 return;
             }
         } catch (Exception $e) {}
-        parent::clearCache($options);
+        parent::_clearCache($options);
     }
 
     public function getTypeName()
