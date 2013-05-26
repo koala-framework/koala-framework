75d51efda5e93604c2e7d23410d881e6b2fa5f77
Niko Sams <niko.sams@gmail.com>
2013-05-26 19:01:20 +0200

    output error if one happens

--- Kwf/Util/ProgressBar/DispatchStatus.php
+++ Kwf/Util/ProgressBar/DispatchStatus.php
@@ -22,10 +22,11 @@ class Kwf_Util_ProgressBar_DispatchStatus
                 $pbarStatus['bgFinished'] = true;
                 if (!preg_match('#^bgproc[a-z0-9]+$#i', $_REQUEST['outputFile'])) throw new Kwf_Exception_AccessDenied();
                 $output = file_get_contents('./temp/'.$_REQUEST['outputFile']);
+                $outputErr = file_get_contents('./temp/'.$_REQUEST['outputFile'].'.err');
                 $outputJson = json_decode($output);
                 if (!$outputJson) {
                     //assign as string
-                    $pbarStatus['bgError'] = $output;
+                    $pbarStatus['bgError'] = $outputErr;
                 } else {
                     $pbarStatus['bgResponse'] = $outputJson;
                     $pbarStatus['bgError'] = file_get_contents('./temp/'.$_REQUEST['outputFile'].'.err');
