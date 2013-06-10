7dc356a4f7e4fe9e5a9ffb50bee57f3cb1a6f497
Niko Sams <ns@vivid-planet.com>
2013-06-10 17:35:32 +0200

    don't use quickwrite and write by default

--- Kwc/Blog/Comments/Directory/Component.php
+++ Kwc/Blog/Comments/Directory/Component.php
@@ -5,7 +5,12 @@ class Kwc_Blog_Comments_Directory_Component extends Kwc_Posts_Directory_Componen
     {
         $ret = parent::getSettings();
         $ret['generators']['detail']['component'] = 'Kwc_Blog_Comments_Detail_Component';
-        $ret['generators']['write']['component'] = 'Kwc_Blog_Comments_Write_Component';
+        unset($ret['generators']['write']);
+
+        //either use write (on child page):
+        //$ret['generators']['write']['component'] = 'Kwc_Blog_Comments_Write_Component';
+
+        //or quickwrite (default):
         $ret['generators']['quickwrite'] = array(
             'class' => 'Kwf_Component_Generator_Static',
             'component' => 'Kwc_Blog_Comments_QuickWrite_Component'
