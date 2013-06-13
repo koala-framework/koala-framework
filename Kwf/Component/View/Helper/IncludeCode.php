bfae99c37ae8974bec1a99ea57d29e8558036d85
Niko Sams <ns@vivid-planet.com>
2013-06-13 15:50:50 +0200

    add default statistics and assets if there is no box
    using that we don't need these in master.tpl

--- Kwf/Component/View/Helper/IncludeCode.php
+++ Kwf/Component/View/Helper/IncludeCode.php
@@ -10,6 +10,8 @@ class Kwf_Component_View_Helper_IncludeCode extends Kwf_Component_View_Helper_Ab
         if (Kwc_Abstract::getFlag($data->componentClass, $flag)) {
             $cmps[] = $data;
         }
+        $statisticsBoxUsed = false;
+        $assetsBoxUsed = false;
         foreach ($cmps as $c) {
             $includeCode = $c->getComponent()->getIncludeCode($position);
             if (is_string($includeCode)) {
@@ -19,6 +21,16 @@ class Kwf_Component_View_Helper_IncludeCode extends Kwf_Component_View_Helper_Ab
                 $componentHelper->setRenderer($this->_getRenderer());
                 $componentHelper->setView($this->_getView());
                 $ret .= $componentHelper->component($includeCode);
+
+                if (is_instance_of($includeCode->componentClass, 'Kwc_Box_Analytics_Component') ||
+                    is_instance_of($includeCode->componentClass, 'Kwc_Statistics_Piwik_Component')
+                ) {
+                    $statisticsBoxUsed = true;
+                }
+                if (is_instance_of($includeCode->componentClass, 'Kwc_Box_Assets_Component')) {
+                    $assetsBoxUsed = true;
+                }
+
             } else if (is_null($includeCode)) {
             } else {
                 throw new Kwf_Exception("invalid getIncludeCode return type");
@@ -29,7 +41,20 @@ class Kwf_Component_View_Helper_IncludeCode extends Kwf_Component_View_Helper_Ab
             if ($v = Kwf_Config::getValue('kwc.favicon')) {
                 $ret .= "<link rel=\"shortcut icon\" href=\"{$v}\" />\n";
             }
+
+            if (!$assetsBoxUsed) {
+                //add default assets if there was no box
+                $a = new Kwf_View_Helper_Assets();
+                $ret .= $a->assets('Frontend');
+            }
+
             $ret .= Kwf_View_Helper_DebugData::debugData();
+        } else if ($position == 'footer') {
+            if (!$statisticsBoxUsed) {
+                //if there was no statistics box output default code
+                //box is required for eg. multidomains
+                $ret .= Kwf_View_Helper_StatisticCode::statisticCode();
+            }
         }
         return $ret;
     }
