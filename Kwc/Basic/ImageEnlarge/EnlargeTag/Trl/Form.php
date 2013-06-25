0f2c5263cf9099523da6c85c4598c8068706eb6e
Niko Sams <ns@vivid-planet.com>
2013-06-25 13:29:53 +0200

    improve trl: show original title and image caption

--- Kwc/Basic/ImageEnlarge/EnlargeTag/Trl/Form.php
+++ Kwc/Basic/ImageEnlarge/EnlargeTag/Trl/Form.php
@@ -10,6 +10,8 @@ class Kwc_Basic_ImageEnlarge_EnlargeTag_Trl_Form extends Kwc_Abstract_Image_Trl_
             'imageTitle'
         );
         if ($imageTitle) {
+            $this->add(new Kwf_Form_Field_ShowField('original_title', trlKwf('Original Title')))
+                ->setData(new Kwf_Data_Trl_OriginalComponent('title'));
             $this->add(new Kwf_Form_Field_TextArea('title', trlKwf('Title')))
                 ->setWidth(350)
                 ->setHeight(80);
