02419489760b979d82e595963e173cf3a1c03796
Franz Unger <ufx@vivid-planet.com>
2013-06-26 13:29:14 +0200

    Change Piwik tracking code to asynchronous mode

--- Kwc/Statistics/Piwik/Component.js
+++ Kwc/Statistics/Piwik/Component.js
@@ -1,26 +1,11 @@
-Ext.ns('Kwc.Statistics.Piwik');
-
-Kwc.Statistics.Piwik.getTracker = function()
-{
-    if (!Kwc.Statistics.Piwik.url || !Kwc.Statistics.Piwik.idSite || typeof Piwik == 'undefined') return null;
-    try {
-        return Piwik.getTracker(Kwc.Statistics.Piwik.url, Kwc.Statistics.Piwik.idSite);
-    } catch( err ) {
-        return null;
-    }
-};
-
 Kwf.Statistics.onCount(function(url, config) {
-    var piwikTracker = Kwc.Statistics.Piwik.getTracker();
-    if (piwikTracker) {
-        if (config.customVariables) {
-            for (var x = 0; x < config.customVariables.length; x++) {
-                var cv = config.customVariables[x];
-                piwikTracker.setCustomVariable(cv.index, cv.name, cv.value, cv.scope);
-            }
+    if (config.customVariables) {
+        for (var x = 0; x < config.customVariables.length; x++) {
+            var cv = config.customVariables[x];
+            _paq.push(["setCustomVariable", cv.index, cv.name, cv.value, cv.scope]);
         }
-        piwikTracker.setCustomUrl(url);
-        piwikTracker.setReferrerUrl(location.href);
-        piwikTracker.trackPageView();
     }
+    _paq.push(["setCustomUrl", url]);
+    _paq.push(["setReferrerUrl", location.href]);
+    _paq.push(["trackPageView"]);
 });
