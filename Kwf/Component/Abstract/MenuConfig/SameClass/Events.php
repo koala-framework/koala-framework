e79511f304a65d2d0640eb17d8bfef2eb1c04c22
Niko Sams <ns@vivid-planet.com>
2013-06-27 09:33:01 +0200

    clear cache when adding/removing invisible component

--- Kwf/Component/Abstract/MenuConfig/SameClass/Events.php
+++ Kwf/Component/Abstract/MenuConfig/SameClass/Events.php
@@ -14,10 +14,20 @@ class Kwf_Component_Abstract_MenuConfig_SameClass_Events extends Kwf_Component_A
             'event' => 'Kwf_Component_Event_Component_Removed',
             'callback' => 'onComponentAddedRemoved'
         );
+        $ret[] = array(
+            'class' => null,
+            'event' => 'Kwf_Component_Event_Component_InvisibleAdded',
+            'callback' => 'onComponentAddedRemoved'
+        );
+        $ret[] = array(
+            'class' => $this->_class,
+            'event' => 'Kwf_Component_Event_Component_InvisibleRemoved',
+            'callback' => 'onComponentAddedRemoved'
+        );
         return $ret;
     }
 
-    public function onComponentAddedRemoved(Kwf_Component_Event_Component_AbstractFlag $ev)
+    public function onComponentAddedRemoved(Kwf_Component_Event_Component_Abstract $ev)
     {
         Kwf_Acl::clearCache();
     }
