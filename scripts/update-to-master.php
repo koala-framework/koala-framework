28597a2a0347982938e2b3b357accba7fe87a53a
Franz Unger <ufx@vivid-planet.com>
2013-06-28 09:53:39 +0200

    Change to new includeCode in update script

--- scripts/update-to-master.php
+++ scripts/update-to-master.php
@@ -8,8 +8,7 @@ echo "Changed $file to master\n";
 
 function glob_recursive($pattern, $flags = 0) {
     $files = glob($pattern, $flags);
-    foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir)
-    {
+    foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
         $files = array_merge($files, glob_recursive($dir.'/'.basename($pattern), $flags));
     }
     return $files;
@@ -34,6 +33,28 @@ function replaceFiles($files, $from, $to) {
     }
 }
 
+function updateIncludeCode()
+{
+    $files = glob_recursive('Master.tpl');
+    $removeBoxes = array('title', 'metatags', 'opengraph', 'piwik', 'analytics', 'assets', 'rssFeeds');
+    foreach ($files as $f) {
+        $c = file_get_contents($f);
+        if (!strpos($c, 'this->includeCode')) {
+            $c = str_replace("<head>\n", "<head>\n        <?=\$this->includeCode('header')?>\n", $c);
+            $c = str_replace("</body>", "    <?=\$this->includeCode('footer')?>\n    </body>", $c);
+            foreach ($removeBoxes as $b) {
+                $c = preg_replace("#^\s*<\?=\\\$this->component\(\\\$this->boxes\['$b'\]\);\?> *\n#im", "", $c);
+            }
+            $c = preg_replace("#^\s*<\?=\\\$this->debugData\(\);\?> *\n#im", "", $c);
+            $c = preg_replace("#^\s*<link rel=\"shortcut icon\" href=\"/assets/web/images/favicon\.ico\" /> *\n#im", "", $c);
+            $c = preg_replace("#^\s*<\?=\\\$this->statisticCode\(\);\?> *\n#im", "", $c);
+            $c = preg_replace("#^\s*<\?=\\\$this->assets\(\'Frontend\'\);\?> *\n#im", "", $c);
+            file_put_contents($f, $c);
+            echo "Updated $f to use new includeCode helper\n";
+        }
+    }
+}
+
 $files = glob_recursive('Component.php');
 $files[] = 'config.ini';
 replaceFiles($files, 'Kwc_Composite_Images_Component', 'Kwc_List_Images_Component');
@@ -42,3 +63,4 @@ replaceFiles($files, 'Kwc_Composite_Downloads_Component', 'Kwc_List_Downloads_Co
 replaceFiles($files, 'Kwc_Composite_ImagesEnlarge_Component', 'Kwc_List_Gallery_Component');
 replaceFiles($files, 'Kwc_Composite_Links_Component', 'Kwc_List_Links_Component');
 checkGallery($files);
+updateIncludeCode();
