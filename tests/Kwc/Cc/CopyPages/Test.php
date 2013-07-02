5557154d379bef7c264c57075b84b837e0729f8e
Niko Sams <ns@vivid-planet.com>
2013-07-02 16:09:27 +0200

    implement test

--- tests/Kwc/Cc/CopyPages/Test.php
+++ tests/Kwc/Cc/CopyPages/Test.php
@@ -8,5 +8,11 @@ class Kwc_Cc_CopyPages_Test extends Kwc_TestAbstract
 
     public function testIt()
     {
+        $html = $this->_root->getComponentById('1')->render();
+        $this->assertContains("test123", $html);
+
+        $html = $this->_root->getComponentById('2')->render();
+        $this->assertContains("test123", $html);
+
     }
 }
