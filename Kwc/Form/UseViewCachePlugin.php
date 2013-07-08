39c15e9060ab0c91df93c37b31da2fa7b2f69dab
Niko Sams <ns@vivid-planet.com>
2013-07-08 14:25:00 +0200

    don't call processInput when form isn't posted

--- Kwc/Form/UseViewCachePlugin.php
+++ Kwc/Form/UseViewCachePlugin.php
@@ -1,11 +1,20 @@
 <?php
 class Kwc_Form_UseViewCachePlugin extends Kwf_Component_Plugin_Abstract
-    implements Kwf_Component_Plugin_Interface_UseViewCache
+    implements Kwf_Component_Plugin_Interface_UseViewCache, Kwf_Component_Plugin_Interface_SkipProcessInput
 {
     public function useViewCache()
     {
         // Checking for specific post-data because there could be more forms on
         // one page and only one gets submited
+        if (isset($_REQUEST[$this->_componentId.'-post'])) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public function skipProcessInput()
+    {
         if (isset($_REQUEST[$this->_componentId.'-post'])) {
             return false;
         } else {
