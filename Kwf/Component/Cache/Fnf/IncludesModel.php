5cd78453f1e6de696c8a97c98923b1493a1a1d8e
Niko Sams <ns@vivid-planet.com>
2013-07-12 08:03:44 +0200

    add missing column to fnf model

--- Kwf/Component/Cache/Fnf/IncludesModel.php
+++ Kwf/Component/Cache/Fnf/IncludesModel.php
@@ -5,7 +5,7 @@ class Kwf_Component_Cache_Fnf_IncludesModel extends Kwf_Component_Cache_Mysql_In
     {
         $config['proxyModel'] = new Kwf_Model_FnF(array(
             'primaryKey' => 'id',
-            'columns' => array('id', 'component_id', 'page_id', 'target_id')
+            'columns' => array('id', 'component_id', 'type', 'target_id', 'target_type')
         ));
         parent::__construct($config);
     }
