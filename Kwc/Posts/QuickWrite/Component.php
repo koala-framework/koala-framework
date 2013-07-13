114efb05182930f5a95f57b8fdc7e5acdc516c14
Niko Sams <niko.sams@gmail.com>
2013-07-13 21:29:50 +0200

    improve quick write: don't link to parent page in success

--- Kwc/Posts/QuickWrite/Component.php
+++ Kwc/Posts/QuickWrite/Component.php
@@ -6,6 +6,7 @@ class Kwc_Posts_QuickWrite_Component extends Kwc_Posts_Write_Component
         $ret = parent::getSettings();
         $ret['generators']['child']['component']['preview'] = null;
         $ret['generators']['child']['component']['lastPosts'] = null;
+        $ret['generators']['child']['component']['form'] = 'Kwc_Posts_QuickWrite_Form_Component';
         $ret['plugins'] = array();
         return $ret;
     }
