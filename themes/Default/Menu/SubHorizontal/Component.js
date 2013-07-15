3e28e54ae85a1243919e3ac11f7f0dc10568f8d2
Tobias Asen <ta@vivid-planet.com>
2013-07-15 10:36:40 +0200

    subMenuHorizontal styled

--- themes/Default/Menu/SubHorizontal/Component.js
+++ themes/Default/Menu/SubHorizontal/Component.js
@@ -1,4 +1,4 @@
-Kwf.onElementReady('.menuSubHorizontal', function(el) {
+Kwf.onElementReady('.defaultMenuSubHorizontal', function(el) {
     var menu = el.child('.menu').dom;
     if (menu.scrollWidth - $(menu).innerWidth() == 0) return false;
     $(menu).before('<a class="arrowLeft"></a>').before('<a class="arrowRight"></a>');
