5aa6324f4ecf3f8047e707a12422aef78e87a1e5
Tobias Asen <ta@vivid-planet.com>
2013-07-16 10:15:33 +0200

    translation changed from trlVps to trlKwf - merge bug from vps

--- Kwc/ParagraphsPassword/Component.php
+++ Kwc/ParagraphsPassword/Component.php
@@ -4,7 +4,7 @@ class Kwc_ParagraphsPassword_Component extends Kwc_Paragraphs_Component
     public static function getSettings()
     {
         $ret = parent::getSettings();
-        $ret['componentName'] = trlVps('Paragraphs passwordprotected');
+        $ret['componentName'] = trlKwf('Paragraphs passwordprotected');
         $ret['extConfig'] = 'Kwc_ParagraphsPassword_ExtConfig';
         $ret['ownModel'] = 'Kwf_Component_FieldModel';
         $ret['plugins'][] = 'Kwc_ParagraphsPassword_Plugin_Component';
