f25c2ab30725c7cf96e14be55a57b3bccb9a2757
Michael Freudenthaler <mf@vivid-planet.com>
2013-07-23 14:52:42 +0200

    auth process simplified

--- Kwc/User/Login/Facebook/ComponentController.php
+++ Kwc/User/Login/Facebook/ComponentController.php
@@ -9,9 +9,6 @@ class Kwc_User_Login_Facebook_ComponentController extends Kwf_Controller_Action
 
         if($token) {
             $adapter->setToken($token);
-            $auth = Kwf_Auth::getInstance();
-            $result  = $auth->authenticate($adapter);
-            $auth->clearIdentity();
             return $adapter->authenticate();
         }
         return null;
