115a6a8f71c5e5c551e7964ec6d33b6946e9552f
Michael Freudenthaler <mf@vivid-planet.com>
2013-07-23 15:02:58 +0200

    code styling

--- Kwc/User/Login/Facebook/ComponentController.php
+++ Kwc/User/Login/Facebook/ComponentController.php
@@ -3,8 +3,7 @@ class Kwc_User_Login_Facebook_ComponentController extends Kwf_Controller_Action
 {
     public function jsonAuthAction()
     {
-        $array = Kwf_Registry::get('config')->kwc->fbAppData;
-        $adapter = new Kwc_User_Login_Facebook_Adapter($array);
+        $adapter = new Kwc_User_Login_Facebook_Adapter();
         $token = $this->_getParam('accessToken');
 
         if($token) {
