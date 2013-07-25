dc270c125d262a3c0fdcb8b75c0a82a54104e95e
Michael Freudenthaler <mf@vivid-planet.com>
2013-07-25 11:30:06 +0200

    code styling

--- Kwc/User/Login/Form/Success/Component.js
+++ Kwc/User/Login/Form/Success/Component.js
@@ -1,7 +1,6 @@
 Kwf.onContentReady(function() {
     Ext.query('.kwcUserLoginFormSuccess').each(function(el) {
-        if (Ext.get(el).isVisible(true)) {
-            if (el.initDone) return;
+        if (Ext.get(el).isVisible(true) && !el.initDone) {
             el.initDone = true;
             var url = Ext.get(el).child('input.redirectTo').getValue();
             location.href = url;
