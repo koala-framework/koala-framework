8c1f96a849c659cc7c098ad15baad99e2c48f9e1
Niko Sams <ns@vivid-planet.com>
2013-07-31 10:32:26 +0200

    better api: pass theme as second parameter

--- Kwf/Component/Theme/Abstract.php
+++ Kwf/Component/Theme/Abstract.php
@@ -1,9 +1,9 @@
 <?php
 class Kwf_Component_Theme_Abstract extends Kwf_Component_Abstract
 {
-    public static function applySettings(&$ret)
+    public static function applySettings(&$ret, $theme)
     {
-        $settings = call_user_func(array($ret['theme'], 'getRootSettings'));
+        $settings = call_user_func(array($theme, 'getRootSettings'));
         foreach ($settings as $k=>$i) {
             if (!isset($ret[$k]) || !is_array($ret[$k])) {
                 $ret[$k] = $i;
