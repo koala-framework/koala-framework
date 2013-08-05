d129263815cae851c99fff872e38bda267536493
Niko Sams <ns@vivid-planet.com>
2013-08-05 14:46:11 +0200

    add update script for converting web to support running in subfolder

--- scripts/update-to-master.php
+++ scripts/update-to-master.php
@@ -3,3 +3,31 @@ $file = is_file('vkwf_branch') ? 'vkwf_branch' : 'kwf_branch';
 file_put_contents($file, "master\n");
 echo "Changed $file to master\n";
 
+function updateAclMenuUrls()
+{
+    $c = file_get_contents('app/Acl.php');
+    $c = preg_replace_callback(
+        '#(new Kwf_Acl_Resource_MenuUrl\\(\'([^\']+)\',(.*?)),\s*\'(/(admin|kwf|vkwf)[^\']+)\'(\s*\)\s*(,\s*\'[^\']+\'\)?);)#s',
+        function($m) {
+            if ('/'.str_replace('_', '/', $m[2]) == $m[4]) {
+                return $m[1].$m[6];
+            } else {
+                return $m[0];
+            }
+        },
+        $c
+    );
+    file_put_contents('app/Acl.php', $c);
+    echo "updated app/Acl.php to set url only where required\n";
+}
+
+function updateHtaccess()
+{
+    $c = file_get_contents('.htaccess');
+    $c = str_replace('RewriteRule ^(.*)$ /bootstrap.php [L]', 'RewriteRule ^(.*)$ bootstrap.php [L]', $c);
+    file_put_contents('.htaccess', $c);
+    echo "updated .htaccess to support running in subfolder\n";
+}
+
+updateAclMenuUrls();
+updateHtaccess();
