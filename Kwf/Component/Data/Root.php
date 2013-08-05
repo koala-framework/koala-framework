d2bae7418fef49bd7b3e59ae6de9dfdd25b753e5
Niko Sams <niko.sams@gmail.com>
2013-08-05 21:07:24 +0200

    don't remove ending slash for home, Data_Home also doesn't do it

--- Kwf/Component/Data/Root.php
+++ Kwf/Component/Data/Root.php
@@ -192,9 +192,6 @@ class Kwf_Component_Data_Root extends Kwf_Component_Data
                 } else {
                     $path = substr($path, strlen($baseUrl));
                 }
-                if ($parsedUrl['path'] == $baseUrl.'/') {
-                    $parsedUrl['path'] = substr($parsedUrl['path'], 0, -1);
-                }
             }
             $path = trim($path, '/');
             $ret = $this->getComponent()->getPageByUrl($path, $acceptLanguage);
