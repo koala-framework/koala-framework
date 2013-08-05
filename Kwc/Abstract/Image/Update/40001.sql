2df493f914f3ffa75d0aaec81abbadb2282abb35
Benjamin Hohenwarter <bh@vivid-planet.com>
2013-08-05 13:11:58 +0200

    Remove database double because it wont be a problem

--- Kwc/Abstract/Image/Update/40001.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `kwc_basic_image` CHANGE `crop_x` `crop_x` DOUBLE NOT NULL;
-ALTER TABLE `kwc_basic_image` CHANGE `crop_y` `crop_y` DOUBLE NOT NULL;
-ALTER TABLE `kwc_basic_image` CHANGE `crop_width` `crop_width` DOUBLE NOT NULL;
-ALTER TABLE `kwc_basic_image` CHANGE `crop_height` `crop_height` DOUBLE NOT NULL;
