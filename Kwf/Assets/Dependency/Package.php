d4907c22cc32e4b4eda3ddecb4dbea9fd3338f9a
Niko Sams <ns@vivid-planet.com>
2013-08-13 10:06:55 +0200

    support print css file extension

--- Kwf/Assets/Dependency/Package.php
+++ Kwf/Assets/Dependency/Package.php
@@ -70,6 +70,7 @@ class Kwf_Assets_Dependency_Package extends Kwf_Assets_Dependency_Abstract
     {
         if ($mimeType == 'text/javascript; charset=utf-8') $ext = 'js';
         else if ($mimeType == 'text/css') $ext = 'css';
+        else if ($mimeType == 'text/css; media=print') $ext = 'css';
         else throw new Kwf_Exception_NotYetImplemented();
 
         $ret = array();
