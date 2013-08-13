bd6eb49a77eee89572b9cb08edc8e276f2b1c178
Mario Kemetinger <mk@vivid-planet.com>
2013-08-13 16:35:36 +0200

    Disable Animation on OptBox because of not so smooth movement

--- Kwc/Statistics/OptBox/Component.js
+++ Kwc/Statistics/OptBox/Component.js
@@ -1,5 +1,6 @@
 Kwf.onElementReady('.kwcStatisticsOptBox', function(el, config) {
     if (Kwf.Statistics.getDefaultOptValue() == 'out' && !Kwf.Statistics.issetUserOptValue()) {
-        el.slideIn('t', { duration: 2 });
+//        el.slideIn('t', { easing: 'linear', duration: 1.25 });
+        el.show(true);
     }
 });
