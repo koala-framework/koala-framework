4cbcc46c849414ad874c42035570a198b507a0f0
Benjamin Hohenwarter <bh@vivid-planet.com>
2013-08-13 13:35:46 +0200

    Add adopt-button for link-trl-component

--- Kwc/Basic/Link/Trl/Form.php
+++ Kwc/Basic/Link/Trl/Form.php
@@ -7,6 +7,9 @@ class Kwc_Basic_Link_Trl_Form extends Kwc_Abstract_Composite_Form
         $this->add(new Kwf_Form_Field_TextField('text', trlKwf('Linktext')))
             ->setWidth(300);
 
+        $this->add(new Kwf_Form_Field_Panel('copy'))
+            ->setHideLabel(true)
+            ->setXtype('kwc.basic.link.trl.copybutton');
         $this->add(new Kwf_Form_Field_ShowField('original_text', trlKwf('Original')))
             ->setData(new Kwf_Data_Trl_OriginalComponent('text'));
 
