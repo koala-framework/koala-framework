fc41c809d80ad86c32dcfefe26929f2e77dd0764
Niko Sams <ns@vivid-planet.com>
2013-08-13 10:05:37 +0200

    process css and js files as we did in old assets loader

--- Kwf/Assets/Dependency/File/Css.php
+++ Kwf/Assets/Dependency/File/Css.php
@@ -5,4 +5,19 @@ class Kwf_Assets_Dependency_File_Css extends Kwf_Assets_Dependency_File
     {
         return 'text/css';
     }
+    public function getContents($language)
+    {
+        $ret = parent::getContents($language);
+
+        $pathType = substr($this->_fileName, 0, strpos($this->_fileName, '/'));
+
+        if ($pathType == 'ext') {
+            //hack um bei ext-css-dateien korrekte pfade für die bilder zu haben
+            $ret = str_replace('../images/', '/assets/ext/resources/images/', $ret);
+        } else if ($pathType == 'mediaelement') {
+            //hack to get the correct paths for the mediaelement pictures
+            $ret = str_replace('url(', 'url(/assets/mediaelement/build/', $ret);
+        }
+        return $ret;
+    }
 }
--- Kwf/Assets/Dependency/File/Js.php
+++ Kwf/Assets/Dependency/File/Js.php
@@ -5,4 +5,53 @@ class Kwf_Assets_Dependency_File_Js extends Kwf_Assets_Dependency_File
     {
         return 'text/javascript; charset=utf-8';
     }
+
+    public function getContents($language)
+    {
+        $ret = parent::getContents($language);
+
+        //TODO same code is in in File_Css too
+        $pathType = substr($this->_fileName, 0, strpos($this->_fileName, '/'));
+        if ($pathType == 'ext') {
+            //hack um bei ext-css-dateien korrekte pfade für die bilder zu haben
+            $ret = str_replace('../images/', '/assets/ext/resources/images/', $ret);
+        } else if ($pathType == 'mediaelement') {
+            //hack to get the correct paths for the mediaelement pictures
+            $ret = str_replace('url(', 'url(/assets/mediaelement/build/', $ret);
+        }
+
+        /*
+        TODO implement
+        $ret = str_replace(
+            '{$application.maxAssetsMTime}',
+            $this->getDependencies()->getMaxFileMTime(),
+            $ret);
+        */
+
+        static $jsLoader;
+        if (!isset($jsLoader)) $jsLoader = new Kwf_Trl_JsLoader();
+
+        $ret = $jsLoader->trlLoad($ret, $language);
+        $ret = $this->_hlp($ret, $language);
+
+        if ($baseUrl = Kwf_Setup::getBaseUrl()) {
+            $ret = preg_replace('#url\\((\s*[\'"]?)/assets/#', 'url($1'.$baseUrl.'/assets/', $ret);
+            $ret = preg_replace('#([\'"])/(kwf|vkwf|admin|assets)/#', '$1'.$baseUrl.'/$2/', $ret);
+        }
+
+        return $ret;
+    }
+
+    private function _hlp($contents, $language)
+    {
+        //TODO 1902 $language verwenden
+        $matches = array();
+        preg_match_all("#hlp\(['\"](.+?)['\"]\)#", $contents, $matches);
+        foreach ($matches[0] as $key => $search) {
+            $r = hlp($matches[1][$key]);
+            $r = str_replace(array("\n", "\r", "'"), array('\n', '', "\\'"), $r);
+            $contents = str_replace($search, "'" . $r . "'", $contents);
+        }
+        return $contents;
+    }
 }
