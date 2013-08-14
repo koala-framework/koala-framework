0affc08bc896108157a022124dbf92458be89556
Niko Sams <ns@vivid-planet.com>
2013-08-14 15:41:00 +0200

    simplify iterator, we can reuse ArrayIterator completely

--- Kwf/Assets/Dependency/RecursiveIterator.php
+++ Kwf/Assets/Dependency/RecursiveIterator.php
@@ -1,18 +1,9 @@
 <?php
 class Kwf_Assets_Dependency_RecursiveIterator extends ArrayIterator implements RecursiveIterator
 {
-    protected $_position;
-    protected $_dependencies;
-
     public function __construct(Kwf_Assets_Dependency_Abstract $dependency)
     {
-        $this->_position = 0;
-        $this->_dependencies = $dependency->getDependencies();
-    }
-
-    public function current()
-    {
-        return $this->_dependencies[$this->_position];
+        parent::__construct($dependency->getDependencies());
     }
 
     public function getChildren()
@@ -23,25 +14,5 @@ class Kwf_Assets_Dependency_RecursiveIterator extends ArrayIterator implements R
     public function hasChildren()
     {
         return (bool)count($this->current()->getDependencies());
-    }
-
-    public function key()
-    {
-        return $this->_position;
-    }
-
-    public function next()
-    {
-        $this->_position++;
-    }
-
-    public function rewind()
-    {
-        $this->_position = 0;
-    }
-
-    public function valid()
-    {
-        return $this->_position < count($this->_dependencies);
     }
 }
