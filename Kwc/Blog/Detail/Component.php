6c4fed7b5b0f879894493f34a7f3e75ff3cfe61b
Niko Sams <niko.sams@gmail.com>
2013-08-16 11:13:45 +0200

    Blog and GreyBox: add previous and next links

--- Kwc/Blog/Detail/Component.php
+++ Kwc/Blog/Detail/Component.php
@@ -7,6 +7,8 @@ class Kwc_Blog_Detail_Component extends Kwc_Directories_Item_Detail_Component
         $ret['generators']['child']['component']['content'] = 'Kwc_Paragraphs_Component';
         $ret['cssClass'] = 'webStandard';
         $ret['placeholder']['backLink'] = trlKwfStatic('Back to overview');
+        $ret['placeholder']['nextLink'] = false;
+        $ret['placeholder']['previousLink'] = false;
         $ret['editComponents'] = array('content');
         $ret['flags']['hasFulltext'] = true;
 
@@ -21,6 +23,23 @@ class Kwc_Blog_Detail_Component extends Kwc_Directories_Item_Detail_Component
         $ret['title'] = $this->getData()->row->title;
         $ret['publish_date'] = $this->getData()->row->publish_date;
         $ret['author'] = $this->getData()->row->author_firstname.' '.$this->getData()->row->author_lastname;
+
+        if ($this->_getPlaceholder('nextLink')) {
+            $s = new Kwf_Component_Select();
+            $s->whereGenerator('detail');
+            $s->order('publish_date', 'ASC');
+            $s->where(new Kwf_Model_Select_Expr_Higher('publish_date', new Kwf_Date($this->getData()->row->publish_date)));
+            $ret['nextPost'] = $this->getData()->parent->getChildComponent($s);
+        }
+
+        if ($this->_getPlaceholder('previousLink')) {
+            $s = new Kwf_Component_Select();
+            $s->whereGenerator('detail');
+            $s->order('publish_date', 'DESC');
+            $s->where(new Kwf_Model_Select_Expr_Lower('publish_date', new Kwf_Date($this->getData()->row->publish_date)));
+            $ret['previousPost'] = $this->getData()->parent->getChildComponent($s);
+        }
+
         return $ret;
     }
 
--- Kwc/Blog/Detail/Component.tpl
+++ Kwc/Blog/Detail/Component.tpl
@@ -1,5 +1,15 @@
 <div class="<?=$this->cssClass;?>">
     <div class="blogContent">
+        <? if ($this->previousPost || $this->nextPost) { ?>
+            <p class="nextPreviousLinks">
+                <? if ($this->previousPost) { ?>
+                    <?=$this->componentLink($this->previousPost, $this->placeholder['previousLink'])?>
+                <? } ?>
+                <? if ($this->nextPost) { ?>
+                    <?=$this->componentLink($this->nextPost, $this->placeholder['nextLink'])?>
+                <? } ?>
+            </p>
+        <? } ?>
         <h1 class="title"><?=$this->componentLink($this->item, $this->title)?></h1>
         <div class="postInfo">
             <?=$this->data->trlKwf('Posted on {0}', $this->date($this->row->publish_date))?>
