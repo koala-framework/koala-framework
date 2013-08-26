ac88c6d98ed399a137a3877be9774a3b534ed2e4
Benjamin Hohenwarter <bh@vivid-planet.com>
2013-08-26 14:07:42 +0200

    Fix incomplete tests

--- tests/Kwc/Basic/Image/Crop/Root.php
+++ tests/Kwc/Basic/Image/Crop/Root.php
@@ -12,6 +12,43 @@ class Kwc_Basic_Image_Crop_Root extends Kwc_Root_Abstract
             'class' => 'Kwf_Component_Generator_Page_Static',
             'component' => 'Kwc_Basic_Image_Crop_ImageFixDimensionComponent'
         );
+        $ret['generators']['page2'] = array(
+            'class' => 'Kwf_Component_Generator_Page_Static',
+            'component' => 'Kwc_Basic_Image_Crop_ImageUserSelectComponent'
+        );
+        $ret['generators']['page10'] = array(
+            'class' => 'Kwf_Component_Generator_Page_Static',
+            'component' => 'Kwc_Basic_Image_Crop_ParentImage_Component'
+        );
+
+        $ret['generators']['page3'] = array(
+            'class' => 'Kwf_Component_Generator_Page_Static',
+            'component' => 'Kwc_Basic_Image_Crop_MultipleDimensionsComponent'
+        );
+        $ret['generators']['page4'] = array(
+            'class' => 'Kwf_Component_Generator_Page_Static',
+            'component' => 'Kwc_Basic_Image_Crop_MultipleDimensionsComponent'
+        );
+        $ret['generators']['page5'] = array(
+            'class' => 'Kwf_Component_Generator_Page_Static',
+            'component' => 'Kwc_Basic_Image_Crop_MultipleDimensionsComponent'
+        );
+        $ret['generators']['page6'] = array(
+            'class' => 'Kwf_Component_Generator_Page_Static',
+            'component' => 'Kwc_Basic_Image_Crop_MultipleDimensionsComponent'
+        );
+        $ret['generators']['page7'] = array(
+            'class' => 'Kwf_Component_Generator_Page_Static',
+            'component' => 'Kwc_Basic_Image_Crop_MultipleDimensionsComponent'
+        );
+        $ret['generators']['page8'] = array(
+            'class' => 'Kwf_Component_Generator_Page_Static',
+            'component' => 'Kwc_Basic_Image_Crop_MultipleDimensionsComponent'
+        );
+        $ret['generators']['page9'] = array(
+            'class' => 'Kwf_Component_Generator_Page_Static',
+            'component' => 'Kwc_Basic_Image_Crop_MultipleDimensionsComponent'
+        );
         return $ret;
     }
 }
