0b4f8d3bba270653d652b389ecc6ce8dde6faa5e
Tobias Asen <ta@vivid-planet.com>
2013-08-29 17:17:12 +0200

    fade mask in and out

--- themes/RedMallee/Menu/Main/Component.js
+++ themes/RedMallee/Menu/Main/Component.js
@@ -2,17 +2,18 @@ Kwf.Utils.ResponsiveEl('.redMalleeMenuMain', [1100, 960, 700, 659, 500]);
 Kwf.Utils.ResponsiveEl('body', [1100, 960, 700, 659, 500]);
 
 $(function(){
-    var selector = $('.redMalleeMenuMain ul li.hasSubmenu');
+    var selector = $('.redMalleeMenuMain > ul > li.hasSubmenu');
     var dropdown = $('.dropdown');
     var mask = $('#mask');
 
-    $(selector).mouseover(function() {
+
+    $(selector).mouseenter(function() {
         if (window.innerWidth > 979) {
-            mask.css('display', 'block');
+            mask.stop().fadeIn(400);
         }
     });
-    $(selector).mouseout(function() {
-        mask.css('display', 'none');
+    $(selector).mouseleave(function() {
+        mask.stop().fadeOut(400);
     });
 });
 
