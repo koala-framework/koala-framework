8455da46a85f92255e24959ff3ad54226f3fda96
Niko Sams <ns@vivid-planet.com>
2013-09-03 08:49:45 +0200

    add clear-cache action that allows deleting the full memcache
    should be used with care

--- Kwf/Controller/Action/Cli/ClearCacheController.php
+++ Kwf/Controller/Action/Cli/ClearCacheController.php
@@ -16,6 +16,25 @@ class Kwf_Controller_Action_Cli_ClearCacheController extends Kwf_Controller_Acti
         exit;
     }
 
+    public function memcacheAction()
+    {
+        if (!Kwf_Cache_Simple::$memcacheHost) {
+            echo "memcache not configured for host\n";
+            exit;
+        }
+        $s = Kwf_Cache_Simple::$memcacheHost.':'.Kwf_Cache_Simple::$memcachePort;
+        echo "Clear the complete memcache on $s?\nThis will effect all other webs using this memcache host.\nAre you REALLY sure you want to do that? [N/y]\n";
+        $stdin = fopen('php://stdin', 'r');
+        $input = trim(strtolower(fgets($stdin, 2)));
+        fclose($stdin);
+        if (($input == 'y')) {
+            Kwf_Cache_Simple::getMemcache()->flush();
+            echo "done\n";
+            exit;
+        }
+        exit(1);
+    }
+
     public function writeMaintenanceAction()
     {
         Kwf_Util_Maintenance::writeMaintenanceBootstrapSelf();
