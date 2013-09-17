84cda42777b3b4f82941101006f3c970a5849479
Michael Freudenthaler <mf@vivid-planet.com>
2013-09-17 16:12:05 +0200

    added a ajax view to the news list component

---
+++ Kwc/News/List/AjaxView/Component.php
@@ -0,0 +1,10 @@
+<?php
+class Kwc_News_List_AjaxView_Component extends Kwc_Directories_List_ViewAjax_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['placeholder']['readMore'] = trlKwfStatic('Read more').' &raquo;';
+        return $ret;
+    }
+}
