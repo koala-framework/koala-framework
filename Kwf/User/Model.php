587c6d85929271b381f4847ac5bde82575014202
Benjamin Hohenwarter <bh@vivid-planet.com>
2013-09-18 11:50:00 +0200

    Remove unused parameter
    This parameter isn't used by anything in this class.

--- Kwf/User/Model.php
+++ Kwf/User/Model.php
@@ -116,7 +116,7 @@ class Kwf_User_Model extends Kwf_Model_RowCache implements Kwf_User_ModelInterfa
      * @param string E-Mail address of user
      * @param string webcode parameter used for Service Model (that can have global users)
      */
-    public function createUserRow($email, $webcode = null)
+    public function createUserRow($email)
     {
         $row = parent::createRow(array('email' => $email));
         $this->_resetPermissions($row);
