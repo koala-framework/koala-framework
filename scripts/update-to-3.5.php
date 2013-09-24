8ee9e66a438cf0afb47e98e4d407a0a6a8659adf
Niko Sams <ns@vivid-planet.com>
2013-09-24 09:04:37 +0200

    update version to 3.5

--- scripts/update-to-3.5.php
+++ scripts/update-to-3.5.php
@@ -1,7 +1,7 @@
 <?
 $file = is_file('vkwf_branch') ? 'vkwf_branch' : 'kwf_branch';
-file_put_contents($file, "master\n");
-echo "Changed $file to master\n";
+file_put_contents($file, "3.5\n");
+echo "Changed $file to 3.5\n";
 
 function updateAclMenuUrls()
 {
