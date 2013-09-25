a91a04616801e36dd10f79c8ebc3e6a3c68036a5
Niko Sams <ns@vivid-planet.com>
2013-09-25 13:09:30 +0200

    cache baseUrl to speed up multiple calls

--- Kwf/Setup.php
+++ Kwf/Setup.php
@@ -131,9 +131,11 @@ class Kwf_Setup
 
     public static function getBaseUrl()
     {
+        static $ret;
+        if (isset($ret)) return $ret;
         $ret = Kwf_Config::getValue('server.baseUrl');
         if ($ret === null && isset($_SERVER['PHP_SELF']) && php_sapi_name() != 'cli') {
-            return substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], '/'));
+            $ret = substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], '/'));
         }
         return $ret;
     }
