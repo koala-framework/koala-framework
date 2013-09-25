6551244e25226746cbcd0ae1339593afe7ea0279
Niko Sams <ns@vivid-planet.com>
2013-09-25 13:08:12 +0200

    always reuse default provider object, also when unserializing package

--- Kwf/Assets/Dependency/Package/Default.php
+++ Kwf/Assets/Dependency/Package/Default.php
@@ -1,6 +1,7 @@
 <?php
 class Kwf_Assets_Dependency_Package_Default extends Kwf_Assets_Dependency_Package
 {
+    private static $_defaultProviderList;
     /**
      * Returns a Default Asset Package (using Kwf_Assets_ProviderList_Default)
      *
@@ -21,9 +22,17 @@ class Kwf_Assets_Dependency_Package_Default extends Kwf_Assets_Dependency_Packag
         return $ret;
     }
 
+    private static function _getDefaultProviderList()
+    {
+        if (!isset(self::$_defaultProviderList)) {
+            self::$_defaultProviderList = new Kwf_Assets_ProviderList_Default();
+        }
+        return self::$_defaultProviderList;
+    }
+
     public function __construct($dependencyName)
     {
-        parent::__construct(new Kwf_Assets_ProviderList_Default(), $dependencyName);
+        parent::__construct(self::_getDefaultProviderList(), $dependencyName);
     }
 
     public function toUrlParameter()
@@ -35,5 +44,20 @@ class Kwf_Assets_Dependency_Package_Default extends Kwf_Assets_Dependency_Packag
     {
         $dependencyName = $parameter;
         return self::getInstance($dependencyName);
+    }
+
+    public function serialize()
+    {
+        $pl = $this->_providerList;
+        unset($this->_providerList); //don't serialize _providerList, will be set to default on unserialize
+        $ret = parent::serialize();
+        $this->_providerList = $pl;
+        return $ret;
+    }
+
+    public function unserialize($serialized)
+    {
+        parent::unserialize($serialized);
+        $this->_providerList = self::_getDefaultProviderList();
     }
 }
