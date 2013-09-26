cb123cbce56e8091d40267eb4cf0c59853b0fb70
Franz Unger <ufx@vivid-planet.com>
2013-09-26 15:25:44 +0200

    Make AnchorField working when LinkTag is not saved yet

--- Kwc/Basic/LinkTag/Intern/AnchorsController.php
+++ Kwc/Basic/LinkTag/Intern/AnchorsController.php
@@ -11,13 +11,9 @@ class Kwc_Basic_LinkTag_Intern_AnchorsController extends Kwf_Controller_Action_A
     protected function _fetchData($order, $limit, $start)
     {
         $ret = array();
-        $componentId = $this->_getParam('componentId') . '-linkTag-child';
         $component = Kwf_Component_Data_Root::getInstance()
-            ->getComponentById($componentId, array('ignoreVisible' => true));
-        if ($component && $component->getComponent() instanceof Kwc_Basic_LinkTag_Intern_Component) {
-            $target = $component->getComponent()->getRow()->target;
-            $component = Kwf_Component_Data_Root::getInstance()
-                ->getComponentById($target, array('ignoreVisible' => true));
+            ->getComponentById($this->_getParam('target'), array('ignoreVisible' => true));
+        if ($component) {
             foreach ($component->getChildComponents(array('flag' => 'hasAnchors')) as $component) {
                 foreach ($component->getComponent()->getAnchors() as $anchor => $name) {
                     $ret[] = array('id' => $anchor, 'name' => '#' . $name);
--- Kwc/Basic/LinkTag/Intern/Form.php
+++ Kwc/Basic/LinkTag/Intern/Form.php
@@ -12,7 +12,8 @@ class Kwc_Basic_LinkTag_Intern_Form extends Kwc_Abstract_Form
 
         $this->add(new Kwf_Form_Field_Select('anchor', trlKwf('Anchor')))
             ->setValues(Kwc_Admin::getInstance($class)->getControllerUrl('Anchors') . '/json-data')
-            ->setShowNoSelection(true);
+            ->setShowNoSelection(true)
+            ->setXtype('anchor');
     }
 
     public function prepareSave($parentRow, $postData)
