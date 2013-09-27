d50ecdc9a03a292f9eca09827eb8dd75912d4113
Mario Kemetinger <mk@vivid-planet.com>
2013-09-27 12:12:11 +0200

    Don't show alt text in form when useParentImage is true

--- Kwc/Basic/Image/Form.php
+++ Kwc/Basic/Image/Form.php
@@ -8,5 +8,12 @@ class Kwc_Basic_Image_Form extends Kwc_Abstract_Image_Form
         }
     }
 
+    protected function _initFieldsAltText()
+    {
+        if (!Kwc_Abstract::getSetting($this->getClass(), 'useParentImage')) {
+            parent::_initFieldsAltText();
+        }
+    }
+
     //imageCaption field will be shown if activated
 }
