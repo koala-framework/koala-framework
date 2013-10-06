df8d9f46deb23b96373a629e44c066bb5c1698e6
Niko Sams <niko.sams@gmail.com>
2013-10-06 14:15:29 +0200

    Make MultiBox boxes useful
    - get multibox components for Master.tpl
    - sort by priority
    - add helper for output
    - add example usage in GreyBox theme

--- Kwf/Component/View/Helper/ComponentWithMaster.php
+++ Kwf/Component/View/Helper/ComponentWithMaster.php
@@ -1,6 +1,14 @@
 <?php
 class Kwf_Component_View_Helper_ComponentWithMaster extends Kwf_Component_View_Helper_Component
 {
+    private static function _sortByPriority(Kwf_Component_Data $c1, Kwf_Component_Data $c2)
+    {
+        if ($c1->priority == $c2->priority) {
+            return 0;
+        }
+        return ($c1->priority > $c2->priority) ? -1 : 1;
+    }
+
     public function componentWithMaster(array $componentWithMaster)
     {
         $last = array_pop($componentWithMaster);
@@ -17,6 +25,16 @@ class Kwf_Component_View_Helper_ComponentWithMaster extends Kwf_Component_View_H
             foreach ($innerComponent->getPageOrRoot()->getChildBoxes() as $box) {
                 $vars['boxes'][$box->box] = $box;
             }
+
+            $vars['multiBoxes'] = array();
+            foreach ($innerComponent->getPageOrRoot()->getRecursiveChildComponents(array('multiBox'=>true)) as $box) {
+                $vars['multiBoxes'][$box->box][] = $box;
+            }
+            //sort by priority
+            foreach ($vars['multiBoxes'] as $box=>$components) {
+                usort($vars['multiBoxes'][$box], array('Kwf_Component_View_Helper_ComponentWithMaster', '_sortByPriority'));
+            }
+
             $vars['cssClass'] = 'frontend';
             $cls = Kwc_Abstract::getSetting($component->componentClass, 'processedCssClass');
             foreach (explode(' ', $cls) as $i) {
