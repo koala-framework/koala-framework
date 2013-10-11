3505d2f406cc02385a301b320ad092a0df3def67
Niko Sams <ns@vivid-planet.com>
2013-10-11 11:29:19 +0200

    fix test, model now requires anchor column

--- tests/Kwc/Basic/LinkIntern/LinkTag/Model.php
+++ tests/Kwc/Basic/LinkIntern/LinkTag/Model.php
@@ -5,6 +5,7 @@ class Kwc_Basic_LinkIntern_LinkTag_Model extends Kwc_Basic_LinkTag_Intern_Model
     {
         $config['proxyModel'] = new Kwf_Model_FnF(array(
             'primaryKey' => 'component_id',
+            'columns' => array(),
             'data'=> array(
                 array('component_id'=>'1', 'target'=>'3'),
             )
