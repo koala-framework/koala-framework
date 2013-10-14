c5683da329c20acfee00547718f9dcb256962e72
Michael Freudenthaler <mf@vivid-planet.com>
2013-10-14 14:24:36 +0200

    display pagePath at search results

--- themes/RedMallee/FulltextSearch/Search/Detail/Component.php
+++ themes/RedMallee/FulltextSearch/Search/Detail/Component.php
@@ -6,4 +6,22 @@ class RedMallee_FulltextSearch_Search_Detail_Component extends Kwc_FulltextSearc
         $ret = parent::getSettings();
         return $ret;
     }
+
+    public function getTemplateVars()
+    {
+        $ret = parent::getTemplateVars();
+        $title = array();
+        $row = $ret['data']->row->data->getPage();
+        if (!$row) {
+            return null;
+        }
+        do {
+            if ($row->name != '') {
+                $title[] = $row->name;
+            }
+        } while ($row = $row->getParentPage());
+        $pagePath = array_reverse($title);
+        $ret['pagePath'] = implode(' » ', $pagePath);
+        return $ret;
+    }
 }
