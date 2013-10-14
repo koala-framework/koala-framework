27ba3a19e57dbbdc8837903d93a043fa9e9c29ba
Niko Sams <ns@vivid-planet.com>
2013-10-14 10:44:44 +0200

    add dynamic provider for tests, fixes google map test

--- Kwf/Assets/Package/TestPackage.php
+++ Kwf/Assets/Package/TestPackage.php
@@ -20,6 +20,7 @@ class Kwf_Assets_Package_TestPackage extends Kwf_Assets_Package
         $providers[] = new Kwf_Assets_Provider_Ini(KWF_PATH.'/dependencies.ini');
         $providers[] = new Kwf_Assets_Provider_Ini($testDependenciesIni);
         $providers[] = new Kwf_Assets_Provider_Components($rootComponentClass);
+        $providers[] = new Kwf_Assets_Provider_Dynamic();
         $providerList = new Kwf_Assets_ProviderList_Abstract($providers);
         parent::__construct($providerList, $dependencyName);
     }
