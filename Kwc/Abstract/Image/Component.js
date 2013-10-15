0960c7cf8c2d0a486b6a2508541c60c74e793252
Niko Sams <ns@vivid-planet.com>
2013-10-15 16:24:46 +0200

    always set inline style with to fix dpr2 image size display
    problem is that we use width: auto in css

--- Kwc/Abstract/Image/Component.js
+++ Kwc/Abstract/Image/Component.js
@@ -15,18 +15,13 @@ Kwf.onContentReady(function(readyEl) {
         el.dom.imgSizeInitDone = true;
         //img tags that set width/height: auto in css don't have size until they are loaded
         //move the size attribute into inline style with respecting aspect ratio
-        if (s.height == 0) {
-            el.dom.style.height = el.dom.getAttribute('height')+'px';
-            if (el.getHeight() < el.dom.getAttribute('height')) {
-                var ratio = el.dom.getAttribute('width') / el.dom.getAttribute('height');
-                el.dom.style.width = (ratio * el.getHeight())+'px';
-            }
-        } else if (s.width == 0) {
-            el.dom.style.width = el.dom.getAttribute('width')+'px';
-            if (el.getWidth() < el.dom.getAttribute('width')) {
-                var ratio = el.dom.getAttribute('height') / el.dom.getAttribute('width');
-                el.dom.style.height = (ratio * el.getWidth())+'px';
-            }
+        //also always set width as style for dpr2 images as width: auto would display them in double size
+        el.dom.style.width = el.dom.getAttribute('width')+'px';
+        el.dom.style.height = el.dom.getAttribute('height')+'px';
+        if (el.getWidth() < el.dom.getAttribute('width')) {
+            //image is responsive, adapt height accordingly
+            var ratio = el.dom.getAttribute('height') / el.dom.getAttribute('width');
+            el.dom.style.height = (ratio * el.getWidth())+'px';
         }
         el.on('load', function() {
             //once the img is loaded remove the style again and let css with: auto do it's work
