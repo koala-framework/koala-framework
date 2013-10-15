0fa06bbf11867e1acdded0e2b6b6e020a96fd205
Mario Kemetinger <mk@vivid-planet.com>
2013-10-15 10:32:35 +0200

    Just set Width as inline-style on img tag

--- Kwc/Abstract/Image/Component.js
+++ Kwc/Abstract/Image/Component.js
@@ -8,29 +8,32 @@ if (window.devicePixelRatio && window.devicePixelRatio > 1) {
     });
 }
 
-//resize image to 100% container width if image is larger than container
 Kwf.onContentReady(function(readyEl) {
     Ext.get(readyEl).select('img', true).each(function(el) {
         var s = el.getSize();
-        if (s.height == 0 || s.width == 0) {
-            //img tags that set width/height: auto in css don't have size until they are loaded
-            //move the size attribute into inline style with respecting aspect ratio
+        if (el.dom.imgSizeInitDone) return;
+        el.dom.imgSizeInitDone = true;
+        //img tags that set width/height: auto in css don't have size until they are loaded
+        //move the size attribute into inline style with respecting aspect ratio
+        if (s.height == 0) {
             el.dom.style.height = el.dom.getAttribute('height')+'px';
             if (el.getHeight() < el.dom.getAttribute('height')) {
                 var ratio = el.dom.getAttribute('width') / el.dom.getAttribute('height');
                 el.dom.style.width = (ratio * el.getHeight())+'px';
             }
+        }
+        if (s.width == 0) {
             el.dom.style.width = el.dom.getAttribute('width')+'px';
             if (el.getWidth() < el.dom.getAttribute('width')) {
                 var ratio = el.dom.getAttribute('height') / el.dom.getAttribute('width');
                 el.dom.style.height = (ratio * el.getWidth())+'px';
             }
-            el.on('load', function() {
-                //once the img is loaded remove the style again and let css with: auto do it's work
-                //required to be able to react to browser window change
-                this.style.width = '';
-                this.style.height = '';
-            }, el.dom);
         }
+        el.on('load', function() {
+            //once the img is loaded remove the style again and let css with: auto do it's work
+            //required to be able to react to browser window change
+            //this.style.width = ''; //don't remove width as that would break with dpr2 images
+            this.style.height = '';
+        }, el.dom);
     }, this);
 }, this, { priority: -1 });
