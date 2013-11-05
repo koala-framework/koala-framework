2c1dc58704a0f98ff68c46af0bdc9d8a7d3e5d62
Benjamin Hohenwarter <bh@vivid-planet.com>
2013-11-05 15:09:57 +0100

    Add update-script to allow null-values for crop-data

---
+++ Kwc/Abstract/Image/Update/40001.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `kwc_basic_image` CHANGE `crop_x` `crop_x` INT( 11 ) NULL;
+ALTER TABLE `kwc_basic_image` CHANGE `crop_y` `crop_y` INT( 11 ) NULL;
+ALTER TABLE `kwc_basic_image` CHANGE `crop_width` `crop_width` INT( 11 ) NULL;
+ALTER TABLE `kwc_basic_image` CHANGE `crop_height` `crop_height` INT( 11 ) NULL;
