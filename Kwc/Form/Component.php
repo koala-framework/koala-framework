4794bc268713d5c18258327dba3b4dc40e742d6c
Mario Kemetinger <mk@vivid-planet.com>
2013-11-07 12:23:09 +0100

    final changes and cleanings

--- Kwc/Form/Component.php
+++ Kwc/Form/Component.php
@@ -43,7 +43,7 @@ class Kwc_Form_Component extends Kwc_Abstract_Composite_Component
 
         $ret['errorStyle'] = null; //default from config.ini: kwc.form.errorStyle
 
-        $ret['cssClass'] = 'responsive default';
+        $ret['cssClass'] = 'default';
 
         return $ret;
     }
