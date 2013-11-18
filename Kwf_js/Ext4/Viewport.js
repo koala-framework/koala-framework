d4f43a4ab048737264234c9ba9505e1335c42ad7
Niko Sams <ns@vivid-planet.com>
2013-11-18 10:47:55 +0100

    ext4 viewport: center region by default

--- Kwf_js/Ext4/Viewport.js
+++ Kwf_js/Ext4/Viewport.js
@@ -3,6 +3,7 @@ Ext4.define('Kwf.Ext4.Viewport', {
     uses: ['Kwf.Ext4.Menu', 'Ext.layout.container.Border'],
     layout: 'border',
     initComponent : function() {
+        this.items[0].region = 'center';
         this.items.push(Ext4.create('Kwf.Ext4.Menu', {
             region: 'north',
             height: 30,
