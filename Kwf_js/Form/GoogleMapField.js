806abf5b49eee779f286b6e49b4ec7a34218da85
Michael Freudenthaler <mf@vivid-planet.com>
2013-11-28 10:11:59 +0100

    fixed the google map backend
    show window after the map is loaded
    
    Conflicts:
    	Vps_js/Form/GoogleMapField.js

--- Kwf_js/Form/GoogleMapField.js
+++ Kwf_js/Form/GoogleMapField.js
@@ -4,32 +4,34 @@ Kwf.Form.GoogleMapsField = Ext.extend(Ext.form.TriggerField,
 	readOnly : false,
 	width : 200,
 	onTriggerClick : function(){
+        Kwf.GoogleMap.load(function(){
         var win = Kwf.Form.GoogleMapsField.GoogleMapWindow; //statische var, nur ein window erstellen??
-        if (!win) {
-            win = new Kwf.Form.GoogleMapWindow({
-				modal: true,
-				title: trlKwf('Select your Position'),
-				width:535,
-				height:500,
-				shadow:true,
-				closeAction: 'hide'
-            });
-            Kwf.Form.GoogleMapsField.GoogleMapWindow = win;
-        }
-		win.purgeListeners();
-        win.on('confirm', function(win, ch) {
-			this.setValue(win.getMarkerPoint());
+            if (!win) {
+                win = new Kwf.Form.GoogleMapWindow({
+                    modal: true,
+                    title: trlKwf('Select your Position'),
+                    width:535,
+                    height:500,
+                    shadow:true,
+                    closeAction: 'hide'
+                });
+                Kwf.Form.GoogleMapsField.GoogleMapWindow = win;
+            }
+            win.purgeListeners();
+            win.on('confirm', function(win, ch) {
+                this.setValue(win.getMarkerPoint());
+            }, this);
+            win.on('clear', function(win, ch) {
+                this.setValue("");
+            }, this);
+            if (this.allowBlank){
+                win.setHideClearButton(false);
+            } else {
+                win.setHideClearButton(true);
+            }
+            win.show();
+            win.setMarkerPoint(this.getValue());
         }, this);
-		win.on('clear', function(win, ch) {
-			this.setValue("");
-        }, this);
-		if (this.allowBlank){
-			win.setHideClearButton(false);
-		} else {
-			win.setHideClearButton(true);
-		}
-		win.show();
-		win.setMarkerPoint(this.getValue());
     }
 });
 Ext.reg('googlemapsfield', Kwf.Form.GoogleMapsField);
