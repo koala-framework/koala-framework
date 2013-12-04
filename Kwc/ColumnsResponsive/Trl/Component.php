446da24b3c9ac5accfb5709464f31221d2175289
Alexander Kaufmann <ak@vivid-planet.com>
2013-12-04 09:39:12 +0100

    added trl support for ColumnsResponsive

---
+++ Kwc/ColumnsResponsive/Trl/Component.php
@@ -0,0 +1,12 @@
+<?php
+class Kwc_ColumnsResponsive_Trl_Component extends Kwc_Abstract_List_Trl_Component
+{
+    public static function getSettings($masterComponentClass)
+    {
+        $ret = Kwc_Chained_Trl_Component::getSettings($masterComponentClass);
+        unset($ret['childModel']);
+        $ret['generators']['child']['class'] = 'Kwc_Chained_Trl_Generator';
+        $ret['extConfig'] = 'Kwc_Abstract_List_Trl_ExtConfigList';
+        return $ret;
+    }
+}
