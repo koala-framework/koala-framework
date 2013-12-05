6fbf10b121909bf42627db0c22491dd57d450d6a
Niko Sams <ns@vivid-planet.com>
2013-12-05 09:57:01 +0100

    fix test: correctly test for minimum content with

--- tests/Kwc/ColumnsResponsive/Basic/ContentWidthTest.php
+++ tests/Kwc/ColumnsResponsive/Basic/ContentWidthTest.php
@@ -9,10 +9,10 @@ class Kwc_ColumnsResponsive_Basic_ContentWidthTest extends Kwc_TestAbstract
     public function test2ColumnsWith50_50()
     {
         $c = $this->_root->getComponentById('3000-1-1');
-        $this->assertEquals(450, (int)$c->getComponent()->getContentWidth());
+        $this->assertEquals(480, (int)$c->getComponent()->getContentWidth());
 
         $c = $this->_root->getComponentById('3000-1-2');
-        $this->assertEquals(450, (int)$c->getComponent()->getContentWidth());
+        $this->assertEquals(480, (int)$c->getComponent()->getContentWidth());
     }
 
     public function test2ColumnsWith75_25()
@@ -21,30 +21,30 @@ class Kwc_ColumnsResponsive_Basic_ContentWidthTest extends Kwc_TestAbstract
         $this->assertEquals(675, (int)$c->getComponent()->getContentWidth());
 
         $c = $this->_root->getComponentById('3000-2-2');
-        $this->assertEquals(225, (int)$c->getComponent()->getContentWidth());
+        $this->assertEquals(480, (int)$c->getComponent()->getContentWidth());
     }
 
     public function test3ColumnsWith33_33_33()
     {
         $c = $this->_root->getComponentById('3000-3-1');
-        $this->assertEquals(300, (int)$c->getComponent()->getContentWidth());
+        $this->assertEquals(480, (int)$c->getComponent()->getContentWidth());
 
         $c = $this->_root->getComponentById('3000-3-2');
-        $this->assertEquals(300, (int)$c->getComponent()->getContentWidth());
+        $this->assertEquals(480, (int)$c->getComponent()->getContentWidth());
 
         $c = $this->_root->getComponentById('3000-3-3');
-        $this->assertEquals(300, (int)$c->getComponent()->getContentWidth());
+        $this->assertEquals(480, (int)$c->getComponent()->getContentWidth());
     }
 
     public function test3ColumnsWith25_50_25()
     {
         $c = $this->_root->getComponentById('3000-4-1');
-        $this->assertEquals(225, (int)$c->getComponent()->getContentWidth());
+        $this->assertEquals(480, (int)$c->getComponent()->getContentWidth());
 
         $c = $this->_root->getComponentById('3000-4-2');
-        $this->assertEquals(450, (int)$c->getComponent()->getContentWidth());
+        $this->assertEquals(480, (int)$c->getComponent()->getContentWidth());
 
         $c = $this->_root->getComponentById('3000-4-3');
-        $this->assertEquals(225, (int)$c->getComponent()->getContentWidth());
+        $this->assertEquals(480, (int)$c->getComponent()->getContentWidth());
     }
 }
