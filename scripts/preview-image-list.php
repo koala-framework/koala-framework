45fb59c215d30b63d6e2273e06337528468ae171
Benjamin Hohenwarter <bh@vivid-planet.com>
2013-12-09 14:30:11 +0100

    Add script to display every image component with alternative preview-image set

---
+++ scripts/preview-image-list.php
@@ -0,0 +1,15 @@
+<?php
+$data = Kwf_Registry::get('db')->query('SELECT * FROM `kwc_basic_image` WHERE data like \'%preview_image":true%\'')->fetchAll();
+foreach ($data as $row) {
+    $component = Kwf_Component_Data_Root::getInstance()->getComponentByDbId($row['component_id'], array('ignoreVisible' => false));
+    if ($component) {
+        if ($component->componentClass != 'Kwc_Basic_LinkTag_Empty_Component') {
+            if ($component->getComponent()->getRow()->kwf_upload_id) {
+                //d($component->componentClass);
+                $components[$component->getAbsoluteUrl()] = '<a href="'.$component->getAbsoluteUrl().'">'.$component->getAbsoluteUrl().'</a>';
+            }
+        }
+//         $components[] = $component->getUrl();
+    }
+}
+d(array_values($components));
