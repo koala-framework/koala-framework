4078f73701f4cc31780f9f4985d57ee3a0e9792d
Daniel Bacher <db@vivid-planet.com>
2013-12-18 08:29:58 +0100

    Added verification for tel:// to url field
    - link type works for Android and iOs - windows does add the slashes

--- Kwc/Basic/LinkTag/Extern/Form.php
+++ Kwc/Basic/LinkTag/Extern/Form.php
@@ -8,7 +8,8 @@ class Kwc_Basic_LinkTag_Extern_Form extends Kwc_Abstract_Form
         $this->add(new Kwf_Form_Field_UrlField('target', trlKwf('Url')))
             ->setWidth(450)
             ->setHelpText(hlpKwf('kwc_basic_linktag_extern_target'))
-            ->setAllowBlank(false);
+            ->setAllowBlank(false)
+            ->setVtype('urltel');
 
         if (Kwc_Abstract::getSetting($class, 'hasPopup')) {
 
