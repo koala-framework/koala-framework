1c939a16ad81cbd725358f610521302df0394156
Mario Kemetinger <mk@vivid-planet.com>
2013-12-18 11:13:45 +0100

    FullWidthImage set correct class

--- Kwc/Basic/FullWidthImage/Component.php
+++ Kwc/Basic/FullWidthImage/Component.php
@@ -1,5 +1,5 @@
 <?php
-class Kwc_FullWidthImage_Component extends Kwc_TextImage_ImageEnlarge_Component
+class Kwc_Basic_FullWidthImage_Component extends Kwc_TextImage_ImageEnlarge_Component
 {
     public static function getSettings()
     {
