f3f9cadb8b44781995806ba6c2fbf4b3db55750c
Mario Kemetinger <mk@vivid-planet.com>
2013-12-18 11:03:27 +0100

    New Component - FullWidthImage

---
+++ Kwc/Basic/FullWidthImage/Component.php
@@ -0,0 +1,19 @@
+<?php
+class Kwc_FullWidthImage_Component extends Kwc_TextImage_ImageEnlarge_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['componentName'] = trlStatic('Picture 100% width');
+        $ret['showHelpText'] = true;
+        $ret['dimensions'] = array(
+            'default'=>array(
+                'text' => trlKwf('full width'),
+                'width' => self::CONTENT_WIDTH,
+                'height' => 0,
+                'cover' => true
+            )
+        );
+        return $ret;
+    }
+}
