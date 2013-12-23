c66c70dd4ebd1626eae92a906fc9b50ab9f9b594
Niko Sams <niko.sams@gmail.com>
2013-12-23 21:09:06 +0100

    recaptcha: reload on error as solving two times the same isn't valid

--- Kwf_js/FrontendForm/Recaptcha.js
+++ Kwf_js/FrontendForm/Recaptcha.js
@@ -1,6 +1,9 @@
 Kwf.FrontendForm.Recaptcha = Ext.extend(Kwf.FrontendForm.Field, {
     getFieldName: function() {
         return this.el.child('div[data-fieldname]').dom.getAttribute('data-fieldname');
+    },
+    onError: function(message) {
+        Recaptcha.reload(); //there can be only a single Recaptcha on the page
     }
 });
 
