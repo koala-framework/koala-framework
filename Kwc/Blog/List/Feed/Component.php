810e1b6742242663c367ccb0de806fde09ff02c8
Niko Sams <niko.sams@gmail.com>
2014-01-07 07:57:42 +0100

    blog feed: enable view cache

--- Kwc/Blog/List/Feed/Component.php
+++ Kwc/Blog/List/Feed/Component.php
@@ -6,7 +6,6 @@ class Kwc_Blog_List_Feed_Component extends Kwc_Directories_List_Feed_Component
         $ret = parent::_getRssEntryByItem($item);
 
         $renderer = new Kwf_Component_Renderer_HtmlExport();
-        $renderer->setEnableCache(false);
         $ret['description'] = $renderer->renderComponent($item->getChildComponent('-content'));
 
         $ret['lastUpdate'] = strtotime($item->publish_date);
