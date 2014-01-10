2d2b467aa476280db65a85244820699a999c6d6e
Niko Sams <ns@vivid-planet.com>
2014-01-10 12:49:51 +0100

    update merged code to changed interface

--- Kwc/User/Login/Form/UseViewCachePlugin.php
+++ Kwc/User/Login/Form/UseViewCachePlugin.php
@@ -2,7 +2,7 @@
 class Kwc_User_Login_Form_UseViewCachePlugin extends Kwf_Component_Plugin_Abstract
     implements Kwf_Component_Plugin_Interface_UseViewCache
 {
-    public function useViewCache()
+    public function useViewCache($renderer)
     {
         return !isset($_REQUEST['redirect']);
     }
