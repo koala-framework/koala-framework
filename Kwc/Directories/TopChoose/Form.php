5d12c7956e939c3e85ebdc93eb85b64e16287308
Alexander Kaufmann <ak@vivid-planet.com>
2014-01-28 14:40:53 +0100

    load possible directories for topChoose dynamicly with remote store
    only shows directories in correct subroot

--- Kwc/Directories/TopChoose/Form.php
+++ Kwc/Directories/TopChoose/Form.php
@@ -5,25 +5,8 @@ class Kwc_Directories_TopChoose_Form extends Kwc_Abstract_Form
     {
         parent::__construct($name, $class);
 
-        $showDirectoryClass = Kwc_Abstract::getSetting($class, 'showDirectoryClass');
-
-        $directories = Kwf_Component_Data_Root::getInstance()
-            ->getComponentsByClass($showDirectoryClass, array('ignoreVisible' => true));
-
-        $values = array();
-        foreach ($directories as $directory) {
-            $title = $directory->getTitle();
-            if (Kwc_Abstract::hasSetting($class, 'componentNameShort')) {
-                $name = Kwc_Abstract::getSetting($class, 'componentNameShort');
-            } else {
-                $name = Kwc_Abstract::getSetting($class, 'componentName');
-            }
-            $name = Kwf_Trl::getInstance()->trlStaticExecute($name);
-            if ($title != $name) $title .= ' - ' . $name;
-            $values[$directory->dbId] = $title;
-        }
-
-        $this->fields->add(new Kwf_Form_Field_Select('directory_component_id', trlKwf('Show')), 300)
-            ->setValues($values);
+        $this->fields->add(new Kwf_Form_Field_Select('directory_component_id', trlKwf('Show'), 300))
+            ->setDisplayField('title')
+            ->setStoreUrl(Kwc_Admin::getInstance($class)->getControllerUrl('Directories').'/json-data');
     }
 }
