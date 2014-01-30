64619657ab87a5b706f7c24de6cc252f99f11736
Daniel Bacher <db@vivid-planet.com>
2014-01-30 17:41:11 +0100

    Added webMenu and default class

--- Kwc/Menu/Mobile/Component.php
+++ Kwc/Menu/Mobile/Component.php
@@ -5,7 +5,7 @@ class Kwc_Menu_Mobile_Component extends Kwc_Abstract
     {
         $ret = parent::getSettings();
         $ret['level'] = array('main');
-        $ret['cssClass'] = 'webListNone';
+        $ret['cssClass'] = 'webListNone webStandard webMenu default';
         $ret['placeholder']['menuLink'] = trlKwfStatic('Menu');
 
         $ret['assets']['dep'][] = 'mustache';
