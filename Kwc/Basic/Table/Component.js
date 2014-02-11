1c2f65a29d2886e43456af3598be56278fa48647
Niko Sams <ns@vivid-planet.com>
2014-02-11 11:49:28 +0100

    Improve Table Responsiveness

--- Kwc/Basic/Table/Component.js
+++ Kwc/Basic/Table/Component.js
@@ -1,1 +1,40 @@
-Kwf.Utils.ResponsiveEl('.kwcBasicTable', [{maxWidth: 635, cls: 'sm635'}, {maxWidth: 400, cls: 'sm400'}]);
+(function() {
+
+var addArrow = function(el) {
+    if (el.hasClass('flipScroll')) {
+        var tbody = el.child('tbody');
+        if (tbody.dom.scrollLeft == 0 && tbody.dom.scrollWidth > el.getWidth()) {
+            el.addClass('arrowRight');
+        } else {
+            el.removeClass('arrowRight');
+        }
+    }
+};
+var flipScroll = function() {
+    Ext.select('.kwcBasicTable.responsiveFlipScroll').each(function(el) {
+        if (el.hasClass('flipScroll')) {
+            if (el.getWidth() > el.dom.flipScrollSetAt) {
+                el.removeClass('flipScroll');
+            }
+        }
+        if (el.getWidth() < el.child('table').getWidth() && !el.hasClass('flipScroll')) {
+            el.addClass('flipScroll');
+            addArrow(el);
+            if (!el.dom.flipScrollSetAt) {
+                el.child('tbody').on('scroll', function() {
+                    addArrow(el);
+                });
+            }
+            el.dom.flipScrollSetAt = el.getWidth();
+        }
+    }, this);
+};
+
+Kwf.onContentReady(function() {
+    flipScroll();
+});
+Ext.fly(window).on('resize', function() {
+    flipScroll();
+}, { buffer: 100 })
+
+})();
