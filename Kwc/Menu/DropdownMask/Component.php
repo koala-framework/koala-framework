fb9f25599d179317e57b3489f63e77f5e566aed4
Daniel Bacher <db@vivid-planet.com>
2014-02-13 14:32:20 +0100

    Upgrade DropdownMask
    - config can be passed with maskParent => '#someId or .someClass'
    mask will be added to this element if passed, else mask will be prepended to body

--- Kwc/Menu/DropdownMask/Component.php
+++ Kwc/Menu/DropdownMask/Component.php
@@ -6,8 +6,20 @@ class Kwc_Menu_DropdownMask_Component extends Kwc_Menu_Dropdown_Component
         $ret = parent::getSettings();
         $ret['cssClass'] .= ' webListNone';
         $ret['assets']['dep'][] = 'jQuery';
+        // Define the mask parent node, if parent is not body, parents css position has to be "relative"
+        $ret['maskParent'] = 'body';
         return $ret;
     }
+
+    public function getTemplateVars()
+    {
+        $ret = parent::getTemplateVars();
+        $ret['config'] = array(
+            'maskParent' => $this->_getSetting('maskParent')
+        );
+        return $ret;
+    }
+
     protected function _getMenuData($parentData = null, $select = array())
     {
         $ret = parent::_getMenuData($parentData, $select);
