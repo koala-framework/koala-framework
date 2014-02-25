6cfb78d2e263e10298c2dbc1e1ed439b3f0f2432
Benjamin Hohenwarter <bh@vivid-planet.com>
2014-02-25 14:29:55 +0100

    Add special exception if first column is component_class

--- Kwc/Directories/Item/Directory/Controller.php
+++ Kwc/Directories/Item/Directory/Controller.php
@@ -29,6 +29,9 @@ class Kwc_Directories_Item_Directory_Controller extends Kwf_Controller_Action_Au
         if ($this->_columns[0] instanceof Kwf_Grid_Column_Button) {
             throw new Kwf_Exception("Override Controller and add at least one column (button must not be first)");
         }
+        if ($this->_columns[0] instanceof Kwf_Grid_Column && $this->_columns[0]->getName() == 'component_class') {
+            throw new Kwf_Exception("Override Controller and add at least one column (component_class must not be first)");
+        }
     }
 
     protected function _initColumns()
