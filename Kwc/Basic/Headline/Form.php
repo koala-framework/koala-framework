a533cd34b6390f29209e4c87da23c78550e3bcd1
Alexander Kaufmann <ak@vivid-planet.com>
2014-03-20 11:07:01 +0100

    get headlines with own function, for easily overriding

--- Kwc/Basic/Headline/Form.php
+++ Kwc/Basic/Headline/Form.php
@@ -5,18 +5,23 @@ class Kwc_Basic_Headline_Form extends Kwc_Abstract_Form
     {
         parent::_initFields();
         $this->fields->add(new Kwf_Form_Field_Select('headline_type', trlKwfStatic('Layer')))
-        ->setValues(array(
-                        'h1' => trlKwfStatic('Headline {0}', 1),
-                        'h2' => trlKwfStatic('Headline {0}', 2),
-                        'h3' => trlKwfStatic('Headline {0}', 3),
-                        'h4' => trlKwfStatic('Headline {0}', 4),
-                        'h5' => trlKwfStatic('Headline {0}', 5),
-                        'h6' => trlKwfStatic('Headline {0}', 6)
-        ));
+            ->setValues($this->_getHeadlines());
         $this->fields->add(new Kwf_Form_Field_TextField('headline1', trlKwfStatic('Text 1')))
             ->setWidth(450)
             ->setAllowBlank(false);
         $this->fields->add(new Kwf_Form_Field_TextField('headline2', trlKwfStatic('Text 2')))
             ->setWidth(450);
+    }
+
+    protected function _getHeadlines()
+    {
+        return array(
+            'h1' => trlKwfStatic('Headline {0}', 1),
+            'h2' => trlKwfStatic('Headline {0}', 2),
+            'h3' => trlKwfStatic('Headline {0}', 3),
+            'h4' => trlKwfStatic('Headline {0}', 4),
+            'h5' => trlKwfStatic('Headline {0}', 5),
+            'h6' => trlKwfStatic('Headline {0}', 6)
+        );
     }
 }
