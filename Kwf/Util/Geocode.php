31908a6e2030d8fafb31e37da0c0ebab7cac358b
Benjamin Hohenwarter <bh@vivid-planet.com>
2014-03-20 15:52:36 +0100

    Kwf_Util_Geolocation didn't use proxy if defined.
    This is needed in cases where you can't directly access another website
    and you need a proxy

--- Kwf/Util/Geocode.php
+++ Kwf/Util/Geocode.php
@@ -16,7 +16,18 @@ class Kwf_Util_Geocode
             'sensor' => 'false'
         );
 
-        $client = new Zend_Http_Client("http://maps.googleapis.com/maps/api/geocode/json");
+        $httpClientConfig = array(
+            'timeout' => 20,
+            'persistent' => false
+        );
+        $config = Kwf_Registry::get('config');
+        if ($config->http && $config->http->proxy && $config->http->proxy->host && $config->http->proxy->port) {
+            $httpClientConfig['adapter'] = 'Zend_Http_Client_Adapter_Proxy';
+            $httpClientConfig['proxy_host'] = $config->http->proxy->host;
+            $httpClientConfig['proxy_port'] = $config->http->proxy->port;
+        }
+
+        $client = new Zend_Http_Client("http://maps.googleapis.com/maps/api/geocode/json", $httpClientConfig);
         $client->setMethod(Zend_Http_Client::GET);
         $client->setParameterGet($getParams);
         $body = utf8_encode($client->request()->getBody());
