f52560d9b70b2715b9af1ea868a1c2f83f1d2d75
Franz Unger <ufx@vivid-planet.com>
2014-03-20 11:20:44 +0100

    Get content width correctly even when selected component is not an image component

--- Kwc/Abstract/Image/ImageFile.php
+++ Kwc/Abstract/Image/ImageFile.php
@@ -12,9 +12,13 @@ class Kwc_Abstract_Image_ImageFile extends Kwf_Form_Field_File
     {
         $ret = parent::load($row, $postData);
         $component = Kwf_Component_Data_Root::getInstance()->getComponentByDbId($row->component_id, array('ignoreVisible' => true));
-        if ($component) {
-            //component can be non-existent if it's in a not selected card
-            $ret[$this->getFieldName()]['contentWidth'] = $component->getComponent()->getMaxContentWidth();
+        if ($component) { //component can be non-existent if it's in a not selected card
+            if (is_instance_of($component->componentClass, 'Kwc_Abstract_Image_Component')) {
+                $contentWidth = $component->getComponent()->getMaxContentWidth();
+            } else {
+                $contentWidth = $component->getComponent()->getContentWidth();
+            }
+            $ret[$this->getFieldName()]['contentWidth'] = $contentWidth;
         }
         return $ret;
     }
