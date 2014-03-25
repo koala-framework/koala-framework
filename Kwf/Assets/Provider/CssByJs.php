6da9f5fc47cb02a40866af592d593343f863b954
Niko Sams <ns@vivid-planet.com>
2014-03-25 14:33:30 +0100

    Assts CssByJS: also provide scss assets for js assets, not just css

--- Kwf/Assets/Provider/CssByJs.php
+++ Kwf/Assets/Provider/CssByJs.php
@@ -7,9 +7,13 @@ class Kwf_Assets_Provider_CssByJs extends Kwf_Assets_Provider_Abstract
         if ($dependency->getMimeType() == 'text/javascript' && $dependency instanceof Kwf_Assets_Dependency_File) {
             $fn = $dependency->getFileName();
             if (substr($fn, -3) == '.js') {
-                $fn = substr($fn, 0, -3).'.css';
-                if (file_exists($fn)) {
-                    $ret[Kwf_Assets_Dependency_Abstract::DEPENDENCY_TYPE_REQUIRES][] = new Kwf_Assets_Dependency_File_Css($fn);
+                $fnCss = substr($fn, 0, -3).'.css';
+                if (file_exists($fnCss)) {
+                    $ret[Kwf_Assets_Dependency_Abstract::DEPENDENCY_TYPE_REQUIRES][] = new Kwf_Assets_Dependency_File_Css($fnCss);
+                }
+                $fnScss = substr($fn, 0, -3).'.scss';
+                if (file_exists($fnScss)) {
+                    $ret[Kwf_Assets_Dependency_Abstract::DEPENDENCY_TYPE_REQUIRES][] = new Kwf_Assets_Dependency_File_Scss($fnScss);
                 }
             }
         }
