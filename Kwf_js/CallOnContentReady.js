e25593727ef0f210b31945d25076551045f47652
Niko Sams <ns@vivid-planet.com>
2014-03-31 10:15:14 +0200

    don't recursively call onContentReady
    this makes profiling much easier as we don't get a deep stack.
    Performance shouldn't change

--- Kwf_js/CallOnContentReady.js
+++ Kwf_js/CallOnContentReady.js
@@ -18,38 +18,59 @@ if (!Kwf.isApp) {
     });
 }
 
+
+Kwf._onReadyIsCalling = false;
+Kwf._onReadyCallQueue = [];
 /**
  * @param element the added/changed dom element
  * @param options newRender (bool): if new elements have been added to the dom or just visiblity/width changed
  */
-Kwf.callOnContentReady = function(el, options) {
-    if (!options) options = {};
-    if (Ext.Element && el instanceof Ext.Element) el = el.dom;
-    if (jQuery && el instanceof jQuery) el = el.get();
-    if (typeof options.newRender === 'undefined') {
-        if (console && console.warn) console.warn('Please set option newRender: true|false on callOnContentReady');
-        options.newRender = true;
-    }
-    if (!el) {
-        if (console && console && console.warn) console.warn('Please pass element argument on callOnContentReady');
-    }
-
-    Kwf._readyHandlers.sort(function(a, b) {
-        return (a.options.priority || 0) - (b.options.priority || 0);
+Kwf.callOnContentReady = function(el, options)
+{
+    Kwf._onReadyCallQueue.push({
+        el: el,
+        options: options
     });
-    for (var i = 0; i < Kwf._readyHandlers.length; i++) {
-        var hndl = Kwf._readyHandlers[i];
-        if (hndl.selector == null) {
-            hndl.fn.call(hndl.scope || window, (el || document.body), options);
-        } else {
-            if (hndl.type == 'jquery') { // OnElementReady jQuery
-                Kwf._callOnJElementReady(hndl, el);
 
-            } else if (hndl.type == 'ext') { // OnElementReady ExtJS
-                Kwf._callOnElementReady(hndl, el);
+    if (Kwf._onReadyIsCalling) {
+        return;
+    }
+
+    while (Kwf._onReadyCallQueue.length) {
+        Kwf._onReadyIsCalling = true;
+        var queueEntry = Kwf._onReadyCallQueue.pop();
+        el = queueEntry.el;
+        options = queueEntry.options;
+
+        if (!options) options = {};
+        if (Ext.Element && el instanceof Ext.Element) el = el.dom;
+        if (jQuery && el instanceof jQuery) el = el.get();
+        if (typeof options.newRender === 'undefined') {
+            if (console && console.warn) console.warn('Please set option newRender: true|false on callOnContentReady');
+            options.newRender = true;
+        }
+        if (!el) {
+            if (console && console && console.warn) console.warn('Please pass element argument on callOnContentReady');
+        }
+
+        Kwf._readyHandlers.sort(function(a, b) {
+            return (a.options.priority || 0) - (b.options.priority || 0);
+        });
+        for (var i = 0; i < Kwf._readyHandlers.length; i++) {
+            var hndl = Kwf._readyHandlers[i];
+            if (hndl.selector == null) {
+                hndl.fn.call(hndl.scope || window, (el || document.body), options);
+            } else {
+                if (hndl.type == 'jquery') { // OnElementReady jQuery
+                    Kwf._callOnJElementReady(hndl, el);
+
+                } else if (hndl.type == 'ext') { // OnElementReady ExtJS
+                    Kwf._callOnElementReady(hndl, el);
+                }
             }
         }
     }
+    Kwf._onReadyIsCalling = false;
 };
 
 /**
