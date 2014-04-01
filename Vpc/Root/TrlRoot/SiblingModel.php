388e9071b20ec443d33098f1c888310222a33370
Michael Freudenthaler <mf@vivid-planet.com>
2014-04-01 14:56:21 +0200

    fixed a typing mistake
    kwc... I don't think so

--- Vpc/Root/TrlRoot/SiblingModel.php
+++ Vpc/Root/TrlRoot/SiblingModel.php
@@ -1,7 +1,7 @@
 <?php
 class Vpc_Root_TrlRoot_SiblingModel extends Vps_Model_Db
 {
-    protected $_table = 'kwc_trl_languages';
+    protected $_table = 'vpc_trl_languages';
     protected $_referenceMap = array(
         'sibling' => array(
             'refModelClass' => 'Vpc_Root_TrlRoot_Model',
