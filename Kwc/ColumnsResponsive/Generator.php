8cebc78e0b14202c28f82428d094185e27e41e21
Niko Sams <ns@vivid-planet.com>
2014-04-02 08:46:53 +0200

    fix duplicating ColumnsResponsive

--- Kwc/ColumnsResponsive/Generator.php
+++ Kwc/ColumnsResponsive/Generator.php
@@ -10,4 +10,23 @@ class Kwc_ColumnsResponsive_Generator extends Kwf_Component_Generator_Table
         $this->_getModel()->setData($parentData->componentClass, $parentData->dbId);
         return $this->_getModel()->getRows($select);
     }
+
+    public function duplicateChild($source, $parentTarget, Zend_ProgressBar $progressBar = null)
+    {
+        if ($progressBar) $progressBar->next();
+        $progressBar = null; //stop here, as getDuplicateProgressSteps doesn't go any deeper
+
+        if ($source->generator !== $this) {
+            throw new Kwf_Exception("you must call this only with the correct source");
+        }
+
+        //we don't need to duplicate the row, we just can re-use the same id as source had
+        $id = $this->_idSeparator . $source->row->{$this->_getModel()->getPrimaryKey()};
+        $target = array_pop($this->getChildData($parentTarget, array('id'=>$id, 'ignoreVisible'=>true, 'limit'=>1)));
+        if (!$target) {
+            return null;
+        }
+        Kwc_Admin::getInstance($source->componentClass)->duplicate($source, $target, $progressBar);
+        return $target;
+    }
 }
