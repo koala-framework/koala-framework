80388fbfd20323f7eb9aea5dd97d3688f339fdaa
Alexander Kaufmann <ak@vivid-planet.com>
2014-04-08 16:14:51 +0200

    optimized update to master -> search for all config.ini's

--- scripts/update-to-master.php
+++ scripts/update-to-master.php
@@ -6,8 +6,9 @@ echo "Changed $file to master\n";
 function glob_recursive($pattern, $flags = 0) {
     $files = glob($pattern, $flags);
     foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
-        if (dirname($dir) == './kwf-lib') continue;
-        if (dirname($dir) == './library') continue;
+        if (dirname($dir) == './kwf-lib' || $dir == './kwf-lib') continue;
+        if (dirname($dir) == './vkwf-lib' || $dir == './vkwf-lib') continue;
+        if (dirname($dir) == './library' || $dir == './library') continue;
         $files = array_merge($files, glob_recursive($dir.'/'.basename($pattern), $flags));
     }
     return $files;
@@ -30,7 +31,7 @@ function updateConfig() {
 }
 
 $files = glob_recursive('Component.php');
-$files[] = 'config.ini';
+$files = array_merge($files, glob_recursive('config.ini'));
 replaceFiles($files, 'Kwc_Columns_Component', 'Kwc_Legacy_Columns_Component');
 replaceFiles($files, 'Kwc_ColumnsResponsive_Component', 'Kwc_Columns_Component');
 replaceFiles($files, 'Kwc_Advanced_DyamicContent_Component', 'Kwc_Advanced_DynamicContent_Component');
