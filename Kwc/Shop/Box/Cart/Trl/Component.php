a57cc1c87b000f608a0fd14f56c42128c42845e0
Alexander Kaufmann <ak@vivid-planet.com>
2014-04-22 16:19:05 +0200

    if no chained is found delete link for cart

--- Kwc/Shop/Box/Cart/Trl/Component.php
+++ Kwc/Shop/Box/Cart/Trl/Component.php
@@ -5,7 +5,12 @@ class Kwc_Shop_Box_Cart_Trl_Component extends Kwc_Chained_Trl_Component
     {
         $ret = parent::getTemplateVars();
         foreach ($ret['links'] as $k=>$i) {
-            $ret['links'][$k]['component'] = Kwc_Chained_Trl_Component::getChainedByMaster($i['component'], $this->getData());
+            $chained = Kwc_Chained_Trl_Component::getChainedByMaster($i['component'], $this->getData());
+            if ($chained) {
+                $ret['links'][$k]['component'] = $chained;
+            } else {
+                unset($ret['links'][$k]);
+            }
         }
         return $ret;
     }
