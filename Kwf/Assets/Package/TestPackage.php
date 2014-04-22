f0646022277d02d312040f2a4518fd228aaf455c
Niko Sams <ns@vivid-planet.com>
2014-04-22 08:27:58 +0200

    fix dependencies for tests, they also require the error handler dependency

--- Kwf/Assets/Package/TestPackage.php
+++ Kwf/Assets/Package/TestPackage.php
@@ -24,6 +24,7 @@ class Kwf_Assets_Package_TestPackage extends Kwf_Assets_Package
         $providers[] = new Kwf_Assets_Provider_Dynamic();
         $providers[] = new Kwf_Assets_Provider_KwfUtils();
         $providers[] = new Kwf_Assets_Provider_DefaultAssets();
+        $providers[] = new Kwf_Assets_Provider_ErrorHandler();
         $providerList = new Kwf_Assets_ProviderList_Abstract($providers);
         parent::__construct($providerList, $dependencyName);
     }
