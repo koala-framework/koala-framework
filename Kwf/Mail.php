eae88f6d00396ef63d8dff7204cb35293515b3a9
Benjamin Hohenwarter <bh@vivid-planet.com>
2014-04-23 17:15:11 +0200

    Revert "Do urldecode before generating checksum."
    This reverts commit 7b5c12e19f1ff49650460fc0ea5a0079e05a9462.

--- Kwf/Mail.php
+++ Kwf/Mail.php
@@ -105,7 +105,7 @@ class Kwf_Mail extends Zend_Mail
                     $type = $parts[3];
                     $checksum = $parts[4];
                     $filename = $parts[6];
-                    if ($checksum != Kwf_Media::getChecksum($class, $id, $type, urldecode($filename))) {
+                    if ($checksum != Kwf_Media::getChecksum($class, $id, $type, $filename)) {
                         throw new Kwf_Exception_AccessDenied('Access to file not allowed.');
                     }
                     $output = Kwf_Media::getOutputWithoutCheckingIsValid($class, $id, $type);
