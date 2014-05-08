f31ee1b213f7586670be1b6bf4cc83a2fa3e322c
Niko Sams <ns@vivid-planet.com>
2014-05-08 08:56:57 +0200

    create scss/config folder

--- scripts/update-to-master.php
+++ scripts/update-to-master.php
@@ -3,3 +3,9 @@ $file = is_file('vkwf_branch') ? 'vkwf_branch' : 'kwf_branch';
 file_put_contents($file, "master\n");
 echo "Changed $file to master\n";
 
+if (!file_exists('scss')) {
+    mkdir('scss');
+    mkdir('scss/config');
+    file_put_contents('scss/config/.gitkeep', '');
+    system('git add scss/config/.gitkeep');
+}
