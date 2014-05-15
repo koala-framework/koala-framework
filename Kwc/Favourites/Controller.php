2a755ba3b3a4c888ccad865200e5d1a1dce406a4
Alexander Kaufmann <ak@vivid-planet.com>
2014-05-15 14:53:18 +0200

    activated view cache for favourites component
    searches for all favourites components on site per javascript
    one ajax requests checks all

--- Kwc/Favourites/Controller.php
+++ Kwc/Favourites/Controller.php
@@ -26,6 +26,23 @@ class Kwc_Favourites_Controller extends Kwf_Controller_Action
         }
     }
 
+    public function jsonGetFavouritesAction()
+    {
+        $rows = array();
+        $authedUser = Kwf_Registry::get('userModel')->getAuthedUser();
+        if ($authedUser) {
+            $modelClass = Kwc_Abstract::getSetting($this->_getParam('class'), 'favouritesModel');
+            $model = Kwf_Model_Abstract::getInstance($modelClass);
+            $select = new Kwf_Model_Select();
+            $select->whereEquals('user_id', $authedUser->id);
+            $select->whereEquals('component_id', $this->_getParam('kwcFavouritesComponentIds'));
+            foreach ($model->export(Kwf_Model_Abstract::FORMAT_ARRAY, $select, array('columns' => array('component_id'))) as $row) {
+                $rows[] = $row['component_id'];
+            }
+        }
+        $this->view->componentIds = $rows;
+    }
+
     protected function _isAllowedComponent()
     {
         return !!Kwf_Registry::get('userModel')->getAuthedUser();
