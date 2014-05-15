582d65987d4599c4166a49c397f72a982d9f16b3
Niko Sams <ns@vivid-planet.com>
2014-05-15 17:04:54 +0200

    improve update script
    - delete unused cache folders
    - create uglifyjs cache folder

--- scripts/update-to-master.php
+++ scripts/update-to-master.php
@@ -3,12 +3,47 @@ $file = is_file('vkwf_branch') ? 'vkwf_branch' : 'kwf_branch';
 file_put_contents($file, "master\n");
 echo "Changed $file to master\n";
 
+
+function deleteCacheFolder($path)
+{
+    if (!file_exists($path)) return;
+    system("git rm $path/.gitignore");
+    clearstatcache();
+    if (!file_exists($path)) return;
+
+    $it = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($path),
+        RecursiveIteratorIterator::CHILD_FIRST
+    );
+    foreach ($it as $file) {
+        if (in_array($file->getBasename(), array('.', '..'))) {
+            continue;
+        } elseif ($file->isDir()) {
+            rmdir($file->getPathname());
+        } elseif ($file->isFile() || $file->isLink()) {
+            unlink($file->getPathname());
+        }
+    }
+    rmdir($path);
+}
+
 if (!file_exists('scss')) {
     mkdir('scss');
     mkdir('scss/config');
     file_put_contents('scss/config/.gitkeep', '');
     system('git add scss/config/.gitkeep');
 }
+
+if (!is_dir('cache/uglifyjs')) {
+    mkdir('cache/uglifyjs');
+    file_put_contents('cache/uglifyjs/.gitignore', "*\n!.gitignore\n");
+    system("git add cache/uglifyjs/.gitignore");
+    echo "folder \"cache/uglifyjs\" created\n";
+}
+
+deleteCacheFolder('cache/events');
+deleteCacheFolder('cache/table');
+deleteCacheFolder('cache/trl');
 
 $c = file_get_contents(".gitignore");
 $c = trim($c)."\nbuild\n";
