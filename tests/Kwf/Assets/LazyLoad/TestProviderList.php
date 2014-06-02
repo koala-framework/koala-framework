b6810dbe98636c6d2020ad35b4c75f6103f4a93a
Niko Sams <niko.sams@gmail.com>
2014-06-02 22:52:02 +0200

    remove ext4 provider, not used

--- tests/Kwf/Assets/LazyLoad/TestProviderList.php
+++ tests/Kwf/Assets/LazyLoad/TestProviderList.php
@@ -4,7 +4,6 @@ class Kwf_Assets_LazyLoad_TestProviderList extends Kwf_Assets_ProviderList_Abstr
     public function __construct()
     {
         parent::__construct(array(
-            new Kwf_Assets_Ext4_Provider(),
             new Kwf_Assets_Provider_JsClassKwf(),
             new Kwf_Assets_Provider_JsClass(KWF_PATH.'/tests', 'kwf/tests'),
         ));
