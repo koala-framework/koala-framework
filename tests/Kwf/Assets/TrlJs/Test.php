66618f9cb9c42871cb9b3b7e64bec59e1cf374db
Niko Sams <ns@vivid-planet.com>
2014-06-03 17:15:48 +0200

    add test for js trl with context

--- tests/Kwf/Assets/TrlJs/Test.php
+++ tests/Kwf/Assets/TrlJs/Test.php
@@ -7,4 +7,14 @@ class Kwf_Assets_TrlJs_Test extends Kwf_Test_TestCase
         $c = $f->getContents('de');
         $this->assertEquals("trl('Ja');", trim($c));
     }
+
+    public function testWithContext()
+    {
+        $f = new Kwf_Assets_Dependency_File_Js('kwf/tests/Kwf/Assets/TrlJs/withContext.js');
+        $c = $f->getContents('de');
+        $this->assertEquals("trl(',')", trim($c));
+
+        $c = $f->getContents('en');
+        $this->assertEquals("trl('.')", trim($c));
+    }
 }
