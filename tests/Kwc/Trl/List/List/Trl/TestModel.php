23ebe4112adbd3bda7dc699015e46721900096a4
Michael Freudenthaler <mf@vivid-planet.com>
2014-06-06 09:22:19 +0200

    correctly support limit count in trl generator
    unset the limit as trl has it's own visible

--- tests/Kwc/Trl/List/List/Trl/TestModel.php
+++ tests/Kwc/Trl/List/List/Trl/TestModel.php
@@ -3,7 +3,8 @@ class Kwc_Trl_List_List_Trl_TestModel extends Kwf_Model_FnF
 {
     protected $_primaryKey = 'component_id';
     protected $_data = array(
-        array('component_id'=>'root-en_test-1', 'visible'=>1),
-        array('component_id'=>'root-en_test-2', 'visible'=>0)
+        array('component_id'=>'root-en_test-1', 'visible'=>0),
+        array('component_id'=>'root-en_test-2', 'visible'=>1),
+        array('component_id'=>'root-en_test-3', 'visible'=>0),
     );
 }
