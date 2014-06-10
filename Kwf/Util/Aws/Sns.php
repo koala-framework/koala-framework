881cf05e71b88e0acc868a9ded613d17bbdc39a0
Niko Sams <ns@vivid-planet.com>
2014-06-10 11:16:15 +0200

    add aws optional dependency, fixes s3 test

--- Kwf/Util/Aws/Sns.php
+++ Kwf/Util/Aws/Sns.php
@@ -1,5 +1,4 @@
 <?php
-require_once Kwf_Config::getValue('externLibraryPath.aws').'/sdk.class.php';
 class Kwf_Util_Aws_Sns extends AmazonSNS
 {
     public function __construct(array $options = array())
--- Kwf/Util/Aws/Sqs.php
+++ Kwf/Util/Aws/Sqs.php
@@ -1,5 +1,4 @@
 <?php
-require_once Kwf_Config::getValue('externLibraryPath.aws').'/sdk.class.php';
 class Kwf_Util_Aws_Sqs extends AmazonSQS
 {
     public function __construct(array $options = array())
