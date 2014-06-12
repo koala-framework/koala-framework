6c55b0fd8cac53a8bae570994cef57104401fb3e
Niko Sams <ns@vivid-planet.com>
2014-06-12 12:23:11 +0200

    add dependencies for TestPackage, fixes tests

--- Kwf/Assets/Package/TestPackage.php
+++ Kwf/Assets/Package/TestPackage.php
@@ -19,6 +19,10 @@ class Kwf_Assets_Package_TestPackage extends Kwf_Assets_Package
         $providers = array();
         $providers[] = new Kwf_Assets_Provider_Ini(KWF_PATH.'/dependencies.ini');
         $providers[] = new Kwf_Assets_Provider_Ini($testDependenciesIni);
+        $providers[] = new Kwf_Assets_Provider_Ini(VENDOR_PATH.'/koala-framework/library-extjs2/dependencies.ini');
+        $providers[] = new Kwf_Assets_Provider_Ini(VENDOR_PATH.'/koala-framework/library-jquery/dependencies.ini');
+        $providers[] = new Kwf_Assets_Provider_Ini(VENDOR_PATH.'/koala-framework/library-swfobject/dependencies.ini');
+        $providers[] = new Kwf_Assets_Provider_Ini(VENDOR_PATH.'/koala-framework/library-swfupload/dependencies.ini');
         $providers[] = new Kwf_Assets_Provider_IniNoFiles();
         $providers[] = new Kwf_Assets_Provider_Components($rootComponentClass);
         $providers[] = new Kwf_Assets_Provider_Dynamic();
