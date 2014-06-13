46ed7fb6349921a912a5b037dfaba467f30a90fd
Niko Sams <ns@vivid-planet.com>
2014-06-13 08:58:42 +0200

    fix tests, configure provider correctly

--- tests/Kwf/Assets/LazyLoad/TestProviderList.php
+++ tests/Kwf/Assets/LazyLoad/TestProviderList.php
@@ -5,6 +5,7 @@ class Kwf_Assets_LazyLoad_TestProviderList extends Kwf_Assets_ProviderList_Abstr
     {
         parent::__construct(array(
             new Kwf_Assets_Provider_JsClassKwf(),
+            new Kwf_Assets_Provider_JsClass(KWF_PATH.'/tests/Kwf/Assets/LazyLoad', 'kwf/tests/Kwf/Assets/LazyLoad', 'Kwf.Assets.LazyLoad'),
             new Kwf_Assets_Provider_Ini(dirname(__FILE__).'/TestDependencies.ini'),
         ));
     }
