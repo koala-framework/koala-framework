1eb107d751eacbb8a8af618f8419cf18a91dca9b
Niko Sams <ns@vivid-planet.com>
2014-06-18 15:52:49 +0200

    fix parse error

--- scripts/update-to-master.php
+++ scripts/update-to-master.php
@@ -19,7 +19,7 @@ if (is_file('vkwf_branch')) {
     echo "removed kwf_branch file, composer.json is used instead\n";
 }
 if (!file_exists('composer.json')) {
-    file_put_contents('composer.json', json_encode($c, defined('JSON_PRETTY_PRINT') ? JSON_PRETTY_PRINT : 0) + defined('JSON_UNESCAPED_SLASHES') ? JSON_UNESCAPED_SLASHES : 0) );
+    file_put_contents('composer.json', json_encode($c, defined('JSON_PRETTY_PRINT') ? JSON_PRETTY_PRINT : 0) + (defined('JSON_UNESCAPED_SLASHES') ? JSON_UNESCAPED_SLASHES : 0) );
     echo "created efault composer.json\n";
 }
 
