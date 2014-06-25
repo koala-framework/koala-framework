8bfc5b3052598b28475406fddbf3e8e515cc811a
Niko Sams <ns@vivid-planet.com>
2014-06-25 11:33:20 +0200

    Newsletter: move form fields to FrontendForm
    allows easier overriding of especially the model

--- Kwc/Newsletter/Unsubscribe/Form/Component.php
+++ Kwc/Newsletter/Unsubscribe/Form/Component.php
@@ -16,27 +16,18 @@ class Kwc_Newsletter_Unsubscribe_Form_Component extends Kwc_Form_Component
     protected function _initForm()
     {
         parent::_initForm();
-        $this->_form->setModel(Kwf_Model_Abstract::getInstance('Kwc_Newsletter_Subscribe_Model'));
         if ($this->_recipient) {
             $this->_form->setId($this->_recipient->id);
         }
-
-        $this->getParentField()->add(new Kwf_Form_Field_ShowField('firstname_interface', trlKwfStatic('Firstname')))
-            ->setData(new Kwc_Newsletter_Unsubscribe_Form_RecipientData('getMailFirstname'));
-        $this->getParentField()->add(new Kwf_Form_Field_ShowField('lastname_interface', trlKwfStatic('Lastname')))
-            ->setData(new Kwc_Newsletter_Unsubscribe_Form_RecipientData('getMailLastname'));
-        $this->getParentField()->add(new Kwf_Form_Field_ShowField('email_interface', trlKwfStatic('E-Mail')))
-            ->setData(new Kwc_Newsletter_Unsubscribe_Form_RecipientData('getMailEmail'));
-    }
-
-    // Falls Unterkomponente will, das Felder zB in ein Fieldset hinzugefügt werden
-    protected function getParentField()
-    {
-        return $this->_form;
     }
 
     protected function _afterSave(Kwf_Model_Row_Interface $row)
     {
+        parent::_afterSave($row);
         $row->mailUnsubscribe();
     }
+
+    //moved to FrontendForm
+    protected final function getParentField()
+    {}
 }
