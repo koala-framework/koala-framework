55c2ae96842dececbe170e7f17fa727be30e9193
Niko Sams <ns@vivid-planet.com>
2014-06-25 16:21:18 +0200

    remove Ext4 javascript, that moved into other repositories

--- Kwf_js/Ext4/Store/CountriesModel.js
+++
@@ -1,9 +0,0 @@
-Ext4.define('Kwf.Ext4.Store.CountriesModel', {
-    extend: 'Ext.data.Model',
-    requires: ['Ext.data.UuidGenerator'],
-    idProperty: 'id',
-    fields: [
-        {name: 'id', type: 'string'},
-        {name: 'name', type: 'string'}
-    ]
-});
