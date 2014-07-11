be1c65c3ab2536c902816857b80569992b94867d
Franz Unger <ufx@vivid-planet.com>
2014-07-11 10:54:58 +0200

    Call attachEditable also in Trl Menu

--- Kwc/Menu/EditableItems/Trl/Component.php
+++ Kwc/Menu/EditableItems/Trl/Component.php
@@ -7,4 +7,13 @@ class Kwc_Menu_EditableItems_Trl_Component extends Kwc_Chained_Trl_Component
         $ret['extConfig'] = 'Kwc_Menu_EditableItems_ExtConfig';
         return $ret;
     }
+
+    public function attachEditableToMenuData(&$menuData)
+    {
+        foreach ($menuData as $k=>&$m) {
+            if (isset($m['editable']) && $m['editable']) {
+                $m['editable'] = Kwc_Chained_Trl_Component::getChainedByMaster($m['editable'], $this->getData());
+            }
+        }
+    }
 }
