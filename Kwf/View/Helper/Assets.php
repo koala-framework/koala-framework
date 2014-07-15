a2e7ea58d0e117504fa960de75dd640ff294b55f
Niko Sams <ns@vivid-planet.com>
2014-07-15 18:15:16 +0200

    also load css deferred

--- Kwf/View/Helper/Assets.php
+++ Kwf/View/Helper/Assets.php
@@ -8,7 +8,15 @@ class Kwf_View_Helper_Assets
         $indent = str_repeat(' ', 8);
         $ret = '';
         foreach ($assetsPackage->getPackageUrls('text/css', $language) as $file) {
-            $ret .= "$indent<link rel=\"stylesheet\" type=\"text/css\" href=\"".htmlspecialchars($file)."\" />\n";
+            if ($async) {
+                $ret .= "<script type=\"text/javascript\">
+                    var se=document.createElement('link');se.type='text/css';se.rel='stylesheet';
+                    se.href='".$file."';
+                    var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(se,s);
+                </script>";
+            } else {
+                $ret .= "$indent<link rel=\"stylesheet\" type=\"text/css\" href=\"".htmlspecialchars($file)."\" />\n";
+            }
         }
         foreach ($assetsPackage->getPackageUrls('text/css; media=print', $language) as $file) {
             $ret .= "$indent<link rel=\"stylesheet\" type=\"text/css\" href=\"".htmlspecialchars($file)."\" ";
@@ -20,9 +28,9 @@ class Kwf_View_Helper_Assets
         foreach ($assetsPackage->getPackageUrls('text/javascript', $language) as $file) {
             if ($async) {
                 $ret .= "<script type=\"text/javascript\">
-                    var se = document.createElement('script'); se.type = 'text/javascript'; se.async = true;
-                    se.src = '".$file."';
-                    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(se, s);
+                    var se=document.createElement('script');se.type='text/javascript';se.async=true;
+                    se.src='".$file."';
+                    var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(se,s);
                 </script>";
             } else {
                 $ret .= "$indent<script type=\"text/javascript\" src=\"".htmlspecialchars($file)."\"></script>\n";
