096116d79db34878c76869c132e1cd33cd112e93
Alexander Kaufmann <ak@vivid-planet.com>
2014-07-16 16:04:31 +0200

    youtube works now with hide and show support for lightbox
    on hide it will paused and on show it will be replayed if autoplay is on

--- Kwc/Advanced/Youtube/Component.js
+++ Kwc/Advanced/Youtube/Component.js
@@ -1,27 +1,36 @@
-Kwf.onElementHide('.kwcAdvancedYoutube', function(el) {
-    if (el.dom.player) {
-        el.dom.player.destroy();
-        el.dom.player = false;
-    }
-});
-
 Kwf.onElementReady('.kwcAdvancedYoutube .youtubePlayer', function(el, config) {
     var kwcAdvancedYoutube = el.findParent('.kwcAdvancedYoutube', 5, true);
-    Kwf.Utils.YoutubePlayer.load(function() {
-        var loadingEl = kwcAdvancedYoutube.child('.outerLoading');
-        loadingEl.enableDisplayMode('block');
-        loadingEl.hide();
-        var youtubePlayerId = el.id;
+    kwcAdvancedYoutube.dom.config = config;
+}, {priority: -1});
 
-        kwcAdvancedYoutube.dom.player = new YT.Player(youtubePlayerId, {
-            height: config.height,
-            width: config.width,
-            videoId: config.videoId,
-            playerVars: config.playerVars
-        });
-        if (config.size == 'custom') {
-            kwcAdvancedYoutube.dom.style.width = 'auto';
-            kwcAdvancedYoutube.dom.style.maxWidth = config.width + 'px';
-        }
-    }, this);
-}, { defer: true, checkVisibility: true });
+Kwf.onElementHide('.kwcAdvancedYoutube', function(el) {
+    if (el.dom.player) {
+        el.dom.player.pauseVideo();
+    }
+}, {defer: true});
+
+Kwf.onElementShow('.kwcAdvancedYoutube .youtubePlayer', function(el) {
+    var kwcAdvancedYoutube = el.findParent('.kwcAdvancedYoutube', 5, true);
+    var config = kwcAdvancedYoutube.dom.config;
+    if (kwcAdvancedYoutube.dom.player) {
+        if (config.playerVars.autoplay) kwcAdvancedYoutube.dom.player.playVideo();
+    } else {
+        Kwf.Utils.YoutubePlayer.load(function() {
+            var loadingEl = kwcAdvancedYoutube.child('.outerLoading');
+            loadingEl.enableDisplayMode('block');
+            loadingEl.hide();
+            var youtubePlayerId = el.createChild().id;
+
+            kwcAdvancedYoutube.dom.player = new YT.Player(youtubePlayerId, {
+                height: config.height,
+                width: config.width,
+                videoId: config.videoId,
+                playerVars: config.playerVars
+            });
+            if (config.size == 'custom') {
+                kwcAdvancedYoutube.dom.style.width = 'auto';
+                kwcAdvancedYoutube.dom.style.maxWidth = config.width + 'px';
+            }
+        }, this);
+    }
+}, {defer: true});
