d6f16373b405c9247826a949bd10a35562e8cb13
Alexander Kaufmann <ak@vivid-planet.com>
2014-07-17 14:13:54 +0200

    optimized parse for log files

--- Kwf/Controller/Action/Logs/CliController.php
+++ Kwf/Controller/Action/Logs/CliController.php
@@ -54,58 +54,42 @@ class Kwf_Controller_Action_Logs_CliController extends Kwf_Controller_Action_Cli
         if (in_array('log', $config) === false) throw new Kwf_Exception('add log updateTag in config');
 
         $model = Kwf_Model_Abstract::getInstance('Kwf_Log_Model');
-        if ($logHandle = opendir('log')) {
-            while (false !== ($logFolder = readdir($logHandle))) {
-                if (in_array($logFolder, $this->_ignore)) continue;
 
-                if ($logSubHandle = opendir('log/' . $logFolder)) {
-                    while (false !== ($logSubFolder = readdir($logSubHandle))) {
-                        if (in_array($logSubFolder, $this->_ignore)) continue;
+        $logMessage = array();
+        foreach (glob('log/*/*/*') as $filename) {
+            $fileInfo = pathinfo($filename);
+            $folders = explode('/', $fileInfo['dirname']);
+            $file = file_get_contents($filename);
 
-                        if ($logFilesHandle = opendir('log/' . $logFolder . '/' . $logSubFolder)) {
-                            while (false !== ($logFile = readdir($logFilesHandle))) {
-                                if (in_array($logFile, $this->_ignore)) continue;
+            for($i = 0; $i < count($this->_start); $i++) {
+                preg_match_all('/('.preg_quote($this->_start[$i]).')(.+?)('.preg_quote($this->_end[$i]).')/si', $file, $matches);
+                if (empty($matches[1])) continue;
 
-                                $file = file_get_contents('log/' . $logFolder . '/' . $logSubFolder . '/' . $logFiles);
-                                for($i = 0; $i < count($this->_start); $i++) {
-                                    preg_match_all('/('.preg_quote($this->_start[$i]).')(.+?)('.preg_quote($this->_end[$i]).')/si', $file, $matches);
-                                    if (empty($matches[1])) continue;
-
-                                    $logMessage[trim(str_replace('**', '', $matches[1][0]))] = trim($matches[2][0]);
-                                }
-
-                                $select = new Kwf_Model_Select();
-                                $select->whereEquals('filename', $logFile);
-                                if ($model->countRows($select) > 0) continue;
-
-                                $model->createRow(array(
-                                    'type' => $logFolder,
-                                    'exception' => $logMessage['Exception'],
-                                    'thrown' => $logMessage['Thrown'],
-                                    'message' => $logMessage['Message'],
-                                    'exception_detail' => $logMessage['ExceptionDetail'],
-                                    'request_uri' => $logMessage['REQUEST_URI'],
-                                    'http_referer' => $logMessage['HTTP_REFERER'],
-                                    'user' => $logMessage['User'],
-                                    'useragent' => $logMessage['_USERAGENT'],
-                                    'get' => $logMessage['_GET'],
-                                    'post' => $logMessage['_POST'],
-                                    'server' => $logMessage['_SERVER'],
-                                    'files' => $logMessage['_FILES'],
-                                    'session' => $logMessage['_SESSION'],
-                                    'date' => $logSubFolder . ' ' . $logMessage['Time']
-                                ))->save();
-                            }
-                            closedir($logFilesHandle);
-                        }
-                    }
-                    closedir($logSubHandle);
-                }
+                $logMessage[trim(str_replace('**', '', $matches[1][0]))] = trim($matches[2][0]);
             }
 
-            closedir($logHandle);
-        }
+            $select = new Kwf_Model_Select();
+            $select->whereEquals('filename', $fileInfo['filename']);
+            if ($model->countRows($select) > 0) continue;
 
+            $model->createRow(array(
+                'type' => $folders[1],
+                'exception' => $logMessage['Exception'],
+                'thrown' => $logMessage['Thrown'],
+                'message' => $logMessage['Message'],
+                'exception_detail' => $logMessage['ExceptionDetail'],
+                'request_uri' => $logMessage['REQUEST_URI'],
+                'http_referer' => $logMessage['HTTP_REFERER'],
+                'user' => $logMessage['User'],
+                'useragent' => $logMessage['_USERAGENT'],
+                'get' => $logMessage['_GET'],
+                'post' => $logMessage['_POST'],
+                'server' => $logMessage['_SERVER'],
+                'files' => $logMessage['_FILES'],
+                'session' => $logMessage['_SESSION'],
+                'date' => $folders[2] . ' ' . $logMessage['Time']
+            ))->save();
+        }
 
         $this->_helper->viewRenderer->setNoRender(true);
     }
