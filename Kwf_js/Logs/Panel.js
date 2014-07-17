b02a8e09e0ad83b0719155dc44d5589953fe96ee
Alexander Kaufmann <ak@vivid-planet.com>
2014-07-17 15:44:44 +0200

    added possibility to parse log files manually, delete all log files and delete all shown log files in db

--- Kwf_js/Logs/Panel.js
+++ Kwf_js/Logs/Panel.js
@@ -12,7 +12,74 @@ Kwf.Logs.Panel = Ext.extend(Ext.Panel, {
             region: 'center',
             bindings: [ form ]
         });
-
+        grid.actions.deleteAll = new Ext.Action({
+            text: trlKwf('Delete All'),
+            icon: '/assets/silkicons/bin_empty.png',
+            cls: 'x-btn-text-icon',
+            handler: function(){
+                Ext.Msg.confirm(
+                    trlKwf('Are you sure?'),
+                    trlKwf('Do you really want to delete all logs?'),
+                    function(result) {
+                        if (result == 'yes') {
+                            Ext.Ajax.request({
+                                url : this.controllerUrl + '/json-delete-all',
+                                params: grid.getBaseParams(),
+                                success: function(response, options, r) {
+                                    Ext.MessageBox.alert(trlKwf('Status'), r.message);
+                                    grid.reload();
+                                },
+                                scope: this
+                            });
+                        }
+                    },
+                    this
+                );
+            },
+            scope: this
+        });
+        grid.actions.parse = new Ext.Action({
+            text: trlKwf('Parse files'),
+            icon: '/assets/silkicons/magnifier.png',
+            cls: 'x-btn-text-icon',
+            handler: function() {
+                Ext.Ajax.request({
+                    url : this.controllerUrl + '/json-parse-files',
+                    params: grid.getBaseParams(),
+                    success: function(response, options, r) {
+                        Ext.MessageBox.alert(trlKwf('Status'), r.message);
+                        grid.reload();
+                    },
+                    scope: this
+                });
+            },
+            scope: this
+        });
+        grid.actions.deleteFiles = new Ext.Action({
+            text: trlKwf('Delete Files'),
+            icon: '/assets/silkicons/script_delete.png',
+            cls: 'x-btn-text-icon',
+            handler: function(){
+                Ext.Msg.confirm(
+                    trlKwf('Are you sure?'),
+                    trlKwf('Do you really want to delete all parsed Files?'),
+                    function(result) {
+                        if (result == 'yes') {
+                            Ext.Ajax.request({
+                                url : this.controllerUrl + '/json-delete-files',
+                                params: grid.getBaseParams(),
+                                success: function(response, options, r) {
+                                    Ext.MessageBox.alert(trlKwf('Status'), r.message);
+                                },
+                                scope: this
+                            });
+                        }
+                    },
+                    this
+                );
+            },
+            scope: this
+        });
 
 
         this.layout = 'border';
