4b88b07c07901fbea8bee8e0dd89695159beb420
Niko Sams <ns@vivid-planet.com>
2014-07-23 08:10:52 +0200

    add two helper actions for debugging frontend assets size

--- Kwf/Controller/Action/Cli/BuildController.php
+++ Kwf/Controller/Action/Cli/BuildController.php
@@ -35,4 +35,48 @@ class Kwf_Controller_Action_Cli_BuildController extends Kwf_Controller_Action_Cl
         Kwf_Util_Build::getInstance()->build($options);
         exit;
     }
+
+    public function showExtDepAction()
+    {
+        $d = Kwf_Assets_Package_Default::getDefaultProviderList()->findDependency('Frontend');
+        $this->_showExtDep($d, array());
+    }
+
+    private function _showExtDep($d, $stack)
+    {
+        $stack[] = $d;
+        if ($d instanceof Kwf_Assets_Dependency_File && $d->getType() == 'ext2' || $d->__toString() == 'KwfOnReady') {
+            $i = 0;
+            foreach ($stack as $s) {
+                $i++;
+                echo str_repeat(' ', $i*2).' '.$s."\n";
+            }
+            return;
+        }
+        foreach ($d->getDependencies(Kwf_Assets_Dependency_Abstract::DEPENDENCY_TYPE_ALL) as $i) {
+            if (!in_array($i, $stack, true)) {
+                $this->_showExtDep($i, $stack);
+            }
+        }
+    }
+
+    public function showFrontendAssetSizesAction()
+    {
+        $packages = array(
+            Kwf_Assets_Package_Default::getInstance('Frontend'),
+        );
+
+        foreach ($packages as $p) {
+            echo "\n".$p->getDependencyName()."\n";
+            foreach ($p->getDependency()->getRecursiveDependencies() as $i) {
+                $sizes[(string)$i] = strlen(gzencode($i->getContentsPacked('en'), 9, FORCE_GZIP));
+            }
+            arsort($sizes);
+            $sumSize = array_sum($sizes);
+            $topSizes = array_slice($sizes, 0, 10);
+            foreach ($topSizes as $name=>$size) {
+                echo "".str_pad(number_format(round(($size/$sumSize)*100, 1), 1).'%', 5).' '.str_pad(Kwf_View_Helper_FileSize::fileSize($size), 10)." $name\n";
+            }
+        }
+    }
 }
