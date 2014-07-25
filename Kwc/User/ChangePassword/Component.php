f0f8e16f51478459755281e246ce06a195fd3b5d
Michael Freudenthaler <mf@vivid-planet.com>
2014-07-25 08:15:16 +0200

    fix for change password component
    the wrong form was used and userProfile was not set

--- Kwc/User/ChangePassword/Component.php
+++ Kwc/User/ChangePassword/Component.php
@@ -4,7 +4,7 @@ class Kwc_User_ChangePassword_Component extends Kwc_Abstract_Composite_Component
     public static function getSettings()
     {
         $ret = parent::getSettings();
-        $ret['generators']['child']['component']['form'] = 'Kwc_User_Edit_Form_Component';
+        $ret['generators']['child']['component']['form'] = 'Kwc_User_ChangePassword_Form_Component';
         return $ret;
     }
 }
