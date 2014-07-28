b6e33e5f1ca359c3fc9a94225bf875da82501020
Benjamin Hohenwarter <bh@vivid-planet.com>
2014-07-28 10:31:32 +0200

    Add test for db-expr to get data from parent of parent

---
+++ tests/Kwf/Model/DbWithConnection/ParentParentExpr/ChildModel.php
@@ -0,0 +1,36 @@
+<?php
+class Kwf_Model_DbWithConnection_ParentParentExpr_ChildModel extends Kwf_Model_Db
+{
+    private $_tableName;
+    public function __construct($config = array())
+    {
+        $this->_tableName = uniqid('dbtest_child');
+        $config['table'] = $this->_tableName;
+
+        $this->_referenceMap = array(
+            'Parent' => 'parent_id->Kwf_Model_DbWithConnection_ParentParentExpr_MiddleModel'
+        );
+        $this->_exprs['parent_foo'] = new Kwf_Model_Select_Expr_Parent('Parent', 'parent_foo');
+
+        parent::__construct($config);
+    }
+
+    public function setUp()
+    {
+        Kwf_Registry::get('db')->query("CREATE TABLE IF NOT EXISTS {$this->_tableName} (
+            `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            `parent_id` INT UNSIGNED NOT NULL,
+            `zatoo` INT
+        ) ENGINE = INNODB");
+        Kwf_Registry::get('db')->query("TRUNCATE TABLE {$this->_tableName}");
+        Kwf_Registry::get('db')->query("INSERT INTO {$this->_tableName}
+                        (id, parent_id, zatoo) VALUES (1, 1, 5)");
+        Kwf_Registry::get('db')->query("INSERT INTO {$this->_tableName}
+                        (id, parent_id, zatoo) VALUES (3, 1, 4)");
+    }
+
+    public function dropTable()
+    {
+        Kwf_Registry::get('db')->query("DROP TABLE IF EXISTS {$this->_tableName}");
+    }
+}
