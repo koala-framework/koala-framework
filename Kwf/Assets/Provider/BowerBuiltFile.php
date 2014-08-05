2a1ea432466b380e67ce9048e5bea96442b2e0d4
Niko Sams <ns@vivid-planet.com>
2014-08-05 09:11:50 +0200

    support qunit lookup

--- Kwf/Assets/Provider/BowerBuiltFile.php
+++ Kwf/Assets/Provider/BowerBuiltFile.php
@@ -17,11 +17,19 @@ class Kwf_Assets_Provider_BowerBuiltFile extends Kwf_Assets_Provider_Abstract
                 'dist/'.$this->_path.'.js',
                 'src/'.$this->_path.'.js',
                 'js/'.$this->_path.'.js',
+                $this->_path.'/'.$this->_path.'.js',
             );
             foreach ($files as $f) {
                 if (substr($f, -6) == '.js.js') $f = substr($f, 0, -3);
                 if (file_exists($dir.'/'.$f)) {
-                    return new Kwf_Assets_Dependency_File_Js($this->_path.'/'.$f);
+                    if (file_exists($dir.'/'.substr($f, 0, -2).'css')) {
+                        return new Kwf_Assets_Dependency_Dependencies(array(
+                            new Kwf_Assets_Dependency_File_Js($this->_path.'/'.$f),
+                            new Kwf_Assets_Dependency_File_Css($this->_path.'/'.substr($f, 0, -2).'css'),
+                        ), $dependencyName);
+                    } else {
+                        return new Kwf_Assets_Dependency_File_Js($this->_path.'/'.$f);
+                    }
                 }
             }
             throw new Kwf_Exception("Can't find built dependency for $dependencyName in vendor/bower_components/$this->_path");
