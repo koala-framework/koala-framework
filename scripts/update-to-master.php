43021e8376b84ee43bb9e6e2a66c61e7e5f2a042
Niko Sams <ns@vivid-planet.com>
2014-08-06 10:24:45 +0200

    ignore .bowerrc

--- scripts/update-to-master.php
+++ scripts/update-to-master.php
@@ -82,6 +82,7 @@ deleteCacheFolder('cache/trl');
 $c = file(".gitignore", FILE_IGNORE_NEW_LINES);
 $c[] = '/build';
 $c[] = '/vendor';
+$c[] = '.bowerrc';
 $c = array_filter($c); //remove empty lines
 $c = array_unique($c);
 file_put_contents('.gitignore', implode("\n", $c)."\n");
