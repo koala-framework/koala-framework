e15da5afcb57c2f0d688c4ac7e74e04733da979c
Niko Sams <ns@vivid-planet.com>
2014-08-20 13:52:56 +0200

    improve detecting mimetype for http urls
    this now detects eg. http://fonts.googleapis.com/css?family=Raleway:200,700,600,400

--- Kwf/Assets/Dependency/HttpUrl.php
+++ Kwf/Assets/Dependency/HttpUrl.php
@@ -29,6 +29,12 @@ class Kwf_Assets_Dependency_HttpUrl extends Kwf_Assets_Dependency_Abstract
         } else if (substr($this->_url, -3) == '.js') {
             return 'text/javascript';
         } else {
+            $path = parse_url($this->_url, PHP_URL_PATH);
+            if (substr($path, -4) == '/css') {
+                return 'text/css';
+            } else if (substr($path, -3) == '/js') {
+                return 'text/javascript';
+            }
             throw new Kwf_Exception("Unknown file type");
         }
     }
