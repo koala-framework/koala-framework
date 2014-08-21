ef81150d19705ac8f908ee7c9be8b3da140ac22c
Alexander Kaufmann <ak@vivid-planet.com>
2014-08-21 10:34:26 +0200

    added possibilty to translate form dynamic subject names

--- Kwc/Form/Dynamic/Trl/Form.php
+++ Kwc/Form/Dynamic/Trl/Form.php
@@ -4,7 +4,45 @@ class Kwc_Form_Dynamic_Trl_Form extends Kwc_Abstract_Form
     protected function _initFields()
     {
         parent::_initFields();
-        $this->add(new Kwf_Form_Field_TextField('submit_caption', trlKwf('Submit Caption')))
+        $fs = new Kwf_Form_Container_FieldSet(trlKwf('Form Properties'));
+        $fs->add(new Kwf_Form_Field_TextField('subject', trlKwf('E-Mail Subject')))
             ->setWidth(400);
+        $fs->add(new Kwf_Form_Field_TextField('submit_caption', trlKwf('Submit Caption')))
+            ->setWidth(400);
+        $this->add($fs);
+
+        $hiddenField = new Kwf_Form_Field_Hidden('send_confirm_mail');
+        $hiddenField->setData(new Kwc_Form_Dynamic_Trl_Data());
+        $cards = new Kwf_Form_Container_Cards();
+        $cards->setCombobox($hiddenField);
+
+        $card = $cards->add(new Kwf_Form_Container_Card());
+        $card->setName('0');
+
+        $card = $cards->add(new Kwf_Form_Container_Card());
+        $card->setName('1');
+
+        $fs = $card->add(new Kwf_Form_Container_FieldSet(trlKwf('Send copy to User')));
+        $fs->add(new Kwf_Form_Field_TextField('confirm_subject', trlKwf('Subject')))
+            ->setWidth(300)
+            ->setAllowBlank(false);
+        $this->add($cards);
+    }
+}
+
+class Kwc_Form_Dynamic_Trl_Data extends Kwf_Data_Abstract
+{
+    public function load($row)
+    {
+        $c = Kwf_Component_Data_Root::getInstance()->getComponentByDbId($row->component_id, array('ignoreVisible'=>true));
+        if ($c) {
+            return $c->chained->getComponent()->getRow()->send_confirm_mail;
+        } else {
+            return '0';
+        }
+    }
+
+    public function save(Kwf_Model_Row_Interface $row, $data)
+    {
     }
 }
