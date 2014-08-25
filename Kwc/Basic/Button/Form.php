b0b00a76f40d34ce7978c0715e8211d4d16c2ee7
Niko Sams <ns@vivid-planet.com>
2014-08-25 11:04:56 +0200

    move style setting to more useable position
    before it was after link settings which was confusing

--- Kwc/Basic/Button/Form.php
+++ Kwc/Basic/Button/Form.php
@@ -6,7 +6,7 @@ class Kwc_Basic_Button_Form extends Kwc_Basic_Link_Form
         parent::_initFields(); 
         $styles = Kwc_Abstract::getSetting($this->getClass(), 'styles');
         if (count($styles) > 1) {
-            $this->add(new Kwf_Form_Field_Select('style', trlKwfStatic('Style')))
+            $this->insertAfter('text', new Kwf_Form_Field_Select('style', trlKwfStatic('Style')))
                 ->setValues($styles)
                 ->setWidth(300);
         }
