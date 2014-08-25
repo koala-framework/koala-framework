6112bdfb76cfbd5fb7dab5a6dcb8db4995432746
Niko Sams <ns@vivid-planet.com>
2014-08-25 09:10:33 +0200

    remove ComponentsDefer dependency, is now part of Components

--- Kwf/Assets/Provider/NoComponents.php
+++ Kwf/Assets/Provider/NoComponents.php
@@ -3,7 +3,7 @@ class Kwf_Assets_Provider_NoComponents extends Kwf_Assets_Provider_Abstract
 {
     public function getDependency($dependencyName)
     {
-        if ($dependencyName == 'Components' || $dependencyName == 'ComponentsDefer' || $dependencyName == 'ComponentsAdmin') {
+        if ($dependencyName == 'Components' || $dependencyName == 'ComponentsAdmin') {
             return new Kwf_Assets_Dependency_Dependencies(array(), $dependencyName);
         }
         return null;
