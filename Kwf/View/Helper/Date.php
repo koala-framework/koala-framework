ab10dce03a6ee28340f1f21862e35de44e97f2c1
Niko Sams <ns@vivid-planet.com>
2014-09-04 14:27:38 +0200

    Date helper: fix getting language

--- Kwf/View/Helper/Date.php
+++ Kwf/View/Helper/Date.php
@@ -10,16 +10,14 @@ class Kwf_View_Helper_Date
     public function date($date, $format = null)
     {
         $language = null;
-        if (!$format) {
-            if (isset($this->_view) && $this->_view->data && $this->_view->data instanceof Kwf_Component_Data) {
-                $format = $this->_view->data->trlKwf('Y-m-d');
-                $language = $this->_view->data->getLanguage();
-            } else if (isset($this->_view) && $this->_view->item && $this->_view->item instanceof Kwf_Component_Data) {
-                $format = $this->_view->item->trlKwf('Y-m-d');
-                $language = $this->_view->item->getLanguage();
-            } else {
-                $format = trlKwf('Y-m-d');
-            }
+        if (isset($this->_view) && $this->_view->data && $this->_view->data instanceof Kwf_Component_Data) {
+            if (!$format) $format = $this->_view->data->trlKwf('Y-m-d');
+            $language = $this->_view->data->getLanguage();
+        } else if (isset($this->_view) && $this->_view->item && $this->_view->item instanceof Kwf_Component_Data) {
+            if (!$format) $format = $this->_view->item->trlKwf('Y-m-d');
+            $language = $this->_view->item->getLanguage();
+        } else {
+            if (!$format) $format = trlKwf('Y-m-d');
         }
 
         if (!$date || substr($date, 0, 10) == '0000-00-00') return '';
