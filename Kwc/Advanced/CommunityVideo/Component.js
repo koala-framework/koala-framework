b45a0554c080e10af181d0da239934bbb999be9a
Daniel Bacher <db@vivid-planet.com>
2014-09-09 13:44:07 +0200

    Rebuild Community Video Component to support mobile devices and full width
    - Add settings for aspect ratio / custom width
    - Supports Vimeo and YouTube
    - Trl Support

---
+++ Kwc/Advanced/CommunityVideo/Component.js
@@ -0,0 +1,31 @@
+Kwf.onJElementReady('.kwcAdvancedCommunityVideo', function(el, config) {
+    el.data('config', config);
+}, {priority: -1});
+
+Kwf.onJElementWidthChange('.kwcAdvancedCommunityVideo', function(el) {
+
+    var config = el.data('config');
+    var iframe = el.find('iframe');
+
+    if(config.fullWidth) {
+        var size = {};
+        switch(config.ratio) {
+            case "16x9":
+                size = {
+                    width: $(el).width(),
+                    height: ($(el).width() / 16) * 9
+                };
+                break;
+            case "4x3":
+                size = {
+                    width: $(el).width(),
+                    height: ($(el).width() / 4) * 3
+                };
+                break;
+            default:
+                return false;
+        }
+        iframe.width(size.width).height(size.height);
+    }
+
+}, {defer: true});
