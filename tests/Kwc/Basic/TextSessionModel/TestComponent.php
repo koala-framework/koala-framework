4ee5a937e0772b647e7c0f9bfaa55ad17021a726
Niko Sams <ns@vivid-planet.com>
2014-09-12 08:34:43 +0200

    fix test

--- tests/Kwc/Basic/TextSessionModel/TestComponent.php
+++ tests/Kwc/Basic/TextSessionModel/TestComponent.php
@@ -5,6 +5,7 @@ class Kwc_Basic_TextSessionModel_TestComponent extends Kwc_Basic_Text_Component
     {
         $ret = parent::getSettings();
         $ret['ownModel'] = 'Kwc_Basic_TextSessionModel_TestModel';
+        $ret['childModel'] = 'Kwc_Basic_TextSessionModel_TestChildComponentsModel';
         $ret['stylesModel'] = 'Kwc_Basic_TextSessionModel_TestStylesModel';
         $ret['generators']['child']['component']['link'] = 'Kwc_Basic_TextSessionModel_Link_Component';
         $ret['generators']['child']['component']['image'] = false;
