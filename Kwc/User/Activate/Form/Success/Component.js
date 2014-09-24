1e1d4cee4e244823ff463ce84dd92733413c246c
Daniel Bacher <db@vivid-planet.com>
2014-09-24 11:10:28 +0200

    Replace Ext code with jQuery

--- Kwc/User/Activate/Form/Success/Component.js
+++ Kwc/User/Activate/Form/Success/Component.js
@@ -1,5 +1,3 @@
-Kwf.onContentReady(function() {
-    Ext2.query('.kwcUserFormSuccess').each(function(el) {
-        window.setTimeout("window.location.href = '/'", 3000);
-    }, this);
+Kwf.onJElementReady('.kwcUserFormSuccess', function(el) {
+    window.setTimeout("window.location.href = '/'", 3000);
 });
