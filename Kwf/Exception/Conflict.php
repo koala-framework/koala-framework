3993e19ea5436834aad8a099c11854fcc820eaf6
Michael Freudenthaler <mf@vivid-planet.com>
2014-10-14 08:11:59 +0200

    added new exception conflict
    error code: 409

---
+++ Kwf/Exception/Conflict.php
@@ -0,0 +1,34 @@
+<?php
+class Kwf_Exception_Conflict extends Kwf_Exception_Abstract
+{
+    public function __construct($message = "Conflict")
+    {
+        parent::__construct($message);
+    }
+
+    public function getHeader()
+    {
+        return 'HTTP/1.1 409 Conflict';
+    }
+
+    public function getTemplate()
+    {
+        return 'Error409';
+    }
+
+    public function log()
+    {
+        if (Kwf_Exception::isDebug()) {
+            return false;
+        }
+
+        $body = '';
+        $body .= $this->_format('REQUEST_URI', isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '(none)');
+        $body .= $this->_format('HTTP_REFERER', isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '(none)');
+        $body .= $this->_format('Time', date('H:i:s'));
+        $body .= $this->_format('_GET', print_r($_GET, true));
+        $body .= $this->_format('_POST', print_r($_POST, true));
+
+        Kwf_Exception_Logger_Abstract::getInstance()->log($this, 'conflict', $body);
+    }
+}
