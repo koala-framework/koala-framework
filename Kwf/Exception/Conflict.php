2f41b9d5dd30e548b16d02c07de876f4a4a09a2d
Michael Freudenthaler <mf@vivid-planet.com>
2014-10-14 10:34:27 +0200

    fixed conflict exception

--- Kwf/Exception/Conflict.php
+++ Kwf/Exception/Conflict.php
@@ -1,5 +1,5 @@
 <?php
-class Kwf_Exception_Conflict extends Kwf_Exception_Abstract
+class Kwf_Exception_Conflict extends Kwf_Exception
 {
     public function __construct($message = "Conflict")
     {
@@ -9,26 +9,5 @@ class Kwf_Exception_Conflict extends Kwf_Exception_Abstract
     public function getHeader()
     {
         return 'HTTP/1.1 409 Conflict';
-    }
-
-    public function getTemplate()
-    {
-        return 'Error409';
-    }
-
-    public function log()
-    {
-        if (Kwf_Exception::isDebug()) {
-            return false;
-        }
-
-        $body = '';
-        $body .= $this->_format('REQUEST_URI', isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '(none)');
-        $body .= $this->_format('HTTP_REFERER', isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '(none)');
-        $body .= $this->_format('Time', date('H:i:s'));
-        $body .= $this->_format('_GET', print_r($_GET, true));
-        $body .= $this->_format('_POST', print_r($_POST, true));
-
-        Kwf_Exception_Logger_Abstract::getInstance()->log($this, 'conflict', $body);
     }
 }
