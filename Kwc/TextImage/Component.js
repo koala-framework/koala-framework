3e47f57b15327242af06d7b7eef4493aebb73108
Daniel Bacher <db@vivid-planet.com>
2014-10-20 15:59:52 +0200

    Change selector for textImage to set/remove class largeImage and largeText

--- Kwc/TextImage/Component.js
+++ Kwc/TextImage/Component.js
@@ -2,7 +2,7 @@ Kwf.Utils.ResponsiveEl('.kwcTextImage', [420]);
 
 //remove largeText class if >55% of element is covered by image
 Kwf.onElementWidthChange('.kwcTextImage', function textImage(el) {
-    var img = el.child('div.image img');
+    var img = el.child('div.image .kwcAbstractImage .container');
     if (img) {
         if (img.getWidth() < (el.getWidth() * 0.55)) {
             el.removeClass('largeImage');
