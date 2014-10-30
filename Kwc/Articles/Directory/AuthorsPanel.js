bd5089a3a0fedceb495b68e500f866b2cc0d0a13
Alexander Kaufmann <ak@vivid-planet.com>
2014-10-30 15:33:44 +0100

    overview of written articles for every author
    also show count of all written by author and possibility to export articles list as csv/xls

---
+++ Kwc/Articles/Directory/AuthorsPanel.js
@@ -0,0 +1,34 @@
+Ext.namespace('Kwc.Articles.Directory');
+Kwc.Articles.Directory.AuthorsPanel = Ext.extend(Kwf.Binding.ProxyPanel, {
+    initComponent: function() {
+        this.articlesGrid = new Kwf.Auto.GridPanel({
+            controllerUrl: this.articlesControllerUrl,
+            region: 'east',
+            width: 500,
+            split: true
+        }, this);
+
+        this.proxyItem = new Kwf.Auto.GridPanel({
+            controllerUrl: this.controllerUrl,
+            region: 'center',
+            bindings: [{
+                item: this.articlesGrid,
+                queryParam: 'author_id'
+            }]
+        }, this);
+
+        this.layout = 'border';
+        this.items = [this.proxyItem, this.articlesGrid];
+
+        Kwc.Articles.Directory.AuthorsPanel.superclass.initComponent.call(this);
+    },
+    applyBaseParams: function(baseParams) {
+        this.articlesGrid.applyBaseParams(baseParams);
+        Kwc.Articles.Directory.AuthorsPanel.superclass.applyBaseParams.apply(this, arguments);
+    },
+    setBaseParams : function(baseParams) {
+        this.articlesGrid.setBaseParams(baseParams);
+        Kwc.Articles.Directory.AuthorsPanel.superclass.setBaseParams.apply(this, arguments);
+    }
+}, this);
+Ext.reg('kwc.articles.directory.authorsPanel', Kwc.Articles.Directory.AuthorsPanel);
