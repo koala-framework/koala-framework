f4741bd2bfd93a1a44a15a7efd38f8be7c37bcc3
Niko Sams <ns@vivid-planet.com>
2014-11-03 13:15:45 +0100

    add _x_org_koala-framework_last extension to pre-build source map
    required to fix assets package build

--- Kwf/Assets/Dependency/File/JsPreBuilt.php
+++ Kwf/Assets/Dependency/File/JsPreBuilt.php
@@ -29,6 +29,14 @@ class Kwf_Assets_Dependency_File_JsPreBuilt extends Kwf_Assets_Dependency_File
         $paths = self::_getAllPaths();
         $pathType = $this->getType();
         $f = $paths[$pathType].substr($this->_sourceMapFile, strpos($this->_sourceMapFile, '/'));
-        return file_get_contents($f);
+        $mapContents = file_get_contents($f);
+        $map = new Kwf_Assets_Util_SourceMap($mapContents, $this->getContentsPacked($language));
+
+        $cacheFile = sys_get_temp_dir().'/kwf-uglifyjs/'.$this->getFileNameWithType().'.map.'.md5($mapContents);
+        if (!file_exists($cacheFile)) {
+            if (!is_dir(dirname($cacheFile))) mkdir(dirname($cacheFile), 0777, true);
+            file_put_contents($cacheFile, $map->getMapContents());
+        }
+        return file_get_contents($cacheFile);
     }
 }
