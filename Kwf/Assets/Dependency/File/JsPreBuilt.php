3d812fede69fa6eae3e9ddff69481e794e24ba40
Niko Sams <ns@vivid-planet.com>
2014-11-03 12:15:37 +0100

    use built (minimied) file if shipped with map by the bower package
    this bings down jQuery size from 40kB to 32kB (gzipped)
    
    currently jQuery is the only package shipping a map

---
+++ Kwf/Assets/Dependency/File/JsPreBuilt.php
@@ -0,0 +1,35 @@
+<?php
+class Kwf_Assets_Dependency_File_JsPreBuilt extends Kwf_Assets_Dependency_File
+{
+    protected $_builtFile;
+    protected $_sourceMapFile;
+
+    public function __construct($fileName, $builtFile, $sourceMapFile)
+    {
+        parent::__construct($fileName);
+        $this->_builtFile = $builtFile;
+        $this->_sourceMapFile = $sourceMapFile;
+    }
+
+    public function getMimeType()
+    {
+        return 'text/javascript';
+    }
+
+    public function getContentsPacked($language)
+    {
+        $paths = self::_getAllPaths();
+        $pathType = $this->getType();
+        $f = $paths[$pathType].substr($this->_builtFile, strpos($this->_builtFile, '/'));
+        p($f);
+        return file_get_contents($f);
+    }
+
+    public function getContentsPackedSourceMap($language)
+    {
+        $paths = self::_getAllPaths();
+        $pathType = $this->getType();
+        $f = $paths[$pathType].substr($this->_sourceMapFile, strpos($this->_sourceMapFile, '/'));
+        return file_get_contents($f);
+    }
+}
