2445762c3b5e10d2101fc8fa1551047c0a0d8c56
Alexander Kaufmann <ak@vivid-planet.com>
2014-11-12 09:25:31 +0100

    make redirect url after activation dynamic

--- Kwc/User/Activate/Form/Success/Component.php
+++ Kwc/User/Activate/Form/Success/Component.php
@@ -7,4 +7,15 @@ class Kwc_User_Activate_Form_Success_Component extends Kwc_Form_Success_Componen
         $ret['assets']['dep'][] = 'KwfOnReadyJQuery';
         return $ret;
     }
+
+    public function getTemplateVars(Kwf_Component_Renderer_Abstract $renderer = null)
+    {
+        $ret = parent::getTemplateVars($renderer);
+        $ret['config'] = array(
+            'redirectUrl' => '/'
+        );
+        return $ret;
+    }
+
+
 }
