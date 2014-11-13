cf2cb34e0a9665701b6c8d3afb69f699c3b5aae1
Benjamin Hohenwarter <bh@vivid-planet.com>
2014-11-13 12:02:12 +0100

    OptBox: Fire cookieOptChanged and listen to optChanged in OptComponent
    This way optBox changes also change checkbox of OptComponent and change
    of OptComponent checkbox hides OptBox.

--- Kwc/Statistics/Opt/Component.js
+++ Kwc/Statistics/Opt/Component.js
@@ -1,6 +1,18 @@
 Kwf.onElementReady('.kwcStatisticsOpt .webForm', function(el, config) {
     el.kwcForm.findField('form_opt').el.on('change', function() {
         this.kwcForm.submit();
+        if (el.kwcForm.findField('form_opt').getValue()) {
+            Kwf.fireComponentEvent('cookieOptChanged', 'in');
+        } else {
+            Kwf.fireComponentEvent('cookieOptChanged', 'out');
+        }
     }, el);
     el.child('.submit').hide();
+    Kwf.onComponentEvent('cookieOptChanged', function(value) {
+        if (value == 'in') {
+            el.kwcForm.findField('form_opt').setValue(true);
+        } else if (value == 'out') {
+            el.kwcForm.findField('form_opt').setValue(false);
+        }
+    });
 }, {defer: true});
