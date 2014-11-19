4b6656314ff0931b3563681efab9e548e5badaaf
Niko Sams <ns@vivid-planet.com>
2014-11-19 17:41:00 +0100

    try to fix travis, maybe that helps

--- tests/Kwf/EyeCandy/SwitchDisplay/Custom/Test.php
+++ tests/Kwf/EyeCandy/SwitchDisplay/Custom/Test.php
@@ -11,7 +11,7 @@ class Kwf_EyeCandy_SwitchDisplay_Custom_Test extends PHPUnit_Framework_TestCase
             }
         }
 
-        $cmd = "phantomjs ../vendor/bower_components/qunit-phantomjs-runner/runner.js ";
+        $cmd = "phantomjs --web-security=false ../vendor/bower_components/qunit-phantomjs-runner/runner.js ";
         $cmd .= "http://".Kwf_Config::getValue('server.domain').Kwf_Setup::getBaseUrl()."/kwf/test/kwf_eye-candy_switch-display_custom_test 20";
         $cmd .= " 2>&1";
         $out = array();
