033eafa986644af3924ee475511040c284e47140
Benjamin Hohenwarter <bh@vivid-planet.com>
2014-11-25 14:30:47 +0100

    Don't validate sessionToken for searchResultController
    Else this results in "missing sessiontoken param" when user is logged in.

---
+++ Kwc/FulltextSearch/Search/ViewAjax/ViewController.php
@@ -0,0 +1,7 @@
+<?php
+class Kwc_FulltextSearch_Search_ViewAjax_ViewController extends Kwc_Directories_List_ViewAjax_ViewController
+{
+    protected function _validateSessionToken()
+    {
+    }
+}
