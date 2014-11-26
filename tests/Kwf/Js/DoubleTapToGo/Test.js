de324dcd5761c91cb2b0f6de4d7db885e7b5882f
Niko Sams <ns@vivid-planet.com>
2014-11-26 13:06:10 +0100

    fix phantomjs test

--- tests/Kwf/Js/DoubleTapToGo/Test.js
+++ tests/Kwf/Js/DoubleTapToGo/Test.js
@@ -121,6 +121,28 @@ var templates = {
         '</div>'
 };
 
+//from http://stackoverflow.com/a/17789929/781662
+if (window._phantom) {
+  // Patch since PhantomJS does not implement click() on HTMLElement. In some
+  // cases we need to execute the native click on an element. However, jQuery's
+  // $.fn.click() does not dispatch to the native function on <a> elements, so we
+  // can't use it in our implementations: $el[0].click() to correctly dispatch.
+  if (!HTMLElement.prototype.click) {
+    HTMLElement.prototype.click = function() {
+      var ev = document.createEvent('MouseEvent');
+      ev.initMouseEvent(
+          'click',
+          /*bubble*/true, /*cancelable*/true,
+          window, null,
+          0, 0, 0, 0, /*coordinates*/
+          false, false, false, false, /*modifier keys*/
+          0/*button=left*/, null
+      );
+      this.dispatchEvent(ev);
+    };
+  }
+}
+
 QUnit.asyncTest("Simulate touch-device and click-trough Levels : 2", function( assert ) {
     expect( 5 );
 
