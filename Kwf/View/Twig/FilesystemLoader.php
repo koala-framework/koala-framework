707254f1630cdad9b9e11d4033e9e78504ccd462
Alexander Kaufmann <ak@vivid-planet.com>
2014-11-26 13:53:09 +0100

    abstracted Kwf/Component/Renderer/Twig/Environment.php to use it everywhere
    for example creating pdfs with dompdf and twig templates

--- Kwf/View/Twig/FilesystemLoader.php
+++ Kwf/View/Twig/FilesystemLoader.php
@@ -1,5 +1,5 @@
 <?php
-class Kwf_Component_Renderer_Twig_FilesystemLoader extends Twig_Loader_Filesystem
+class Kwf_View_Twig_FilesystemLoader extends Twig_Loader_Filesystem
 {
     //don't validate, we can trust our file names
     protected function validateName($name)
