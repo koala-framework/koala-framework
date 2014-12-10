b5b4ef260d0b97afd732d1c9b968611ecc3c7ebd
Alexander Kaufmann <ak@vivid-planet.com>
2014-12-10 14:02:31 +0100

    added test for Kwf_Model_Union, when orderField is Expr -> error

---
+++ tests/Kwf/Model/Union/DbWithExpr/Test.php
@@ -0,0 +1,33 @@
+<?php
+class Kwf_Model_Union_DbWithExpr_Test extends Kwf_Model_Union_Abstract_Test
+{
+    public function setUp()
+    {
+        parent::setUp();
+        Kwf_Model_Abstract::getInstance('Kwf_Model_Union_DbWithExpr_Model1')->setUp();
+        Kwf_Model_Abstract::getInstance('Kwf_Model_Union_DbWithExpr_Model2')->setUp();
+        $this->_m = Kwf_Model_Abstract::getInstance('Kwf_Model_Union_DbWithExpr_TestModel');
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        Kwf_Model_Abstract::getInstance('Kwf_Model_Union_DbWithExpr_Model1')->dropTable();
+        Kwf_Model_Abstract::getInstance('Kwf_Model_Union_DbWithExpr_Model2')->dropTable();
+    }
+
+    public function testGetRowsWithExprOrder()
+    {
+        $s = new Kwf_Model_Select();
+        $s->order('baz');
+        $rows = $this->_m->getRows($s);
+        $this->assertEquals(6, count($rows));
+        $this->assertEquals('2', $rows[0]->baz);
+        $this->assertEquals('cc', $rows[1]->baz);
+        $this->assertEquals('cc3', $rows[2]->baz);
+        $this->assertEquals('foobar', $rows[3]->baz);
+        $this->assertEquals('foobar', $rows[4]->baz);
+        $this->assertEquals('foobar', $rows[5]->baz);
+    }
+}
+
---
+++ tests/Kwf/Model/Union/DbWithExpr/TestModel.php
@@ -0,0 +1,9 @@
+<?php
+class Kwf_Model_Union_DbWithExpr_TestModel extends Kwf_Model_Union_Db_TestModel
+{
+    protected $_models = array(
+        '1m' => 'Kwf_Model_Union_DbWithExpr_Model1',
+        '2m' => 'Kwf_Model_Union_DbWithExpr_Model2',
+    );
+}
+
