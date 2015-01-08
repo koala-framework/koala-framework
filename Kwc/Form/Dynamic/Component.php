e6c66e95ecb7c9e0ddadf5cf2d8064218fbbeef7
Niko Sams <ns@vivid-planet.com>
2015-01-08 10:24:40 +0100

    Don't check for spam if form is behind a login
    This is not needed and could lead to false positives

--- Kwc/Form/Dynamic/Component.php
+++ Kwc/Form/Dynamic/Component.php
@@ -16,6 +16,19 @@ class Kwc_Form_Dynamic_Component extends Kwc_Abstract_Composite_Component
 
     public function getMailSettings()
     {
+        $spamCheck = true;
+        $c = $this->getData();
+        while ($c) {
+            foreach (Kwc_Abstract::getSetting($c->componentClass, 'plugins') as $plugin) {
+                if (is_instance_of($plugin, 'Kwf_Component_Plugin_Interface_Login')) {
+                    $spamCheck = false;
+                    break;
+                }
+            }
+            if ($c->isPage) break;
+            $c = $c->parent;
+        }
+
         $row = $this->getRow();
         return array(
             'recipient' => $row->recipient,
@@ -25,6 +38,8 @@ class Kwc_Form_Dynamic_Component extends Kwc_Abstract_Composite_Component
             'send_confirm_mail' => $row->send_confirm_mail,
             'confirm_field_component_id' => $row->confirm_field_component_id,
             'confirm_subject' => $row->confirm_subject,
+
+            'check_spam' => $spamCheck
         );
     }
 }
