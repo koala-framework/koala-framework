274476d043084c4bad2f1cae60ba52f542fbf692
Niko Sams <ns@vivid-planet.com>
2015-01-09 11:25:53 +0100

    Image Component: move fields into SEO fieldset
    improves usability by hiding rarely used fields

--- Kwc/Abstract/Image/Form.php
+++ Kwc/Abstract/Image/Form.php
@@ -8,13 +8,19 @@ class Kwc_Abstract_Image_Form extends Kwc_Abstract_Composite_Form
 
         $this->add($this->_createImageUploadField(Kwc_Abstract::getSetting($this->getClass(), 'imageLabel')));
 
+        if (Kwc_Abstract::getSetting($this->getClass(), 'editFilename') || Kwc_Abstract::getSetting($this->getClass(), 'altText')) {
+            $fs = $this->add(new Kwf_Form_Container_FieldSet('SEO'));
+            $fs->setCollapsible(true);
+            $fs->setCollapsed(true);
+        }
         if (Kwc_Abstract::getSetting($this->getClass(), 'editFilename')) {
-            $this->add(new Kwf_Form_Field_TextField('filename', trlKwf('Filename')))
+            $fs->add(new Kwf_Form_Field_TextField('filename', trlKwf('Filename')))
                 ->setAutoFillWithFilename('filename') //to find it by MultiFileUpload and in JavaScript
-                ->setVtype('alphanum');
+                ->setVtype('alphanum')
+                ->setWidth(300);
         }
         if (Kwc_Abstract::getSetting($this->getClass(), 'altText')) {
-            $this->add(new Kwf_Form_Field_TextField('alt_text', trlKwf('Alt Text')))
+            $fs->add(new Kwf_Form_Field_TextField('alt_text', trlKwf('Alt Text')))
                 ->setHelpText(trlKwf('Optional: Describe this image for visually handicapped people and search engines.'))
                 ->setWidth(300);
         }
--- Kwc/Abstract/Image/ImageFile.js
+++ Kwc/Abstract/Image/ImageFile.js
@@ -6,18 +6,16 @@ Kwc.Abstract.Image.ImageFile = Ext2.extend(Kwf.Form.File, {
         Kwc.Basic.DownloadTag.Panel.superclass.initComponent.call(this);
         this.on('uploaded', function(field, value) {
             if (value) {
-                this.ownerCt.find('autoFillWithFilename', 'filename').forEach(function (f) {
-                    var v = value.uploaded_filename || value.filename;
-                    v = v.toLowerCase().replace(/ä/g, 'ae').replace(/ö/g, 'oe')
-                        .replace(/ü/g, 'ue').replace(/ß/g, 'ss')
-                        .replace(/[^a-z0-9]/g, '_').replace(/__+/g, '_');
-                    f.setValue(v);
-                }, this);
-                this.ownerCt.find('autoFillWithFilename', 'filenameWithExt').forEach(function (f) {
-                    if (f.getValue() == '') {
-                        f.setValue(value.filename+'.'+value.extension);
-                    }
-                }, this);
+                var fs = this.ownerCt.ownerCt.items.find(function(i){return i.xtype=='fieldset'});
+                if (fs) {
+                    fs.find('autoFillWithFilename', 'filename').forEach(function (f) {
+                        var v = value.uploaded_filename || value.filename;
+                        v = v.toLowerCase().replace(/ä/g, 'ae').replace(/ö/g, 'oe')
+                            .replace(/ü/g, 'ue').replace(/ß/g, 'ss')
+                            .replace(/[^a-z0-9]/g, '_').replace(/__+/g, '_');
+                        f.setValue(v);
+                    }, this);
+                }
             }
         }, this);
     },
