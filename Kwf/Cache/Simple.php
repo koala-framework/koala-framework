1bc218c91af5b6603de44f4282263f7d0153b247
Niko Sams <ns@vivid-planet.com>
2015-01-12 14:41:24 +0100

    Fix non-ascii cacheIds for Simple Cache
    fixes problem when caching non-ascii trl values (eg. cyrillic)

--- Kwf/Cache/Simple.php
+++ Kwf/Cache/Simple.php
@@ -95,8 +95,16 @@ class Kwf_Cache_Simple
 
     private static function _processId($cacheId)
     {
-        $cacheId = str_replace('-', '__', $cacheId);
-        $cacheId = preg_replace('#[^a-zA-Z0-9_]#', '_', $cacheId);
+        if (function_exists('quoted_printable_encode')) {
+            //php 5.3+
+            $cacheId = quoted_printable_encode($cacheId);
+        } else if (function_exists('imap_8bit')) {
+            //imap module
+            $cacheId = imap_8bit($cacheId);
+        } else {
+            //fallback
+            $cacheId = base64_encode($cacheId);
+        }
         return $cacheId;
     }
 
@@ -131,7 +139,16 @@ class Kwf_Cache_Simple
     //for 'file' backend
     public static function _getFileNameForCacheId($cacheId)
     {
-        $cacheId = preg_replace('#[^a-zA-Z0-9_-]#', '_', $cacheId);
+        if (function_exists('quoted_printable_encode')) {
+            //php 5.3+
+            $cacheId = quoted_printable_encode($cacheId);
+        } else if (function_exists('imap_8bit')) {
+            //imap module
+            $cacheId = imap_8bit($cacheId);
+        } else {
+            //fallback
+            $cacheId = base64_encode($cacheId);
+        }
         if (strlen($cacheId) > 50) {
             $cacheId = substr($cacheId, 0, 50).md5($cacheId);
         }
