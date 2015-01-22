592ad203a8af33833e9574350cf6c8b3c998b339
Alexander Kaufmann <ak@vivid-planet.com>
2015-01-22 09:44:57 +0100

    use onJElementShow for redirect after login

--- Kwc/User/Login/Form/Success/Component.js
+++ Kwc/User/Login/Form/Success/Component.js
@@ -1,4 +1,4 @@
-Kwf.onJElementReady('.kwcUserLoginFormSuccess', function(el) {
+Kwf.onJElementShow('.kwcUserLoginFormSuccess', function(el) {
     var url = el.find('input.redirectTo').val();
     location.href = url;
-}, {checkVisibility: true});
+});
