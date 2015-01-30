addefddd27d06a7dc3dc1c3bb701dee249cd5b30
Niko Sams <ns@vivid-planet.com>
2015-01-30 09:41:54 +0100

    Complete Auth Redirect interface
    - add implementation for usage in frontend login
    - add proxy/union redirects
    - add more required interface methods

--- Kwf/User/Auth/Interface/Redirect.php
+++ Kwf/User/Auth/Interface/Redirect.php
@@ -1,6 +1,8 @@
 <?php
 interface Kwf_User_Auth_Interface_Redirect
 {
+    public function getLoginRedirectLabel();
     public function getLoginRedirectUrl($redirectBackUrl);
-    public function loginByRedirectBackParams($params);
+    public function getUserToLoginByParams(array $params);
+    public function createSampleLoginLinks($absoluteUrl);
 }
