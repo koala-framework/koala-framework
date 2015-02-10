koala-framework/koala-framework

commit 93d9e47357c18b3328ba764cecabb27d324fb433

Use REQUEST_URI instead of REDIRECT_URL
For some reason REDIRECT_URL is /index.html when it should be / (on Ubuntu 14.04)

According to http://stackoverflow.com/a/6483938/781662 should be used

Kwf/Setup.php
@@ -163,7 +163,7 @@ class Kwf_Setup
         if (isset($requestPath)) return $requestPath;
         switch (php_sapi_name()) {
             case 'apache2handler':
-                $requestPath = $_SERVER['REDIRECT_URL'];
+                $requestPath = $_SERVER['REQUEST_URI'];
                 break;
             case 'cli':
                 $requestPath = false;

commit cf2c26beb31e2b7452012c037bca1ed2d66282a9

Fix REQUEST_URI usage, it also contains query parameters
Simply cut them off

Fixes bug introduced with 93d9e47357c18b3328ba764cecabb27d324fb433

Kwf/Setup.php
@@ -164,6 +164,7 @@ class Kwf_Setup
         switch (php_sapi_name()) {
             case 'apache2handler':
                 $requestPath = $_SERVER['REQUEST_URI'];
+                $requestPath = strtok($requestPath, '?');
                 break;
             case 'cli':
                 $requestPath = false;