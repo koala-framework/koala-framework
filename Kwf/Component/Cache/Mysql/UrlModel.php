b82dfc35a1a9d5f69dba1a37730ed2fb9e19b1bb
Niko Sams <ns@vivid-planet.com>
2015-02-25 17:16:55 +0100

    Improve performance for component cache models
    - don't use additional proxy model (less overhead, for tests use FnF direclty)
    - disable modelobserver (not needed at all here)

--- Kwf/Component/Cache/Mysql/UrlModel.php
+++ Kwf/Component/Cache/Mysql/UrlModel.php
@@ -1,5 +1,10 @@
 <?php
-class Kwf_Component_Cache_Mysql_UrlModel extends Kwf_Model_Db_Proxy
+class Kwf_Component_Cache_Mysql_UrlModel extends Kwf_Model_Db
 {
     protected $_table = 'cache_component_url';
+
+    protected function _updateModelObserver($options, array $ids = null)
+    {
+    }
+
 }
