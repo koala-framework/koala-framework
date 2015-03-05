2ac6daa865ac81e79ca40d432b22402a70db6dc2
Niko Sams <ns@vivid-planet.com>
2015-03-05 08:40:38 +0100

    Fix home link when home is in other category

--- Kwc/Box/HomeLink/Component.php
+++ Kwc/Box/HomeLink/Component.php
@@ -12,7 +12,7 @@ class Kwc_Box_HomeLink_Component extends Kwc_Abstract_Composite_Component
     public function getTemplateVars()
     {
         $ret = parent::getTemplateVars();
-        $ret['home'] = $this->getData()->parent->getChildPage(array('home' => true), array());
+        $ret['home'] = $this->getData()->getSubroot()->getChildPage(array('home' => true), array());
         return $ret;
     }
 }
