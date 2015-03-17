bc0a785f4e860b0fb3f7a8495022cfac284509e3
Niko Sams <ns@vivid-planet.com>
2015-03-17 14:02:17 +0100

    Fix Guestbook Trl admin

--- Kwc/Posts/Directory/Trl/Component.php
+++ Kwc/Posts/Directory/Trl/Component.php
@@ -1,6 +1,14 @@
 <?php
 class Kwc_Posts_Directory_Trl_Component extends Kwc_Directories_Item_Directory_Trl_Component
 {
+
+    public static function getSettings($masterComponentClass)
+    {
+        $ret = parent::getSettings($masterComponentClass);
+        $ret['extConfig'] = 'Kwf_Component_Abstract_ExtConfig_None';
+        return $ret;
+    }
+
     public function getTemplateVars()
     {
         $ret = parent::getTemplateVars();
