ac599e24017062488fe6cd76ea1e77a0a258dce3
Mario Kemetinger <mk@vivid-planet.com>
2015-03-19 10:52:51 +0100

    Columns expanded with option 5col layout

--- Kwc/Columns/Component.php
+++ Kwc/Columns/Component.php
@@ -69,6 +69,11 @@ class Kwc_Columns_Component extends Kwc_Abstract_List_Component
                 'columns' => 4,
                 'colSpans' => array(1,1,1,1),
                 'name' => "4 $columnsTrl (25% - 25% - 25% - 25%)"
+            ),
+            '5col-20_20_20_20_20' => array(
+                'columns' => 5,
+                'colSpans' => array(1,1,1,1,1),
+                'name' => "5 $columnsTrl (20% - 20% - 20% - 20%)"
             )
         );
         return $ret;
