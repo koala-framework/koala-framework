7345402ce58598490d16226a784d6d0d3f571c4f
Niko Sams <ns@vivid-planet.com>
2015-03-20 10:21:01 +0100

    Fix tinymce jquery dependency

--- Kwf/Assets/TinyMce/Provider.php
+++ Kwf/Assets/TinyMce/Provider.php
@@ -7,4 +7,14 @@ class Kwf_Assets_TinyMce_Provider extends Kwf_Assets_Provider_Abstract
             return new Kwf_Assets_TinyMce_BuildDependency();
         }
     }
+
+    public function getDependenciesForDependency(Kwf_Assets_Dependency_Abstract $dependency)
+    {
+        $deps = array();
+        if ($dependency instanceof Kwf_Assets_TinyMce_BuildDependency) {
+            $deps[Kwf_Assets_Dependency_Abstract::DEPENDENCY_TYPE_REQUIRES][] = $this->_providerList->findDependency('jQuery');
+
+        }
+        return $deps;
+    }
 }
