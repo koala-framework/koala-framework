e66853b4cd2bc0cbab6b5fe949599f8e591d8cb1
Niko Sams <ns@vivid-planet.com>
2015-03-26 12:42:50 +0100

    Fix getChainedByMaster if there are not additional subroots
    fixes test

--- Kwc/Chained/Start/Component.php
+++ Kwc/Chained/Start/Component.php
@@ -23,7 +23,8 @@ class Kwc_Chained_Start_Component extends Kwc_Abstract
         foreach (Kwf_Component_Data_Root::getInstance()->getComponentsByClass($componentClass, $parentDataSelect) as $chainedStart) {
             //if additional subroots are above trl subroot (eg. domains)
             if ($sr = $chainedStart->parent->getSubroot()) {
-                if ($sr != $master->getSubroot()->parent->getSubroot()) {
+                $masterSr = $master->getSubroot();
+                if ($masterSr->parent && $sr != $masterSr->parent->getSubroot()) {
                     continue;
                 }
             }
