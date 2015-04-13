b360781b85d8ba1140ce2577a41f60b24102e23d
Alexander Kaufmann <ak@vivid-planet.com>
2015-04-13 11:20:30 +0200

    shop products detail event contentChanged needs component data for deleting cache

--- Kwc/Shop/Products/Detail/Events.php
+++ Kwc/Shop/Products/Detail/Events.php
@@ -24,9 +24,11 @@ class Kwc_Shop_Products_Detail_Events extends Kwc_Directories_Item_Detail_Events
 
     public function onPriceRowUpdate(Kwf_Component_Event_Row_Abstract $ev)
     {
-        $c = Kwf_Component_Data_Root::getInstance()->getComponentByDbId('shopProducts_'.$ev->row->getParentRow('Product')->id);
-        if ($c) {
-            $this->fireEvent(new Kwf_Component_Event_Component_ContentChanged($this->_class, $c));
+        $components = Kwf_Component_Data_Root::getInstance()->getComponentsByDbId(
+            'shopProducts_'.$ev->row->getParentRow('Product')->id, array('ignoreVisible' => true)
+        );
+        foreach ($components as $component) {
+            $this->fireEvent(new Kwf_Component_Event_Component_ContentChanged($this->_class, $component));
         }
     }
 }
