49f5b2db3b8cfde8dfe33b5d593a52331c12f151
Franz Unger <ufx@vivid-planet.com>
2015-04-27 15:29:40 +0200

    Revert "Use getPrimaryKey() instead of 'id'"
    This reverts commit 6cfe207bf6199467a68e23c6b87418ce7722cce4.

--- Kwf/Component/Generator/Events/Table.php
+++ Kwf/Component/Generator/Events/Table.php
@@ -123,16 +123,16 @@ class Kwf_Component_Generator_Events_Table extends Kwf_Component_Generator_Event
     {
         if ($this->_getGenerator()->hasSetting('dbIdShortcut') && $this->_getGenerator()->getSetting('dbIdShortcut')) {
             $dbId = $this->_getGenerator()->getSetting('dbIdShortcut') .
-                $row->{$row->getPrimaryKey()};
+                $row->id;
             $ret = Kwf_Component_Data_Root::getInstance()->getComponentsByDbId($dbId, $select);
         } else if ($row->getModel()->hasColumn('component_id')) {
             $dbId = $row->component_id .
                 $this->_getGenerator()->getIdSeparator() .
-                $row->{$row->getPrimaryKey()};
+                $row->id;
             $ret = Kwf_Component_Data_Root::getInstance()->getComponentsByDbId($dbId, $select);
         } else {
             $cls = $this->_getClassFromRow($row);
-            $select['id'] = $this->_getGenerator()->getIdSeparator().$row->{$row->getPrimaryKey()};
+            $select['id'] = $this->_getGenerator()->getIdSeparator().$row->id;
             $ret = Kwf_Component_Data_Root::getInstance()
                 ->getComponentsBySameClass($cls, $select);
         }
