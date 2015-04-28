1ed335d7b7e8da8e140499a4f312472d4c15aa4c
Daniel Bacher <db@vivid-planet.com>
2015-04-28 17:47:25 +0200

    Add frontend HTML5 file upload - Listen on drop and change - Progressbar - File Limit Setting

--- Kwf/Form/Field/File.php
+++ Kwf/Form/Field/File.php
@@ -22,7 +22,7 @@ class Kwf_Form_Field_File extends Kwf_Form_Field_SimpleAbstract
         } else if (strtolower(substr($maxSize, -1))=='g') {
             $maxSize = substr($maxSize, 0, -1)*1024*1024*1024;
         }
-        $this->setFileSizeLimit($maxSize.' B');
+        $this->setFileSizeLimit($maxSize);
         $this->setEmptyMessage(trlKwfStatic("Please choose a file"));
     }
 
@@ -162,9 +162,11 @@ class Kwf_Form_Field_File extends Kwf_Form_Field_SimpleAbstract
 
     public function getTemplateVars($values, $namePostfix = '', $idPrefix = '')
     {
+
         $name = $this->getFieldName();
         $value = isset($values[$name]) ? $values[$name] : null;
         $ret = parent::getTemplateVars($values, $namePostfix, $idPrefix);
+        $fileSizeLimit = $this->getFileSizeLimit();
 
         $name = htmlspecialchars($name);
         $ret['id'] = $idPrefix.str_replace(array('[', ']'), array('_', '_'), $name.$namePostfix);
@@ -181,9 +183,10 @@ class Kwf_Form_Field_File extends Kwf_Form_Field_SimpleAbstract
         $ret['html'] .= "<div class=\"kwfFormFieldFileInnerContent\">\n";
         $ret['html'] .= "<div class=\"imagePath kwfFormFieldFileUploadWrapper\">\n";
             $ret['html'] .= "<input class=\"fileSelector\" type=\"file\" id=\"$ret[id]\" name=\"$name$namePostfix\" ".
+                            " data-file-size-limit=\"$fileSizeLimit\" ".
                             " style=\"width: {$this->getWidth()}px\" onchange=\"document.getElementById(this.id+'_underlayText').value = this.value;\" />";
             $ret['html'] .= '<div class="underlayFileSelector">';
-            $ret['html'] .= '<input type="text" id="'.$ret['id'].'_underlayText" style="width: '.$this->getWidth().'px;" />';
+            $ret['html'] .= '<input type="text" id="'.$ret['id'].'_underlayText" class="kwfFormFieldFileUnderlayText" style="width: '.$this->getWidth().'px;" />';
             $ret['html'] .= ' <a href="#" class="kwfFormFieldFileUploadButton" onclick="return false;">'.$this->getFrontendButtonText().'</a>';
             $ret['html'] .= '</div>';
         $ret['html'] .= '</div>';
@@ -200,7 +203,7 @@ class Kwf_Form_Field_File extends Kwf_Form_Field_SimpleAbstract
             $uploadId = '0';
             $hashKey = '';
         }
-        $ret['html'] .= "<input type=\"hidden\" name=\"{$name}_upload_id{$namePostfix}\" ".
+        $ret['html'] .= "<input type=\"hidden\" class=\"kwfUploadIdField\" name=\"{$name}_upload_id{$namePostfix}\" ".
                     " value=\"$uploadId$hashKey\" />";
         $ret['html'] .= '</div>';
         return $ret;
---
+++ Kwf_js/FrontendForm/File.js
@@ -0,0 +1,120 @@
+Kwf.FrontendForm.File = Ext2.extend(Kwf.FrontendForm.Field, {
+    initField: function() {
+        this.dropContainer = $(this.el.dom);
+        this.fileInput = $(this.el.dom).find('input[type="file"]');
+        this.uploadIdField = this.dropContainer.find('input.kwfUploadIdField');
+        this.fileSizeLimit = this.fileInput.data('fileSizeLimit');
+
+        // Prevent Event-Bubbling
+        this.dropContainer.on('dragenter', function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+        });
+
+        this.dropContainer.on('dragover', function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+        });
+
+        this.dropContainer.get(0).addEventListener('drop', this.onDrop.bind(this));
+        this.fileInput.get(0).addEventListener('change', this.onDrop.bind(this));
+
+    },
+    onDrop: function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        var files = e.dataTransfer ? e.dataTransfer.files : e.target.files;
+        var file = files[0];
+
+        if (file.size > this.fileSizeLimit) {
+            return alert(trl('Allowed upload size exceeded max. allowed upload size {0} MB', this.fileSizeLimit/1048576));
+        }
+
+        var progressbar = $(
+            '<div class="kwfFormFieldUploadProgressBar">' +
+                '<div class="inner">' +
+                    '<span class="progress"></span>' +
+                    '<span class="processing">'+trlKwf("Processing")+'...</span>' +
+                '</div>' +
+            '</div>');
+
+        this.dropContainer.prepend(progressbar);
+
+        var uploadIdField = this.uploadIdField;
+
+        this.dropContainer.find('input.kwfFormFieldFileUnderlayText').val(file.name);
+
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            var xhr = new XMLHttpRequest();
+            var url = '/kwf/media/upload/json-upload';
+            if (Kwf.sessionToken) url += '?kwfSessionToken='+Kwf.sessionToken;
+            xhr.open('POST', url);
+            xhr.setRequestHeader('X-Upload-Name', encodeURIComponent(file.name));
+            xhr.setRequestHeader('X-Upload-Size', file.size);
+            xhr.setRequestHeader('X-Upload-Type', file.type);
+            xhr.overrideMimeType('text/plain; charset=x-user-defined-binary');
+
+
+            xhr.upload.onprogress = function(data) {
+                if (data.lengthComputable) {
+                    var progress = (data.loaded / data.total) * 100;
+                    if (progress < 100) {
+                        progressbar.find('span.progress').css('width', progress+'%');
+                    } else {
+                        progressbar.find('span.progress').css('width', '100%');
+                        progressbar.find('span.progress').hide();
+                        progressbar.find('span.processing').addClass('visible');
+                    }
+                }
+            };
+
+            xhr.send(file);
+
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    progressbar.fadeOut(function() {
+                        $(this).remove();
+                    });
+
+                    var response;
+                    try {
+                        response = JSON.parse(xhr.response);
+                    } catch (e) {};
+
+                    if (response) {
+                        uploadIdField.val(response.value.uploadId+'_'+response.value.hashKey);
+                    }
+                } else if (xhr.readyState == 4 && xhr.status !== 200) {
+                    return alert(trl('An error occured, please try again later'));
+                }
+            }
+        };
+
+        reader.readAsDataURL(file);
+
+    },
+    getFieldName: function() {
+        var inp = this.el.child('input.fileSelector');
+        if (!inp) return null;
+        return inp.dom.name;
+    },
+    getValue: function() {
+        var inp = this.el.child('input[type="hidden"]');
+        if (!inp) return null;
+        var ret = inp.dom.value;
+        return ret;
+    },
+    clearValue: function() {
+        var inp = this.el.child('input[type="hidden"]');
+        inp.dom.value = '';
+    },
+    setValue: function(value) {
+        var inp = this.el.child('input[type="hidden"]');
+        inp.dom.value = value;
+    }
+});
+
+Kwf.FrontendForm.fields['kwfFormFieldFile'] = Kwf.FrontendForm.File;
