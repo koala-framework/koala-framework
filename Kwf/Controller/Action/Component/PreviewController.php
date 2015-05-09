d4217109ca825455d6f49b366ae72bee2ae251fd
Niko Sams <niko.sams@gmail.com>
2015-05-09 10:30:30 +0200

    Fix XSS Issue in preview
    fixes #337

--- Kwf/Controller/Action/Component/PreviewController.php
+++ Kwf/Controller/Action/Component/PreviewController.php
@@ -5,7 +5,10 @@ class Kwf_Controller_Action_Component_PreviewController extends Kwf_Controller_A
     {
         $this->view->config = Kwf_Registry::get('config')->preview->toArray();
         $this->view->xtype = 'kwf.component.preview';
-        $this->view->initialUrl = $this->_getParam('url');
+        $this->view->initialUrl = null;
+        if (preg_match('#^https?://#', $this->view->initialUrl)) {
+            $this->view->initialUrl = $this->_getParam('url');
+        }
         if (!$this->view->initialUrl) {
             $this->view->initialUrl = 'http://'.$_SERVER['HTTP_HOST'].Kwf_Setup::getBaseUrl().'/';
         }
