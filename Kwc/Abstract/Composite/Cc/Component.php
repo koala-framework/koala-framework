2fb96250df9c1a72296d85de25afb30e409d9ed7
Niko Sams <ns@vivid-planet.com>
2015-05-26 13:48:50 +0200

    Add renderer for Composite_Cc getTemplateVars
    fixes Cc html component (which needs renderer)

--- Kwc/Abstract/Composite/Cc/Component.php
+++ Kwc/Abstract/Composite/Cc/Component.php
@@ -1,9 +1,9 @@
 <?php
 class Kwc_Abstract_Composite_Cc_Component extends Kwc_Chained_Cc_Component
 {
-    public function getTemplateVars()
+    public function getTemplateVars(Kwf_Component_Renderer_Abstract $renderer = null)
     {
-        $ret = parent::getTemplateVars();
+        $ret = parent::getTemplateVars($renderer);
         foreach ($this->getData()->getChildComponents(array('generator' => 'child')) as $c) {
             if ($ret[$c->id]) $ret[$c->id] = $c; // Bei TextImage kann zB. Bild ausgeblendet werden und soll dann in Übersetzung auch nicht angezeigt werden
         }
