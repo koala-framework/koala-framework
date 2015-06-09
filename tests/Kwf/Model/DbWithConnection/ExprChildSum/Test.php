5795a2e3282cc65fc3814ce2e6319b1b7948dafd
Alexander Kaufmann <ak@vivid-planet.com>
2015-06-09 08:05:51 +0200

    added some tests for sorting model exprs

--- tests/Kwf/Model/DbWithConnection/ExprChildSum/Test.php
+++ tests/Kwf/Model/DbWithConnection/ExprChildSum/Test.php
@@ -52,4 +52,25 @@ class Kwf_Model_DbWithConnection_ExprChildSum_Test extends Kwf_Test_TestCase
         $sum = $row->foo_value_sum;
         $this->assertEquals(6, $sum);
     }
+
+    public function testChildSumWithExpressionOrder()
+    {
+        $select = new Kwf_Model_Select();
+        $s = new Kwf_Model_Select();
+        $s->order('foo_value_sum', 'DESC');
+        $s->expr('foo_value_sum');
+        $row = $this->_modelBar->getRow($s);
+        $sum = $row->foo_value_sum;
+        $this->assertEquals(6, $sum);
+    }
+
+    public function testChildSumWithExpressionOrderLazy()
+    {
+        $select = new Kwf_Model_Select();
+        $s = new Kwf_Model_Select();
+        $s->order('foo_value_sum', 'DESC');
+        $row = $this->_modelBar->getRow($s);
+        $sum = $row->foo_value_sum;
+        $this->assertEquals(6, $sum);
+    }
 }
