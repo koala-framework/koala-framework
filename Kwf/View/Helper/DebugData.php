d522048deb1f4c920cce7b13afa5ecca78014f7f
Niko Sams <ns@vivid-planet.com>
2015-06-11 10:11:16 +0200

    Support uniquePrefix for DebugData output

--- Kwf/View/Helper/DebugData.php
+++ Kwf/View/Helper/DebugData.php
@@ -8,12 +8,17 @@ class Kwf_View_Helper_DebugData
         if ($config['benchmark'] || $config['menu'] || !$config['error']['log'] || (Kwf_Component_Data_Root::getInstance() && Kwf_Component_Data_Root::getInstance()->filename)) {
             $indent = str_repeat(' ', 8);
             $ret .= "<script type=\"text/javascript\">\n";
-            $ret .=$indent."if (typeof Kwf == 'undefined') Kwf = {};\n";
-            $ret .=$indent."Kwf.Debug = {};\n";
-            $ret .=$indent.'Kwf.Debug.displayErrors = '.(!$config['error']['log'] ? 'true' : 'false').";\n";
-            $ret .=$indent.'Kwf.Debug.benchmark = '.($config['benchmark'] ? 'true' : 'false').";\n";
+            $kwf = 'Kwf';
+            if ($uniquePrefix = Kwf_Config::getValue('application.uniquePrefix')) {
+                $ret .=$indent."if (typeof $uniquePrefix == 'undefined') $uniquePrefix = {};\n";
+                $kwf = $uniquePrefix.'.'.$kwf;
+            }
+            $ret .=$indent."if (typeof $kwf == 'undefined') $kwf = {};\n";
+            $ret .=$indent."$kwf.Debug = {};\n";
+            $ret .=$indent."$kwf.Debug.displayErrors = ".(!$config['error']['log'] ? 'true' : 'false').";\n";
+            $ret .=$indent."$kwf.Debug.benchmark = ".($config['benchmark'] ? 'true' : 'false').";\n";
             if (Kwf_Component_Data_Root::getInstance() && Kwf_Component_Data_Root::getInstance()->filename) {
-                $ret .=$indent.'Kwf.Debug.rootFilename = \'/'.Kwf_Component_Data_Root::getInstance()->filename."';\n";
+                $ret .=$indent."$kwf.Debug.rootFilename = '/".Kwf_Component_Data_Root::getInstance()->filename."';\n";
             }
             $ret .=$indent."</script>\n";
         }
