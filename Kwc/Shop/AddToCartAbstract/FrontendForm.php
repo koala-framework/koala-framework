745805c14a9dda674240ddf9f12d42200c00a371
Niko Sams <ns@vivid-planet.com>
2015-06-18 10:07:57 +0200

    remove & from row parameter
    was previously used to allow adding amount to existing row.
    But that was a very hackish solution and was removed

--- Kwc/Shop/AddToCartAbstract/FrontendForm.php
+++ Kwc/Shop/AddToCartAbstract/FrontendForm.php
@@ -1,7 +1,7 @@
 <?php
 class Kwc_Shop_AddToCartAbstract_FrontendForm extends Kwf_Form
 {
-    protected function _beforeInsert(&$row)
+    protected function _beforeInsert($row)
     {
         if (!$row->amount) {
             $row->amount = 1;
