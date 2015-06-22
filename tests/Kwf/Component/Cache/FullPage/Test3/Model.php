0ae55fe9ac7c8c364e433d223dcb5ec98953112e
Niko Sams <ns@vivid-planet.com>
2015-06-22 11:12:33 +0200

    Add test for fullpage clearing
    This test asserts implementation details (like how many entries have been deleted) - which should not be done - but in this case we want to test if not too many entries get deleted.
    
    When deleting a component cache entry the includes of type componentLink don't need to be followed. (bad for performance)

---
+++ tests/Kwf/Component/Cache/FullPage/Test3/Model.php
@@ -0,0 +1,9 @@
+<?php
+class Kwf_Component_Cache_FullPage_Test3_Model extends Kwf_Model_FnF
+{
+    protected $_primaryKey = 'component_id';
+
+    protected $_data = array(
+        array('component_id'=>'root_test3', 'test'=>'content3')
+    );
+}
