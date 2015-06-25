702f6ba061d9aa1ff0c4d6002ed48a982200797a
Niko Sams <ns@vivid-planet.com>
2015-06-25 10:04:32 +0200

    Clear all view caches because cache_component_includes changed incompatible

---
+++ Kwf/Update/38017.php
@@ -0,0 +1,13 @@
+<?php
+class Kwf_Update_38017 extends Kwf_Update
+{
+    protected $_tags = array('kwc');
+
+    public function update()
+    {
+        //clear all view caches, required for 7a36ae58a
+        Kwf_Cache_Simple::getMemcache()->flush();
+        Kwf_Registry::get('db')->query('TRUNCATE cache_component');
+        Kwf_Registry::get('db')->query('TRUNCATE cache_component_includes');
+    }
+}
