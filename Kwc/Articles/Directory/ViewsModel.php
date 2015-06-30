d9ac7f537f80c589c4ca6b916b81f80b7ebfaf12
Franz Unger <ufx@vivid-planet.com>
2015-06-30 16:26:10 +0200

    Fix wrong spelling

--- Kwc/Articles/Directory/ViewsModel.php
+++ Kwc/Articles/Directory/ViewsModel.php
@@ -5,7 +5,7 @@ class Kwc_Articles_Directory_ViewsModel extends Kwf_Model_Db
 
     // referenceModel Users has to be set in project
     protected $_referenceMap = array(
-        'Acticle' => 'article_id->Kwc_Articles_Directory_Model',
+        'Article' => 'article_id->Kwc_Articles_Directory_Model',
     );
 
     public function __construct(array $config = array())
@@ -17,7 +17,7 @@ class Kwc_Articles_Directory_ViewsModel extends Kwf_Model_Db
     protected function _init()
     {
         parent::_init();
-        $this->_exprs['article_is_top'] = new Kwf_Model_Select_Expr_Parent('Acticle', 'is_top');
-        $this->_exprs['article_visible'] = new Kwf_Model_Select_Expr_Parent('Acticle', 'visible');
+        $this->_exprs['article_is_top'] = new Kwf_Model_Select_Expr_Parent('Article', 'is_top');
+        $this->_exprs['article_visible'] = new Kwf_Model_Select_Expr_Parent('Article', 'visible');
     }
 }
