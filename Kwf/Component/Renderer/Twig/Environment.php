5d6a71ac5e0f2ff7395dfaea9bca74ec2d3adb69
Niko Sams <ns@vivid-planet.com>
2015-07-14 10:32:20 +0200

    Support rendering master template as twig

--- Kwf/Component/Renderer/Twig/Environment.php
+++ Kwf/Component/Renderer/Twig/Environment.php
@@ -1,9 +1,21 @@
 <?php
 class Kwf_Component_Renderer_Twig_Environment extends Kwf_View_Twig_Environment
 {
+    private $_renderer;
     public function __construct(Kwf_Component_Renderer_Abstract $renderer)
     {
         parent::__construct();
         $this->addGlobal('renderer', new Kwf_Component_Renderer_Twig_Helper($renderer));
+        $this->addFunction('includeCode', new Twig_SimpleFunction('includeCode', array($this, 'includeCode'), array(
+            'needs_context' => true,
+        )));
+        $this->_renderer = $renderer;
+    }
+
+    public function includeCode($context, $position)
+    {
+        $helper = $this->_renderer->getHelper('includeCode');
+        $helper->setView((object)$context);
+        return new Twig_Markup($helper->includeCode($position), 'utf-8');
     }
 }
