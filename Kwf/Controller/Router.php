koala-framework/koala-framework

commit 105e189aa76d59a0f6bafda1428e55dd9136c15f

Remove unused Trl controllers
we don't translate in the web anymore

Kwf/Controller/Router.php
@@ -35,12 +35,6 @@ class Kwf_Controller_Router extends Zend_Controller_Router_Rewrite
                     array('module'     => 'kwf_controller_action_pool',
                           'controller' => 'pools',
                           'action'     => 'index')));
-        $this->AddRoute('kwf_trl', new Zend_Controller_Router_Route(
-                    '/kwf/trl/:controller/:action',
-                    array('module'     => 'kwf_controller_action_trl',
-                          'controller' => 'index',
-                          'action'     => 'index')));
-        $this->AddRoute('kwf_debug', new Zend_Controller_Router_Route(
                     '/kwf/debug/:controller/:action',
                     array('module'     => 'kwf_controller_action_debug',
                           'controller' => 'index',

Kwf/Controller/Action/Trl/Grid.js
@@ -1,18 +1,0 @@
-Ext2.namespace('Kwf.Trl');
-
-Kwf.Trl.Grid = Ext2.extend(Kwf.Auto.GridPanel, {
-    initComponent : function()
-    {
-        Kwf.Trl.Grid.superclass.initComponent.call(this);
-        this.on('loaded', function() {
-            this.getEditDialog().getAutoForm().on('beforeloadform', function(form, data) {
-                form.items.items.each(function(value, index) {
-                    if (!data[value.name] && !data[this.language+'_plural'] && value.name.match(/.*_plural/)) {
-                        value.hide();
-
-                    }
-                }, this);
-            }, this);
-        }, this);
-    }
-});

Kwf/Controller/Action/Trl/KwfController.php
@@ -1,118 +1,0 @@
-<?php
-class Kwf_Controller_Action_Trl_KwfController extends Kwf_Controller_Action_Auto_Grid
-{
-    protected $_modelName = "Kwf_Trl_Model_Kwf";
-    protected $_buttons = array();
-    protected $_sortable = true;
-    protected $_defaultOrder = 'id';
-    protected $_paging = 30;
-    protected $_editDialog = array('controllerUrl'=>'/kwf/trl/kwf-edit',
-                                   'width'=>600,
-                                   'height'=>550);
-
-    protected function _initColumns()
-    {
-        $lang = $this->_getLanguage();
-
-        $this->_filters['text'] = array(
-            'type'=>'TextField',
-            'width'=>180,
-            'queryFields' => array($lang, $lang.'_plural')
-        );
-        $this->_filters['empty'] = array(
-            'type'=>'Button',
-            'text' => trlKwf('only empty'),
-            'icon' => new Kwf_Asset('textfield'),
-            'cls' => 'x2-btn-text-icon',
-            'skipWhere' => true
-        );
-
-
-        $this->_columns->add(new Kwf_Grid_Column_Button('edit'));
-        $this->_columns->add(new Kwf_Grid_Column('id', 'Id', 50));
-        $this->_columns->add(new Kwf_Grid_Column('context', trlKwf('Context'), 100));
-        $this->_columns->add(new Kwf_Grid_Column($lang, $lang.' '.trlKwf('Singular'), 350));
-        $this->_columns->add(new Kwf_Grid_Column($lang.'_plural', $lang.' '.trlKwf('Plural'), 150));
-
-        $langs = self::getLanguages();
-        if ($langs) {
-            foreach ($langs as $lang) {
-                if ($lang != $this->_getLanguage()) {
-                    $this->_columns->add(new Kwf_Grid_Column($lang, $lang.' '.trlKwf('Singular'), 350));
-                    $this->_columns->add(new Kwf_Grid_Column($lang.'_plural', $lang.' '.trlKwf('Plural'), 150));
-                }
-            }
-        }
-
-        parent::_initColumns();
-    }
-
-    protected function _getSelect()
-    {
-        $ret = parent::_getSelect();
-        if ($this->_getParam('query_empty')) {
-            $or = array();
-            $langs = self::getLanguages();
-            foreach ($langs as $lang) {
-                $or[] = new Kwf_Model_Select_Expr_Equal($lang, '');
-            }
-            $ret->where(new Kwf_Model_Select_Expr_Or($or));
-
-        }
-        return $ret;
-    }
-
-    protected function _getLanguage()
-    {
-        return 'en';
-    }
-
-    static public function getLanguages()
-    {
-        $config = Zend_Registry::get('config');
-        $langs = array();
-        if ($config->webCodeLanguage) $langs[] = $config->webCodeLanguage;
-
-        $possibleUserLanguages = array();
-        if ($config->languages) {
-            foreach ($config->languages as $lang=>$name) {
-                $possibleUserLanguages[] = $lang;
-            }
-        }
-        $userModel = Kwf_Registry::get('userModel');
-        if (isset($userModel->getAuthedUser()->language) &&
-            $userModel->getAuthedUser()->language &&
-            in_array($userModel->getAuthedUser()->language, $possibleUserLanguages))
-        {
-            $langs[] = $userModel->getAuthedUser()->language;
-        }
-
-        if (Kwf_Component_Data_Root::getComponentClass()) {
-            $lngClasses = array();
-            foreach(Kwc_Abstract::getComponentClasses() as $c) {
-                if (Kwc_Abstract::hasSetting($c, 'baseProperties') &&
-                    in_array('language', Kwc_Abstract::getSetting($c, 'baseProperties'))
-                ) {
-                    $lngClasses[] = $c;
-                }
-            }
-            $lngs = Kwf_Component_Data_Root::getInstance()
-                ->getComponentsBySameClass($lngClasses, array('ignoreVisible'=>true));
-            foreach ($lngs as $c) {
-                if (Kwf_Registry::get('acl')->getComponentAcl()->isAllowed($userModel->getAuthedUser(), $c)) {
-                    $langs[] = $c->getLanguage();
-                }
-            }
-        }
-        return array_unique($langs);
-    }
-
-    public function indexAction ()
-    {
-        $config = array(
-            'controllerUrl' => $this->getRequest()->getBaseUrl().'/'.ltrim($this->getRequest()->getPathInfo(), '/'),
-            'language' => $this->_getLanguage()
-        );
-        $this->view->ext('Kwf.Trl.Grid', $config);
-    }
-}

Kwf/Controller/Action/Trl/KwfEditController.php
@@ -1,44 +1,0 @@
-<?php
-class Kwf_Controller_Action_Trl_KwfEditController extends Kwf_Controller_Action_Auto_Form
-{
-    protected $_permissions = array('save', 'add');
-    protected $_modelName = 'Kwf_Trl_Model_Kwf';
-    protected $_colNames = array();
-
-    protected function _initFields()
-    {
-        $lang = $this->_getLanguage();
-        $this->_form->add(new Kwf_Form_Field_ShowField('context', trlKwf('Context')));
-        $this->_form->add(new Kwf_Form_Field_ShowField($lang, $lang.' '.trlKwf('Singular')));
-        $this->_form->add(new Kwf_Form_Field_ShowField($lang.'_plural', $lang.' '.trlKwf('Plural')));
-
-        $langs = Kwf_Controller_Action_Trl_KwfController::getLanguages();
-        if ($langs) {
-            foreach ($langs as $lang) {
-                if ($lang != $this->_getLanguage()) {
-                    $this->_form->add(new Kwf_Form_Field_TextField($lang, $lang." ".trlKwf("Singular")))->setWidth(400);
-                    $this->_colNames[] = $lang;
-
-                    $this->_form->add(new Kwf_Form_Field_TextField($lang."_plural", $lang." ".trlKwf("Plural")))->setWidth(400);
-                    $this->_colNames[] = $lang."_plural";
-                }
-            }
-        }
-    }
-
-    protected function _getLanguage()
-    {
-        return 'en';
-    }
-
-    protected function _beforeSave(Kwf_Model_Row_Interface $row)
-    {
-        parent::_beforeSave($row);
-        foreach ($this->_colNames as $colName)
-        if (!$row->{$colName}) {
-            unset($row->{$colName});
-        }
-    }
-
-
-}

Kwf/Controller/Action/Trl/WebBasicController.php
@@ -1,97 +1,0 @@
-<?php
-class Kwf_Controller_Action_Trl_WebBasicController extends Kwf_Controller_Action_Auto_Grid
-{
-    protected $_modelName = "Kwf_Trl_Model_Web";
-    protected $_buttons = array('save');
-    protected $_sortable = false;
-    protected $_defaultOrder = 'id';
-    protected $_paging = 30;
-    protected $_columns;
-    protected $_colNames = array();
-    protected $_showAllLanguages = false;
-
-    protected function _initColumns()
-    {
-        $this->_filters['text'] = array(
-            'type'=>'TextField',
-            'width'=>80
-        );
-
-        $config = Zend_Registry::get('config');
-        $weblang = $config->webCodeLanguage;
-
-        $this->_columns->add(new Kwf_Grid_Column('id'));
-        $this->_columns->add(new Kwf_Grid_Column('context', trlKwf('Context')))
-            ->setWidth(50);
-
-        $languages = array();
-        $languages[] = $weblang;
-        $plural = array();
-        $role = $this->_getAuthData()->role;
-        $user_lang = $this->_getAuthData()->language;
-        $showAllLanguages = $role == 'admin' || $this->_showAllLanguages;
-
-        //defintion der zu übersetzenden sprachen
-        if ($showAllLanguages) {
-            if ($config->languages) {
-                foreach($config->languages as $language) {
-                    if ($language != $weblang) {
-                        $languages[] = $language;
-                    }
-                }
-            }
-        } else {
-            if ($user_lang != $weblang)
-                $languages[] = $user_lang;
-        }
-
-        //ausgabe der einzahl Felder
-        foreach ($languages as $lang) {
-            //Singular
-            if ($lang == $weblang) {
-                $this->_columns->add(new Kwf_Grid_Column($lang, $lang." ".trlKwf("Singular")))
-                    ->setWidth(200)
-                    ->setRenderer('notEditable');
-                $this->_colNames[] = $lang;
-            } else {
-                if ($lang == $user_lang || $showAllLanguages) {
-                    $this->_columns->add(new Kwf_Grid_Column($lang, $lang." ".trlKwf("Singular")))
-                        ->setEditor(new Kwf_Form_Field_TextField())
-                        ->setWidth(200);;
-                    $this->_colNames[] = $lang;
-                }
-            }
-        }
-
-        //Ausgabe der Plural felder
-        foreach ($languages as $lang) {
-            //Plural
-            if ($lang == $weblang) {
-                $this->_columns->add(new Kwf_Grid_Column($lang."_plural", $lang." ".trlKwf("Plural")))
-                    ->setWidth(200)
-                    ->setRenderer('notEditable');
-                $this->_colNames[] = $lang."_plural";
-            } else {
-                if ($lang == $user_lang || $showAllLanguages) {
-                    $this->_columns->add(new Kwf_Grid_Column($lang."_plural", $lang." ".trlKwf("Plural")))
-                        ->setEditor(new Kwf_Form_Field_TextField())
-                        ->setWidth(200);
-                    $this->_colNames[] = $lang."_plural";
-                }
-            }
-       }
-
-        parent::_initColumns();
-    }
-
-    protected function _beforeSave(Kwf_Model_Row_Interface $row, $submitRow)
-    {
-        parent::_beforeSave($row, $submitRow);
-        foreach ($this->_colNames as $colName)
-        if (!$row->{$colName}) {
-            $row->{$colName} = null;
-        } else if ($row->$colName == ' ') {
-            $row->$colName = '';
-        }
-    }
-}

Kwf/Controller/Action/Trl/WebController.php
@@ -1,14 +1,0 @@
-<?php
-class Kwf_Controller_Action_Trl_WebController extends Kwf_Controller_Action_Trl_KwfController
-{
-    protected $_modelName = "Kwf_Trl_Model_Web";
-    protected $_editDialog = array('controllerUrl'=>'/kwf/trl/web-edit',
-                                   'width'=>600,
-                                   'height'=>550);
-
-    protected function _getLanguage()
-    {
-        $config = Zend_Registry::get('config');
-        return $config->webCodeLanguage;
-    }
-}

Kwf/Controller/Action/Trl/WebEditController.php
@@ -1,11 +1,0 @@
-<?php
-class Kwf_Controller_Action_Trl_WebEditController extends Kwf_Controller_Action_Trl_KwfEditController
-{
-    protected $_modelName = 'Kwf_Trl_Model_Web';
-
-    protected function _getLanguage()
-    {
-        $config = Zend_Registry::get('config');
-        return $config->webCodeLanguage;
-    }
-}

dependencies.ini
@@ -535,10 +535,6 @@ KwfTooltips.dep[] = ExtQuickTips
 KwfMailDecode.files[] = kwf/Kwf_js/MailDecode.js
 KwfMailDecode.dep[] = KwfOnReady
 
-KwfTrlAdmin.files[] = kwf/Kwf/Controller/Action/Trl/Grid.js
-KwfTrlAdmin.dep[] = KwfAutoGrid
-KwfTrlAdmin.dep[] = KwfAutoForm
-
 KwfStatistics.files[] = kwf/Kwf_js/Statistics.js
 
 KwfUpload.files[] = kwf/Kwf_js/Utils/Upload.js

commit 0325af032bd48df5b74cfb66aa2f83bdd3a87522

Fix syntax error introduced with 105e189aa

Kwf/Controller/Router.php
@@ -35,6 +35,7 @@ class Kwf_Controller_Router extends Zend_Controller_Router_Rewrite
                     array('module'     => 'kwf_controller_action_pool',
                           'controller' => 'pools',
                           'action'     => 'index')));
+        $this->AddRoute('kwf_debug', new Zend_Controller_Router_Route(
                     '/kwf/debug/:controller/:action',
                     array('module'     => 'kwf_controller_action_debug',
                           'controller' => 'index',