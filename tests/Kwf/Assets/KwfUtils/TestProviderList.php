8db8bceef5d16e9ae49eb2ec8644322971c3649f
Benjamin Hohenwarter <bh@vivid-planet.com>
2015-07-23 10:00:19 +0200

    Remove Kwf_Assets_Provider_KwfUtils usage, class doesn't exist anymore

--- tests/Kwf/Assets/KwfUtils/TestProviderList.php
+++ tests/Kwf/Assets/KwfUtils/TestProviderList.php
@@ -4,7 +4,6 @@ class Kwf_Assets_KwfUtils_TestProviderList extends Kwf_Assets_ProviderList_Abstr
     public function __construct()
     {
         $providers = array(
-            new Kwf_Assets_Provider_KwfUtils(),
             new Kwf_Assets_Provider_JsClass(KWF_PATH.'/tests', 'kwf/tests'),
             new Kwf_Assets_Provider_Ini(KWF_PATH.'/dependencies.ini'),
             new Kwf_Assets_Provider_Ini(VENDOR_PATH.'/koala-framework/extjs2/dependencies.ini'),
