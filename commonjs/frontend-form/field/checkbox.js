21901acf13cf86a21cfc8a8b0cb615eb1282bb86
Michael Tiefenthaler <mt@vivid-planet.com>
2015-07-23 12:58:05 +0200

    Fix javascript problem
    In $('someelement').each(function(element){...}) we need to use "$(element)" instead of plane "element" to apply a jQuery funktion on it.

--- commonjs/frontend-form/field/checkbox.js
+++ commonjs/frontend-form/field/checkbox.js
@@ -1,3 +1,4 @@
+var $ = require('jQuery');
 var fieldRegistry = require('kwf/frontend-form/field-registry');
 var Field = require('kwf/frontend-form/field/field');
 var kwfExtend = require('kwf/extend');
@@ -5,7 +6,7 @@ var kwfExtend = require('kwf/extend');
 var Checkbox = kwfExtend(Field, {
     initField: function() {
         this.el.select('input').each(function(input) {
-            input.on('click', function() {
+            $(input).on('click', function() {
                 this.el.trigger('kwf-form-change', this.getValue());
             }, this);
         }, this);
