koala-framework/koala-framework

commit bf77cecf6b9b94ed54f6558600556da6c80bbe65

Avoid slow in_array, instead use with spl_object_hash as hashmap

Kwf/Assets/Dependency/Abstract.php
@@ -215,7 +215,7 @@ abstract class Kwf_Assets_Dependency_Abstract
             $nonDefer = array();
             $this->_getDependenciesNonDefer($this, $nonDefer, array());
             foreach ($ret as $k=>$i) {
-                if (in_array($i, $nonDefer, true)) {
+                if (!isset($nonDefer[spl_object_hash($i)])) {
                     unset($ret[$k]);
                 }
             }
@@ -225,31 +225,31 @@ abstract class Kwf_Assets_Dependency_Abstract
 
     private function _getDependenciesNonDefer($dep, &$processed, $stack)
     {
-        if (in_array($dep, $processed, true)) {
+        if (isset($processed[spl_object_hash($dep)])) {
             return;
         }
-        $stack[] = $dep;
+        $stack[spl_object_hash($dep)] = true;
         if (!$dep->getDeferLoad()) {
             $requires = $dep->getDependencies(Kwf_Assets_Dependency_Abstract::DEPENDENCY_TYPE_ALL);
             foreach ($requires as $i) {
-                if (!in_array($i, $stack, true)) {
+                if (!isset($stack[spl_object_hash($i)])) {
                     $this->_getDependenciesNonDefer($i, $processed, $stack);
                 }
             }
-            if (in_array($dep, $processed, true)) {
+            if (isset($processed[spl_object_hash($dep)])) {
                 return;
             }
 
-            $processed[] = $dep;
+            $processed[spl_object_hash($dep)] = true;
         }
     }
 
     private function _getFilteredUniqueDependenciesProcessDep($dep, $mimeType, &$processed, $stack, $includeSelf)
     {
-        if (in_array($dep, $processed, true)) {
+        if (isset($processed[spl_object_hash($dep)])) {
             return array();
         }
-        $stack[] = $dep;
+        $stack[spl_object_hash($dep)] = true;
 
         $ret = array();
         if ($mimeType == 'text/javascript' && $dep->getDeferLoad()) {
@@ -262,7 +262,7 @@ abstract class Kwf_Assets_Dependency_Abstract
 
         foreach ($dep->getDependencies(Kwf_Assets_Dependency_Abstract::DEPENDENCY_TYPE_REQUIRES) as $i) {
             if (!$i) throw new Kwf_Exception("$dep returned invalid dependency");
-            if (!in_array($i, $stack, true)) {
+            if (!isset($stack[spl_object_hash($i)])) {
                 foreach ($this->_getFilteredUniqueDependenciesProcessDep($i, $mimeType, $processed, $stack, true) as $j) {
                     $ret[] = $j;
                 }
@@ -271,18 +271,18 @@ abstract class Kwf_Assets_Dependency_Abstract
 
         foreach ($dep->getDependencies(Kwf_Assets_Dependency_Abstract::DEPENDENCY_TYPE_COMMONJS) as $i) {
             if (!$i) throw new Kwf_Exception("$dep returned invalid dependency");
-            if (!in_array($i, $stack, true)) {
+            if (!isset($stack[spl_object_hash($i)])) {
                 foreach ($this->_getFilteredUniqueDependenciesProcessDep($i, $mimeType, $processed, $stack, false) as $j) {
                     $ret[] = $j;
                 }
             }
         }
 
-        if (in_array($dep, $processed, true)) {
+        if (isset($processed[spl_object_hash($dep)])) {
             return $ret;
         }
 
-        $processed[] = $dep;
+        $processed[spl_object_hash($dep)] = true;
 
         $mimeMatches = $dep->getMimeType() == $mimeType
             || ($mimeType == 'text/javascript; defer2' && $dep->getMimeType() == 'text/javascript');

Kwf/Assets/ProviderList/Abstract.php
@@ -93,8 +93,8 @@ class Kwf_Assets_ProviderList_Abstract implements Serializable
     private function _setDependenciesForDependency(Kwf_Assets_Dependency_Abstract $dependency)
     {
         static $set = array();
-        if (in_array($dependency, $set, true)) return;
-        $set[] = $dependency;
+        if (isset($set[spl_object_hash($dependency)])) return;
+        $set[spl_object_hash($dependency)] = true;
 
         foreach ($dependency->getDependencies(Kwf_Assets_Dependency_Abstract::DEPENDENCY_TYPE_ALL) as $d) {
             $this->_setDependenciesForDependency($d);

Kwf/Assets/Dependency/File/Js.php
@@ -46,7 +46,7 @@ class Kwf_Assets_Dependency_File_Js extends Kwf_Assets_Dependency_File
         } else {
             $buildFile = sys_get_temp_dir().'/kwf-uglifyjs/'.$fileName.'.v2'.md5(file_get_contents($this->getAbsoluteFileName()));
         }
-        $useTrl = !in_array($pathType, array('ext2'));
+        $useTrl = $pathType != 'ext2';
         if (substr($this->getAbsoluteFileName(), 0, 24) == 'vendor/bower_components/') {
             //dependencies loaded via bower never use kwf translation system
             $useTrl = false;

Kwf/Assets/Dependency/Iterator/UniqueFilter.php
@@ -5,8 +5,8 @@ class Kwf_Assets_Dependency_Iterator_UniqueFilter extends RecursiveFilterIterato
     public function accept()
     {
         $cur = $this->current();
-        if (!in_array($cur, $this->_processed, true)) {
-            $this->_processed[] = $cur;
+        if (!isset($this->_processed[spl_object_hash($cur)])) {
+            $this->_processed[spl_object_hash($cur)] = true;
             return true;
         }
         return false;

commit 2828632771866458b5e1918fdd700d0d40b1d81e

Fix regression introduced with bf77cecf

Kwf/Assets/Dependency/Abstract.php
@@ -215,7 +215,7 @@ abstract class Kwf_Assets_Dependency_Abstract
             $nonDefer = array();
             $this->_getDependenciesNonDefer($this, $nonDefer, array());
             foreach ($ret as $k=>$i) {
-                if (!isset($nonDefer[spl_object_hash($i)])) {
+                if (isset($nonDefer[spl_object_hash($i)])) {
                     unset($ret[$k]);
                 }
             }