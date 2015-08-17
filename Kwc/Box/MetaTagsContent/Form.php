733ce4c331e6349b1f99f80fd7b7631d4f4203a5
Michael Freudenthaler <mf@vivid-planet.com>
2015-08-17 16:08:10 +0200

    Remove keywords meta tag
    A summary of which meta tags google uses can be found here
    https://support.google.com/webmasters/answer/79812

--- Kwc/Box/MetaTagsContent/Form.php
+++ Kwc/Box/MetaTagsContent/Form.php
@@ -7,8 +7,5 @@ class Kwc_Box_MetaTagsContent_Form extends Kwc_Abstract_Form
         $this->add(new Kwf_Form_Field_TextArea('description', trlKwf('Description')))
             ->setWidth(400)
             ->setHeight(100);
-        $this->add(new Kwf_Form_Field_TextArea('keywords', trlKwf('Keywords')))
-            ->setWidth(400)
-            ->setHeight(100);
     }
 }
