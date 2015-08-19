0918a0aab707936196de9e9acb72bb51aaa39b9c
Niko Sams <ns@vivid-planet.com>
2015-08-19 11:29:54 +0200

    Add baseUrl

--- Kwf/Util/RobotsTxt.php
+++ Kwf/Util/RobotsTxt.php
@@ -3,12 +3,13 @@ class Kwf_Util_RobotsTxt
 {
     public static function output()
     {
+        $baseUrl = Kwf_Setup::getBaseUrl();
         $contents = "User-agent: *\n".
-            "Disallow: /admin/\n".
-            "Disallow: /kwf/util/kwc/render\n"; //used to load eg. lightbox content async, we don't want getting that indexed
+            "Disallow: $baseUrl/admin/\n".
+            "Disallow: $baseUrl/kwf/util/kwc/render\n"; //used to load eg. lightbox content async, we don't want getting that indexed
 
         $contents .= "Sitemap: http".(isset($_SERVER['HTTPS']) ? 's' : '')."://"
-                        .$_SERVER['HTTP_HOST'].Kwf_Setup::getBaseUrl()."/sitemap.xml\n";
+                        .$_SERVER['HTTP_HOST'].$baseUrl."/sitemap.xml\n";
 
         Kwf_Media_Output::output(array(
             'contents' => $contents,
