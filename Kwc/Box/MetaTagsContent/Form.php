0d83963a4bac0c9bd1176cd484d551095cc061e8
Niko Sams <ns@vivid-planet.com>
2015-08-20 13:02:43 +0200

    Add open graph mage meta tag to MetaTagsContent component
    Probalby not a smoth upgrade but:
    - update script tries to convert content
    - kwf-upgrade script tries to remove openGraph box

--- Kwc/Box/MetaTagsContent/Form.php
+++ Kwc/Box/MetaTagsContent/Form.php
@@ -1,12 +1,14 @@
 <?php
-class Kwc_Box_MetaTagsContent_Form extends Kwc_Abstract_Form
+class Kwc_Box_MetaTagsContent_Form extends Kwc_Abstract_Composite_Form
 {
+    protected $_createFieldsets = false;
     protected function _initFields()
     {
-        parent::_initFields();
         $this->add(new Kwf_Form_Field_TextArea('description', 'META Description')) //no trl
             ->setWidth(400)
             ->setHeight(80)
             ->setHelpText(trlKwf('Optional, but important for SEO. Short description of the page content in about 170 characters. Important for Google, Facebook, etc.'));
+
+        parent::_initFields();
     }
 }
