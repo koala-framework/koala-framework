5d7f8d0840a42535975ed605ce282773b9d0f3a5
Franz Unger <ufx@vivid-planet.com>
2015-08-20 10:21:34 +0200

    Repair Sitemap in a simple way, correct solution is in 3.10

--- Kwf/Component/Sitemap.php
+++ Kwf/Component/Sitemap.php
@@ -3,36 +3,25 @@ class Kwf_Component_Sitemap
 {
     public function outputSitemap(Kwf_Component_Data $page)
     {
-        $xml = new XmlWriter();
-        $xml->openMemory();
-        $xml->startDocument('1.0', 'UTF-8');
-        $xml->startElement('urlset');
-        $xml->writeAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
-        foreach (array_unique($this->_getSitemap($page)) as $url) {
-            $xml->startElement('url');
-            $xml->writeElement('loc', $url);
-            $xml->endElement();
-        }
-        $xml->endElement();
-        $xml->endDocument();
         header('Content-Type: text/xml; charset=utf-8');
-        echo $xml->outputMemory(true);
+        echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+        echo "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
+        foreach ($this->_getSitemap($page) as $url) {
+            echo "<url>\n";
+            echo " <loc>".htmlspecialchars($url)."</loc>\n";
+            echo "</url>\n";
+        }
+        echo "</urlset>\n";
         exit;
     }
 
     private function _getSitemap(Kwf_Component_Data $page)
     {
         $sites = array();
-        if (is_instance_of($page->componentClass, 'Kwc_Mail_Redirect_Component') || // TODO: das gehört natürlich noch gscheit gemacht
-            is_instance_of($page->componentClass, 'Kwc_Advanced_Amazon_Nodes_ProductsDirectory_Component')
-        ) {
-            return $sites;
+        if (substr($page->url, 0, 1) == '/') {
+            $sites[] = $page->getAbsoluteUrl();
         }
-
-        if ($page->url) {
-            $sites[] = 'http://' . Kwf_Registry::get('config')->server->domain . $page->url;
-        }
-        foreach ($page->getChildPseudoPages(array(), array('pseudoPage'=>false)) as $childPage) {
+        foreach ($page->getChildPseudoPages(array('pageGenerator' => true)) as $childPage) {
             $sites = array_merge($sites, $this->_getSitemap($childPage));
         }
         return $sites;
