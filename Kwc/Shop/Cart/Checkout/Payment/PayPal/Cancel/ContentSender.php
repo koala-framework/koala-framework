3598facd22154d7f706d0d664fcda6b3598f1bd5
Alexander Kaufmann <ak@vivid-planet.com>
2015-09-15 13:39:25 +0200

    on cancel set order status always to cart in paypal payment

--- Kwc/Shop/Cart/Checkout/Payment/PayPal/Cancel/ContentSender.php
+++ Kwc/Shop/Cart/Checkout/Payment/PayPal/Cancel/ContentSender.php
@@ -8,7 +8,7 @@ class Kwc_Shop_Cart_Checkout_Payment_PayPal_Cancel_ContentSender extends Kwf_Com
             Kwc_Shop_Cart_Orders::setCartOrderId($session->paypalCartId);
             $order = Kwf_Model_Abstract::getInstance(Kwc_Abstract::getSetting($this->_data->parent->parent->parent->componentClass, 'childModel'))
                 ->getReferencedModel('Order')->getCartOrder();
-            if ($order && $order->status == 'pending') {
+            if ($order) {
                 $order->status = 'cart';
                 $order->save();
             }
