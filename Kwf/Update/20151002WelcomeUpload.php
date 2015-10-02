6ec520fbee90007e3f72ad744e901bc32b9edc7f
Niko Sams <ns@vivid-planet.com>
2015-10-02 10:27:39 +0200

    Fix update script

--- Kwf/Update/20151002WelcomeUpload.php
+++ Kwf/Update/20151002WelcomeUpload.php
@@ -11,8 +11,8 @@ class Kwf_Update_20151002WelcomeUpload extends Kwf_Update
             if ($row['Field'] == 'kwf_upload_id' && substr($row['Type'], 0, 3) == 'int') {
                 $db->query('UPDATE kwf_welcome SET kwf_upload_id=NULL');
                 $db->query('UPDATE kwf_welcome SET login_kwf_upload_id=NULL');
-                $db->query('ALTER TABLE  `kwf_welcome` CHANGE  `kwf_upload_id`  `kwf_upload_id` VARBINARY( 36 ) UNSIGNED NULL DEFAULT NULL ,
-                    CHANGE  `login_kwf_upload_id`  `login_kwf_upload_id` VARBINARY( 36 ) UNSIGNED NULL DEFAULT NULL');
+                $db->query('ALTER TABLE  `kwf_welcome` CHANGE  `kwf_upload_id`  `kwf_upload_id` VARBINARY( 36 ) NULL DEFAULT NULL ,
+                    CHANGE  `login_kwf_upload_id`  `login_kwf_upload_id` VARBINARY( 36 ) NULL DEFAULT NULL');
                 break;
             }
         }
