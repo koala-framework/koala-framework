120baff2d00e7665a833fa22e0762754ba934bb1
Niko Sams <ns@vivid-planet.com>
2015-10-02 11:07:00 +0200

    Add html5shiv for IE<9
    This makes it possible to use html5 tags (finally)

--- commonjs/frontend-core.js
+++ commonjs/frontend-core.js
@@ -1,2 +1,3 @@
 require('kwf/frontend-preview');
 require('kwf/replace-image-dpr');
+document.write('<!--[if lt IE 9]><script type="text/javascript" src="/assets/html5shiv/dist/html5shiv.min.js"><' + '/script><![endif]-->');
