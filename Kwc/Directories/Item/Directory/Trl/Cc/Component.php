1e3cb058e0b83fbebd99ebda67af491bed8d6fb7
Niko Sams <ns@vivid-planet.com>
2015-10-05 13:16:08 +0200

    Fix Trl+CC: Fix Directory, wrong language was used

--- Kwc/Directories/Item/Directory/Trl/Cc/Component.php
+++ Kwc/Directories/Item/Directory/Trl/Cc/Component.php
@@ -1,6 +1,15 @@
 <?php
 class Kwc_Directories_Item_Directory_Trl_Cc_Component extends Kwc_Directories_Item_Directory_Cc_Component
 {
+    public function getItemDirectory()
+    {
+        return self::getChainedByMaster(
+            $this->getData()->chained->getComponent()->getItemDirectory(),
+            $this->getData(),
+            array('ignoreVisible' => true)
+        );
+    }
+
     protected function _getChainedComponent()
     {
         return $this->getData()->chained->chained;
