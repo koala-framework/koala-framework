1580d4014a7cbd4cc681cc721abc985afc0d0fff
Niko Sams <ns@vivid-planet.com>
2015-10-07 14:57:39 +0200

    Fix bug in StaticSelect
    When static select model contains unused data selecting it without parentData (in
    getRecursiveChildComponents) it was used and the componentClass constraint was ignored

---
+++ tests/Kwf/Component/Generator/StaticSelectChildTablePage/Banner2/Child/Component.php
@@ -0,0 +1,13 @@
+<?php
+class Kwf_Component_Generator_StaticSelectChildTablePage_Banner2_Child_Component extends Kwc_Abstract_Composite_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['generators']['page'] = array(
+            'class' => 'Kwf_Component_Generator_Page_Static',
+            'component' => 'Kwf_Component_Generator_StaticSelectChildTablePage_Banner2_Child_Page_Component'
+        );
+        return $ret;
+    }
+}
