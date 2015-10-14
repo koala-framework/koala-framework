df5744b335a499d9bb55cd0bf1ab80dc8fc0cf03
Niko Sams <ns@vivid-planet.com>
2015-10-14 18:48:50 +0200

    Assets: Add common decorator implementation that can be re-used

--- Kwf/Assets/CommonJs/JQueryPluginDecoratorDependency.php
+++ Kwf/Assets/CommonJs/JQueryPluginDecoratorDependency.php
@@ -1,13 +1,6 @@
 <?php
-class Kwf_Assets_CommonJs_JQueryPluginDecoratorDependency extends Kwf_Assets_Dependency_Abstract
+class Kwf_Assets_CommonJs_JQueryPluginDecoratorDependency extends Kwf_Assets_Dependency_Decorator_Abstract
 {
-    private $_dep;
-    public function __construct(Kwf_Assets_Dependency_Abstract $dep)
-    {
-        $this->_dep = $dep;
-        parent::__construct();
-    }
-
     private function _getPrependCode()
     {
         return "var jQuery = require('jQuery');\n";
@@ -20,46 +13,11 @@ class Kwf_Assets_CommonJs_JQueryPluginDecoratorDependency extends Kwf_Assets_Dep
         return $ret;
     }
 
-    public function usesLanguage()
-    {
-        return $this->_dep->usesLanguage();
-    }
-
-    public function getContentsSource()
-    {
-        return $this->_dep->getContentsSource();
-    }
-
-    public function getContentsSourceString()
-    {
-        return $this->_dep->getContentsSourceString();
-    }
-
     public function getContentsPacked($language)
     {
         $c = $this->_dep->getContentsPacked($language);
         $ret = Kwf_SourceMaps_SourceMap::createEmptyMap($this->_getPrependCode());
         $ret->concat($c);
         return $ret;
-    }
-
-    public function getMimeType()
-    {
-        return $this->_dep->getMimeType();
-    }
-
-    public function getMTime()
-    {
-        return $this->_dep->getMTime();
-    }
-
-    public function warmupCaches()
-    {
-        return $this->_dep->warmupCaches();
-    }
-
-    public function __toString()
-    {
-        return 'JQueryPluginDecorator('.$this->_dep->__toString().')';
     }
 }
