b251ac7ea823d604c95e08ee6253f6c6cfa1001e
Alexander Kaufmann <ak@vivid-planet.com>
2015-10-22 15:33:10 +0200

    mail trl component supports now mail sending ;)
    mail sending will be called from child mail abstract which is a mail,
    that get its content from parent

---
+++ Kwc/Mail/Trl/Mail/Component.php
@@ -0,0 +1,48 @@
+<?php
+class Kwc_Mail_Trl_Mail_Component extends Kwc_Mail_Abstract_Component
+{
+    public static function getSettings($masterComponentClass)
+    {
+        $ret = parent::getSettings();
+        $ret['mailHtmlStyles'] = Kwc_Abstract::getSetting($masterComponentClass, 'mailHtmlStyles');
+        $ret['plugins'] = Kwc_Abstract::getSetting($masterComponentClass, 'plugins');
+
+        $ret['recipientSources'] = Kwc_Abstract::getSetting($masterComponentClass, 'recipientSources');
+
+        $ret['fromName'] = Kwc_Abstract::getSetting($masterComponentClass, 'fromName');
+        $ret['fromEmail'] = Kwc_Abstract::getSetting($masterComponentClass, 'fromEmail');
+        $ret['replyEmail'] = Kwc_Abstract::getSetting($masterComponentClass, 'replyEmail');
+        $ret['bcc'] = Kwc_Abstract::getSetting($masterComponentClass, 'bcc');
+        $ret['returnPath'] = Kwc_Abstract::getSetting($masterComponentClass, 'returnPath');
+        $ret['subject'] = Kwc_Abstract::getSetting($masterComponentClass, 'subject');
+        $ret['attachImages'] = Kwc_Abstract::getSetting($masterComponentClass, 'attachImages');
+        $ret['trackViews'] = Kwc_Abstract::getSetting($masterComponentClass, 'trackViews');
+        $ret['docType'] = Kwc_Abstract::getSetting($masterComponentClass, 'docType');
+
+        $ret['flags'] = Kwc_Abstract::getSetting($masterComponentClass, 'flags');
+        return $ret;
+    }
+
+    public function getTemplateVars(Kwf_Component_Renderer_Abstract $renderer = null)
+    {
+        $ret = parent::getTemplateVars($renderer);
+        $ret['parent'] = $this->getData()->parent;
+        return $ret;
+    }
+
+    public function getRecipientSources()
+    {
+        return $this->getData()->parent->getComponent()->getRecipientSources();
+    }
+
+    public function getPlaceholders(Kwc_Mail_Recipient_Interface $recipient = null)
+    {
+        return $this->getData()->parent->getComponent()->getPlaceholders($recipient);
+    }
+
+    protected function _getSubject()
+    {
+        return $this->getData()->parent->getComponent()->getRow()->subject;
+    }
+}
+
