1f9b1838e8812bac21175e5e7190818b11b75de9
Alexander Kaufmann <ak@vivid-planet.com>
2015-10-27 15:11:39 +0100

    user union row calls write log from its sourceModel

--- Kwf/User/UnionRow.php
+++ Kwf/User/UnionRow.php
@@ -1,4 +1,11 @@
 <?php
 class Kwf_User_UnionRow extends Kwf_Model_Union_Row
 {
+    public function writeLog($messageType)
+    {
+        $row = $this->getSourceRow();
+        if (method_exists($row, 'writeLog')) {
+            $row->writeLog($messageType);
+        }
+    }
 }
