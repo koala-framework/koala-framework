204362ab2aed8b45eb969c761e7538070ba5f6c7
Franz Unger <ufx@vivid-planet.com>
2015-11-26 10:22:27 +0100

    Calculate contentWidth only when image size is set to contentWidth

--- Kwc/Abstract/Image/ImageFile.php
+++ Kwc/Abstract/Image/ImageFile.php
@@ -14,7 +14,20 @@ class Kwc_Abstract_Image_ImageFile extends Kwf_Form_Field_File
         $component = Kwf_Component_Data_Root::getInstance()->getComponentByDbId($row->component_id, array('ignoreVisible' => true));
         if ($component) { //component can be non-existent if it's in a not selected card
             if (is_instance_of($component->componentClass, 'Kwc_Abstract_Image_Component')) {
-                $contentWidth = $component->getComponent()->getMaxContentWidth();
+                $contentWidth = null;
+                $usesContentWidth = false;
+                foreach (Kwc_Abstract::getSetting($component->componentClass, 'dimensions') as $dim) {
+                    if (isset($dim['width'])) {
+                        if ($dim['width'] == Kwc_Abstract_Image_Component::CONTENT_WIDTH) {
+                            $usesContentWidth = true;
+                        } else if ($dim['width'] > $contentWidth) {
+                            $contentWidth = $dim['width'];
+                        }
+                    }
+                }
+                if ($usesContentWidth) {
+                    $contentWidth = $component->getComponent()->getMaxContentWidth();
+                }
             } else {
                 $contentWidth = $component->getComponent()->getContentWidth();
             }
