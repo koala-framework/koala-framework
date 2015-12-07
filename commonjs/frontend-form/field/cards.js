60b2059d3b0f3054dfaa75a99fde0d73729bb0e5
Franz Unger <ufx@vivid-planet.com>
2015-12-07 08:19:45 +0100

    Improve field/cards.js

--- commonjs/frontend-form/field/cards.js
+++ commonjs/frontend-form/field/cards.js
@@ -1,3 +1,4 @@
+var $ = require('jQuery');
 var fieldRegistry = require('kwf/frontend-form/field-registry');
 var Field = require('kwf/frontend-form/field/field');
 var kwfExtend = require('kwf/extend');
@@ -6,7 +7,7 @@ var Cards = kwfExtend(Field, {
     initField: function() {
         var config = this.form.getFieldConfig(this.getFieldName());
         var combobox = this.form.findField(config.combobox);
-        $(combobox.el.find('input.submit')).remove(); //remove non-js fallback
+        combobox.el.find('input.submit').remove(); //remove non-js fallback
         combobox.el.on('change', (function() {
             this.el.find('.kwfUp-kwfFormContainerCard .kwfFormCard').addClass('inactive');
             this.el.find('.kwfUp-kwfFormContainerCard.kwfUp-'+combobox.getValue()+' .kwfFormCard').removeClass('inactive');
