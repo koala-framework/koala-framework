06452fa29c1e3a8f07a9d0d872a663b8b4c90ad5
Franz Unger <ufx@vivid-planet.com>
2015-12-09 17:23:11 +0100

    Use empty removeList for Filter_Ascii

--- Kwf/Filter/Ascii.php
+++ Kwf/Filter/Ascii.php
@@ -8,6 +8,7 @@ class Kwf_Filter_Ascii implements Zend_Filter_Interface
 {
     public function filter($value)
     {
+        URLify::$remove_list = array();
         return URLify::filter($value, 60, 'de');
     }
 }
