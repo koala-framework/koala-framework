a265e098b80d03bee30b3c52db0df2e09264e5e9
Niko Sams <ns@vivid-planet.com>
2015-12-11 09:34:18 +0100

    fix uniquePrefix, - has to be added

--- Kwf/Assets/Filter/Css/UniquePrefix.php
+++ Kwf/Assets/Filter/Css/UniquePrefix.php
@@ -3,7 +3,10 @@ class Kwf_Assets_Filter_Css_UniquePrefix extends Kwf_Assets_Filter_Css_SelectorR
 {
     public function __construct($prefix = null)
     {
-        $prefix = $prefix ? $prefix : Kwf_Config::getValue('application.uniquePrefix');
+        if (!$prefix) {
+            $prefix = Kwf_Config::getValue('application.uniquePrefix');
+            if ($prefix) $prefix .= '-';
+        }
         parent::__construct(array(
             'kwfUp-' => $prefix
         ));
