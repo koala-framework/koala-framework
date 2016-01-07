678c16222a4587dc032ec5493976d22d69f4a005
Niko Sams <ns@vivid-planet.com>
2016-01-07 09:25:39 +0100

    Clone object before modifying it
    fixes replacements done multiple times

--- Kwf/Assets/Dependency/Decorator/StringReplace.php
+++ Kwf/Assets/Dependency/Decorator/StringReplace.php
@@ -25,6 +25,7 @@ class Kwf_Assets_Dependency_Decorator_StringReplace extends Kwf_Assets_Dependenc
     public function getContentsPacked($language)
     {
         $ret = $this->_dep->getContentsPacked($language);
+        $ret = clone $ret;
         foreach ($this->_getReplacements() as $k=>$i) {
             $ret->stringReplace($k, $i);
         }
