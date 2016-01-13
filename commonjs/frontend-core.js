bc97060487cfb3977ad81d10ce6ea2810d8dea2a
Niko Sams <ns@vivid-planet.com>
2016-01-13 18:05:45 +0100

    Don't load underscore in non-defer javascript
    instead manually implement polyfill

--- commonjs/frontend-core.js
+++ commonjs/frontend-core.js
@@ -1,8 +1,27 @@
-var _ = require('underscore');
-
-if (typeof Array.prototype.forEach != 'function') {
-    Array.prototype.forEach = function (fn, scope) {
-        _.forEach(this, fn, scope);
+//source: https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+if (!Array.prototype.forEach) {
+    Array.prototype.forEach = function(callback, thisArg) {
+        var T, k;
+        if (this == null) {
+            throw new TypeError(' this is null or not defined');
+        }
+        var O = Object(this);
+        var len = O.length >>> 0;
+        if (typeof callback !== "function") {
+            throw new TypeError(callback + ' is not a function');
+        }
+        if (arguments.length > 1) {
+            T = thisArg;
+        }
+        k = 0;
+        while (k < len) {
+            var kValue;
+            if (k in O) {
+                kValue = O[k];
+                callback.call(T, kValue, k, O);
+            }
+            k++;
+        }
     };
 }
 
