dcf98c27ae9fefe9b100993874fa767656b67dee
Niko Sams <ns@vivid-planet.com>
2016-01-14 14:55:05 +0100

    Improve commonjs assets for backbone applications in frontend

--- Kwf/Assets/Provider/KwfCommonJs.php
+++ Kwf/Assets/Provider/KwfCommonJs.php
@@ -9,6 +9,12 @@ class Kwf_Assets_Provider_KwfCommonJs extends Kwf_Assets_Provider_Abstract
                 $ret = new Kwf_Assets_Dependency_File_Js('kwf/commonjs/'.$dependencyName.'.js');
                 return $ret;
             }
+        } else if (substr($dependencyName, 0, 13) == 'web/commonjs/') {
+            $dependencyName = substr($dependencyName, 13);
+            if (file_exists('./commonjs/'.$dependencyName.'.js')) {
+                $ret = new Kwf_Assets_Dependency_File_Js('web/commonjs/'.$dependencyName.'.js');
+                return $ret;
+            }
         }
     }
 }
