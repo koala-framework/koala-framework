dbe2741577fc20d090772229a9ccd3403a2600f7
Niko Sams <ns@vivid-planet.com>
2016-01-14 15:19:50 +0100

    Allow usage of commonjs dependencies anywhere in web

--- Kwf/Assets/Provider/KwfCommonJs.php
+++ Kwf/Assets/Provider/KwfCommonJs.php
@@ -9,10 +9,10 @@ class Kwf_Assets_Provider_KwfCommonJs extends Kwf_Assets_Provider_Abstract
                 $ret = new Kwf_Assets_Dependency_File_Js('kwf/commonjs/'.$dependencyName.'.js');
                 return $ret;
             }
-        } else if (substr($dependencyName, 0, 13) == 'web/commonjs/') {
-            $dependencyName = substr($dependencyName, 13);
-            if (file_exists('./commonjs/'.$dependencyName.'.js')) {
-                $ret = new Kwf_Assets_Dependency_File_Js('web/commonjs/'.$dependencyName.'.js');
+        } else if (substr($dependencyName, 0, 4) == 'web/') {
+            $dependencyName = substr($dependencyName, 4);
+            if (file_exists('./'.$dependencyName.'.js')) {
+                $ret = new Kwf_Assets_Dependency_File_Js('web/'.$dependencyName.'.js');
                 return $ret;
             }
         }
