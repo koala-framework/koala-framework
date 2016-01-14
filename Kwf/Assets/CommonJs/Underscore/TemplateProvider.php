4e6ccac7a6727533d6479344060b2bbddb937ab9
Alexander Kaufmann <ak@vivid-planet.com>
2016-01-14 17:12:45 +0100

    add same named scss dependency for underscore.tpl provider

--- Kwf/Assets/CommonJs/Underscore/TemplateProvider.php
+++ Kwf/Assets/CommonJs/Underscore/TemplateProvider.php
@@ -5,9 +5,19 @@ class Kwf_Assets_CommonJs_Underscore_TemplateProvider extends Kwf_Assets_Provide
     {
         if (substr($dependencyName, -15) == '.underscore.tpl') {
             $ret = new Kwf_Assets_CommonJs_Underscore_TemplateDependency($dependencyName);
-            $ret->addDependency(Kwf_Assets_Dependency_Abstract::DEPENDENCY_TYPE_COMMONJS, $this->_providerList->findDependency('underscore'), 'underscore');
-            return $ret;
+            $ret->addDependency(
+                Kwf_Assets_Dependency_Abstract::DEPENDENCY_TYPE_COMMONJS,
+                $this->_providerList->findDependency('underscore'),
+                'underscore'
+            );
 
+            if (file_exists(substr($ret->getAbsoluteFileName(), 0, -15) . '.scss')) {
+                $ret->addDependency(
+                    Kwf_Assets_Dependency_Abstract::DEPENDENCY_TYPE_REQUIRES,
+                    new Kwf_Assets_Dependency_File_Scss(substr($dependencyName, 0, -15) . '.scss')
+                );
+            }
+            return $ret;
         }
         return null;
     }
