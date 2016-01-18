852d49483bc51d25e61ad68677101b5d2911c1af
Niko Sams <ns@vivid-planet.com>
2016-01-18 15:49:16 +0100

    Add tag to update scripts

--- Kwf/Update/20150929PagesMeta.sql
+++ Kwf/Update/20150929PagesMeta.sql
@@ -1,3 +1,4 @@
+#tags: kwc
 ALTER TABLE  `kwf_pages_meta` ADD  `sitemap_changefreq` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER  `meta_noindex`;
 ALTER TABLE  `kwf_pages_meta` ADD  `sitemap_priority` DECIMAL(2, 1) NOT NULL AFTER  `meta_noindex`;
 UPDATE kwf_pages_meta SET sitemap_changefreq = 'weekly';
