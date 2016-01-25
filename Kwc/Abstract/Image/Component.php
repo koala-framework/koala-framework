baa60c8e4708d6b9e73dce3b3760e81878dc5d15
Niko Sams <ns@vivid-planet.com>
2016-01-25 17:52:08 +0100

    Default value to 80

--- Kwc/Abstract/Image/Component.php
+++ Kwc/Abstract/Image/Component.php
@@ -70,7 +70,7 @@ class Kwc_Abstract_Image_Component extends Kwc_Abstract_Composite_Component
         $ret['defineWidth'] = false;
         $ret['maxWidthImageWidth'] = true;
         $ret['inlineTags'] = false;
-        $ret['imageCompressionQuality'] = 75;
+        $ret['imageCompressionQuality'] = 80;
         return $ret;
     }
 
