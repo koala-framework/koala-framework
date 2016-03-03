46bb5c55b5859c43947f106d34e5a3bb175007bd
Alexander Kaufmann <ak@vivid-planet.com>
2016-03-03 11:23:48 +0100

    underscore template dependency supports now unique prefix

--- Kwf/Assets/CommonJs/Underscore/TemplateDependency.php
+++ Kwf/Assets/CommonJs/Underscore/TemplateDependency.php
@@ -8,6 +8,18 @@ class Kwf_Assets_CommonJs_Underscore_TemplateDependency extends Kwf_Assets_Depen
         $contents = str_replace("'", "\\'", $contents);
         $contents = "var _ = require('underscore');\n".
                     "module.exports = _.template('".$contents."');\n";
+
+        $replacements = array();
+        if (strpos($contents, 'kwfUp-') !== false) {
+            if (Kwf_Config::getValue('application.uniquePrefix')) {
+                $replacements['kwfUp-'] = Kwf_Config::getValue('application.uniquePrefix').'-';
+            } else {
+                $replacements['kwfUp-'] = '';
+            }
+        }
+        foreach ($replacements as $search=>$replace) {
+            $contents = str_replace($search, $replace, $contents);
+        }
         return Kwf_SourceMaps_SourceMap::createEmptyMap($contents);
     }
 }
