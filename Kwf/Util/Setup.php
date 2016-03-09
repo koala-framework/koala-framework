6e1c323ead669a5015d46e17af2f25d0092cd005
Niko Sams <ns@vivid-planet.com>
2016-03-09 11:19:10 +0100

    Remove deprecated iconv_set_encoding call
    causes errors with php 5.6

--- Kwf/Util/Setup.php
+++ Kwf/Util/Setup.php
@@ -153,7 +153,6 @@ class Kwf_Util_Setup
         $ret .= "error_reporting(E_ALL & ~E_STRICT);\n";
         $ret .= "date_default_timezone_set('Europe/Berlin');\n";
         $ret .= "mb_internal_encoding('UTF-8');\n";
-        $ret .= "iconv_set_encoding('internal_encoding', 'utf-8');\n";
         $ret .= "set_error_handler(array('Kwf_Debug', 'handleError'), E_ALL & ~E_STRICT);\n";
         $ret .= "set_exception_handler(array('Kwf_Debug', 'handleException'));\n";
         $ret .= "umask(000); //nicht 002 weil wwwrun und kwcms in unterschiedlichen gruppen\n";
