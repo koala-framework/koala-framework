fef4c3ab1c0f381bcc7acc92f1aff67332dc1f28
Niko Sams <ns@vivid-planet.com>
2016-04-07 18:15:34 +0200

    Don't rebuild all assets when doing composer update

--- Kwf/Assets/ContentsCache.php
+++ Kwf/Assets/ContentsCache.php
@@ -17,7 +17,7 @@ class Kwf_Assets_ContentsCache
     {
         $this->_cache[$cacheId] = $map;
 
-        $cacheFile = 'cache/assetdeps/'.md5($cacheId).'v1';
+        $cacheFile = 'cache/assetdeps/'.md5($cacheId).'v2';
 
         $data = $map->getMapContentsData(true); //this will trigger _generateMappings
         file_put_contents($cacheFile, serialize($map));
@@ -27,11 +27,33 @@ class Kwf_Assets_ContentsCache
             foreach ($data->{'_x_org_koala-framework_masterFiles'} as $f) {
                 $masterFiles[] = array(
                     'file' => $f,
-                    'md5' => file_exists($f) ? md5_file($f) : false
+                    'hash' => $this->_hashFile($f)
                 );
             }
         }
         file_put_contents($cacheFile.'.masterFiles', serialize($masterFiles));
+    }
+
+    private function _hashFile($f)
+    {
+        if (isset($this->_filemd5Cache[$f])) {
+            return $this->_filemd5Cache[$f];
+        }
+        $ret = null;
+        if (!file_exists($f)) {
+            $ret = false;
+        } else if (substr($f, -13) == '/package.json') {
+            //package.json changes on install even though installed version didn't change (_shasum field)
+            $c = json_decode(file_get_contents($f), true);
+            if (isset($c['_resolved'])) {
+                $ret = $c['_resolved'];
+            }
+        }
+        if (is_null($ret)) {
+            $ret = md5_file($f);
+        }
+        $this->_filemd5Cache[$f] = $ret;
+        return $ret;
     }
 
     public function load($cacheId)
@@ -40,21 +62,14 @@ class Kwf_Assets_ContentsCache
             return $this->_cache[$cacheId];
         }
 
-        $cacheFile = 'cache/assetdeps/'.md5($cacheId).'v1';
+        $cacheFile = 'cache/assetdeps/'.md5($cacheId).'v2';
         if (!file_exists($cacheFile) || !file_exists($cacheFile.'.masterFiles')) {
             return false;
         } else {
             $masterFiles = unserialize(file_get_contents($cacheFile.'.masterFiles'));
             $mtime = filemtime($cacheFile);
             foreach ($masterFiles as $i) {
-                if (!isset($this->_filemd5Cache[$i['file']])) {
-                    if (!file_exists($i['file'])) {
-                        $this->_filemd5Cache[$i['file']] = false;
-                    } else {
-                        $this->_filemd5Cache[$i['file']] = md5_file($i['file']);
-                    }
-                }
-                if ($i['md5'] != $this->_filemd5Cache[$i['file']]) {
+                if ($i['hash'] != $this->_hashFile($i['file'])) {
                     return false;
                 }
             }
