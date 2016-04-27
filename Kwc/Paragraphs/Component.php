d8650d6dec2137705030a983bf7051cd0e5247fe
Niko Sams <ns@vivid-planet.com>
2016-04-27 14:45:37 +0200

    Fix substr usage
    testing, know

--- Kwc/Paragraphs/Component.php
+++ Kwc/Paragraphs/Component.php
@@ -61,7 +61,7 @@ class Kwc_Paragraphs_Component extends Kwc_Abstract
             if ($this->_getSetting('useMobileBreakpoints') && $row->device_visible) $cssClass .= ' ' . $this->_getBemClass($row->device_visible);
             $cssClass .= ' '.$this->_getBemClass(
                     'outer'.ucfirst($paragraph->row->component),
-                    'outer'.ucfirst(substr(Kwf_Component_Abstract::formatRootElementClass($paragraph->componentClass, ''), 0, 6))
+                    'outer'.ucfirst(substr(Kwf_Component_Abstract::formatRootElementClass($paragraph->componentClass, ''), 6))
                 );
             $preHtml = '';
             $postHtml = '';
