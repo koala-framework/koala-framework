d6fd12d0b8d3039b8bcfa319c653763e1659cd19
Franz Unger <ufx@vivid-planet.com>
2016-05-04 09:24:08 +0200

    Make Cookie Opt Banner Text customizable

--- Kwc/Statistics/Opt/Component.defer.js
+++ Kwc/Statistics/Opt/Component.defer.js
@@ -1,16 +1,15 @@
 var onReady = require('kwf/on-ready');
 var cookieOpt = require('kwf/cookie-opt');
-var t = require('kwf/trl');
 
-onReady.onRender('.kwcClass', function(el) {
+onReady.onRender('.kwcClass', function(el, config) {
     var checkbox = el.find('input[type="checkbox"]');
     function update() {
         if (cookieOpt.getOpt() == 'in') {
             checkbox[0].checked = true;
-            var label = t.trl('Cookies are set when visiting this webpage. Click to deactivate cookies.');
+            var label = config.textOptIn;
         } else {
             checkbox[0].checked = false;
-            var label = t.trl('No cookies are set when visiting this webpage. Click to activate cookies.');
+            var label = config.textOptOut;
         }
         el.find('label').html(label);
     }
--- Kwc/Statistics/Opt/Component.php
+++ Kwc/Statistics/Opt/Component.php
@@ -1,11 +1,30 @@
 <?php
-class Kwc_Statistics_Opt_Component extends Kwc_Abstract
+class Kwc_Statistics_Opt_Component extends Kwc_Abstract_Composite_Component
 {
     public static function getSettings()
     {
         $ret = parent::getSettings();
         $ret['rootElementClass'] = 'kwfUp-webStandard';
         $ret['componentName'] = trlKwfStatic('Cookie Opt In / Opt Out');
+        $ret['ownModel'] = 'Kwf_Component_FieldModel';
+        return $ret;
+    }
+
+    public function getTemplateVars(Kwf_Component_Renderer_Abstract $renderer = null)
+    {
+        $ret = parent::getTemplateVars($renderer);
+        $config = array();
+        $config['textOptIn'] = $this->getRow()->text_opt_in;
+        if (!$config['textOptIn']) {
+            $config['textOptIn'] = $this->getData()->trl('Cookies are set when visiting this webpage. Click to deactivate cookies.');
+        }
+        $config['textOptOut'] = $this->getRow()->text_opt_out;
+        if (!$config['textOptOut']) {
+            $config['textOptOut'] = $this->getData()->trl('No cookies are set when visiting this webpage. Click to activate cookies.');
+        }
+        $config['textOptIn'] = nl2br($config['textOptIn']);
+        $config['textOptOut'] = nl2br($config['textOptOut']);
+        $ret['config'] = $config;
         return $ret;
     }
 }
