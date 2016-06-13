1443330040ce87ad272e96efbe9484a84ba264a2
Niko Sams <ns@vivid-planet.com>
2016-06-13 12:56:44 +0200

    Add missing dependency for lightbox

--- Kwc/Advanced/LightboxParagraphs/Component.php
+++ Kwc/Advanced/LightboxParagraphs/Component.php
@@ -6,6 +6,7 @@ class Kwc_Advanced_LightboxParagraphs_Component extends Kwc_Paragraphs_Component
         $ret = parent::getSettings();
         $ret['componentName'] = trlKwfStatic('Paragraphs in Lightbox');
         $ret['contentSender'] = 'Kwf_Component_Abstract_ContentSender_Lightbox';
+        $ret['assetsDefer']['dep'][] = 'KwfLightbox';
 
         $ret['contentWidth'] = 600;
         return $ret;
