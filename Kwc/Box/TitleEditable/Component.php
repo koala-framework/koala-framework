0ff22db7ac4352cbcf8fd510d577b7163afac50f
Franz Unger <ufx@vivid-planet.com>
2016-07-18 09:18:31 +0200

    Add _getSubrootTitle to Box_TitleEditable_Component

--- Kwc/Box/TitleEditable/Component.php
+++ Kwc/Box/TitleEditable/Component.php
@@ -14,21 +14,29 @@ class Kwc_Box_TitleEditable_Component extends Kwc_Box_Title_Component
         if (trim($this->_getRow()->title)) return $this->_getRow()->title;
 
         //if no title is configured get from next subroot/root
+        $title = $this->_getSubrootTitle();
+        if ($title) {
+            $ret = $this->getData()->getTitle(); //append own title
+            if ($ret) $ret .= ' - ';
+            return $ret.$title;
+        }
+
+        return parent::_getTitle();
+    }
+
+    protected function _getSubrootTitle()
+    {
         $c = $this->getData()->parent;
         while($c) {
             if (($c->inherits && Kwc_Abstract::getFlag($c->componentClass, 'subroot')) || $c->componentId == 'root') {
                 $title = $c->getChildComponent(array('id'=>'-'.$this->getData()->id, 'componentClass'=>$this->getData()->componentClass));
                 if ($title) {
                     $title = $title->getComponent()->_getRow()->title;
-                }
-                if ($title) {
-                    $ret = $this->getData()->getTitle(); //append own title
-                    if ($ret) $ret .= ' - ';
-                    return $ret.$title;
+                    if ($title) return $title;
                 }
             }
             $c = $c->parent;
         }
-        return parent::_getTitle();
+        return null;
     }
 }
