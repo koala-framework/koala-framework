595a4b6c12643f80f6e2ba89dfbe35e5393eff03
Niko Sams <ns@vivid-planet.com>
2016-07-25 17:50:28 +0200

    Add possibility to inehrit master template including styles
    - add renderer.getComponentMasterTemplate for inheriting in twig
    - add masterExtends setting for inheriting css

--- Kwf/Component/Renderer/Twig/TemplateLocator.php
+++ Kwf/Component/Renderer/Twig/TemplateLocator.php
@@ -1,49 +1,64 @@
 <?php
 class Kwf_Component_Renderer_Twig_TemplateLocator
 {
-    public static function getComponentTemplate($componentClass)
+    public static function getComponentTemplate($componentClass, $type = 'Component')
     {
-        $cacheId = 'twig-cmp-'.$componentClass;
+        $cacheId = 'twig-cmp-'.$componentClass.'-'.$type;
         $ret = Kwf_Cache_SimpleStatic::fetch($cacheId);
         if ($ret !== false) return $ret;
 
-        static $namespaces;
-        if (!isset($namespaces)) {
-            $namespaces = include VENDOR_PATH.'/composer/autoload_namespaces.php';
-        }
-
-        $pos = strpos($componentClass, '_');
-        $ns1 = substr($componentClass, 0, $pos+1);
-
-        $pos = strpos($componentClass, '_', $pos+1);
-        if ($pos !== false) {
-            $ns2 = substr($componentClass, 0, $pos+1);
-        } else {
-            $ns2 = $componentClass;
-        }
-
         $file = null;
+        if ($type == 'Master'
+            && Kwc_Abstract::hasSetting($componentClass, 'masterTemplate')
+            && Kwc_Abstract::getSetting($componentClass, 'masterTemplate')
+        ) {
+            $relativeToIncludePath = Kwc_Abstract::getSetting($componentClass, 'masterTemplate');
+        } else {
+            static $namespaces;
+            if (!isset($namespaces)) {
+                $namespaces = include VENDOR_PATH.'/composer/autoload_namespaces.php';
+            }
 
-        $dirs = false;
-        if (isset($namespaces[$ns2])) {
-            $dirs = $namespaces[$ns2];
-        } else if (isset($namespaces[$ns1])) {
-            $dirs = $namespaces[$ns1];
-        }
-        if ($dirs !== false) {
-            if (count($dirs) == 1) {
-                $file = $dirs[0].'/'.str_replace('_', '/', $componentClass).'.twig';
+            $pos = strpos($componentClass, '_');
+            $ns1 = substr($componentClass, 0, $pos+1);
+
+            $pos = strpos($componentClass, '_', $pos+1);
+            if ($pos !== false) {
+                $ns2 = substr($componentClass, 0, $pos+1);
             } else {
-                foreach ($dirs as $dir) {
-                    if (file_exists($dir.'/'.str_replace('_', '/', $componentClass).'.twig')) {
-                        $dir = rtrim($dir, '/');
-                        if (VENDOR_PATH == '../vendor') { //hack for tests. proper solution would be not to change cwd into /tests
-                            if ($dir == KWF_PATH) {
-                                $dir = '..';
+                $ns2 = $componentClass;
+            }
+
+            $relativeToIncludePath = $componentClass;
+            if (substr($relativeToIncludePath, -10) == '_Component') {
+                $relativeToIncludePath = substr($relativeToIncludePath, 0, -10);
+            }
+            $relativeToIncludePath .= '_'.$type;
+            $relativeToIncludePath = str_replace('_', '/', $relativeToIncludePath).'.twig';
+
+
+            $dirs = false;
+            if (isset($namespaces[$ns2])) {
+                $dirs = $namespaces[$ns2];
+            } else if (isset($namespaces[$ns1])) {
+                $dirs = $namespaces[$ns1];
+            }
+
+            if ($dirs !== false) {
+                if (count($dirs) == 1) {
+                    $file = $dirs[0].'/'.$relativeToIncludePath;
+                } else {
+                    foreach ($dirs as $dir) {
+                        if (file_exists($dir.'/'.$relativeToIncludePath)) {
+                            $dir = rtrim($dir, '/');
+                            if (VENDOR_PATH == '../vendor') { //hack for tests. proper solution would be not to change cwd into /tests
+                                if ($dir == KWF_PATH) {
+                                    $dir = '..';
+                                }
                             }
+                            $file = $dir.'/'.$relativeToIncludePath;
+                            break;
                         }
-                        $file = $dir.'/'.str_replace('_', '/', $componentClass).'.twig';
-                        break;
                     }
                 }
             }
@@ -54,14 +69,14 @@ class Kwf_Component_Renderer_Twig_TemplateLocator
         } else {
             $ret = null;
             foreach (explode(PATH_SEPARATOR, get_include_path()) as $ip) {
-                $file = $ip.'/'.str_replace('_', '/', $componentClass).'.twig';
+                $file = $ip.'/'.$relativeToIncludePath;
                 if (file_exists(getcwd().'/'.$file)) {
                     $ret = getcwd().'/'.$file;
                     break;
                 }
             }
             if (!$ret) {
-                throw new Kwf_Exception("Can't find template $componentClass");
+                throw new Kwf_Exception("Can't find $type template for $componentClass $relativeToIncludePath");
             }
         }
 
