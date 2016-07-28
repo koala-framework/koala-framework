08c4c170538daeb01979409f9668c75c451442d0
Franz Unger <ufx@vivid-planet.com>
2016-07-28 09:53:11 +0200

    Make TitleEditable component more flexible
    to be used as masterAsChild

--- Kwc/Box/TitleEditable/Events.php
+++ Kwc/Box/TitleEditable/Events.php
@@ -4,7 +4,9 @@ class Kwc_Box_TitleEditable_Events extends Kwc_Box_Title_Events
     protected function _onOwnRowUpdate(Kwf_Component_Data $c, Kwf_Events_Event_Row_Abstract $event)
     {
         parent::_onOwnRowUpdate($c, $event);
-        if (Kwc_Abstract::getFlag($c->parent->componentClass, 'subroot') || $c->parent instanceof Kwf_Component_Data_Root) {
+        $page = $c;
+        while ($page && !$page->inherits) $page = $page->parent;
+        if (Kwc_Abstract::getFlag($page->componentClass, 'subroot') || $page instanceof Kwf_Component_Data_Root) {
             $this->fireEvent(new Kwf_Component_Event_Component_RecursiveContentChanged(
                 $this->_class, $c
             ));
