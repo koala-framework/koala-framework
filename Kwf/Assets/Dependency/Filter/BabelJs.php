1a013d0e8cf3946d9a15ece9fbd70181ff4c72ec
Alexander Kaufmann <ak@vivid-planet.com>
2016-08-22 13:45:02 +0200

    removed static functions from Assets Dependency Filter BabelJs
    also made it easier to override nodePath and arguments for babel

--- Kwf/Assets/Dependency/Filter/BabelJs.php
+++ Kwf/Assets/Dependency/Filter/BabelJs.php
@@ -1,19 +1,24 @@
 <?php
 class Kwf_Assets_Dependency_Filter_BabelJs
 {
-    public static function build($sourceFile)
+    private $_sourceFile;
+
+    public function __construct($sourceFile)
+    {
+        $this->_sourceFile = $sourceFile;
+    }
+
+    public function build()
     {
         $babel = getcwd()."/".VENDOR_PATH."/bin/node ".getcwd().'/node_modules/babel-cli/bin/babel.js';
         $cmd = "$babel ";
-        $cmd .= "--minified ";
-        $cmd .= "--no-babelrc ";
-        $cmd .= "--source-maps inline ";
-        $cmd .= "--presets es2015 ";
-        $cmd .= "--plugins transform-runtime ";
-        $cmd .= escapeshellarg($sourceFile);
+        $arguments = implode(' ', array_values($this->_getArguments()));
+        $cmd .= "{$arguments} ";
+        $cmd .= escapeshellarg($this->_sourceFile);
         $cmd .= " 2>&1";
         $out = array();
-        putenv("NODE_PATH=".getcwd()."/node_modules".PATH_SEPARATOR.getcwd()."/");
+        $nodePath = implode(PATH_SEPARATOR, $this->_getNodePath());
+        putenv("NODE_PATH={$nodePath}");
         exec($cmd, $out, $retVal);
         putenv("NODE_PATH=");
 
@@ -22,5 +27,24 @@ class Kwf_Assets_Dependency_Filter_BabelJs
         }
         $contents = implode("\n", $out);
         return Kwf_SourceMaps_SourceMap::createFromInline($contents);
+    }
+
+    protected function _getArguments()
+    {
+        return array(
+            'minified' => '--minified',
+            'noBabelrc' => '--no-babelrc',
+            'sourceMaps' => '--source-maps inline',
+            'presets' => '--presets es2015',
+            'plugins' => '--plugins transform-runtime'
+        );
+    }
+
+    protected function _getNodePath()
+    {
+        return array(
+            getcwd()."/node_modules",
+            getcwd()."/"
+        );
     }
 }
