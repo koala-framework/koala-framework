86cd7e096726dc9843f3485a26d661c74fedb107
Niko Sams <ns@vivid-planet.com>
2016-09-13 13:23:28 +0200

    Increase memory limit for build
    required in vwpkw5 for 4.2 update

--- Kwf/Util/Build.php
+++ Kwf/Util/Build.php
@@ -56,7 +56,7 @@ class Kwf_Util_Build
         Kwf_Cache_SimpleStatic::disableFileCache();
         Kwf_Events_ModelObserver::getInstance()->disable();
 
-        Kwf_Util_MemoryLimit::set(1024);
+        Kwf_Util_MemoryLimit::set(1024*2);
         Kwf_Registry::set('db', false);
 
         if ($typeNames == 'all') {
