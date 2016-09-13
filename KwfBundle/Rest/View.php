8b1e51328ed88390cdfff33a35677d75bc404078
Niko Sams <ns@vivid-planet.com>
2016-09-13 15:04:23 +0200

    Fix incompatibility with base view class

--- KwfBundle/Rest/View.php
+++ KwfBundle/Rest/View.php
@@ -4,16 +4,16 @@ use FOS\RestBundle\View\View as ViewBase;
 
 class View extends ViewBase
 {
-    private $context;
-
-    public function setContext($context)
-    {
-        $this->context = $context;
-        return $this;
-    }
-
-    public function getContext()
-    {
-        return $this->context;
-    }
+//     private $context;
+//
+//     public function setContext($context)
+//     {
+//         $this->context = $context;
+//         return $this;
+//     }
+//
+//     public function getContext()
+//     {
+//         return $this->context;
+//     }
 }
--- KwfBundle/Rest/ViewHandler.php
+++ KwfBundle/Rest/ViewHandler.php
@@ -1,9 +1,6 @@
 <?php
 namespace KwfBundle\Rest;
 use FOS\RestBundle\View\ViewHandler as ViewHandlerBase;
-use FOS\RestBundle\View\View;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ViewHandler extends ViewHandlerBase
 {
