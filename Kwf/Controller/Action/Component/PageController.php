koala-framework/koala-framework

commit f3d4e4f0586f84a6a048455dbcc56bba41ce6f5f

Convert DenyAddComponentClass plugin to Events
- plugin interface was added in 4.2, so this is not an incompatible change

Kwf/Controller/Action/Component/PageController.php
@@ -101,16 +101,17 @@ class Kwf_Controller_Action_Component_PageController extends Kwf_Controller_Acti
             $componentValues = $fields['component']->getValues();
         }
         foreach ($possibleComponentClasses as $key=>$componentClass) {
-            foreach (Kwf_Component_Data_Root::getInstance()->getPlugins('Kwf_Component_PluginRoot_Interface_DenyAddComponentClass') as $p) {
-                if ($p->isComponentClassAddDenied($componentOrParent, $componentClass)) {
-                    if ($this->_getParam('id') && $componentOrParent->componentClass == $componentClass) {
-                        //in edit-mode allow already used componentClass
-                    } else {
-                        unset($componentValues[$key]);
-                        continue 2;
-                    }
+            $ev = new Kwf_Component_Event_Component_FilterAddComponentClass($componentClass, $componentOrParent);
+            Kwf_Events_Dispatcher::fireEvent($ev);
+            if (!$ev->deny) {
+                if ($this->_getParam('id') && $componentOrParent->componentClass == $componentClass) {
+                    //in edit-mode allow already used componentClass
+                } else {
+                    unset($componentValues[$key]);
+                    continue 2;
                 }
             }
+
             $component = array(
                 'componentClass' => $componentClass,
                 'inheritClasses' => $inheritClasses

Kwc/Paragraphs/Controller.php
@@ -54,10 +54,10 @@ class Kwc_Paragraphs_Controller extends Kwf_Controller_Action_Auto_Kwc_Grid
 
         $this->view->deniedComponentClasses = array();
         foreach (Kwc_Abstract::getChildComponentClasses($this->_getParam('class'), 'paragraphs') as $componentKey=>$componentClass) {
-            foreach (Kwf_Component_Data_Root::getInstance()->getPlugins('Kwf_Component_PluginRoot_Interface_DenyAddComponentClass') as $p) {
-                if ($p->isComponentClassAddDenied($c, $componentClass)) {
-                    $this->view->deniedComponentClasses[] = $componentClass;
-                }
+            $ev = new Kwf_Component_Event_Component_FilterAddComponentClass($componentClass, $c);
+            Kwf_Events_Dispatcher::fireEvent($ev);
+            if ($ev->deny) {
+                $this->view->deniedComponentClasses[] = $componentClass;
             }
         }
     }
@@ -99,10 +99,10 @@ class Kwc_Paragraphs_Controller extends Kwf_Controller_Action_Auto_Kwc_Grid
                     }
                 }
             }
-            foreach (Kwf_Component_Data_Root::getInstance()->getPlugins('Kwf_Component_PluginRoot_Interface_DenyAddComponentClass') as $p) {
-                if ($p->isComponentClassAddDenied($paragraphsData, $class)) {
-                    throw new Kwf_Exception("Adding '$class' is denied for $paragraphsData->componentId by plugin");
-                }
+            $ev = new Kwf_Component_Event_Component_FilterAddComponentClass($class, $paragraphsData);
+            Kwf_Events_Dispatcher::fireEvent($ev);
+            if ($ev->deny) {
+                throw new Kwf_Exception("Adding '$class' is denied for $paragraphsData->componentId by plugin");
             }
 
             $row = $this->_model->createRow();

Kwc/Root/Category/Generator.php
@@ -542,12 +542,12 @@ class Kwc_Root_Category_Generator extends Kwf_Component_Generator_Abstract
             throw new Kwf_Exception("didn't find just duplicated component '$newRow->id' below '{$parentTarget->componentId}'");
         }
 
-        foreach (Kwf_Component_Data_Root::getInstance()->getPlugins('Kwf_Component_PluginRoot_Interface_DenyAddComponentClass') as $p) {
-            if ($p->isComponentClassAddDenied($target, $source->componentClass)) {
-                //increment for number of child pages as those will be skipped
-                if ($progressBar) $progressBar->next($this->getDuplicateProgressSteps($source)-1);
-                return null;
-            }
+        $ev = new Kwf_Component_Event_Component_FilterAddComponentClass($source->componentClass, $target);
+        Kwf_Events_Dispatcher::fireEvent($ev);
+        if ($ev->deny) {
+            //increment for number of child pages as those will be skipped
+            if ($progressBar) $progressBar->next($this->getDuplicateProgressSteps($source)-1);
+            return null;
         }
 
         Kwc_Admin::getInstance($source->componentClass)->duplicate($source, $target, $progressBar);

Kwf/Component/Event/Component/FilterAddComponentClass.php
@@ -1,0 +1,12 @@
+<?php
+class Kwf_Component_Event_Component_FilterAddComponentClass extends Kwf_Events_Event_Abstract
+{
+    public $componentOrParent;
+    public $deny = false;
+
+    public function __construct($class, $componentOrParent)
+    {
+        parent::__construct($class);
+        $this->componentOrParent = $componentOrParent;
+    }
+}

Kwf/Component/Generator/Table.php
@@ -321,10 +321,10 @@ class Kwf_Component_Generator_Table extends Kwf_Component_Generator_Abstract
             throw new Kwf_Exception("you must call this only with the correct source");
         }
 
-        foreach (Kwf_Component_Data_Root::getInstance()->getPlugins('Kwf_Component_PluginRoot_Interface_DenyAddComponentClass') as $p) {
-            if ($p->isComponentClassAddDenied($parentTarget, $source->componentClass)) {
-                return null;
-            }
+        $ev = new Kwf_Component_Event_Component_FilterAddComponentClass($source->componentClass, $parentTarget);
+        Kwf_Events_Dispatcher::fireEvent($ev);
+        if ($ev->deny) {
+            return null;
         }
 
         $newRow = $this->_duplicateRow($source, $parentTarget);

Kwf/Component/PluginRoot/Interface/DenyAddComponentClass.php
@@ -1,5 +1,0 @@
-<?php
-interface Kwf_Component_PluginRoot_Interface_DenyAddComponentClass
-{
-    public function isComponentClassAddDenied(Kwf_Component_Data $data, $componentClass);
-}

commit 411960c6eb17fafe0943ada4dc1882d5be07a902

There are no 2 loops to continue.
Fixes bug introduced in f3d4e4f

Kwf/Controller/Action/Component/PageController.php
@@ -108,7 +108,7 @@ class Kwf_Controller_Action_Component_PageController extends Kwf_Controller_Acti
                     //in edit-mode allow already used componentClass
                 } else {
                     unset($componentValues[$key]);
-                    continue 2;
+                    continue;
                 }
             }
 