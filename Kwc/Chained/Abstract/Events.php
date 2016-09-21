1a29be8205786fe48a8da64b8c4a9991199ad0ed
Ricky Smith <rs@vivid-planet.com>
2016-09-21 09:33:51 +0200

    Fix further PHP 7 issues

--- Kwc/Chained/Abstract/Events.php
+++ Kwc/Chained/Abstract/Events.php
@@ -13,7 +13,7 @@ class Kwc_Chained_Abstract_Events extends Kwc_Abstract_Events
         return $ret;
     }
 
-    public function onMasterContentChanged()
+    public function onMasterContentChanged(Kwf_Component_Event_Component_ContentChanged $event)
     {
     }
 }
