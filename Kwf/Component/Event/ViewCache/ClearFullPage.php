65f83b6f4be6d03bc472ed4461cb2db28f8a761f
Niko Sams <ns@vivid-planet.com>
2016-10-03 13:12:24 +0200

    Add domain_component_id to cache_component
    Can be used by kwf-varnish to clear varnish cache

--- Kwf/Component/Event/ViewCache/ClearFullPage.php
+++ Kwf/Component/Event/ViewCache/ClearFullPage.php
@@ -1,11 +1,13 @@
 <?php
 class Kwf_Component_Event_ViewCache_ClearFullPage extends Kwf_Events_Event_Abstract
 {
+    public $domainComponentId;
     public $urls;
 
-    public function __construct($componentClass, array $urls)
+    public function __construct($componentClass, $domainComponentId, array $urls)
     {
         parent::__construct($componentClass);
+        $this->domainComponentId = $domainComponentId;
         $this->urls = $urls;
     }
 }
---
+++ Kwf/Update/20161003CacheComponentDomainComponentId.sql
@@ -0,0 +1,2 @@
+#tags: kwc
+ALTER TABLE `cache_component` ADD `domain_component_id` VARCHAR(255) NULL AFTER `url`;
