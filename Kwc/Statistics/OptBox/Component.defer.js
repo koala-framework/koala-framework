826a077ca26dff7c5c8f338b13c4ca0e249a2f2b
Franz Unger <ufx@vivid-planet.com>
2016-10-06 17:21:35 +0200

    Rewrite cookieOpt mechanism
    Make cookieOpt asynchron to be able to use remote cookie setting scripts
    Remove componentEvent and subscribe directly to onOptChange

--- Kwc/Statistics/OptBox/Component.defer.js
+++ Kwc/Statistics/OptBox/Component.defer.js
@@ -3,22 +3,23 @@ var onReady = require('kwf/on-ready');
 var cookieOpt = require('kwf/cookie-opt');
 
 onReady.onRender('.kwcClass', function (el, config) {
-    if (!cookieOpt.isSetOpt()) {
-        if (config.showBanner) {
-            setTimeout(function(){
+    cookieOpt.load(function(api) {
+        if (!api.getCookieIsSet()) {
+            if (config.showBanner) {
                 $('body').addClass('kwfUp-showCookieBanner');
-            }, 1000);
+                onReady.callOnContentReady($('body'), { action: 'widthChange' });
+            }
+            el.show();
+            el.find('.kwcBem__accept').click(function(e) {
+                e.preventDefault();
+                api.setOpt('in');
+                el.hide();
+                $('body').removeClass('kwfUp-showCookieBanner').addClass('kwfUp-cookieAccepted');
+                onReady.callOnContentReady($('body'), { action: 'widthChange' });
+            });
         }
-        el.show();
-        el.find('.kwcBem__accept').click(function(e) {
-            e.preventDefault();
-            cookieOpt.setOpt('in');
-            el.hide();
-            $('body').removeClass('kwfUp-showCookieBanner');
+        api.onOptChanged(function (value) {
+            $('body').find('.kwcClass').hide();
         });
-    }
-});
-
-cookieOpt.onOptChange(function(value) {
-    $('body').find('.kwcClass').hide();
+    });
 });
