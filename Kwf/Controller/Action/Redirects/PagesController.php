65025c0516fcabe1ef7ad6bec0c17bb3660b59bc
Alexander Kaufmann <ak@vivid-planet.com>
2016-10-07 07:23:20 +0200

    show pages with setting showInLinkInternAdmin in redirect pages controller

--- Kwf/Controller/Action/Redirects/PagesController.php
+++ Kwf/Controller/Action/Redirects/PagesController.php
@@ -1,4 +1,5 @@
 <?php
 class Kwf_Controller_Action_Redirects_PagesController extends Kwf_Controller_Action_Component_PagesController
 {
+    protected $_modelName = 'Kwf_Controller_Action_Redirects_PagesModel';
 }
