83912a293d3459aa9a52c703248e98fbf326e9c1
Mario Kemetinger <mk@vivid-planet.com>
2016-10-12 07:36:36 +0200

    true not as string - ooops

--- Kwc/Statistics/OptBox/Form.php
+++ Kwc/Statistics/OptBox/Form.php
@@ -5,7 +5,7 @@ class Kwc_Statistics_OptBox_Form extends Kwc_Abstract_Composite_Form
     {
         $this->add(new Kwf_Form_Field_Checkbox('show_banner', trlKwf('Show Banner')));
         $this->add(new Kwf_Form_Field_TextField('headline', trlKwf('Headline')))
-            ->setAllowBlank('true')
+            ->setAllowBlank(true)
             ->setWidth(500);
         $this->add(new Kwf_Form_Field_TextField('text', trlKwf('Text')))
             ->setDefaultValue(trlKwf('This website uses cookies to help us give you the best experience when you visit our website.'))
