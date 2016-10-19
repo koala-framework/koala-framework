f259d6036599a43b1f3c42be00b734428e046254
Niko Sams <ns@vivid-planet.com>
2016-10-19 12:24:00 +0200

    Fix E_STRICT error

--- Kwc/Advanced/IntegratorTemplate/Embed/ContentSender.php
+++ Kwc/Advanced/IntegratorTemplate/Embed/ContentSender.php
@@ -1,11 +1,10 @@
 <?php
 class Kwc_Advanced_IntegratorTemplate_Embed_ContentSender extends Kwf_Component_Abstract_ContentSender_Default
 {
-    protected function _render($includeMaster)
+    protected function _render($includeMaster, &$hasDynamicParts)
     {
         header('X-Robots-Tag: noindex');
         $domain = 'http'.(isset($_SERVER['HTTPS']) ? 's' : '').'://'.$_SERVER['HTTP_HOST'];
-        $hasDynamicParts = false;
         $ret = parent::_render($includeMaster, $hasDynamicParts);
         $ret = str_replace('href="/', 'href="' . $domain . '/', $ret);
         $ret = str_replace('src="/', 'src="' . $domain . '/', $ret);
