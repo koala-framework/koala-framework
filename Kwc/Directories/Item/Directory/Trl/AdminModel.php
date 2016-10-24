koala-framework/koala-framework

commit 6777326db77af66519ed1bf070504cf99aed8554

Fix strict mode errors

Kwc/Directories/Item/Directory/Trl/AdminModel.php
@@ -95,7 +95,7 @@ class Kwc_Directories_Item_Directory_Trl_AdminModel extends Kwf_Model_Proxy
         return $this->_proxyModel->countRows($select);
     }
 
-    public function getRowByProxiedRow($proxiedRow, $componentId)
+    public function getRowByProxiedRowAndComponentId($proxiedRow, $componentId)
     {
         $id = $proxiedRow->getInternalId().$componentId;
         if (!isset($this->_rows[$id])) {

Kwc/Directories/Item/Directory/Trl/AdminModelRowset.php
@@ -11,6 +11,6 @@ class Kwc_Directories_Item_Directory_Trl_AdminModelRowset extends Kwf_Model_Prox
     {
         $row = $this->_rowset->current();
         if (is_null($row)) return null;
-        return $this->_model->getRowByProxiedRow($row, $this->_componentId);
+        return $this->_model->getRowByProxiedRowAndComponentId($row, $this->_componentId);
     }
 }

Kwc/Directories/Item/Directory/Trl/ControllerEditButton.php
@@ -1,13 +1,13 @@
 <?php
 class Kwc_Directories_Item_Directory_Trl_ControllerEditButton extends Kwf_Grid_Column_Button
 {
-    public function load($row, $role)
+    public function load($row, $role, $info)
     {
         if ($row->getModel()->hasColumn('component') &&
             $this->getEditComponent() != $row->component
         ) {
             return self::BUTTON_INVISIBLE;
         }
-        return parent::load($row, $role);
+        return parent::load($row, $role, $info);
     }
 }

commit 0c27957ed2dba94d46b6c794f77455e40c11d9fe

Use correct method that takes componentId argument
Fixes bug introduced in 6777326db77af66519ed1bf070504cf99aed8554

Kwc/Directories/Item/Directory/Trl/AdminModel.php
@@ -78,7 +78,7 @@ class Kwc_Directories_Item_Directory_Trl_AdminModel extends Kwf_Model_Proxy
             throw new Kwf_Exception("invalid select");
         }
         $proxyRow = $this->_proxyModel->getRow($select);
-        return $this->getRowByProxiedRow($proxyRow, $componentId);
+        return $this->getRowByProxiedRowAndComponentId($proxyRow, $componentId);
     }
 
     public function countRows($where = array())