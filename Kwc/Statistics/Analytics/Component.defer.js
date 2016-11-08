38f226e44ce418ef4031c06828d48878f9f60c06
Niko Sams <ns@vivid-planet.com>
2016-11-08 14:32:24 +0100

    Statistics_Analytics: port to JS CookieOpt
    - server side plugin is not needed anymore (better performance, one less dynamic part)
    - js api can be used

---
+++ Kwc/Statistics/Analytics/Component.defer.js
@@ -0,0 +1,28 @@
+var onReady = require('kwf/on-ready');
+var cookieOpt = require('kwf/cookie-opt');
+
+onReady.onRender('.kwcClass', function(el) {
+    if (!el.data('ignore-code') && !location.search.match(/[\?&]kwcPreview/)) {
+        cookieOpt.load(function(api) {
+            if (api.getOpt() != 'out') {
+                (function (i, s, o, g, r, a, m) {
+                    i['GoogleAnalyticsObject'] = r;
+                    i[r] = i[r] || function () {
+                            (i[r].q = i[r].q || []).push(arguments)
+                        }, i[r].l = 1 * new
+                            Date();
+                    a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0];
+                    a.async = 1;
+                    a.src = g;
+                    m.parentNode.insertBefore(a, m)
+                })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+                ga('create', el.data('code'), 'auto');
+                ga('send', 'pageview');
+            }
+        });
+    }
+});
+
+
