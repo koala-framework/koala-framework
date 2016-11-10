2df1e60cac311ba54cfe188d15fc8a9923bb9087
Franz Unger <ufx@vivid-planet.com>
2016-11-10 10:12:10 +0100

    Add Recaptcha field as FormField Component

---
+++ Kwc/Form/Field/Recaptcha/Component.php
@@ -0,0 +1,18 @@
+<?php
+class Kwc_Form_Field_Recaptcha_Component extends Kwc_Form_Field_Abstract_Component
+{
+    public static function getSettings($param = null)
+    {
+        $ret = parent::getSettings($param);
+        $ret['componentName'] = trlKwfStatic('Form.Recaptcha');
+        $ret['componentIcon'] = 'lock_go';
+        return $ret;
+    }
+
+    protected function _getFormField()
+    {
+        $ret = new Kwf_Form_Field_Recaptcha($this->getData()->componentId);
+        $ret->setFieldLabel($this->getRow()->field_label);
+        return $ret;
+    }
+}
