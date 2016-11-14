249956ca6bc59b79a65f07a0dbfd9df23a52aeb2
Franz Unger <ufx@vivid-planet.com>
2016-11-14 13:30:29 +0100

    Move Recaptcha Component to bottom

--- Kwc/Form/Field/Recaptcha/Component.php
+++ Kwc/Form/Field/Recaptcha/Component.php
@@ -6,6 +6,7 @@ class Kwc_Form_Field_Recaptcha_Component extends Kwc_Form_Field_Abstract_Compone
         $ret = parent::getSettings($param);
         $ret['componentName'] = trlKwfStatic('Form.Recaptcha');
         $ret['componentIcon'] = 'lock_go';
+        $ret['componentPriority'] = -1;
         return $ret;
     }
 
