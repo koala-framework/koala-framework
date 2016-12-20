dda4d138d0ad7361458760e561e216c8168729fc
Alexander Kaufmann <ak@vivid-planet.com>
2016-12-20 16:31:56 +0100

    check only for redirect type when not empty, is for older kwf versions

--- Kwc/Mail/Redirect/Component.php
+++ Kwc/Mail/Redirect/Component.php
@@ -49,7 +49,7 @@ class Kwc_Mail_Redirect_Component extends Kwc_Abstract
     {
         $r = $this->_getRedirectRow();
 
-        if (isset($r->type) && $r->type != 'redirect') {
+        if (isset($r->type) && $r->type && $r->type != 'redirect') {
             throw new Kwf_Exception('Invalid type');
         }
 
