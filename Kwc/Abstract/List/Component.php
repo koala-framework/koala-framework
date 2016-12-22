9e31cfc9fb6682683a77de3627c01ee46c08d65f
Franz Unger <ufx@vivid-planet.com>
2016-12-22 14:09:04 +0100

    Add possibility to use non child components for List

--- Kwc/Abstract/List/Component.php
+++ Kwc/Abstract/List/Component.php
@@ -47,10 +47,15 @@ class Kwc_Abstract_List_Component extends Kwc_Abstract
         return $select;
     }
 
+    protected function _getListItems()
+    {
+        return $this->getData()->getChildComponents($this->getSelect());
+    }
+
     public function getTemplateVars(Kwf_Component_Renderer_Abstract $renderer)
     {
         $ret = parent::getTemplateVars($renderer);
-        $children = $this->getData()->getChildComponents($this->getSelect());
+        $children = $this->_getListItems();
 
         // children ist die alte methode, bleibt drin wegen kompatibilität
         $ret['children'] = $children;
