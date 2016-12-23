737afbd9e7ea548d478cf4eacc76c11224030b5b
Alexander Kaufmann <ak@vivid-planet.com>
2016-12-23 11:22:59 +0100

    fixed redirect after login, onReady has to use kwcClass to find correct element

--- Kwc/User/Login/Form/Success/Component.js
+++ Kwc/User/Login/Form/Success/Component.js
@@ -1,6 +1,6 @@
 var onReady = require('kwf/on-ready');
 
-onReady.onShow('.kwcUserLoginFormSuccess', function(el) {
+onReady.onShow('.kwcClass', function(el) {
     var url = el.find('input.redirectTo').val();
     if (!url) url = location.href;
     location.href = url;
