525e4ced82c95ea06906512fe469bc9b620baec9
Michael Tiefenthaler <mt@vivid-planet.com>
2016-12-30 12:58:11 +0100

    Add class-prefix to prevent .../dpr2/dpr2/... urls
    The script to create dpr2 imageurls will be called two times. One time from the e18 templatehost-page
    and from the e18 page. This creates wrong imageurls with /dpr2/dpr2/... in it. To prevent this, we add
    the kwfUp- prefix.

--- commonjs/replace-image-dpr.js
+++ commonjs/replace-image-dpr.js
@@ -1,7 +1,7 @@
 var onReady = require('kwf/on-ready');
 
 if (window.devicePixelRatio && window.devicePixelRatio > 1) {
-    onReady.onRender('.kwfReplaceImageDpr2', function(el) {
+    onReady.onRender('.kwfUp-kwfReplaceImageDpr2', function(el) {
         if (el.get(0).tagName.toLowerCase() == 'img') {
             el.get(0).src = el.get(0).src.replace('/images/', '/images/dpr2/');
         } else {
