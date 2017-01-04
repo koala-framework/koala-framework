deba91e9247057edd49fdbe15ede145bdf9bec20
Niko Sams <ns@vivid-planet.com>
2017-01-04 09:07:46 +0100

    Fix Kwc_Error output when boxes with required processInput exist in master
    Fixes SearchBox (which is a Form and needs processInput)

--- Kwc/Errors/Abstract/Component.php
+++ Kwc/Errors/Abstract/Component.php
@@ -1,6 +1,13 @@
 <?php
 abstract class Kwc_Errors_Abstract_Component extends Kwc_Abstract
 {
+    public static function getSettings($param = null)
+    {
+        $ret = parent::getSettings($param);
+        $ret['contentSender'] = 'Kwc_Errors_ContentSender';
+        return $ret;
+    }
+
     protected $_exception;
     public function setException($e)
     {
