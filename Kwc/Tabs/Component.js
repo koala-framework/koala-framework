ebd680ec9cf1e2bec6ec0f47bd61cb565378bc04
David Neumaier <dn@vivid-planet.com>
2017-01-27 10:45:43 +0100

    added a tab pre-selection functionality, so that any tab element can be provided with a child index that will automatically be selected after page load by using location.hash. also added backend internal link anchor support

--- Kwc/Tabs/Component.js
+++ Kwc/Tabs/Component.js
@@ -2,5 +2,8 @@ var onReady = require('kwf/on-ready');
 var Tabs = require('kwf/tabs/tabs');
 
 onReady.onRender('.kwcClass', function tabs(el) {
-    el.tabsObject = new Tabs(el);
+    el.tabsObject = new Tabs(el, {
+        // provide the hashPrefix value in order for the internal link anchor functionality to work.
+        hashPrefix: el.data('hash-prefix')
+    });
 });
