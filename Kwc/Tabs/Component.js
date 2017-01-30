6477dfb636a1517c486b7d0e318c0b4f75911845
David Neumaier <dn@vivid-planet.com>
2017-01-30 10:32:04 +0100

    Remove kwfTabs class for Kwc_Tabs, we initialize it ourselfe

--- Kwc/Tabs/Component.js
+++ Kwc/Tabs/Component.js
@@ -2,6 +2,10 @@ var onReady = require('kwf/on-ready');
 var Tabs = require('kwf/tabs/tabs');
 
 onReady.onRender('.kwcClass', function tabs(el) {
+    if (el.hasClass('kwfUp-kwfTabs')) {
+        console.error("Remove kwfUp-kwfTabs class for", el);
+        return;
+    }
     el.tabsObject = new Tabs(el, {
         // provide the hashPrefix value in order for the internal link anchor functionality to work.
         hashPrefix: el.data('hash-prefix')
--- Kwc/Tabs/Component.php
+++ Kwc/Tabs/Component.php
@@ -10,7 +10,6 @@ class Kwc_Tabs_Component extends Kwc_Abstract_List_Component
         $ret['componentCategory'] = 'layout';
         $ret['componentPriority'] = 80;
         $ret['rootElementClass'] = 'kwfUp-webStandard';
-        $ret['assetsDefer']['dep'][] = 'KwfTabs';
         $ret['extConfig'] = 'Kwc_Tabs_ExtConfig';
         $ret['contentWidthSubtract'] = 20;
         $ret['flags']['hasAnchors'] = true;
