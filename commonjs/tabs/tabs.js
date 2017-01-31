6babf24e4c9d7e0bfc9f42166a93aa627bd6c58f
David Neumaier <dn@vivid-planet.com>
2017-01-31 12:49:32 +0100

    rewrote the Tabs class and corresponding CSS files for CSS3 Transistions instead of jQuery animations

--- commonjs/tabs/tabs.js
+++ commonjs/tabs/tabs.js
@@ -36,7 +36,6 @@ var Tabs = function(el, config)
     this._activeTabIdx = null;
     this.switchEls = this.el.find('> .' + this.config.linkClass);
     this.contentEls = this.el.find('> .' + this.config.contentClass);
-    this.fxDuration = .5;
 
     var tabsLinks = $('<div class="' + this.config.linksClass + '"></div>').appendTo(this.el.first());
     this.tabsContents = $('<div class="' + this.config.contentsClass + '" data-width="100%"></div>').appendTo(this.el.first());
@@ -50,7 +49,7 @@ var Tabs = function(el, config)
         tabsLinks.append(this.switchEls[i]);
         var swEl = $(this.switchEls[i]);
 
-        $(this.contentEls[i]).hide();
+        $(this.contentEls[i]).removeClass(this.config.contentActiveClass);
         $(this.switchEls[i]).removeClass(this.config.linkActiveClass);
 
         // if it is important, show on startup
@@ -92,85 +91,42 @@ var Tabs = function(el, config)
     }
 
     if (activeTabIdx !== false) {
-        $(this.switchEls[activeTabIdx]).addClass(this.config.linkActiveClass);
-        $(this.contentEls[activeTabIdx]).show();
+        this.activateTab(activeTabIdx);
         this._activeTabIdx = activeTabIdx;
     }
 };
 
 Tabs.prototype = {
 
-    on: function(event, cb, scope)
-    {
+    on: function(event, cb, scope) {
         if (typeof scope != 'undefined') cb = cb.bind(scope);
         $(this.el).on('kwfUp-tabs-'+event, cb);
     },
 
-    fireEvent: function(event)
-    {
+    fireEvent: function(event) {
         var args = [].shift.call(arguments);
         $(this.el).trigger('kwfUp-tabs-'+event, args);
     },
 
     activateTab: function(idx) {
-        // passed arguments are: this, newIndex, oldIndex
-        var el = this;
         this.fireEvent('beforeTabActivate', this, idx, this._activeTabIdx);
         if (this._activeTabIdx == idx) return;
 
         var newContentEl = $(this.contentEls[idx]);
-        $(this.switchEls[idx]).addClass(this.config.linkActiveClass);
-        newContentEl.css('z-index', '1');
-        newContentEl.show();
+        var oldContentEl = $(this.contentEls[this._activeTabIdx]);
+        var newLinkEl = $(this.switchEls[idx]);
+        var oldLinkEl = $(this.switchEls[this._activeTabIdx]);
+
+        $(this.tabsContents).css('height', newContentEl.css('height'));
+
+        oldLinkEl.removeClass(this.config.linkActiveClass);
+        newLinkEl.addClass(this.config.linkActiveClass);
+        oldContentEl.removeClass(this.config.contentActiveClass);
         newContentEl.addClass(this.config.contentActiveClass);
 
-        var oldContentEl = $(this.contentEls[this._activeTabIdx]);
+        onReady.callOnContentReady(this.contentEls[idx], {newRender: false});
+        onReady.callOnContentReady(this.el, {action: 'show'});
 
-        oldContentEl.stop();
-        newContentEl.stop();
-        this.tabsContents.stop();
-        if (this._activeTabIdx !== null) {
-            $(this.switchEls[this._activeTabIdx]).removeClass(this.config.linkActiveClass);
-            oldContentEl.css({
-                'z-index': 2,
-                'position': 'absolute'
-            });
-            newContentEl.css({
-                'z-index': 1,
-                'position': 'absolute'
-            });
-            onReady.callOnContentReady(this.contentEls[idx], {newRender: false});
-            oldContentEl.hide();
-        }
-        if (this._activeTabIdx !== null) {
-            oldContentEl.show();
-
-            newContentEl.show();
-            onReady.callOnContentReady(this.el, {action: 'show'});
-            newContentEl.hide();
-
-            newContentEl.fadeIn({
-                duration: this.fxDuration,
-                complete: function() {
-                    $(this).closest().css('height', 'auto');     //set the height after animation to auto because there are Components who change height when they are inside a tab
-                }
-            });
-            oldContentEl.fadeOut({
-                duration: this.fxDuration,
-                complete: function() {
-                    oldContentEl.css('position', 'static');
-                    oldContentEl.removeClass(el.config.contentActiveClass);
-                    newContentEl.css('position', 'static');
-                    newContentEl.show();
-                    newContentEl.css('height', 'auto');
-                }
-            });
-        }
-
-        this.tabsContents.height(oldContentEl.height());
-        this.tabsContents.animate({ height: newContentEl.height() });
-
-        // passed arguments are: this, newIndex, oldIndex
         this.fireEvent('tabActivate', this, idx, this._activeTabIdx);
         statistics.trackView(document.location.href + '#tab' + (idx + 1));
 
@@ -180,19 +136,6 @@ Tabs.prototype = {
             // if this tab uses a hashPrefix to be recognized from the URL, set the new value.
             window.location.replace(window.location.href.split('#')[0] + '#' + this.config.hashPrefix + ':' + idx);
         }
-    },
-
-    getIdxByContentEl: function(el) {
-        if (el.dom) el = el.dom;
-        for (var i = 0; i < this.contentEls.length; i++) {
-            if (this.contentEls[i] === el) return i;
-        }
-        return (-1);
-    },
-
-    getContentElByIdx: function(idx) {
-        if (this.contentEls[idx]) return this.contentEls[idx];
-        return null;
     }
 };
 
