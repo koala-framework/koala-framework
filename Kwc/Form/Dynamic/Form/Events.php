089c625803e30564a471caff8cb39db27b7a0edc
Franz Unger <ufx@vivid-planet.com>
2017-03-02 08:19:41 +0100

    Fix Dynamic Form Events when parent class get overwritten

--- Kwc/Form/Dynamic/Form/Events.php
+++ Kwc/Form/Dynamic/Form/Events.php
@@ -4,11 +4,13 @@ class Kwc_Form_Dynamic_Form_Events extends Kwc_Abstract_Events
     public function getListeners()
     {
         $ret = parent::getListeners();
-        $ret[] = array(
-            'class' => 'Kwc_Form_Dynamic_Component',
-            'event' => 'Kwf_Component_Event_Component_ContentChanged',
-            'callback' => 'onContentChange'
-        );
+        foreach ($this->_getCreatingClasses($this->_class) as $class) {
+            $ret[] = array(
+                'class' => $class,
+                'event' => 'Kwf_Component_Event_Component_ContentChanged',
+                'callback' => 'onContentChange'
+            );
+        }
         return $ret;
     }
 
