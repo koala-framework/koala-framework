ca0b6cfb1a0630a0bf68da93f8f6ab91a1b1042d
Alexander Kaufmann <ak@vivid-planet.com>
2017-03-07 15:16:13 +0100

    if remote addr is not available for subscribers log ip will be null now

--- Kwc/Newsletter/Subscribe/Row.php
+++ Kwc/Newsletter/Subscribe/Row.php
@@ -65,7 +65,7 @@ class Kwc_Newsletter_Subscribe_Row extends Kwf_Model_Db_Row
 
     public function getLogIp()
     {
-        return ($this->_logIp) ? $this->_logIp : $_SERVER['REMOTE_ADDR'];
+        return ($this->_logIp) ? $this->_logIp : (array_key_exists('REMOTE_ADDR', $_SERVER)) ? $_SERVER['REMOTE_ADDR'] : null;
     }
 
     public function writeLog($message, $saveImmediatly = false)
