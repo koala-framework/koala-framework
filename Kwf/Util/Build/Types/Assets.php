414854774c2dbb2afb4aa29edf7251a7b3b89e6f
Niko Sams <ns@vivid-planet.com>
2017-03-24 12:34:35 +0100

    rebuild node-sass if broken

--- Kwf/Util/Build/Types/Assets.php
+++ Kwf/Util/Build/Types/Assets.php
@@ -74,7 +74,18 @@ class Kwf_Util_Build_Types_Assets extends Kwf_Util_Build_Types_Abstract
 */
     protected function _build($options)
     {
-        passthru('NODE_PATH=vendor/koala-framework/koala-framework/node_modules_add ./vendor/bin/webpack --progress --colors', $retVal);
+        //check if node-sass is working correctly, if not try to npm rebuild
+        //works around possible issues with binaries used by node-sass
+        exec("./vendor/bin/node ./node_modules/.bin/node-sass --version", $output, $retVal);
+        if ($retVal) {
+            //node-sass doesn't work
+            passthru("./vendor/bin/npm rebuild node-sass", $retVal);
+            if ($retVal) {
+                throw new Kwf_Exception("node-sass rebuild failed");
+            }
+        }
+
+        passthru('NODE_PATH=vendor/koala-framework/koala-framework/node_modules_add ./vendor/bin/node  vendor/bin/webpack --progress --colors', $retVal);
         if ($retVal) {
             throw new Kwf_Exception("webpack failed");
         }
