94a976f02bef3a2847643f1ec687ac436a6a8ac8
Niko Sams <ns@vivid-planet.com>
2017-03-30 16:42:34 +0200

    ccw now start webpack-dev-server

--- Kwf/Controller/Action/Cli/Web/ClearCacheWatcherController.php
+++ Kwf/Controller/Action/Cli/Web/ClearCacheWatcherController.php
@@ -8,6 +8,25 @@ class Kwf_Controller_Action_Cli_Web_ClearCacheWatcherController extends Kwf_Cont
 
     public function indexAction()
     {
-        Kwf_Util_ClearCache_Watcher::watch();
+        $port = null;
+        if (file_exists('temp/webpack-dev-server-port')) {
+            $port = file_get_contents('temp/webpack-dev-server-port');
+        }
+        while (true) {
+            if ($port) {
+                $r = @socket_create_listen($port);
+                if ($r) {
+                    socket_close($r);
+                    break;
+                }
+                echo "port $port not available, choosing a random one";
+            }
+            $port = rand(1024, 65535);
+        }
+        file_put_contents('temp/webpack-dev-server-port', $port);
+        $host = trim(`hostname`);
+        $cmd = "NODE_PATH=vendor/koala-framework/koala-framework/node_modules_add vendor/bin/node node_modules/.bin/webpack-dev-server --progress --host=$host --port=$port --color";
+        echo $cmd."\n";
+        passthru($cmd);
     }
 }
--- Kwf/View/Helper/Assets.php
+++ Kwf/View/Helper/Assets.php
@@ -17,6 +17,15 @@ class Kwf_View_Helper_Assets
         $c = preg_replace('#</?head>#', '', $c);
         $c = str_replace('/assets/build/./', '/assets/build/', $c);
 
+        if (Kwf_Config::getValue('debug.webpackDevServer')) {
+            if (!file_exists('temp/webpack-dev-server-port')) {
+                throw new Kwf_Exception("webpack-dev-server not running, please start clear-cache-watcher");
+            }
+            $port = file_get_contents('temp/webpack-dev-server-port');
+            $host = trim(`hostname`);
+            $c = str_replace('/assets/build/', "http://$host:$port/assets/build/", $c);
+        }
+
         $ret .= $c;
         return $ret;
     }
