d535d80267f3129ed593d4b8f4e429a1a63eaa28
Niko Sams <ns@vivid-planet.com>
2017-04-05 13:41:44 +0200

    Remove ie8 compatibility: remove html5shiv and es5shim

--- Kwf/Assets/Package/ComponentFrontend.php
+++ Kwf/Assets/Package/ComponentFrontend.php
@@ -49,13 +49,4 @@ class Kwf_Assets_Package_ComponentFrontend extends Kwf_Assets_Package_Default
 
         return $ret;
     }
-
-    public function getPackageUrls($mimeType, $language)
-    {
-        $ret = parent::getPackageUrls($mimeType, $language);
-        if ($mimeType == 'text/javascript; ie8') {
-            $ret[] = '/assets/es5-shim/es5-shim.js';
-        }
-        return $ret;
-    }
 }
--- commonjs/frontend-core.js
+++
@@ -1,1 +0,0 @@
-document.write('<!--[if lt IE 9]><script type="text/javascript" src="/assets/html5shiv/dist/html5shiv.min.js"><' + '/script><![endif]-->');
