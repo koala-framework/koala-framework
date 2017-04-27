6bd77cb247a03bc949c0095d98db79630103e5e6
Franz Unger <ufx@vivid-planet.com>
2017-04-27 08:12:40 +0200

    Show correct text when clicking cookie opt checkbox

--- Kwc/Statistics/Opt/Component.defer.js
+++ Kwc/Statistics/Opt/Component.defer.js
@@ -10,10 +10,15 @@ Kwf.onElementReady('.kwcStatisticsOpt .webForm', function(el, config) {
         }
     }, el);
     Kwf.onComponentEvent('cookieOptChanged', function(value) {
+        var field = el.kwcForm.findField('form_opt');
         if (value == 'in') {
-            el.kwcForm.findField('form_opt').setValue(true);
-        } else if (value == 'out') {
-            el.kwcForm.findField('form_opt').setValue(false);
+            var fieldValue = true;
+            var labelText = trlKwf('Cookies are set when visiting this webpage. Click to deactivate cookies.');
+        } else {
+            var fieldValue = false;
+            var labelText = trlKwf('No cookies are set when visiting this webpage. Click to activate cookies.');
         }
+        field.setValue(fieldValue);
+        field.el.child('.boxLabel').update(labelText);
     });
 }, {defer: true});
