c614436c91a9396187ba2226afd82e4d5df485d0
Niko Sams <ns@vivid-planet.com>
2017-05-10 08:52:12 +0200

    If build is a git repo don't remove .git folder

--- Kwf/Controller/Action/Cli/BuildController.php
+++ Kwf/Controller/Action/Cli/BuildController.php
@@ -55,7 +55,7 @@ class Kwf_Controller_Action_Cli_BuildController extends Kwf_Controller_Action_Cl
                 RecursiveIteratorIterator::CHILD_FIRST
             );
             foreach ($files as $fileinfo) {
-                if ($fileinfo->getFilename() != '.gitignore') {
+                if ($fileinfo->getFilename() != '.gitignore' && $fileinfo->getFilename() != '.git') {
                     $todo = ($fileinfo->isDir() ? 'rmdir' : 'unlink');
                     $todo($fileinfo->getRealPath());
                 }
