77335ef906180b7bed7626d6e2b7b2d13f431b66
Niko Sams <ns@vivid-planet.com>
2017-05-30 13:19:08 +0200

    Exclude User related components in sitemap.xml

--- Kwc/User/DeleteAccount/Component.php
+++ Kwc/User/DeleteAccount/Component.php
@@ -5,6 +5,7 @@ class Kwc_User_DeleteAccount_Component extends Kwc_Abstract_Composite_Component
     {
         $ret = parent::getSettings($param);
         $ret['generators']['child']['component']['form'] = 'Kwc_User_DeleteAccount_Form_Component';
+        $ret['flags']['noIndex'] = true;
         return $ret;
     }
 }
