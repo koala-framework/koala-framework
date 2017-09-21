89e3b108e5e1334c0134dbcfa4c5eb6e19cf7da9
Niko Sams <ns@vivid-planet.com>
2017-09-21 13:54:12 +0200

    add support for NO_PROGRESS env var for less verbose output during assets build

--- Kwf/Util/Build/Types/Assets.php
+++ Kwf/Util/Build/Types/Assets.php
@@ -72,7 +72,9 @@ class Kwf_Util_Build_Types_Assets extends Kwf_Util_Build_Types_Abstract
 */
     protected function _build($options)
     {
-        passthru('NODE_PATH=vendor/koala-framework/koala-framework/node_modules_build ./vendor/bin/node  node_modules/.bin/webpack --progress --colors', $retVal);
+        $cmd = 'NODE_PATH=vendor/koala-framework/koala-framework/node_modules_build ./vendor/bin/node  node_modules/.bin/webpack --colors';
+        if (!isset($_SERVER['NO_PROGRESS'])) $cmd .= ' --progress';
+        passthru($cmd, $retVal);
         if ($retVal) {
             throw new Kwf_Exception("webpack failed");
         }
