f9e057e3b63a9b90f4aa3d85147cb9f9e1040aad
David Neumaier <dn@vivid-planet.com>
2017-10-23 16:28:46 +0200

    deny access to welcome controller to normal users by default

--- Kwf/Acl/Component.php
+++ Kwf/Acl/Component.php
@@ -89,6 +89,8 @@ class Kwf_Acl_Component extends Kwf_Acl
 
         $this->deny('guest', 'kwf_welcome_welcome');
         $this->deny('guest', 'kwf_component_pages');
+        $this->deny('user', 'kwf_welcome_welcome');
+        $this->deny('user', 'kwf_component_pages');
 
         $this->allow('superuser', 'kwf_media_upload_any');
 
