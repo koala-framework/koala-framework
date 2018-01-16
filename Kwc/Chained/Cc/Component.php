cbded12d7756200e758bcfa7882a65cd03786f71
Niko Sams <ns@vivid-planet.com>
2018-01-16 15:12:49 +0100

    Remove assetsPackage from components
    instead use require.ensure to load dependencies async

--- Kwc/Chained/Cc/Component.php
+++ Kwc/Chained/Cc/Component.php
@@ -5,7 +5,7 @@ class Kwc_Chained_Cc_Component extends Kwc_Chained_Abstract_Component
     {
         $ret = parent::getSettings($masterComponentClass);
         $copySettings = array('componentName', 'componentIcon', 'editComponents', 'viewCache', 'contentSender', 'plugins', 'masterTemplate', 'resetMaster');
-        $copyFlags = array('processInput', 'menuCategory', 'hasHome', 'chainedType', 'subroot', 'hasAlternativeComponent', 'hasFulltext', 'skipFulltext', 'skipFulltextRecursive', 'assetsPackage');
+        $copyFlags = array('processInput', 'menuCategory', 'hasHome', 'chainedType', 'subroot', 'hasAlternativeComponent', 'hasFulltext', 'skipFulltext', 'skipFulltextRecursive');
         $ret = Kwc_Chained_Abstract_Component::getChainedSettings($ret, $masterComponentClass, 'Cc', $copySettings, $copyFlags);
         return $ret;
     }
