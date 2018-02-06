a7bca5a978edd5640ac07846e0662a79c6323e6d
Alexander Kaufmann <ak@vivid-planet.com>
2018-02-06 08:36:52 +0100

    abstracted same code for linkTag komponents in newsletter detail mail paragraphs

--- Kwc/Newsletter/Detail/Mail/Paragraphs/LinkTag/EditSubscriber/Admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Kwc_Newsletter_Detail_Mail_Paragraphs_LinkTag_EditSubscriber_Admin
-    extends Kwc_Basic_LinkTag_Abstract_Admin
-{
-    public function componentToString(Kwf_Component_Data $data)
-    {
-        return Kwc_Abstract::getSetting($this->_class, 'componentName');
-    }
-}
