63f13f430ebf21d54cba49883e779af62e8182c6
Alexander Kaufmann <ak@vivid-planet.com>
2018-02-08 08:00:11 +0100

    added optional subscribe mail that is editable
    it uses same paragraphs as newsletter mail

---
+++ Kwc/Newsletter/Subscribe/MailEditable/Component.php
@@ -0,0 +1,39 @@
+<?php
+class Kwc_Newsletter_Subscribe_MailEditable_Component extends Kwc_Mail_Editable_Component
+{
+    public static function getSettings($param = null)
+    {
+        $ret = parent::getSettings($param);
+        $ret['recipientSources']['sub'] = 'Kwc_Newsletter_Subscribe_Model';
+        $ret['generators']['content']['component'] = 'Kwc_Newsletter_Subscribe_MailEditable_Content_Component';
+        return $ret;
+    }
+
+    public function getTemplateVars(Kwf_Component_Renderer_Abstract $renderer)
+    {
+        $ret = parent::getTemplateVars($renderer);
+        $ret = array_merge($ret, $this->getMailData());
+        return $ret;
+    }
+
+    public function getPlaceholders(Kwc_Mail_Recipient_Interface $recipient = null)
+    {
+        $ret = parent::getPlaceholders($recipient);
+        if (isset($_SERVER['HTTP_HOST'])) {
+            $ret['host'] = $_SERVER['HTTP_HOST'];
+        } else {
+            $ret['host'] = Kwf_Registry::get('config')->server->domain;
+        }
+        return $ret;
+    }
+
+    public function getSubject(Kwc_Mail_Recipient_Interface $recipient = null)
+    {
+        return $this->getData()->trlKwf('Newsletter subscription');
+    }
+
+    public function getNameForEdit()
+    {
+        return $this->getData()->trlKwf('Subscription') . ' (' . $this->getData()->getDomainComponent()->name . ')';
+    }
+}
