15d7e8b81924e06d9385bb792caaf8f375b8dbec
Alexander Kaufmann <ak@vivid-planet.com>
2018-02-08 09:48:18 +0100

    show preview of newsletter subscribe mail with placeholders not reseted
    placeholders are not reseted because user should see plain version

--- Kwc/Newsletter/Subscribe/MailEditable/Component.php
+++ Kwc/Newsletter/Subscribe/MailEditable/Component.php
@@ -6,23 +6,37 @@ class Kwc_Newsletter_Subscribe_MailEditable_Component extends Kwc_Mail_Editable_
         $ret = parent::getSettings($param);
         $ret['recipientSources']['sub'] = 'Kwc_Newsletter_Subscribe_Model';
         $ret['generators']['content']['component'] = 'Kwc_Newsletter_Subscribe_MailEditable_Content_Component';
+
+        $ret['assetsAdmin']['files'][] = 'kwf/Kwc/Newsletter/Subscribe/MailEditable/Panel.js';
+        $ret['assetsAdmin']['files'][] = 'kwf/Kwc/Newsletter/Subscribe/MailEditable/PreviewPanel.js';
         return $ret;
     }
 
     public function getTemplateVars(Kwf_Component_Renderer_Abstract $renderer)
     {
         $ret = parent::getTemplateVars($renderer);
-        $ret = array_merge($ret, $this->getMailData());
+        if ($mailData = $this->getMailData()) $ret = array_merge($ret, $mailData);
+
+        if (!array_key_exists('doubleOptInComponent', $ret)) {
+            $ret['doubleOptInComponent'] = $this->getData()->parent->getChildComponent('_doubleOptIn');
+        }
+
+        if (!array_key_exists('editComponent', $ret)) {
+            $nlData = $this->getData()->parent->getComponent()->getSubscribeToNewsletterComponent();
+            $ret['editComponent'] = $nlData->getChildComponent('_editSubscriber');
+        }
         return $ret;
     }
 
     public function getPlaceholders(Kwc_Mail_Recipient_Interface $recipient = null)
     {
         $ret = parent::getPlaceholders($recipient);
-        if (isset($_SERVER['HTTP_HOST'])) {
-            $ret['host'] = $_SERVER['HTTP_HOST'];
-        } else {
-            $ret['host'] = Kwf_Registry::get('config')->server->domain;
+        if ($recipient) {
+            if (isset($_SERVER['HTTP_HOST'])) {
+                $ret['host'] = $_SERVER['HTTP_HOST'];
+            } else {
+                $ret['host'] = Kwf_Registry::get('config')->server->domain;
+            }
         }
         return $ret;
     }
