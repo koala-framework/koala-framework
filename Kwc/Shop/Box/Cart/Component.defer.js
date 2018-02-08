9520ab1f9e7020879c8e3c2e8f6faad31fd572ef
mt@vivid-planet.com <mt@vivid-planet.com>
2018-02-08 16:26:09 +0100

    Fix wrong exchange of cartbox-domelement

--- Kwc/Shop/Box/Cart/Component.defer.js
+++ Kwc/Shop/Box/Cart/Component.defer.js
@@ -11,7 +11,7 @@ onReady.onRender('.kwcClass',function(el) {
                 url: url,
                 data: { componentId: el.data('component-id') },
                 success: function(response) {
-                    el.html(response);
+                    $('.kwcClass').html(response);
                     onReady.callOnContentReady(el, {newRender: true});
                 }
             });
