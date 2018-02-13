f6171e99f11662b3feec4950dec3f695d9fecaed
Alexander Kaufmann <ak@vivid-planet.com>
2018-02-13 14:28:47 +0100

    disabled csrf check for newsletter subscribe api because anyone can subscribe in frontend too

--- Kwc/Newsletter/Subscribe/ApiController.php
+++ Kwc/Newsletter/Subscribe/ApiController.php
@@ -11,6 +11,11 @@ class Kwc_Newsletter_Subscribe_ApiController extends Kwf_Controller_Action
         return true;
     }
 
+    protected function _validateCsrf()
+    {
+        //allow for everyone, as anyone can subscribe in frontend form too
+    }
+
     public function preDispatch()
     {
         parent::preDispatch();
