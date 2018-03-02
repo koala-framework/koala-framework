c50c50f9f8bfe2627f9ba2d46ec9a3377465bcb9
Ricky Smith <rs@vivid-planet.com>
2018-03-02 13:16:46 +0100

    Correctly translate the componentName

--- Kwc/Newsletter/Detail/Mail/Paragraphs/LinkTag/Abstract/Admin.php
+++ Kwc/Newsletter/Detail/Mail/Paragraphs/LinkTag/Abstract/Admin.php
@@ -4,6 +4,6 @@ class Kwc_Newsletter_Detail_Mail_Paragraphs_LinkTag_Abstract_Admin
 {
     public function componentToString(Kwf_Component_Data $data)
     {
-        return Kwc_Abstract::getSetting($this->_class, 'componentName');
+        return $data->trlStaticExecute(Kwc_Abstract::getSetting($this->_class, 'componentName'));
     }
 }
