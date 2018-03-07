d209de6ef4b0b3db0a0f1b08d2d8700a81c75e86
Alexander Kaufmann <ak@vivid-planet.com>
2018-03-07 08:10:01 +0100

    disable csrf protection in kwf bundle for option requests

--- KwfBundle/EventListener/CsrfProtection.php
+++ KwfBundle/EventListener/CsrfProtection.php
@@ -4,13 +4,16 @@ namespace KwfBundle\EventListener;
 
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpFoundation\Request;
 
 class CsrfProtection
 {
     public function onKernelRequest(GetResponseEvent $event)
     {
         $request = $event->getRequest();
-        if ($request->headers->get('X-Requested-With') !== 'XMLHttpRequest') {
+        if ($request->headers->get('X-Requested-With') !== 'XMLHttpRequest' &&
+            $request->getMethod() !== Request::METHOD_OPTIONS) {
+
             throw new AccessDeniedHttpException('Missing X-Requested-With header');
         }
     }
