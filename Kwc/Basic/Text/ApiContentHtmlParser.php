9d5128e92e384c339e3fa7a9a4fc28295902a1e3
Niko Sams <ns@vivid-planet.com>
2018-04-12 13:55:42 +0200

    Implement ApiContent for more components

---
+++ Kwc/Basic/Text/ApiContentHtmlParser.php
@@ -0,0 +1,94 @@
+<?php
+class Kwc_Basic_Text_ApiContentHtmlParser
+{
+    protected $_parser;
+    protected $_stack;
+    protected $_childComponents;
+
+
+    public function __construct()
+    {
+    }
+
+    protected function endElement($parser, $element)
+    {
+        array_pop($this->_stack);
+    }
+
+    protected function startElement($parser, $element, $attributes)
+    {
+        $current = $this->_stack[count($this->_stack)-1];
+        if (!isset($current)) $current->children = array();
+        $newBlock = null;
+        if (strtolower($element) == 'a') {
+            if (isset($this->_childComponents[$attributes['HREF']])) {
+                $newBlock['component'] = $this->_childComponents[$attributes['HREF']];
+            } else {
+                $newBlock = null;
+            }
+        } else {
+            $newBlock = array(
+                'element' => strtolower($element),
+            );
+        }
+
+        if ($newBlock) {
+            $newBlock = (object)$newBlock;
+            $current->children[] = $newBlock;
+        }
+        array_push($this->_stack, $newBlock);
+    }
+
+    protected function characterData($parser, $cdata)
+    {
+        $current = $this->_stack[count($this->_stack)-1];
+
+
+        if ($current) {
+            if (!isset($current)) $current->children = array();
+            $current->children[] = (object)array(
+                'text' => $cdata
+            );
+        }
+    }
+
+    public function parse(Kwf_Component_Data $data)
+    {
+        $row = $data->getComponent()->getRow();
+        $html = $row->content;
+        foreach ($data->getChildComponents() as $c) {
+            $this->_childComponents[$c->dbId] = $c;
+        }
+
+        $this->_stack = array(
+            (object)array(
+                'element' => 'html',
+            )
+        );
+        $this->_parser = xml_parser_create();
+
+        xml_set_object($this->_parser, $this);
+
+        xml_set_element_handler(
+            $this->_parser,
+            'startElement',
+            'endElement'
+        );
+
+        xml_set_character_data_handler(
+            $this->_parser,
+            'characterData'
+        );
+
+        xml_set_default_handler(
+            $this->_parser,
+            'characterData'
+        );
+
+        xml_parse($this->_parser,
+            "<BODY>".$html."</BODY>",
+            true);
+
+        return $this->_stack[0]->children[0]->children;
+    }
+}
