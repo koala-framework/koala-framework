04a80dd7415bd3e7a862b17c39b033f8a701bfa7
Alexander Kaufmann <ak@vivid-planet.com>
2018-04-16 09:36:48 +0200

    symfony exceptionHandler catches now httpexceptions and returns this as valid responses and not as 500er exception
    is useful for apis when you are working with special 400er errors

--- KwfBundle/EventListener/ExceptionHandler.php
+++ KwfBundle/EventListener/ExceptionHandler.php
@@ -2,13 +2,33 @@
 namespace KwfBundle\EventListener;
 
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ExceptionHandler
 {
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
         if ($event->isPropagationStopped()) return;
-        if ($event->getException() instanceof \Exception) {
+
+        $exception = $event->getException();
+        if ($exception instanceof HttpExceptionInterface) {
+            if (in_array('application/json', $event->getRequest()->getAcceptableContentTypes())) {
+                $response = new JsonResponse();
+                if ($message = $exception->getMessage()) {
+                    $response->setData(array(
+                        'message' => $message
+                    ));
+                }
+            } else {
+                $response = new Response();
+                $response->setContent($exception->getMessage());
+            }
+
+            $response->setStatusCode($exception->getStatusCode());
+            $event->setResponse($response);
+        } else if ($exception instanceof \Exception) {
             $event->stopPropagation();
             \Kwf_Debug::handleException($event->getException());
             exit;
