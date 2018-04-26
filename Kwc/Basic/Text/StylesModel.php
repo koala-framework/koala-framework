c8c1f4722ddc5ef5e78ebb2304e31e8f53e535a4
Alexander Kaufmann <ak@vivid-planet.com>
2018-04-26 08:33:14 +0200

    fixed parsing rte styles in production, webpack build minimize kicks every unneeded whitespaces

--- Kwc/Basic/Text/StylesModel.php
+++ Kwc/Basic/Text/StylesModel.php
@@ -23,18 +23,21 @@ class Kwc_Basic_Text_StylesModel extends Kwf_Model_Db_Proxy
         $styles = array();
         $up = Kwf_Config::getValue('application.uniquePrefix');
         if ($up) $up .= '-';
+
         if (strpos($masterContent, ".{$up}webStandard")===false) return $styles;
         $up = str_replace('-', '\-', $up);
-        preg_match_all("#\.{$up}webStandard\s+(span|p|h[1-6])(\.(.+))?\s+{([^}]+)}\s*/\*(.*)\*/#mU", $masterContent, $m);
+        preg_match_all("#\.{$up}webStandard\s+(span|p|h[1-6])(\.(.+))?(\s+)?{([^}]+)}\s*\/\*(.*)\*\/#mU", $masterContent, $m);
+
         foreach (array_keys($m[1]) as $i) {
             $styles[] = array(
                 'id' => 'master'.$i,
-                'name' => trim($m[5][$i]),
+                'name' => trim($m[6][$i]),
                 'tagName' => $m[1][$i],
                 'className' => $m[3][$i],
-                'styles' => $m[4][$i],
+                'styles' => $m[5][$i],
             );
         }
+
         return $styles;
     }
 
