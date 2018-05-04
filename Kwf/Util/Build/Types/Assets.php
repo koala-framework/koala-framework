0fb4753061244fdd045c05f9b104eaab6c71aa5a
Ricky Smith <rs@vivid-planet.com>
2018-05-04 10:17:07 +0200

    Remove the https parameter from the build command
    This is only required for dev-builds.

--- Kwf/Util/Build/Types/Assets.php
+++ Kwf/Util/Build/Types/Assets.php
@@ -74,9 +74,6 @@ class Kwf_Util_Build_Types_Assets extends Kwf_Util_Build_Types_Abstract
     {
         $cmd = 'NODE_PATH=vendor/koala-framework/koala-framework/node_modules_build ./vendor/bin/node  node_modules/.bin/webpack --colors';
         if (!isset($_SERVER['NO_PROGRESS'])) $cmd .= ' --progress';
-        if (Kwf_Config::getValue('server.https')) {
-            $cmd .= ' --https';
-        }
         passthru($cmd, $retVal);
         if ($retVal) {
             throw new Kwf_Exception("webpack failed");
