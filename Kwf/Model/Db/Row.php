koala-framework/koala-framework

commit 59a49cbd7e5b940e087556503ef1a7b416a5ad3c

Save added row-object in model earlier
Problem was when in an child-row-after-insert ->getParentRow() was used the row-object
was created newly (e.g. Kwc_NewsletterCategory_Subscribe_SubscriberToCategoryRow

Kwf/Model/Db/Row.php
@@ -87,13 +87,13 @@ class Kwf_Model_Db_Row extends Kwf_Model_Row_Abstract
         $this->_beforeSave();
         if ($insert || $this->_isDirty()) {
             $ret = $this->_row->save();
+            $this->_model->afterInsert($this);
         } else {
             $ret = $this->{$this->_getPrimaryKey()};
         }
         parent::_saveWithoutResetDirty(); //siblings nach uns speichern; damit auto-inc id vorhanden
         if ($insert) {
             $this->_afterInsert();
-            $this->_model->afterInsert($this);
         } else {
             $this->_afterUpdate();
         }

commit f914999a9c1b068f6e76aeb3029d8cc0d6889b48

Execute afterInsert only when inserting
Fixes Bug introduced with 59a49cbd7e5b940e087556503ef1a7b416a5ad3c

Kwf/Model/Db/Row.php
@@ -87,9 +87,11 @@ class Kwf_Model_Db_Row extends Kwf_Model_Row_Abstract
         $this->_beforeSave();
         if ($insert || $this->_isDirty()) {
             $ret = $this->_row->save();
-            $this->_model->afterInsert($this);
         } else {
             $ret = $this->{$this->_getPrimaryKey()};
+        }
+        if ($insert) {
+            $this->_model->afterInsert($this);
         }
         parent::_saveWithoutResetDirty(); //siblings nach uns speichern; damit auto-inc id vorhanden
         if ($insert) {