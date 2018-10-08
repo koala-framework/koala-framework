d7c81d86b72ba9e96933dd3ad40f589e479940d4
Franz Unger <ufx@vivid-planet.com>
2018-10-08 14:33:10 +0200

    Use own div for integrator-template js
    Hijacking the body for using onReady didn't work properly

--- Kwc/Advanced/IntegratorTemplate/Embed/Component.php
+++ Kwc/Advanced/IntegratorTemplate/Embed/Component.php
@@ -21,6 +21,7 @@ class Kwc_Advanced_IntegratorTemplate_Embed_Component extends Kwc_Abstract_Compo
     {
         $ret = parent::getTemplateVars($renderer);
         $ret['includeCode'] = $this->getIncludeCode('content');
+        $ret['baseUrl'] = $this->getData()->getDomainComponent()->getAbsoluteUrl();
         return $ret;
     }
 
--- Kwc/Advanced/IntegratorTemplate/Embed/ContentSender.php
+++ Kwc/Advanced/IntegratorTemplate/Embed/ContentSender.php
@@ -7,10 +7,6 @@ class Kwc_Advanced_IntegratorTemplate_Embed_ContentSender extends Kwf_Component_
         $domain = 'http'.(isset($_SERVER['HTTPS']) ? 's' : '').'://'.$_SERVER['HTTP_HOST'];
         $ret = parent::_render($includeMaster, $hasDynamicParts);
         $ret = preg_replace('#(href|src|action)=(["\'])(/[^/])#', '$1=$2' . $domain . '$3', $ret);
-        $up = Kwf_Config::getValue('uniquePrefix');
-        $up = $up ? $up.'-' : '';
-        $class = str_replace('kwfUp-', $up, Kwf_Component_Abstract::formatRootElementClass($this->_data->componentClass, '').'Master');
-        $ret = preg_replace('#<body class="([^"]+)"#', '<body class="\\1 '.$class.'" data-'.$up.'base-url="'.$domain.'" ', $ret);
         $ret = preg_replace('#<!-- postRenderPlugin.*?-->#s', '', $ret);
         return $ret;
     }
