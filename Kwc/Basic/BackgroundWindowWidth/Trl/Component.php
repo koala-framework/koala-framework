000f47e531053973ed2899a1493a1c482569af39
mt@vivid-planet.com <mt@vivid-planet.com>
2018-11-09 11:14:00 +0100

    getSettings function in backgroundWindowWidth was not compatible updated for 5.1

--- Kwc/Basic/BackgroundWindowWidth/Trl/Component.php
+++ Kwc/Basic/BackgroundWindowWidth/Trl/Component.php
@@ -1,7 +1,7 @@
 <?php
 class Kwc_Basic_BackgroundWindowWidth_Trl_Component extends Kwc_Abstract_Composite_Trl_Component
 {
-    public static function getSettings($masterComponentClass)
+    public static function getSettings($masterComponentClass = null)
     {
         $ret = parent::getSettings($masterComponentClass);
         $ret['throwContentChangedOnOwnMasterModelUpdate'] = true;
