7ec643d707a35a1614e0d09d8f750b3e41a27875
Benjamin Hohenwarter <bh@vivid-planet.com>
2018-11-30 08:52:49 +0100

    Add ignoreVisible for getting directory before content-changed event,
    because changing to invisible directory does not fire content-change

--- Kwc/Directories/TopChoose/Events.php
+++ Kwc/Directories/TopChoose/Events.php
@@ -6,7 +6,7 @@ class Kwc_Directories_TopChoose_Events extends Kwc_Abstract_Events
         parent::_onOwnRowUpdate($c, $event);
         if ($event->isDirty('directory_component_id')) {
             $subroots = Kwf_Component_Data_Root::getInstance()
-                ->getComponentsByDbId($event->row->directory_component_id);
+                ->getComponentsByDbId($event->row->directory_component_id, array('ignoreVisible'=>true));
             foreach ($subroots as $subroot) {
                 $this->fireEvent(new Kwc_Directories_List_EventDirectoryChanged($this->_class, $subroot));
             }
