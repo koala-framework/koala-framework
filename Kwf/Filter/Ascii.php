e31c508c6eee9383b0d884aa5ca9871434e531a6
Alexander Kaufmann <ak@vivid-planet.com>
2019-01-09 08:37:29 +0100

    add length option for ascii filter

--- Kwf/Filter/Ascii.php
+++ Kwf/Filter/Ascii.php
@@ -6,10 +6,20 @@
  */
 class Kwf_Filter_Ascii implements Zend_Filter_Interface
 {
+    /**
+     * @var int
+     */
+    private $_length;
+
+    public function __construct($length = 60)
+    {
+        $this->_length = $length;
+    }
+
     public function filter($value)
     {
         URLify::$remove_list = array();
-        $value = URLify::filter($value, 60, 'de');
+        $value = URLify::filter($value, $this->_length, 'de');
         $value = str_replace('-', '_', $value);
         return $value;
     }
