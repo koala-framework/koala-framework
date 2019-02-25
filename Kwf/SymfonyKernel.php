d1d7b90b407119c9638ab04014cfe9700c0e1425
Simon Radner <sr@vivid-planet.com>
2019-02-25 15:21:17 +0100

    Suport existence of parameters.local.yml

--- Kwf/SymfonyKernel.php
+++ Kwf/SymfonyKernel.php
@@ -9,7 +9,7 @@ abstract class Kwf_SymfonyKernel extends Kernel
     {
         $environment = (Kwf_Config::getValue('symfony.environment.name')) ? Kwf_Config::getValue('symfony.environment.name') : 'prod';
         $debug = (Kwf_Config::getValue('symfony.environment.debug')) ? Kwf_Config::getValue('symfony.environment.debug') : false;
-        
+
         parent::__construct($environment, $debug);
 
         AnnotationRegistry::registerLoader(array('Kwf_Loader', 'loadClass'));
@@ -33,5 +33,9 @@ abstract class Kwf_SymfonyKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load("{$this->getRootDir()}/config/config_{$this->getEnvironment()}.yml");
+        $parametersLocal = $this->getRootDir() . '/config/parameters.local.yml';
+        if (file_exists($parametersLocal) && is_readable($parametersLocal)) {
+            $loader->load($parametersLocal); // local parameters always win
+        }
     }
 }
