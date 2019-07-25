koala-framework/koala-framework

commit 31373e09be45dac5a0c928c79800bb49f56578b2

Use Data-Class for Button Column
This way it's easier to implement custom behaviour

Kwc/Directories/Item/Directory/Controller.php
@@ -65,7 +65,8 @@ class Kwc_Directories_Item_Directory_Controller extends Kwf_Controller_Action_Au
                 $name = Kwf_Trl::getInstance()->trlStaticExecute(Kwc_Abstract::getSetting($ec['componentClass'], 'componentName'));
                 $icon = Kwc_Abstract::getSetting($ec['componentClass'], 'componentIcon');
                 $icon = new Kwf_Asset($icon);
-                $this->_columns->add(new Kwc_Directories_Item_Directory_ControllerEditButton('edit_'.$i, ' ', 20))
+                $this->_columns->add(new Kwf_Grid_Column_Button('edit_'.$i, ' ', 20))
+                    ->setData(new Kwc_Directories_Item_Directory_ControllerEditButtonData())
                     ->setColumnType('editContent')
                     ->setEditComponentClass($ec['componentClass'])
                     ->setEditComponent($ec['component'])

Kwc/Directories/Item/Directory/ControllerEditButtonData.php
@@ -1,0 +1,13 @@
+<?php
+class Kwc_Directories_Item_Directory_ControllerEditButtonData extends Kwf_Data_Button
+{
+    public function load($row, array $info = array())
+    {
+        if ($row->getModel()->hasColumn('component') &&
+            $this->getEditComponent() != $row->component
+        ) {
+            return Kwf_Grid_Column_Button::BUTTON_INVISIBLE;
+        }
+        return parent::load($row, $info);
+    }
+}

Kwc/Directories/Item/Directory/ControllerEditButton.php
@@ -1,13 +1,0 @@
-<?php
-class Kwc_Directories_Item_Directory_ControllerEditButton extends Kwf_Grid_Column_Button
-{
-    public function load($row, $role, $info)
-    {
-        if ($row->getModel()->hasColumn('component') &&
-            $this->getEditComponent() != $row->component
-        ) {
-            return self::BUTTON_INVISIBLE;
-        }
-        return parent::load($row, $role, $info);
-    }
-}

Kwf/Data/Button.php
@@ -1,0 +1,8 @@
+<?php
+class Kwf_Data_Button extends Kwf_Data_Empty
+{
+    public function load($row, array $info = array())
+    {
+        return Kwf_Grid_Column_Button::BUTTON_VISIBLE;
+    }
+}

Kwf/Grid/Column/Button.php
@@ -11,11 +11,6 @@ class Kwf_Grid_Column_Button extends Kwf_Grid_Column
         $this->setRenderer('cellButton');
         $this->setSortable(false);
         $this->setColumnType('button');
-        $this->setData(new Kwf_Data_Empty());
-    }
-
-    public function load($row, $role, $info)
-    {
-        return self::BUTTON_VISIBLE;
+        $this->setData(new Kwf_Data_Button());
     }
 }

commit aea318768c55b759802a6e4fcb2bed05347d767a

Fix missing editcomponent in button data
Error introduced in 31373e09be45dac5a0c928c79800bb49f56578b2

Kwc/Directories/Item/Directory/Controller.php
@@ -65,8 +65,10 @@ class Kwc_Directories_Item_Directory_Controller extends Kwf_Controller_Action_Au
                 $name = Kwf_Trl::getInstance()->trlStaticExecute(Kwc_Abstract::getSetting($ec['componentClass'], 'componentName'));
                 $icon = Kwc_Abstract::getSetting($ec['componentClass'], 'componentIcon');
                 $icon = new Kwf_Asset($icon);
+                $data = new Kwc_Directories_Item_Directory_ControllerEditButtonData();
+                $data->setEditComponent($ec['component']);
                 $this->_columns->add(new Kwf_Grid_Column_Button('edit_'.$i, ' ', 20))
-                    ->setData(new Kwc_Directories_Item_Directory_ControllerEditButtonData())
+                    ->setData($data)
                     ->setColumnType('editContent')
                     ->setEditComponentClass($ec['componentClass'])
                     ->setEditComponent($ec['component'])

Kwc/Directories/Item/Directory/ControllerEditButtonData.php
@@ -1,10 +1,17 @@
 <?php
 class Kwc_Directories_Item_Directory_ControllerEditButtonData extends Kwf_Data_Button
 {
+    private $_editComponent;
+
+    public function setEditComponent($editComponent)
+    {
+        $this->_editComponent = $editComponent;
+    }
+
     public function load($row, array $info = array())
     {
         if ($row->getModel()->hasColumn('component') &&
-            $this->getEditComponent() != $row->component
+            $this->_editComponent != $row->component
         ) {
             return Kwf_Grid_Column_Button::BUTTON_INVISIBLE;
         }