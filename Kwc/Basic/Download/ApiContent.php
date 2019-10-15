448d221618b6e5722eaa48d399057a2b8448db64
Benjamin Hohenwarter <bh@vivid-planet.com>
2019-10-15 15:10:47 +0200

    Implement content-api for downloads-list

---
+++ Kwc/Basic/Download/ApiContent.php
@@ -0,0 +1,11 @@
+<?php
+class Kwc_Basic_Download_ApiContent implements Kwf_Component_ApiContent_Interface
+{
+    public function getContent(Kwf_Component_Data $data)
+    {
+        return array(
+            'infotext' => $data->getComponent()->getRow()->infotext,
+            'download' => $data->getChildComponent('-downloadTag')
+        );
+    }
+}
