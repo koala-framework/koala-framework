e9d8f8e81f96cadca15070c15bb407f62df8b02c
Benjamin Hohenwarter <bh@vivid-planet.com>
2019-11-14 08:12:26 +0100

    ContentApi: Add forgotten show-more-link-text value -.-'

--- Kwc/List/Gallery/ApiContent.php
+++ Kwc/List/Gallery/ApiContent.php
@@ -8,6 +8,7 @@ class Kwc_List_Gallery_ApiContent extends Kwc_Abstract_List_ApiContent
         $ret['columns'] = $row->columns;
         if (Kwc_Abstract::getSetting($data->componentClass, 'showMoreLink')) {
             $ret['showPics'] = $row->show_pics;
+            $ret['showMoreLinkText'] = $row->show_more_link_text;
         }
         return $ret;
     }
