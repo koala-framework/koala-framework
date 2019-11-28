7b559a71351db45536614d275e809274b96a75c1
Benjamin Hohenwarter <bh@vivid-planet.com>
2019-11-28 13:36:31 +0100

    ContentApi: Handle empty image data

--- Kwc/Basic/Image/ApiContent.php
+++ Kwc/Basic/Image/ApiContent.php
@@ -3,6 +3,9 @@ class Kwc_Basic_Image_ApiContent implements Kwf_Component_ApiContent_Interface
 {
     public function getContent(Kwf_Component_Data $data)
     {
+        if (!$data->hasContent()) {
+            return new stdClass();
+        }
         return $data->getComponent()->getApiData();
     }
 }
