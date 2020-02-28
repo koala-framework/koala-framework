701bdaf22b34ce0bcf46a2735c08330c9931b55f
Benjamin Hohenwarter <bh@vivid-planet.com>
2020-02-28 10:21:35 +0100

    MenuApi: Fix link-intern on invisible page

--- Kwc/Basic/LinkTag/Intern/ApiContent.php
+++ Kwc/Basic/LinkTag/Intern/ApiContent.php
@@ -14,6 +14,7 @@ class Kwc_Basic_LinkTag_Intern_ApiContent implements Kwf_Component_ApiContent_In
 
     public function getTargetLinkContent($targetData)
     {
+        if (!$targetData) return array();
         $targetContentType = Kwc_Abstract::getSetting($targetData->componentClass, 'apiContentType');
         if (is_instance_of (Kwc_Abstract::getSetting($targetData->componentClass, 'contentSender'), 'Kwf_Component_Abstract_ContentSender_Lightbox')) {
             $targetContentType = 'lightbox';
