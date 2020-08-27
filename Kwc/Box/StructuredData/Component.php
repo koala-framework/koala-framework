f1b8207ea2d288aee7aa3d962d2833da9cb03ad5
Michael Tiefenthaler <mt@vivid-planet.com>
2020-08-27 15:29:31 +0200

    Add structured data

---
+++ Kwc/Box/StructuredData/Component.php
@@ -0,0 +1,34 @@
+<?php
+class Kwc_Box_StructuredData_Component extends Kwc_Abstract_Composite_Component
+{
+    //JSON validate regex
+    const REGEX = '
+      /
+      (?(DEFINE)
+         (?<number>   -? (?= [1-9]|0(?!\d) ) \d+ (\.\d+)? ([eE] [+-]? \d+)? )    
+         (?<boolean>   true | false | null )
+         (?<string>    " ([^"\\\\]* | \\\\ ["\\\\bfnrt\/] | \\\\ u [0-9a-f]{4} )* " )
+         (?<array>     \[  (?:  (?&json)  (?: , (?&json)  )*  )?  \s* \] )
+         (?<pair>      \s* (?&string) \s* : (?&json)  )
+         (?<object>    \{  (?:  (?&pair)  (?: , (?&pair)  )*  )?  \s* \} )
+         (?<json>   \s* (?: (?&number) | (?&boolean) | (?&string) | (?&array) | (?&object) ) \s* )
+      )
+      \A (?&json) \Z
+      /six   
+    ';
+
+    public static function getSettings($param = null)
+    {
+        $ret = parent::getSettings($param);
+        $ret['componentName'] = trlKwfStatic('Structured Data');
+        $ret['flags']['hasHeaderIncludeCode'] = true;
+        $ret['componentIcon'] = 'tag';
+        $ret['ownModel'] = 'Kwc_Box_StructuredData_Model';
+        return $ret;
+    }
+
+    public function getIncludeCode()
+    {
+        return '<script type="application/ld+json">' . $this->getRow()->content . '</script>';
+    }
+}
