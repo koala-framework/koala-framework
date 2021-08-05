2f35434d6d69a8ebfebd4202a3a8ed860aa3bc56
Vanessa Winklbauer <vanessa.winklbauer@vivid-planet.com>
2021-08-05 10:28:21 +0200

    Add target settings for download link

--- Kwc/Basic/DownloadTag/Data.php
+++ Kwc/Basic/DownloadTag/Data.php
@@ -23,15 +23,51 @@ class Kwc_Basic_DownloadTag_Data extends Kwf_Component_Data
         }
     }
 
+    private $_linkRow;
+    private function _getLinkRow()
+    {
+        if (!isset($this->_linkRow)) {
+            $m = Kwc_Abstract::createOwnModel($this->componentClass);
+            $cols = array('open_type', 'width', 'height', 'menubar', 'toolbar', 'locationbar', 'statusbar', 'scrollbars', 'resizable');
+            $this->_linkRow = (object)$m->fetchColumnsByPrimaryId($cols, $this->dbId);
+        }
+        return $this->_linkRow;
+    }
+
+
     public function getAbsoluteUrl()
     {
         return $this->url;
     }
 
+    //this is the copy from Kwc_Basic_LinkTag_Extern_Data
     public function getLinkDataAttributes()
     {
         $ret = parent::getLinkDataAttributes();
-        $ret['kwc-popup'] = 'blank';
+        if (!Kwc_Abstract::getSetting($this->componentClass, 'hasPopup')) {
+            $type = Kwc_Abstract::getSetting($this->componentClass, 'openType');
+            if ($type == 'blank') {
+                $ret['kwc-popup'] = 'blank';
+            } else {
+                throw new Kwf_Exception_NotYetImplemented();
+            }
+        }
+        $row = $this->_getLinkRow();
+        if (!isset($row->open_type) || !$row->open_type) return '';
+        if ($row->open_type == 'popup') {
+            $pop = array();
+            if ($row->width) $pop[] = 'width='.$row->width;
+            if ($row->height) $pop[] = 'height='.$row->height;
+            $pop[] = 'menubar='.($row->menubar ? 'yes' : 'no');
+            $pop[] = 'toolbar='.($row->toolbar ? 'yes' : 'no');
+            $pop[] = 'location='.($row->locationbar ? 'yes' : 'no');
+            $pop[] = 'status='.($row->statusbar ? 'yes' : 'no');
+            $pop[] = 'scrollbars='.($row->scrollbars ? 'yes' : 'no');
+            $pop[] = 'resizable='.($row->resizable ? 'yes' : 'no');
+            $ret['kwc-popup'] = implode(',', $pop);
+        } else if ($row->open_type == 'blank') {
+            $ret['kwc-popup'] = 'blank';
+        }
         return $ret;
     }
 
