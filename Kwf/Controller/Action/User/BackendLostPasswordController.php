ee2e59ef095f1c9d0f983d72ebc1150b6762b179
Georg Schreglmann <georg.schreglmann@vivid-planet.com>
2021-08-13 08:10:14 +0200

    Block lost password, if blockExternalAdminAccess is set

--- Kwf/Controller/Action/User/BackendLostPasswordController.php
+++ Kwf/Controller/Action/User/BackendLostPasswordController.php
@@ -5,6 +5,31 @@ class Kwf_Controller_Action_User_BackendLostPasswordController extends Kwf_Contr
 
     public function preDispatch()
     {
+        if (Kwf_Config::getValue('blockExternalAdminAccess')) {
+            $currentIp = $_SERVER['REMOTE_ADDR'];
+            $valid = false;
+            foreach (Kwf_Config::getValueArray('allowedAdminIPs') as $ip) {
+                if (substr($ip, -1)=='*') {
+                    $i = substr($ip, 0, -1);
+                    if (substr($currentIp, 0, strlen($i)) == $i){
+                        $valid = true;
+                    }
+                } else if (substr($ip, 0, 1)=='*') {
+                    $i = substr($ip, 1);
+                    if (substr($currentIp, -strlen($i)) == $i){
+                        $valid = true;
+                    }
+                } else {
+                    if ($currentIp == $ip){
+                        $valid = true;
+                    }
+                }
+            }
+            if (!$valid) {
+                throw new Kwf_Exception_AccessDenied();
+            }
+        }
+
         $this->getHelper('viewRenderer')->setNoController(true);
         $this->getHelper('viewRenderer')->setViewScriptPathNoControllerSpec('user/:action.:suffix');
         parent::preDispatch();
