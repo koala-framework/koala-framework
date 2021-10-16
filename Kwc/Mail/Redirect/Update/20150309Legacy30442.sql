e62940bf0926d6d46342628de697c039ad2e1443
Alexander Kaufmann <kaufmann.alexander@gmx.at>
2021-10-16 14:39:22 +0200

    fix: sql error, add missing comma

--- Kwc/Mail/Redirect/Update/20150309Legacy30442.sql
+++ Kwc/Mail/Redirect/Update/20150309Legacy30442.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `kwc_mail_redirect` (
   `id` int(10) unsigned NOT NULL auto_increment,
-  `value` varchar(255) NOT NULL
+  `value` varchar(255) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB;
 ALTER TABLE `kwc_mail_redirect` ADD INDEX ( `value` );
