koala-framework/koala-framework

commit 1af75ca18bfc01c2444b05cf4838c1b1ac7372fb

Fix cache-issues with menu-cc component
changing page-title did not clear cache of cc-menu components

Kwc/Menu/Cc/Events.php
@@ -1,0 +1,4 @@
+<?php
+class Kwc_Menu_Cc_Events extends Kwc_Menu_Events
+{
+}

commit d4b4384242afba22e0e2a0cab089ba56d3dc4a80

Fix bugs introduced with 1af75ca18bfc01c2444b05cf4838c1b1ac7372fb
Just do the same with Cc like Trl does for every menu component

Kwc/Menu/Cc/Events.php
@@ -1,4 +1,10 @@
 <?php
 class Kwc_Menu_Cc_Events extends Kwc_Menu_Events
 {
+    protected function _initSettings()
+    {
+        $mcc = Kwc_Abstract::getSetting($this->_class, 'masterComponentClass');
+        $this->_level = $menuLevel = Kwc_Abstract::getSetting($mcc, 'level');
+        $this->_emptyIfSingleEntry = $menuLevel = Kwc_Abstract::getSetting($mcc, 'emptyIfSingleEntry');
+    }
 }

Kwc/Menu/OtherCategory/Cc/Events.php
@@ -1,0 +1,11 @@
+<?php
+class Kwc_Menu_OtherCategory_Cc_Events extends Kwc_Menu_OtherCategory_Events
+{
+    protected function _initSettings()
+    {
+        $masterComponentClass = Kwc_Abstract::getSetting($this->_class, 'masterComponentClass');
+        $masterMenuComponentClass = Kwc_Abstract::getSetting($masterComponentClass, 'menuComponentClass');
+        $trlMenuComponentClass = Kwc_Chained_Abstract_Component::getChainedComponentClass($masterMenuComponentClass, 'Cc');
+        $this->_menuComponentClass = $trlMenuComponentClass;
+    }
+}

Kwc/Menu/ParentMenu/Cc/Events.php
@@ -1,0 +1,11 @@
+<?php
+class Kwc_Menu_ParentMenu_Cc_Events extends Kwc_Menu_ParentMenu_Events
+{
+    protected function _initSettings()
+    {
+        $masterComponentClass = Kwc_Abstract::getSetting($this->_class, 'masterComponentClass');
+        $masterMenuComponentClass = Kwc_Abstract::getSetting($masterComponentClass, 'menuComponentClass');
+        $trlMenuComponentClass = Kwc_Chained_Abstract_Component::getChainedComponentClass($masterMenuComponentClass, 'Cc');
+        $this->_menuComponentClass = $trlMenuComponentClass;
+    }
+}